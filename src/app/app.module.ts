4306432884f6829685d285a461665a0fac4b90df
Jeffarese <jeff@wallapop.com>
2017-06-22 12:25:12 +0200

    [ADM-2403] Remove windowref provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { LoginModule } from './login/login.module';
     HttpModule,
     AppRoutingModule
   ],
-  providers: [WindowRef],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
