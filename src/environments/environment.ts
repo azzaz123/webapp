ad74f1f042fa1f0927fb56b77a24b312cb16d1c7
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2017-06-27 18:10:57 +0200

    [ADM-2459] Add environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
-export const environment = {
+export const environment: any = {
   production: false,
-  loginUrl: 'http://es.dev.wallapop.com:8080/login'
+  name: 'local',
+  baseUrl: 'http://dock7.corp.wallapop.com:8080/',
+  xmppDomain: 'dock7.wallapop.com',
+  wsUrl: 'ws://dock7.corp.wallapop.com:5281/ws-xmpp',
+  loginUrl: 'http://es.dev.wallapop.com:8080/login',
+  bypass: 'p3-9p0dJk2cHp3-4RsW0'
 };
+
