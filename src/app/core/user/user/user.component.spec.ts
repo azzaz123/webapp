50bbf9936542011fc3fc7544b5b377668905f8f3
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2017-06-28 16:36:54 +0200

    [ADM-2464] Remove getLatest item from user component

--- src/app/core/user/user/user.component.spec.ts
+++ src/app/core/user/user/user.component.spec.ts
@@ -1,10 +1,9 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UserComponent } from './user.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { MOCK_USER, USER_ID, MOCK_ITEM, LATEST_ITEM_COUNT, ItemService } from 'shield';
-import { Observable } from 'rxjs/Observable';
+import { ItemService, MOCK_USER } from 'shield';
 
 describe('Component: User', () => {
 
@@ -15,36 +14,11 @@ describe('Component: User', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [UserComponent],
-      providers: [
-        {
-          provide: ItemService, useValue: {
-          getLatest() {
-          }
-        }
-        }
-      ],
       schemas: [NO_ERRORS_SCHEMA]
     });
     fixture = TestBed.createComponent(UserComponent);
     component = TestBed.createComponent(UserComponent).componentInstance;
     itemService = TestBed.get(ItemService);
     component.user = MOCK_USER;
-    spyOn(itemService, 'getLatest').and.returnValue(Observable.of({
-      data: MOCK_ITEM,
-      count: LATEST_ITEM_COUNT
-    }));
-  });
-
-  it('should get the latest selling item', () => {
-    component.ngOnChanges();
-    expect(itemService.getLatest).toHaveBeenCalledWith(USER_ID);
-    expect(component.user.sellingItem).toBe(MOCK_ITEM);
-    expect(component.user.itemsCount).toBe(LATEST_ITEM_COUNT);
-  });
-
-  it('should not get the item if already loaded', () => {
-    component.user.sellingItem = MOCK_ITEM;
-    component.ngOnChanges();
-    expect(itemService.getLatest).not.toHaveBeenCalled();
   });
 });
--- src/app/core/user/user/user.component.ts
+++ src/app/core/user/user/user.component.ts
@@ -1,39 +1,18 @@
-import { Component, Input, OnChanges, OnDestroy } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { User } from 'shield';
-import { ItemService, ItemDataResponse } from 'shield';
 
 @Component({
   selector: 'tsl-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
-export class UserComponent implements OnChanges, OnDestroy {
+export class UserComponent {
 
   @Input() user: User;
   @Input() phone: string;
   @Input() callsPanel: boolean;
-  private active = true;
 
-  constructor(private itemService: ItemService) {
-  }
-
-  ngOnChanges(changes?: any) {
-    if (!this.user.sellingItem && !this.callsPanel) {
-      this.itemService.getLatest(this.user.id).takeWhile(() => {
-        return this.active;
-      }).subscribe((res: ItemDataResponse) => {
-        this.user.sellingItem = res.data;
-        // this.user.sellingItem = new Item(res.data.id, res.data.legacyId, res.data.owner, res.data.title,
-        //   res.data.description, res.data.categoryId, res.data.location, res.data.salePrice, res.data.currencyCode,
-        //   res.data.modifiedDate, res.data.url, res.data.flags, res.data.actionsAllowed, res.data.saleConditions,
-        //   res.data.mainImage, res.data.images, res.data.webLink );
-        this.user.itemsCount = res.count;
-      });
-    }
-  }
-
-  ngOnDestroy() {
-    this.active = false;
+  constructor() {
   }
 
 }
