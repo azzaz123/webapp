aec1f607af2c3a8499b4d68706b8117be966ebd3
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2017-07-03 16:11:04 +0200

    [ADM-2464] Remove unused select component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StarsComponent } from './stars/stars.component';
 import { SanitizedBackgroundDirective } from './sanitized-background/sanitized-background.directive';
-import { SelectComponent } from './select/select.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SpinnerComponent } from './spinner/spinner.component';
 import { MdIconModule } from '@angular/material';
@@ -17,13 +16,11 @@ import { MdIconModule } from '@angular/material';
     CommonModule,
     StarsComponent,
     SanitizedBackgroundDirective,
-    SelectComponent,
     SpinnerComponent
   ],
   declarations: [
     StarsComponent,
     SanitizedBackgroundDirective,
-    SelectComponent,
     SpinnerComponent
   ]
 })
