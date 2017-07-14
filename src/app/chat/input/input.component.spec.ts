879f2a3cd48a7b476147ca79f00dc5685c316866
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2017-07-14 13:49:41 +0200

    [ADM-2513] Add tests

--- src/app/chat/input/input.component.spec.ts
+++ src/app/chat/input/input.component.spec.ts
@@ -2,7 +2,7 @@
 
 import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { InputComponent } from './input.component';
-import { MessageService, MOCK_CONVERSATION, EventService, Conversation } from 'shield';
+import { MessageService, MOCK_CONVERSATION, USER_ID, EventService, Conversation, XmppService } from 'shield';
 import { By } from '@angular/platform-browser';
 
 class MockMessageService {
@@ -15,19 +15,52 @@ describe('Component: Input', () => {
   let component: InputComponent;
   let messageService: MessageService;
   let fixture: ComponentFixture<InputComponent>;
+  let eventService: EventService;
+  let xmppService: XmppService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [InputComponent],
       providers: [
         {provide: MessageService, useClass: MockMessageService},
+        {provide: XmppService, useValue: {
+          isBlocked() {
+          }
+        }},
         EventService
       ]
     });
     fixture = TestBed.createComponent(InputComponent);
     component = TestBed.createComponent(InputComponent).componentInstance;
     messageService = TestBed.get(MessageService);
+    eventService = TestBed.get(EventService);
+    xmppService = TestBed.get(XmppService);
     spyOn(messageService, 'send');
+  });
+
+  describe('ngOnInit', () => {
+    it('should disable input when CONNECTION_ERROR', () => {
+      component.ngOnInit();
+      eventService.emit(EventService.CONNECTION_ERROR);
+      expect(component.disable).toBeTruthy();
+    });
+    it('should disable input when CONNECTION_RESTORED', () => {
+      component.ngOnInit();
+      eventService.emit(EventService.CONNECTION_RESTORED);
+      expect(component.disable).toBeFalsy();
+    });
+    it('should disable input when USER_BLOCKED', () => {
+      component.currentConversation = MOCK_CONVERSATION();
+      component.ngOnInit();
+      eventService.emit(EventService.USER_BLOCKED, USER_ID);
+      expect(component.disable).toBeTruthy();
+    });
+    it('should disable input when USER_UNBLOCKED', () => {
+      component.currentConversation = MOCK_CONVERSATION();
+      component.ngOnInit();
+      eventService.emit(EventService.USER_UNBLOCKED, USER_ID);
+      expect(component.disable).toBeFalsy();
+    });
   });
 
   describe('sendMessage', () => {
@@ -87,6 +120,7 @@ describe('Component: Input', () => {
         }
       };
       spyOn(component.messageArea.nativeElement, 'focus');
+      component.currentConversation = MOCK_CONVERSATION();
     });
 
     it('should focus the message area', fakeAsync(() => {
@@ -98,6 +132,19 @@ describe('Component: Input', () => {
     it('should not do anything if there is no message to read', () => {
       component.messageArea = undefined;
       component.ngOnChanges();
+    });
+
+    it('should disable input if user is blocked', () => {
+      spyOn(xmppService, 'isBlocked').and.returnValue(true);
+      component.ngOnChanges();
+      expect(component.disable).toBeTruthy();
+    });
+
+    it('should enaable input if user is blocked', () => {
+      component.disable = true;
+      spyOn(xmppService, 'isBlocked').and.returnValue(false);
+      component.ngOnChanges();
+      expect(component.disable).toBeFalsy();
     });
 
   });
