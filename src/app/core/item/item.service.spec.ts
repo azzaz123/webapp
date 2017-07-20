731197f571eeaf43d56233b1e56642b57b9369f9
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2017-07-20 17:38:07 +0200

    [ADM-2493] Fix tests

--- src/app/core/item/item.service.spec.ts
+++ src/app/core/item/item.service.spec.ts
@@ -1,11 +1,19 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { TEST_HTTP_PROVIDERS, I18nService, TrackingService, EventService, UserService } from 'shield';
 
 import { ItemService } from './item.service';
 
 describe('ItemService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ItemService]
+      providers: [
+        ItemService,
+        ...TEST_HTTP_PROVIDERS,
+        {provide: I18nService, useValue: {}},
+        {provide: TrackingService, useValue: {}},
+        {provide: EventService, useValue: {}},
+        {provide: UserService, useValue: {}},
+      ]
     });
   });
 
