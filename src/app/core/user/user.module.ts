79c6c1d393e4f791e2f28c4eaea405924a018b0b
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2017-08-16 17:16:47 +0200

    [ADM-2635] Remove user type component

--- src/app/core/user/user.module.ts
+++ src/app/core/user/user.module.ts
@@ -4,7 +4,6 @@ import { UserAvatarComponent } from './user-avatar/user-avatar.component';
 import { ShieldModule, UtilsModule } from 'shield';
 import { SharedModule } from '../../shared/shared.module';
 import { MdIconModule } from '@angular/material';
-import { UserTypeComponent } from './user-type/user-type.component';
 import { ItemModule } from '../item/item.module';
 
 @NgModule({
@@ -15,7 +14,7 @@ import { ItemModule } from '../item/item.module';
     ItemModule,
     UtilsModule
   ],
-  declarations: [UserComponent, UserAvatarComponent, UserTypeComponent ],
-  exports: [UserComponent, UserAvatarComponent, UserTypeComponent]
+  declarations: [UserComponent, UserAvatarComponent ],
+  exports: [UserComponent, UserAvatarComponent]
 })
 export class UserModule { }
