ee0576c10f4a86d5b308c9bc390291975be80722
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2017-08-21 15:14:53 +0200

    [ADM-2517] Fix category service test

--- src/app/core/category/category.service.spec.ts
+++ src/app/core/category/category.service.spec.ts
@@ -4,6 +4,7 @@ import { CategoryService } from './category.service';
 import { Observable } from "rxjs/Observable";
 import { CATEGORY_DATA_WEB } from "../../../tests/category.fixtures";
 import { CategoryResponse } from "./category-response.interface";
+import { ResponseOptions, Response } from '@angular/http';
 let service: CategoryService;
 let http: HttpService;
 
@@ -25,6 +26,8 @@ describe('CategoryService', () => {
     });
     it('should return the json from the categories', () => {
       let response: CategoryResponse[];
+      const res: ResponseOptions = new ResponseOptions({body: JSON.stringify(CATEGORY_DATA_WEB)});
+      spyOn(http, 'getNoBase').and.returnValue(Observable.of(new Response(res)));
       service.getCategories().subscribe((data: CategoryResponse[]) => {
         response = data;
       });
