7320be03105bd7ec10eed3cf7566e54eaa63979b
Jeffarese <jeff@wallapop.com>
2017-08-28 17:24:59 +0200

    [change-backend-urls] Change environment used to import

--- src/app/core/item/item-avatar/item-avatar.component.ts
+++ src/app/core/item/item-avatar/item-avatar.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnChanges } from '@angular/core';
 import { FAKE_ITEM_IMAGE_SMALL_BASE_PATH, FAKE_ITEM_IMAGE_SMALL_LIGHT_BASE_PATH, Item } from 'shield';
-import { environment } from '../../../../environments/environment.prod';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'tsl-item-avatar',
