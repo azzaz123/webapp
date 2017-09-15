1e2bd3e1c0071a2d5d27d9ba8cd25ee071406ca9
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2017-09-15 10:42:00 +0200

    Remove unused imports

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MessageService, UserService } from 'shield';
-import { NgbDropdownConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'tsl-sidebar',
