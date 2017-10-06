9f80d0f9a99c02b64ba410a39e28962140e6b2ed
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2017-10-06 13:49:05 +0200

    [ADM-2774] Add feature button icon

--- src/app/config/icons.config.ts
+++ src/app/config/icons.config.ts
@@ -84,4 +84,5 @@ export function configIcons(mdIconRegistry: MdIconRegistry, sanitizer: DomSaniti
   mdIconRegistry.addSvgIcon('commons-congrats', sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/commons-congrats.svg'));
   mdIconRegistry.addSvgIcon('congrats', sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/congrats.svg'));
   mdIconRegistry.addSvgIcon('items-empty-state', sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/items-empty-state.svg'));
+  mdIconRegistry.addSvgIcon('feature', sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/feature.svg'));
 }
