20f0baf9257ae2382da980ac151d244b26aeebcf
tmirun <guang@wallapop.com>
2017-10-17 14:58:59 +0200

    [ADM-2822] add card.directive test

--- src/app/core/card/card.directive.spec.ts
+++ src/app/core/card/card.directive.spec.ts
@@ -1,8 +1,24 @@
-import { CardDirective } from './card.directive';
+import { CardImageDirective,
+  CardContentDirective,
+  CardFooterDirective } from './card.directive';
 
-describe('CardDirective', () => {
+describe('CardImageDirective', () => {
   it('should create an instance', () => {
-    const directive = new CardDirective();
+    const directive = new CardImageDirective();
+    expect(directive).toBeTruthy();
+  });
+});
+
+describe('CardContentDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CardContentDirective();
+    expect(directive).toBeTruthy();
+  });
+});
+
+describe('CardFooterDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CardFooterDirective();
     expect(directive).toBeTruthy();
   });
 });
