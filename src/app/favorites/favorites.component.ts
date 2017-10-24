1283851b6ba0a11a3cabe699f068683d6039ad13
tmirun <guang@wallapop.com>
2017-10-24 11:00:55 +0200

    [ADM-2822] commit Favorite page for change history

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ItemService } from '../core/item/item.service';
+import { ItemsData } from '../core/item/item-response.interface';
+import { Item } from 'shield';
+
 @Component({
   selector: 'tsl-favorites',
   templateUrl: './favorites.component.html',
@@ -7,9 +11,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoritesComponent implements OnInit {
 
-  constructor() { }
+  // public items: Item[] = [];
+  public selectedStatus: string = 'published';
+  public loading: boolean = true;
+  private init: number = 0;
+  private size: number = 20;
+  public end:boolean = false;
+
+  public masonryOptions = {
+    gutter: 20
+  }
+
+  public items = [
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+    // new Item(null, null, null, 'title fjdsaljfdlsaj djsa lfj slkfj lsaddj l', 'Description', null, null, 999, 'EUR', null, null, null, null, null, null, null, null, null),
+  ];
+
+  constructor(public itemService: ItemService
+  ) { }
 
   ngOnInit() {
+    // this.itemService.mineFavorites(this.init).subscribe((itemsData: ItemsData) => {
+    //   console.log(itemsData)
+    // });
+    this.itemService.mine(this.init, this.selectedStatus).subscribe((itemsData: ItemsData) => {
+      const items = itemsData.data;
+      this.init = itemsData.init;
+      this.items = this.items.concat(items);
+      this.loading = false;
+      this.end = !this.init;
+
+      console.log(this.items);
+    });
+  }
+
+  loadMore() {
+    console.log('load more');
   }
 
 }
--- src/app/favorites/favorites.module.ts
+++ src/app/favorites/favorites.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MdIconModule } from '@angular/material';
 import { CoreModule } from '../core/core.module';
+import { MasonryModule } from 'angular2-masonry';
+import { InfiniteScrollModule } from 'angular2-infinite-scroll';
 
 import { FavoritesRoutingModule, favoritesRoutedComponents } from './favorites.routes';
 import { FavoritesComponent } from './favorites.component';
@@ -9,7 +12,10 @@ import { FavoritesComponent } from './favorites.component';
   imports: [
     CommonModule,
     FavoritesRoutingModule,
-    CoreModule
+    CoreModule,
+    MdIconModule,
+    MasonryModule,
+    InfiniteScrollModule
   ],
   declarations: [
     FavoritesComponent,
