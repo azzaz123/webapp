9b9ad347fc7c1ba50f73f65bbe74719372d49845
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2017-10-27 13:03:20 +0200

    [ADM-2868] Handle error on checkout

--- src/app/catalog/list/list.component.ts
+++ src/app/catalog/list/list.component.ts
@@ -6,7 +6,8 @@ import {
   Item,
   ItemBulkResponse,
   PaymentService,
-  TrackingService
+  TrackingService,
+  DEFAULT_ERROR_MESSAGE
 } from 'shield';
 import { ItemService } from '../../core/item/item.service';
 import { ItemChangeEvent } from './catalog-item/item-change.interface';
@@ -66,7 +67,8 @@ export class ListComponent implements OnInit {
           modalRef.componentInstance.code = params.code;
           modalRef.result.then(() => {
             this.router.navigate(['catalog/list']);
-          }, () => {});
+          }, () => {
+          });
         }
       });
     });
@@ -170,7 +172,12 @@ export class ListComponent implements OnInit {
         this.sabadellSubmit.emit(orderId);
       });
     }, (error: Response) => {
-      this.errorService.show(error);
+      this.deselect();
+      if (error) {
+        this.errorService.show(error);
+      } else {
+        this.toastr.error(DEFAULT_ERROR_MESSAGE);
+      }
     });
   }
 
