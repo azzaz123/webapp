79a2e29fa878f5343d750713cce905d32db64246
tmirun <guang@wallapop.com>
2017-11-03 13:20:02 +0100

    [DIS-227] using shield mock_user

--- src/app/chat/user-detail/user-detail.component.spec.ts
+++ src/app/chat/user-detail/user-detail.component.spec.ts
@@ -4,36 +4,14 @@ import { UserDetailComponent } from './user-detail.component';
 import { CUSTOM_ELEMENTS_SCHEMA, SimpleChange } from '@angular/core';
 import { UserService } from '../../core/user/user.service';
 import { Observable } from 'rxjs/Observable';
-import { USER_DATA, User } from 'shield';
+import { MOCK_USER } from 'shield';
 import { RESPONSE_RATE, SCORING_STARS, USER_INFO_RESPONSE } from '../../../tests/user.fixtures';
 
-const MOCK_USER = new User(
-  USER_DATA.id,
-  USER_DATA.micro_name,
-  USER_DATA.image,
-  USER_DATA.location,
-  USER_DATA.stats,
-  USER_DATA.validations,
-  USER_DATA.verification_level,
-  USER_DATA.scoring_stars,
-  USER_DATA.scoring_starts,
-  USER_DATA.response_rate,
-  USER_DATA.online,
-  USER_DATA.type,
-  USER_DATA.received_reports,
-  USER_DATA.web_slug
-);
-
 class MockUserService {
-
-  public updateBlockStatus() {
-  }
-
 
   getInfo() {
     return Observable.of(USER_INFO_RESPONSE)
   }
-
 }
 
 describe('UserDetailComponent', () => {
