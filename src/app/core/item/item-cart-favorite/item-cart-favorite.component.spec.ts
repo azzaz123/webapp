a4d65cfc7a2bfc615361ec28514e3df823e885c8
tmirun <tmirun@hotmail.com>
2017-11-09 15:51:37 +0100

    [ADM-2822] start building item-cart-favorite component test

--- src/app/core/item/item-cart-favorite/item-cart-favorite.component.spec.ts
+++ src/app/core/item/item-cart-favorite/item-cart-favorite.component.spec.ts
@@ -1,22 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { SharedModule } from '../../../shared/shared.module';
 import { ItemCartFavoriteComponent } from './item-cart-favorite.component';
 import { MdIconModule } from '@angular/material';
 import { ItemService } from '../item.service';
+import { WindowRef } from 'shield';
+
+import {
+  MOCK_ITEM
+} from 'shield';
 
 describe('ItemCartFavoriteComponent', () => {
   let component: ItemCartFavoriteComponent;
   let fixture: ComponentFixture<ItemCartFavoriteComponent>;
   let element: HTMLElement;
 
+  let itemService: ItemService;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [SharedModule, MdIconModule, ItemService],
+      imports: [SharedModule, MdIconModule],
       declarations: [ ItemCartFavoriteComponent ],
-      providers: [{
-        provide: ItemService, useValue: ItemService
-      }],
+      providers: [
+        WindowRef,
+        { provide: ItemService, user: {}},
+        { provide: 'SUBDOMAIN', useValue: 'www'}
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
@@ -25,7 +36,8 @@ describe('ItemCartFavoriteComponent', () => {
     fixture = TestBed.createComponent(ItemCartFavoriteComponent);
     component = fixture.componentInstance;
     element = fixture.nativeElement;
-
+    component.item = MOCK_ITEM;
+    itemService = TestBed.get(ItemService);
     fixture.detectChanges();
   });
 
