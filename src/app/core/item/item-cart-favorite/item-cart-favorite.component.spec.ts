fb2e9f86b96b012a01fad21f7acca568a91d8624
tmirun <tmirun.app@gmail.com>
2017-11-10 10:20:46 +0100

    [ADM-2822] commit for change PC

--- src/app/core/item/item-cart-favorite/item-cart-favorite.component.spec.ts
+++ src/app/core/item/item-cart-favorite/item-cart-favorite.component.spec.ts
@@ -2,10 +2,12 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { SharedModule } from '../../../shared/shared.module';
+import { Observable } from 'rxjs/Observable';
 import { ItemCartFavoriteComponent } from './item-cart-favorite.component';
 import { MdIconModule } from '@angular/material';
 import { ItemService } from '../item.service';
 import { WindowRef } from 'shield';
+import { environment } from '../../../../environments/environment';
 
 import {
   MOCK_ITEM
@@ -17,14 +19,26 @@ describe('ItemCartFavoriteComponent', () => {
   let element: HTMLElement;
 
   let itemService: ItemService;
+  let windowRef: WindowRef;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [SharedModule, MdIconModule],
       declarations: [ ItemCartFavoriteComponent ],
       providers: [
-        WindowRef,
-        { provide: ItemService, user: {}},
+        {provide: WindowRef, useValue: {
+            nativeWindow: {
+              location: {
+                href: environment.siteUrl
+              }
+            }
+          }
+        },
+        { provide: ItemService, user: {
+          favoriteItem () {
+            return Observable.of({});
+          }
+        }},
         { provide: 'SUBDOMAIN', useValue: 'www'}
       ],
       schemas: [NO_ERRORS_SCHEMA]
@@ -38,7 +52,32 @@ describe('ItemCartFavoriteComponent', () => {
     element = fixture.nativeElement;
     component.item = MOCK_ITEM;
     itemService = TestBed.get(ItemService);
+    windowRef = TestBed.get(WindowRef);
     fixture.detectChanges();
+  });
+
+  describe('when coponent is created', () => {
+    it('should define homeUrl', () => {
+      // console.log(component.homeUrl)
+    })
+  });
+
+  describe('goToItemDetail', () => {
+    it('should change window url', () => {
+      component.goToItemDetail();
+      console.log(windowRef.nativeWindow.location.href);
+    })
+  });
+
+  describe('removeFavorite', ()=>{
+    it('should set favorited property to false', () => {
+      component.removeFavorite();
+      expect(component.item.favorited).toBeFalsy();
+    });
+    it('should set favorited property to false', () => {
+      component.removeFavorite();
+      expect(component.item.favorited).toBeFalsy();
+    });
   });
 
   it('should be created', () => {
