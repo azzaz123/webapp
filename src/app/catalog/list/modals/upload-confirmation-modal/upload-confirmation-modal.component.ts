5cdd3d192ea4c93d5e7d3c8caa41ff3ca7487b44
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2017-11-10 11:00:59 +0100

    [ADM-2914] Add Twitter share

--- src/app/catalog/list/modals/upload-confirmation-modal/upload-confirmation-modal.component.ts
+++ src/app/catalog/list/modals/upload-confirmation-modal/upload-confirmation-modal.component.ts
@@ -11,15 +11,23 @@ export class UploadConfirmationModalComponent implements OnInit {
 
   public item: Item;
 
-  constructor(public activeModal: NgbActiveModal, private window: WindowRef) { }
+  constructor(public activeModal: NgbActiveModal, private window: WindowRef) {
+  }
 
   ngOnInit() {
   }
 
   public facebookShare() {
-    const url = 'https://www.facebook.com/dialog/share?app_id=258778180928082&display=popup&href=' + encodeURI(this.item.webLink);
+    const url = 'https://www.facebook.com/dialog/share?app_id=258778180928082&display=popup&href=' + encodeURIComponent(this.item.webLink);
     this.window.nativeWindow.open(url, 'fbShareWindow', 'height=450, width=550, toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+  }
 
+  public twitterShare() {
+    const text = 'Mira que acabo de encontrar en @Wallapop:';
+    const url = 'https://twitter.com/intent/tweet?' +
+      'text=' + encodeURIComponent(text) +
+      '&url=' + encodeURIComponent(this.item.webLink);
+    this.window.nativeWindow.open(url, 'twShareWindow', 'height=269,width=550, toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
   }
 
 }
