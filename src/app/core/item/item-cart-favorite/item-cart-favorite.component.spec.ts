448a6b2a34a2c11755414cd905d8290b329d811b
tmirun <tmirun@hotmail.com>
2017-11-13 16:25:50 +0100

    [ADM-2822] add test item-card-favorite.component

--- src/app/core/item/item-cart-favorite/item-cart-favorite.component.spec.ts
+++ src/app/core/item/item-cart-favorite/item-cart-favorite.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed, fakeAsync, tick} from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { SharedModule } from '../../../shared/shared.module';
@@ -6,12 +6,13 @@ import { Observable } from 'rxjs/Observable';
 import { ItemCartFavoriteComponent } from './item-cart-favorite.component';
 import { MdIconModule } from '@angular/material';
 import { ItemService } from '../item.service';
-import { WindowRef } from 'shield';
+import { WindowRef, USER_ID } from 'shield';
 import { environment } from '../../../../environments/environment';
-
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import {
   MOCK_ITEM
 } from 'shield';
+import {ConfirmationModalComponent} from '../../../shared/confirmation-modal/confirmation-modal.component';
 
 describe('ItemCartFavoriteComponent', () => {
   let component: ItemCartFavoriteComponent;
@@ -20,13 +21,24 @@ describe('ItemCartFavoriteComponent', () => {
 
   let itemService: ItemService;
   let windowRef: WindowRef;
+  let subdomain: string;
+  let modalService: NgbModal;
+
+  const modalRef: any = {
+    result: Promise.resolve({
+      score: 4,
+      comments: 'comment',
+      userId: USER_ID
+    }),
+    componentInstance: {}
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [SharedModule, MdIconModule],
       declarations: [ ItemCartFavoriteComponent ],
       providers: [
-        {provide: WindowRef, useValue: {
+        { provide: WindowRef, useValue: {
             nativeWindow: {
               location: {
                 href: environment.siteUrl
@@ -34,11 +46,18 @@ describe('ItemCartFavoriteComponent', () => {
             }
           }
         },
-        { provide: ItemService, user: {
-          favoriteItem () {
-            return Observable.of({});
+        { provide: ItemService, useValue: {
+            favoriteItem () {
+              return Observable.of({});
+            }
           }
-        }},
+        },
+        { provide: NgbModal, useValue: {
+            open() {
+              return modalRef;
+            }
+          }
+        },
         { provide: 'SUBDOMAIN', useValue: 'www'}
       ],
       schemas: [NO_ERRORS_SCHEMA]
@@ -53,34 +72,62 @@ describe('ItemCartFavoriteComponent', () => {
     component.item = MOCK_ITEM;
     itemService = TestBed.get(ItemService);
     windowRef = TestBed.get(WindowRef);
-    fixture.detectChanges();
-  });
+    modalService = TestBed.get(NgbModal);
+    subdomain = TestBed.get('SUBDOMAIN');
 
-  describe('when coponent is created', () => {
-    it('should define homeUrl', () => {
-      // console.log(component.homeUrl)
-    })
+    fixture.detectChanges();
   });
 
   describe('goToItemDetail', () => {
     it('should change window url', () => {
+      const MOCK_ITEM_URL: string = environment.siteUrl.replace('es', subdomain) + 'item/' + MOCK_ITEM.webSlug;
       component.goToItemDetail();
-      console.log(windowRef.nativeWindow.location.href);
+      expect(windowRef.nativeWindow.location.href).toEqual(MOCK_ITEM_URL);
     })
   });
 
-  describe('removeFavorite', ()=>{
+  describe('removeFavorite', () => {
+    beforeEach(() => {
+      spyOn(component.onFavoriteChange, 'emit')
+    })
     it('should set favorited property to false', () => {
       component.removeFavorite();
       expect(component.item.favorited).toBeFalsy();
     });
-    it('should set favorited property to false', () => {
+    it('should call onFavoriteChange emit method', () => {
       component.removeFavorite();
-      expect(component.item.favorited).toBeFalsy();
+      expect(component.onFavoriteChange.emit).toHaveBeenCalledWith(MOCK_ITEM);
     });
   });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
+  describe('removeFavoriteModal', () => {
+    let removeFavoriteButton;
+    beforeEach(fakeAsync(() => {
+      spyOn(component, 'removeFavoriteModal').and.callThrough();
+      spyOn(modalService, 'open').and.callThrough();
+      spyOn(component, 'removeFavorite').and.callThrough();
+      removeFavoriteButton = fixture.debugElement.nativeElement.querySelector('tsl-card-footer');
+    }));
+
+    it('when click tsl-cardFooter should call removeFavoriteModal method', () => {
+      removeFavoriteButton.click();
+      expect(component.removeFavoriteModal).toHaveBeenCalled();
+    });
+
+    it('should open accept modal', () => {
+      removeFavoriteButton.click();
+      expect(modalService.open).toHaveBeenCalledWith(ConfirmationModalComponent);
+    });
+
+    it('should set modal type "3" ', () => {
+      removeFavoriteButton.click();
+      expect(modalService.open).toHaveBeenCalledWith(ConfirmationModalComponent);
+    });
+
+    it('should call removeFavorite method ', fakeAsync(() => {
+      removeFavoriteButton.click();
+      tick();
+      expect(component.removeFavorite).toHaveBeenCalled();
+    }));
+  })
 });
