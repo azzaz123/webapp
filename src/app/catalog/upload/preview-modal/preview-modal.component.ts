042d294f135868435cd6acd1d4b4bb673d301f61
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2017-11-14 15:52:43 +0100

    [ADM-2885] Add preview cars

--- src/app/catalog/upload/preview-modal/preview-modal.component.ts
+++ src/app/catalog/upload/preview-modal/preview-modal.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { CarKeysService } from '../upload-car/car-keys.service';
 
 @Component({
   selector: 'tsl-preview-modal',
@@ -9,7 +10,31 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 export class PreviewModalComponent {
 
   public itemPreview: any;
+  public bodyType: string;
 
-  constructor(public activeModal: NgbActiveModal) { }
+  get hasCarExtras() {
+    return this.itemPreview.gearbox ||
+      this.itemPreview.body_type ||
+      this.itemPreview.num_seats ||
+      this.itemPreview.engine;
+  }
+
+  get hasExtras() {
+    return this.itemPreview.sale_conditions.fix_price ||
+      this.itemPreview.sale_conditions.shipping_allowed ||
+      this.itemPreview.sale_conditions.exchange_allowed;
+  }
+
+  constructor(public activeModal: NgbActiveModal,
+              private carKeysService: CarKeysService) {
+  }
+
+  public getBodyType() {
+    if (this.itemPreview.body_type) {
+      this.carKeysService.getTypeName(this.itemPreview.body_type).subscribe((bodyType: string) => {
+        this.bodyType = bodyType;
+      });
+    }
+  }
 
 }
