e6748002320b9363efc4728877b908ba0cade60d
Joan <joan.tapia@wallapop.com>
2017-11-22 14:19:48 +0100

    -

--- src/app/layout/topbar/geolocation/geolocation.component.ts
+++ src/app/layout/topbar/geolocation/geolocation.component.ts
@@ -15,7 +15,7 @@ export class GeolocationComponent implements OnInit {
   private MIN_LENGTH = 3;
   public focus: boolean;
   @Output() public newCoordinate = new EventEmitter<Coordinate>();
-  public model: any = {};
+  public model: any = {description: ''};
 
   constructor(private geolocationService: GeolocationService, private cookieService: CookieService) { }
 
