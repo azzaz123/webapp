50949eb49f9176e1bd0d4fb16cf12136da8f79ea
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2017-12-12 15:23:32 +0100

    [ADM-3062] Add messages item in menu and remove upload

--- src/app/config/icons.config.ts
+++ src/app/config/icons.config.ts
@@ -91,7 +91,7 @@ export function configIcons(mdIconRegistry: MdIconRegistry, sanitizer: DomSaniti
   mdIconRegistry.addSvgIcon('wall', sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/wall.svg'));
   mdIconRegistry.addSvgIcon('check', sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/check.svg'));
   mdIconRegistry.addSvgIcon('navigation_catalog', sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/navigation_catalog.svg'));
-  mdIconRegistry.addSvgIcon('navigation_upload', sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/navigation_upload.svg'));
+  mdIconRegistry.addSvgIcon('navigation_messages', sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/navigation_messages.svg'));
   mdIconRegistry.addSvgIcon('navigation_favorites', sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/navigation_favorites.svg'));
   mdIconRegistry.addSvgIcon('navigation_logout', sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/navigation_logout.svg'));
   mdIconRegistry.addSvgIcon('delete', sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/delete.svg'));
