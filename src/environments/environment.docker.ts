e50bd695cf2d6e4600e71c0538c586e91d86b0d7
Joan <joan.tapia@wallapop.com>
2017-12-19 17:05:35 +0100

    add docker infrastructure

---
+++ src/environments/environment.docker.ts
@@ -0,0 +1,10 @@
+export const environment: any = {
+  production: true,
+  name: 'prod',
+  baseUrl: 'http://api-gateway:9000/',
+  xmppDomain: 'wallapop.com',
+  wsUrl: 'wss://mongooseimprotool-prod.wallapop.com:443/ws-xmpp',
+  siteUrl: 'http://web:8090/',
+  appboy: '47b54d36-7a86-4c05-9bfc-2d7b8aadd1ce',
+  clickStreamURL: 'https://collector.wallapop.com/clickstream.json/sendEvents'
+};
