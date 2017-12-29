eb58d000d8693dfd516f6b133ddd4e15daa191b0
Joan <joan.tapia@wallapop.com>
2017-12-29 12:41:59 +0100

    add sed instruction to change docknum

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -2,8 +2,8 @@ export const environment: any = {
   production: false,
   name: 'docker',
   baseUrl: 'http://api-gateway:9000/',
-  xmppDomain: 'wallapop.com',
-  wsUrl: 'wss://mongooseimprotool-prod.wallapop.com:443/ws-xmpp',
+  xmppDomain: 'dockXXX.wallapop.com',
+  wsUrl: 'wss://mongooseim:5282/ws-xmpp',
   siteUrl: 'http://web:8090/',
   appboy: '47b54d36-7a86-4c05-9bfc-2d7b8aadd1ce',
   clickStreamURL: 'https://collector.wallapop.com/clickstream.json/sendEvents'
