02a934329c5474795340939f2b23aa3fc2c7eb49
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2018-01-04 18:05:14 +0100

    [ADM-3166] Start modal

---
+++ src/app/profile/unsubscribe-modal/unsubscribe-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tsl-unsubscribe-modal',
+  templateUrl: './unsubscribe-modal.component.html',
+  styleUrls: ['./unsubscribe-modal.component.scss']
+})
+export class UnsubscribeModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
