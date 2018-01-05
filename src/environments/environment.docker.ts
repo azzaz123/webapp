bd9138c6701e883cbca88867951b481e6ada76f0
Joan <joan.tapia@wallapop.com>
2018-01-05 14:09:50 +0100

    solve error in url

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -1,10 +1,10 @@
 export const environment: any = {
   production: false,
   name: 'docker',
-  baseUrl: 'https://apigw.dockXXX.wallapp.com/',
+  baseUrl: 'https://apigw.dockXXX.wallapop.com/',
   xmppDomain: 'dockXXX.wallapop.com',
   wsUrl: 'wss://mongooseim:5282/ws-xmpp',
-  siteUrl: 'https://es.dockXXX.wallapp.com/',
+  siteUrl: 'https://es.dockXXX.wallapop.com/',
   appboy: '47b54d36-7a86-4c05-9bfc-2d7b8aadd1ce',
   cookieSuffix: 'dockXXX',
   clickStreamURL: 'https://collector.wallapop.com/clickstream.json/sendEvents'
