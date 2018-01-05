f42a1e517696e82264a5df9128b86fa1861de97f
enric.pagerols <enric.pagerols@wallapop.com>
2018-01-05 10:51:30 +0100

    ADM-3059 Add reviews empty state

--- src/app/config/icons.config.ts
+++ src/app/config/icons.config.ts
@@ -146,4 +146,5 @@ export function configIcons(mdIconRegistry: MdIconRegistry, sanitizer: DomSaniti
   mdIconRegistry.addSvgIcon('car-type-4X4', sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/4X4.svg'));
   mdIconRegistry.addSvgIcon('car-type-van', sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/van.svg'));
   mdIconRegistry.addSvgIcon('car-type-others', sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/others.svg'));
+  mdIconRegistry.addSvgIcon('reviews-empty-state', sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/reviews-empty-state.svg'));
 }
