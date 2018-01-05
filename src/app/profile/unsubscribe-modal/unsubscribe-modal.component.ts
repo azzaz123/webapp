a8c2de087cb9b3ec6254f294c2d530cc56db19b5
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2018-01-05 12:45:56 +0100

    [ADM-3166] Second step modal

--- src/app/profile/unsubscribe-modal/unsubscribe-modal.component.ts
+++ src/app/profile/unsubscribe-modal/unsubscribe-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'tsl-unsubscribe-modal',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UnsubscribeModalComponent implements OnInit {
 
-  constructor() { }
+  public step = 1;
+  public reasons: any[];
+  public selectedReason: number;
+  public customReason: string;
+
+  constructor(public activeModal: NgbActiveModal) { }
 
   ngOnInit() {
+    this.reasons = [{
+      id: 1,
+      title: 'Ya no utilizo la app'
+    }, {
+      id: 2,
+      title: 'No encuentro productos interesantes ni compradores interesados'
+    }, {
+      id: 3,
+      title: 'Utilizo otra plataforma'
+    }, {
+      id: 4,
+      title: 'Tengo problemas técnicos con el servicio'
+    }, {
+      id: 5,
+      title: 'He tenido problemas de seguridad'
+    }];
+  }
+
+  public send() {
+    this.activeModal.close();
   }
 
 }
