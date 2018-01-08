ed8f62b4228119150bb7b67c4b9e276bcd5a64f9
Joan <joan.tapia@wallapop.com>
2018-01-08 12:41:02 +0100

    change chat websocket url in docker envs

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -3,7 +3,7 @@ export const environment: any = {
   name: 'docker',
   baseUrl: 'https://dockXXX.wallapop.com/',
   xmppDomain: 'dockXXX.wallapop.com',
-  wsUrl: 'wss://mongooseim:5282/ws-xmpp',
+  wsUrl: 'wss://dockXXX.wallapop.com:5282/ws-xmpp',
   siteUrl: 'https://es.dockXXX.wallapop.com/',
   appboy: '47b54d36-7a86-4c05-9bfc-2d7b8aadd1ce',
   cookieSuffix: 'dockXXX',
