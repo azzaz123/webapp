c541982b8494020e06f2a7364167e5f264e1752b
Joan <joan.tapia@wallapop.com>
2018-01-09 16:07:45 +0100

    -

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,9 +6,9 @@
 export const environment: any = {
   production: false,
   name: 'local',
-  baseUrl: 'http://dock122.corp.wallapop.com:8080/',
-  xmppDomain: 'dock122.wallapop.com',
-  wsUrl: 'ws://dock122.corp.wallapop.com:5281/ws-xmpp',
+  baseUrl: 'https://apigw.dockXXX.wallapop.com/',
+  xmppDomain: 'dock115.wallapop.com',
+  wsUrl: 'ws://dock115.wallapop.com:5282/ws-xmpp',
   siteUrl: 'http://es.dev.wallapop.com:8080/',
   bypass: 'p3-9p0dJk2cHp3-4RsW0',
   appboy: '516bd193-25b2-48b9-b79d-f8516f104d2f',
