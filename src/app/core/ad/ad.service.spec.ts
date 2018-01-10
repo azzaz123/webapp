63021c5f3fc84ce2be1b10c1a922256ea9ade4a9
tmirun <tmirun@hotmail.com>
2018-01-10 16:08:50 +0100

    [DIS-357] add ad service test

--- src/app/core/ad/ad.service.spec.ts
+++ src/app/core/ad/ad.service.spec.ts
@@ -1,15 +1,64 @@
-import { TestBed, inject } from '@angular/core/testing';
-
+import {
+  TestBed,
+  fakeAsync,
+  tick,
+  discardPeriodicTasks
+} from '@angular/core/testing';
 import { AdService } from './ad.service';
+import { TEST_HTTP_PROVIDERS, HttpService } from 'shield';
+import { ResponseOptions, Response } from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
 
+let service: AdService;
+let http: HttpService;
+let mockBackend: MockBackend;
 describe('AdService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AdService]
+      providers: [
+        AdService,
+        ...TEST_HTTP_PROVIDERS
+      ]
     });
+
+    service = TestBed.get(AdService);
+    http = TestBed.get(HttpService);
+    mockBackend = TestBed.get(MockBackend);
   });
 
-  it('should be created', inject([AdService], (service: AdService) => {
-    expect(service).toBeTruthy();
-  }));
+  describe ('getRefreshRate', () => {
+    it('should refresh ads with its rate' , fakeAsync(() => {
+      const refreshRate = 1000;
+      mockBackend.connections.subscribe((connection: MockConnection) => {
+        const res: ResponseOptions = new ResponseOptions({body: refreshRate});
+        connection.mockRespond(new Response(res));
+      });
+      const numberOfTimes = 2;
+      let timesCalled = 0 ;
+
+      service.getRefreshRate().subscribe(() => {
+        timesCalled++;
+      });
+      tick(numberOfTimes * refreshRate);
+
+      expect(timesCalled).toEqual(numberOfTimes);
+      discardPeriodicTasks();
+    }));
+
+    it('when not receiving refresh rate, should not refresh', fakeAsync(() => {
+      mockBackend.connections.subscribe((connection: MockConnection) => {
+        const res: ResponseOptions = new ResponseOptions({body: 0});
+        connection.mockRespond(new Response(res));
+      });
+      let refreshed = false;
+
+      service.getRefreshRate().subscribe(() => {
+        refreshed = true;
+      });
+
+      tick(2000);
+      expect(refreshed).toBeFalsy();
+      discardPeriodicTasks();
+    }))
+  })
 });
