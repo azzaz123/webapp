53342739b6a8585bff4df979ce4c13c5e4c3ebff
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2018-01-15 17:12:19 +0100

    [ADM-3069] Fix clickStreamURL

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -7,5 +7,5 @@ export const environment: any = {
   siteUrl: 'https://es.dockXXX.wallapop.com/',
   appboy: '47b54d36-7a86-4c05-9bfc-2d7b8aadd1ce',
   cookieSuffix: 'dockXXX',
-  clickStreamURL: 'https://collector.wallapop.com/clickstream.json/sendEvents'
+  clickStreamURL: 'https://precollector.wallapop.com/clickstream.json/sendEvents'
 };
