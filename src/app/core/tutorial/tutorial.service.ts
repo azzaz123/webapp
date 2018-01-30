0f3bd38f777503437c7332ffa19476052b83c42a
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2018-01-30 12:59:04 +0100

    [ADM-3335] Tutorial steps

---
+++ src/app/core/tutorial/tutorial.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class TutorialService {
+
+  step = 0;
+
+  constructor() { }
+
+}
--- src/app/tutorial/tutorial.component.ts
+++ src/app/tutorial/tutorial.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { TutorialService } from '../core/tutorial/tutorial.service';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'tsl-tutorial',
   templateUrl: './tutorial.component.html',
-  styleUrls: ['./tutorial.component.scss']
+  styleUrls: ['./tutorial.component.scss'],
+  animations: [
+    trigger(
+      'fade', [
+        transition(':enter', [
+          style({opacity: 0}),
+          animate('500ms', style({opacity: 1}))
+        ]),
+        transition(':leave', [
+          style({opacity: 1}),
+          animate('500ms', style({opacity: 0}))
+        ])
+      ]
+    )
+  ],
 })
-export class TutorialComponent implements OnInit {
+export class TutorialComponent implements OnDestroy {
 
-  constructor() { }
+  constructor(public tutorialService: TutorialService) { }
 
-  ngOnInit() {
+  ngOnDestroy() {
+    this.tutorialService.step = 0;
+  }
+
+  nextStep() {
+    this.tutorialService.step++;
+  }
+
+  exit() {
+
   }
 
 }
