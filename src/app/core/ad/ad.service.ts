e47a01077ea3ff6535dee394a8da93ddb0edba51
tmirun <tmirun@hotmail.com>
2018-01-31 11:40:25 +0100

    [DIS-365] improve && execute in the first time refresh ads

--- src/app/core/ad/ad.service.ts
+++ src/app/core/ad/ad.service.ts
@@ -24,7 +24,10 @@ export class AdService {
               private userService: UserService,
               private cookieService: CookieService
   ) {
+    this.initKeyWordsFromCookies();
+  }
 
+  private initKeyWordsFromCookies() {
     this.adKeyWords.brand = this.cookieService.get('brand');
     this.adKeyWords.content = this.cookieService.get('content');
     this.adKeyWords.category = this.cookieService.get('category');
@@ -33,19 +36,24 @@ export class AdService {
   }
 
   public startAdsRefresh(): void {
-    this.adsRefreshSubscription = this.userService.me().map((user: User) => {
+    if (this.adsRefreshSubscription && !this.adsRefreshSubscription.closed) { return ; }
+    this.adsRefreshSubscription = this.userService.me().do((user: User) => {
       this.adKeyWords.gender = user.gender;
-      return user
     }).flatMap(() => {
       return this.http.getNoBase(environment.siteUrl + this.ENDPOINT_REFRESH_RATE).map(res => res.json())
     }).flatMap((refreshRate: number) => {
-      return refreshRate ? Observable.interval(refreshRate) : Observable.of(refreshRate)
+      refreshRate = 5000;
+      return refreshRate ? Observable.timer(0, refreshRate) : Observable.of(0)
     }).subscribe(() => {
-      Object.keys(this.adKeyWords).forEach((key) => {
-        googletag.pubads().setTargeting(key, this.adKeyWords[key]);
-      });
-      googletag.pubads().refresh();
+      this.refreshAdWithKeyWords();
     });
+  }
+
+  private refreshAdWithKeyWords(): void {
+    Object.keys(this.adKeyWords).forEach((key) => {
+      googletag.pubads().setTargeting(key, this.adKeyWords[key]);
+    });
+    googletag.pubads().refresh();
   }
 
   public stopAdsRefresh(): void {
