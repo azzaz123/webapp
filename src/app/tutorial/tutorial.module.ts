b27431bf76ccf957ef233ed878f849cbec826159
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2018-02-05 13:14:27 +0100

    [ADM-3335] Add tracking

--- src/app/tutorial/tutorial.module.ts
+++ src/app/tutorial/tutorial.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { tutorialRoutedComponents, TutorialRoutingModule } from './tutorial.routes';
 import { MdIconModule } from '@angular/material';
+import { TrackingModule } from '../core/tracking/tracking.module';
 
 @NgModule({
   imports: [
     CommonModule,
     TutorialRoutingModule,
-    MdIconModule
+    MdIconModule,
+    TrackingModule
   ],
   declarations: [tutorialRoutedComponents]
 })
