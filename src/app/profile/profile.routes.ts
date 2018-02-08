e5c78d512a3f74e47f1e6acd19520cc45941c32e
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2018-02-08 16:31:42 +0100

    [ADM-3196] Open confirmation dialog when user exit from profile page without saving

--- src/app/profile/profile.routes.ts
+++ src/app/profile/profile.routes.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoggedGuard } from '../core/user/logged.guard';
 import { ProfileComponent } from './profile.component';
+import { ExitConfirmGuard } from '../shared/guards/exit-confirm.guard';
 
 const routes: Routes = [
   {
     path: 'profile',
     component: ProfileComponent,
     canActivate: [LoggedGuard],
+    canDeactivate: [ExitConfirmGuard],
     data: {
       isMyZone: true
     }
