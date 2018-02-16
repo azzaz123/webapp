f31a05fe75988208c8622d2a13e8b9051a626306
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2018-02-16 11:15:58 +0100

    [ADM-3419] Call real endpoint passing items ids

--- src/app/catalog/checkout/checkout.component.ts
+++ src/app/catalog/checkout/checkout.component.ts
@@ -13,9 +13,29 @@ export class CheckoutComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.itemService.getItemsWithAvailableProducts(['1']).subscribe((itemsWithProducts: ItemWithProducts[]) => {
+    this.itemService.getItemsWithAvailableProducts(this.itemService.selectedItems).subscribe((itemsWithProducts: ItemWithProducts[]) => {
       console.log(itemsWithProducts);
     });
   }
+
+  /*public featureItems() {
+    const order: Order[] = this.selectedProducts.map((product: SelectedProduct) => {
+      return {
+        item_id: product.itemId,
+        product_id: product.product.durations[0].id
+      }
+    });
+    this.loading = true;
+    this.onAction.emit({
+      order: order,
+      total: this.total
+    });
+
+    let result = order.map(purchase => ({ item_id: purchase.item_id, bump_type: purchase.product_id }));
+    this.trackingService.track(TrackingService.CATALOG_FEATURED_CHECKOUT, { selected_products: result });
+    ga('send', 'event', 'Item', 'bump-cart');
+    gtag('event', 'conversion', {'send_to': 'AW-829909973/oGcOCL7803sQ1dfdiwM'});
+    fbq('track', '176083133152402', {});
+  }*/
 
 }
