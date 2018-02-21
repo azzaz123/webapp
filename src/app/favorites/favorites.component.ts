3a153401d2094e62c52907042c55142d1b9d69b9
Alex Masnou <alex.masnou@wallapop.com>
2018-02-21 16:54:26 +0100

    [ADM-3413] Get number of favorites from API

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ItemService } from '../core/item/item.service';
 import { ItemsData } from '../core/item/item-response.interface';
 import { Item } from 'shield';
+import { UserService } from '../core/user/user.service';
+import { UserStatsResponse } from '../core/user/user-stats.interface';
 
 @Component({
   selector: 'tsl-favorites',
@@ -14,16 +16,17 @@ export class FavoritesComponent implements OnInit {
   public selectedStatus: string = 'published';
   public loading: boolean = false;
   public end: boolean = false;
+  public favoritesNum: number;
 
   public masonryOptions = {
     gutter: 20
   };
 
-  constructor(public itemService: ItemService
-  ) { }
+  constructor(public itemService: ItemService, private userService: UserService) { }
 
   ngOnInit() {
     this.getItems();
+    this.getFavoritesNum();
   }
 
   public getItems(append?: boolean) {
@@ -47,11 +50,18 @@ export class FavoritesComponent implements OnInit {
     if (this.items.length) {
       const index = this.items.indexOf(item);
       this.items.splice(index, 1);
+      this.favoritesNum--;
     }
   }
 
   public loadMore() {
     this.getItems(true);
+  }
+
+  public getFavoritesNum() {
+    this.userService.getStats().subscribe((stats: UserStatsResponse) => {
+      this.favoritesNum = stats.counters.favorites;
+    });
   }
 
 }
