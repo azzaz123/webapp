8cb07e4359eb4912a6691bc2772dac5c7896c07a
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2018-02-27 16:19:34 +0100

    [ADM-3422] Redirect to catalog if no selected items

--- src/app/catalog/checkout/checkout.component.ts
+++ src/app/catalog/checkout/checkout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ItemService } from '../../core/item/item.service';
 import { ItemWithProducts } from '../../core/item/item-response.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'tsl-checkout',
@@ -11,10 +12,15 @@ export class CheckoutComponent implements OnInit {
 
   itemsWithProducts: ItemWithProducts[];
 
-  constructor(private itemService: ItemService) {
+  constructor(private itemService: ItemService,
+              private router: Router) {
   }
 
   ngOnInit() {
+    if (!this.itemService.selectedItems.length) {
+      this.router.navigate(['catalog/list']);
+      return;
+    }
     this.itemService.getItemsWithAvailableProducts(this.itemService.selectedItems).subscribe((itemsWithProducts: ItemWithProducts[]) => {
       this.itemsWithProducts = itemsWithProducts;
     });
