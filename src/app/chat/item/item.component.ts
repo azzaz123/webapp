97d7bfd3865988a81a12a5bbd0372d468ec1fba7
Alexandra Haret <alexandra.haret@wallapop.com>
2018-03-16 15:07:47 +0100

    Added new methods for edit and reserve

--- src/app/chat/item/item.component.ts
+++ src/app/chat/item/item.component.ts
@@ -1,19 +1,34 @@
-import { Component, Inject, Input, OnChanges, OnDestroy } from '@angular/core';
-import { Item, ItemCounters, ItemService } from 'shield';
+import { Component, Inject, Input, OnInit,  OnChanges, OnDestroy } from '@angular/core';
+import { UserService } from '../../core/user/user.service';
+import { ItemService } from '../../core/item/item.service';
+import { TrackingService } from '../../core/tracking/tracking.service';
+import { Item, ItemCounters, User } from 'shield';
 
 @Component({
   selector: 'tsl-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.scss']
 })
-export class ItemComponent implements OnChanges, OnDestroy {
+export class ItemComponent implements OnInit, OnChanges, OnDestroy {
 
   @Input() item: Item;
+  @Input() user: User;
   public itemUrl: string;
   private active = true;
+  private allowReserve: boolean;
+  private myUserId: string;
 
   constructor(private itemService: ItemService,
+              private userService: UserService,
+              private trackingService: TrackingService,
               @Inject('SUBDOMAIN') private subdomain: string) {
+  }
+
+  ngOnInit() {
+    this.userService.me().subscribe(
+      (user: User) => {
+        this.myUserId = user.id;
+      });
   }
 
   ngOnChanges(changes?: any) {
@@ -33,12 +48,32 @@ export class ItemComponent implements OnChanges, OnDestroy {
   }
 
   prevent($event: Event) {
-    if (this.itemUrl === '#') {
+    const el = $event.target as Element;
+    if (this.itemUrl === '#' ||
+        el.classList.contains('btn-reserve') ||
+        el.classList.contains('btn-sold')) {
       $event.preventDefault();
     }
   }
 
   stopPropagation($event: Event) {
     $event.stopPropagation();
+  }
+
+  isItemOwner() {
+    return (this.item.owner === this.myUserId);
+  }
+
+  canEdit() {
+    return (this.isItemOwner() && !this.item.sold);
+  }
+
+  public toggleReserve(item: Item) {
+    this.itemService.reserveItem(item.id, !item.reserved).subscribe(() => {
+      item.reserved = !item.reserved;
+      if (item.reserved) {
+        this.trackingService.track(TrackingService.CHAT_PRODUCT_RESERVED, {product_id: item.id});
+      }
+    });
   }
 }
