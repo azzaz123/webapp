bf61d458bf517f3c46fff44d3c84a105684b15a9
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2018-04-17 10:21:14 -0500

    [ADM-3536] Fix env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,10 @@
 export const environment: any = {
   production: false,
   name: 'local',
-  baseUrl: 'https://beta.wallapop.com/',
-  xmppDomain: 'beta.wallapop.com',
-  wsUrl: 'wss://mongooseimprotool-beta.wallapop.com:443/ws-xmpp',
-  siteUrl: 'https://es.beta.wallapop.com/',
-  bypass: 'p3-9p0dJk2cHp3-4RsW0',
+  baseUrl: 'https://apigw.dock133.wallapop.com/',
+  xmppDomain: 'dock133.wallapop.com',
+  wsUrl: 'wss://dock133.wallapop.com:5282/ws-xmpp',
+  siteUrl: 'https://es.dock133.wallapop.com/',
   appboy: '516bd193-25b2-48b9-b79d-f8516f104d2f',
   cookieSuffix: 'Local',
   clickStreamURL: 'https://precollector.wallapop.com/clickstream.json/sendEvents'
