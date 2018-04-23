d47ce53b7cdaa7f52abd5f5697054e8ef2e1255f
tmirun <tmirun@hotmail.com>
2018-04-23 11:23:49 +0200

    [DIS-589] Change Name of privacy interfaces

--- src/app/core/privacy/privacy.service.ts
+++ src/app/core/privacy/privacy.service.ts
@@ -2,48 +2,58 @@ import { Injectable } from '@angular/core';
 import { Response } from '@angular/http';
 import { HttpService } from '../http/http.service';
 import {
-  PrivacyPermissionRequestData,
-  PrivacyPermissions,
-  PRIVACY_PERMISSION_STATUS,
-  PrivacyPermission
-} from './privacy-permission.interface';
+  PrivacyRequestData,
+  PrivacyList,
+  PrivacyVersionItem,
+  PRIVACY_STATUS
+} from './privacy';
+import {Subject} from 'rxjs/Subject';
 
 @Injectable()
 export class PrivacyService {
 
   private API_URL = 'api/v3/privacy';
-  private _privacyPermissions: PrivacyPermissions;
+  private _privacyList: PrivacyList;
+  public allowSegmentation$: Subject<boolean> = new Subject<boolean>()
 
   constructor(
     private http: HttpService
   ) { }
 
-  public updatePrivacyPermission(data: PrivacyPermissionRequestData) {
+  public updatePrivacy(data: PrivacyRequestData) {
     return this.http.post(this.API_URL, data)
       .map((r: Response) => r.json());
   }
 
-  public getPrivacyPermissions() {
+  public getPrivacyList() {
     return this.http.get(this.API_URL)
-      .map((r: Response) => r.json());
+      .map((r: Response) => r.json())
+      .map((privacyList: PrivacyList) => {
+        this._privacyList = privacyList;
+
+        const segmentationStatus = this.getPrivacyState('gdpr_display', '0');
+        this.allowSegmentation$.next(segmentationStatus === PRIVACY_STATUS.allow ? true : false);
+
+        return privacyList;
+      });
   }
 
-  public getPrivacyPermissionState(permissionName: string, version: string) {
-    if (!this._privacyPermissions ||
-      !this._privacyPermissions[permissionName] ||
-      !this._privacyPermissions[permissionName].length) {
-      return PRIVACY_PERMISSION_STATUS.unknown;
+  public getPrivacyState(permissionName: string, version: string): string {
+    if (!this._privacyList ||
+      !this._privacyList[permissionName] ||
+      !this._privacyList[permissionName].length) {
+      return PRIVACY_STATUS.unknown;
     } else {
-      for (const key in this._privacyPermissions[permissionName]) {
-        if (this._privacyPermissions[permissionName].hasOwnProperty(key)) {
-          const permission: PrivacyPermission = this._privacyPermissions[permissionName][key][version]
-          if (!permission) {
-            return PRIVACY_PERMISSION_STATUS.unknown;
+      for (const key in this._privacyList[permissionName]) {
+        if (this._privacyList[permissionName].hasOwnProperty(key)) {
+          const permission: PrivacyVersionItem = this._privacyList[permissionName][key];
+          if (permission && permission.version === version) {
+            return permission.allow ? PRIVACY_STATUS.allow : PRIVACY_STATUS.disallow;
           }
-          return permission.allow ? PRIVACY_PERMISSION_STATUS.allow : PRIVACY_PERMISSION_STATUS.disallow;
         }
       }
     }
+    return PRIVACY_STATUS.unknown;
   }
 
 }
---
+++ src/app/core/privacy/privacy.ts
@@ -0,0 +1,30 @@
+export const PRIVACY_STATUS = {
+  allow: 'allow',
+  disallow: 'disallow',
+  unknown: 'unknown'
+};
+
+export class PrivacyRequestData {
+  constructor(privacyName, version, allow) {
+    this[privacyName] = {
+      version,
+      allow
+    };
+  }
+}
+
+export interface PrivacyVersionItem {
+  version: string;
+  allow: boolean;
+}
+
+export interface PrivacyList {
+  gdpr_display: PrivacyVersionItem[];
+  privacy_policy: PrivacyVersionItem[];
+}
+
+export interface PrivacyStatus {
+  allow?: string;
+  disallow?: string;
+  unknown?: string;
+}
