db471071056225113b4ae7dcea88a93968b58b47
Alexandra Haret <alexandra.haret@wallapop.com>
2018-04-26 12:11:21 +0200

    [ADM-3647] Remove extra comma

--- src/app/core/connection/connection.service.ts
+++ src/app/core/connection/connection.service.ts
@@ -6,15 +6,15 @@ import { EventService } from '../event/event.service';
 export class ConnectionService {
 
   constructor(private winRef: WindowRef,
-              private eventService: EventService) { }
+    private eventService: EventService) { }
 
-  public checkConnection() {
-    this.winRef.nativeWindow.addEventListener('offline', () => {
-      this.eventService.emit(EventService.CONNECTION_ERROR, );
-    });
-    this.winRef.nativeWindow.addEventListener('online', () => {
-      this.eventService.emit(EventService.CONNECTION_RESTORED);
-    });
+    public checkConnection() {
+      this.winRef.nativeWindow.addEventListener('offline', () => {
+        this.eventService.emit(EventService.CONNECTION_ERROR);
+      });
+      this.winRef.nativeWindow.addEventListener('online', () => {
+        this.eventService.emit(EventService.CONNECTION_RESTORED);
+      });
+    }
   }
 
-}
