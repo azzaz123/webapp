2c8a0b0aa3d0517e081b7783ee69bc952f6fb5c3
Alex Masnou <alex.masnou@wallapop.com>
2018-05-03 12:27:37 +0200

    [ADM-3668] Remove items from cart and list

--- src/app/catalog/checkout/checkout-pro/checkout-pro-item/checkout-pro-item.component.ts
+++ src/app/catalog/checkout/checkout-pro/checkout-pro-item/checkout-pro-item.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ItemWithProducts } from '../../../../core/item/item-response.interface';
 import { NgbDateStruct, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
-import { CartProItem } from '../cart-pro/cart-pro-item.interface';
+import { CartProItem, CartProChange } from '../cart-pro/cart-pro-item.interface';
 import { CartProService } from '../cart-pro/cart-pro.service';
 
 @Component({
@@ -16,6 +16,9 @@ export class CheckoutProItemComponent implements OnInit {
   @Output() dateFocus: EventEmitter<CartProItem> = new EventEmitter();
 
   constructor(private calendar: NgbCalendar, private cartProService: CartProService) {
+    this.cartProService.cart$.subscribe((cartProChange: CartProChange) => {
+      this.onRemoveOrClean(cartProChange);
+    });
   }
 
   ngOnInit() {
@@ -25,24 +28,24 @@ export class CheckoutProItemComponent implements OnInit {
     this.dateFocus.emit(this.cartProItem);
   }
 
-  resetItem() {
-    delete this.cartProItem.fromDate;
-    delete this.cartProItem.toDate;
-    delete this.cartProItem.bumpType;
-  }
-
   selectBump(type: string) {
     if (!this.cartProItem.fromDate && !this.cartProItem.toDate) {
       this.cartProItem.fromDate = this.calendar.getToday();
       this.cartProItem.toDate = this.calendar.getNext(this.calendar.getToday(), 'd', 1);
     }
-    if (this.cartProItem.bumpType === type) {
-      delete this.cartProItem.bumpType;
+    this.cartProItem.bumpType === type ? this.removeItem() : this.cartProItem.bumpType = type;
+    this.cartProService.add(this.cartProItem);
+  }
+
+  onRemoveOrClean(cartProChange: CartProChange) {
+    if (cartProChange.action === 'remove' && cartProChange.itemId === this.cartProItem.item.id || cartProChange.action === 'clean') {
       delete this.cartProItem.fromDate;
       delete this.cartProItem.toDate;
-    } else {
-      this.cartProItem.bumpType = type;
+      delete this.cartProItem.bumpType;
     }
-    this.cartProService.add(this.cartProItem);
+  }
+
+  removeItem() {
+    this.cartProService.remove(this.cartProItem.item.id, this.cartProItem.bumpType);
   }
 }
