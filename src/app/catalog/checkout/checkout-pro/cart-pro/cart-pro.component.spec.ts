588bd380bc25caf73d1c068249b6022907608c60
Alex Masnou <alex.masnou@wallapop.com>
2018-05-07 15:18:29 +0200

    [ADM-3668] Working on new tests

--- src/app/catalog/checkout/checkout-pro/cart-pro/cart-pro.component.spec.ts
+++ src/app/catalog/checkout/checkout-pro/cart-pro/cart-pro.component.spec.ts
@@ -1,16 +1,57 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CartProComponent } from './cart-pro.component';
+import { FormsModule } from '@angular/forms';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CartService } from '../../cart/cart.service';
+import { Observable } from 'rxjs/Observable';
+import { CartPro } from '../../cart/cart-pro';
+import { CartChange } from '../../cart/cart-item.interface';
+import { ITEM_ID } from '../../../../../tests/item.fixtures.spec';
+import { ItemService } from '../../../../core/item/item.service';
 
 describe('CartProComponent', () => {
   let component: CartProComponent;
   let fixture: ComponentFixture<CartProComponent>;
 
+  const CART = new CartPro();
+  const CART_CHANGE: CartChange = {
+    action: 'add',
+    cart: CART,
+    itemId: ITEM_ID,
+    type: 'citybump'
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CartProComponent ]
+      imports: [FormsModule],
+      declarations: [CartProComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        {
+          provide: CartService, useValue: {
+            cart$: Observable.of(CART_CHANGE),
+            createInstance() {
+              return new CartPro();
+            },
+            remove() {
+            },
+            clean() {
+            }
+          }
+        },
+        {
+          provide: ItemService, useValue: {
+            purchaseProducts() {
+              return Observable.of({});
+            },
+            deselectItems() {
+            }
+          }
+        },
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
