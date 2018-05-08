b9bbee9d1f1d03854e56a98eda1a4f22cf0a5e73
Genís Díaz <Genisdf89@gmail.com>
2018-05-08 17:21:24 +0200

    [ADM-3682] cart service cart pro extras method tested

--- src/app/catalog/checkout/cart/cart.service.spec.ts
+++ src/app/catalog/checkout/cart/cart.service.spec.ts
@@ -3,9 +3,11 @@ import { TestBed } from '@angular/core/testing';
 import { CartService } from './cart.service';
 import { CART_ITEM_CITYBUMP, ITEM_ID, MOCK_ITEM_V3 } from '../../../../tests/item.fixtures.spec';
 import { Cart } from './cart';
-import { CartChange } from './cart-item.interface';
+import { CartChange, CartProExtrasPack } from './cart-item.interface';
 import { CartBase } from './cart-base';
 import { CartPro } from './cart-pro';
+import { CartProExtras } from './cart-pro-extras';
+import { PREPARED_PACKS, PACK_ID } from '../../../../tests/payments.fixtures.spec';
 
 describe('CartService', () => {
 
@@ -127,4 +129,49 @@ describe('CartService', () => {
     });
   });
 
+
+  describe('CartProExtras type events', () => {
+    const cart: CartBase = new CartProExtras();
+
+    beforeEach(() => {
+      service.createInstance(new CartProExtras);
+    });
+
+    it('should create an instance of CartProExtra', () => {
+      expect(cart instanceof CartProExtras).toBe(true);
+    });
+
+    describe('addProExtras', () => {
+      it('should call add method of cart pro extras model with parameters', () => {
+        spyOn(service['cart'], 'add');
+        const cartProExtrasPack: CartProExtrasPack = {
+          pack: PREPARED_PACKS[0].packs[0]
+        };
+
+        service.addProExtras(cartProExtrasPack, PREPARED_PACKS[0].packs[0].name.toLowerCase());
+
+        expect(service['cart'].add).toHaveBeenCalledWith(cartProExtrasPack, PREPARED_PACKS[0].packs[0].name.toLowerCase());
+        expect(cartChange.cart instanceof CartProExtras).toBe(true);
+        expect(cartChange.packId).toBe(PREPARED_PACKS[0].packs[0].id);
+        expect(cartChange.type).toBe(PREPARED_PACKS[0].packs[0].name.toLowerCase());
+        expect(cartChange.action).toBe('add');
+        expect(cartChange.itemId).toBeFalsy();
+      });
+    });
+
+    describe('removeProExtras', () => {
+      it('should call removeCartItem method of cart pro extras model with parameters', () => {
+        spyOn(service['cart'], 'removeCartItem');
+
+        service.removeProExtras(PACK_ID, TYPE, 0);
+
+        expect(service['cart'].removeCartItem).toHaveBeenCalledWith(TYPE, PACK_ID, 0);
+        expect(cartChange.cart instanceof CartProExtras).toBe(true);
+        expect(cartChange.packId).toBe(PACK_ID);
+        expect(cartChange.type).toBe(TYPE);
+        expect(cartChange.action).toBe('remove');
+        expect(cartChange.itemId).toBeFalsy();
+      });
+    });
+  });
 });
