147f0f9a4560c1bb8c41a5cad6c342391321f4e5
tmirun <tmirun@hotmail.com>
2018-05-10 17:09:27 +0200

    [DIS-654] add getGDPRText test

--- src/app/shared/gdpr-modal/gdpr-modal.component.spec.ts
+++ src/app/shared/gdpr-modal/gdpr-modal.component.spec.ts
@@ -1,14 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Response, ResponseOptions } from '@angular/http';
 import { GdprModalComponent } from './gdpr-modal.component';
+import { HttpService } from '../../core/http/http.service';
+import { Observable } from 'rxjs/Observable';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { environment } from '../../../environments/environment';
 
 describe('GdprModalComponent', () => {
   let component: GdprModalComponent;
   let fixture: ComponentFixture<GdprModalComponent>;
+  let http: HttpService;
+
+  const MOCK_HTTP_SERVICE = {
+    getNoBase() {
+      return Observable.of(new Response(new ResponseOptions({body: 'text'})));
+    }
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GdprModalComponent ]
+      declarations: [ GdprModalComponent ],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        NgbActiveModal,
+        {
+          provide: HttpService,
+          useValue: MOCK_HTTP_SERVICE
+        }
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +36,30 @@ describe('GdprModalComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(GdprModalComponent);
     component = fixture.componentInstance;
+    http = TestBed.get(HttpService);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('getGDPRText', () => {
+    beforeEach(() => {
+      spyOn(http, 'getNoBase').and.callThrough();
+    });
+
+    it('should get spanish text by default ', () => {
+      const url = environment.siteUrl + 'rest/gdpr/popup/es';
+
+      component.ngOnInit();
+
+      expect(http.getNoBase).toHaveBeenCalledWith(url);
+    });
+
+    it('should get spanish text by default ', () => {
+      const url = environment.siteUrl + 'rest/gdpr/popup/gb';
+      spyOn(component, 'getSubdomain').and.returnValue('web-en');
+
+      component.ngOnInit();
+
+      expect(http.getNoBase).toHaveBeenCalledWith(url);
+    });
   });
 });
