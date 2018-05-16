77737bb505d0eb0f73e31317ad66c454d81d0064
Alex Masnou <alex.masnou@wallapop.com>
2018-05-16 15:39:21 +0200

    [ADM-3668] Removed console.log

--- src/app/catalog/checkout/checkout-pro/cart-pro/cart-pro.component.ts
+++ src/app/catalog/checkout/checkout-pro/cart-pro/cart-pro.component.ts
@@ -43,7 +43,6 @@ export class CartProComponent implements OnInit {
       this.perks = perks;
       this.paymentsService.getStatus().subscribe((status: ScheduledStatus) => {
         this.status = status;
-        console.log(this.perks, this.status);
       });
     });
   }
