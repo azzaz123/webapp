78133fd81af0f945e1e959436f3f0ac64f9f6d26
Genís Díaz <Genisdf89@gmail.com>
2018-05-17 17:21:49 +0200

    [ADM-3682] FIX PR comments

--- src/app/catalog/checkout/checkout-extras-pro/checkout-extras-pro.component.spec.ts
+++ src/app/catalog/checkout/checkout-extras-pro/checkout-extras-pro.component.spec.ts
@@ -4,9 +4,10 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { PaymentService } from '../../../core/payments/payment.service';
 import { Observable } from 'rxjs/Observable';
 import { createPacksFixture, PREPARED_PACKS } from '../../../../tests/payments.fixtures.spec';
+import { FormGroup, FormControl } from '@angular/forms';
 
 
-describe('CheckoutExtrasProComponent', () => {
+fdescribe('CheckoutExtrasProComponent', () => {
   let component: CheckoutExtrasProComponent;
   let fixture: ComponentFixture<CheckoutExtrasProComponent>;
   let paymentService: PaymentService;
@@ -48,6 +49,51 @@ describe('CheckoutExtrasProComponent', () => {
 
     it('should preparePacks and return a understanding format', () => {
       expect(component.packs).toEqual(PREPARED_PACKS);
+    });
+  });
+
+  describe('onBillingInfoChange', () => {
+    const billingInfoForm = new FormGroup({
+      cif: new FormControl(),
+      city: new FormControl(),
+      company_name: new FormControl(),
+      country: new FormControl(),
+      email: new FormControl(),
+      name: new FormControl(),
+      phone: new FormControl(),
+      postal_code: new FormControl(),
+      street: new FormControl(),
+      surname: new FormControl(),
+      id: new FormControl()
+    });
+    const billingInfoFormValues = {
+      cif: 'cif',
+      city: 'city',
+      company_name: 'company_name',
+      country: 'country',
+      email: 'email',
+      name: 'name',
+      phone: 'phone',
+      postal_code: 'postal_code',
+      street: 'street',
+      surname: 'surname',
+      id: 'id'
+    };
+
+    it('should receive a form and set billingInfoForm with it', () => {
+      billingInfoForm.patchValue(billingInfoFormValues);
+
+      component.onBillingInfoChange(billingInfoForm);
+
+      expect(component.billingInfoForm).toEqual(billingInfoForm);
+    });
+  });
+
+  describe('onBillingInfoMissing', () => {
+    it('should receive a bolean and set billingInfoFormEnabled with it', () => {
+      component.onBillingInfoMissing(true);
+
+      expect(component.billingInfoFormEnabled).toBe(true);
     });
   });
 });
