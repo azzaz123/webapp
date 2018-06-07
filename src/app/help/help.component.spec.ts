2c188d5fbe11a75b4a8d533a29bdd5f885df1930
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2018-06-07 08:53:18 -0500

    [ADM-3794] Fix test

--- src/app/help/help.component.spec.ts
+++ src/app/help/help.component.spec.ts
@@ -1,14 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HelpComponent } from './help.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
 
-describe('HelpComponent', () => {
+fdescribe('HelpComponent', () => {
   let component: HelpComponent;
   let fixture: ComponentFixture<HelpComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HelpComponent ]
+      declarations: [ HelpComponent ],
+      providers: [{
+        provide: Router, useValue: {
+          events: Observable.of({})
+        }
+      }],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
