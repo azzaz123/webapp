64ffd9ecef7abe7311b411d8f182fa04931156ae
Genís Díaz <Genisdf89@gmail.com>
2018-06-14 17:03:53 +0200

    [ADM-3857] keeps open seach input in pro catalog

--- src/app/shared/search-input/search-input.component.spec.ts
+++ src/app/shared/search-input/search-input.component.spec.ts
@@ -22,7 +22,7 @@ describe('SearchInputComponent', () => {
   });
 
   it('should stream the term', fakeAsync(() => {
-    const TEXT: string = 'hola';
+    const TEXT = 'hola';
     let term: string;
     component.term$.subscribe((t) => {
       term = t;
@@ -31,34 +31,5 @@ describe('SearchInputComponent', () => {
     tick(400);
 
     expect(term).toBe(TEXT);
-  }));
-
-  it('should handle document click', fakeAsync(() => {
-    component.active = true;
-    document.dispatchEvent(new Event('click'));
-    tick();
-
-    expect(component.active).toBeFalsy();
-  }));
-
-  it('should skip click on the component', fakeAsync(() => {
-    component.active = true;
-    fixture.nativeElement.dispatchEvent(new Event('click'));
-    tick();
-
-    expect(component.active).toBeTruthy();
-  }));
-
-  it('should skip click if input has text', fakeAsync(() => {
-    component.active = true;
-    component.input = {
-      nativeElement: {
-        value: 'hola'
-      }
-    };
-    fixture.nativeElement.dispatchEvent(new Event('click'));
-    tick();
-
-    expect(component.active).toBeTruthy();
   }));
 });
