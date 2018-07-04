5e8c7149bfebd9db1aa0a07dac747e13ad920c7d
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2018-07-04 09:46:34 -0400

    [ADM-3894] Refactoring

--- src/app/catalog/upload/upload-realestate/upload-realestate.component.spec.ts
+++ src/app/catalog/upload/upload-realestate/upload-realestate.component.spec.ts
@@ -191,7 +191,7 @@ describe('UploadRealestateComponent', () => {
         component.uploadForm.get('title').patchValue('new title');
         fixture.detectChanges();
 
-        expect(formChanged).toBeTruthy();
+        expect(formChanged).toBe(true);
       });
     });
 
@@ -317,6 +317,7 @@ describe('UploadRealestateComponent', () => {
     it('should call updateRealEstateLocation when location change if in edit mode', () => {
       component.item = MOCK_REALESTATE;
       spyOn(itemService, 'updateRealEstateLocation').and.callThrough();
+
       component.emitLocation();
 
       expect(itemService.updateRealEstateLocation).toHaveBeenCalledWith(MOCK_REALESTATE.id, USER_LOCATION_COORDINATES);
