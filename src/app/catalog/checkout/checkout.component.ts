2462c21d0ba5a1066a246a383dc1137daff4ce1d
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2018-07-04 10:34:45 -0400

    [ADM-3891] Feature a product from queryparam

--- src/app/catalog/checkout/checkout.component.ts
+++ src/app/catalog/checkout/checkout.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ItemService } from '../../core/item/item.service';
 import { ItemWithProducts } from '../../core/item/item-response.interface';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { BumpTutorialComponent } from './bump-tutorial/bump-tutorial.component';
 
 @Component({
@@ -16,18 +16,38 @@ export class CheckoutComponent implements OnInit {
   @ViewChild(BumpTutorialComponent) bumpTutorial: BumpTutorialComponent;
 
   constructor(private itemService: ItemService,
-              private router: Router) {
+              private router: Router,
+              private route: ActivatedRoute) {
   }
 
   ngOnInit() {
+    this.route.params.subscribe((params: any) => {
+      if (params.itemId) {
+        this.getProductsFromParamsItem(params.itemId);
+      } else {
+        this.getProductsFromSelectedItems();
+      }
+    });
+
+  }
+
+  private getProductsFromSelectedItems() {
     if (!this.itemService.selectedItems.length) {
       this.router.navigate(['catalog/list']);
       return;
     }
-    this.itemService.getItemsWithAvailableProducts(this.itemService.selectedItems).subscribe((itemsWithProducts: ItemWithProducts[]) => {
-      this.itemsWithProducts = itemsWithProducts;
-      this.provincialBump = !this.itemsWithProducts[0].products['168'].citybump;
-    });
+    this.itemService.getItemsWithAvailableProducts(this.itemService.selectedItems)
+      .subscribe((itemsWithProducts: ItemWithProducts[]) => this.setItems(itemsWithProducts));
+  }
+
+  private getProductsFromParamsItem(itemId: string) {
+    this.itemService.getItemsWithAvailableProducts([itemId])
+      .subscribe((itemsWithProducts: ItemWithProducts[]) => this.setItems(itemsWithProducts));
+  }
+
+  private setItems(itemsWithProducts: ItemWithProducts[]) {
+    this.itemsWithProducts = itemsWithProducts;
+    this.provincialBump = !this.itemsWithProducts[0].products['168'].citybump;
   }
 
 }
