555bb123d68dcd8fbb182b16289ec6529e7a34dd
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2018-07-09 09:27:27 -0400

    [ADM-3845] Wallacoins base layout

--- src/app/wallacoins/wallacoins.module.ts
+++ src/app/wallacoins/wallacoins.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { wallacoinsRoutedComponents, WallacoinsRoutingModule } from './wallacoins.routes';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule,
     WallacoinsRoutingModule
   ],
   declarations: [
