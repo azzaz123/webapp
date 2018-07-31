2011d6d9a739304a8065fbd5e0b6231b6814fd7c
enric.pagerols <enric.pagerols@wallapop.com>
2018-07-31 11:26:13 +0200

    [CLOUD-675] CLOUD-675 Add headlesschrome

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,10 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+const ChromiumRevision = require('puppeteer/package.json').puppeteer.chromium_revision;
+const Downloader = require('puppeteer/utils/ChromiumDownloader');
+const revisionInfo = Downloader.revisionInfo(Downloader.currentPlatform(), ChromiumRevision);
+
+process.env.CHROME_BIN = revisionInfo.executablePath;
 
 module.exports = function (config) {
   config.set({
@@ -29,7 +34,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['PhantomJS'],
+    browsers: ['ChromeHeadless', 'PhantomJS', 'Chrome'],
     singleRun: true,
     browserNoActivityTimeout: 60000
   });
