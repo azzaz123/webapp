0eb0b1454780d00ca659ae45d79ec65b0865e76b
enric.pagerols <enric.pagerols@wallapop.com>
2018-09-13 10:03:09 +0200

    [angular-update] ADM-4086 remove single run from karma

--- karma.conf.js
+++ karma.conf.js
@@ -27,9 +27,8 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
+    autoWatch: false,
     browsers: ['HeadlessChrome', 'Chrome'],
-    singleRun: false,
     customLaunchers:{
       HeadlessChrome:{
         base: 'ChromeHeadless',
