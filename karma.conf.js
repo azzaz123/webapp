eb13d0aba9323d334389d9731638edd534900970
enric.pagerols <enric.pagerols@wallapop.com>
2018-09-14 12:02:54 +0200

    [angular-update] ADM-4086 Downgrade Jasmine and Karma

--- karma.conf.js
+++ karma.conf.js
@@ -11,12 +11,14 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-spec-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client:{
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      jasmine: {
+        random: false
+      }
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
