efb5538b3d17d07a40a6347443e24aac8e04453a
Alexandra Haret <alexandra.haret@wallapop.com>
2018-10-10 17:01:34 +0200

    [CHATO-2387] Add missing unit tests

--- src/app/core/message/archive.service.spec.ts
+++ src/app/core/message/archive.service.spec.ts
@@ -119,12 +119,12 @@ describe('MsgArchiveService', () => {
     userSerice = TestBed.get(UserService);
   });
 
-  describe('getEventsSince', () => {
+  describe('getAllEvents', () => {
     it('should make a get request to the chat endpoint with the correct parameters', () => {
       spyOn(http, 'get').and.returnValue(Observable.of({}));
-      service.getEventsSince('0');
+      service.getAllEvents('mockThreadId');
 
-      expect(http.get).toHaveBeenCalledWith(API_URL, new Object({ since: '0', 'page-size': pageSize }));
+      expect(http.get).toHaveBeenCalledWith(API_URL, new Object({ since: '0', 'page-size': pageSize, 'conversation_hash': 'mockThreadId' }));
     });
 
     it('should make more http.get requests if the header returns a x-nextpage since value', () => {
@@ -139,19 +139,19 @@ describe('MsgArchiveService', () => {
       });
       spyOn(http, 'get').and.returnValues(Observable.of(new Response(res)), Observable.of(new Response(res2)));
 
-      service.getEventsSince('0').subscribe();
+      service.getAllEvents('mockThreadId').subscribe();
 
-      expect(http.get).toHaveBeenCalledWith(API_URL, new Object({ since: '0', 'page-size': pageSize }));
-      expect(http.get).toHaveBeenCalledWith(API_URL, new Object({ since: nextPageTimestamp, 'page-size': pageSize }));
+      expect(http.get).toHaveBeenCalledWith(API_URL, new Object({ since: '0', 'page-size': pageSize, 'conversation_hash': 'mockThreadId' }));
+      expect(http.get).toHaveBeenCalledWith(API_URL, new Object({ since: nextPageTimestamp, 'page-size': pageSize, 'conversation_hash': 'mockThreadId' }));
     });
 
-    it('should process and add each message from returned by the get request into response.messages', fakeAsync(() => {
+    it('should process and add each message returned by the get request into response.messages', fakeAsync(() => {
       const expectedMessages = createMockMessageEvents(12);
       const res: ResponseOptions = new ResponseOptions({ body: JSON.stringify(expectedMessages), headers: new Headers({}) });
       spyOn(http, 'get').and.returnValue(Observable.of(new Response(res)));
       let response: MsgArchiveResponse;
 
-      service.getEventsSince('0').subscribe(r => response = r);
+      service.getAllEvents('mockThreadId').subscribe(r => response = r);
       tick();
 
       expectedMessages.forEach(m => {
@@ -168,13 +168,126 @@ describe('MsgArchiveService', () => {
         spyOn(http, 'get').and.returnValue(Observable.of(new Response(res)));
         let response: MsgArchiveResponse;
 
-        service.getEventsSince('0').subscribe(r => response = r);
+        service.getAllEvents('mockThreadId').subscribe(r => response = r);
         tick();
 
         response.messages.map(m => {
           m.fromSelf ? expect(m.status).toBe(messageStatus.SENT) : expect(m.status).toBe(messageStatus.RECEIVED);
         });
       }));
+
+    describe('process receivedReceipts', () => {
+      const expectedReceivedReceipts = createMockReceivedEvents(5);
+      const duplicatedEvents = expectedReceivedReceipts.concat(expectedReceivedReceipts);
+      const res: ResponseOptions = new ResponseOptions({ body: JSON.stringify(duplicatedEvents), headers: new Headers({}) });
+
+      it('should process and add ONLY unique receivedReceipts returned by the get request into response.receivedReceipts', fakeAsync(() => {
+        spyOn(http, 'get').and.returnValue(Observable.of(new Response(res)));
+        let response: MsgArchiveResponse;
+
+        service.getAllEvents('mockThreadId').subscribe(r => response = r);
+        tick();
+
+        expectedReceivedReceipts.forEach(m => {
+          const receiptCount = response.receivedReceipts.filter(msg => msg.messageId === m.event.message_id).length;
+          expect(receiptCount).toBe(1);
+        });
+        expect(response.receivedReceipts.length).toBe(expectedReceivedReceipts.length);
+      }));
+    });
+
+    describe('process readReceipts', () => {
+      const allReadReceipts = createMockReadEvents(10);
+      const res: ResponseOptions = new ResponseOptions({ body: JSON.stringify(allReadReceipts), headers: new Headers({}) });
+      let response: MsgArchiveResponse;
+      let readReceiptFromSelf;
+      let readReceiptFromOther;
+
+      beforeEach(fakeAsync(() => {
+        spyOn(http, 'get').and.returnValue(Observable.of(new Response(res)));
+
+        service.getAllEvents('mockThreadId').subscribe(r => response = r);
+        tick();
+
+        readReceiptFromSelf = response.readReceipts.filter(r => r.to === OTHER_USER_ID);
+        readReceiptFromOther = response.readReceipts.filter(r => r.to === USER_ID);
+      }));
+
+      it('should add only the latest readReceipt from self to the response', () => {
+        allReadReceipts.filter(m => m.event.to_user_hash === OTHER_USER_ID).map(ev => {
+          expect(readReceiptFromSelf[0].timestamp).toBeGreaterThanOrEqual(Number(ev.event.created_ts));
+        });
+        expect(readReceiptFromSelf.length).toBe(1);
+      });
+
+      it('should add only the latest readReceipt from other to the response', () => {
+        allReadReceipts.filter(m => m.event.to_user_hash === USER_ID).map(ev => {
+          expect(readReceiptFromOther[0].timestamp).toBeGreaterThanOrEqual(Number(ev.event.created_ts));
+        });
+        expect(readReceiptFromOther.length).toBe(1);
+      });
+    });
+  });
+
+  describe('getEventsSince', () => {
+    const timestamp = (new Date('2018-10-01')).toISOString();
+    const nanoTimestamp = (new Date(timestamp).getTime() / 1000) + '000';
+
+    it('should make a get request to the chat endpoint with the correct parameters', () => {
+      spyOn(http, 'get').and.returnValue(Observable.of({}));
+      service.getEventsSince(timestamp);
+
+      expect(http.get).toHaveBeenCalledWith(API_URL, new Object({ since: nanoTimestamp, 'page-size': pageSize }));
+    });
+
+    it('should make more http.get requests if the header returns a x-nextpage since value', () => {
+      const nextPageTimestamp = '1234567890.000000';
+      const res: ResponseOptions = new ResponseOptions({
+        body: JSON.stringify(createMockMessageEvents(3)),
+        headers: new Headers({ 'x-nextpage': 'since=' + nextPageTimestamp })
+      });
+      const res2: ResponseOptions = new ResponseOptions({
+        body: JSON.stringify({}),
+        headers: new Headers({})
+      });
+      spyOn(http, 'get').and.returnValues(Observable.of(new Response(res)), Observable.of(new Response(res2)));
+
+      service.getEventsSince(timestamp).subscribe();
+
+      expect(http.get).toHaveBeenCalledWith(API_URL, new Object({ since: nanoTimestamp, 'page-size': pageSize }));
+      expect(http.get).toHaveBeenCalledWith(API_URL, new Object({ since: nextPageTimestamp, 'page-size': pageSize }));
+    });
+
+    it('should process and add each message returned by the get request into response.messages', fakeAsync(() => {
+      const expectedMessages = createMockMessageEvents(12);
+      const res: ResponseOptions = new ResponseOptions({ body: JSON.stringify(expectedMessages), headers: new Headers({}) });
+      spyOn(http, 'get').and.returnValue(Observable.of(new Response(res)));
+      let response: MsgArchiveResponse;
+
+      service.getEventsSince(timestamp).subscribe(r => response = r);
+      tick();
+
+      expectedMessages.forEach(m => {
+        const msgInResponse = response.messages.find(msg => msg.id === m.event.message_id);
+        expect(msgInResponse instanceof Message).toBe(true);
+      });
+      expect(response.messages.length).toBe(expectedMessages.length);
+    }));
+
+    it(`should set the status to RECEIVED for all messages that are NOT fromSelf nd SENT for messages fromSelf,
+    before processig receipts`, fakeAsync(() => {
+      const messages = createMockMessageEvents(6);
+      const res: ResponseOptions = new ResponseOptions({ body: JSON.stringify(messages), headers: new Headers({}) });
+      spyOn(http, 'get').and.returnValue(Observable.of(new Response(res)));
+      let response: MsgArchiveResponse;
+
+      service.getEventsSince(timestamp).subscribe(r => response = r);
+      tick();
+
+      response.messages.map(m => {
+        m.fromSelf ? expect(m.status).toBe(messageStatus.SENT) : expect(m.status).toBe(messageStatus.RECEIVED);
+      });
+    }));
 
     describe('process receivedReceipts', () => {
       const expectedReceivedReceipts = createMockReceivedEvents(5);
@@ -227,84 +340,85 @@ describe('MsgArchiveService', () => {
         expect(readReceiptFromOther.length).toBe(1);
       });
     });
-
-    describe('updateStatuses', () => {
-      it(`should update the status of messages fromSelf to RECEIVED, when the server response includes
-      a corresponding receipt`, fakeAsync(() => {
-          const receivedEvent = [{
-            event: {
-              conversation_hash: CONVERSATION_ID,
-              created_ts: severMessagesFromSelf[0].event.created_ts,
-              from_user_hash: OTHER_USER_ID,
-              message_id: severMessagesFromSelf[0].event.message_id,
-              to_user_hash: USER_ID
-            },
-            id: severMessagesFromSelf[0].event.message_id,
-            ts: severMessagesFromSelf[0].ts,
-            type: 'chat.message.received'
-          }];
-          const serverResponse = severMessagesFromSelf.concat(receivedEvent);
-          const res: ResponseOptions = new ResponseOptions({ body: JSON.stringify(serverResponse), headers: new Headers({}) });
-          spyOn(http, 'get').and.returnValue(Observable.of(new Response(res)));
-          let response: MsgArchiveResponse;
-
-          service.getEventsSince('0').subscribe(r => response = r);
-          tick();
-
-          expect(response.messages[0].status).toBe(messageStatus.RECEIVED);
-          expect(response.messages[1].status).toBe(messageStatus.SENT);
-          expect(response.messages[2].status).toBe(messageStatus.SENT);
-        }));
-
-      it(`should update the status of messages fromSelf to READ, for messages with a timestamp prior or equal to the
-      read receipt timestamp`, fakeAsync(() => {
-          const readReceipt = [{
-            event: {
-              conversation_hash: CONVERSATION_ID,
-              created_ts: severMessagesFromSelf[1].event.created_ts,
-              to_user_hash: USER_ID
-            },
-            id: '1',
-            ts: severMessagesFromSelf[1].event.created_ts,
-            type: 'chat.conversation.read'
-          }];
-          const serverResponse = severMessagesFromSelf.concat(readReceipt);
-          const res: ResponseOptions = new ResponseOptions({ body: JSON.stringify(serverResponse), headers: new Headers({}) });
-          spyOn(http, 'get').and.returnValue(Observable.of(new Response(res)));
-          let response: MsgArchiveResponse;
-
-          service.getEventsSince('0').subscribe(r => response = r);
-          tick();
-
-          expect(response.messages[0].status).toBe(messageStatus.READ);
-          expect(response.messages[1].status).toBe(messageStatus.READ);
-          expect(response.messages[2].status).toBe(messageStatus.SENT);
-        }));
-
-      it(`should update the status of messages NOT fromSelf to READ, for messages with a timestamp prior or equal to the
-        read receipt timestamp`, fakeAsync(() => {
-          const readReceipt = [{
-            event: {
-              conversation_hash: CONVERSATION_ID,
-              created_ts: severMessagesFromOther[0].event.created_ts,
-              to_user_hash: OTHER_USER_ID
-            },
-            id: '1',
-            ts: severMessagesFromOther[1].event.created_ts,
-            type: 'chat.conversation.read'
-          }];
-          const serverResponse = severMessagesFromOther.concat(readReceipt);
-          const res: ResponseOptions = new ResponseOptions({ body: JSON.stringify(serverResponse), headers: new Headers({}) });
-          spyOn(http, 'get').and.returnValue(Observable.of(new Response(res)));
-          let response: MsgArchiveResponse;
-
-          service.getEventsSince('0').subscribe(r => response = r);
-          tick();
-
-          expect(response.messages[0].status).toBe(messageStatus.READ);
-          expect(response.messages[1].status).toBe(messageStatus.RECEIVED);
-          expect(response.messages[2].status).toBe(messageStatus.RECEIVED);
-        }));
-    });
   });
+
+  describe('updateStatuses', () => {
+    it(`should update the status of messages fromSelf to RECEIVED, when the server response includes a
+    corresponding receipt`, fakeAsync(() => {
+      const receivedEvent = [{
+        event: {
+          conversation_hash: CONVERSATION_ID,
+          created_ts: severMessagesFromSelf[0].event.created_ts,
+          from_user_hash: OTHER_USER_ID,
+          message_id: severMessagesFromSelf[0].event.message_id,
+          to_user_hash: USER_ID
+        },
+        id: severMessagesFromSelf[0].event.message_id,
+        ts: severMessagesFromSelf[0].ts,
+        type: 'chat.message.received'
+      }];
+      const serverResponse = severMessagesFromSelf.concat(receivedEvent);
+      const res: ResponseOptions = new ResponseOptions({ body: JSON.stringify(serverResponse), headers: new Headers({}) });
+      spyOn(http, 'get').and.returnValue(Observable.of(new Response(res)));
+      let response: MsgArchiveResponse;
+
+      service.getEventsSince('0').subscribe(r => response = r);
+      tick();
+
+      expect(response.messages[0].status).toBe(messageStatus.RECEIVED);
+      expect(response.messages[1].status).toBe(messageStatus.SENT);
+      expect(response.messages[2].status).toBe(messageStatus.SENT);
+    }));
+
+    it(`should update the status of messages fromSelf to READ, for messages with a timestamp prior or equal to
+    the read receipt timestamp`, fakeAsync(() => {
+      const readReceipt = [{
+        event: {
+          conversation_hash: CONVERSATION_ID,
+          created_ts: severMessagesFromSelf[1].event.created_ts,
+          to_user_hash: USER_ID
+        },
+        id: '1',
+        ts: severMessagesFromSelf[1].event.created_ts,
+        type: 'chat.conversation.read'
+      }];
+      const serverResponse = severMessagesFromSelf.concat(readReceipt);
+      const res: ResponseOptions = new ResponseOptions({ body: JSON.stringify(serverResponse), headers: new Headers({}) });
+      spyOn(http, 'get').and.returnValue(Observable.of(new Response(res)));
+      let response: MsgArchiveResponse;
+
+      service.getEventsSince('0').subscribe(r => response = r);
+      tick();
+
+      expect(response.messages[0].status).toBe(messageStatus.READ);
+      expect(response.messages[1].status).toBe(messageStatus.READ);
+      expect(response.messages[2].status).toBe(messageStatus.SENT);
+    }));
+
+    it(`should update the status of messages NOT fromSelf to READ, for messages with a timestamp prior or equal to
+    the read receipt timestamp`, fakeAsync(() => {
+      const readReceipt = [{
+        event: {
+          conversation_hash: CONVERSATION_ID,
+          created_ts: severMessagesFromOther[0].event.created_ts,
+          to_user_hash: OTHER_USER_ID
+        },
+        id: '1',
+        ts: severMessagesFromOther[1].event.created_ts,
+        type: 'chat.conversation.read'
+      }];
+      const serverResponse = severMessagesFromOther.concat(readReceipt);
+      const res: ResponseOptions = new ResponseOptions({ body: JSON.stringify(serverResponse), headers: new Headers({}) });
+      spyOn(http, 'get').and.returnValue(Observable.of(new Response(res)));
+      let response: MsgArchiveResponse;
+
+      service.getEventsSince('0').subscribe(r => response = r);
+      tick();
+
+      expect(response.messages[0].status).toBe(messageStatus.READ);
+      expect(response.messages[1].status).toBe(messageStatus.RECEIVED);
+      expect(response.messages[2].status).toBe(messageStatus.RECEIVED);
+    }));
+  });
+
 });
