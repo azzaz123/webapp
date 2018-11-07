a52150990309ae481709cc95a91d7b0bbab8ac73
Alex Masnou <alex.masnou@wallapop.com>
2018-11-07 11:55:14 +0100

    [angular-update] Auto run tests

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: false,
+    autoWatch: true,
     browsers: ['HeadlessChrome', 'Chrome'],
     singleRun: false,
     customLaunchers:{
