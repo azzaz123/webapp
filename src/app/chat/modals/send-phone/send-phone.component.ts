20ff281c30cfec68b1017610db05877605ab1c9a
Alexandra Haret <alexandra.haret@wallapop.com>
2018-11-08 15:10:34 +0100

    [CHATO-2757] Add validation & error toast for phone number input

--- src/app/chat/modals/send-phone/send-phone.component.ts
+++ src/app/chat/modals/send-phone/send-phone.component.ts
@@ -3,7 +3,8 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { MessageService } from '../../../core/message/message.service';
 import { Conversation } from '../../../core/conversation/conversation';
 import { PersistencyService } from '../../../core/persistency/persistency.service';
-import { Observable } from 'rxjs/Observable';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { ErrorsService } from '../../../core/errors/errors.service';
 
 @Component({
   selector: 'tsl-send-phone',
@@ -14,25 +15,39 @@ export class SendPhoneComponent implements OnInit, AfterContentInit {
 
   @Input() conversation: Conversation;
   @ViewChild('phoneInput') phoneField: ElementRef;
-  private phone: Observable<string>;
+  public sendPhoneForm: FormGroup;
 
   constructor(
-    public activeModal: NgbActiveModal,
+    private fb: FormBuilder,
     private messageService: MessageService,
-    private persistencyService: PersistencyService
-  ) { }
+    private persistencyService: PersistencyService,
+    private errorsService: ErrorsService,
+    public activeModal: NgbActiveModal
+  ) {
+    this.sendPhoneForm = this.fb.group({
+      phone: ['', [Validators.required]]
+    });
+  }
 
   ngOnInit() {
-    this.persistencyService.getPhoneNumber().subscribe((r) => this.phone = r.phone);
+    this.persistencyService.getPhoneNumber().subscribe((r) => {
+      this.sendPhoneForm.setValue({
+        phone: r.phone,
+      });
+    });
   }
 
   ngAfterContentInit() {
     this.phoneField.nativeElement.focus();
   }
 
-  public createPhoneNumberMessage(phone) {
-    this.messageService.createPhoneNumberMessage(this.conversation, phone);
-    this.activeModal.close();
+  public createPhoneNumberMessage() {
+    if (this.sendPhoneForm.valid) {
+      this.messageService.createPhoneNumberMessage(this.conversation, this.sendPhoneForm.value.phone);
+      this.activeModal.close();
+    } else if (!this.sendPhoneForm.controls.phone.valid) {
+      this.sendPhoneForm.controls.phone.markAsDirty();
+      this.errorsService.i18nError('formErrors');
+    }
   }
-
 }
