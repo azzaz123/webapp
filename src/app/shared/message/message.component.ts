98d554393eea9e60ea09886195c85724e97de66a
Alexandra Haret <alexandra.haret@wallapop.com>
2018-11-08 10:32:31 +0100

    [CHATO-2757] Add method to open SendPhone modal from phoneRequest message

--- src/app/shared/message/message.component.ts
+++ src/app/shared/message/message.component.ts
@@ -1,5 +1,8 @@
 import { Component, Inject, Input, OnInit } from '@angular/core';
-import { Message } from '../../core/message/message';
+import { Message, phoneRequestState } from '../../core/message/message';
+import { NgbModalRef, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { SendPhoneComponent } from '../../chat/modals/send-phone/send-phone.component';
+import { Conversation } from '../../core/conversation/conversation';
 
 @Component({
   selector: 'tsl-message',
@@ -11,12 +14,21 @@ export class MessageComponent implements OnInit {
   @Input() message: Message;
   @Input() showUserInfo: boolean;
   @Input() callsPanel: boolean;
+  @Input() currentConversation: Conversation;
   public userWebSlug: string;
+  public phoneRequestState = phoneRequestState;
 
-  constructor(@Inject('SUBDOMAIN') private subdomain: string) {
+  constructor(@Inject('SUBDOMAIN')
+    private subdomain: string,
+    private modalService: NgbModal) {
   }
 
   ngOnInit() {
     this.userWebSlug = this.message.user ? this.message.user.getUrl(this.subdomain) : null;
+  }
+
+  openDialog() {
+    const modalRef: NgbModalRef = this.modalService.open(SendPhoneComponent, {windowClass: 'phone-request'});
+    modalRef.componentInstance.conversation = this.currentConversation;
   }
 }
