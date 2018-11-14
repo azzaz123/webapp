15faa895e17d33da92563aed5b9d1d5077d219e2
Alexandra Haret <alexandra.haret@wallapop.com>
2018-11-14 09:37:55 +0100

    [CHATO-2757] Add unit test for opening modal in message component

--- src/app/shared/message/message.component.spec.ts
+++ src/app/shared/message/message.component.spec.ts
@@ -1,16 +1,24 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { MessageComponent } from './message.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NO_ERRORS_SCHEMA, DebugElement } from '@angular/core';
 import { MOCK_MESSAGE } from '../../../tests/message.fixtures.spec';
 import { USER_ID, USER_WEB_SLUG } from '../../../tests/user.fixtures.spec';
 import { User } from '../../core/user/user';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { By } from '@angular/platform-browser';
+import { SendPhoneComponent } from '../../chat/modals/send-phone/send-phone.component';
+import { phoneRequestState } from '../../core/message/message';
+import { MOCK_CONVERSATION } from '../../../tests/conversation.fixtures.spec';
 
 const WEB_SLUG_USER = 'https://www.wallapop.com/user/';
 
 describe('Component: Message', () => {
   let component: MessageComponent;
   let fixture: ComponentFixture<MessageComponent>;
+  let modalService: NgbModal;
+  let element: DebugElement;
+  const componentInstance: any = { SendPhoneComponent: jasmine.createSpy('SendPhoneComponent') };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,6 +26,16 @@ describe('Component: Message', () => {
       schemas: [NO_ERRORS_SCHEMA],
       providers: [
         {provide: 'SUBDOMAIN', useValue: 'www'},
+        {
+          provide: NgbModal, useValue: {
+            open() {
+              return {
+                result: Promise.resolve(),
+                componentInstance: componentInstance
+              };
+            }
+          }
+        },
       ]
     })
     .compileComponents();
@@ -25,6 +43,7 @@ describe('Component: Message', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MessageComponent);
+    modalService = TestBed.get(NgbModal);
     component = fixture.componentInstance;
     MOCK_MESSAGE.user = new User(USER_ID, null, null, null, null, null, null, null, null, null, null, null, null, USER_WEB_SLUG);
     component.message = MOCK_MESSAGE;
@@ -34,6 +53,36 @@ describe('Component: Message', () => {
   describe('ngOnInit', () => {
     it('should set userWebSlug', () => {
       expect(component.userWebSlug).toBe(WEB_SLUG_USER + USER_WEB_SLUG);
+    });
+  });
+
+
+  describe('openDialog', () => {
+    beforeEach(() => {
+      spyOn(modalService, 'open').and.callThrough();
+      component.message.phoneRequest = phoneRequestState.pending;
+      component.currentConversation = MOCK_CONVERSATION();
+      fixture.detectChanges();
+      element = fixture.debugElement.queryAll(By.css('button.btn'))[0];
+      component.ngOnInit();
+    });
+
+    it('should open phoneRequest modal when the button is clicked', () => {
+      element.triggerEventHandler('click', {});
+
+      expect(modalService.open).toHaveBeenCalledWith(SendPhoneComponent, {
+        windowClass: 'phone-request'
+      });
+    });
+
+    it('should set the modal conversation to the currentConversation, when the modal is opened by clicking the button', () => {
+      component['modalRef'] = <any>{
+        componentInstance: componentInstance
+      };
+
+      element.triggerEventHandler('click', {});
+
+      expect(component['modalRef'].componentInstance.conversation).toBe(component.currentConversation);
     });
   });
 
