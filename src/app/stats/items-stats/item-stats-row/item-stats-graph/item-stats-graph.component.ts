1415b4b490abd5790864ddc4072dd8d25c0da398
enric.pagerols <enric.pagerols@wallapop.com>
2018-11-19 15:27:26 +0100

    [5.19.0] Fix day of week in user stats

--- src/app/stats/items-stats/item-stats-row/item-stats-graph/item-stats-graph.component.ts
+++ src/app/stats/items-stats/item-stats-row/item-stats-graph/item-stats-graph.component.ts
@@ -168,6 +168,9 @@ export class ItemStatsGraphComponent implements AfterViewInit, OnDestroy {
         'valueField': 'views',
         'fillColorsField': 'colorViews',
       });
+      this.chartOptions.chartCursor.categoryBalloonFunction = function(date) {
+        return date.toLocaleDateString(this.locale, { weekday: 'long', day: 'numeric' });
+      };
     }
     if (this.locale === 'es') {
       this.AmCharts.dayNames = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
