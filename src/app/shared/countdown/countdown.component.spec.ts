5f6f2e95d95826a68c0689b56e0f82f1cfcc2bb2
tmirun <tmirun@hotmail.com>
2018-11-19 12:28:26 +0100

    [ADM-4178] correct moment test sequence

--- src/app/shared/countdown/countdown.component.spec.ts
+++ src/app/shared/countdown/countdown.component.spec.ts
@@ -29,12 +29,13 @@ describe('CountdownComponent', () => {
 
   it('should accept moment format', () => {
     const lastTime = moment().add(2, 'hours');
-    const diffTime = lastTime.diff(moment());
-    const format = 'DDD HH:mm:ss';
+
     component.value = lastTime.valueOf();
     component.format = format;
     component.ngOnInit();
-    console.log(component.message);
+
+    const diffTime = lastTime.diff(moment());
+    const format = 'DDD HH:mm:ss';
     expect(component.message).toEqual(moment.utc(diffTime).format(format));
   });
 
