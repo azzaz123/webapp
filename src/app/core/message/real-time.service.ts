a365a0d3ad025ee1a070b5bb02f5ccb34e402749
Alexandra Haret <alexandra.haret@wallapop.com>
2018-12-05 16:56:52 +0100

    [CHATO-2809] Add subscribeEventNewMessage to handle sending deliveryReceipts

--- src/app/core/message/real-time.service.ts
+++ src/app/core/message/real-time.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
 import { XmppService } from '../xmpp/xmpp.service';
 import { Conversation } from '../conversation/conversation';
+import { EventService } from '../event/event.service';
+import { Message } from './message';
+import { PersistencyService } from '../persistency/persistency.service';
 
 @Injectable()
 export class RealTimeService {
 
-  constructor(private xmpp: XmppService) { }
+  constructor(private xmpp: XmppService,
+              private eventService: EventService,
+              private persistencyService: PersistencyService) {
+    this.subscribeEventNewMessage();
+  }
 
   public connect(userId: string, accessToken: string) {
     this.xmpp.connect(userId, accessToken);
@@ -30,4 +37,17 @@ export class RealTimeService {
   public sendRead(to: string, conversationId: string) {
     this.xmpp.sendConversationStatus(to, conversationId);
   }
+
+  private subscribeEventNewMessage() {
+    this.eventService.subscribe(EventService.NEW_MESSAGE, (message: Message, replaceTimestamp: boolean, withDeliveryReceipt: boolean) => {
+      if (!message.fromSelf && withDeliveryReceipt) {
+        this.persistencyService.findMessage(message.id).subscribe(() => { }, (error) => {
+          if (error.reason === 'missing') {
+            this.sendDeliveryReceipt(message.from, message.id, message.conversationId);
+          }
+        });
+      }
+    });
+  }
+
 }
