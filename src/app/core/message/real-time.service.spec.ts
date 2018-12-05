2ff508fa49c63619ea7d321f2b37b0680eaf3f3c
Alexandra Haret <alexandra.haret@wallapop.com>
2018-12-05 16:57:20 +0100

    [CHATO-2809] Add unit tests for sendReceivedReceipt

--- src/app/core/message/real-time.service.spec.ts
+++ src/app/core/message/real-time.service.spec.ts
@@ -5,6 +5,14 @@ import { EventService } from '../event/event.service';
 import { PersistencyService } from '../persistency/persistency.service';
 import { MockedPersistencyService } from '../../../tests/persistency.fixtures.spec';
 import { TrackingService } from '../tracking/tracking.service';
+import { Observable } from 'rxjs/Observable';
+import { Message } from './message';
+import { USER_ID, OTHER_USER_ID } from '../../../tests/user.fixtures.spec';
+import { CONVERSATION_ID } from '../../../tests/conversation.fixtures.spec';
+
+let service: RealTimeService;
+let persistencyService: PersistencyService;
+let eventService: EventService;
 
 describe('RealTimeService', () => {
   beforeEach(() => {
@@ -17,9 +25,67 @@ describe('RealTimeService', () => {
         {provide: TrackingService, useValue: {}}
       ]
     });
+
+    service = TestBed.get(RealTimeService);
+    eventService = TestBed.get(EventService);
+    persistencyService = TestBed.get(PersistencyService);
   });
 
-  it('should be created', inject([RealTimeService], (service: RealTimeService) => {
-    expect(service).toBeTruthy();
-  }));
+  describe('sendReceivedReceipt', () => {
+    it(`should call sendDeliveryReceipt when a NEW_MESSAGE event is emitted for a message that requests the delivery
+    and is NOT fromSelf`, () => {
+      spyOn(service, 'sendDeliveryReceipt');
+      spyOn(persistencyService, 'findMessage').and.returnValue(Observable.throw({
+        reason: 'missing'
+      }));
+      const msg = new Message('someId', CONVERSATION_ID, 'from other', OTHER_USER_ID);
+      msg.fromSelf = false;
+
+      eventService.emit(EventService.NEW_MESSAGE, msg, false, true);
+
+      expect(service.sendDeliveryReceipt).toHaveBeenCalledWith(msg.from, msg.id, msg.conversationId);
+    });
+
+    it('should NOT call sendDeliveryReceipt if the new message is fromSelf', () => {
+      spyOn(service, 'sendDeliveryReceipt');
+      spyOn(persistencyService, 'findMessage').and.returnValue(Observable.throw({
+        reason: 'missing'
+      }));
+      const msg = new Message('someId', CONVERSATION_ID, 'from myself!', USER_ID);
+      msg.fromSelf = true;
+
+      eventService.emit(EventService.NEW_MESSAGE, msg, false, true);
+
+      expect(service.sendDeliveryReceipt).not.toHaveBeenCalled();
+    });
+
+    it(`should NOT call sendDeliveryReceipt if a NEW_MESSAGE event is emitted without the deliveryReceipt parameter,
+      or with the deliveryRecipt parameter set to FALSE`, () => {
+      spyOn(service, 'sendDeliveryReceipt');
+      spyOn(persistencyService, 'findMessage').and.returnValue(Observable.throw({
+        reason: 'missing'
+      }));
+      const msg = new Message('someId', CONVERSATION_ID, 'from myself!', USER_ID);
+      msg.fromSelf = false;
+
+      eventService.emit(EventService.NEW_MESSAGE, msg, false, false);
+
+      expect(service.sendDeliveryReceipt).not.toHaveBeenCalled();
+
+      eventService.emit(EventService.NEW_MESSAGE, msg);
+
+      expect(service.sendDeliveryReceipt).not.toHaveBeenCalled();
+    });
+
+    it('should NOT call sendDeliveryReceipt if the message already exists (persistencyService.findMessage returns a value)', () => {
+      spyOn(service, 'sendDeliveryReceipt');
+      spyOn(persistencyService, 'findMessage').and.returnValue(Observable.of({}));
+      const msg = new Message('someId', CONVERSATION_ID, 'from myself!', USER_ID);
+      msg.fromSelf = false;
+
+      eventService.emit(EventService.NEW_MESSAGE, msg, false, true);
+
+      expect(service.sendDeliveryReceipt).not.toHaveBeenCalled();
+    });
+  });
 });
