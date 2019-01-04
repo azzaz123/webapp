f432adbd5bd98e2f110736a4b14114c2ced7ee8f
Alexandra Haret <alexandra.haret@wallapop.com>
2019-01-04 12:03:33 +0100

    [CHATO-2838] Add new MESSAGE_SENT event and correct typos in comments

--- src/app/core/event/event.service.ts
+++ src/app/core/event/event.service.ts
@@ -15,6 +15,8 @@ export class EventService {
   public static CHAT_LAST_RECEIVED_TS = 'chatLastReceivedTs';
   public static MESSAGE_READ_ACK = 'messageReadAck';
   public static MESSAGE_SENT_ACK = 'messageSentAck'; // TODO - remove after xmpp-old.service is removed
+  public static CONVERSATION_CEATED = 'conversationCreated'; // TODO - remove after xmpp-old.service is removed
+  public static MESSAGE_SENT = 'messageSent';
   public static MESSAGE_RECEIVED_ACK = 'messageReceivedAck';
   public static MSG_ARCHIVE_LOADED = 'archiveLoaded';
   public static MSG_ARCHIVE_LOADING = 'archiveLoading';
