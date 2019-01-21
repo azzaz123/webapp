175bb5631fb751cc413de82a39334c5827743e59
Alexandra Haret <alexandra.haret@wallapop.com>
2019-01-21 15:09:43 +0100

    [CHATO-2786] Remove console logs used for testing

--- src/app/core/http/http.service.ts
+++ src/app/core/http/http.service.ts
@@ -79,9 +79,6 @@ export class HttpService extends Http {
       return error.flatMap((err: HttpErrorResponse, index: number) => {
         const delay = Math.min(this.initialRetryInterval * Math.pow(2, index), this.maxRetryInterval);
         if (this.retryOnStatuses.indexOf(err.status) !== -1 && withDelayedRetry) {
-          if (typeof body === 'string') { // TODO - remove after QA testing
-            console.log('%cPACK  post retry FAILED: ' + JSON.parse(body).sessions[0].events[0].id, 'color: DarkMagenta');
-          }
           if (index === 0) {
             this.eventService.emit(EventService.HTTP_REQUEST_FAILED, url);
           }
