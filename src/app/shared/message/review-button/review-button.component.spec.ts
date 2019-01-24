b15fcf2ba2ed56192f349f07664e19cdc4e5e463
Alexandra Haret <alexandra.haret@wallapop.com>
2019-01-24 13:31:02 +0100

    [CHATO-2884] Add unit tests for reviewAsSeller in review-button.component

--- src/app/shared/message/review-button/review-button.component.spec.ts
+++ src/app/shared/message/review-button/review-button.component.spec.ts
@@ -1,3 +1,4 @@
+import * as _ from 'lodash';
 import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { ReviewButtonComponent } from './review-button.component';
 import { Observable } from 'rxjs';
@@ -10,7 +11,11 @@ import { ConversationService } from '../../../core/conversation/conversation.ser
 import { ITEM_ID, MOCK_ITEM } from '../../../../tests/item.fixtures.spec';
 import { UserService } from '../../../core/user/user.service';
 import { MOCK_MESSAGE } from '../../../../tests/message.fixtures.spec';
-import { REVIEW_DATA_BUYER } from '../../../../tests/review.fixtures.spec';
+import { MOCK_CONVERSATION } from '../../../../tests/conversation.fixtures.spec';
+import { SoldModalComponent } from '../../modals/sold-modal/sold-modal.component';
+import { ConversationUser } from '../../../core/item/item-response.interface';
+
+const conv = MOCK_CONVERSATION();
 
 describe('ReviewButtonComponent', () => {
   let component: ReviewButtonComponent;
@@ -34,7 +39,10 @@ describe('ReviewButtonComponent', () => {
           provide: ConversationService, useValue: {
           getItemFromConvId() {
             return MOCK_ITEM;
-          }
+          },
+          get() {
+            return Observable.of(conv);
+           }
         }
         },
         {
@@ -72,10 +80,6 @@ describe('ReviewButtonComponent', () => {
     fixture.detectChanges();
     reviewService = TestBed.get(ReviewService);
     modalService = TestBed.get(NgbModal);
-  });
-
-  it('should be created', () => {
-    expect(component).toBeTruthy();
   });
 
   describe('ngOnInit', () => {
@@ -117,27 +121,80 @@ describe('ReviewButtonComponent', () => {
   });
 
   describe('openDialog', () => {
-    beforeEach(fakeAsync(() => {
+    const expectedConversationsUser: ConversationUser = {
+      id: conv.user.id,
+      micro_name: conv.user.microName,
+      last_message: _.last(conv.messages.filter(msg => msg.from === conv.user.id)),
+      image: conv.user.image
+    };
+
+    beforeEach(() => {
       spyOn(modalService, 'open').and.callThrough();
-      spyOn(reviewService, 'createAsBuyer').and.callThrough();
       spyOn(localStorage, 'setItem');
-      component.openDialog();
-      tick();
-    }));
-    it('should call open', () => {
-      expect(modalService.open).toHaveBeenCalledWith(ReviewModalComponent, {windowClass: 'review'});
     });
-    it('should set item in the modal component', () => {
-      expect(modalRef.componentInstance.item).toEqual(MOCK_ITEM);
+
+    describe('as buyer', () => {
+      beforeEach(fakeAsync(() => {
+        component['isSeller'] = false;
+        component.openDialog();
+        tick();
+      }));
+
+      it('should open the ReviewModalComponent', () => {
+        expect(modalService.open).toHaveBeenCalledWith(ReviewModalComponent, {windowClass: 'review'});
+      });
+
+      it('should set item in the modal component', () => {
+        expect(modalRef.componentInstance.item).toEqual(MOCK_ITEM);
+      });
+
+      it('should set userToReview in the modal component', () => {
+        expect(modalRef.componentInstance.userToReview).toEqual(expectedConversationsUser);
+      });
+
+      it('should set thread in the modal component', () => {
+        expect(modalRef.componentInstance.thread).toEqual(component.message.conversationId);
+      });
+
+      it('should set showButton false', () => {
+        expect(component.showButton).toBe(false);
+      });
+
+      it('should set local storage', () => {
+        expect(localStorage.setItem).toHaveBeenCalledWith(USER_ID + '.item.' + ITEM_ID + '.reviewed', 'true');
+      });
     });
-    it('should call createAsBuyer', () => {
-      expect(reviewService.createAsBuyer).toHaveBeenCalledWith(REVIEW_DATA_BUYER);
-    });
-    it('should set showButton false', () => {
-      expect(component.showButton).toBeFalsy();
-    });
-    it('should set local storage', () => {
-      expect(localStorage.setItem).toHaveBeenCalledWith(USER_ID + '.item.' + ITEM_ID + '.reviewed', 'true');
+
+    describe('as seller', () => {
+      beforeEach(fakeAsync(() => {
+        component['isSeller'] = true;
+        component.openDialog();
+        tick();
+      }));
+
+      it('should open the SoldModalComponent', fakeAsync(() => {
+        expect(modalService.open).toHaveBeenCalledWith(SoldModalComponent, {windowClass: 'review'});
+      }));
+
+      it('should set item in the modal component', () => {
+        expect(modalRef.componentInstance.item).toEqual(MOCK_ITEM);
+      });
+
+      it('should set userToReview in the modal component', () => {
+        expect(modalRef.componentInstance.userToReview).toEqual(expectedConversationsUser);
+      });
+
+      it('should set isSeller to true in the modal component', () => {
+        expect(modalRef.componentInstance.isSeller).toBe(true);
+      });
+
+      it('should set canChooseBuyer to false in the modal component', () => {
+        expect(modalRef.componentInstance.canChooseBuyer).toBe(false);
+      });
+
+      it('should set showButton false', () => {
+        expect(component.showButton).toBe(false);
+      });
     });
   });
 });
