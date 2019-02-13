6f577a06f2804f1c4cf1354ea6a9cb41a41fcf17
Alexandra Haret <alexandra.haret@wallapop.com>
2019-02-13 13:30:19 +0100

    [CHATO-2900] Replace inline mock with JSON mock

--- src/app/chat-with-inbox/inbox/inbox.service.ts
+++ src/app/chat-with-inbox/inbox/inbox.service.ts
@@ -3,60 +3,14 @@ import { HttpService } from '../../core/http/http.service';
 import { Observable } from 'rxjs';
 import { PersistencyService } from '../../core/persistency/persistency.service';
 import { InboxConversation } from '../../core/conversation/conversation';
-import { Item } from '../../core/item/item';
+import { InboxItem } from '../../core/item/item';
 import { User } from '../../core/user/user';
 import { Message, messageStatus } from '../../core/message/message';
 
 @Injectable()
 
 export class InboxService {
-
-  private str = `
-  {
-    "user_hash": "mxzorp9np7z9",
-    "page": 1,
-    "page_size": 1000,
-    "conversations": [
-    {
-    "conversation_hash": "p61y8o5np265",
-    "last_message": {
-    "message_id": "02E8347D-526B-4042-8AA8-A7F343488D12",
-    "from_user_hash": "mxzorp9np7z9",
-    "to_user_hash": "7v6gwyklr5ze",
-    "text": "Vale gracias",
-    "timestamp": "2019-01-28T09:00:20.573038Z"
-    },
-    "number_of_unread_messages": 3,
-    "last_read_sent_timestamp": "2019-01-28T09:00:13.787855Z",
-    "last_read_received_timestamp": "2019-01-28T09:05:58.368427Z"
-    },
-    {
-    "conversation_hash": "pzpr20o8l463",
-    "last_message": {
-    "message_id": "58AC7C92-1441-4D65-A05B-3FF1EABE48E2",
-    "from_user_hash": "mxzorp9np7z9",
-    "to_user_hash": "xpzp3dpqnk63",
-    "text": "Vale perfecto",
-    "timestamp": "2019-01-28T08:44:01.571872Z"
-    },
-    "number_of_unread_messages": 0,
-    "last_read_sent_timestamp": "2019-01-28T08:43:48.581939Z",
-    "last_read_received_timestamp": "2019-01-27T14:52:24.006958Z"
-    },
-    {
-    "conversation_hash": "qzmym3nq7gzv",
-    "last_message": {
-    "message_id": "0C2840FB-10A9-4793-B38C-EA7C9B8EDF67",
-    "from_user_hash": "mxzorp9np7z9",
-    "to_user_hash": "g0j2wmggemjy",
-    "text": "Bones, m\u0027interessa la bici, de quin amperatje es la bateria que li falta??",
-    "timestamp": "2019-01-27T21:58:13.924516Z"
-    }
-    }
-    ]
-   }
-  `;
-  private mockResponse = JSON.parse(this.str);
+  private mockResponseUrl = 'assets/json/inbox-projection-data-contract.json';
 
   constructor(private http: HttpService,
     private persistencyService: PersistencyService) { }
