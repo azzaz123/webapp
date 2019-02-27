072d6ab2060be197048e00a78b0bd068576cfa11
Alexandra Haret <alexandra.haret@gmail.com>
2019-02-27 18:11:59 +0100

    [CHATO-2968] Add unit tests for processing enw messages in inboxService

--- src/app/core/inbox/inbox.service.spec.ts
+++ src/app/core/inbox/inbox.service.spec.ts
@@ -13,6 +13,7 @@ import { MockTrackingService } from '../../../tests/tracking.fixtures.spec';
 import { MockMessageService } from '../../../tests/message.fixtures.spec';
 import { FeatureflagService } from '../user/featureflag.service';
 import { EventService } from '../event/event.service';
+import { Message } from '../message/message';
 
 let service: InboxService;
 let http: HttpService;
@@ -64,6 +65,17 @@ describe('InboxService', () => {
       parsedConversaitonsResponse = service['buildConversations'](JSON.parse(MOCK_INBOX_API_RESPONSE).conversations);
     });
 
+    it('should subscribe to the NEW_MESSAGE and CHAT_SIGNAL events', () => {
+      spyOn(eventService, 'subscribe');
+      const eventServiceSubscribeArgs = [];
+
+      service.init();
+      eventService.subscribe['calls'].allArgs().map(call => eventServiceSubscribeArgs.push(call[0]));
+
+      expect(eventServiceSubscribeArgs).toContain(EventService.NEW_MESSAGE);
+      expect(eventServiceSubscribeArgs).toContain(EventService.CHAT_SIGNAL);
+    });
+
     it('should make an HTTP get request to get the inbox', () => {
       service.init();
 
@@ -103,6 +115,80 @@ describe('InboxService', () => {
 
       expect(eventService.emit).toHaveBeenCalledWith(EventService.CHAT_CAN_PROCESS_RT, true);
     });
+
+    describe('when a NEW_MESSAGE event is emitted', () => {
+      let conversation, currentLastMessage;
+      beforeEach(() => {
+        service.init();
+        conversation = service.conversations[0];
+        currentLastMessage = conversation.lastMessage;
+      });
+
+      it('should update the lastMessage and the modifiedDate wth the new message', () => {
+        const newMessage = new Message('mockId', conversation.id, 'hola!', 'mockUserId', new Date());
+
+        eventService.emit(EventService.NEW_MESSAGE, newMessage);
+
+        expect(service.conversations[0].lastMessage).toEqual(newMessage);
+        expect(service.conversations[0].modifiedDate).toEqual(newMessage.date);
+      });
+
+      it('should NOT update the lastMessage NOR the modifiedDate if the new message has the same ID as the current lastMessage', () => {
+        console.log(conversation, currentLastMessage);
+        const newMessage = new Message(currentLastMessage.id, conversation, 'hola!', 'mockUserId');
+        eventService.emit(EventService.NEW_MESSAGE, newMessage);
+
+        expect(conversation.lastMessage).toEqual(currentLastMessage);
+        expect(conversation.modifiedDate).toEqual(currentLastMessage.date);
+      });
+
+      it('should increment the unread counters by one for each new message not fromSelf', () => {
+        const unreadCounterBefore = service.conversations[0].unreadCounter;
+        const count = 3;
+        for (let i = 0; i < count; i++) {
+          const msg = new Message('mockId' + i, conversation.id, 'hola!', 'mockUserId');
+          msg.fromSelf = false;
+          eventService.emit(EventService.NEW_MESSAGE, msg);
+        }
+
+        expect(service.conversations[0].unreadCounter).toEqual(unreadCounterBefore + count);
+        expect(messageService.totalUnreadMessages).toEqual(unreadCounterBefore + count);
+      });
+
+      it('should NOT increase the unread counts if the new message has the same ID as the current lastMessage', () => {
+        const unreadCounterBefore = service.conversations[0].unreadCounter;
+        const newMessage = new Message(currentLastMessage.id, conversation.id, 'hola!', 'mockUserId');
+        newMessage.fromSelf = false;
+
+        eventService.emit(EventService.NEW_MESSAGE, newMessage);
+
+        expect(service.conversations[0].unreadCounter).toEqual(unreadCounterBefore);
+        expect(messageService.totalUnreadMessages).toEqual(unreadCounterBefore);
+      });
+
+      it('should only increase the unread counters for new messages NOT fromSelf and with unique IDs', () => {
+        const unreadCounterBefore = service.conversations[0].unreadCounter;
+        const newMessage = new Message('mockId', conversation.id, 'hola!', 'mockUserId');
+        newMessage.fromSelf = false;
+
+        eventService.emit(EventService.NEW_MESSAGE, newMessage);
+        eventService.emit(EventService.NEW_MESSAGE, newMessage);
+        eventService.emit(EventService.NEW_MESSAGE, newMessage);
+
+        expect(service.conversations[0].unreadCounter).toEqual(unreadCounterBefore + 1);
+        expect(messageService.totalUnreadMessages).toEqual(unreadCounterBefore + 1);
   });
 
+      it('should NOT increase conversation.unreadCount NOR messageService.totalUnreadMessages for new messages fromSelf', () => {
+        const newMessage = new Message('mockId', conversation, 'hola!', 'mockUserId');
+        newMessage.fromSelf = true;
+        const unreadCounterBefore = service.conversations[0].unreadCounter;
+
+        eventService.emit(EventService.NEW_MESSAGE, newMessage);
+
+        expect(service.conversations[0].unreadCounter).toEqual(unreadCounterBefore);
+        expect(messageService.totalUnreadMessages).toEqual(unreadCounterBefore);
+      });
+    });
+  });
 });
