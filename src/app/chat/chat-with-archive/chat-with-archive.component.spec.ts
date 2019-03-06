7277cbefcdc15080dc31e92efa70ff8668157a0b
Alexandra Haret <alexandra.haret@gmail.com>
2019-03-06 18:49:57 +0100

    [CHATO-2985] Remove placeholder tests from new components and console.log

--- src/app/chat/chat-with-archive/chat-with-archive.component.spec.ts
+++ src/app/chat/chat-with-archive/chat-with-archive.component.spec.ts
@@ -18,8 +18,4 @@ describe('ChatWithArchiveComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
