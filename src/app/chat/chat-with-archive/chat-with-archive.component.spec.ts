387de40587f6fd24bfd40f0a1ff6afd15582131f
Alexandra Haret <alexandra.haret@gmail.com>
2019-03-06 17:12:52 +0100

    [CHATO-2985] Create new chat-with-inbox / chat-with-archive child components in chat

---
+++ src/app/chat/chat-with-archive/chat-with-archive.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ChatWithArchiveComponent } from './chat-with-archive.component';
+
+describe('ChatWithArchiveComponent', () => {
+  let component: ChatWithArchiveComponent;
+  let fixture: ComponentFixture<ChatWithArchiveComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ChatWithArchiveComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ChatWithArchiveComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/chat/chat-with-inbox/chat-with-inbox.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ChatWithInboxComponent } from './chat-with-inbox.component';
+
+describe('ChatWithInboxComponent', () => {
+  let component: ChatWithInboxComponent;
+  let fixture: ComponentFixture<ChatWithInboxComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ChatWithInboxComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ChatWithInboxComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
