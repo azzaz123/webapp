537e03e465db93093717a709e51fde3fc5b2f3a2
Alexandra Haret <alexandra.haret@gmail.com>
2019-03-06 17:12:52 +0100

    [CHATO-2985] Create new chat-with-inbox / chat-with-archive child components in chat

---
+++ src/app/chat/chat-with-archive/chat-with-archive.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tsl-chat-with-archive',
+  templateUrl: './chat-with-archive.component.html',
+  styleUrls: ['./chat-with-archive.component.scss']
+})
+
+export class ChatWithArchiveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/chat/chat-with-inbox/chat-with-inbox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tsl-chat-with-inbox',
+  templateUrl: './chat-with-inbox.component.html',
+  styleUrls: ['./chat-with-inbox.component.scss']
+})
+export class ChatWithInboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
