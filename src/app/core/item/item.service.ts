c6f7dca3a95436ad0b217fd074772c66f17eb9b1
Alex Masnou <alex.masnou@wallapop.com>
2019-03-15 11:11:41 +0100

    Fix bug when editing the item

--- src/app/core/item/item.service.ts
+++ src/app/core/item/item.service.ts
@@ -261,12 +261,12 @@ export class ItemService extends ResourceService {
       content.delivery_info,
       ITEM_TYPES.CONSUMER_GOODS,
       content.extra_info ? {
-        object_type: {
-          id: content.extra_info.object_type.id.toString()
-        },
+        object_type: content.extra_info.object_type ? {
+          id: content.extra_info.object_type.id ? content.extra_info.object_type.id.toString() : null
+        } : null,
         brand: content.extra_info.brand,
         model: content.extra_info.model
-      } : undefined,
+      } : undefined
     );
   }
 
