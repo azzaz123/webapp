cbf3b1189b83248fb0dbcb059a0658057aacd587
Alexandra Haret <alexandra.haret@gmail.com>
2019-03-25 12:00:22 +0100

    [CHATO-2923] Refactor showDate, remove empty methods and add unit tests

--- src/app/chat/chat-with-inbox/current-conversation/current-conversation.component.ts
+++ src/app/chat/chat-with-inbox/current-conversation/current-conversation.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
-import { InboxMessage, phoneRequestState } from '../message/inbox-message';
+import { Component, Input } from '@angular/core';
+import { InboxMessage } from '../message/inbox-message';
 import { InboxConversation } from '../inbox/inbox-conversation/inbox-conversation';
 
 @Component({
@@ -7,21 +7,15 @@ import { InboxConversation } from '../inbox/inbox-conversation/inbox-conversatio
   templateUrl: './current-conversation.component.html',
   styleUrls: ['./current-conversation.component.scss']
 })
-export class CurrentConversationComponent implements OnInit, OnChanges {
+export class CurrentConversationComponent {
 
   @Input() currentConversation: InboxConversation;
 
   constructor() { }
 
-  ngOnInit() {
-    console.log(this.currentConversation);
-  }
 
-  ngOnChanges() {
-  }
-
-  public showDate(previousMessage: InboxMessage, currentMessage: InboxMessage): boolean {
-    return previousMessage ? new Date(previousMessage.date).toDateString() !== new Date(currentMessage.date).toDateString() : true;
+  public showDate(currentMessage: InboxMessage, nextMessage: InboxMessage): boolean {
+    return nextMessage ? new Date(currentMessage.date).toDateString() !== new Date(nextMessage.date).toDateString() : true;
   }
 
 }
