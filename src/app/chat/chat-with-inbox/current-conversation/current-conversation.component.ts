f7432d757266ddd0ac37a88a4a0f4ca6e622a774
Alexandra Haret <alexandra.haret@gmail.com>
2019-03-27 10:13:43 +0100

    [CHATO-3049] Add formatting and styles for date marker

--- src/app/chat/chat-with-inbox/current-conversation/current-conversation.component.ts
+++ src/app/chat/chat-with-inbox/current-conversation/current-conversation.component.ts
@@ -13,9 +13,21 @@ export class CurrentConversationComponent {
 
   constructor() { }
 
+  public momentConfig: any = {
+    lastDay: '[Yesterday]',
+    sameDay: '[Today]',
+    nextDay: 'dddd, D MMM',
+    lastWeek: 'dddd, D MMM',
+    nextWeek: 'dddd, D MMM',
+    sameElse: 'dddd, D MMM'
+  };
 
   public showDate(currentMessage: InboxMessage, nextMessage: InboxMessage): boolean {
     return nextMessage ? new Date(currentMessage.date).toDateString() !== new Date(nextMessage.date).toDateString() : true;
+  }
+
+  public dateIsThisYear(date) {
+    return date.getFullYear() === new Date().getFullYear();
   }
 
 }
