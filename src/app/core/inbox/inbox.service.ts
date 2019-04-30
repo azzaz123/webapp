7b465e436b76f60108b23b47b615089de6903cc8
Aitor Pagán <aitor.pagan@wallapop.com>
2019-04-30 12:46:43 +0200

    [CHATO-3111] Displaying Archived Conversations

--- src/app/core/inbox/inbox.service.ts
+++ src/app/core/inbox/inbox.service.ts
@@ -16,7 +16,9 @@ const USER_BASE_PATH = environment.siteUrl +  'user/';
 
 export class InboxService {
   private API_URL = 'bff/messaging/inbox';
+  private ARCHIVED_API_URL = '/bff/messaging/archived';
   private _conversations: InboxConversation[];
+  private _archivedConversations: InboxConversation[];
   private selfId: string;
   private nextPageToken: number = null;
   private pageSize = 30;
@@ -40,6 +42,14 @@ export class InboxService {
     return this._conversations;
   }
 
+  set archivedConversations(value: InboxConversation[]) {
+    this._archivedConversations = value;
+  }
+
+  get archivedConversations(): InboxConversation[] {
+    return this._archivedConversations;
+  }
+
   public getInboxFeatureFlag(): Observable<boolean> {
     return this.featureflagService.getFlag('web_inbox_projections');
   }
@@ -48,13 +58,18 @@ export class InboxService {
     this.conversationService.subscribeChatEvents();
     this.selfId = this.userService.user.id;
     this.conversationService.selfId = this.selfId;
-    this.getInbox()
+    this.subscribeArchiveEvents();
+    this.subscribeUnarchiveEvents();
+    Observable.forkJoin(this.getInbox(), this.getArchivedInbox())
     .catch(() => {
       this.errorRetrievingInbox = true;
-      return this.persistencyService.getStoredInbox();
+      return [this.persistencyService.getStoredInbox(), this.persistencyService.getArchivedStoredInbox()];
     })
-    .subscribe((conversations: InboxConversation[]) => {
+    .subscribe((result) => {
+      const conversations = result[0];
+      const archived = result[1];
       this.eventService.emit(EventService.INBOX_LOADED, conversations);
+      this.eventService.emit(EventService.ARCHIVED_INBOX_LOADED, archived);
       this.eventService.emit(EventService.CHAT_CAN_PROCESS_RT, true);
     });
   }
@@ -86,6 +101,15 @@ export class InboxService {
     });
   }
 
+  private getArchivedInbox(): Observable<any> {
+    return this.http.get(this.ARCHIVED_API_URL, {
+      page_size: this.pageSize
+    })
+    .map(res => {
+      return this.archivedConversations = this.processArchivedInboxResponse(res);
+    });
+  }
+
   private getNextPage(): Observable<any> {
       return this.http.get(this.API_URL, {
         page_size: this.pageSize,
@@ -105,6 +129,17 @@ export class InboxService {
         && this.conversations.find(existingConv => existingConv.id === newConv.hash)) ? null : newConv;
     });
     return this.buildConversations(newConvs);
+  }
+
+  private processArchivedInboxResponse(res: Response): InboxConversation[] {
+    const r = res.json();
+    // this.nextPageToken = r.next_from || null;
+    // In order to avoid adding repeated conversations
+    const newConvs = r.conversations.filter(newConv => {
+      return (this.archivedConversations
+        && this.archivedConversations.find(existingConv => existingConv.id === newConv.hash)) ? null : newConv;
+    });
+    return newConvs.map((conv) => InboxConversation.fromJSON(conv, this.selfId) );
   }
 
   private buildConversations(conversations): InboxConversation[] {
