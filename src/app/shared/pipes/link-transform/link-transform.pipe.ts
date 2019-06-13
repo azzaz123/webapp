f58c29b57c3305465d04a884aee07df4fa013226
pwrona <przemekwrona25@gmail.com>
2019-06-13 15:07:19 +0200

    CHATO-3291 replace link by html tag

--- src/app/shared/pipes/link-transform/link-transform.pipe.ts
+++ src/app/shared/pipes/link-transform/link-transform.pipe.ts
@@ -1,11 +1,22 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
   name: 'linkTransform'
 })
 export class LinkTransformPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return value + ' text';
+  private static readonly LINK_EXPRESION = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
+
+  transform(value: string, args?: any): any {
+    const linkRegEx = new RegExp(LinkTransformPipe.LINK_EXPRESION);
+
+    new Set(linkRegEx.exec(value))
+      .forEach(link => value = value.replace(link, this.getFormattedLink(link)));
+
+    return value;
+  }
+
+  private getFormattedLink(link: string): string {
+    return `<a href="${link}" target="_blank">${link}</a>`;
   }
 }
