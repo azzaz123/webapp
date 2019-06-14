0ff7a7cb82a3b9a0d9a71db420f132850fd2ab88
pwrona <przemekwrona25@gmail.com>
2019-06-14 11:01:08 +0200

    CHATO-3291 change return type from any to string, rename variable value to message and disable lint warning

--- src/app/shared/pipes/link-transform/link-transform.pipe.ts
+++ src/app/shared/pipes/link-transform/link-transform.pipe.ts
@@ -5,16 +5,17 @@ import {Pipe, PipeTransform} from '@angular/core';
 })
 export class LinkTransformPipe implements PipeTransform {
 
+  // tslint:disable-next-line:max-line-length
   private static readonly LINK_REG_EXP = /(https?:\/\/(?:www\.|(?!www))[\w\d][\w\d-]+[\w\d]\.[^\s]{2,}|www\.[\w\d][\w\d-]+[\w\d]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[\w\d]+\.[^\s]{2,}|www\.[\w\d]+\.[^\s]{2,})/g;
 
   private static getFormattedLink(link: string): string {
     return `<a href="${link}" target="_blank">${link}</a>`;
   }
 
-  transform(value: string, args?: any): any {
-    new Set(value.match(LinkTransformPipe.LINK_REG_EXP))
-      .forEach(link => value = value.replace(new RegExp(link, 'g'), LinkTransformPipe.getFormattedLink(link)));
+  transform(message: string, args?: any): string {
+    new Set(message.match(LinkTransformPipe.LINK_REG_EXP))
+      .forEach(link => message = message.replace(new RegExp(link, 'g'), LinkTransformPipe.getFormattedLink(link)));
 
-    return value;
+    return message;
   }
 }
