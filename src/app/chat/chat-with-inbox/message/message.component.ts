ca8c3d88fdeeafd7860b78606e020feb2fb0ac18
pwrona <przemekwrona25@gmail.com>
2019-07-09 17:51:15 +0200

    CHATO-3203 add logic to review item

--- src/app/chat/chat-with-inbox/message/message.component.ts
+++ src/app/chat/chat-with-inbox/message/message.component.ts
@@ -2,10 +2,14 @@ import { Input, OnInit } from '@angular/core';
 
 import { InboxMessage } from './inbox-message';
 import { InboxConversation } from '../inbox/inbox-conversation';
+import { InboxItem, InboxUser } from '../inbox';
 
 export abstract class MessageComponent implements OnInit {
 
   @Input() message: InboxMessage;
+  @Input() user: InboxUser;
+  @Input() item: InboxItem;
+
   @Input() currentConversation: InboxConversation;
 
   ngOnInit() {
--- src/app/chat/chat-with-inbox/message/third-voice-message/third-voice-button/third-voice-button.component.ts
+++ src/app/chat/chat-with-inbox/message/third-voice-message/third-voice-button/third-voice-button.component.ts
@@ -1,15 +1,14 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
-import { ReviewButtonComponent } from '../../../../../shared/message/review-button/review-button.component';
 import { ConversationService } from '../../../../../core/conversation/conversation.service';
 import { ReviewService } from '../../../../../core/review/review.service';
 import { UserService } from '../../../../../core/user/user.service';
 import { ReviewModalComponent } from '../../../../../shared/modals/review-modal/review-modal.component';
 import { SoldModalComponent } from '../../../../../shared/modals/sold-modal/sold-modal.component';
-import { Message } from '../../../../../core/message/message';
-import { Item } from '../../../../../core/item/item';
 import { ConversationUser } from '../../../../../core/item/item-response.interface';
+import { InboxItem, InboxUser } from '../../../inbox';
+import { InboxMessage } from '../../inbox-message';
 
 @Component({
   selector: 'tsl-third-voice-button',
@@ -18,13 +17,11 @@ import { ConversationUser } from '../../../../../core/item/item-response.interfa
 })
 export class ThirdVoiceButtonComponent implements OnInit {
 
-  @Input() message: Message;
-  @Output() finishedReview = new EventEmitter();
+  @Input() message: InboxMessage;
+  @Input() user: InboxUser;
+  @Input() item: InboxItem;
+
   public showButton: boolean;
-  private item: Item;
-  private storageKey: string;
-  private conversationUser: ConversationUser;
-  private isSeller: boolean;
 
   constructor(private conversationService: ConversationService,
               private reviewService: ReviewService,
@@ -33,46 +30,45 @@ export class ThirdVoiceButtonComponent implements OnInit {
   }
 
   ngOnInit() {
-    // this.item = this.conversationService.getItemFromThread(this.message.thread);
-    // this.conversationService.get(this.message.thread).subscribe((conv) => {
-    //   this.conversationUser = {
-    //     id: conv.user.id,
-    //     micro_name: conv.user.microName,
-    //     last_message: _.last(conv.messages.filter(msg => msg.from === conv.user.id)),
-    //     image: conv.user.image
-    //   };
-    //   this.isSeller = this.item.owner !== this.conversationUser.id;
-    // });
-    // this.storageKey = this.userService.user.id + '.item.' + this.item.id + '.reviewed';
-    // const alreadyReviewed: string = localStorage.getItem(this.storageKey);
-    // if (!alreadyReviewed) {
-    //   this.reviewService.check(this.item.id).subscribe((reviewLeft: boolean) => {
-    //     if (reviewLeft) {
-    //       localStorage.setItem(this.storageKey, 'true');
-    //       this.showButton = false;
-    //     } else {
-    //       this.showButton = true;
-    //     }
-    //   });
-    // } else {
-    //   this.showButton = false;
-    // }
-    this.showButton = true;
+    const alreadyReviewed: string = localStorage.getItem(this.getStorageKey());
+    if (!alreadyReviewed) {
+      this.reviewService.check(this.item.id).subscribe((isReviewed: boolean) => {
+        if (isReviewed) {
+          localStorage.setItem(this.getStorageKey(), 'true');
+          this.showButton = false;
+        } else {
+          this.showButton = true;
+        }
+      });
+    } else {
+      this.showButton = false;
+    }
+  }
+
+  private buildConversationUser(): ConversationUser {
+    return {
+      id: this.user.id,
+      micro_name: this.user.microName
+    } as ConversationUser;
   }
 
   public openDialog() {
-    this.isSeller ? this.reviewAsSeller() : this.reviewAsBuyer();
+    this.item.isMine ? this.reviewAsSeller() : this.reviewAsBuyer();
+  }
+
+  private getStorageKey(): string {
+    return `${this.user.id}.item.${this.item.id}.reviewed`;
   }
 
   private reviewSent() {
     this.showButton = false;
-    localStorage.setItem(this.storageKey, 'true');
+    localStorage.setItem(this.getStorageKey(), 'true');
   }
 
   private reviewAsBuyer() {
     const modalRef: NgbModalRef = this.modalService.open(ReviewModalComponent, { windowClass: 'review' });
     modalRef.componentInstance.item = this.item;
-    modalRef.componentInstance.userToReview = this.conversationUser;
+    modalRef.componentInstance.userToReview = this.buildConversationUser();
     modalRef.componentInstance.thread = this.message.thread;
     modalRef.result.then(() => this.reviewSent(), () => {
     });
@@ -81,8 +77,8 @@ export class ThirdVoiceButtonComponent implements OnInit {
   private reviewAsSeller() {
     const modalRef: NgbModalRef = this.modalService.open(SoldModalComponent, { windowClass: 'review' });
     modalRef.componentInstance.item = this.item;
-    modalRef.componentInstance.userToReview = this.conversationUser;
-    modalRef.componentInstance.isSeller = this.isSeller;
+    modalRef.componentInstance.userToReview = this.buildConversationUser();
+    modalRef.componentInstance.isSeller = this.item.isMine;
     modalRef.componentInstance.canChooseBuyer = false;
     modalRef.result.then(() => this.showButton = false, () => {
     });
