25695cca19471d73e9ead6ba60773182b3d0aeef
pwrona <przemekwrona25@gmail.com>
2019-07-09 11:51:55 +0200

    CHATO-3203 add test when clear current conversation

--- src/app/chat/chat-with-inbox/message/message.component.spec.ts
+++ src/app/chat/chat-with-inbox/message/message.component.spec.ts
@@ -1,7 +1,5 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MessageComponent } from './message.component';
-import { LinkTransformPipe } from '../../../shared/pipes/link-transform';
+import { CREATE_MOCK_INBOX_CONVERSATION } from '../../../../tests/inbox.fixtures.spec';
 
 class MessageComponentMock extends MessageComponent {
 }
@@ -11,13 +9,24 @@ describe('MessageComponent', () => {
 
   beforeEach(() => {
     component = new MessageComponentMock();
+    component.message = CREATE_MOCK_INBOX_CONVERSATION().messages[0];
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  // it('should clear current conversation if has no phone request', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should clear inbox conversation', () => {
+    component.message.phoneRequest = 'PHONE_NUMBER';
+    component.currentConversation = CREATE_MOCK_INBOX_CONVERSATION();
+    component.ngOnInit();
+    expect(component.currentConversation).not.toBeNull();
+  });
+
+  it('should not clear inbox conversation', () => {
+    component.message.phoneRequest = null;
+    component.currentConversation = CREATE_MOCK_INBOX_CONVERSATION();
+    component.ngOnInit();
+    expect(component.currentConversation).toBeNull();
+  });
 });
