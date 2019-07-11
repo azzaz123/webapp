053568e35003ad2e3536b88b4f95105b65a4f1d9
pwrona <przemekwrona25@gmail.com>
2019-07-11 17:11:15 +0200

    CHATO-3203 create new flag to manage coversation content

--- src/app/chat/chat-with-inbox/current-conversation/current-conversation.component.ts
+++ src/app/chat/chat-with-inbox/current-conversation/current-conversation.component.ts
@@ -30,6 +30,7 @@ export class CurrentConversationComponent implements OnInit, OnDestroy {
   @Input() currentConversation: InboxConversation;
   @Input() conversationsTotal: number;
   @Input() connectionError: boolean;
+  @Input() loadingError: boolean;
 
   constructor(private eventService: EventService,
     private modalService: NgbModal,
