7a9d5abe07364798cfc7a52e7dd8b115896bb4a6
pwrona <przemekwrona25@gmail.com>
2019-07-17 13:57:49 +0200

    CHATO-3385 run all test success

--- src/app/chat/chat-with-inbox/scrolling-message/scrolling-message.component.spec.ts
+++ src/app/chat/chat-with-inbox/scrolling-message/scrolling-message.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ScrollingMessageComponent } from './scrolling-message.component';
+import { SharedModule } from '../../../shared/shared.module';
+import { MatIconModule } from '@angular/material';
 
 describe('ScrollingMessageComponent', () => {
   let component: ScrollingMessageComponent;
@@ -8,7 +10,12 @@ describe('ScrollingMessageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ScrollingMessageComponent ]
+      imports: [
+        MatIconModule
+      ],
+      declarations: [
+        ScrollingMessageComponent
+      ]
     })
     .compileComponents();
   }));
