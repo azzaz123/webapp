1a07e6b46eaa366cbf5a80c9c314d42d00424dc6
Alex Masnou <alex.masnou@wallapop.com>
2019-09-04 11:58:47 +0200

    Generate TS interfaces from mParticle schemas repo

---
+++ mparticle.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const glob = require('glob');
+const { compileFromFile } = require('json-schema-to-typescript');
+
+glob('node_modules/mparticle_json_validation/**/*.json', {}, (err, schemas) => {
+    schemas.forEach((schema) => {
+        const interfaceName = schema.replace(/^.*[\\\/]/, '').replace('.json', '.interface.ts').replace(/_/g, '-');
+
+        compileFromFile(schema).then(ts => fs.writeFileSync(`src/app/core/tracking/events-interfaces/${interfaceName}`, ts));
+    });
+});
