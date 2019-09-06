9076f926aaf613897ef0c1791609fdaa21cc91c2
Alex Masnou <alex.masnou@wallapop.com>
2019-09-06 14:17:43 +0200

    Move constants to constants file

--- src/app/core/analytics/interfaces/event.interface.ts
+++ src/app/core/analytics/interfaces/event.interface.ts
@@ -1,14 +1,3 @@
-export const EVENT_TYPES = {
-    Navigation: 1,
-    Location: 2,
-    Search: 3,
-    Transaction: 4,
-    UserContent: 5,
-    UserPreference: 6,
-    Social: 7,
-    Other: 8
-}
-
 export interface EventInterface {
     name: string;
     eventType: number;
