25621e8858168a2296b13c393b8fd46e659f4fd0
pwrona <przemekwrona25@gmail.com>
2019-09-06 14:11:57 +0200

    CHATO-3468 add metric to send duplicated messages

--- src/app/core/remote-console/remote-console.service.spec.ts
+++ src/app/core/remote-console/remote-console.service.spec.ts
@@ -2,17 +2,9 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { RemoteConsoleService } from './remote-console.service';
-import { DeviceDetectorService, DeviceInfo } from 'ngx-device-detector';
+import { DeviceDetectorService } from 'ngx-device-detector';
+import { DeviceDetectorServiceMock } from '../../../tests';
 import * as logger from 'loglevel';
-
-class DeviceDetectorServiceMock {
-  getDeviceInfo(): DeviceInfo {
-    return {
-      browser: 'Chrome',
-      browser_version: '76.0.3809.132'
-    } as DeviceInfo;
-  }
-}
 
 describe('RemoteConsoleService', () => {
 
@@ -53,11 +45,29 @@ describe('RemoteConsoleService', () => {
       'browser': 'CHROME',
       'browser_version': '76.0.3809.132',
       'user_id': USER_ID,
-      'connection_time': CONNECTION_TIME,
       'metric_type': 'XMPP_CONNECTION_TIME',
       'message': 'xmpp connection time',
+      'connection_time': CONNECTION_TIME,
       'connection_type': '',
       'ping_time_ms': 0
+    }));
+  });
+
+  it('should call xmpp conection with parameters', () => {
+    const USER_ID = 'USER_ID';
+    const CONVERSATIONS_BY_ID = new Map();
+    CONVERSATIONS_BY_ID['xa4ld642'] = 2;
+    spyOn(logger, 'info');
+
+    service.sendDuplicateConversations(USER_ID, CONVERSATIONS_BY_ID);
+
+    expect(logger.info).toHaveBeenCalledWith(JSON.stringify({
+      'browser': 'CHROME',
+      'browser_version': '76.0.3809.132',
+      'user_id': 'USER_ID',
+      'metric_type': 'DUPLICATE_CONVERSATION',
+      'message': 'send log when user see duplicate conversation in inbox',
+      'conversations_group_by_id': { 'xa4ld642': 2 }
     }));
   });
 });
--- src/app/core/remote-console/remote-console.service.ts
+++ src/app/core/remote-console/remote-console.service.ts
@@ -19,11 +19,23 @@ export class RemoteConsoleService {
       browser: _.toUpper(device.browser),
       browser_version: device.browser_version,
       user_id: userId,
-      connection_time: connectionTime,
       metric_type: MetricTypeEnum.XMPP_CONNECTION_TIME,
       message: 'xmpp connection time',
+      connection_time: connectionTime,
       connection_type: _.toUpper(navigator['connection']['type']),
       ping_time_ms: navigator['connection']['rtt']
+    }));
+  }
+
+  sendDuplicateConversations(userId: string, conversationsGroupById: Map<string, number>): void {
+    const device = this.deviceService.getDeviceInfo();
+    logger.info(JSON.stringify({
+      browser: _.toUpper(device.browser),
+      browser_version: device.browser_version,
+      user_id: userId,
+      metric_type: MetricTypeEnum.DUPLICATE_CONVERSATION,
+      message: 'send log when user see duplicate conversation in inbox',
+      conversations_group_by_id: conversationsGroupById
     }));
   }
 }
