4db26fb3e1809e69522ca20e5185e68ff39dc945
enric.pagerols <enric.pagerols@wallapop.com>
2019-10-03 13:46:23 +0200

    [PQP-280]: PQP-280 Cancel subs modal

--- src/app/profile/subscription/modals/edit-subscription-modal.component.ts
+++ src/app/profile/subscription/modals/edit-subscription-modal.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { StripeService } from '../../../core/stripe/stripe.service';
-import { SubscriptionsService } from '../../../core/subscriptions/subscriptions.service';
 import { SubscriptionsResponse, Tier } from '../../../core/subscriptions/subscriptions.interface';
 import { ToastrService } from 'ngx-toastr';
 import { I18nService } from '../../../core/i18n/i18n.service';
 import { EventService } from '../../../core/event/event.service';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'tsl-edit-subscription-modal',
@@ -31,7 +30,8 @@ export class EditSubscriptionModalComponent implements OnInit {
   constructor(public activeModal: NgbActiveModal,
               private toastr: ToastrService,
               private i18n: I18nService,
-              private eventService: EventService) {
+              private eventService: EventService,
+              private modalService: NgbModal) {
   }
 
   ngOnInit() {
@@ -52,11 +52,14 @@ export class EditSubscriptionModalComponent implements OnInit {
   }
 
   public cancelSubscription() {
-    this.loading = true;
-    //cancel subs endpoint
-    this.close();
-    this.toastr.success(this.i18n.getTranslations('cancelSubscriptionSuccessTitle') + ' ' + this.i18n.getTranslations('cancelSubscriptionSuccessBody'));
-    this.eventService.emit('subscriptionChange');
+    const modal = EditSubscriptionModalComponent
+    let modalRef: NgbModalRef = this.modalService.open(modal, {windowClass: 'review'});
+    modalRef.componentInstance.subscription = this.subscription;
+    modalRef.result.then(() => {
+      modalRef = null;
+      this.toastr.success(this.i18n.getTranslations('cancelSubscriptionSuccessTitle') + ' ' + this.i18n.getTranslations('cancelSubscriptionSuccessBody'));
+      this.eventService.emit('subscriptionChange');
+    }, () => {});
   }
 
   public selectListingLimit(tier: Tier): void {
