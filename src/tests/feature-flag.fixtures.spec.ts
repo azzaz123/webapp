ff9f9831ce4b39765762aae9ce4b1aeb9dc38300
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2019-10-04 09:55:13 +0200

    [FRON-125]: Change fixtures to match new variable name

--- src/tests/feature-flag.fixtures.spec.ts
+++ src/tests/feature-flag.fixtures.spec.ts
@@ -2,9 +2,9 @@ import { Observable } from 'rxjs';
 import { FeatureFlagResponse, FEATURE_FLAGS_ENUM } from '../app/core/user/featureflag.service';
 
 export const mockFeatureFlags: FeatureFlagResponse[] = [
-  { name: 'flag1', active: false },
-  { name: 'flag2', active: true },
-  { name: 'flag3', active: false }
+  { name: 'flag1', isActive: false },
+  { name: 'flag2', isActive: true },
+  { name: 'flag3', isActive: false }
 ];
 
 export enum mockFeatureFlagsEnum {
