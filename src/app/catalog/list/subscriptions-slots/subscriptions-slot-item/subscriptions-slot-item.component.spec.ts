1e61e987e245bd4df73757910fc8350eaf083309
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2019-10-10 17:53:06 +0200

    [PQP-770]: [PQP-851]: Add tests for SubscriptionsSlotItem

--- src/app/catalog/list/subscriptions-slots/subscriptions-slot-item/subscriptions-slot-item.component.spec.ts
+++ src/app/catalog/list/subscriptions-slots/subscriptions-slot-item/subscriptions-slot-item.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SubscriptionsSlotItemComponent } from './subscriptions-slot-item.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MOCK_SUBSCRIPTION_SLOT_CARS } from '../../../../../tests/subscriptions.fixtures.spec';
+import { CATEGORY_DATA_WEB } from '../../../../../tests/category.fixtures.spec';
 
 describe('SubscriptionsSlotItemComponent', () => {
   let component: SubscriptionsSlotItemComponent;
@@ -20,10 +21,46 @@ describe('SubscriptionsSlotItemComponent', () => {
     fixture = TestBed.createComponent(SubscriptionsSlotItemComponent);
     component = fixture.componentInstance;
     component.subscriptionSlot = MOCK_SUBSCRIPTION_SLOT_CARS;
+    component.selectedSubscriptionSlot = MOCK_SUBSCRIPTION_SLOT_CARS;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('isSelected', () => {
+    it('should return true when selected slot and own subscription are the same', () => {
+      expect(component.isSelected()).toBeTruthy();
+    });
+
+    it('should return false when selected slot and own subscription are different', () => {
+      component.selectedSubscriptionSlot = { category: CATEGORY_DATA_WEB[1], available: 2, limit: 2 };
+
+      expect(component.isSelected()).toBeFalsy();
+    });
+  });
+
+  describe('onClick', () => {
+    const event = { stopPropagation: () => {} };
+
+    beforeEach(() => {
+      spyOn(component.selected, 'emit').and.callThrough();
+      spyOn(event, 'stopPropagation').and.callThrough();
+    });
+
+    it('should emit value', () => {
+      component.onClick(MOCK_SUBSCRIPTION_SLOT_CARS, event);
+
+      expect(component.selected.emit).toHaveBeenCalledTimes(1);
+      expect(component.selected.emit).toHaveBeenCalledWith(MOCK_SUBSCRIPTION_SLOT_CARS);
+      expect(event.stopPropagation).toHaveBeenCalledTimes(0);
+    });
+
+    it('should stopPropagation when null subscription', () => {
+      component.onClick(null, event);
+
+      expect(event.stopPropagation).toHaveBeenCalledTimes(1);
+    });
   });
 });
