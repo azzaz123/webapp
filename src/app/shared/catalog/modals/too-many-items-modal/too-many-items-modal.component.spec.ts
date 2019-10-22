1a87486d4c43a3670b82773ecf5b84c814c76d23
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2019-10-22 18:39:40 +0200

    [PQP-770]: Fix tests

--- src/app/shared/catalog/modals/too-many-items-modal/too-many-items-modal.component.spec.ts
+++ src/app/shared/catalog/modals/too-many-items-modal/too-many-items-modal.component.spec.ts
@@ -1,7 +1,9 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { MatIconModule } from '@angular/material';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { TooManyItemsModalComponent } from './too-many-items-modal.component';
+import { ButtonComponent } from '../../../button/button.component';
 
 describe('ItemAvatarComponent', () => {
   let component: TooManyItemsModalComponent;
@@ -13,7 +15,11 @@ describe('ItemAvatarComponent', () => {
         MatIconModule
       ],
       declarations: [
-        TooManyItemsModalComponent
+        TooManyItemsModalComponent,
+        ButtonComponent
+      ],
+      providers: [
+        NgbActiveModal
       ]
     })
     .compileComponents();
