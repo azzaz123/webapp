ffd17eb02c50f143b3e291d2dcb844dee929bef2
enric.pagerols <enric.pagerols@wallapop.com>
2019-10-22 18:34:21 +0200

    [PQP-280]: PQP-280 PR fixes

--- src/app/profile/profile.component.spec.ts
+++ src/app/profile/profile.component.spec.ts
@@ -3,7 +3,7 @@ import { ProfileComponent } from './profile.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { UserService } from '../core/user/user.service';
 import { Observable } from 'rxjs';
-import { MOCK_USER, MOTORPLAN_DATA, USER_WEB_SLUG, USERS_STATS_RESPONSE, PROFILE_SUB_INFO } from '../../tests/user.fixtures.spec';
+import { MOCK_USER, MOTORPLAN_DATA, USER_WEB_SLUG, USERS_STATS_RESPONSE, PROFILE_SUB_INFO, PROFILE_NOT_SUB_INFO } from '../../tests/user.fixtures.spec';
 import { I18nService } from '../core/i18n/i18n.service';
 import { environment } from '../../environments/environment';
 import { NgxPermissionsModule } from 'ngx-permissions';
@@ -11,7 +11,7 @@ import { SubscriptionsService } from '../core/subscriptions/subscriptions.servic
 import { StripeService } from '../core/stripe/stripe.service';
 import { HttpService } from '../core/http/http.service';
 import { FeatureflagService } from '../core/user/featureflag.service';
-import { SUBSCRIPTIONS, MAPPED_SUBSCRIPTIONS } from '../../tests/subscriptions.fixtures.spec';
+import { MAPPED_SUBSCRIPTIONS } from '../../tests/subscriptions.fixtures.spec';
 import { CategoryService } from '../core/category/category.service';
 import { CATEGORY_DATA_WEB } from '../../tests/category.fixtures.spec';
 
@@ -19,6 +19,7 @@ describe('ProfileComponent', () => {
   let component: ProfileComponent;
   let fixture: ComponentFixture<ProfileComponent>;
   let userService: UserService;
+  let categoryService: CategoryService;
   let subscriptionsService: SubscriptionsService;
   let stripeService: StripeService;
   let featureflagService: FeatureflagService;
@@ -53,7 +54,7 @@ describe('ProfileComponent', () => {
             },
             logout() {},
             getMotorPlans() {
-              return Observable.of(PROFILE_SUB_INFO);
+              return Observable.of({});
             },
             isProfessional() {
               return Observable.of(false);
@@ -104,6 +105,7 @@ describe('ProfileComponent', () => {
     fixture = TestBed.createComponent(ProfileComponent);
     component = fixture.componentInstance;
     userService = TestBed.get(UserService);
+    categoryService = TestBed.get(CategoryService);
     spyOn(userService, 'me').and.callThrough();
     spyOn(userService, 'isProUser').and.returnValue(Observable.of(true));
     spyOn(userService, 'getStats').and.callThrough();
@@ -149,6 +151,17 @@ describe('ProfileComponent', () => {
       expect(userService.getStats).toHaveBeenCalled();
       expect(component.userStats).toBe(USERS_STATS_RESPONSE);
     });
+
+    it('should set isNewSubscription to true if the user is subscribed with stripe and not a Cardealer', () => {
+      component.isNewSubscription = false;
+      spyOn(categoryService, 'getCategories').and.callThrough();
+      spyOn(userService, 'getMotorPlans').and.returnValue(Observable.of(PROFILE_NOT_SUB_INFO));
+
+      component.ngOnInit();
+
+      expect(component.isNewSubscription).toBe(true);
+    });
+
   });
 
   describe('logout', () => {
