c2ff232704773f3d26d8d5695a7f29c7a846d555
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2019-12-11 17:12:45 +0100

    [FRON-168]: Fix unit tests

--- src/app/core/mobile-blocker/mobile-blocker.component.spec.ts
+++ src/app/core/mobile-blocker/mobile-blocker.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MobileBlockerComponent } from './mobile-blocker.component';
+import { DeviceDetectorServiceMock } from '../../../tests';
+import { UserService } from '../user/user.service';
+import { DeviceDetectorService } from 'ngx-device-detector';
+import { of } from 'rxjs';
 
 describe('MobileBlockerComponent', () => {
   let component: MobileBlockerComponent;
@@ -8,7 +12,11 @@ describe('MobileBlockerComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MobileBlockerComponent ]
+      declarations: [ MobileBlockerComponent ],
+      providers: [
+        { provide: UserService, useValue: { isProUser: () => of(false) }},
+        { provide: DeviceDetectorService, useClass: DeviceDetectorServiceMock }
+      ]
     })
     .compileComponents();
   }));
