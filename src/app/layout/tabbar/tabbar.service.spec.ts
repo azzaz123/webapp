78ea30d02df1130a10189c9bfb978e7f88caba8e
Alex Masnou <alex.masnou@wallapop.com>
2019-12-13 16:05:50 +0100

    [FRON-147]: Improve tab bar layout in tablet devices

--- src/app/layout/tabbar/tabbar.service.spec.ts
+++ src/app/layout/tabbar/tabbar.service.spec.ts
@@ -2,6 +2,36 @@ import { TestBed } from '@angular/core/testing';
 
 import { TabbarService } from './tabbar.service';
 
+let tabBarService: TabbarService;
+
 describe('TabbarService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [TabbarService]
+    });
+
+    tabBarService = TestBed.get(TabbarService);
+  });
+
+  describe('hideTabBar', () => {
+    it('should emit tabBarHidden event with value true', () => {
+      let tabBarHidden: boolean;
+      tabBarService.tabBarHidden$.subscribe(hidden => tabBarHidden = hidden);
+
+      tabBarService.hideTabBar();
+
+      expect(tabBarHidden).toBe(true);
+    });
+  });
+
+  describe('showTabBar', () => {
+    it('should emit tabBarHidden event with value false', () => {
+      let tabBarHidden: boolean;
+      tabBarService.tabBarHidden$.subscribe(hidden => tabBarHidden = hidden);
+
+      tabBarService.showTabBar();
+
+      expect(tabBarHidden).toBe(false);
+    });
+  });
 });
