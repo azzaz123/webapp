5cd94a9f43ee17c7b67e506fd666f6dff2bdd66d
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2019-12-16 17:19:20 +0100

    [FRON-193]: Remove HttpService in SuggesterService

--- src/app/layout/topbar/suggester/suggester.service.ts
+++ src/app/layout/topbar/suggester/suggester.service.ts
@@ -2,22 +2,17 @@ import { IDictionary } from './../../../shared/models/dictionary.interface';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { SuggesterResponse } from './suggester-response.interface';
-import { HttpServiceNew } from '../../../core/http/http.service.new';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
 
 export const SUGGESTER_ENDPOINT = 'api/v3/suggesters/search';
 @Injectable()
 export class SuggesterService {
 
-
-  constructor(private http: HttpServiceNew) { }
+  constructor(private http: HttpClient) { }
 
   public getSuggestions(query: string): Observable<SuggesterResponse[]> {
-    const params: IDictionary[] = [{
-      key: 'keyword',
-      value: query
-    }];
-    
-    return this.http.get(SUGGESTER_ENDPOINT, params);
+    return this.http.get<SuggesterResponse[]>(environment.baseUrl + SUGGESTER_ENDPOINT, { params: { keyword: query } });
   }
 
 }
