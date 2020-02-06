28b558ff731a2ab51d1b6887b8a210377b66a41a
Alex Masnou <alex.masnou@wallapop.com>
2020-02-06 17:16:31 +0100

    [FRON-131]: Migrate item-stats service to use new http client

--- src/app/stats/items-stats/item-stats-row/item-stats-graph/item-stats.service.spec.ts
+++ src/app/stats/items-stats/item-stats-row/item-stats-graph/item-stats.service.spec.ts
@@ -1,41 +1,40 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ItemStatsService } from './item-stats.service';
-import { TEST_HTTP_PROVIDERS } from '../../../../../tests/utils.spec';
-import { HttpService } from '../../../../core/http/http.service';
-import { ITEM_STATISTIC_RESPONSE } from '../../../../../tests/statistics.fixtures.spec';
-import { ResponseOptions } from '@angular/http';
-import { Observable } from 'rxjs';
-import { Response } from '@angular/http';
+import { HttpClientTestingModule, TestRequest, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../../environments/environment';
 
 describe('ItemStatsService', () => {
 
-  let http: HttpService;
+  let http: HttpClient;
+  let httpMock: HttpTestingController;
   let service: ItemStatsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
-        ItemStatsService,
-        ...TEST_HTTP_PROVIDERS
-      ]
+        ItemStatsService
+      ],
+      imports: [HttpClientTestingModule],
     });
+
     service = TestBed.get(ItemStatsService);
-    http = TestBed.get(HttpService);
+    http = TestBed.get(HttpClient);
+    httpMock = TestBed.get(HttpTestingController);
   });
 
   describe('getStatistics', () => {
-    it('should call endpoint and return response', () => {
-      const res: ResponseOptions = new ResponseOptions({body: JSON.stringify(ITEM_STATISTIC_RESPONSE)});
-      spyOn(http, 'get').and.returnValue(Observable.of(new Response(res)));
-      let response;
+    it('should call endpoint', () => {
+      const itemId = '123';
+      const expectedUrl = `${environment.baseUrl}api/v3/statistics/item/${itemId}`;
 
-      service.getStatistics('123').subscribe((r) => {
-        response = r;
-      });
+      service.getStatistics(itemId).subscribe();
+      const req: TestRequest = httpMock.expectOne(expectedUrl);
+      req.flush({});
 
-      expect(http.get).toHaveBeenCalledWith('api/v3/statistics/item/123');
-      expect(response).toEqual(ITEM_STATISTIC_RESPONSE);
+      expect(req.request.url).toBe(expectedUrl);
+      expect(req.request.method).toBe('GET');
     });
   });
 });
