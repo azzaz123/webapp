8f6998cc2d45d842eef498764724d91f9ed9fde9
pwrona <przemekwrona25@gmail.com>
2020-02-26 16:42:48 +0100

    CHATO-250 remove ConversationService in UnArchiveButtonComponent

--- src/app/shared/unarchive-button/unarchive-button.component.spec.ts
+++ src/app/shared/unarchive-button/unarchive-button.component.spec.ts
@@ -4,18 +4,15 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { UnarchiveButtonComponent } from './unarchive-button.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { Observable } from 'rxjs';
-import { ConversationService } from '../../core/conversation/conversation.service';
-import { CONVERSATION_ID, MOCK_CONVERSATION } from '../../../tests/conversation.fixtures.spec';
 import { Lead } from '../../core/conversation/lead';
 import { MockTrackingService } from '../../../tests/tracking.fixtures.spec';
 import { TrackingService } from '../../core/tracking/tracking.service';
 import { CallsService } from '../../core/conversation/calls.service';
-import { MOCK_CALL, CALL_ID } from '../../../tests/call.fixtures';
+import { CALL_ID, MOCK_CALL } from '../../../tests/call.fixtures';
 
 describe('UnarchiveButtonComponent', () => {
   let component: UnarchiveButtonComponent;
   let fixture: ComponentFixture<UnarchiveButtonComponent>;
-  let conversationService: ConversationService;
   let callsService: CallsService;
   let trackingService: TrackingService;
 
@@ -25,14 +22,7 @@ describe('UnarchiveButtonComponent', () => {
         NgbModule.forRoot()
       ],
       providers: [
-        {provide: TrackingService, useClass: MockTrackingService},
-        {
-          provide: ConversationService, useValue: {
-          unarchive() {
-            return Observable.of({});
-          }
-        }
-        },
+        { provide: TrackingService, useClass: MockTrackingService },
         {
           provide: CallsService, useValue: {
             unarchive() {
@@ -41,7 +31,7 @@ describe('UnarchiveButtonComponent', () => {
           }
         }
       ],
-      declarations: [ UnarchiveButtonComponent ],
+      declarations: [UnarchiveButtonComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
     .compileComponents();
@@ -50,7 +40,6 @@ describe('UnarchiveButtonComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UnarchiveButtonComponent);
     component = fixture.componentInstance;
-    conversationService = TestBed.get(ConversationService);
     callsService = TestBed.get(CallsService);
     trackingService = TestBed.get(TrackingService);
     fixture.detectChanges();
@@ -59,16 +48,6 @@ describe('UnarchiveButtonComponent', () => {
   describe('unarchive', () => {
     beforeEach(() => {
       spyOn(trackingService, 'track').and.callThrough();
-    });
-
-    it('should call unarchive conversation if the lead is conversation and send a conversation mark pending track', () => {
-      spyOn(conversationService, 'unarchive').and.callThrough();
-      component.lead = <Lead>MOCK_CONVERSATION();
-
-      component.unarchive(new Event('click'));
-
-      expect(conversationService.unarchive).toHaveBeenCalledWith(CONVERSATION_ID);
-      expect(trackingService.track).toHaveBeenCalledWith(TrackingService.CONVERSATION_MARK_PENDING);
     });
 
     it('should call unarchive call if the lead is call and send a call mark pending track', () => {
