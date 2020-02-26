773bcfccc981a9320af12fbf7ef1b94b97fd5464
pwrona <przemekwrona25@gmail.com>
2020-02-26 16:35:20 +0100

    CHATO-250 remove ConversationService in ArchiveButtonComponent

--- src/app/shared/archive-button/archive-button.component.spec.ts
+++ src/app/shared/archive-button/archive-button.component.spec.ts
@@ -1,19 +1,16 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ArchiveButtonComponent } from './archive-button.component';
 import { Observable } from 'rxjs';
-import { ConversationService } from '../../core/conversation/conversation.service';
 import { TrackingService } from '../../core/tracking/tracking.service';
 import { MockTrackingService } from '../../../tests/tracking.fixtures.spec';
-import { CONVERSATION_ID, MOCK_CONVERSATION } from '../../../tests/conversation.fixtures.spec';
 import { CallsService } from '../../core/conversation/calls.service';
-import { MOCK_CALL, CALL_ID } from '../../../tests/call.fixtures';
+import { CALL_ID, MOCK_CALL } from '../../../tests/call.fixtures';
 
 describe('ArchiveButtonComponent', () => {
   let component: ArchiveButtonComponent;
   let fixture: ComponentFixture<ArchiveButtonComponent>;
-  let conversationService: ConversationService;
   let callsService: CallsService;
   let trackingService: TrackingService;
 
@@ -21,13 +18,6 @@ describe('ArchiveButtonComponent', () => {
     TestBed.configureTestingModule({
       providers: [
         {provide: TrackingService, useClass: MockTrackingService},
-        {
-          provide: ConversationService, useValue: {
-          archive() {
-            return Observable.of({});
-          }
-        }
-        },
         {
           provide: CallsService, useValue: {
             archive() {
@@ -45,7 +35,6 @@ describe('ArchiveButtonComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ArchiveButtonComponent);
     component = fixture.componentInstance;
-    conversationService = TestBed.get(ConversationService);
     callsService = TestBed.get(CallsService);
     trackingService = TestBed.get(TrackingService);
     fixture.detectChanges();
@@ -59,16 +48,6 @@ describe('ArchiveButtonComponent', () => {
       component.click.subscribe(() => {
         called = true;
       });
-    });
-
-    it('should call conversationService.archive if lead is Conversation and send conversation process tracking', () => {
-      spyOn(conversationService, 'archive').and.callThrough();
-      component.lead = MOCK_CONVERSATION();
-
-      component.archive(new Event(''));
-
-      expect(conversationService.archive).toHaveBeenCalledWith(CONVERSATION_ID);
-      expect(trackingService.track).toHaveBeenCalledWith(TrackingService.CONVERSATION_PROCESSED);
     });
 
     it('should call callService.archive if lead is Call and send call process tracking', () => {
