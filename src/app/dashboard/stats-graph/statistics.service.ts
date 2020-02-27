b7d1101a9a8e9dea012450f3822c303242a30189
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2020-02-27 17:28:44 +0100

    [FRON-131]: Refactor tests for statistics service

--- src/app/dashboard/stats-graph/statistics.service.ts
+++ src/app/dashboard/stats-graph/statistics.service.ts
@@ -4,15 +4,15 @@ import { StatisticFullResponse } from './statistic-response.interface';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 
+export const STATISTICS_ENDPOINT = 'api/v3/protool/dashboard/statistics/';
+
 @Injectable()
 export class StatisticsService {
 
   constructor(private http: HttpClient) {
   }
 
-  public getStatistics(duration = '30'): Observable<StatisticFullResponse> {
-    return this.http.get<StatisticFullResponse>(`${environment.baseUrl}api/v3/protool/dashboard/statistics/`, {
-      params: { durationInDays: duration}
-    });
+  public getStatistics(durationInDays: string = '30'): Observable<StatisticFullResponse> {
+    return this.http.get<StatisticFullResponse>(`${environment.baseUrl}${STATISTICS_ENDPOINT}`, { params: { durationInDays } });
   }
 }
