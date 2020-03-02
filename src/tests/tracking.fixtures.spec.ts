51f862a38f6da21776952a1d2a23ec269cf2cc63
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2020-03-02 16:10:36 +0100

    [FRON-253]: Remove addTrackingEvent from fixtures

--- src/tests/tracking.fixtures.spec.ts
+++ src/tests/tracking.fixtures.spec.ts
@@ -15,13 +15,9 @@ export const TRACKING_EVENT: TrackingEvent = new TrackingEvent({
 
 /* istanbul ignore next */
 export class MockTrackingService {
-
-  addTrackingEvent() {}
-
   track(event: TrackingEventBase, params?: any) {
   }
   trackMultiple(events: Array<TrackingEventData>) {
   }
-
 }
 
