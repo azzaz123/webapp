a8125e1b4f3d8eacb5e1835cfce2898e105ec86f
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2020-03-03 17:58:00 +0100

    [PQP-1458]: Add tests for consumer goods in subscription service

--- src/tests/subscriptions.fixtures.spec.ts
+++ src/tests/subscriptions.fixtures.spec.ts
@@ -54,6 +54,22 @@ export const MOCK_SUBSCRIPTION_SLOT_CARS: SubscriptionSlot = {
   limit: 10
 };
 
+export const SUBSCRIPTION_CONSUMER_GOODS_NOT_SUBSCRIBED: SubscriptionsResponse = {
+  id: 'abcd-1234-efgh-5678',
+  category_id: 0,
+  subscribed_from: null,
+  selected_tier_id: null,
+  default_tier_id: 'plan_Fsf0Htv8L6Ox91',
+  tiers:
+  [
+    {
+      id: 'plan_Fsf0Htv8L6Ox91',
+      price: 39.99,
+      currency: '€'
+    }   
+  ]
+}
+
 export const SUBSCRIPTIONS: SubscriptionsResponse[] = 
 [
   {
