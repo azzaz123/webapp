1e7e803d32f534a85a8596fc818f8f68af52f13a
pwrona <przemekwrona25@gmail.com>
2020-03-04 17:13:53 +0100

    FRON-255 uncomment test

--- src/app/chat/chat.component.spec.ts
+++ src/app/chat/chat.component.spec.ts
@@ -160,7 +160,7 @@ describe('Component: ChatComponent with ItemId', () => {
       expect(component.conversationsLoading).toEqual(true);
     });
 
-    xit('should set connectionError to FALSE when a EventService.CHAT_RT_CONNECTED event is emitted', () => {
+    it('should set connectionError to FALSE when a EventService.CHAT_RT_CONNECTED event is emitted', () => {
       const inboxConversation: InboxConversation = CREATE_MOCK_INBOX_CONVERSATION();
       inboxConversation.messages[inboxConversation.messages.length - 1].status = MessageStatus.PENDING;
 
