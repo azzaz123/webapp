cb03aeed794a00e4ebd0d804924a4e7b36b3a101
pwrona <przemekwrona25@gmail.com>
2020-03-06 14:12:29 +0100

    FRON-3939 add third voice for review ¡

--- src/app/chat/message/third-voice-review/third-voice-review.component.spec.ts
+++ src/app/chat/message/third-voice-review/third-voice-review.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ThirdVoiceReviewComponent } from './third-voice-review.component';
+import { ThirdVoiceMessageComponent } from '../third-voice-message';
+import { MatIconModule } from '@angular/material';
+import { SharedModule } from '../../../shared/shared.module';
+import { CommonModule } from '@angular/common';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { CREATE_MOCK_INBOX_CONVERSATION } from '../../../../tests/inbox.fixtures.spec';
+import { InboxMessage, MessageType } from '../../model';
 
 describe('ThirdVoiceReviewComponent', () => {
   let component: ThirdVoiceReviewComponent;
@@ -8,7 +15,11 @@ describe('ThirdVoiceReviewComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ThirdVoiceReviewComponent ]
+      imports: [SharedModule, MatIconModule, CommonModule],
+      declarations: [
+        ThirdVoiceReviewComponent,
+        ThirdVoiceMessageComponent],
+      schemas: [NO_ERRORS_SCHEMA],
     })
     .compileComponents();
   }));
@@ -16,10 +27,34 @@ describe('ThirdVoiceReviewComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ThirdVoiceReviewComponent);
     component = fixture.componentInstance;
+    component.message = component.message = CREATE_MOCK_INBOX_CONVERSATION().messages[0];
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('isReview', () => {
+
+    it('should return false if message type is not review', () => {
+      component.message = new InboxMessage('message-id', 'conversation-id', 'text', 'user-id', true,
+        new Date(), 'SEND', MessageType.TEXT);
+      expect(component.isReview()).toBeFalsy();
+
+      component.message = new InboxMessage('message-id', 'conversation-id', 'text', 'user-id', true,
+        new Date(), 'SEND', MessageType.PRICE_DROP);
+      expect(component.isReview()).toBeFalsy();
+
+      component.message = new InboxMessage('message-id', 'conversation-id', 'text', 'user-id', true,
+        new Date(), 'SEND', MessageType.DROP_PRICE);
+      expect(component.isReview()).toBeFalsy();
+    });
+
+    it('should return false if message type is review', () => {
+      component.message = new InboxMessage('message-id', 'conversation-id', 'text', 'user-id', true,
+        new Date(), 'SEND', MessageType.REVIEW);
+      expect(component.isReview()).toBeTruthy();
+    });
   });
 });
--- src/app/chat/message/third-voice-review/third-voice-review.component.ts
+++ src/app/chat/message/third-voice-review/third-voice-review.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { InboxItem, InboxMessage, InboxUser, MessageType } from '../../model';
+import { eq } from 'lodash-es';
 
 @Component({
   selector: 'tsl-third-voice-review',
   templateUrl: './third-voice-review.component.html',
   styleUrls: ['./third-voice-review.component.scss']
 })
-export class ThirdVoiceReviewComponent implements OnInit {
+export class ThirdVoiceReviewComponent {
 
-  constructor() { }
+  public static ALLOW_MESSAGES_TYPES = [MessageType.REVIEW];
 
-  ngOnInit() {
+  @Input() message: InboxMessage;
+  @Input() user: InboxUser;
+  @Input() item: InboxItem;
+
+  public isReview(): boolean {
+    return eq(this.message.type, MessageType.REVIEW);
   }
-
 }
