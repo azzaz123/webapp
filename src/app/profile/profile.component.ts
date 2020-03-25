1da22985ea58c5272383d4241daddeb5f30d303b
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2020-03-25 19:18:39 +0100

    [PQP-1622]: Refactor unit tests in profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,7 +3,7 @@ import { UserService } from '../core/user/user.service';
 import { User } from '../core/user/user';
 import { MotorPlan, MotorPlanType, ProfileSubscriptionInfo } from '../core/user/user-response.interface';
 import { I18nService } from '../core/i18n/i18n.service';
-import { UserStatsResponse } from '../core/user/user-stats.interface';
+import { UserStats } from '../core/user/user-stats.interface';
 import { SubscriptionsService } from '../core/subscriptions/subscriptions.service';
 import { flatMap, finalize } from 'rxjs/operators';
 
@@ -16,27 +16,20 @@ export class ProfileComponent implements OnInit {
 
   public userUrl: string;
   public isPro: boolean;
-  public userStats: UserStatsResponse;
-  public loading = false;
+  public userStats: UserStats;
 
-  constructor(private userService: UserService,
-              protected i18n: I18nService,
-              private subscriptionsService: SubscriptionsService,
-              @Inject('SUBDOMAIN') private subdomain: string) {
+  constructor(
+    private userService: UserService,
+    @Inject('SUBDOMAIN') private subdomain: string) {
   }
 
   ngOnInit() {
-    this.loading = true;
-    this.userService.me().subscribe((user: User) => {
-      if (user) {
-        this.userUrl = user.getUrl(this.subdomain);
-        this.isPro = user.featured;
-      }
+    this.userService.me().subscribe(user => {
+      this.userUrl = user.getUrl(this.subdomain);
+      this.isPro = user.featured;
     });
 
-    this.userService.getStats().subscribe((userStats: UserStatsResponse) => {
-      this.userStats = userStats;
-    });
+    this.userService.getStats().subscribe(userStats => this.userStats = userStats);
   }
 
   public logout($event: any) {
