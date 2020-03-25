836b774a245a1f3b689a9b15569a957be414c842
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2020-03-25 10:04:31 +0100

    [PQP-1622]: Remove old logic for profile subscriptions

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -5,7 +5,7 @@ import { MotorPlan, MotorPlanType, ProfileSubscriptionInfo } from '../core/user/
 import { I18nService } from '../core/i18n/i18n.service';
 import { UserStatsResponse } from '../core/user/user-stats.interface';
 import { SubscriptionsService } from '../core/subscriptions/subscriptions.service';
-import { flatMap } from 'rxjs/operators';
+import { flatMap, finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'tsl-profile',
@@ -15,12 +15,8 @@ import { flatMap } from 'rxjs/operators';
 export class ProfileComponent implements OnInit {
 
   public userUrl: string;
-  public motorPlan: MotorPlanType;
-  public showSubscriptionTab: boolean;
   public isPro: boolean;
   public userStats: UserStatsResponse;
-  public isSubscriptionsActive: boolean;
-  public isNewSubscription = false;
   public loading = false;
 
   constructor(private userService: UserService,
@@ -34,35 +30,12 @@ export class ProfileComponent implements OnInit {
     this.userService.me().subscribe((user: User) => {
       if (user) {
         this.userUrl = user.getUrl(this.subdomain);
+        this.isPro = user.featured;
       }
-    });
-    this.userService.getMotorPlan().subscribe((motorPlan: MotorPlan) => {
-      const motorPlanTypes = this.i18n.getTranslations('motorPlanTypes');
-      if (motorPlan) {
-        this.motorPlan = motorPlanTypes.filter((p: MotorPlanType) => p.subtype === motorPlan.subtype)[0];
-        this.showSubscriptionTab = motorPlan.type === 'motor_plan_pro';
-      }
-    });
-    this.isProUser();
-    this.userService.getStats().subscribe((userStats: UserStatsResponse) => {
-      this.userStats = userStats;
     });
 
-    this.subscriptionsService.isSubscriptionsActive$()
-    .subscribe(val => {
-      if (val) {
-        this.isSubscriptionsActive = val;
-        this.userService.isProfessional().subscribe((isProfessional: boolean) => {
-          if (!isProfessional) {
-            this.subscriptionType();
-          }
-          else {
-            this.loading = false;
-          }
-        });
-      } else {
-        this.loading = false;
-      }
+    this.userService.getStats().subscribe((userStats: UserStatsResponse) => {
+      this.userStats = userStats;
     });
   }
 
@@ -70,34 +43,4 @@ export class ProfileComponent implements OnInit {
     $event.preventDefault();
     this.userService.logout();
   }
-
-  private subscriptionType(cache: boolean = true): void {
-    this.subscriptionsService.getSubscriptions(cache).subscribe(response => {
-      if (response) {
-        response.map(subscription => {
-          if (subscription.selected_tier_id) {
-            this.isNewSubscription = true;
-            this.loading = false;
-          }
-        })
-        if (!this.isNewSubscription) {
-          this.userService.getMotorPlans().subscribe((subscriptionInfo: ProfileSubscriptionInfo) => {
-            if (subscriptionInfo.status !== "PURCHASE_ACTIVE") {
-              this.isNewSubscription = true;
-              this.loading = false;
-            }
-          });
-        }
-      } else {
-        this.loading = false;
-      }
-    }, () => { this.loading = false; }); 
-  }
-
-  private isProUser(): void {
-    this.userService.isProUser().subscribe((isPro: boolean) => {
-      this.isPro = isPro;
-    });
-  }
-
 }
