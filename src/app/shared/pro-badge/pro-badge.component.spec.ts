8d55c644a955894c0fbc486734454b6de7ab2787
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2020-03-30 22:08:05 +0200

    [PQP-1562]: Add tests for PRO badge

--- src/app/shared/pro-badge/pro-badge.component.spec.ts
+++ src/app/shared/pro-badge/pro-badge.component.spec.ts
@@ -1,13 +1,24 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, getTestBed } from '@angular/core/testing';
 
 import { ProBadgeComponent } from './pro-badge.component';
+import { MatIconModule } from '@angular/material';
+import { UserService } from '../../core/user/user.service';
+import { By } from '@angular/platform-browser';
 
 describe('ProBadgeComponent', () => {
-  let component: ProBadgeComponent;
+  let injector: TestBed;
   let fixture: ComponentFixture<ProBadgeComponent>;
+  let component: ProBadgeComponent;
+  let userService: UserService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ MatIconModule ],
+      providers: [{
+        provide: UserService, useValue: {
+          isPro: false
+        } 
+      }],
       declarations: [ ProBadgeComponent ]
     })
     .compileComponents();
@@ -17,9 +28,29 @@ describe('ProBadgeComponent', () => {
     fixture = TestBed.createComponent(ProBadgeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    injector = getTestBed();
+    userService = injector.get(UserService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('when user is not PRO', () => {
+    it('should hide the PRO badge', () => {
+      const proBadgeHTML = fixture.debugElement.query(By.css('.ProBadge')).nativeNode;
+      expect(proBadgeHTML.hasAttribute('hidden')).toBe(true);
+    });
+  });
+  
+  describe('when user is PRO', () => {
+    it('should show the PRO badge', () => {
+      spyOn(userService, 'isPro').and.returnValue(true);
+      fixture.detectChanges();
+
+      const proBadgeHTML = fixture.debugElement.query(By.css('.ProBadge')).nativeNode;
+      expect(proBadgeHTML.hasAttribute('hidden')).toBe(false);
+    });
   });
 });
