20d28085771dc0157323b1ed3faf81ce16475013
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2020-03-30 22:08:26 +0200

    [PQP-1562]: Add tests for subscription benefits component

--- src/app/profile/subscription/subscription-benefits/subscription-benefits.component.spec.ts
+++ src/app/profile/subscription/subscription-benefits/subscription-benefits.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SubscriptionBenefitsComponent } from './subscription-benefits.component';
+import { MatIconModule } from '@angular/material';
+import { SubscriptionsService } from '../../../core/subscriptions/subscriptions.service';
+import { MockSubscriptionService, MOCK_SUBSCRIPTION_BENEFITS } from '../../../../tests/subscriptions.fixtures.spec';
+import { By } from '@angular/platform-browser';
 
 describe('SubscriptionBenefitsComponent', () => {
   let component: SubscriptionBenefitsComponent;
@@ -8,7 +12,11 @@ describe('SubscriptionBenefitsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SubscriptionBenefitsComponent ]
+      imports: [ MatIconModule ],
+      declarations: [ SubscriptionBenefitsComponent ],
+      providers: [
+        { provide: SubscriptionsService, useClass: MockSubscriptionService }
+      ]
     })
     .compileComponents();
   }));
@@ -22,4 +30,18 @@ describe('SubscriptionBenefitsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should get benefits from backend', () => {
+    component.ngOnInit();
+
+    expect(component.benefits).toEqual(MOCK_SUBSCRIPTION_BENEFITS);
+  });
+
+  it('should show as many subscription benefits as they come from backend', () => {
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    const benefitsHTML = fixture.debugElement.queryAll(By.css('.SubscriptionBenefitCard'));
+    expect(benefitsHTML.length).toEqual(MOCK_SUBSCRIPTION_BENEFITS.length);
+  })
 });
