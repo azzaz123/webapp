2b8c00b79e50b0d3a33d98028ddad01a0b24d70f
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2020-04-06 17:45:13 +0200

    [PQP-1636]: Fix tests in modal

--- src/app/profile/subscription/modals/discount-available-unsubscribe-in-app-modal/discount-available-unsubscribe-in-app-modal.component.spec.ts
+++ src/app/profile/subscription/modals/discount-available-unsubscribe-in-app-modal/discount-available-unsubscribe-in-app-modal.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DiscountAvailableUnsubscribeInAppModalComponent } from './discount-available-unsubscribe-in-app-modal.component';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('DiscountAvailableUnsubscribeInAppModalComponent', () => {
   let component: DiscountAvailableUnsubscribeInAppModalComponent;
@@ -8,7 +10,11 @@ describe('DiscountAvailableUnsubscribeInAppModalComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DiscountAvailableUnsubscribeInAppModalComponent ]
+      declarations: [ DiscountAvailableUnsubscribeInAppModalComponent ],
+      providers: [
+        NgbActiveModal
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
