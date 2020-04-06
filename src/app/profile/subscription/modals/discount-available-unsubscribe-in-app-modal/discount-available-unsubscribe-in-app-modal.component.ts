64d4b424e5d5127dc24700b81b0df0d68b6ab996
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2020-04-06 17:10:54 +0200

    [PQP-1636]: Add Apple and Google subscription help links

--- src/app/profile/subscription/modals/discount-available-unsubscribe-in-app-modal/discount-available-unsubscribe-in-app-modal.component.ts
+++ src/app/profile/subscription/modals/discount-available-unsubscribe-in-app-modal/discount-available-unsubscribe-in-app-modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { SUPPORT_MAIL } from '../../../../core/constants';
+import { SUPPORT_MAIL, APPLE_STORE_SUBSCRIPTIONS_HELP_URL, GOOGLE_PLAY_SUBSCRIPTIONS_HELP_URL } from '../../../../core/constants';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -10,6 +10,8 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 export class DiscountAvailableUnsubscribeInAppModalComponent implements OnInit {
 
   public supportMail = SUPPORT_MAIL;
+  public appleStoreSubscriptionsHelpURL = APPLE_STORE_SUBSCRIPTIONS_HELP_URL;
+  public googlePlaySubscriptionsHelpURL = GOOGLE_PLAY_SUBSCRIPTIONS_HELP_URL;
 
   constructor(public activeModal: NgbActiveModal) { }
 
