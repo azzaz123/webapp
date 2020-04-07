2a536827b2efe44314413052bac6398ecc72ebff
pwrona <przemekwrona25@gmail.com>
2020-04-07 17:32:37 +0200

    CHATO-4013 change avatar size for tablets

--- src/app/shared/item-avatar/item-avatar.component.ts
+++ src/app/shared/item-avatar/item-avatar.component.ts
@@ -14,7 +14,7 @@ export class ItemAvatarComponent implements OnChanges {
   public fallback: string;
   @Input() fallbackLight: boolean;
   @Input() item: Item;
-  @Input() size: 'small' | 'medium' | 'big' | 'bigger' | 'rectangle-xl' | 'full';
+  @Input() size: 'small' | 'medium' | 'big' | 'bigger' | 'xl' | 'rectangle-xl' | 'full';
   @Input() showAvailableStatus = true;
 
   constructor() {
