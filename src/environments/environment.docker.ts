f71a23990d395710e570ca5110d49905d3fc5f0a
Alex Masnou <alex.masnou@wallapop.com>
2020-04-20 09:35:36 +0200

    [FRON-295]: Change dockXXX replace method

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -1,15 +1,32 @@
 import { Environment } from './environment.interface';
 
+export const tryGetDockNum = (): string => {
+  let result = 'XXX';
+
+  const dockPrefix = 'dock';
+  const dockPrefixPos = window.location.hostname.indexOf(dockPrefix);
+
+  if (dockPrefixPos !== -1) {
+    try {
+      result = window.location.hostname.substring(dockPrefixPos + dockPrefix.length, dockPrefixPos + dockPrefix.length + 3);
+    } catch { }
+  }
+
+  return result;
+};
+
+export const docknum = tryGetDockNum();
+
 export const environment: Environment = {
   production: false,
   profeatures: true,
   name: 'docker',
-  baseUrl: 'https://apigw.dockXXX.wallapop.com/',
-  xmppDomain: 'dockXXX.wallapop.com',
-  wsUrl: 'wss://dockXXX.wallapop.com:5282/ws-xmpp',
-  siteUrl: 'https://es.dockXXX.wallapop.com/',
+  baseUrl: `https://apigw.${docknum}.wallapop.com/`,
+  xmppDomain: `${docknum}.wallapop.com`,
+  wsUrl: `wss://${docknum}.wallapop.com:5282/ws-xmpp`,
+  siteUrl: `https://es.${docknum}.wallapop.com/`,
   appboy: '516bd193-25b2-48b9-b79d-f8516f104d2f',
-  cookieSuffix: 'dockXXX',
+  cookieSuffix: `${docknum}`,
   clickStreamURL: 'https://precollector.wallapop.com/clickstream.json/sendEvents',
   stripeKey: 'pk_test_YMrctQusG3vLNQkCSRYKdqwF',
   mParticleKey: 'us1-04eb03d316b54f4f99265340abe886e6',
