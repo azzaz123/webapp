6509beb5532c167cfc773a8ce9e1e9e2e7b7c13e
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2020-05-06 12:21:49 +0200

    [FRON-81]: Setup mock variables and libraries in Jest setup

--- src/configs/jest/global-mocks.fixtures.spec.ts
+++ src/configs/jest/global-mocks.fixtures.spec.ts
@@ -31,7 +31,8 @@ googletag = {
 
  apstag = {
   fetchBids ({slots, timeout}, callback) { callback(); },
-  setDisplayBids() {}
+  setDisplayBids() {},
+  init() {}
 };
 
  Criteo = {
@@ -46,4 +47,30 @@ googletag = {
  __cmp = function(arg1, arg2, callback) {};
  quancastOptions =  {es: {}, en: {}};
 
- appboy = function() {}
+
+ document.head.appendChild(document.createElement('script'));
+
+// Below you will find definitions given by `jest-preset-angular` to mock DOM interactions
+Object.defineProperty(window, 'CSS', { value: null });
+
+Object.defineProperty(document, 'doctype', {
+  value: '<!DOCTYPE html>'
+});
+
+Object.defineProperty(window, 'getComputedStyle', {
+  value: () => {
+    return {
+      display: 'none',
+      appearance: ['-webkit-appearance']
+    };
+  }
+});
+
+Object.defineProperty(document.body.style, 'transform', {
+  value: () => {
+    return {
+      enumerable: true,
+      configurable: true,
+    };
+  },
+});
--- src/configs/jest/jest.config.ts
+++ src/configs/jest/jest.config.ts
@@ -1,33 +1,25 @@
 // This line loads the necessary presets for jest to work with Angular
 import 'jest-preset-angular';
 
-// Import global mocks defined by us
-import './global-mocks.fixtures.spec';
-
-// TODO: Investigate why we can not load polyfill file passing the tsconfig.json to Jest
+// TODO: An issue with `jest-preset-angular` is using only "compilerOptions" in tsconfig, using necessary imports here
+// More info about the issue: https://github.com/thymikee/jest-preset-angular/issues/347
 import '@angular/localize/init';
 
-// Extra definitons for mocking DOM interactions
-Object.defineProperty(window, 'CSS', { value: null });
+// Libraries needed in the tests
+import * as XMPP from '../../../node_modules/stanza.io/build/stanzaio.bundle.min.js';
+import * as Visibility from '../../../node_modules/visibilityjs/lib/visibility.core.js';
+import * as appboy from '../../../node_modules/appboy-web-sdk/appboy.min.js';
 
-Object.defineProperty(document, 'doctype', {
-  value: '<!DOCTYPE html>'
-});
+// For some reason, the typings and the imports are not asigning themselves as they are in the Angular app.
+// Manual assignment here
+window['XMPP'] = XMPP;
+window['Visibility'] = Visibility;
+window['appboy'] = appboy;
 
-Object.defineProperty(window, 'getComputedStyle', {
-  value: () => {
-    return {
-      display: 'none',
-      appearance: ['-webkit-appearance']
-    };
-  }
-});
+// Mock libraries we can not import because script is loaded in index.html
+import './global-mocks.fixtures.spec';
 
-Object.defineProperty(document.body.style, 'transform', {
-  value: () => {
-    return {
-      enumerable: true,
-      configurable: true,
-    };
-  },
-});
+// Extra definitions for ads libraries
+import '../../assets/js/adsConfig.js';
+import '../../assets/js/quantcast.js';
+
