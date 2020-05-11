d63108e0919d6fde5e3026593d4ddab167496984
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2020-05-11 12:17:25 +0200

    [ADV-1112]: Add Didomi keys to environments

--- src/environments/environment.interface.ts
+++ src/environments/environment.interface.ts
@@ -12,4 +12,5 @@ export interface Environment {
   stripeKey: string;
   mParticleKey: string;
   remoteConsoleUrl: string;
+  didomiKey: string;
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,5 +14,6 @@ export const environment: Environment = {
   clickStreamURL: 'https://precollector.wallapop.com/clickstream.json/sendEvents',
   stripeKey: 'pk_test_YMrctQusG3vLNQkCSRYKdqwF',
   mParticleKey: 'us1-04eb03d316b54f4f99265340abe886e6',
-  remoteConsoleUrl: 'https://client-metrics.beta.wallapop.com/metrics/chat'
+  remoteConsoleUrl: 'https://client-metrics.beta.wallapop.com/metrics/chat',
+  didomiKey: '56d12b9e-69db-4641-a15a-9c19f73aed7d'
 };
