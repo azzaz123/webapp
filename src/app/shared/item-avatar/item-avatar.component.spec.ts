eff858c702eb4cf5b83d53788bda87ffe57a366d
Przemyslaw Wrona <przemek.wrona@wallapop.com>
2020-05-13 17:03:15 +0200

    CHATO-4054 improve tests

--- src/app/shared/item-avatar/item-avatar.component.spec.ts
+++ src/app/shared/item-avatar/item-avatar.component.spec.ts
@@ -4,8 +4,9 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ItemAvatarComponent } from './item-avatar.component';
 import { MatIconModule } from '@angular/material';
 import { SanitizedBackgroundDirective } from '../sanitized-background/sanitized-background.directive';
-import { FAKE_ITEM_IMAGE_SMALL_BASE_PATH, FAKE_ITEM_IMAGE_SMALL_LIGHT_BASE_PATH } from '../../core/item/item';
-import { CREATE_MOCK_INBOX_CONVERSATION } from '../../../tests/inbox.fixtures.spec';
+import { FAKE_ITEM_IMAGE_SMALL_BASE_PATH, FAKE_ITEM_IMAGE_SMALL_LIGHT_BASE_PATH, Item } from '../../core/item/item';
+import { MOCK_ITEM } from '../../../tests/item.fixtures.spec';
+import { USER_ID } from '../../../tests/user.fixtures.spec';
 
 describe('ItemAvatarComponent', () => {
   let component: ItemAvatarComponent;
@@ -31,20 +32,20 @@ describe('ItemAvatarComponent', () => {
   });
 
   it('should set the avatar and the fallback', () => {
-    component.item = CREATE_MOCK_INBOX_CONVERSATION();
+    component.item = MOCK_ITEM;
     component.ngOnChanges();
     expect(component.fallback).toBe(FAKE_ITEM_IMAGE_SMALL_BASE_PATH);
   });
 
   it('should set the light fallback', () => {
-    component.item = CREATE_MOCK_INBOX_CONVERSATION();
+    component.item = MOCK_ITEM;
     component.fallbackLight = true;
     component.ngOnChanges();
     expect(component.fallback).toBe(FAKE_ITEM_IMAGE_SMALL_LIGHT_BASE_PATH);
   });
 
   it('should set the avatar with empty string', () => {
-    component.item = CREATE_MOCK_INBOX_CONVERSATION();
+    component.item = new Item('1', 1, USER_ID);
     component.ngOnChanges();
     expect(component.avatar).toBe('');
   });
