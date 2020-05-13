56626fcad9588dbc8c43e90b862cf88b72cd5508
UXDESIGNERLIN <chia-ling@wallapop.com>
2020-05-13 20:23:11 +0200

    [FRON-301]: remove ngOnInit

--- src/app/layout/toast/toast.component.ts
+++ src/app/layout/toast/toast.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ToastService } from '../../layout/toast/toast.service';
 
 @Component({
@@ -6,11 +6,10 @@ import { ToastService } from '../../layout/toast/toast.service';
   templateUrl: './toast.component.html',
   styleUrls: ['./toast.component.scss']
 })
-export class ToastComponent implements OnInit {
+export class ToastComponent {
 
   constructor(public toastService:ToastService) { }
 
-  ngOnInit(): void {
-  }
+ 
 
 }
