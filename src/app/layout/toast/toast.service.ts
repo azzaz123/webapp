aab155487c414c47c4329791e3d1695e8d8b708f
UXDESIGNERLIN <chia-ling@wallapop.com>
2020-05-13 09:49:05 +0200

    [FRON-301]: remove toast service comments

--- src/app/layout/toast/toast.service.ts
+++ src/app/layout/toast/toast.service.ts
@@ -16,24 +16,6 @@ export class ToastService {
   public remove(toast: Toast): void {
     this.toasts = this.toasts.filter(t => t !== toast);
   }
-  /*success(text:string, title?:string):void {
-    if (title == undefined) {
-      this.toasts.push({ text,  ...{ classname: 'bg-success text-light', delay: 10000 } });
-      this.toastService.success(text);
-    } else {
-      this.toasts.push({ text, title });
-      this.toastService.success(text, title);
-    }
-  }
 
-  error(text:string, title?:string):void {
-    if (title == undefined) {
-      this.toasts.push({ text, ...{ classname: 'bg-danger  text-light', delay: 10000 } });
-      this.toastService.error(text);
-    } else {
-      this.toasts.push({ text, title });
-      this.toastService.error(text, title);
-    }
-  }*/
 
 }
