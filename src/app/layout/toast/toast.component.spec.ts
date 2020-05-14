4229c7a6de3d221b9e737f0741795b2207aa3333
UXDESIGNERLIN <chia-ling@wallapop.com>
2020-05-14 15:17:19 +0200

    [FRON-301]: ui testing for toast

--- src/app/layout/toast/toast.component.spec.ts
+++ src/app/layout/toast/toast.component.spec.ts
@@ -1,25 +1,108 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, getTestBed } from '@angular/core/testing';
 
 import { ToastComponent } from './toast.component';
+import { ToastService } from './toast.service';
+import { By } from '@angular/platform-browser';
+import { Toast } from './toast.interface';
 
 describe('ToastComponent', () => {
-  let component: ToastComponent;
+  let injector: TestBed;
   let fixture: ComponentFixture<ToastComponent>;
+  let component: ToastComponent;
+  let toastService: ToastService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ToastComponent ]
+      providers: [{
+        provide: toastService, useValue: {
+          toasts: []
+        }
+      }],
+      declarations: [ToastComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ToastComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    injector = getTestBed();
+    toastService = injector.inject<ToastService>(ToastService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('success toast', () => {    
+    describe("when toast is triggered", () => {
+      let toastHTML;
+      const MOCKED_TOAST: Toast = { text: 'success toast', type: 'success' }
+  
+      beforeEach(() => {
+        toastService.show(MOCKED_TOAST);
+        fixture.detectChanges();
+      })
+
+      // xit('should have the `success` class', () => {
+      //   toastHTML = fixture.debugElement.query(By.directive(ToastComponent)).childNodes[0].nativeNode;
+
+      //   expect(toastHTML.hasAttribute['class']).toBe(MOCKED_TOAST.type);
+      // })
+  
+      it("should show the toast component", () => {
+        toastHTML = fixture.debugElement.query(By.css('.toast-test')).nativeNode;
+        expect(toastHTML).toBeTruthy();
+      })
+  
+      it("should show the toast text", () => {
+        toastHTML = fixture.debugElement.query(By.css('.toast-test')).nativeNode;
+
+        expect(toastHTML.innerHTML).toBe(MOCKED_TOAST.text)
+      })
+
+      it("shouldn't show the toast title", () => {
+        toastHTML = fixture.debugElement.query(By.css('.toast-title'));
+        expect(toastHTML).toBeFalsy();
+      })
+    })
+  })
+
+  describe('error toast', () => {
+    describe('when toast is triggered', () => {
+      let toastHTML;
+      const MOCKED_TOAST: Toast = { text: 'success toast', type: 'error'}
+
+      beforeEach(() => {
+        toastService.show(MOCKED_TOAST);
+        fixture.detectChanges();
+      })
+
+      it("should show the toast component", () => {
+        toastHTML = fixture.debugElement.query(By.css('.toast-test')).nativeNode;
+        expect(toastHTML).toBeTruthy();
+      })
+
+      it("should show the toast text", () => {
+        toastHTML = fixture.debugElement.query(By.css('.toast-test')).nativeNode;
+        expect(toastHTML.innerHTML).toBe(MOCKED_TOAST.text)
+      })
+
+      it("shouldn't show the toast title if there is not toast title", () => {
+        toastHTML = fixture.debugElement.query(By.css('.toast-title'));
+        expect(toastHTML).toBeFalsy();
+      })
+
+      it("should show the toast title if there is toast title", () => {
+        MOCKED_TOAST.title = 'Oops';
+        toastService.show(MOCKED_TOAST);
+        fixture.detectChanges();
+        toastHTML = fixture.debugElement.query(By.css('.toast-title'));
+        expect(toastHTML).toBeTruthy();
+      })
+
+    })
   });
 });
