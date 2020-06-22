984377728b89a7f8c1f6d3fa5f8cda5c5f41c386
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2020-06-22 16:53:19 +0200

    [TNS-575]: Rename mocks

--- src/app/core/trust-and-safety/trust-and-safety.fixtures.spec.ts
+++ src/app/core/trust-and-safety/trust-and-safety.fixtures.spec.ts
@@ -1,7 +1,9 @@
-export const MOCK_IS_STARTER_RESPONSE_WITH_STARTER_USER = {
+import { StarterResponse, GenerateSessionIdResponse } from './trust-and-safety.interface';
+
+export const MOCK_STARTER_USER_RESPONSE: StarterResponse = {
   starter: true
 };
 
-export const MOCK_IS_STARTER_RESPONSE_WITH_NON_STARTER_USER = {
+export const MOCK_NON_STARTER_USER_RESPONSE: StarterResponse = {
   starter: false
 };
