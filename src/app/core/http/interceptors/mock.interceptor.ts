99471ad6150741d834b52723124e0122626fe126
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2020-07-08 15:32:32 +0200

    [TNS-632]: Remove unused import

--- src/app/core/http/interceptors/mock.interceptor.ts
+++ src/app/core/http/interceptors/mock.interceptor.ts
@@ -7,7 +7,6 @@ import {
   HttpResponse
 } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { USER_STARTER_ENDPOINT } from 'app/core/trust-and-safety/trust-and-safety.service';
 
 export interface MockUrl {
   url: string;
