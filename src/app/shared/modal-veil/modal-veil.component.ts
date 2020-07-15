19f2eedf3321fb5176d95262de4f62f59b69e590
enric.pagerols <enric.pagerols@wallapop.com>
2020-07-15 19:22:24 +0200

    [PQP-1981]: Modal veil test

---
+++ src/app/shared/modal-veil/modal-veil.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'tsl-modal-veil',
+  templateUrl: './modal-veil.component.html',
+  styleUrls: ['./modal-veil.component.scss']
+})
+export class ModalVeil implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
