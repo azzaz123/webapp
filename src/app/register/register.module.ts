265e1732404efb9cd66d5dd4826095c7d21a2e68
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2020-07-31 12:24:40 +0200

    [TNS-692]: Create register service

--- src/app/register/register.module.ts
+++ src/app/register/register.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { registerRoutedComponents, registerNonroutedComponents, RegisterRoutingModule} from './register.routes';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RegisterService } from './register.service';
 
 @NgModule({
   imports: [
@@ -9,6 +10,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     FormsModule,
     ReactiveFormsModule,
     RegisterRoutingModule
+  ],
+  providers: [
+    RegisterService
   ],
   declarations: [
     registerRoutedComponents,
