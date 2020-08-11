71a62a86f404c0a375f90f1482346d557cc56762
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2020-08-11 11:18:29 +0200

    [TNS-694]: Remove coins permission in wallacoins tutorial

--- src/app/wallacoins/wallacoins-tutorial/wallacoins-tutorial.component.ts
+++ src/app/wallacoins/wallacoins-tutorial/wallacoins-tutorial.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgbActiveModal, NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
 import { NgbSlideEvent } from '@ng-bootstrap/ng-bootstrap/carousel/carousel';
-import { UserService } from '../../core/user/user.service';
 
 @Component({
   selector: 'tsl-wallacoins-tutorial',
@@ -12,16 +11,10 @@ export class WallacoinsTutorialComponent implements OnInit {
 
   @ViewChild(NgbCarousel, { static: true }) public carousel: NgbCarousel;
   public isLast: boolean;
-  public withCoins: boolean;
 
-  constructor(public activeModal: NgbActiveModal,
-              private userService: UserService) {
-  }
+  constructor(public activeModal: NgbActiveModal) { }
 
   ngOnInit() {
-    this.userService.hasPerm('coins').subscribe((withCoins: boolean) => {
-      this.withCoins = withCoins;
-    });
   }
 
   public onSlide($event: NgbSlideEvent) {
