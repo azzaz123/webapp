b7d131430422a254a08dfaf43665ff090b7a139d
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2020-08-11 19:21:51 +0200

    [TNS-694]: Adat logged guard tests

--- src/app/core/user/logged.guard.spec.ts
+++ src/app/core/user/logged.guard.spec.ts
@@ -5,12 +5,10 @@ import { AccessTokenService } from '../http/access-token.service';
 import { CookieService } from 'ngx-cookie';
 import * as CryptoJS from 'crypto-js';
 
-const mockCurrentUrl = 'https://web.wallapop.com/chat';
-
 describe('LoggedGuard', (): void => {
-
   let loggedGuard: LoggedGuard;
   let accessTokenService: AccessTokenService;
+  const mockCurrentUrl = 'https://web.wallapop.com/chat';
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -38,57 +36,64 @@ describe('LoggedGuard', (): void => {
   });
 
   describe('canActivate', (): void => {
-    it('should return false and redirect to SEO web with pending redirect when no access token', () => {
-      const decriptAux =
-        (toDecrypt: string) => CryptoJS.AES.decrypt(decodeURIComponent(toDecrypt), REDIRECT_SECRET).toString(CryptoJS.enc.Utf8);
-      const expectedUrl = `${environment.siteUrl}login?redirectUrl=`;
-      const expectedRedirectQueryParam = mockCurrentUrl;
+    describe('when no access token', () => {
+      it('should return false and redirect to SEO web with pending redirect', () => {
+        const decriptAux =
+          (toDecrypt: string) => CryptoJS.AES.decrypt(decodeURIComponent(toDecrypt), REDIRECT_SECRET).toString(CryptoJS.enc.Utf8);
+        const expectedUrl = `${environment.siteUrl}login?redirectUrl=`;
+        const expectedRedirectQueryParam = mockCurrentUrl;
 
-      const result = loggedGuard.canActivate();
-      const resultRedirectQueryParam = window.location.href.split('?')[1].replace('redirectUrl=', '');
+        const result = loggedGuard.canActivate();
+        const resultRedirectQueryParam = window.location.href.split('?')[1].replace('redirectUrl=', '');
 
-      expect(result).toEqual(false);
-      expect(expectedUrl.startsWith(expectedUrl)).toEqual(true);
-      expect(decriptAux(resultRedirectQueryParam)).toEqual(expectedRedirectQueryParam);
-    });
-
-    it('should return true and NOT redirect to SEO web if access token', () => {
-      accessTokenService.storeAccessToken('abc');
-      const notExpectedUrl = `${environment.siteUrl}login?redirectUrl=`;
-
-      const result = loggedGuard.canActivate();
-
-      expect(result).toBeTruthy();
-      expect(window.location.href.startsWith(notExpectedUrl)).toEqual(false);
-    });
-  });
-
-  describe('canLoad', () => {
-    const redirectUrl = encodeURIComponent(window.location.href);
-
-    describe('when there is no access token in cookies', () => {
-      beforeEach(() => accessTokenService.deleteAccessToken());
-
-      it('should return false and redirect', () => {
-        let result: boolean;
-
-        result = loggedGuard.canLoad();
-
-        expect(result).toBeFalsy();
-        expect(window.location.href).toBe(`${environment.siteUrl}login?redirectUrl=${redirectUrl}`);
+        expect(result).toEqual(false);
+        expect(window.location.href.startsWith(expectedUrl)).toEqual(true);
+        expect(decriptAux(resultRedirectQueryParam)).toEqual(expectedRedirectQueryParam);
       });
     });
 
-    describe('when there is access token in cookies', () => {
-      beforeEach(() => accessTokenService.storeAccessToken('mockToken'));
+    describe('when access token in cookies', () => {
+      beforeEach(() => accessTokenService.storeAccessToken('abc'));
 
-      it('should return true and do NOT redirect if access token', () => {
-        let result: boolean;
+      it('should return true and NOT redirect to SEO web if access token', () => {
+        const notExpectedUrl = `${environment.siteUrl}login?redirectUrl=`;
 
-        result = loggedGuard.canLoad();
+        const result = loggedGuard.canActivate();
 
-        expect(result).toBeTruthy();
-        expect(window.location.href).not.toBe(`${environment.siteUrl}login?redirectUrl=${redirectUrl}`);
+        expect(result).toEqual(true);
+        expect(window.location.href.startsWith(notExpectedUrl)).toEqual(false);
+      });
+    });
+
+  });
+
+  describe('canLoad', () => {
+    describe('when no access token', () => {
+      it('should return false and redirect to SEO web with pending redirect', () => {
+        const decriptAux =
+          (toDecrypt: string) => CryptoJS.AES.decrypt(decodeURIComponent(toDecrypt), REDIRECT_SECRET).toString(CryptoJS.enc.Utf8);
+        const expectedUrl = `${environment.siteUrl}login?redirectUrl=`;
+        const expectedRedirectQueryParam = mockCurrentUrl;
+
+        const result = loggedGuard.canActivate();
+        const resultRedirectQueryParam = window.location.href.split('?')[1].replace('redirectUrl=', '');
+
+        expect(result).toEqual(false);
+        expect(window.location.href.startsWith(expectedUrl)).toEqual(true);
+        expect(decriptAux(resultRedirectQueryParam)).toEqual(expectedRedirectQueryParam);
+      });
+    });
+
+    describe('when access token in cookies', () => {
+      beforeEach(() => accessTokenService.storeAccessToken('abc'));
+
+      it('should return true and NOT redirect to SEO web if access token', () => {
+        const notExpectedUrl = `${environment.siteUrl}login?redirectUrl=`;
+
+        const result = loggedGuard.canActivate();
+
+        expect(result).toEqual(true);
+        expect(window.location.href.startsWith(notExpectedUrl)).toEqual(false);
       });
     });
   });
