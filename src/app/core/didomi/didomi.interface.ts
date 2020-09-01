e4ee55dddd0bedf00619a760dc5b7cba45d09b11
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2020-09-01 17:01:59 +0200

    [ADV-1273]:  Don't segmentate if user disables at least one purpose

--- src/app/core/didomi/didomi.interface.ts
+++ src/app/core/didomi/didomi.interface.ts
@@ -1,4 +1,10 @@
 export interface DidomiLibrary {
-  getUserConsentStatusForPurpose(purpouseKey: string): boolean;
-  getUserConsentStatusForVendor(vendorKey: string): boolean;
+  getUserConsentStatusForAll(): DidomiUserConsents;
+}
+
+export interface DidomiUserConsents {
+  purposes: {
+    enabled: string[],
+    disabled: string[]
+  };
 }
