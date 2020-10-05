13ea4877cb5d1fd12e00aab75d69c87817f396ab
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2020-10-05 17:24:23 +0200

    Remove dead code in message service

--- src/app/chat/service/message.service.ts
+++ src/app/chat/service/message.service.ts
@@ -1,18 +1,12 @@
 import { Injectable } from '@angular/core';
-import { UUID } from 'angular2-uuid';
 import { Subject } from 'rxjs';
 import { Conversation } from '../../core/conversation/conversation';
 import { Message } from '../../core/message/message';
 import { UserService } from '../../core/user/user.service';
 import { User } from '../../core/user/user';
-import { ConnectionService } from '../../core/connection/connection.service';
-
-import { EventService } from '../../core/event/event.service';
 import { I18nService } from '../../core/i18n/i18n.service';
-import { TrackingService } from '../../core/tracking/tracking.service';
 import { RealTimeService } from '../../core/message/real-time.service';
-import { InboxConversation, InboxMessage, MessageStatus, MessageType } from '../model';
-import { XmppBodyMessage } from '../../core/xmpp/xmpp.interface';
+import { InboxConversation } from '../model';
 
 @Injectable()
 export class MessageService {
@@ -22,10 +16,7 @@ export class MessageService {
 
   constructor(private realTime: RealTimeService,
               private userService: UserService,
-              private connectionService: ConnectionService,
-              private i18n: I18nService,
-              private trackingService: TrackingService,
-              private eventService: EventService) {
+              private i18n: I18nService) {
   }
 
   set totalUnreadMessages(value: number) {
@@ -36,21 +27,6 @@ export class MessageService {
 
   get totalUnreadMessages(): number {
     return this._totalUnreadMessages;
-  }
-
-  public addUserInfoToArray(conversation: Conversation, messages: Message[]): Message[] {
-    return messages.map((message) => this.addUserInfo(conversation, message));
-  }
-
-  public addUserInfo(conversation: Conversation, message: Message): Message {
-    const self: User = this.userService.user;
-    const other: User = conversation.user;
-    const fromId: string = message.from;
-    message.user = (fromId === self.id) ? self : other;
-    /* fromSelf: The second part of condition is used to exclude 3rd voice messages, where 'from' = the id of the user
-    logged in, but they should not be considered messages fromSelf */
-    message.fromSelf = fromId === self.id && !message.payload;
-    return message;
   }
 
   public send(conversation: InboxConversation, message: string): string {
