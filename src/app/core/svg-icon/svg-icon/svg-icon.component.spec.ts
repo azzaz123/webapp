bea2b7e4298c38960ec567f736f9f83c1335815f
Laia <laialopezz99@gmail.com>
2020-10-23 09:06:38 +0200

    [WTF-42]: Testing Done

--- src/app/core/svg-icon/svg-icon/svg-icon.component.spec.ts
+++ src/app/core/svg-icon/svg-icon/svg-icon.component.spec.ts
@@ -1,36 +1,87 @@
-import { ElementRef } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, ElementRef } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DomSanitizer } from '@angular/platform-browser';
+import { of } from 'rxjs';
+import { SvgService } from '../svg.service';
 
 import { SvgIconComponent } from './svg-icon.component';
 
-fdescribe('SvgIconComponent', () => {
+describe('SvgIconComponent', () => {
   let component: SvgIconComponent;
   let fixture: ComponentFixture<SvgIconComponent>;
- 
+  let svgService: SvgService;
+  let domSanitizer: DomSanitizer;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SvgIconComponent ],
+      imports: [
+        HttpClientTestingModule
+      ],
+      declarations: [SvgIconComponent],
       providers: [
         {
           provide: ElementRef, useValue: {
-            nativeElement: { }
+            nativeElement: {}
           }
         }
-      ]
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SvgIconComponent);
 
     component = fixture.componentInstance;
+    svgService = TestBed.inject(SvgService);
+    domSanitizer = TestBed.inject(DomSanitizer);
 
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit()', () => {
+    describe('should do calls', () => {
+      beforeEach(() => {
+        component.src = 'mySvg.svg';
+        fixture.detectChanges();
+        spyOn(svgService, 'getIconByPath').and.returnValue(of(component.src));
+
+      });
+
+      it('should call svgService', () => {
+        component.ngOnInit();
+
+        expect(svgService.getIconByPath).toHaveBeenCalled();
+      });
+
+      it('should call Sanitizer', () => {
+        spyOn(domSanitizer, 'bypassSecurityTrustHtml').and.returnValue('svgElement');
+        spyOn(domSanitizer, 'sanitize').and.returnValue('safeSvg');
+
+        component.ngOnInit();
+
+        expect(domSanitizer.bypassSecurityTrustHtml).toHaveBeenCalledWith(component.src);
+        expect(domSanitizer.sanitize).toHaveBeenCalled();
+      });
+    });
+
+    describe('shouldnt make calls', () => {
+      it('shouldnt call svgService', () => {
+        component.src = 'myPhoto.png';
+        spyOn(svgService, 'getIconByPath');
+
+        fixture.detectChanges();
+        component.ngOnInit();
+
+        expect(svgService.getIconByPath).not.toHaveBeenCalled();
+      });
+    });
   });
 
 });
--- src/app/core/svg-icon/svg.service.spec.ts
+++ src/app/core/svg-icon/svg.service.spec.ts
@@ -1,16 +1,38 @@
 import { TestBed } from '@angular/core/testing';
-
 import { SvgService } from './svg.service';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 
 describe('SvgService', () => {
   let service: SvgService;
+  let httpClient: HttpClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
+    httpClient = TestBed.inject(HttpClient);
     service = TestBed.inject(SvgService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getIconByPath', () => {
+    it('should call httpClient', () => {
+      const path = 'svgPath';
+      spyOn(httpClient, 'get').and.returnValue(of('svg'));
+
+      service.getIconByPath(path);
+
+      expect(httpClient.get).toHaveBeenCalledWith(
+        path, {
+        responseType: 'text'
+      });
+    });
   });
 });
