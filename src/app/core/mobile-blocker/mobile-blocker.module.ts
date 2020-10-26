9fedbcb6249971902ed70987b83b005f9b7d90cf
Laia <laialopezz99@gmail.com>
2020-10-26 15:38:48 +0100

    [WTF-44]: [Refactor template - OwnSvgComponent] First 50

--- src/app/core/mobile-blocker/mobile-blocker.module.ts
+++ src/app/core/mobile-blocker/mobile-blocker.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MobileBlockerComponent } from './mobile-blocker.component';
 import { MatIconModule } from '@angular/material';
+import { SharedModule } from 'app/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatIconModule
+    MatIconModule,
+    SharedModule
   ],
   declarations: [
     MobileBlockerComponent
