1387b2c12239b7721b5a2ae1a7b415284cfe7128
Laia <laialopezz99@gmail.com>
2020-10-29 13:11:17 +0100

    [WTF-44]: [Review comments] Second part

--- src/app/chat/modals/report-user/report-user.component.ts
+++ src/app/chat/modals/report-user/report-user.component.ts
@@ -34,15 +34,15 @@ export class ReportUserComponent implements OnInit {
     });
   }
 
-  private getSvgPath(reasonId: string): string {
+  public getSvgPath(reasonId: number): string {
     const path = '/assets/icons/';
-    const pathCases: object = {
-      '0': 'ghost.svg',
-      '3': 'scam.svg',
-      '4': 'suspicious.svg',
-      '5': 'behaviour.svg',
-      '6': 'no-show.svg',
-      '7': 'defective.svg'
+    const pathCases = {
+      0: 'ghost.svg',
+      3: 'scam.svg',
+      4: 'suspicious.svg',
+      5: 'behaviour.svg',
+      6: 'no-show.svg',
+      7: 'defective.svg'
     };
 
     return `${path}${pathCases[reasonId]}`;
--- src/app/shared/catalog/cart/cart.component.ts
+++ src/app/shared/catalog/cart/cart.component.ts
@@ -212,7 +212,8 @@ export class CartComponent implements OnInit, OnDestroy {
     this.setCardInfo(selectedCard);
   }
 
-  private handleIconPath(type: string): string {
-    return `/assets/icons/wing-${type.split('bump')[0]}.svg`;
+  public handleIconPath(type: string): string {
+    const iconBump = type.replace('bump', '');
+    return `/assets/icons/wing-${iconBump}.svg`;
   }
 }
