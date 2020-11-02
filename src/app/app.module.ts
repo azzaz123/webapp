1c3632ae6608a13ade6f2fbe0414d87e3271c00e
Gonzalo Morales <gonzalomorales93@gmail.com>
2020-11-02 17:59:31 +0100

    [WEB-14]: route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,7 +30,7 @@ import { environment } from '../environments/environment';
     AppRoutingModule,
     LayoutModule,
     NgxPermissionsModule.forRoot(),
-    ServiceWorkerModule.register('./ngsw-worker.js', {
+    ServiceWorkerModule.register('src/ngsw-worker.js', {
       enabled: true,
       registrationStrategy: "registerImmediately"
     })
