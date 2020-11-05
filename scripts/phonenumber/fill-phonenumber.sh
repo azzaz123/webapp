6ae37f7f30a80dafccc4bad13ab591458984e043
Laia Lopez <laialopezz99@gmail.com>
2020-11-05 16:50:54 +0100

    [WEB-36]: Refactor

--- scripts/phonenumber/fill-phonenumber.sh
+++ scripts/phonenumber/fill-phonenumber.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+"" > ./src/assets/js/metadata-phonenumber.js
 libphonenumber-generate-metadata ./src/assets/js/metadata-phonenumber.js --countries ES --extended && 
 echo "export const metadata = $(cat ./src/assets/js/metadata-phonenumber.js)" > ./src/assets/js/metadata-phonenumber.js
