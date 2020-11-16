6a5a1cb8cb6317fabf4ef3297666f05ada36462c
Laia Lopez <laialopezz99@gmail.com>
2020-11-16 12:04:41 +0100

    [FRON-406]: Refactor FormValidators (Trim)

--- src/app/core/form-validators/formValidators.func.ts
+++ src/app/core/form-validators/formValidators.func.ts
@@ -1,12 +1,11 @@
 import { AbstractControl } from '@angular/forms';
-import { cloneDeep } from 'lodash-es';
 
 export function whitespaceValidator(
   control: AbstractControl
 ): { [key: string]: boolean } {
   const isEmpty =
     control.value && typeof control.value === 'string'
-      ? cloneDeep(control.value).replace(/\s+/g, '') === ''
+      ? control.value.trim() === ''
       : false;
 
   return !isEmpty ? null : { whitespace: true };
