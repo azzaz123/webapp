36d8e3e165a7cecb468cf4e4f62b2bbd776c39ca
Laia Lopez <laialopezz99@gmail.com>
2020-11-17 17:29:53 +0100

    [WEB-74]: Loc uncomment

--- src/app/profile/profile-info/profile-info.component.ts
+++ src/app/profile/profile-info/profile-info.component.ts
@@ -106,11 +106,11 @@ export class ProfileInfoComponent implements CanComponentDeactivate {
     if (this.user.location) {
       userData = {
         ...userData,
-        // location: {
-        //   address: this.user.location.title,
-        //   latitude: this.user.location.approximated_latitude,
-        //   longitude: this.user.location.approximated_longitude,
-        // },
+        location: {
+          address: this.user.location.title,
+          latitude: this.user.location.approximated_latitude,
+          longitude: this.user.location.approximated_longitude,
+        },
       };
     }
 
