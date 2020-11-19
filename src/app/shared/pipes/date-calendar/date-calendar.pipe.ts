de1d1301384edab1b792129ae6a8e7b4f4b0cda0
Angel <ajarac92@gmail.com>
2020-11-19 17:16:49 +0100

    [WEB-92]: Hide pure false

--- src/app/shared/pipes/date-calendar/date-calendar.pipe.ts
+++ src/app/shared/pipes/date-calendar/date-calendar.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import * as moment from 'moment';
 
-@Pipe({ name: 'dateCalendar', pure: false })
+@Pipe({ name: 'dateCalendar' })
 export class DateCalendarPipe implements PipeTransform {
   transform(value: Date, momentCalendarSpec: moment.CalendarSpec): any {
     return moment(value).calendar(null, momentCalendarSpec);
