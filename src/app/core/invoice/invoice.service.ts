a112a0c5ff2a795b08792ce9f9428466aeac45d2
Laia Lopez <laialopezz99@gmail.com>
2020-11-24 17:23:15 +0100

    [PQP-2045]: Endpoints Updated

--- src/app/core/invoice/invoice.service.ts
+++ src/app/core/invoice/invoice.service.ts
@@ -3,69 +3,79 @@ import { environment } from 'environments/environment';
 import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Invoice } from './invoice.interface';
-import { MOCK_INVOICE_HISTORY, MOCK_INVOICE_DOWNLOAD } from '../../../tests/invoice.fixtures.spec';
 import { CategoryService } from '../category/category.service';
 import { mergeMap, map, catchError } from 'rxjs/operators';
 import { CategoryResponse } from '../category/category-response.interface';
 
-export const INVOICE_HISTORY_ENDPOINT = 'api/v3/users/me/invoice-history';
-export const INVOICE_DOWNLOAD_ENDPOINT = 'api/v3/users/me/invoice-download';
+export const PAYMENTS_API_URL = 'api/v3/payments';
+export const INVOICE_HISTORY_ENDPOINT = `${PAYMENTS_API_URL}/c2b/stripe/subscriptions/transactions`;
+export const INVOICE_DOWNLOAD_ENDPOINT = `${PAYMENTS_API_URL}/billing-info/invoice/document`;
 
 @Injectable()
 export class InvoiceService {
-
   public invoices: Invoice[];
 
-  constructor(private http: HttpClient,
-              private categoryService: CategoryService) {
-  }
+  constructor(
+    private http: HttpClient,
+    private categoryService: CategoryService
+  ) {}
 
   public getInvoices(cache: boolean = true): Observable<Invoice[]> {
     if (this.invoices && cache) {
       return of(this.invoices);
     }
 
-    return this.categoryService.getCategories()
-    .pipe(
+    return this.categoryService.getCategories().pipe(
       mergeMap((categories) => {
-        // TODO: Call correct endpoint when backend has developed it.
-        //return this.http.get<Invoice[]>(`${environment.baseUrl}${INVOICE_HISTORY_ENDPOINT}`).pipe(
-        return of(MOCK_INVOICE_HISTORY).pipe(
-        catchError((error) => {
-          return of(error);
-        }))
-        .pipe(
-          map((invoices: Invoice[]) => {
-            if (invoices.length > 0) {
-              return invoices.map((invoice: Invoice) => this.mapInvoices(invoice, categories))
-            }
-          })
-        );
+        return this.http
+          .get<Invoice[]>(`${environment.baseUrl}${INVOICE_HISTORY_ENDPOINT}`)
+          .pipe(
+            catchError((error) => {
+              return of(error);
+            })
+          )
+          .pipe(
+            map((invoices: Invoice[]) => {
+              if (invoices.length > 0) {
+                return invoices.map((invoice: Invoice) =>
+                  this.mapInvoices(invoice, categories)
+                );
+              }
+            })
+          );
       })
-    )
-  };
+    );
+  }
 
   public downloadInvoice(invoice: Invoice): Observable<Invoice[]> {
-    return this.categoryService.getCategories()
-    .pipe(
-      mergeMap((categories) => {
-        // TODO: Call correct endpoint when backend has developed it.
-        //return this.http.get<Invoice[]>(`${environment.baseUrl}${INVOICE_DOWNLOAD_ENDPOINT}/invoice.id`).pipe(
-        return of(MOCK_INVOICE_DOWNLOAD).pipe(
-        catchError((error) => {
-          return of(error);
-        }));
+    return this.categoryService.getCategories().pipe(
+      mergeMap(() => {
+        return this.http
+          .get<Invoice[]>(
+            `${environment.baseUrl}${INVOICE_DOWNLOAD_ENDPOINT}/${invoice.id}`
+          )
+          .pipe(
+            catchError((error) => {
+              return of(error);
+            })
+          );
       })
-    )
-  };
+    );
+  }
 
-  private mapInvoices(invoice: Invoice, categories: CategoryResponse[]): Invoice {
-    let category = categories.find((category: CategoryResponse) => invoice.category_id === category.category_id);
-    
+  private mapInvoices(
+    invoice: Invoice,
+    categories: CategoryResponse[]
+  ): Invoice {
+    let category = categories.find(
+      (category: CategoryResponse) =>
+        invoice.category_id === category.category_id
+    );
+
     if (!category && invoice.category_id === 0) {
       category = this.categoryService.getConsumerGoodsCategory();
     }
-    
+
     if (category) {
       invoice.category_name = category.name;
       invoice.category_icon = category.icon_id;
--- src/app/shared/invoice-history/invoice-item/invoice-item.component.ts
+++ src/app/shared/invoice-history/invoice-item/invoice-item.component.ts
@@ -8,13 +8,11 @@ import { InvoiceService } from 'app/core/invoice/invoice.service';
   styleUrls: ['./invoice-item.component.scss'],
 })
 export class InvoiceItemComponent {
-  
   @Input() invoice: Invoice;
   @Input() active: boolean;
 
   constructor(private invoiceService: InvoiceService) {}
 
-  // TODO: call endpoint to download PDF (to be decided with backend)
   public downloadInvoice(e: Event, invoice: Invoice) {
     e.stopPropagation();
     if (invoice.available && this.active) {
@@ -22,6 +20,5 @@ export class InvoiceItemComponent {
         console.log('PDF File: ', pdfFile);
       });
     }
-    
   }
 }
