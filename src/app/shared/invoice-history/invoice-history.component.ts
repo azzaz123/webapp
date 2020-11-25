b3cb3b6799b7a66422aa2f3b0ec1ca4bb184c5f1
Laia Lopez <laialopezz99@gmail.com>
2020-11-25 14:00:51 +0100

    [PQP-2045]: Invoice History Messages + Styled + Loading

--- src/app/shared/invoice-history/invoice-history.component.ts
+++ src/app/shared/invoice-history/invoice-history.component.ts
@@ -9,9 +9,12 @@ import { Invoice } from 'app/core/invoice/invoice.interface';
 })
 export class InvoiceHistoryComponent implements OnInit {
   @Input() active: boolean;
+  @Input() isBillingInfo: boolean;
+  public loading = false;
   public invoices: Invoice[];
   public limit = 5;
   public total: number;
+  public isErrorLoading = false;
   private LOAD_MORE_QUANTITY = 5;
 
   constructor(private invoiceService: InvoiceService) {}
@@ -21,10 +24,17 @@ export class InvoiceHistoryComponent implements OnInit {
   }
 
   private getInvoice(): void {
-    this.invoiceService.getInvoices().subscribe((invoices) => {
-      this.invoices = invoices;
-      this.total = invoices && invoices.length ? this.invoices.length : 0;
-    });
+    this.loading = true;
+    this.invoiceService.getInvoices().subscribe(
+      (invoices) => {
+        this.invoices = invoices;
+        this.total = invoices && invoices.length ? this.invoices.length : 0;
+        this.loading = false;
+      },
+      (error) => {
+        this.isErrorLoading = true;
+      }
+    );
   }
 
   public loadMore(): void {
@@ -48,5 +58,22 @@ export class InvoiceHistoryComponent implements OnInit {
         );
       })
     );
+  }
+
+  isShowed(keyMessage: string): boolean {
+    switch (keyMessage) {
+      case 'EmptyHistory':
+        return (
+          (!this.invoices || (this.invoices && !this.invoices.length)) &&
+          this.isBillingInfo &&
+          !this.isErrorLoading
+        );
+      case 'NotOldInvoices':
+        return (
+          (!this.invoices || (this.invoices && !this.invoices.length)) &&
+          !this.isBillingInfo &&
+          !this.isErrorLoading
+        );
+    }
   }
 }
