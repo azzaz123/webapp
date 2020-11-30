bb42b05fe7152a87efc5ec70fb277cdc00883a82
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2020-11-30 18:13:53 +0100

    [WEB-142]: Add alias paths to Jest config

--- src/configs/jest/jest.config.js
+++ src/configs/jest/jest.config.js
@@ -1,6 +1,10 @@
 // This file contains the definition of the configuration passed to jest
 // Note: this will load the jest-preset-angular file (jest.config.ts)
 
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('../../../tsconfig');
+const mappedPaths = pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src/' });
+
 module.exports = {
   rootDir: "../../../",
   preset: "jest-preset-angular",
@@ -13,10 +17,14 @@ module.exports = {
     }
   },
   moduleNameMapper: {
+    ...mappedPaths,
     "^lodash-es$": "lodash"
   },
   testPathIgnorePatterns: [
     "node_modules/",
     "fixtures\.spec\.ts",
-  ]
+  ],
+  watchPathIgnorePatterns: [
+		"node_modules/"
+	]
 };
