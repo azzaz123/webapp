4c85913dae6836617eeacab2536ea2d159814985
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2020-11-30 14:31:43 +0100

    [WEB-140]: Set default docknum in postinstall

--- scripts/postinstall/postinstall.sh
+++ scripts/postinstall/postinstall.sh
@@ -3,6 +3,9 @@
 echo "Removing unused moment locales..."
 node scripts/postinstall/remove-unused-moment-locales.js
 
+echo "Setting up default docker..."
+sh scripts/docknum/update.sh 121
+
 echo "Sync githooks with husky..."
 husky install
 
