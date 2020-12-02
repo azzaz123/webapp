0490e71bff928b2c318ccbc863de1867abb8afb5
Irene Blanco <ireneblanco@MBPIreneBlanco.home>
2020-12-02 15:48:04 +0100

    [WEB-132]: LoggedGuard - If no session navigate to /login instead of public web

--- src/app/core/user/logged.guard.ts
+++ src/app/core/user/logged.guard.ts
@@ -1,14 +1,18 @@
-import { CanActivate, CanLoad } from '@angular/router';
+import { CanActivate, CanLoad, Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { AccessTokenService } from '../http/access-token.service';
 import * as CryptoJSAES from 'crypto-js/aes';
+import { PUBLIC_PATHS } from 'app/app-routing-constants';
 
 export const REDIRECT_SECRET = 'redirectSecretBRUH';
 
 @Injectable()
 export class LoggedGuard implements CanActivate, CanLoad {
-  constructor(private accessTokenService: AccessTokenService) {}
+  constructor(
+    private accessTokenService: AccessTokenService,
+    private router: Router
+  ) {}
 
   private _getEncryptedAndEncodedRedirect(): string {
     const encryptedCurrentUrl = CryptoJSAES.encrypt(
@@ -21,10 +25,7 @@ export class LoggedGuard implements CanActivate, CanLoad {
 
   private _loggedGuardLogic(): boolean {
     if (!this.accessTokenService.accessToken) {
-      const redirect = `${
-        environment.siteUrl
-      }login?redirectUrl=${this._getEncryptedAndEncodedRedirect()}`;
-      window.location.href = redirect;
+      this.router.navigate(['/', PUBLIC_PATHS.LOGIN]);
       return false;
     }
     return true;
