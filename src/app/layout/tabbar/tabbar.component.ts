693cf7e00fa5f0f100e337d1c1e5489ba185ead1
Irene Blanco <ireneblanco@MBPIreneBlanco.home>
2020-12-03 09:16:56 +0100

    [WEB-132]: TabBar - Added isLogged property and it s udpating

--- src/app/layout/tabbar/tabbar.component.ts
+++ src/app/layout/tabbar/tabbar.component.ts
@@ -4,6 +4,8 @@ import { UserService } from '../../core/user/user.service';
 import { User } from '../../core/user/user';
 import { TabbarService } from './tabbar.service';
 import { MessageService } from '../../chat/service/message.service';
+import { EventService } from '@core/event/event.service';
+import { PUBLIC_PATHS } from 'app/app-routing-constants';
 
 @Component({
   selector: 'tsl-tabbar',
@@ -15,17 +17,20 @@ export class TabbarComponent implements OnInit {
   public homeUrl: string;
   public hidden = false;
   public hasUnreadMessages = false;
+  public isLogged: boolean;
 
   constructor(
     private userService: UserService,
     private tabBarService: TabbarService,
     private messageService: MessageService,
+    private eventService: EventService,
     @Inject('SUBDOMAIN') private subdomain: string
   ) {
     this.homeUrl = environment.siteUrl.replace('es', this.subdomain);
   }
 
   ngOnInit() {
+    this.isLogged = this.userService.isLogged;
     this.userService.me().subscribe((user) => (this.user = user));
     this.tabBarService.tabBarHidden$.subscribe(
       (hidden) => (this.hidden = hidden)
@@ -33,6 +38,12 @@ export class TabbarComponent implements OnInit {
     this.messageService.totalUnreadMessages$.subscribe(
       (unreadMessages) => (this.hasUnreadMessages = !!unreadMessages)
     );
+    this.eventService.subscribe(EventService.USER_LOGIN, () => {
+      this.isLogged = this.userService.isLogged;
+    });
+    this.eventService.subscribe(EventService.USER_LOGOUT, () => {
+      this.isLogged = this.userService.isLogged;
+    });
   }
 
   private isTextInputOrTextarea(elementType: string) {
