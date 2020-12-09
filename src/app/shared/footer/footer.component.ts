48feac349fbcbe88eaae3014af668623dc41e433
Irene Blanco <irene.blanco@wallapop.com>
2020-12-09 13:10:44 +0100

    [WEB-134]: Footer - Component logic for excluded languages

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { I18nService } from '@core/i18n/i18n.service';
+import { FOOTER_LINKS } from './constants/footer-constants';
+import { FooterLinkSection } from './interfaces/footer-link-section.interface';
 
 @Component({
   selector: 'tsl-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent implements OnInit {
-  constructor() {}
+export class FooterComponent {
+  public currentYear = new Date().getFullYear();
+  public readonly FOOTER_LINKS: FooterLinkSection[] = FOOTER_LINKS;
 
-  ngOnInit() {}
+  constructor(private i18nService: I18nService) {
+    this.FOOTER_LINKS = FOOTER_LINKS.filter(
+      (footerLinkSection: FooterLinkSection) => {
+        return !(footerLinkSection.excludedLanguages || []).includes(
+          this.i18nService.locale
+        );
+      }
+    );
+  }
 }
