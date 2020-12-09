e66173668d28dbda1c8c935296a0f42d03b1625a
Irene Blanco <irene.blanco@wallapop.com>
2020-12-09 09:46:34 +0100

    [WEB-134]: Footer - Created FOOTER_LINKS constant

---
+++ src/app/shared/footer/footer-constants.ts
@@ -0,0 +1,70 @@
+export const FOOTER_LINKS = [
+  {
+    title: $localize`:@@FooterLinksWallapopTitle:wallapop`,
+    links: [
+      {
+        label: $localize`:@@FooterLinksAboutLabel:About`,
+        href: $localize`:@@FooterLinksAboutHref:https://about.wallapop.com/en`,
+      },
+      {
+        label: $localize`:@@FooterLinksPressLabel:Press`,
+        href: $localize`:@@FooterLinksPressHref:https://about.wallapop.com/prensa`,
+      },
+      {
+        label: $localize`:@@FooterLinksJobsLabel:Jobs`,
+        href: $localize`:@@FooterLinksJobsHref:https://apply.workable.com/wallapop`,
+      },
+      {
+        label: $localize`:@@FooterLinksTeamLabel:Team`,
+        href: $localize`:@@FooterLinksTeamHref:https://about.wallapop.com/en/team`,
+      },
+    ],
+  },
+  {
+    title: $localize`:@@FooterLinksSupportTitle:Support`,
+    links: [
+      {
+        label: $localize`:@@FooterLinksFaqLabel:FAQ`,
+        href: $localize`:@@FooterLinksFaqHref:https://ayuda.wallapop.com/hc/en-us`,
+      },
+      {
+        label: $localize`:@@FooterLinksPublishRulesLabel:Publishing rules`,
+        href: $localize`:@@FooterLinksPublishRulesHref:https://ayuda.wallapop.com/hc/en-us/sections/360001177137-Publishing-rules`,
+      },
+      {
+        label: $localize`:@@FooterLinksSecurityTipsLabel:Security tips`,
+        href: $localize`:@@FooterLinksSecurityTipsHref:https://ayuda.wallapop.com/hc/en-us/sections/360001177157-Security-tips`,
+      },
+    ],
+  },
+  {
+    title: $localize`:@@FooterLinksLegalTitle:Legal`,
+    links: [
+      {
+        label: $localize`:@@FooterLinksTermsLabel:Terms`,
+        href: $localize`:@@FooterLinksTermsHref:https://about.wallapop.com/en/legal-terms-and-conditions`,
+      },
+      {
+        label: $localize`:@@FooterLinksPrivacyLabel:Privacy`,
+        href: $localize`:@@FooterLinksPrivacyHref:https://about.wallapop.com/en/privacy-policy`,
+      },
+      {
+        label: $localize`:@@FooterLinksCookiessLabel:Cookies`,
+        href: $localize`:@@FooterLinksCookiesHref:https://about.wallapop.com/en/cookies`,
+      },
+    ],
+  },
+  {
+    title: $localize`:@@FooterLinksMotorTitle:Motor`,
+    links: [
+      {
+        label: $localize`:@@FooterLinksParticularLabel:Particular`,
+        href: $localize`:@@FooterLinksParticularHref:https://about.wallapop.com/cars-buyer/`,
+      },
+      {
+        label: $localize`:@@FooterLinksProfessionalLabel:Professional`,
+        href: $localize`:@@FooterLinksProfessionalHref:https://about.wallapop.com/motor/`,
+      },
+    ],
+  },
+];
