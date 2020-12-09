a9d254f8888f902c80eca2eabfcc2a51b73d0319
Irene Blanco <irene.blanco@wallapop.com>
2020-12-09 16:57:44 +0100

    [WEB-134]: Footer - Creation test

--- src/app/shared/footer/footer.component.spec.ts
+++ src/app/shared/footer/footer.component.spec.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { I18nService } from '@core/i18n/i18n.service';
+import { SvgIconModule } from '@core/svg-icon/svg-icon.module';
 
 import { FooterComponent } from './footer.component';
 
@@ -9,7 +13,16 @@ describe('FooterComponent', () => {
   beforeEach(
     waitForAsync(() => {
       TestBed.configureTestingModule({
+        imports: [SvgIconModule, HttpClientTestingModule],
         declarations: [FooterComponent],
+        providers: [
+          {
+            provide: I18nService,
+            useValue: {
+              getTranslations() {},
+            },
+          },
+        ],
       }).compileComponents();
     })
   );
