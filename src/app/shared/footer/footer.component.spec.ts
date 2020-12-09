3d5308f116fd7a068de9356609351155227d94b0
Irene Blanco <irene.blanco@wallapop.com>
2020-12-09 17:16:36 +0100

    [WEB-134]: Footer - Tests for excluded language

--- src/app/shared/footer/footer.component.spec.ts
+++ src/app/shared/footer/footer.component.spec.ts
@@ -1,14 +1,19 @@
-import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { I18nService } from '@core/i18n/i18n.service';
 import { SvgIconModule } from '@core/svg-icon/svg-icon.module';
 
 import { FooterComponent } from './footer.component';
+import { FOOTER_LINKS } from './constants/footer-constants';
+import { FooterLink, FooterLinkSection } from './interfaces/footer.interface';
+import { DebugElement } from '@angular/core';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
+  let i18nService: I18nService;
 
   beforeEach(
     waitForAsync(() => {
@@ -20,6 +25,7 @@ describe('FooterComponent', () => {
             provide: I18nService,
             useValue: {
               getTranslations() {},
+              locale: 'en',
             },
           },
         ],
@@ -30,10 +36,46 @@ describe('FooterComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
+    el = de.nativeElement;
+    i18nService = TestBed.inject(I18nService);
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should not have excluded links on template', () => {
+    const excluded = FOOTER_LINKS.filter(
+      (footerLinkSection: FooterLinkSection) => {
+        return (footerLinkSection.excludedLanguages || []).includes(
+          i18nService.locale
+        );
+      }
+    );
+
+    excluded.forEach((footerLinkSection: FooterLinkSection) => {
+      footerLinkSection.links.forEach((sectionLink: FooterLink) => {
+        expect(el.querySelector(`[href="${sectionLink.href}"]`)).toBeFalsy();
+      });
+    });
+  });
+
+  it('should have all links except the excluded ones on template', () => {
+    const excluded = FOOTER_LINKS.filter(
+      (footerLinkSection: FooterLinkSection) => {
+        return !(footerLinkSection.excludedLanguages || []).includes(
+          i18nService.locale
+        );
+      }
+    );
+
+    excluded.forEach((footerLinkSection: FooterLinkSection) => {
+      footerLinkSection.links.forEach((sectionLink: FooterLink) => {
+        expect(el.querySelector(`[href="${sectionLink.href}"]`)).toBeTruthy();
+      });
+    });
   });
 });
