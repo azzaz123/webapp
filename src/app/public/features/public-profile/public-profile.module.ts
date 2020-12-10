f97da1091f147b1d051cda60af23dc1cac866eb1
Laia Lopez <laialopezz99@gmail.com>
2020-12-10 17:51:21 +0100

    [WEB-152]: PublicProfileService + PublicProfileModule Updated

--- src/app/public/features/public-profile/public-profile.module.ts
+++ src/app/public/features/public-profile/public-profile.module.ts
@@ -4,9 +4,15 @@ import {
   PublicProfileRoutingModule,
   publicProfileRoutedComponents,
 } from './public-profile-routing.module';
+import { StatsComponent } from '@features/stats/pages/stats.component';
+import { HeaderComponent } from '@shared/header/header.component';
 
 @NgModule({
-  declarations: [publicProfileRoutedComponents],
+  declarations: [
+    publicProfileRoutedComponents,
+    HeaderComponent,
+    StatsComponent,
+  ],
   imports: [CommonModule, PublicProfileRoutingModule],
 })
 export class PublicProfileModule {}
