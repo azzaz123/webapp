dfcb1d241caadec9a0fd4a7af7a0b7c81717ff09
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2020-12-10 12:18:37 +0100

    [WEB-151]: Create public layout module

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublicRoutingModule } from './public.routing.module';
 import { PublicCoreModule } from './core/public-core.module';
+import { PublicLayoutModule } from './layout/public-layout.module';
 
 @NgModule({
-  imports: [CommonModule, PublicCoreModule, PublicRoutingModule],
+  imports: [
+    CommonModule,
+    PublicCoreModule,
+    PublicLayoutModule,
+    PublicRoutingModule,
+  ],
 })
 export class PublicModule {}
