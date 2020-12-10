16f9668f7afd8431450d96ae79180ea3ff9515dc
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2020-12-10 12:17:54 +0100

    [WEB-151]: Create public core module

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublicRoutingModule } from './public.routing.module';
+import { PublicCoreModule } from './core/public-core.module';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, PublicRoutingModule],
+  imports: [CommonModule, PublicCoreModule, PublicRoutingModule],
 })
 export class PublicModule {}
