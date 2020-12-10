4451b8ce00c953f224fe4754024ba9898495d939
Laia Lopez <laialopezz99@gmail.com>
2020-12-10 18:16:10 +0100

    [WEB-152]: ProfileTabs Component

--- src/app/public/features/public-profile/public-profile.module.ts
+++ src/app/public/features/public-profile/public-profile.module.ts
@@ -8,6 +8,7 @@ import { StatsComponent } from '@features/stats/pages/stats.component';
 import { HeaderComponent } from '@shared/header/header.component';
 import { ShareUserComponent } from './components/share-user/share-user.component';
 import { LikeUserComponent } from './components/like-user/like-user.component';
+import { ProfileTabsComponent } from './components/profile-tabs/profile-tabs.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { LikeUserComponent } from './components/like-user/like-user.component';
     StatsComponent,
     ShareUserComponent,
     LikeUserComponent,
+    ProfileTabsComponent,
   ],
   imports: [CommonModule, PublicProfileRoutingModule],
 })
