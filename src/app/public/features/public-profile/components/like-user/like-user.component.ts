cc3052c53e3fd44525a2d4e3f982ed2b2f1dba35
Laia Lopez <laialopezz99@gmail.com>
2020-12-10 18:09:51 +0100

    [WEB-152]: LikeUser + ShareUser Component

---
+++ src/app/public/features/public-profile/components/like-user/like-user.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tsl-like-user',
+  templateUrl: './like-user.component.html',
+  styleUrls: ['./like-user.component.scss'],
+})
+export class LikeUserComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/public/features/public-profile/public-profile.module.ts
+++ src/app/public/features/public-profile/public-profile.module.ts
@@ -6,12 +6,16 @@ import {
 } from './public-profile-routing.module';
 import { StatsComponent } from '@features/stats/pages/stats.component';
 import { HeaderComponent } from '@shared/header/header.component';
+import { ShareUserComponent } from './components/share-user/share-user.component';
+import { LikeUserComponent } from './components/like-user/like-user.component';
 
 @NgModule({
   declarations: [
     publicProfileRoutedComponents,
     HeaderComponent,
     StatsComponent,
+    ShareUserComponent,
+    LikeUserComponent,
   ],
   imports: [CommonModule, PublicProfileRoutingModule],
 })
