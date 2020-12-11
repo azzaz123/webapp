374b8268d9980a188e1ffc35b157fb144f1a1b5c
Irene Blanco <irene.blanco@wallapop.com>
2020-12-11 13:46:44 +0100

    [WEB-154]: ProfileTabs - Logic on component

--- src/app/public/features/public-profile/components/profile-tabs/profile-tabs.component.ts
+++ src/app/public/features/public-profile/components/profile-tabs/profile-tabs.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { PROFILE_TABS } from './constants/profile-tab-constants';
+import { Component, Input, OnInit } from '@angular/core';
+import { PUBLIC_PROFILE_PATHS } from '../../public-profile-routing-constants';
+import { ProfileTab, PROFILE_TABS } from './constants/profile-tab-constants';
 
 @Component({
   selector: 'tsl-profile-tabs',
@@ -8,7 +9,15 @@ import { PROFILE_TABS } from './constants/profile-tab-constants';
 })
 export class ProfileTabsComponent implements OnInit {
   readonly PROFILE_TABS = PROFILE_TABS;
+  readonly INFO_TAB_PATH = PUBLIC_PROFILE_PATHS.INFO;
+
+  @Input() userStats = {};
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    PROFILE_TABS.map((tab: ProfileTab) => {
+      tab.count = this.userStats[tab.id] || 0;
+    });
+  }
 }
