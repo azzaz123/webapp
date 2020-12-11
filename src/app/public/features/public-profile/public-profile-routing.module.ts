e984301c3e6c49d0a3af38f063778786e4198e22
Laia Lopez <laialopezz99@gmail.com>
2020-12-11 09:14:06 +0100

    [WEB-152]: PublicProfilePaths + Updated components

--- src/app/public/features/public-profile/public-profile-routing.module.ts
+++ src/app/public/features/public-profile/public-profile-routing.module.ts
@@ -4,23 +4,28 @@ import { ReviewsComponent } from '@features/reviews/pages/reviews.component';
 import { InfoComponent } from './pages/info/info.component';
 import { PublicProfileComponent } from './pages/public-profile.component';
 import { PublishedComponent } from './pages/published/published.component';
+import { PUBLIC_PROFILE_PATHS } from './public-profile-routing-constants';
 
 const routes: Routes = [
   {
     path: '',
     component: PublicProfileComponent,
     children: [
-      { path: '', pathMatch: 'full', redirectTo: 'published' },
       {
-        path: 'published',
+        path: '',
+        pathMatch: 'full',
+        redirectTo: PUBLIC_PROFILE_PATHS.PUBLISHED,
+      },
+      {
+        path: PUBLIC_PROFILE_PATHS.PUBLISHED,
         component: PublishedComponent,
       },
       {
-        path: 'reviews',
+        path: PUBLIC_PROFILE_PATHS.REVIEWS,
         component: ReviewsComponent,
       },
       {
-        path: 'info',
+        path: PUBLIC_PROFILE_PATHS.INFO,
         component: InfoComponent,
       },
     ],
--- src/app/public/features/public-profile/public-profile.module.ts
+++ src/app/public/features/public-profile/public-profile.module.ts
@@ -4,8 +4,8 @@ import {
   PublicProfileRoutingModule,
   publicProfileRoutedComponents,
 } from './public-profile-routing.module';
-import { StatsComponent } from '@features/stats/pages/stats.component';
-import { HeaderComponent } from '@shared/header/header.component';
+import { UserStatsComponent } from './components/user-stats/user-stats.component';
+import { UserProfileHeaderComponent } from './components/user-profile-header/user-profile-header.component';
 import { ShareUserComponent } from './components/share-user/share-user.component';
 import { LikeUserComponent } from './components/like-user/like-user.component';
 import { ProfileTabsComponent } from './components/profile-tabs/profile-tabs.component';
@@ -13,8 +13,8 @@ import { ProfileTabsComponent } from './components/profile-tabs/profile-tabs.com
 @NgModule({
   declarations: [
     publicProfileRoutedComponents,
-    HeaderComponent,
-    StatsComponent,
+    UserProfileHeaderComponent,
+    UserStatsComponent,
     ShareUserComponent,
     LikeUserComponent,
     ProfileTabsComponent,
