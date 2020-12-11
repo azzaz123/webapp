1ddbca8940f0ce4ec3ce8e5af7532641f8bb1ade
Irene Blanco <irene.blanco@wallapop.com>
2020-12-11 11:42:09 +0100

    [WEB-154]: PublicModule - Added FooterModule

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -3,9 +3,15 @@ import { PublicRoutingModule } from './public.routing.module';
 import { PublicCoreModule } from './core/public-core.module';
 import { PublicLayoutModule } from './layout/public-layout.module';
 import { PublicComponent } from './public.component';
+import { FooterModule } from '@shared/footer/footer.module';
 
 @NgModule({
-  imports: [PublicCoreModule, PublicLayoutModule, PublicRoutingModule],
+  imports: [
+    PublicCoreModule,
+    PublicLayoutModule,
+    PublicRoutingModule,
+    FooterModule,
+  ],
   declarations: [PublicComponent],
   bootstrap: [PublicComponent],
 })
