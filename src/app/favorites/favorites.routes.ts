80f0d88ee64a97913df766fc56573d6c8350f717
ChiaLing <hochialing91@gmail.com>
2020-12-11 10:36:06 +0100

    Created routes

--- src/app/favorites/favorites.routes.ts
+++ src/app/favorites/favorites.routes.ts
@@ -4,19 +4,36 @@ import { FavoritesComponent } from './favorites.component';
 import { LoggedGuard } from '../core/user/logged.guard';
 import { NgxPermissionsGuard } from 'ngx-permissions';
 import { PERMISSIONS } from '../core/user/user';
+import { ItemCartFavoriteComponent } from './item-cart-favorite/item-cart-favorite.component';
+import { ProfileCardFavoriteComponent } from './profile-card-favorite/profile-card-favorite.component';
+import { ExitConfirmGuard } from '@shared/guards/exit-confirm.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: FavoritesComponent,
     canActivate: [LoggedGuard, NgxPermissionsGuard],
+   /*  canDeactivate: [ExitConfirmGuard],
+    canActivateChild: [NgxPermissionsGuard], */
     data: {
       isMyZone: true,
       permissions: {
         except: PERMISSIONS.professional,
         redirectTo: '/pro/profile',
-      },
+      }
     },
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'items' },
+      {
+        path: 'favorites/items',
+        component: ItemCartFavoriteComponent,
+      },
+      {
+        path: 'favorites/profile',
+        component: ProfileCardFavoriteComponent,
+      }
+    ]
+
   },
 ];
 
@@ -24,6 +41,6 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class FavoritesRoutingModule {}
+export class FavoritesRoutingModule { }
 
 export const favoritesRoutedComponents = [FavoritesComponent];
