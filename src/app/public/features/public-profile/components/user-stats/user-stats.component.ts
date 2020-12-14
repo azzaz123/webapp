ca79ac4d8350f6294744f23686035e9ed0984bb8
Laia Lopez <laialopezz99@gmail.com>
2020-12-14 13:19:01 +0100

    [WEB-153]: UserStats Updating

--- src/app/public/features/public-profile/components/user-stats/user-stats.component.ts
+++ src/app/public/features/public-profile/components/user-stats/user-stats.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { UserStats } from '@core/user/user-stats.interface';
 
 @Component({
   selector: 'tsl-user-stats',
   templateUrl: './user-stats.component.html',
   styleUrls: ['./user-stats.component.scss'],
 })
-export class UserStatsComponent implements OnInit {
+export class UserStatsComponent {
+  @Input() userStats: UserStats;
   constructor() {}
-
-  ngOnInit(): void {}
 }
