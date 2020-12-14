9a88139f421c528d6e56d7d3a1a430dbdde896fd
Irene Blanco <irene.blanco@wallapop.com>
2020-12-14 09:48:09 +0100

    [WEB-155]: Created FavouriteUserModule

--- src/app/public/features/public-profile/public-profile.module.ts
+++ src/app/public/features/public-profile/public-profile.module.ts
@@ -7,8 +7,8 @@ import {
 import { UserStatsComponent } from './components/user-stats/user-stats.component';
 import { UserProfileHeaderComponent } from './components/user-profile-header/user-profile-header.component';
 import { ShareUserComponent } from './components/share-user/share-user.component';
-import { FavouriteUserComponent } from './components/favourite-user/favourite-user.component';
 import { ProfileTabsComponent } from './components/profile-tabs/profile-tabs.component';
+import { FavouriteUserModule } from './components/favourite-user/favourite-user.module';
 
 @NgModule({
   declarations: [
@@ -16,9 +16,8 @@ import { ProfileTabsComponent } from './components/profile-tabs/profile-tabs.com
     UserProfileHeaderComponent,
     UserStatsComponent,
     ShareUserComponent,
-    FavouriteUserComponent,
     ProfileTabsComponent,
   ],
-  imports: [CommonModule, PublicProfileRoutingModule],
+  imports: [CommonModule, PublicProfileRoutingModule, FavouriteUserModule],
 })
 export class PublicProfileModule {}
