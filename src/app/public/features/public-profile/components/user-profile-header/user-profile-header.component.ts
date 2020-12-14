507515357a7411a59b77e274896d86f22bfb292e
Laia Lopez <laialopezz99@gmail.com>
2020-12-14 09:57:00 +0100

    [WEB-153]: UserProfileInfo Start

--- src/app/public/features/public-profile/components/user-profile-header/user-profile-header.component.ts
+++ src/app/public/features/public-profile/components/user-profile-header/user-profile-header.component.ts
@@ -1,12 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserInfo } from '../../core/public-profile.interface';
+import { PublicProfileService } from '../../core/services/public-profile.service';
+import { PLACEHOLDER_AVATAR } from '@core/user/user';
 
 @Component({
   selector: 'tsl-user-profile-header',
   templateUrl: './user-profile-header.component.html',
   styleUrls: ['./user-profile-header.component.scss'],
 })
-export class UserProfileHeaderComponent implements OnInit {
-  constructor() {}
+export class UserProfileHeaderComponent implements OnInit, OnDestroy {
+  @Input() userId: string;
+  userImg = PLACEHOLDER_AVATAR;
+  subscriptions: Subscription[] = [];
+  userInfo;
 
-  ngOnInit(): void {}
+  constructor(private publicProfileService: PublicProfileService) {}
+
+  ngOnInit(): void {
+    this.getUserInformation();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+  }
+
+  getUserInformation(): void {
+    this.subscriptions.push(
+      this.publicProfileService
+        .getUser(this.userId)
+        .subscribe((userInfo: UserInfo) => {
+          this.userInfo = userInfo;
+          console.log('userInfo => ', userInfo);
+        })
+    );
+  }
 }
