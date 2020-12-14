829592d92835c1caaa14e941af802d34d33f6d5e
Laia Lopez <laialopezz99@gmail.com>
2020-12-14 13:13:37 +0100

    [WEB-153]: PublicProfileModule Added Shared

--- src/app/public/features/public-profile/public-profile.module.ts
+++ src/app/public/features/public-profile/public-profile.module.ts
@@ -9,7 +9,7 @@ import { UserProfileHeaderComponent } from './components/user-profile-header/use
 import { ShareUserComponent } from './components/share-user/share-user.component';
 import { LikeUserComponent } from './components/like-user/like-user.component';
 import { ProfileTabsComponent } from './components/profile-tabs/profile-tabs.component';
-import { SvgIconModule } from '@core/svg-icon/svg-icon.module';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -20,6 +20,6 @@ import { SvgIconModule } from '@core/svg-icon/svg-icon.module';
     LikeUserComponent,
     ProfileTabsComponent,
   ],
-  imports: [CommonModule, PublicProfileRoutingModule, SvgIconModule],
+  imports: [CommonModule, PublicProfileRoutingModule, SharedModule],
 })
 export class PublicProfileModule {}
