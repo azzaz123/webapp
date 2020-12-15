78a1b0650fc1fa1e2c396967552596dba074e1e3
Irene Blanco <irene.blanco@wallapop.com>
2020-12-15 08:04:05 +0100

    [WEB-155]: FavouriteUserModule - Added PublicProfileService as provider

--- src/app/public/features/public-profile/components/favourite-user/favourite-user.module.ts
+++ src/app/public/features/public-profile/components/favourite-user/favourite-user.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FavouriteUserComponent } from './favourite-user.component';
 import { FavouriteIconModule } from '@public/core/components/favourite-icon/favourite-icon.module';
+import { PublicProfileService } from '@public/core/services/public-profile.service';
 
 @NgModule({
   declarations: [FavouriteUserComponent],
   imports: [CommonModule, FavouriteIconModule],
   exports: [FavouriteUserComponent],
+  providers: [PublicProfileService],
 })
 export class FavouriteUserModule {}
