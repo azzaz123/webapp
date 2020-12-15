49a3c6f5b160d9ba0f5f38b28126e6f943ed111d
Laia Lopez <laialopezz99@gmail.com>
2020-12-15 18:34:10 +0100

    [WEB-153]: Public profile testing mocks

--- src/app/public/fixtures/public-profile.fixtures.spec.ts
+++ src/app/public/fixtures/public-profile.fixtures.spec.ts
@@ -38,19 +38,24 @@ export const IMAGE: Image = {
       'http://dock9.wallapop.com:8080/shnm-portlet/images?pictureId=500002512&pictureSize=W320',
     xlarge:
       'http://dock9.wallapop.com:8080/shnm-portlet/images?pictureId=500002512&pictureSize=W320',
-    xsmall:
+    xmall:
       'http://dock9.wallapop.com:8080/shnm-portlet/images?pictureId=500002512&pictureSize=W320',
   },
 };
 
 export const MOCK_USER_STATS: UserStats = {
   counters: {
-    publish: 1,
-    buys: 4,
-    sells: 1,
-    reviews: 1,
-    sold: 1,
-    reports_received: 1,
+    publish: 0,
+    buys: 0,
+    sells: 0,
+    favorites: 0,
+    views: 0,
+    profile_favorited_received: 0,
+    profile_favorited: 0,
+    reviews: 0,
+    sold: 0,
+    reports_received: 5,
+    onHold: 0,
   },
   ratings: {
     reviews: 0,
@@ -69,4 +74,63 @@ export const MOCK_USER_INFO: UserInfo = {
   web_slug: USER_WEB_SLUG,
   isPro: false,
   image: IMAGE,
+};
+
+export const RATINGS = [
+  {
+    type: 'reviews',
+    value: 0,
+  },
+];
+
+export const COUNTERS = [
+  {
+    type: 'publish',
+    value: 0,
+  },
+  {
+    type: 'buys',
+    value: 0,
+  },
+  {
+    type: 'sells',
+    value: 0,
+  },
+  {
+    type: 'favorites',
+    value: 0,
+  },
+  {
+    type: 'views',
+    value: 0,
+  },
+  {
+    type: 'profile_favorited_received',
+    value: 0,
+  },
+  {
+    type: 'profile_favorited',
+    value: 0,
+  },
+  {
+    type: 'reviews',
+    value: 0,
+  },
+  {
+    type: 'sold',
+    value: 0,
+  },
+  {
+    type: 'reports_received',
+    value: 5,
+  },
+  {
+    type: 'onHold',
+    value: 0,
+  },
+];
+
+export const USERS_STATS = {
+  ratings: RATINGS,
+  counters: COUNTERS,
 };
