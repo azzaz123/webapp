502593ebbca906c5bef9d32b8b0ddd85df2f8be7
Laia Lopez <laialopezz99@gmail.com>
2020-12-15 18:31:31 +0100

    [WEB-153]: Testing Updated UserProfileHeader + UserStats

--- src/app/public/features/public-profile/components/user-stats/user-stats.component.spec.ts
+++ src/app/public/features/public-profile/components/user-stats/user-stats.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { DeviceDetectorService } from 'ngx-device-detector';
+import { RouterTestingModule } from '@angular/router/testing';
 import { UserStatsComponent } from './user-stats.component';
 
 describe('UserStatsComponent', () => {
   let component: UserStatsComponent;
   let fixture: ComponentFixture<UserStatsComponent>;
+  let deviceDetectorService: DeviceDetectorService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [UserStatsComponent],
+      providers: [
+        {
+          provide: DeviceDetectorService,
+          useValue: {},
+        },
+      ],
     }).compileComponents();
   });
 
