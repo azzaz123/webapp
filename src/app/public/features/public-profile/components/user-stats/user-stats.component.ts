6e7c12a7d9f99e379f458cf91ce78086d229e76b
Laia Lopez <laialopezz99@gmail.com>
2020-12-15 12:18:10 +0100

    [WEB-153]: UserStats Styled + Logic

--- src/app/public/features/public-profile/components/user-stats/user-stats.component.ts
+++ src/app/public/features/public-profile/components/user-stats/user-stats.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { UserStats } from '@core/user/user-stats.interface';
+import { UserInfo } from '../../core/public-profile.interface';
 
 @Component({
   selector: 'tsl-user-stats',
@@ -8,5 +9,14 @@ import { UserStats } from '@core/user/user-stats.interface';
 })
 export class UserStatsComponent {
   @Input() userStats: UserStats;
+  @Input() userInfo: UserInfo;
+  isPhone = false;
+
   constructor() {}
+
+  togglePhone(): void {
+    this.isPhone = !this.isPhone;
+  }
+
+  showLocation(): void {}
 }
