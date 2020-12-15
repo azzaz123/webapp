1c93bab88416f6e6f29f0fae87858754ced75070
Irene Blanco <irene.blanco@wallapop.com>
2020-12-15 10:44:46 +0100

    [WEB-154]: PublicLayoutModule - Set FooterModule as export instead of import

--- src/app/public/layout/public-layout.module.ts
+++ src/app/public/layout/public-layout.module.ts
@@ -2,6 +2,6 @@ import { NgModule } from '@angular/core';
 import { FooterModule } from '@shared/footer/footer.module';
 
 @NgModule({
-  imports: [FooterModule],
+  exports: [FooterModule],
 })
 export class PublicLayoutModule {}
