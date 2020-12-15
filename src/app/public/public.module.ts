9be70e37ec70db779b5014304582e96ec1fd7baa
Irene Blanco <irene.blanco@wallapop.com>
2020-12-15 10:27:50 +0100

    [WEB-154]: Moved FooterModule from PublicModule to PublicLayoutModule

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -3,15 +3,9 @@ import { PublicRoutingModule } from './public.routing.module';
 import { PublicCoreModule } from './core/public-core.module';
 import { PublicLayoutModule } from './layout/public-layout.module';
 import { PublicComponent } from './public.component';
-import { FooterModule } from '@shared/footer/footer.module';
 
 @NgModule({
-  imports: [
-    PublicCoreModule,
-    PublicLayoutModule,
-    PublicRoutingModule,
-    FooterModule,
-  ],
+  imports: [PublicCoreModule, PublicLayoutModule, PublicRoutingModule],
   declarations: [PublicComponent],
   bootstrap: [PublicComponent],
 })
