708ac525fcc9fc85ffb80e9143ee3b3c447dbcca
Irene Blanco <irene.blanco@wallapop.com>
2020-12-16 09:29:17 +0100

    [WEB-155]: FavouriteUser - Added CheckSessionModule

--- src/app/public/features/public-profile/components/favourite-user/favourite-user.module.ts
+++ src/app/public/features/public-profile/components/favourite-user/favourite-user.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FavouriteUserComponent } from './favourite-user.component';
 import { FavouriteIconModule } from '@public/core/components/favourite-icon/favourite-icon.module';
 import { PublicProfileService } from '@public/core/services/public-profile.service';
+import { CheckSessionModule } from '@public/core/directives/check-session/check-session.module';
 
 @NgModule({
   declarations: [FavouriteUserComponent],
-  imports: [CommonModule, FavouriteIconModule],
+  imports: [CommonModule, FavouriteIconModule, CheckSessionModule],
   exports: [FavouriteUserComponent],
   providers: [PublicProfileService],
 })
