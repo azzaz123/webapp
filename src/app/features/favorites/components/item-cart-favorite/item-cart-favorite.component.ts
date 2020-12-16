cac93fdccac3f2fda35e960e3e3910c421e8f9b4
ChiaLing <hochialing91@gmail.com>
2020-12-16 16:09:24 +0100

    Removed router in constructor

--- src/app/features/favorites/components/item-cart-favorite/item-cart-favorite.component.ts
+++ src/app/features/favorites/components/item-cart-favorite/item-cart-favorite.component.ts
@@ -26,7 +26,6 @@ export class ItemCartFavoriteComponent implements OnInit {
     private itemService: ItemService,
     private modalService: NgbModal,
     private trackingService: TrackingService,
-    private router: Router,
     @Inject('SUBDOMAIN') private subdomain: string
   ) {}
 
@@ -53,7 +52,6 @@ export class ItemCartFavoriteComponent implements OnInit {
 
   removeFavorite() {
     this.itemService.favoriteItem(this.item.id, false).subscribe(() => {
-      console.log('cart removed item');
       this.item.favorited = false;
       this.onFavoriteChange.emit(this.item);
       this.trackingService.track(TrackingService.FAVOURITES_BUTTON_UNFAVORITE);
