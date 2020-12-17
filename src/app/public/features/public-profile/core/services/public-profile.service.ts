c20d957f3a70a2fdf07ccee0c48f33fc32262de6
Irene Blanco <irene.blanco@wallapop.com>
2020-12-17 08:26:24 +0100

    [WEB-155]: Moved public-profile-request.interface  to correct folder

--- src/app/public/features/public-profile/core/services/public-profile.service.ts
+++ src/app/public/features/public-profile/core/services/public-profile.service.ts
@@ -2,11 +2,11 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import {
   IsFavouriteBodyResponse,
   MarkAsFavouriteBodyRequest,
-} from '@public/core/interfaces/public-profile-request.interface';
-import { map } from 'rxjs/operators';
+} from '../interfaces/public-profile-request.interface';
 
 export const PROFILE_API_URL = 'api/v3/users/';
 export const FAVOURITE_API_PATH = 'favorite';
