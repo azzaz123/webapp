ed7c25ca303eb2a721e2b4501b5744df4d9c5065
Laia Lopez <laialopezz99@gmail.com>
2020-12-21 12:55:33 +0100

    [WEB-153]: [COMMENTS] Loading added

--- src/app/public/features/public-profile/pages/public-profile.component.ts
+++ src/app/public/features/public-profile/pages/public-profile.component.ts
@@ -4,6 +4,7 @@ import { User } from '@core/user/user';
 import { Image } from '@core/user/user-response.interface';
 import { UserStats } from '@core/user/user-stats.interface';
 import { forkJoin, Subscription } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 import { PublicProfileService } from '../core/services/public-profile.service';
 
 @Component({
@@ -15,6 +16,7 @@ export class PublicProfileComponent implements OnInit, OnDestroy {
   public userId: string;
   public userStats: UserStats;
   public userInfo: User;
+  public loading = false;
   private subscriptions: Subscription[] = [];
 
   constructor(
@@ -40,18 +42,21 @@ export class PublicProfileComponent implements OnInit, OnDestroy {
   }
 
   private getUserInfoAndStats(): void {
+    this.loading = true;
     this.subscriptions.push(
       forkJoin([
         this.publicProfileService.getUser(this.userId),
         this.publicProfileService.getStats(this.userId),
         this.publicProfileService.getCoverImage(this.userId),
-      ]).subscribe(
-        ([userInfo, userStats, coverImage]: [User, UserStats, Image]) => {
-          this.userInfo = userInfo;
-          this.userInfo.coverImage = coverImage;
-          this.userStats = userStats;
-        }
-      )
+      ])
+        .pipe(finalize(() => (this.loading = false)))
+        .subscribe(
+          ([userInfo, userStats, coverImage]: [User, UserStats, Image]) => {
+            this.userInfo = userInfo;
+            this.userInfo.coverImage = coverImage;
+            this.userStats = userStats;
+          }
+        )
     );
   }
 }
