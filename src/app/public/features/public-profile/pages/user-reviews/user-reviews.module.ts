4e7c1eccf090d98a21aabdac9ff93887618341cc
Irene Blanco <irene.blanco@wallapop.com>
2020-12-22 16:08:55 +0100

    [WEB-160]: UserReviewsModule - Usage of UserReviewsService

--- src/app/public/features/public-profile/pages/user-reviews/user-reviews.module.ts
+++ src/app/public/features/public-profile/pages/user-reviews/user-reviews.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReviewItemModule } from '@features/reviews/components/review-item/review-item.module';
+import { UserReviewsService } from '@public/features/public-profile/core/services/user-reviews/user-reviews.service';
 import { UserReviewsComponent } from './user-reviews.component';
 
 @NgModule({
   imports: [CommonModule, ReviewItemModule],
   declarations: [UserReviewsComponent],
-  providers: [],
+  providers: [UserReviewsService],
   exports: [UserReviewsComponent],
 })
 export class UserReviewsModule {}
