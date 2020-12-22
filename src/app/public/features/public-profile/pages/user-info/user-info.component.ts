c9ac557d40f524bba12de8f67e39d0739255b5be
Laia Lopez <laialopezz99@gmail.com>
2020-12-22 09:59:41 +0100

    [WEB-161]: Checks + Margin + HereMaps

--- src/app/public/features/public-profile/pages/user-info/user-info.component.ts
+++ src/app/public/features/public-profile/pages/user-info/user-info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Coordinate } from '@core/geolocation/address-response.interface';
+import { SOCIAL_MEDIA_INFO } from '../../public-profile-routing-constants';
 
 @Component({
   selector: 'tsl-user-info',
@@ -6,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-info.component.scss'],
 })
 export class UserInfoComponent implements OnInit {
+  public coordinates: Coordinate;
   constructor() {}
 
   ngOnInit(): void {}
+
+  open(socialMedia: SOCIAL_MEDIA_INFO): void {
+    if (socialMedia === SOCIAL_MEDIA_INFO.EMAIL) {
+    } else if (socialMedia === SOCIAL_MEDIA_INFO.FACEBOOK) {
+    }
+
+    // It's phone handle
+    return;
+  }
 }
