759b000e1dc671c64397ee7d2f6324f29bbf019f
Irene Blanco <irene.blanco@wallapop.com>
2020-12-22 14:59:04 +0100

    [WEB-160]: StarsModule - Added missing SvgIconModule

--- src/app/shared/stars/stars.module.ts
+++ src/app/shared/stars/stars.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SvgIconModule } from '@core/svg-icon/svg-icon.module';
 import { StarsComponent } from './stars.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, SvgIconModule],
   declarations: [StarsComponent],
   exports: [StarsComponent],
 })
