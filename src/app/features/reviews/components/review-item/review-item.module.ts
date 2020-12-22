64b9887a4450766050ebdd357415bafb75bec8dc
Irene Blanco <irene.blanco@wallapop.com>
2020-12-22 15:15:44 +0100

    [WEB-160]: ReviewItemModule - Added missing imports

--- src/app/features/reviews/components/review-item/review-item.module.ts
+++ src/app/features/reviews/components/review-item/review-item.module.ts
@@ -1,9 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SvgIconModule } from '@core/svg-icon/svg-icon.module';
 import { UserReviewService } from '@features/reviews/core/user-review.service';
+import { SanitizedBackgroundModule } from '@shared/sanitized-background/sanitized-background.module';
+import { StarsModule } from '@shared/stars/stars.module';
+import { UserAvatarModule } from '@shared/user-avatar/user-avatar.module';
 import { ReviewItemComponent } from './review-item.component';
 
 @NgModule({
-  imports: [],
+  imports: [
+    CommonModule,
+    SvgIconModule,
+    StarsModule,
+    UserAvatarModule,
+    SanitizedBackgroundModule,
+  ],
   declarations: [ReviewItemComponent],
   providers: [UserReviewService],
   exports: [ReviewItemComponent],
