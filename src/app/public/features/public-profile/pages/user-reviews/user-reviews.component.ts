2c2d6b04ba7a52acb583a19a9de10b02cbc9b44c
Irene Blanco <irene.blanco@wallapop.com>
2020-12-23 12:51:24 +0100

    [WEB-160]: UserReviews - Updated with last services

--- src/app/public/features/public-profile/pages/user-reviews/user-reviews.component.ts
+++ src/app/public/features/public-profile/pages/user-reviews/user-reviews.component.ts
@@ -1,59 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { UserReviewsService } from '@public/features/public-profile/core/services/user-reviews/user-reviews.service';
+import { Component } from '@angular/core';
+import { MapReviewService } from '@public/features/public-profile/pages/user-reviews/services/map-review/map-review.service';
 import { PublicProfileService } from '@public/features/public-profile/core/services/public-profile.service';
+import { PaginationService } from '@public/core/services/pagination/pagination.service';
 
 @Component({
   selector: 'tsl-user-reviews',
   templateUrl: './user-reviews.component.html',
   styleUrls: ['./user-reviews.component.scss'],
 })
-export class UserReviewsComponent implements OnInit {
+export class UserReviewsComponent {
   public reviews = [];
   constructor(
     private publicProfileService: PublicProfileService,
-    private userReviewsService: UserReviewsService
+    private mapReviewService: MapReviewService,
+    private paginationService: PaginationService
   ) {
-    this.publicProfileService
-      .getReviews('1kmzngw3g6n3')
+    this.paginationService
+      .getItems(this.publicProfileService.getReviews('1kmzngw3g6n3'))
       .subscribe((test: any) => {
-        console.log(test);
-        this.reviews = [
-          {
-            item: { category_id: 13100 },
-            review: {
-              date: 1456864940000,
-              scoring: 100,
-              comments: ' ',
-              is_shipping_transaction: false,
-            },
-            user: {
-              id: 'evjrdprn4l6k',
-              micro_name: 'Irene R.',
-              image: {
-                original:
-                  'http://cdn.wallapop.com/images/13/0b/hw/__/c13p19311167/i142695722.jpg?pictureSize=W1024',
-                xsmall:
-                  'http://cdn.wallapop.com/images/13/0b/hw/__/c13p19311167/i142695722.jpg?pictureSize=W320',
-                small:
-                  'http://cdn.wallapop.com/images/13/0b/hw/__/c13p19311167/i142695722.jpg?pictureSize=W320',
-                large:
-                  'http://cdn.wallapop.com/images/13/0b/hw/__/c13p19311167/i142695722.jpg?pictureSize=W800',
-                medium:
-                  'http://cdn.wallapop.com/images/13/0b/hw/__/c13p19311167/i142695722.jpg?pictureSize=W640',
-                xlarge:
-                  'http://cdn.wallapop.com/images/13/0b/hw/__/c13p19311167/i142695722.jpg?pictureSize=W1024',
-                original_width: 1024,
-                original_height: 1024,
-              },
-              web_slug: 'irene-19311167',
-            },
-            type: 'buy',
-          },
-        ];
-
-        this.reviews = this.userReviewsService.mapReviews(this.reviews);
+        this.reviews = this.mapReviewService.mapItems(test.results);
       });
   }
-
-  ngOnInit(): void {}
 }
--- src/app/public/features/public-profile/pages/user-reviews/user-reviews.module.ts
+++ src/app/public/features/public-profile/pages/user-reviews/user-reviews.module.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReviewItemModule } from '@features/reviews/components/review-item/review-item.module';
-import { UserReviewsService } from '@public/features/public-profile/core/services/user-reviews/user-reviews.service';
+import { MapReviewService } from '@public/features/public-profile/pages/user-reviews/services/map-review/map-review.service';
 import { UserReviewsComponent } from './user-reviews.component';
 
 @NgModule({
   imports: [CommonModule, ReviewItemModule],
   declarations: [UserReviewsComponent],
-  providers: [UserReviewsService],
+  providers: [MapReviewService],
   exports: [UserReviewsComponent],
 })
 export class UserReviewsModule {}
