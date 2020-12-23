2b17f60fa2f6cc62c06b0ec06e9126c31606e140
Laia Lopez <laialopezz99@gmail.com>
2020-12-23 10:30:13 +0100

    [WEB-161]: UserInfo Testing

--- src/app/public/features/public-profile/pages/user-info/user-info.component.spec.ts
+++ src/app/public/features/public-profile/pages/user-info/user-info.component.spec.ts
@@ -1,12 +1,18 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { MOCK_USER } from '@fixtures/user.fixtures.spec';
+import { PublicProfileService } from '../../core/services/public-profile.service';
 
 import { UserInfoComponent } from './user-info.component';
 
 describe('UserInfoComponent', () => {
+  let publicProfileService: PublicProfileService;
   const mapTag = 'tsl-here-maps';
   const userResponseTag = 'tsl-user-response-rate';
-  const containerClass = 'UserInfo';
+  const containerClass = '.UserInfo';
+  const fakeMapClass = '.fake-map';
 
   let component: UserInfoComponent;
   let fixture: ComponentFixture<UserInfoComponent>;
@@ -15,6 +21,15 @@ describe('UserInfoComponent', () => {
     await TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       declarations: [UserInfoComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        {
+          provide: PublicProfileService,
+          useValue: {
+            user: MOCK_USER,
+          },
+        },
+      ],
     }).compileComponents();
   });
 
@@ -22,6 +37,7 @@ describe('UserInfoComponent', () => {
     fixture = TestBed.createComponent(UserInfoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    publicProfileService = TestBed.inject(PublicProfileService);
   });
 
   it('should create', () => {
@@ -29,18 +45,49 @@ describe('UserInfoComponent', () => {
   });
 
   describe('when the user is not defined...', () => {
-    it('should NOT show the user info', () => {});
+    it('should NOT show the user info', () => {
+      component.user = null;
+
+      fixture.detectChanges();
+      const containerDiv = fixture.debugElement.query(By.css(containerClass));
+
+      expect(containerDiv).toBeFalsy();
+    });
   });
 
   describe('when the user is defined...', () => {
-    it('should show the user info', () => {});
+    it('should show the user info', () => {
+      const containerDiv = fixture.debugElement.query(By.css(containerClass));
+
+      expect(containerDiv).toBeTruthy();
+    });
 
     describe('when the user NOT have coordinates', () => {
-      it('should NOT show the map', () => {});
+      it('should NOT show the map', () => {
+        component.coordinates = null;
+
+        fixture.detectChanges();
+        const mapComponent = fixture.debugElement.query(By.css(mapTag));
+        const fakeMapComponent = fixture.debugElement.query(
+          By.css(fakeMapClass)
+        );
+
+        expect(mapComponent).toBeFalsy();
+        expect(fakeMapComponent).toBeTruthy();
+      });
     });
 
     describe('when the user have coordinates', () => {
-      it('should show the map', () => {});
+      it('should show the map', () => {
+        const mapComponent = fixture.debugElement.query(By.css(mapTag))
+          .nativeElement;
+        const fakeMapComponent = fixture.debugElement.query(
+          By.css(fakeMapClass)
+        );
+
+        expect(mapComponent).toBeTruthy();
+        expect(fakeMapComponent).toBeFalsy();
+      });
     });
   });
 });
