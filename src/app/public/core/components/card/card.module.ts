e90f59f7751e124a674c2021d6632c4088fff0c8
Laia Lopez <laialopezz99@gmail.com>
2020-12-23 17:29:47 +0100

    [WEB-157]: Corrected import

--- src/app/public/core/components/card/card.module.ts
+++ src/app/public/core/components/card/card.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CardComponent } from '@shared/card/card.component';
+import { CardComponent } from './card.component';
 
 @NgModule({
   declarations: [CardComponent],
