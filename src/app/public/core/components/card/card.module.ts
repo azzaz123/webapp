467f5558b54e8d451777dded2844df2180492770
Laia Lopez <laialopezz99@gmail.com>
2020-12-24 08:49:36 +0100

    [WEB-157]: CardModule Updated

--- src/app/public/core/components/card/card.module.ts
+++ src/app/public/core/components/card/card.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card.component';
+import { FavouriteIconModule } from '../favourite-icon/favourite-icon.module';
 
 @NgModule({
   declarations: [CardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FavouriteIconModule],
   exports: [CardComponent],
 })
 export class CardModule {}
