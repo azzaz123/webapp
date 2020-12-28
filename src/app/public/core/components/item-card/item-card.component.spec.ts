5e9f2173ba0f500c712cf19fac052cf5326a8dce
Irene Blanco <irene.blanco@wallapop.com>
2020-12-28 14:09:33 +0100

    [WEB-157]: ItemCard - Basic test passing

--- src/app/public/core/components/item-card/item-card.component.spec.ts
+++ src/app/public/core/components/item-card/item-card.component.spec.ts
@@ -1,6 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SvgIconModule } from '@core/svg-icon/svg-icon.module';
+import { ItemApiModule } from '@public/core/services/api/item/item-api.service';
+import { CustomCurrencyModule } from '@shared/pipes/custom-currency/custom-currency.module';
+import { SanitizedBackgroundModule } from '@shared/sanitized-background/sanitized-background.module';
+import { FavouriteIconModule } from '../favourite-icon/favourite-icon.module';
 
 import { ItemCardComponent } from './item-card.component';
+import { ItemCardService } from './services/item-card.service';
 
 describe('ItemCardComponent', () => {
   let component: ItemCardComponent;
@@ -9,6 +16,15 @@ describe('ItemCardComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ItemCardComponent],
+      imports: [
+        CommonModule,
+        FavouriteIconModule,
+        CustomCurrencyModule,
+        SvgIconModule,
+        SanitizedBackgroundModule,
+        ItemApiModule,
+      ],
+      providers: [ItemCardService],
     }).compileComponents();
   });
 
