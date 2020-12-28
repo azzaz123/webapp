3b9cd56315542fd157dbef860f3e316eff0813a0
Irene Blanco <irene.blanco@wallapop.com>
2020-12-28 16:23:17 +0100

    [WEB-157]: ItemCard - Tests

--- src/app/public/core/components/item-card/item-card.component.spec.ts
+++ src/app/public/core/components/item-card/item-card.component.spec.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SvgIconModule } from '@core/svg-icon/svg-icon.module';
+import { MOCK_ITEM } from '@fixtures/item.fixtures.spec';
 import { ItemApiModule } from '@public/core/services/api/item/item-api.service';
 import { CustomCurrencyModule } from '@shared/pipes/custom-currency/custom-currency.module';
 import { SanitizedBackgroundModule } from '@shared/sanitized-background/sanitized-background.module';
@@ -12,6 +14,8 @@ import { ItemCardService } from './services/item-card.service';
 describe('ItemCardComponent', () => {
   let component: ItemCardComponent;
   let fixture: ComponentFixture<ItemCardComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -31,10 +35,206 @@ describe('ItemCardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ItemCardComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
+    el = de.nativeElement;
+    component.item = MOCK_ITEM;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('favourite', () => {
+    const favouriteIconSelector = 'tsl-favourite-icon';
+    const favouriteIconAttr = 'ng-reflect-active';
+    let favouriteIconElement: HTMLElement;
+
+    describe('when is favourite', () => {
+      beforeEach(() => {
+        component.item.flags.favorite = true;
+        fixture.detectChanges();
+        favouriteIconElement = el.querySelector(favouriteIconSelector);
+      });
+
+      it('should show item as favourite', () => {
+        expect(
+          favouriteIconElement.getAttribute(favouriteIconAttr) === 'true'
+        ).toBeTruthy();
+      });
+
+      it('should change favourite state on favourite icon click', () => {
+        favouriteIconElement.click();
+        fixture.detectChanges();
+
+        expect(
+          favouriteIconElement.getAttribute(favouriteIconAttr) === 'false'
+        ).toBeTruthy();
+      });
+    });
+
+    describe('when is NOT favourite', () => {
+      beforeEach(() => {
+        component.item.flags.favorite = false;
+        fixture.detectChanges();
+        favouriteIconElement = el.querySelector(favouriteIconSelector);
+      });
+
+      it('should show item as NOT favourite', () => {
+        expect(
+          favouriteIconElement.getAttribute(favouriteIconAttr) === 'false'
+        ).toBeTruthy();
+      });
+
+      it('should change favourite state on favourite icon click', () => {
+        favouriteIconElement.click();
+        fixture.detectChanges();
+
+        expect(
+          favouriteIconElement.getAttribute(favouriteIconAttr) === 'true'
+        ).toBeTruthy();
+      });
+    });
+  });
+
+  describe('favourite', () => {
+    const favouriteIconSelector = 'tsl-favourite-icon';
+    const favouriteIconAttr = 'ng-reflect-active';
+    let favouriteIconElement: HTMLElement;
+
+    describe('when is favourite', () => {
+      beforeEach(() => {
+        component.item.flags.favorite = true;
+        fixture.detectChanges();
+        favouriteIconElement = el.querySelector(favouriteIconSelector);
+      });
+
+      it('should show item as favourite', () => {
+        expect(
+          favouriteIconElement.getAttribute(favouriteIconAttr) === 'true'
+        ).toBeTruthy();
+      });
+
+      it('should change favourite state on favourite icon click', () => {
+        favouriteIconElement.click();
+        fixture.detectChanges();
+
+        expect(
+          favouriteIconElement.getAttribute(favouriteIconAttr) === 'false'
+        ).toBeTruthy();
+      });
+    });
+
+    describe('when is NOT favourite', () => {
+      beforeEach(() => {
+        component.item.flags.favorite = false;
+        fixture.detectChanges();
+        favouriteIconElement = el.querySelector(favouriteIconSelector);
+      });
+
+      it('should show item as NOT favourite', () => {
+        expect(
+          favouriteIconElement.getAttribute(favouriteIconAttr) === 'false'
+        ).toBeTruthy();
+      });
+
+      it('should change favourite state on favourite icon click', () => {
+        favouriteIconElement.click();
+        fixture.detectChanges();
+
+        expect(
+          favouriteIconElement.getAttribute(favouriteIconAttr) === 'true'
+        ).toBeTruthy();
+      });
+    });
+  });
+
+  describe('bumped', () => {
+    const iconPartialSrc = 'bumped';
+
+    describe('when is bumped', () => {
+      beforeEach(() => {
+        component.item.flags.bumped = true;
+        fixture.detectChanges();
+      });
+
+      it('should show item as bumped', () => {
+        expect(el.querySelector(`[src*="${iconPartialSrc}"]`)).toBeTruthy();
+      });
+    });
+
+    describe('when is bumped and reserved', () => {
+      beforeEach(() => {
+        component.item.flags.bumped = true;
+        component.item.flags.reserved = true;
+        fixture.detectChanges();
+      });
+
+      it('should show item as bumped and reserved', () => {
+        const reservedPartialSrc = 'reserved';
+
+        expect(el.querySelector(`[src*="${iconPartialSrc}"]`)).toBeTruthy();
+        expect(el.querySelector(`[src*="${reservedPartialSrc}"]`)).toBeTruthy();
+      });
+    });
+
+    describe('when is NOT bumped', () => {
+      beforeEach(() => {
+        component.item.flags.bumped = false;
+        fixture.detectChanges();
+      });
+
+      it('should NOT show item as bumped', () => {
+        expect(el.querySelector(`[src*="${iconPartialSrc}"]`)).toBeFalsy();
+      });
+    });
+  });
+
+  describe('sold', () => {
+    const iconPartialSrc = 'sold';
+
+    describe('when is sold', () => {
+      beforeEach(() => {
+        component.item.flags.sold = true;
+        fixture.detectChanges();
+      });
+
+      it('should show item as sold', () => {
+        expect(el.querySelector(`[src*="${iconPartialSrc}"]`)).toBeTruthy();
+      });
+    });
+
+    describe('when is NOT sold', () => {
+      beforeEach(() => {
+        component.item.flags.sold = false;
+        fixture.detectChanges();
+      });
+
+      it('should NOT show item as sold', () => {
+        expect(el.querySelector(`[src*="${iconPartialSrc}"]`)).toBeFalsy();
+      });
+    });
+  });
+
+  describe('reserved', () => {
+    const iconPartialSrc = 'reserved';
+
+    describe('when is reserved', () => {
+      beforeEach(() => {
+        component.item.flags.reserved = true;
+        fixture.detectChanges();
+      });
+
+      it('should show item as reserved', () => {
+        expect(el.querySelector(`[src*="${iconPartialSrc}"]`)).toBeTruthy();
+      });
+    });
+
+    describe('when is NOT reserved', () => {
+      beforeEach(() => {
+        component.item.flags.reserved = false;
+        fixture.detectChanges();
+      });
+
+      it('should NOT show item as reserved', () => {
+        expect(el.querySelector(`[src*="${iconPartialSrc}"]`)).toBeFalsy();
+      });
+    });
   });
 });
