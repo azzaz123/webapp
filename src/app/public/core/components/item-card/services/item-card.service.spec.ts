86f02898c982f3ac1fc1f4c9d1f29ce6b5b754da
Irene Blanco <irene.blanco@wallapop.com>
2020-12-28 13:51:56 +0100

    [WEB-157]: Created ItemCardService

---
+++ src/app/public/core/components/item-card/services/item-card.service.spec.ts
@@ -0,0 +1,40 @@
+import { TestBed } from '@angular/core/testing';
+import { ItemApiService } from '@public/core/services/api/item/item-api.module';
+import { ItemApiModule } from '@public/core/services/api/item/item-api.service';
+import { ItemCardService } from './item-card.service';
+
+describe('ItemCardService', () => {
+  let itemApiService: ItemApiService;
+  let itemCardService: ItemCardService;
+  const ITEM_ID = '123';
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ItemCardService],
+      imports: [ItemApiModule],
+    });
+
+    itemApiService = TestBed.inject(ItemApiService);
+    itemCardService = TestBed.inject(ItemCardService);
+  });
+
+  describe('markAsFavourite', () => {
+    it('should call the api service to mark the selected item as favorit', () => {
+      spyOn(itemApiService, 'markAsFavourite');
+
+      itemCardService.markAsFavourite(ITEM_ID);
+
+      expect(itemApiService.markAsFavourite).toBeCalledWith(ITEM_ID);
+    });
+  });
+
+  describe('unmarkAsFavourite', () => {
+    it('should call the api service to unmark the selected item as favorite', () => {
+      spyOn(itemApiService, 'unmarkAsFavourite');
+
+      itemCardService.unmarkAsFavourite(ITEM_ID);
+
+      expect(itemApiService.unmarkAsFavourite).toBeCalledWith(ITEM_ID);
+    });
+  });
+});
