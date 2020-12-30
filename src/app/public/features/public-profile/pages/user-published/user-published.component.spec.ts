b0b8d65988bbdd24f59b6d4d6300a9e1a599a6da
Irene Blanco <irene.blanco@wallapop.com>
2020-12-30 17:08:27 +0100

    [WEB-159]: UserPublished - Test

--- src/app/public/features/public-profile/pages/user-published/user-published.component.spec.ts
+++ src/app/public/features/public-profile/pages/user-published/user-published.component.spec.ts
@@ -1,28 +1,46 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ItemCardListModule } from '@public/core/components/item-card-list/item-card-list.module';
 import { PublicProfileService } from '../../core/services/public-profile.service';
+import { MapItemService } from './services/map-item/map-item.service';
 
 import { UserPublishedComponent } from './user-published.component';
 
 describe('UserPublishedComponent', () => {
   let component: UserPublishedComponent;
+  let de: DebugElement;
+  let el: HTMLElement;
   let fixture: ComponentFixture<UserPublishedComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, ItemCardListModule],
       declarations: [UserPublishedComponent],
-      providers: [PublicProfileService],
+      providers: [PublicProfileService, MapItemService],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(UserPublishedComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
+    el = de.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('when component inits', () => {
+    const itemCardSelector = 'tsl-public-item-card';
+
+    it('should print same amount of items recieved', () => {
+      const componentItemsLength = component.items.length;
+      const domItemsLength = el.querySelectorAll(itemCardSelector).length;
+
+      expect(componentItemsLength).toEqual(domItemsLength);
+    });
   });
 });
