0ecd24a741beefe494f74938bdf26820d22af742
Irene Blanco <irene.blanco@wallapop.com>
2020-12-30 16:36:18 +0100

    [WEB-159]: UserPublished - Added MapItemService

--- src/app/public/features/public-profile/pages/user-published/user-published.module.ts
+++ src/app/public/features/public-profile/pages/user-published/user-published.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SpinnerModule } from '@shared/spinner/spinner.module';
+import { MapItemService } from './services/map-item/map-item.service';
 import { UserPublishedComponent } from './user-published.component';
 
 @NgModule({
   imports: [CommonModule, SpinnerModule],
   declarations: [UserPublishedComponent],
-  providers: [],
+  providers: [MapItemService],
   exports: [UserPublishedComponent],
 })
 export class UserPublishedModule {}
