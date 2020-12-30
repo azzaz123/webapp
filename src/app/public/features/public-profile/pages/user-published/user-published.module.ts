968e01bdb823543e1a428857c294beb8614b1bfb
Irene Blanco <irene.blanco@wallapop.com>
2020-12-30 16:36:32 +0100

    [WEB-159]: UserPublished - Added ItemCardListModule

--- src/app/public/features/public-profile/pages/user-published/user-published.module.ts
+++ src/app/public/features/public-profile/pages/user-published/user-published.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ItemCardListModule } from '@public/core/components/item-card-list/item-card-list.module';
 import { SpinnerModule } from '@shared/spinner/spinner.module';
 import { MapItemService } from './services/map-item/map-item.service';
 import { UserPublishedComponent } from './user-published.component';
 
 @NgModule({
-  imports: [CommonModule, SpinnerModule],
+  imports: [CommonModule, SpinnerModule, ItemCardListModule],
   declarations: [UserPublishedComponent],
   providers: [MapItemService],
   exports: [UserPublishedComponent],
