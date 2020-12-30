a18a3165a5411b12037aa9d10cbdb81f432fccae
Irene Blanco <irene.blanco@wallapop.com>
2020-12-30 13:16:10 +0100

    [WEB-159]: UserPublished - Better catch for errors

--- src/app/public/features/public-profile/pages/user-published/user-published.component.ts
+++ src/app/public/features/public-profile/pages/user-published/user-published.component.ts
@@ -27,24 +27,38 @@ export class UserPublishedComponent implements OnInit {
   private loadItems(): void {
     this.loading = true;
 
-    this.paginationService
-      .getItems(
-        this.publicProfileService.getPublishedItems(
-          this.publicProfileService.user.id,
-          this.nextPaginationItem
+    try {
+      this.paginationService
+        .getItems(
+          this.publicProfileService.getPublishedItems(
+            this.publicProfileService.user.id,
+            this.nextPaginationItem
+          )
         )
-      )
-      .pipe(
-        finalize(() => (this.loading = false)),
-        take(1)
-      )
-      .subscribe((response: PaginationResponse<Item>) => {
-        this.items = this.items.concat(response.results);
-        this.nextPaginationItem = response.init;
-      });
+        .pipe(
+          finalize(() => (this.loading = false)),
+          take(1)
+        )
+        .subscribe(
+          (response: PaginationResponse<Item>) => {
+            this.items = this.items.concat(response.results);
+            this.nextPaginationItem = response.init;
+          },
+          () => {
+            this.onError();
+          }
+        );
+    } catch (err: any) {
+      this.onError();
+    }
   }
 
   public loadMore(): void {
     this.loadItems();
+  }
+
+  private onError(): void {
+    this.items = [];
+    this.loading = false;
   }
 }
