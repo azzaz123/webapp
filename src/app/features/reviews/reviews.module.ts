4c4083e2efd26c1f28c17a139cf21c0e13e5e887
Irene Blanco <irene.blanco@wallapop.com>
2020-12-30 16:49:58 +0100

    [WEB-159]: ReviewsModule - Added missing ReviewItemModule

--- src/app/features/reviews/reviews.module.ts
+++ src/app/features/reviews/reviews.module.ts
@@ -8,6 +8,7 @@ import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { UserReviewService } from './core/user-review.service';
 import { SharedModule } from 'app/shared/shared.module';
 import { SanitizedBackgroundModule } from '@shared/sanitized-background/sanitized-background.module';
+import { ReviewItemModule } from './components/review-item/review-item.module';
 
 @NgModule({
   imports: [
