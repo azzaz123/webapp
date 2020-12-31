93c9023625549dc1a5edca67d3e5a7be8e594711
Irene Blanco <irene.blanco@wallapop.com>
2020-12-31 13:29:27 +0100

    [WEB-190]: ErrorBox - Controller

--- src/app/shared/error-box/error-box.component.ts
+++ src/app/shared/error-box/error-box.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ERROR_BOX_EXIT_TYPE } from './interfaces/error-box-exit-type';
+import { ErrorBoxExit } from './interfaces/error-box-exit.interface';
 @Component({
   selector: 'tsl-error-box',
   templateUrl: './error-box.component.html',
   styleUrls: ['./error-box.component.scss'],
 })
-export class ErrorBoxComponent {}
+export class ErrorBoxComponent {
+  @Input() img: string;
+  @Input() title: string;
+  @Input() subtitle: string;
+  @Input() exit: ErrorBoxExit;
+
+  @Output() exitClick: EventEmitter<void> = new EventEmitter<void>();
+
+  readonly ERROR_BOX_EXIT_TYPE = ERROR_BOX_EXIT_TYPE;
+
+  constructor() {}
+}
