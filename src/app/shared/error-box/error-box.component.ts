19bfa511cecc4bfe521b1e7df7bdd89e27c4edd4
Irene Blanco <irene.blanco@wallapop.com>
2020-12-31 10:48:47 +0100

    [WEB-190]: Created ErrorBox

---
+++ src/app/shared/error-box/error-box.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tsl-error-box',
+  templateUrl: './error-box.component.html',
+  styleUrls: ['./error-box.component.scss'],
+})
+export class ErrorBoxComponent {}
