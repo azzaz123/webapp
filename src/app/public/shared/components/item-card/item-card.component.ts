bd7a5769b12903f310c1c33bcc0fe90d21e3b9a9
Irene Blanco <irene.blanco@wallapop.com>
2021-01-04 17:36:51 +0100

    [WEB-157]: ItemCard - Removed unused code and changed detection strategy

--- src/app/public/shared/components/item-card/item-card.component.ts
+++ src/app/public/shared/components/item-card/item-card.component.ts
@@ -1,32 +1,25 @@
 import {
+  ChangeDetectionStrategy,
   Component,
   EventEmitter,
   Input,
-  OnDestroy,
   Output,
 } from '@angular/core';
 import { Item } from '@core/item/item';
 import { FAKE_ITEM_IMAGE_SMALL_LIGHT_BASE_PATH } from '@core/profile/profile';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'tsl-public-item-card',
   templateUrl: './item-card.component.html',
   styleUrls: ['./item-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ItemCardComponent implements OnDestroy {
+export class ItemCardComponent {
   @Input() item: Item;
   @Input() showDescription = true;
   @Output() toggleFavourite: EventEmitter<void> = new EventEmitter<void>();
 
   public readonly IMAGE_FALLBACK = FAKE_ITEM_IMAGE_SMALL_LIGHT_BASE_PATH;
-  private subscriptions: Subscription[] = [];
 
   constructor() {}
-
-  ngOnDestroy(): void {
-    this.subscriptions.forEach((subscription: Subscription) =>
-      subscription.unsubscribe()
-    );
-  }
 }
