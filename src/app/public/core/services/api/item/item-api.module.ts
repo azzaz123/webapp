0a056fe848e33ab342c89c2ef8a182749567a5b2
Irene Blanco <irene.blanco@wallapop.com>
2021-01-04 14:13:27 +0100

    [WEB-157]: Removed multiple HttpClientModule instances

--- src/app/public/core/services/api/item/item-api.module.ts
+++ src/app/public/core/services/api/item/item-api.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ItemApiService } from './item-api.service';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports: [HttpClientModule],
   providers: [ItemApiService],
 })
 export class ItemApiModule {}
