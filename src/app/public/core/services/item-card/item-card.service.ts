d3d61868149984ca2f7b3e6688951657e4c86f40
Irene Blanco <irene.blanco@wallapop.com>
2021-01-04 17:16:51 +0100

    [WEB-157]: ItemCardService - Relocated and changed logic

---
+++ src/app/public/core/services/item-card/item-card.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Item } from '@core/item/item';
+import { MarkAsFavouriteBodyResponse } from '@public/core/services/api/item/interfaces/item-response.interface';
+import { ItemApiService } from '@public/core/services/api/item/item-api.service';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+
+@Injectable()
+export class ItemCardService {
+  constructor(private itemApiService: ItemApiService) {}
+
+  public toggleFavourite(item: Item): void {
+    item.flags.favorite = !item.flags.favorite;
+
+    (item.flags.favorite
+      ? this.markAsFavourite(item.id)
+      : this.unmarkAsFavourite(item.id)
+    )
+      .pipe(take(1))
+      .subscribe(
+        () => {},
+        () => {
+          item.flags.favorite = item.flags.favorite;
+        }
+      );
+  }
+
+  private markAsFavourite(id: string): Observable<MarkAsFavouriteBodyResponse> {
+    return this.itemApiService.markAsFavourite(id);
+  }
+
+  private unmarkAsFavourite(
+    id: string
+  ): Observable<MarkAsFavouriteBodyResponse> {
+    return this.itemApiService.unmarkAsFavourite(id);
+  }
+}
