7450315683ae3f5799c641dec1bba61ad04d77b9
Alex Masnou <alex.masnou@wallapop.com>
2021-01-05 13:32:23 +0100

    [WEB-175]: Add topbar, tabbar and footer components to public layout

--- src/app/public/layout/public-layout.module.ts
+++ src/app/public/layout/public-layout.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
+import { FooterModule } from '@layout/footer/footer.module';
+import { TabbarModule } from '@layout/tabbar/tabbar.module';
+import { TopbarModule } from '@layout/topbar/topbar.module';
 
 @NgModule({
-  imports: [],
+  imports: [TopbarModule, TabbarModule, FooterModule],
+  exports: [TopbarModule, TabbarModule, FooterModule],
 })
 export class PublicLayoutModule {}
