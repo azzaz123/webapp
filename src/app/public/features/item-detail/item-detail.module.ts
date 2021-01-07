fe09516991279e6caf82ef2b12c594c285c8f305
Laia Lopez <laialopezz99@gmail.com>
2021-01-07 13:07:51 +0100

    [WEB-234]: Routing Module Added to ItemDetailModule

--- src/app/public/features/item-detail/item-detail.module.ts
+++ src/app/public/features/item-detail/item-detail.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ItemDetailService } from './core/services/item-detail.service';
-import { itemDetailRoutedComponents } from './item-detail-routing.module';
+import {
+  itemDetailRoutedComponents,
+  ItemDetailRoutingModule,
+} from './item-detail-routing.module';
 
 @NgModule({
   declarations: [itemDetailRoutedComponents],
-  imports: [CommonModule],
+  imports: [CommonModule, ItemDetailRoutingModule],
   providers: [ItemDetailService],
 })
 export class ItemDetailModule {}
