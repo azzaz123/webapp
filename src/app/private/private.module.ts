cbff59d66708695cf879d520115525a062a90e29
Alex Masnou <alex.masnou@wallapop.com>
2021-01-07 13:01:43 +0100

    [WEB-176]: Use alias imports in mobile blocker module

--- src/app/private/private.module.ts
+++ src/app/private/private.module.ts
@@ -1,9 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { PrivateComponent } from './private.component';
 import { PrivateCoreModule } from './core/private-core.module';
 import { PrivateLayoutModule } from './layout/private-layout.module';
 import { PrivateRoutingModule } from './private-routing.module';
-import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [
