6d2462e0e0cfc2e4571df17b371d1a241a1a488e
Laia Lopez <laialopezz99@gmail.com>
2021-01-07 17:28:04 +0100

    [WEB-235]: Device handle in ItemDetailComponent

--- src/app/public/features/item-detail/pages/item-detail.component.spec.ts
+++ src/app/public/features/item-detail/pages/item-detail.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DeviceDetectorServiceMock } from '@fixtures/remote-console.fixtures.spec';
+import { CookieService } from 'ngx-cookie';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 import { ItemDetailComponent } from './item-detail.component';
 
@@ -9,12 +12,20 @@ describe('ItemDetailComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ItemDetailComponent],
+      providers: [
+        { provide: DeviceDetectorService, useClass: DeviceDetectorServiceMock },
+        {
+          provide: CookieService,
+          useValue: {},
+        },
+      ],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ItemDetailComponent);
     component = fixture.componentInstance;
+
     fixture.detectChanges();
   });
 
--- src/app/public/features/item-detail/pages/item-detail.component.ts
+++ src/app/public/features/item-detail/pages/item-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DeviceService } from '@core/device/device.service';
+import { DeviceType } from '@core/device/deviceType.enum';
 
 @Component({
   selector: 'tsl-item-detail',
@@ -6,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-detail.component.scss'],
 })
 export class ItemDetailComponent implements OnInit {
-  constructor() {}
+  device: DeviceType;
 
-  ngOnInit(): void {}
+  constructor(private deviceService: DeviceService) {}
+
+  ngOnInit(): void {
+    this.device = this.deviceService.getDeviceType();
+  }
 }
