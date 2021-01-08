2d6afb43369438011056911c51658b43e7d7cda1
Irene Blanco <irene.blanco@wallapop.com>
2021-01-08 15:15:05 +0100

    [WEB-231]: ItemCardList - Created stories

---
+++ src/app/public/features/public-profile/pages/user-published/components/item-card-list/item-card-list.stories.ts
@@ -0,0 +1,93 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { AccessTokenService } from '@core/http/access-token.service';
+import { ItemApiService } from '@public/core/services/api/item/item-api.service';
+import { CheckSessionService } from '@public/core/services/check-session/check-session.service';
+import { ItemCardService } from '@public/core/services/item-card/item-card.service';
+import { MOCK_ITEM_1 } from '@public/shared/components/item-card/item-card.mock.stories';
+import { ItemCardModule } from '@public/shared/components/item-card/item-card.module';
+import { moduleMetadata } from '@storybook/angular';
+import { Story, Meta } from '@storybook/angular/types-6-0';
+import { CookieModule } from 'ngx-cookie';
+import { DeviceDetectorService } from 'ngx-device-detector';
+import { ItemCardListComponent } from './item-card-list.component';
+
+export default {
+  title: 'Webapp/ItemCardList',
+  component: ItemCardListComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [ItemCardListComponent],
+      imports: [
+        CommonModule,
+        ItemCardModule,
+        HttpClientModule,
+        CookieModule.forRoot(),
+      ],
+      providers: [
+        DeviceDetectorService,
+        ItemCardService,
+        CheckSessionService,
+        ItemApiService,
+        AccessTokenService,
+      ],
+    }),
+  ],
+} as Meta;
+
+const Template: Story<ItemCardListComponent> = (
+  args: ItemCardListComponent
+) => ({
+  component: ItemCardListComponent,
+  props: args,
+});
+
+const ITEMS = [
+  MOCK_ITEM_1,
+  MOCK_ITEM_1,
+  MOCK_ITEM_1,
+  MOCK_ITEM_1,
+  MOCK_ITEM_1,
+  MOCK_ITEM_1,
+  MOCK_ITEM_1,
+];
+
+export const Large = Template.bind({});
+Large.args = {
+  items: ITEMS,
+};
+Large.parameters = {
+  viewport: {
+    defaultViewport: 'LG',
+  },
+};
+
+export const Medium = Template.bind({});
+Medium.args = {
+  items: ITEMS,
+};
+Medium.parameters = {
+  viewport: {
+    defaultViewport: 'MD',
+  },
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  items: ITEMS,
+};
+Small.parameters = {
+  viewport: {
+    defaultViewport: 'SM',
+  },
+};
+
+export const ExtraSmall = Template.bind({});
+ExtraSmall.args = {
+  items: ITEMS,
+};
+ExtraSmall.parameters = {
+  viewport: {
+    defaultViewport: 'XS',
+  },
+};
