ed15f251e3c85713478dc9c6c276e7b6103ecc1d
Irene Blanco <irene.blanco@wallapop.com>
2021-01-12 18:15:14 +0100

    [storybook]: Storybook - Removed broken compodoc import

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,4 @@
-import { setCompodocJson } from '@storybook/addon-docs/angular';
-import docJson from '../documentation.json';
 import { CUSTOM_VIEWPORTS } from './viewports/custom-viewports';
-
-setCompodocJson(docJson);
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
