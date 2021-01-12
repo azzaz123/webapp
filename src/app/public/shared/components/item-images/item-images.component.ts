381ae3de8cdb6535f6aa13bc798831d401915da6
Laia Lopez <laialopezz99@gmail.com>
2021-01-12 18:24:10 +0100

    [WEB-250]: Translations + WIP Style

--- src/app/public/shared/components/item-images/item-images.component.ts
+++ src/app/public/shared/components/item-images/item-images.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Image } from '@core/user/user-response.interface';
 import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
 import { SWIPE_TYPE } from '@public/shared/constants/swipeType.enum';
 import { IMAGE, IMAGE_2 } from '@fixtures/user.fixtures.spec';
+import { ItemFlags } from '@core/item/item-response.interface';
 
 @Component({
   selector: 'tsl-item-images',
@@ -12,7 +13,19 @@ import { IMAGE, IMAGE_2 } from '@fixtures/user.fixtures.spec';
 export class ItemImagesComponent implements OnInit {
   @ViewChild(NgbCarousel) public carousel: NgbCarousel;
   public SWIPE_TYPE_ENUM = SWIPE_TYPE;
-  images: Image[] = [IMAGE, IMAGE_2, IMAGE, IMAGE_2];
+  @Input() images: Image[] = [IMAGE, IMAGE_2, IMAGE, IMAGE_2];
+  @Input() itemFlags: ItemFlags = {
+    pending: false,
+    sold: true,
+    favorite: false,
+    reserved: true,
+    removed: false,
+    banned: false,
+    expired: false,
+    review_done: false,
+    bumped: true,
+    highlighted: false,
+  };
 
   constructor() {}
   ngOnInit(): void {}
@@ -26,4 +39,6 @@ export class ItemImagesComponent implements OnInit {
       this.carousel.next();
     }
   }
+
+  openModalImageSlider(): void {}
 }
--- src/app/public/shared/components/item-images/item-images.module.ts
+++ src/app/public/shared/components/item-images/item-images.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ItemImagesComponent } from './item-images.component';
+import { SvgIconModule } from '@core/svg-icon/svg-icon.module';
 
 @NgModule({
   declarations: [ItemImagesComponent],
-  imports: [CommonModule, NgbModule],
+  imports: [CommonModule, NgbModule, SvgIconModule],
   exports: [ItemImagesComponent],
 })
 export class ItemImagesModule {}
