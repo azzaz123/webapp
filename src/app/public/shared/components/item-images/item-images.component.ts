498be74112495a0346f142de97686686af8d0e81
Laia Lopez <laialopezz99@gmail.com>
2021-01-13 13:39:56 +0100

    [WEB-250]: Flags deleted from controller + Pointer SCSS

--- src/app/public/shared/components/item-images/item-images.component.ts
+++ src/app/public/shared/components/item-images/item-images.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Image } from '@core/user/user-response.interface';
 import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
 import { SWIPE_TYPE } from '@public/shared/constants/swipeType.enum';
-import { ItemFlags } from '@core/item/item-response.interface';
 import { FAKE_ITEM_IMAGE_SMALL_LIGHT_BASE_PATH } from '@core/item/item';
 
 @Component({
@@ -15,7 +14,6 @@ export class ItemImagesComponent implements OnInit {
   public SWIPE_TYPE_ENUM = SWIPE_TYPE;
   @ViewChild(NgbCarousel) public carousel: NgbCarousel;
   @Input() images: Image[];
-  @Input() itemFlags: ItemFlags;
 
   constructor() {}
   ngOnInit(): void {}
