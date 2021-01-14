a3599678681418dcfd11892175e95b363b4d7c1c
Irene Blanco <irene.blanco@wallapop.com>
2021-01-14 17:47:01 +0100

    [WEB-268]: Created RecommenderApiService

---
+++ src/app/public/core/services/api/recommender/recommender-api.service.spec.ts
@@ -0,0 +1,43 @@
+import {
+  HttpClientTestingModule,
+  TestRequest,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import {
+  RecommenderApiService,
+  GET_RECOMENDATIONS_ENDPOINT,
+} from './recommender-api.service';
+
+describe('RecommenderApiService', () => {
+  let httpMock: HttpTestingController;
+  let recommenderApiService: RecommenderApiService;
+  const ITEM_ID = '123';
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [RecommenderApiService],
+      imports: [HttpClientTestingModule],
+    });
+
+    recommenderApiService = TestBed.inject(RecommenderApiService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  describe('getRecommendedItemsByItemId', () => {
+    it('should ask for the recommended items', () => {
+      const expectedUrl = GET_RECOMENDATIONS_ENDPOINT(ITEM_ID);
+
+      recommenderApiService.getRecommendedItemsByItemId(ITEM_ID).subscribe();
+      const req: TestRequest = httpMock.expectOne(expectedUrl);
+      req.flush({});
+
+      expect(req.request.url).toBe(expectedUrl);
+      expect(req.request.method).toBe('GET');
+    });
+  });
+});
---
+++ src/app/public/core/services/api/recommender/recommender-api.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
+import { Observable } from 'rxjs';
+import { RecommendedItemsBodyResponse } from './interfaces/recommender-response.interface';
+
+export const RECOMMENDER_API_URL = (userId: string) =>
+  `${environment.baseUrl}api/v3/recommender/${userId}`;
+export const GET_RECOMENDATIONS_ENDPOINT = (id: string) =>
+  `${RECOMMENDER_API_URL(id)}`;
+
+@Injectable()
+export class RecommenderApiService {
+  constructor(private http: HttpClient) {}
+
+  public getRecommendedItemsByItemId(
+    id: string
+  ): Observable<RecommendedItemsBodyResponse> {
+    return this.http.get(GET_RECOMENDATIONS_ENDPOINT(id)) as Observable<
+      RecommendedItemsBodyResponse
+    >;
+  }
+}
