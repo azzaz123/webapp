294e837a72a106f09a0f22789f3106eae9b47e39
Irene Blanco <irene.blanco@wallapop.com>
2021-01-14 17:39:51 +0100

    [WEB-268]: Created RecommenderItem interface

---
+++ src/app/public/core/services/api/recommender/interfaces/recommender-item.interface.ts
@@ -0,0 +1,12 @@
+export interface RecommenderItem {
+  category_id: number;
+  currency: string;
+  favorited: boolean;
+  id: string;
+  images: any[];
+  price: number;
+  seller_id: string;
+  shipping_allowed: boolean;
+  title: string;
+  web_slug: string;
+}
