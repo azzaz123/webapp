9a2da8f88e37dfdde4766c3541da0f0e17b342e9
Angel <ajarac92@gmail.com>
2021-01-18 13:56:59 +0100

    [WEB-237]: Fix app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,14 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { FooterModule } from '@shared/footer/footer.module';
 import { isSWEnabled } from 'environments/environment';
-import { CookieModule } from 'ngx-cookie';
-import { NgxPermissionsModule } from 'ngx-permissions';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
 import { CoreModule } from './core/core.module';
+import { HttpModule } from './core/http/http.module';
 import { DataModule } from './data/data.module';
 import { PROVIDERS } from './providers';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpModule } from './core/http/http.module';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { isSWEnabled } from 'environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
