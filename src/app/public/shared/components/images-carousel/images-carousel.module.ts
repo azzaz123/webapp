ded9cc6b546c872a0fd9e5c1cd5f8d8b21cac326
Laia Lopez <laialopezz99@gmail.com>
2021-01-18 13:24:46 +0100

    [WEB-283]: PublicSharedModule Flag + ImagesCarousel

--- src/app/public/shared/components/images-carousel/images-carousel.module.ts
+++ src/app/public/shared/components/images-carousel/images-carousel.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 import { ImagesCarouselComponent } from './images-carousel.component';
 import { ImageFallbackModule } from '@public/core/directives/image-fallback/image-fallback.module';
+import { PublicSharedModule } from '@public/shared/public-shared.module';
 
 @NgModule({
   declarations: [ImagesCarouselComponent],
-  imports: [CommonModule, NgbCarouselModule, ImageFallbackModule],
+  imports: [
+    CommonModule,
+    NgbCarouselModule,
+    ImageFallbackModule,
+    PublicSharedModule,
+  ],
   exports: [ImagesCarouselComponent],
 })
 export class ImagesCarouselModule {}
--- src/app/public/shared/public-shared.module.ts
+++ src/app/public/shared/public-shared.module.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ItemFlagModule } from './components/item-flag/item-flag.module';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ItemFlagModule],
+  exports: [ItemFlagModule],
 })
 export class PublicSharedModule {}
