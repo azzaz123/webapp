1571e249503d9562c0a3f7cc4554193ab52e9f9b
Laia Lopez <laialopezz99@gmail.com>
2021-01-18 20:02:35 +0100

    [WEB-283]: ItemFlag Testing

--- src/app/public/shared/components/item-flag/item-flag.component.spec.ts
+++ src/app/public/shared/components/item-flag/item-flag.component.spec.ts
@@ -1,14 +1,19 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SvgIconModule } from '@core/svg-icon/svg-icon.module';
+import { ITEM_FLAG_TYPES } from './item-flag-constants';
 
 import { ItemFlagComponent } from './item-flag.component';
 
 describe('ItemFlagComponent', () => {
   let component: ItemFlagComponent;
   let fixture: ComponentFixture<ItemFlagComponent>;
+  const svgTag = 'tsl-svg-icon';
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ItemFlagComponent],
+      imports: [SvgIconModule, HttpClientTestingModule],
     }).compileComponents();
   });
 
@@ -20,5 +25,32 @@ describe('ItemFlagComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('when we pass an item type...', () => {
+    beforeEach(() => {
+      component.type = ITEM_FLAG_TYPES.DEFAULT;
+
+      fixture.detectChanges();
+    });
+
+    it('should load the icon', () => {
+      const svgComponent: HTMLElement = fixture.nativeElement.querySelector(
+        `.ItemFlag--${component.type.toLocaleLowerCase()} > ${svgTag}`
+      );
+
+      expect(svgComponent).toBeTruthy();
+      expect(svgComponent.getAttribute('ng-reflect-src')).toContain(
+        component.type.toLocaleLowerCase()
+      );
+    });
+
+    it('should load the ng-content container', () => {
+      const ngContent: HTMLElement = fixture.nativeElement.querySelector(
+        `.ItemFlag > .ItemFlag__content`
+      );
+
+      expect(ngContent).toBeTruthy();
+    });
   });
 });
