3ddbde980752ae8c11ad8655d3b7954d4f48cc7f
Laia Lopez <laialopezz99@gmail.com>
2021-01-18 18:38:11 +0100

    [WEB-283]: Testing

--- src/app/public/shared/components/images-carousel/images-carousel.component.ts
+++ src/app/public/shared/components/images-carousel/images-carousel.component.ts
@@ -45,17 +45,19 @@ export class ImagesCarouselComponent {
   }
 
   get leftFlag(): FlagProperties {
-    const flagStatus = Object.keys(this.itemFlags).find(
-      (itemStatus: string) => {
-        if (
-          LEFT_FLAGS.some((flag) => flag.id === itemStatus) &&
-          this.itemFlags[itemStatus]
-        ) {
-          return itemStatus;
+    if (this.itemFlags) {
+      const flagStatus = Object.keys(this.itemFlags).find(
+        (itemStatus: string) => {
+          if (
+            LEFT_FLAGS.some((flag) => flag.id === itemStatus) &&
+            this.itemFlags[itemStatus]
+          ) {
+            return itemStatus;
+          }
         }
-      }
-    );
+      );
 
-    return LEFT_FLAGS.find((flag) => flag.id === flagStatus);
+      return LEFT_FLAGS.find((flag) => flag.id === flagStatus);
+    }
   }
 }
