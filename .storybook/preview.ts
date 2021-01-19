6f15b65949a493e0d1ae199d871b011288028706
Pablo Macias Moreira <pablo.macias@mati-dev.es>
2021-01-19 18:50:21 +0100

    [WEB-244]: Removed unnecessary imports in preview.ts

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,4 @@
 import { CUSTOM_VIEWPORTS } from './viewports/custom-viewports';
-import '../src/polyfills';
-import '../src/style/styles.scss';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
