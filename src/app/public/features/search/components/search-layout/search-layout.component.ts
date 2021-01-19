964f34524bfcb7795c9a190370831c2cb3bbc098
Pablo Macias Moreira <pablo.macias@mati-dev.es>
2021-01-19 17:38:23 +0100

    [WEB-244]: Added unsubscription for layout component

--- src/app/public/features/search/components/search-layout/search-layout.component.ts
+++ src/app/public/features/search/components/search-layout/search-layout.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ViewportService } from '@core/viewport/viewport.service';
 import { ViewportType } from '@core/viewport/viewport.enum';
+import { Subscription } from 'rxjs/internal/Subscription';
 
 // Naming should be abstracted if this component is reused
 @Component({
@@ -8,18 +9,23 @@ import { ViewportType } from '@core/viewport/viewport.enum';
   templateUrl: './search-layout.component.html',
   styleUrls: ['./search-layout.component.scss'],
 })
-export class SearchLayoutComponent implements OnInit {
+export class SearchLayoutComponent implements OnInit, OnDestroy {
   public shouldRenderRightColumn: boolean;
   public shouldRenderBottomRow: boolean;
+  private subscription: Subscription;
 
   constructor(private viewportService: ViewportService) {}
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.viewportService.$onViewportChange.subscribe((viewport) => {
       this.shouldRenderBottomRow =
         viewport === ViewportType.XS || viewport === ViewportType.SM;
       this.shouldRenderRightColumn =
         viewport === ViewportType.XL || viewport === ViewportType.XXL;
     });
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
