84a89cb71c0c752aa4bcb4e2f5aafaf39bba693a
Laia Lopez <laialopezz99@gmail.com>
2021-01-20 15:29:10 +0100

    [WEB-272]: ItemSpecification

--- src/app/public/core/constants/item-specifications-constants.ts
+++ src/app/public/core/constants/item-specifications-constants.ts
@@ -1,22 +1,15 @@
+import { CAR_ICON } from '@public/shared/components/counter-specification/constants/car-icons-constants';
+import { REAL_ESTATE_ICON } from '@public/shared/components/counter-specification/constants/realestate-icons-constants';
 import { CAR_SPECIFICATION_TYPE } from './item-specifications/cars-constants';
-import {
-  REAL_ESTATE_ICON,
-  REAL_STATE_TYPE,
-} from './item-specifications/realestate-constants';
+import { REAL_STATE_TYPE } from './item-specifications/realestate-constants';
 
-export interface ICON_SPECIFICATION {
-  label: string;
-  icon: REAL_ESTATE_ICON;
+export interface ITEM_SPECIFICATION {
   type: REAL_STATE_TYPE | CAR_SPECIFICATION_TYPE;
+  label?: LABEL;
+  icon?: REAL_ESTATE_ICON | CAR_ICON;
 }
 
-export interface NUMERIC_SPECIFICATION {
-  type: REAL_STATE_TYPE | CAR_SPECIFICATION_TYPE;
-  label?: string;
-  translations?: TRANSLATIONS_OPTIONS;
-}
-
-export interface TRANSLATIONS_OPTIONS {
+export interface LABEL {
   singular: string;
-  plural: string;
+  plural?: string;
 }
