01f23ca20c6bf45d6c04b44f01bc6851f11d681f
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-01-20 13:21:17 +0100

    [WEB-252]: Add RichAudience to sources

--- src/app/core/ads/ads.config.ts
+++ src/app/core/ads/ads.config.ts
@@ -1,8 +1,12 @@
 export const GOOGLE_PUBLISHER_TAG_URL =
   'https://securepubads.g.doubleclick.net/tag/js/gpt.js';
 
+export const RICH_AUDIENCE_URL =
+  'https://bridge.richmediastudio.com/ab083674fb8200b877a6983126e4477d/wallapop/pb_wrapper/dev/lib.js';
+
 export const ADS_SOURCES: string[] = [
   GOOGLE_PUBLISHER_TAG_URL,
+  RICH_AUDIENCE_URL,
   'https://static.criteo.net/js/ld/publishertag.js',
   '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
 ];
