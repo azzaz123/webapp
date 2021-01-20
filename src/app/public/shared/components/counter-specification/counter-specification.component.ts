50a861fc19a932c65b29d3e3ad9480abc1f8e148
Laia Lopez <laialopezz99@gmail.com>
2021-01-20 18:21:37 +0100

    [WEB-272]: [Component] Getting counter info

--- src/app/public/shared/components/counter-specification/counter-specification.component.ts
+++ src/app/public/shared/components/counter-specification/counter-specification.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ItemSpecification } from '@public/core/constants/item-specifications-constants';
+import { CAR_SPECIFICATION_TYPE } from '@public/core/constants/item-specifications/cars-constants';
+import { REAL_STATE_TYPE } from '@public/core/constants/item-specifications/realestate-constants';
+import { COUNTER_TYPE } from './constants/counter-type-constants';
 
 @Component({
   selector: 'tsl-counter-specification',
@@ -6,7 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./counter-specification.component.scss'],
 })
 export class CounterSpecificationComponent implements OnInit {
+  @Input() type: REAL_STATE_TYPE | CAR_SPECIFICATION_TYPE;
+  @Input() counter: string | number;
+  @Input() label: string;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  get counterInfo(): ItemSpecification {
+    if (this.label) {
+      COUNTER_TYPE.find((counter) => counter.type === this.type);
+    } else {
+      return null;
+    }
+  }
 }
