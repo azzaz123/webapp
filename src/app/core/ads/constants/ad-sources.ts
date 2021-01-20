ccfa1419f7a9e59e2357ac209cc845eac88a21a5
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-01-20 16:53:03 +0100

    [WEB-252]: Move Amazon Publisher Service to asset

--- src/app/core/ads/constants/ad-sources.ts
+++ src/app/core/ads/constants/ad-sources.ts
@@ -9,9 +9,13 @@ export const CRITEO_URL = 'https://static.criteo.net/js/ld/publishertag.js';
 export const GOOGLE_AD_SENSE_URL =
   'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
 
+export const AMAZON_PUBLISHER_SERVICE_URL =
+  './assets/js/amazon-publisher-service-wrapper.js';
+
 export const ADS_SOURCES: string[] = [
   GOOGLE_PUBLISHER_TAG_URL,
   RICH_AUDIENCE_URL,
   CRITEO_URL,
   GOOGLE_AD_SENSE_URL,
+  AMAZON_PUBLISHER_SERVICE_URL,
 ];
