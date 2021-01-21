5ab1193153b85d994d5428edac42314d12a895b9
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-01-21 14:18:25 +0100

    [WEB-252]: Initial ads fixtures

---
+++ src/tests/ads.fixtures.spec.ts
@@ -0,0 +1,35 @@
+import { Observable, Subscriber } from 'rxjs';
+
+export class MockAmazonPublisherService {
+  public isLibraryRefDefined() {
+    return true;
+  }
+
+  public init() {}
+
+  public requestBid() {
+    return new Observable((observer: Subscriber<void>) => {
+      observer.complete();
+    });
+  }
+}
+
+export class MockCriteoService {
+  public isLibraryRefDefined() {
+    return true;
+  }
+
+  public requestBid() {
+    return new Observable((observer: Subscriber<void>) => {
+      observer.complete();
+    });
+  }
+}
+
+export class MockGooglePublisherTagService {
+  public isLibraryRefDefined() {
+    return true;
+  }
+
+  public init() {}
+}
