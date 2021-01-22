edaa9323f142f2a902ccc82cecaa0e3bf4c43e16
Laia Lopez <laialopezz99@gmail.com>
2021-01-22 13:36:07 +0100

    [WEB-290]: Constants

---
+++ src/app/public/shared/constants/user-basic-info-constants.ts
@@ -0,0 +1,23 @@
+export enum SIZE {
+  SMALL = 'small',
+  BIG = 'big',
+}
+
+export interface StyleProperties {
+  size: SIZE;
+  imageSize: number;
+  fontSize: string;
+}
+
+export const STYLE_SIZES: StyleProperties[] = [
+  {
+    size: SIZE.SMALL,
+    imageSize: 40,
+    fontSize: '18px',
+  },
+  {
+    size: SIZE.BIG,
+    imageSize: 80,
+    fontSize: '26px',
+  },
+];
