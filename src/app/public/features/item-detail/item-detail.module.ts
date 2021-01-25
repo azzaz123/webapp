dda5815410310a41a6c9869e1a918cc3739e2d7e
Laia Lopez <laialopezz99@gmail.com>
2021-01-25 18:01:13 +0100

    [WEB-275]: Styled item stats

--- src/app/public/features/item-detail/item-detail.module.ts
+++ src/app/public/features/item-detail/item-detail.module.ts
@@ -8,6 +8,7 @@ import {
 import { AdsModule } from '@public/shared/components/ads/ads.module';
 import { ItemImagesCarouselModule } from '@public/shared/components/item-images-carousel/item-images-carousel.module';
 import { CustomCurrencyModule } from '@shared/pipes/custom-currency/custom-currency.module';
+import { SvgIconModule } from '@core/svg-icon/svg-icon.module';
 
 @NgModule({
   declarations: [itemDetailRoutedComponents],
@@ -17,6 +18,7 @@ import { CustomCurrencyModule } from '@shared/pipes/custom-currency/custom-curre
     AdsModule,
     ItemImagesCarouselModule,
     CustomCurrencyModule,
+    SvgIconModule,
   ],
   providers: [ItemDetailService],
 })
