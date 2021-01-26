5d1b2df1b2da4fdaaae23336d2caa64a4aab1fb7
Laia Lopez <laialopezz99@gmail.com>
2021-01-26 15:19:22 +0100

    [WEB-290]: Deleted unused ngOnInit

--- src/app/public/shared/components/user-basic-info/user-basic-info.component.ts
+++ src/app/public/shared/components/user-basic-info/user-basic-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { User } from '@core/user/user';
 import { UserStats } from '@core/user/user-stats.interface';
 import {
@@ -12,12 +12,10 @@ import {
   templateUrl: './user-basic-info.component.html',
   styleUrls: ['./user-basic-info.component.scss'],
 })
-export class UserBasicInfoComponent implements OnInit {
+export class UserBasicInfoComponent {
   @Input() userStats: UserStats;
   @Input() userInfo: User;
   @Input() styleSize: USER_INFO_SIZE = USER_INFO_SIZE.SMALL;
-
-  ngOnInit(): void {}
 
   get sizeProperties(): StyleProperties {
     return STYLE_SIZES.find((style) => style.size === this.styleSize);
