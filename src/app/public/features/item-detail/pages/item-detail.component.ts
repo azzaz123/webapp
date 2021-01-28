ee82964c2afe9041c54fd3f6a3a481b8a66f0fe8
Laia Lopez <laialopezz99@gmail.com>
2021-01-28 11:15:42 +0100

    [WEB-278]: Handle undefined + location

--- src/app/public/features/item-detail/pages/item-detail.component.ts
+++ src/app/public/features/item-detail/pages/item-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ItemDetailLocation } from './constants/item-detail.interface';
 import { DeviceService } from '@core/device/device.service';
 import { DeviceType } from '@core/device/deviceType.enum';
 import { Coordinate } from '@core/geolocation/address-response.interface';
@@ -12,6 +13,7 @@ import { finalize } from 'rxjs/operators';
 @Component({
   selector: 'tsl-item-detail',
   templateUrl: './item-detail.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./item-detail.component.scss'],
 })
 export class ItemDetailComponent implements OnInit {
@@ -22,7 +24,7 @@ export class ItemDetailComponent implements OnInit {
   public itemFlags: ItemFlags;
   public images: string[];
   public item: Item = MOCK_ITEM;
-  public coordinates: Coordinate;
+  public itemLocation: ItemDetailLocation;
 
   constructor(
     private deviceService: DeviceService,
@@ -31,15 +33,18 @@ export class ItemDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.device = this.deviceService.getDeviceType();
-    this.handleLocation();
+    this.handleCoordinates();
   }
 
-  private handleLocation(): void {
+  private handleCoordinates(): void {
     if (this.item.location) {
       this.setLocation({
+        zip: this.item.location.zip,
+        city: this.item.location.city,
         latitude: this.item.location.approximated_latitude,
         longitude: this.item.location.approximated_longitude,
       });
+
       this.approximatedLocation = this.item.location.approximated_location;
     } else {
       this.loading = true;
@@ -48,19 +53,41 @@ export class ItemDetailComponent implements OnInit {
         .pipe(finalize(() => (this.loading = false)))
         .subscribe((user: User) => {
           this.setLocation({
+            zip: user.location.zip,
+            city: user.location.city,
             latitude: user.location.approximated_latitude,
             longitude: user.location.approximated_longitude,
           });
+
           this.approximatedLocation = user.location.approximated_location;
         });
     }
   }
 
-  private setLocation(coordinates: Coordinate): void {
-    this.coordinates = coordinates;
+  private setLocation(itemLocation: ItemDetailLocation): void {
+    this.itemLocation = itemLocation;
   }
 
   set approximatedLocation(isApproximated: boolean) {
     this.isApproximateLocation = isApproximated;
+  }
+
+  get locationHaveCoordinates(): boolean {
+    return !!this.itemLocation?.latitude && !!this.itemLocation?.longitude;
+  }
+
+  get coordinates(): Coordinate {
+    return {
+      latitude: this.itemLocation.latitude,
+      longitude: this.itemLocation.longitude,
+    };
+  }
+
+  get locationSpecifications(): string {
+    if (!!this.itemLocation?.zip && !!this.itemLocation?.city) {
+      return `${this.itemLocation.zip}, ${this.itemLocation.city}`;
+    } else {
+      return $localize`:@@Undefined:Undefined`;
+    }
   }
 }
