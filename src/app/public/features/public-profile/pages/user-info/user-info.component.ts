4fd729734c5c5d62a99cf222685dae0c8fd8b1dc
Irene Blanco <irene.blanco@wallapop.com>
2021-02-01 09:06:07 +0100

    [WEB-303]: View location scroll - Now only managed from user-stats

--- src/app/public/features/public-profile/pages/user-info/user-info.component.ts
+++ src/app/public/features/public-profile/pages/user-info/user-info.component.ts
@@ -1,40 +1,24 @@
-import {
-  Component,
-  ElementRef,
-  OnDestroy,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Coordinate } from '@core/geolocation/address-response.interface';
-import { ScrollIntoViewService } from '@core/scroll-into-view/scroll-into-view';
 import { User } from '@core/user/user';
 import {
   UserExtrainfo,
   UserValidations,
 } from '@core/user/user-response.interface';
-import { DeviceDetectorService } from 'ngx-device-detector';
 import { Subscription } from 'rxjs';
 import { PublicProfileService } from '../../core/services/public-profile.service';
-import { MAP_REDIRECTION } from '../../public-profile-routing-constants';
 @Component({
   selector: 'tsl-user-info',
   templateUrl: './user-info.component.html',
   styleUrls: ['./user-info.component.scss'],
 })
 export class UserInfoComponent implements OnInit, OnDestroy {
-  @ViewChild('map') mapView: ElementRef;
   private subscriptions: Subscription[] = [];
   public coordinates: Coordinate;
   public user: User;
   public userValidations: UserValidations;
 
-  constructor(
-    private deviceService: DeviceDetectorService,
-    private publicProfileService: PublicProfileService,
-    private route: ActivatedRoute,
-    private scrollIntoViewService: ScrollIntoViewService
-  ) {}
+  constructor(private publicProfileService: PublicProfileService) {}
 
   ngOnInit(): void {
     this.getUser();
@@ -42,18 +26,6 @@ export class UserInfoComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.subscriptions.forEach((x) => x.unsubscribe());
-  }
-
-  ngAfterViewInit(): void {
-    if (this.user && this.deviceService.isMobile()) {
-      this.subscriptions.push(
-        this.route.fragment.subscribe((fragment) => {
-          if (fragment?.includes(MAP_REDIRECTION)) {
-            this.scrollIntoMap();
-          }
-        })
-      );
-    }
   }
 
   private getUser(): void {
@@ -78,9 +50,5 @@ export class UserInfoComponent implements OnInit, OnDestroy {
       this.user?.location?.approximated_latitude &&
       this.user?.location?.approximated_longitude
     );
-  }
-
-  private scrollIntoMap(): void {
-    this.scrollIntoViewService.scrollToElement(this.mapView.nativeElement);
   }
 }
