55aa7bd587e2b5f006e625f8c5d7d04bf222a729
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-02-04 08:34:48 +0100

    [WEB-287]: Fixed bug for the translation

--- src/app/features/chat/children/inbox/components/inbox-conversation/inbox-conversation.component.ts
+++ src/app/features/chat/children/inbox/components/inbox-conversation/inbox-conversation.component.ts
@@ -3,11 +3,7 @@ import { I18nService } from '@core/i18n/i18n.service';
 import { ThirdVoiceDropPriceComponent } from '@features/chat/children/message/components/third-voice-drop-price';
 import { ThirdVoiceReviewComponent } from '@features/chat/children/message/components/third-voice-review';
 import { InboxConversationService } from '@features/chat/core/inbox/inbox-conversation.service';
-import {
-  InboxConversation,
-  InboxMessage,
-  MessageType,
-} from '@features/chat/core/model';
+import { InboxConversation, InboxMessage, MessageType } from '@features/chat/core/model';
 import { includes } from 'lodash-es';
 import { CalendarSpec } from 'moment';
 
@@ -21,32 +17,21 @@ export class InboxConversationComponent {
   @Input() archiveConversation = false;
 
   public unreadCounterDisplayLimit = 99;
-  public momentConfig: CalendarSpec = {
-    lastDay: '[Yesterday]',
-    sameDay: 'HH:mm',
-    nextDay: 'ddd',
-    lastWeek: 'D MMM.',
-    nextWeek: 'ddd',
-    sameElse: 'D MMM.',
-  };
+  public momentConfig: CalendarSpec;
 
-  constructor(
-    private inboxConversationService: InboxConversationService,
-    private i18n: I18nService
-  ) {}
+  constructor(private inboxConversationService: InboxConversationService, private i18n: I18nService) {
+    this.momentConfig = i18n.getTranslations('defaultDaysMomentConfig');
+  }
 
   public dateIsThisYear(): boolean {
     return this.conversation && this.conversation.modifiedDate
-      ? this.conversation.modifiedDate.getFullYear() ===
-          new Date().getFullYear()
+      ? this.conversation.modifiedDate.getFullYear() === new Date().getFullYear()
       : false;
   }
 
   public onClickArchiveConversation(): void {
     this.inboxConversationService.sendReadSignal(this.conversation);
-    this.inboxConversationService
-      .archive$(this.conversation)
-      .subscribe(() => (this.conversation = null));
+    this.inboxConversationService.archive$(this.conversation).subscribe(() => (this.conversation = null));
   }
 
   public isText(inboxMessage: InboxMessage): boolean {
@@ -54,17 +39,11 @@ export class InboxConversationComponent {
   }
 
   public isThirdVoiceDropPrice(messageType: MessageType): boolean {
-    return includes(
-      ThirdVoiceDropPriceComponent.ALLOW_MESSAGES_TYPES,
-      messageType
-    );
+    return includes(ThirdVoiceDropPriceComponent.ALLOW_MESSAGES_TYPES, messageType);
   }
 
   public isThirdVoiceReview(messageType: MessageType): boolean {
-    return includes(
-      ThirdVoiceReviewComponent.ALLOW_MESSAGES_TYPES,
-      messageType
-    );
+    return includes(ThirdVoiceReviewComponent.ALLOW_MESSAGES_TYPES, messageType);
   }
 
   public getThirdVoiceTranslation(inboxMessage: InboxMessage): string {
