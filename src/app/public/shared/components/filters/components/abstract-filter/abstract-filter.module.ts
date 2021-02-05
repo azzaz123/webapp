8d96ed111d5b5f033f97b1aaea9e6362fd9deac9
Pablo Macias Moreira <pablo.macias@mati-dev.es>
2021-02-05 12:32:39 +0100

    [WEB-311]: Dropdown behaviour done

--- src/app/public/shared/components/filters/components/abstract-filter/abstract-filter.module.ts
+++ src/app/public/shared/components/filters/components/abstract-filter/abstract-filter.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { BubbleModule } from '@public/shared/components/bubble/bubble.module';
 import { FilterTemplateComponent } from '@public/shared/components/filters/components/abstract-filter/filter-template/filter-template.component';
 import { HttpModule } from '@core/http/http.module';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  imports: [CommonModule, BubbleModule, HttpModule],
+  imports: [CommonModule, BubbleModule, HttpModule, NgbDropdownModule],
   declarations: [FilterTemplateComponent],
   exports: [FilterTemplateComponent],
 })
--- src/app/public/shared/components/filters/components/abstract-filter/abstract-filter.stories.ts
+++ src/app/public/shared/components/filters/components/abstract-filter/abstract-filter.stories.ts
@@ -6,6 +6,7 @@ import { AbstractFilter } from '@public/shared/components/filters/components/abs
 import { FILTER_VARIANT } from '@public/shared/components/filters/components/abstract-filter/abstract-filter.enum';
 import { FilterTemplateComponent } from '@public/shared/components/filters/components/abstract-filter/filter-template/filter-template.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -21,7 +22,7 @@ import { HttpClientModule } from '@angular/common/http';
       (bubbleClick)="handleBubbleClick()"
     >
       <!--   Extended content   -->
-      <p *ngIf="storyContent">{{ storyContent }}</p>
+      <span class="px-3 py-0" *ngIf="storyContent">{{ storyContent }}</span>
       <!--   End extended content   -->
     </tsl-filter-template>
   `,
@@ -58,7 +59,6 @@ class StoryAbstractFilterComponent extends AbstractFilter {
 
   // BEFOREMERGE: Seems to do it twice?
   public handleBubbleClick(): void {
-    console.log('Hey');
     this.storyBubbleClick.emit();
   }
 }
@@ -71,7 +71,8 @@ export default {
   },
   decorators: [
     moduleMetadata({
-      imports: [BubbleModule, HttpClientModule],
+      providers: [],
+      imports: [BubbleModule, HttpClientModule, NgbDropdownModule],
       declarations: [StoryAbstractFilterComponent, FilterTemplateComponent],
     }),
   ],
