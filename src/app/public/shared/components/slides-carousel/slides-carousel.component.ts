cfe8b03e793b78c6c5eb09ba28e922a7e09edccf
Laia Lopez <laialopezz99@gmail.com>
2021-02-10 12:53:47 +0100

    [WEB-286]: initialIndex carousel + CurrentSlide handle

--- src/app/public/shared/components/slides-carousel/slides-carousel.component.ts
+++ src/app/public/shared/components/slides-carousel/slides-carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, ContentChildren, EventEmitter, Input, Output, QueryList, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ContentChildren, EventEmitter, Input, Output, QueryList, ViewChild } from '@angular/core';
 import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
 import { FAKE_ITEM_IMAGE_SMALL_LIGHT_BASE_PATH } from '@core/item/item';
 import { CarouselSliderDirective } from './directives/carousel-slider.directive';
@@ -7,29 +7,28 @@ import { SlideCarousel } from './slides-carousel.interface';
 @Component({
   selector: 'tsl-slides-carousel',
   templateUrl: './slides-carousel.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./slides-carousel.component.scss'],
 })
 export class SlidesCarouselComponent {
   @ContentChildren(CarouselSliderDirective) sections: QueryList<CarouselSliderDirective>;
   @ViewChild(NgbCarousel) public carousel: NgbCarousel;
   @Output() slideClick: EventEmitter<SlideCarousel> = new EventEmitter<SlideCarousel>();
-  @Input() slideIndex: number;
+  @Input() initialIndex: number = 0;
   @Input() className: string;
 
   public readonly IMAGE_FALLBACK = FAKE_ITEM_IMAGE_SMALL_LIGHT_BASE_PATH;
   public readonly NGB_SLIDE = 'ngb-slide-';
   public slides: CarouselSliderDirective[];
+  public activeId: string;
 
   ngAfterContentInit() {
     this.slides = this.sections.toArray();
+    this.activeId = this.NGB_SLIDE + this.initialIndex;
   }
 
   public emitCurrentIndex(slideIndex: number): void {
     this.slideClick.emit({ index: slideIndex });
-  }
-
-  get indexSlide(): number {
-    return parseInt(this.currentSlide.replace(this.NGB_SLIDE, ''));
   }
 
   get currentSlide(): string {
