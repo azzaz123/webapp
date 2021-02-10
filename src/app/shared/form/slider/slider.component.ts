68b3010dd5adb1e9c79ac7a1f7ccd4c019e3eb1d
Irene Blanco <irene.blanco@wallapop.com>
2021-02-10 15:00:25 +0100

    [WEB-348]: SliderComponent - ControlValueAccessor implemention

--- src/app/shared/form/slider/slider.component.ts
+++ src/app/shared/form/slider/slider.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'tsl-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SliderComponent),
+      multi: true,
+    },
+  ],
 })
-export class SliderComponent {}
+export class SliderComponent implements ControlValueAccessor {
+  value: any;
+  isDisabled: boolean;
+  onChange = (_: any) => {};
+  onTouch = () => {};
+
+  writeValue(obj: any): void {
+    this.value = obj;
+    console.log('HOLA1', obj);
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+  setDisabledState(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+}
