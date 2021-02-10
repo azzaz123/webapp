3534294e89372fdf940348595b21c096636c96df
Alex Masnou <alex.masnou@wallapop.com>
2021-02-10 09:49:30 +0100

    [WTF-86]: Remove unnecessary Braze events

--- src/app/features/catalog-pro/modals/pro-bump-confirmation-modal/pro-bump-confirmation-modal.component.ts
+++ src/app/features/catalog-pro/modals/pro-bump-confirmation-modal/pro-bump-confirmation-modal.component.ts
@@ -7,18 +7,9 @@ import { UserService } from '@core/user/user.service';
   templateUrl: './pro-bump-confirmation-modal.component.html',
   styleUrls: ['./pro-bump-confirmation-modal.component.scss'],
 })
-export class ProBumpConfirmationModalComponent implements OnInit {
+export class ProBumpConfirmationModalComponent {
   public code: string;
   public extras: string;
 
-  constructor(public activeModal: NgbActiveModal, private userService: UserService) {}
-
-  ngOnInit() {
-    this.userService.me().subscribe(() => {
-      if (this.code === '200' || this.code === '201') {
-        appboy.logCustomEvent('VisibilityPurchaseSuccess', { platform: 'web' });
-      } else {
-      }
-    });
-  }
+  constructor(public activeModal: NgbActiveModal) {}
 }
