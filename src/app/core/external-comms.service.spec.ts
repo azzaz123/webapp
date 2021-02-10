dfd512d460978f35bb6701b38342994769c8f0b7
Alex Masnou <alex.masnou@wallapop.com>
2021-02-10 13:52:04 +0100

    [WTF-86]: Add tests for ExternalCommsService

--- src/app/core/external-comms.service.spec.ts
+++ src/app/core/external-comms.service.spec.ts
@@ -1,5 +1,5 @@
-import { TestBed } from '@angular/core/testing';
-
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { environment } from '@environments/environment';
 import { ExternalCommsService } from './external-comms.service';
 
 describe('ExternalCommsService', () => {
@@ -10,7 +10,36 @@ describe('ExternalCommsService', () => {
     service = TestBed.inject(ExternalCommsService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('when initializing Braze communications', () => {
+    it('should initialize Barze configuration', fakeAsync(() => {
+      const MOCK_BRAZE_CONFIG = { enableHtmlInAppMessages: true };
+      spyOn(appboy, 'initialize');
+
+      service.initializeBrazeCommunications();
+      tick();
+
+      expect(appboy.initialize).toHaveBeenCalledWith(environment.appboy, MOCK_BRAZE_CONFIG);
+    }));
+
+    it('should enable displaying automatically Braze in-app messages', fakeAsync(() => {
+      spyOn(appboy.display, 'automaticallyShowNewInAppMessages');
+
+      service.initializeBrazeCommunications();
+      tick();
+
+      expect(appboy.display.automaticallyShowNewInAppMessages).toHaveBeenCalledTimes(1);
+    }));
+
+    it('should emit an event when the Braze library is ready to be used', fakeAsync(() => {
+      let brazeReadyEventSent = false;
+      service.brazeReady$.subscribe(() => {
+        brazeReadyEventSent = true;
+      });
+
+      service.initializeBrazeCommunications();
+      tick();
+
+      expect(brazeReadyEventSent).toBe(true);
+    }));
   });
 });
