df5b45f95027d7487512a964b47a5e750ec22e6d
Laia Lopez <laialopezz99@gmail.com>
2021-02-11 11:29:47 +0100

    [WEB-286]: [Testing] ItemFullSizeCarouselComponent

--- src/app/public/features/item-detail/components/item-fullsize-carousel/item-fullsize-carousel.component.spec.ts
+++ src/app/public/features/item-detail/components/item-fullsize-carousel/item-fullsize-carousel.component.spec.ts
@@ -1,38 +1,71 @@
+import { DecimalPipe } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { MockCookieService } from '@fixtures/cookies.fixtures.spec';
 import { ItemApiService } from '@public/core/services/api/item/item-api.service';
 import { CheckSessionService } from '@public/core/services/check-session/check-session.service';
 import { ItemCardService } from '@public/core/services/item-card/item-card.service';
+import { CustomCurrencyPipe } from '@shared/pipes';
 import { CookieService } from 'ngx-cookie';
 import { ItemFullSizeCarouselComponent } from './item-fullsize-carousel.component';
+import { MOCK_ITEM, MOCK_ITEM_GBP } from '@fixtures/item.fixtures.spec';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('ItemFullSizeCarouselComponent', () => {
-  const itemBackButtonClass = '.ItemHeader__backButton';
+  const parentContainer = '.ItemDetailImages';
+  const backButtonClass = '.ItemHeader__backButton';
+  const itemPriceClass = '.ItemHeader__price';
   const itemInfoClass = '.ItemInfo';
+  const favouriteButtonId = '#favouriteButton';
+  const currencies = {
+    EUR: '€',
+    GBP: '£',
+  };
 
   let component: ItemFullSizeCarouselComponent;
   let fixture: ComponentFixture<ItemFullSizeCarouselComponent>;
+  let itemCardService: ItemCardService;
+  let checkSessionService: CheckSessionService;
+  let decimalPipe: DecimalPipe;
+  let el: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ItemFullSizeCarouselComponent],
+      declarations: [ItemFullSizeCarouselComponent, CustomCurrencyPipe],
       imports: [HttpClientModule],
       providers: [
-        CheckSessionService,
+        {
+          provide: CheckSessionService,
+          useValue: {
+            hasSession() {
+              return true;
+            },
+            checkSessionAction() {},
+          },
+        },
         ItemCardService,
         ItemApiService,
         {
           provide: CookieService,
           useValue: MockCookieService,
         },
+        DecimalPipe,
       ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ItemFullSizeCarouselComponent);
     component = fixture.componentInstance;
+    component.item = MOCK_ITEM;
+    itemCardService = TestBed.inject(ItemCardService);
+    checkSessionService = TestBed.inject(CheckSessionService);
+    decimalPipe = TestBed.inject(DecimalPipe);
+    el = fixture.debugElement.nativeElement;
+    window.scrollTo = jest.fn();
+
     fixture.detectChanges();
   });
 
@@ -40,26 +73,82 @@ describe('ItemFullSizeCarouselComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('when we click on the back button...', () => {
-    it('should hidde the component', () => {});
-  });
-
-  describe('when we have an item...', () => {
-    it('should show the item title', () => {});
-
-    it('should show the item price with their currency', () => {});
-  });
-
-  describe('when we click on th favourite button...', () => {
-    describe('and we have a current session...', () => {
-      it('should toggle the favourite button', () => {});
+  describe('when the component is showing...', () => {
+    beforeEach(() => {
+      component.show();
+      fixture.detectChanges();
     });
-    describe('and we NOT have a current session...', () => {
-      it('should check the session action', () => {});
-    });
-  });
 
-  describe('when we clic on the chat button...', () => {
-    it('should redirect to the item chat page', () => {});
+    describe('when we click on the back button...', () => {
+      it('should hidde the component', () => {
+        spyOn(component, 'hidde').and.callThrough();
+        const backButton = fixture.debugElement.nativeElement.querySelector(backButtonClass);
+
+        backButton.click();
+        fixture.detectChanges();
+
+        expect(component.hidden).toBe(true);
+        expect(component.hidde).toHaveBeenCalled();
+        expect(fixture.debugElement.query(By.css(parentContainer))).toBeFalsy();
+      });
+    });
+
+    describe('when we clic on the chat button...', () => {
+      it('should redirect to the item chat page', () => {
+        const itemURL = `/chat?itemId=${component.item.id}`;
+        const chatButton = fixture.debugElement.nativeElement.querySelector('.ChatButton');
+
+        expect(chatButton.routerLink).toEqual([itemURL]);
+      });
+    });
+
+    describe('when we click on th favourite button...', () => {
+      describe('and we have a current session...', () => {
+        it('should toggle the favourite button', () => {
+          spyOn(itemCardService, 'toggleFavourite');
+          const favouriteButton = fixture.debugElement.nativeElement.querySelector(favouriteButtonId);
+
+          favouriteButton.click();
+
+          expect(itemCardService.toggleFavourite).toHaveBeenCalledWith(component.item);
+        });
+      });
+      describe('and we NOT have a current session...', () => {
+        it('should check the session action', () => {
+          spyOn(checkSessionService, 'hasSession').and.returnValue(false);
+          spyOn(checkSessionService, 'checkSessionAction');
+          const favouriteButton = fixture.debugElement.nativeElement.querySelector(favouriteButtonId);
+
+          favouriteButton.click();
+
+          expect(checkSessionService.checkSessionAction).toHaveBeenCalled();
+        });
+      });
+    });
+
+    describe('when we have an item...', () => {
+      it('should show the item title', () => {
+        const infoItemLabels = fixture.debugElement.query(By.css(itemInfoClass)).queryAll(By.css('span'));
+
+        expect(infoItemLabels.length).toBe(2);
+        expect(infoItemLabels[0].nativeElement.innerHTML).toBe(component.item.title);
+      });
+
+      describe('when the item currency code is in euros...', () => {
+        it('should show the price and the euros symbol', () => {
+          expect(el.querySelector(itemPriceClass).innerHTML).toEqual(`${decimalPipe.transform(component.item.salePrice)}${currencies.EUR}`);
+        });
+      });
+
+      describe('when the item currency code is in dollars...', () => {
+        beforeEach(() => {
+          component.item = MOCK_ITEM_GBP;
+          fixture.detectChanges();
+        });
+        it('should show the price and the dollar symbol', () => {
+          expect(el.querySelector(itemPriceClass).innerHTML).toEqual(`${currencies.GBP}${decimalPipe.transform(component.item.salePrice)}`);
+        });
+      });
+    });
   });
 });
