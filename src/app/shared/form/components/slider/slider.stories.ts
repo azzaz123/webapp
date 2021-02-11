13173c081f0516473bffdb680c6fa0f8ae4139e3
Irene Blanco <irene.blanco@wallapop.com>
2021-02-11 17:14:17 +0100

    [WEB-348]: SliderComponent - Stories

--- src/app/shared/form/components/slider/slider.stories.ts
+++ src/app/shared/form/components/slider/slider.stories.ts
@@ -1,32 +1,61 @@
 import { Story } from '@storybook/angular/types-6-0';
 import { moduleMetadata } from '@storybook/angular';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { SliderModule } from './slider.module';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'tsl-story-slider',
   template: `
-    <h4>NgModel</h4>
-    <tsl-slider [(ngModel)]="slider"></tsl-slider>
-    <h4 class="mt-4">FormGroup</h4>
+    <h4>NgModel: {{ slider }}</h4>
+    <tsl-slider
+      [(ngModel)]="slider"
+      [min]="min"
+      [max]="max"
+      [units]="units"
+      [stepsConfig]="stepsConfig"
+      [valueTooltip]="valueTooltip"
+      [limitTooltip]="limitTooltip"
+      [limitless]="limitless"
+    ></tsl-slider>
+    <h4 class="mt-4">FormGroup: {{ formGroup.value.slider[0] }} - {{ formGroup.value.slider[1] }}</h4>
     <form [formGroup]="formGroup">
-      <tsl-slider formControlName="slider"></tsl-slider>
+      <tsl-slider
+        formControlName="slider"
+        [min]="min"
+        [max]="max"
+        [units]="units"
+        [stepsConfig]="stepsConfig"
+        [valueTooltip]="valueTooltip"
+        [limitTooltip]="limitTooltip"
+        [limitless]="limitless"
+      ></tsl-slider>
     </form>
   `,
 })
 class StorySliderComponent {
   formGroup = new FormGroup({
-    slider: new FormControl('formControlValue'),
+    slider: new FormControl([20, 200]),
   });
 
-  slider: string = 'ngModelValue';
+  slider: number = 80;
+
+  @Input() min: number = 0;
+  @Input() max: number = 5000;
+  @Input() units: string;
+  @Input() stepsConfig: { range: number[]; step: number }[] = [];
+  @Input() valueTooltip: boolean = true;
+  @Input() limitTooltip: boolean = true;
+  @Input() limitless: boolean = false;
+  // ngOnInit(): void {
+  //   console.log(this.min);
+  //   this.formGroup.get('slider').disable();
+  // }
 }
 
 export default {
   title: 'Webapp/Shared/Form/Components/Slider',
   component: StorySliderComponent,
-  argTypes: {},
   decorators: [
     moduleMetadata({
       declarations: [StorySliderComponent],
@@ -38,8 +67,58 @@ export default {
 const Template: Story<StorySliderComponent> = (args) => ({
   props: args,
   template: `
-      <tsl-story-slider></tsl-story-slider>
+      <tsl-story-slider 
+        [min]="min" 
+        [max]="max" 
+        [stepsConfig]="stepsConfig" 
+        [units]="units"
+        [valueTooltip]="valueTooltip"
+        [limitTooltip]="limitTooltip"
+        [limitless]="limitless"></tsl-story-slider>
     `,
 });
 
 export const Default = Template.bind({});
+Default.args = {
+  min: 0,
+  max: 5000,
+  valueTooltip: true,
+  limitTooltip: true,
+  limitless: false,
+};
+
+export const StepsConfig = Template.bind({});
+StepsConfig.args = {
+  ...Default.args,
+  stepsConfig: [
+    { range: [0, 10], step: 1 },
+    { range: [10, 100], step: 10 },
+    { range: [100, 1000], step: 100 },
+    { range: [1000, 5000], step: 1000 },
+    { range: [5000, 10000], step: 5000 },
+  ],
+};
+
+export const Units = Template.bind({});
+Units.args = {
+  ...StepsConfig.args,
+  units: 'km',
+};
+
+export const WithoutValueTooltip = Template.bind({});
+WithoutValueTooltip.args = {
+  ...Default.args,
+  valueTooltip: false,
+};
+
+export const WithoutLimitTooltip = Template.bind({});
+WithoutLimitTooltip.args = {
+  ...Default.args,
+  limitTooltip: false,
+};
+
+export const Limitless = Template.bind({});
+Limitless.args = {
+  ...Default.args,
+  limitless: true,
+};
