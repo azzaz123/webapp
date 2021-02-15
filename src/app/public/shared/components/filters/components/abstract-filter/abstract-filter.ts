1852d455341fc93e6252efc945868b20b62cf995
Irene Blanco <irene.blanco@wallapop.com>
2021-02-15 13:14:36 +0100

    [WEB-312]: AbstractFilter - Added FilterConfig<T> generic to manage MapKey

--- src/app/public/shared/components/filters/components/abstract-filter/abstract-filter.ts
+++ src/app/public/shared/components/filters/components/abstract-filter/abstract-filter.ts
@@ -5,9 +5,9 @@ import { FilterParameter } from '@public/shared/components/filters/interfaces/fi
 import { FILTER_VARIANT } from '@public/shared/components/filters/components/abstract-filter/abstract-filter.enum';
 import { FilterTemplateComponent } from '@public/shared/components/filters/components/abstract-filter/filter-template/filter-template.component';
 
-export abstract class AbstractFilter implements Filter {
+export abstract class AbstractFilter<T> implements Filter {
   @Input() variant: FILTER_VARIANT = FILTER_VARIANT.BUBBLE;
-  @Input() config: FilterConfig = { id: '', title: '' };
+  @Input() config: FilterConfig<T> = { id: '', title: '', mapKey: null };
   @Input() value: FilterParameter[] = [];
   @Output() change = new EventEmitter<FilterParameter[]>();
   @Output() clear = new EventEmitter<void>();
