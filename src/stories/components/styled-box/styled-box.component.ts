862874374d933efedf1ac7ee3f83323bf732dd2b
Pablo Macias Moreira <pablo.macias@mati-dev.es>
2021-02-15 10:32:30 +0100

    [WEB-247]: Recovered changes on stories utils after merge conflict

--- src/stories/components/styled-box/styled-box.component.ts
+++ src/stories/components/styled-box/styled-box.component.ts
@@ -1,10 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, DoCheck, Input } from '@angular/core';
 import { NgStyle } from '@angular/common';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'stories-styled-box',
   templateUrl: './styled-box.component.html',
 })
-export class StyledBoxComponent {
+export class StyledBoxComponent implements DoCheck {
   @Input() style: NgStyle;
+  @Input() text?: string;
+
+  public ngDoCheck(): void {
+    if (this.text) {
+      console.log(this.text);
+    }
+  }
 }
