7dd1d1af2ba2551f2430da9549f6caa54a0fff8a
Laia Lopez <laialopezz99@gmail.com>
2021-02-16 16:14:10 +0100

    [WEB-346]: [Testing] MapSpecificationsService

--- src/app/public/features/item-detail/core/services/map-specifications/map-specifications.service.spec.ts
+++ src/app/public/features/item-detail/core/services/map-specifications/map-specifications.service.spec.ts
@@ -1,4 +1,10 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  MOCK_COUNTER_SPECIFICATIONS_CAR,
+  MOCK_COUNTER_SPECIFICATIONS_REAL_ESTATE,
+  MOCK_MAP_SPECIFICATIONS_CAR,
+  MOCK_MAP_SPECIFICATIONS_REAL_ESTATE,
+} from '@fixtures/map-specifications.fixtures.spec';
 
 import { MapSpecificationsService } from './map-specifications.service';
 
@@ -12,5 +18,19 @@ describe('MapSpecificationsService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('when we map the car specifications...', () => {
+    it('should return their counter specifications', () => {
+      expect(service.mapCarSpecifications(MOCK_MAP_SPECIFICATIONS_CAR)).toStrictEqual(MOCK_COUNTER_SPECIFICATIONS_CAR);
+    });
+  });
+
+  describe('when we map the realestate specifications...', () => {
+    it('should return their counter specifications', () => {
+      expect(service.mapRealestateSpecifications(MOCK_MAP_SPECIFICATIONS_REAL_ESTATE)).toStrictEqual(
+        MOCK_COUNTER_SPECIFICATIONS_REAL_ESTATE
+      );
+    });
   });
 });
