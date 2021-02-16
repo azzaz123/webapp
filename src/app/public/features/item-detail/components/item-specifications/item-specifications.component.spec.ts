2b1e1a1f5014b0e1a6f1df31befd28b02e8499c3
Laia Lopez <laialopezz99@gmail.com>
2021-02-16 15:47:32 +0100

    [WEB-346]: [Testing] ItemSpecificationsComponent

--- src/app/public/features/item-detail/components/item-specifications/item-specifications.component.spec.ts
+++ src/app/public/features/item-detail/components/item-specifications/item-specifications.component.spec.ts
@@ -1,4 +1,9 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { MOCK_ITEM_SPECIFICATIONS } from '@fixtures/item-specifications.fixtures.spec';
+import { CounterSpecificationComponent } from '@public/shared/components/counter-specification/counter-specification.component';
+import { CounterSpecificationModule } from '@public/shared/components/counter-specification/counter-specification.module';
 
 import { ItemSpecificationsComponent } from './item-specifications.component';
 
@@ -9,6 +14,7 @@ describe('ItemSpecificationsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ItemSpecificationsComponent],
+      imports: [CounterSpecificationModule, HttpClientTestingModule],
     }).compileComponents();
   });
 
@@ -20,5 +26,23 @@ describe('ItemSpecificationsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe(`when we don't have item specifications...`, () => {
+    it('should not render any counter specification', () => {
+      const counterSpecification = fixture.debugElement.query(By.directive(CounterSpecificationComponent));
+      expect(counterSpecification).toBeFalsy();
+    });
+  });
+
+  describe('when we have item specifications...', () => {
+    beforeEach(() => {
+      component.itemSpecifications = MOCK_ITEM_SPECIFICATIONS;
+      fixture.detectChanges();
+    });
+    it('should render as many counter specifications as item specifications we have', () => {
+      const counterSpecification = fixture.debugElement.queryAll(By.directive(CounterSpecificationComponent));
+      expect(counterSpecification.length).toBe(component.itemSpecifications.length);
+    });
   });
 });
