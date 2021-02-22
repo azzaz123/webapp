2df9ab96e3d2cdc85a7172f41e6ad736055aac22
Irene Blanco <irene.blanco@wallapop.com>
2021-02-22 18:05:16 +0100

    [WEB-312-b]: RangeFilterComponent - Updated stories

--- src/app/public/shared/components/filters/components/range-filter/range-filter.stories.ts
+++ src/app/public/shared/components/filters/components/range-filter/range-filter.stories.ts
@@ -1,12 +1,46 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MockCookieService } from '@fixtures/cookies.fixtures.spec';
 import { SliderFormModule } from '@shared/form/components/slider/slider-form.module';
 import { moduleMetadata } from '@storybook/angular';
 import { Story, Meta } from '@storybook/angular/types-6-0';
-import { min } from 'rxjs/operators';
+import { CookieService } from 'ngx-cookie';
+import { FilterParameter } from '../../interfaces/filter-parameter.interface';
+import { FILTER_VARIANT } from '../abstract-filter/abstract-filter.enum';
 import { AbstractFilterModule } from '../abstract-filter/abstract-filter.module';
+import { RangeFilterConfig } from './interfaces/range-filter-config.interface';
+import { RangeFilterParams } from './interfaces/range-filter-params.interface';
 import { RangeFilterComponent } from './range-filter.component';
+
+@Component({
+  selector: 'tsl-filters',
+  template: `
+    <div>
+      <h1>Bubble variant</h1>
+      <tsl-range-filter [variant]="${FILTER_VARIANT.BUBBLE}" [value]="value" [config]="config" (change)="changeBubble($event)">
+      </tsl-range-filter>
+      <h1>Content variant</h1>
+      <div style="border: 1px dashed black; background-color: white">
+        <tsl-range-filter [variant]="${FILTER_VARIANT.CONTENT}" [value]="value" [config]="config" (change)="changeContent($event)">
+        </tsl-range-filter>
+      </div>
+    </div>
+  `,
+})
+class FiltersComponent {
+  public value: RangeFilterParams;
+  public config: RangeFilterConfig;
+
+  changeBubble(value: RangeFilterParams): void {
+    this.value = value;
+  }
+
+  changeContent(value: RangeFilterParams): void {
+    this.value = value;
+  }
+}
 
 export default {
   title: 'Webapp/Public/Shared/Components/Filters/RangeFilterComponent',
@@ -14,13 +48,13 @@ export default {
     moduleMetadata({
       declarations: [RangeFilterComponent],
       imports: [CommonModule, AbstractFilterModule, SliderFormModule, ReactiveFormsModule, HttpClientModule],
+      providers: [{ provide: CookieService, useValue: MockCookieService }],
     }),
   ],
-  argTypes: { change: { action: 'change' } },
 } as Meta;
 
-const Template: Story<RangeFilterComponent> = (args: RangeFilterComponent) => ({
-  component: RangeFilterComponent,
+const Template: Story<FiltersComponent> = (args: FiltersComponent) => ({
+  component: FiltersComponent,
   props: args,
 });
 
@@ -33,10 +67,10 @@ Default.args = {
     },
     title: 'How much do you want to pay?',
     icon: '/assets/icons/joke.svg',
+    range: [10, 200],
+    bubblePlaceholder: 'Price',
+    units: '€',
   },
-  range: [10, 200],
-  placeholder: 'Price',
-  units: '€',
 };
 
 export const Limitless = Template.bind({});
@@ -47,32 +81,38 @@ Limitless.args = {
 
 export const Steps = Template.bind({});
 Steps.args = {
-  ...Default.args,
-  stepsConfig: [
-    { range: [0, 10], step: 1 },
-    { range: [10, 100], step: 10 },
-    { range: [100, 1000], step: 100 },
-    { range: [1000, 5000], step: 1000 },
-    { range: [5000, 10000], step: 5000 },
-  ],
+  config: {
+    ...Default.args.config,
+    stepsConfig: [
+      { range: [0, 10], step: 1 },
+      { range: [10, 100], step: 10 },
+      { range: [100, 1000], step: 100 },
+      { range: [1000, 5000], step: 1000 },
+      { range: [5000, 10000], step: 5000 },
+    ],
+  },
 };
 
 export const StepsAndLimitless = Template.bind({});
 StepsAndLimitless.args = {
-  ...Default.args,
-  limitless: true,
-  stepsConfig: [
-    { range: [0, 10], step: 1 },
-    { range: [10, 100], step: 10 },
-    { range: [100, 1000], step: 100 },
-    { range: [1000, 5000], step: 1000 },
-    { range: [5000, 10000], step: 5000 },
-  ],
+  config: {
+    ...Default.args.config,
+    limitless: true,
+    stepsConfig: [
+      { range: [0, 10], step: 1 },
+      { range: [10, 100], step: 10 },
+      { range: [100, 1000], step: 100 },
+      { range: [1000, 5000], step: 1000 },
+      { range: [5000, 10000], step: 5000 },
+    ],
+  },
 };
 
 export const WithValue = Template.bind({});
 WithValue.args = {
-  ...Steps.args,
+  config: {
+    ...StepsAndLimitless.args.config,
+  },
   value: [
     {
       key: 'min',
