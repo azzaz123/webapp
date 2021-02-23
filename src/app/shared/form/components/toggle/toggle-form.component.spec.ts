c1ffc0f41922330cad09e7e1c6c09084a91b26a8
Irene Blanco <irene.blanco@wallapop.com>
2021-02-23 18:33:13 +0100

    [WEB-314]: ToggleFormComponent - Tests

--- src/app/shared/form/components/toggle/toggle-form.component.spec.ts
+++ src/app/shared/form/components/toggle/toggle-form.component.spec.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { ToggleFormComponent } from './toggle-form.component';
 
 describe('ToggleFormComponent', () => {
@@ -11,7 +12,7 @@ describe('ToggleFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CommonModule],
+      imports: [CommonModule, FormsModule],
       declarations: [ToggleFormComponent],
     }).compileComponents();
 
@@ -24,5 +25,45 @@ describe('ToggleFormComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('when user clicks', () => {
+    const toggleClick = () => {
+      el.querySelector('input').click();
+    };
+
+    describe('and had no value', () => {
+      it('should has true as value', () => {
+        toggleClick();
+
+        expect(component.value).toBeTruthy();
+      });
+    });
+
+    describe('and value was true', () => {
+      beforeEach(() => {
+        component.value = true;
+        fixture.detectChanges();
+      });
+
+      it('should has false as value', () => {
+        toggleClick();
+
+        expect(component.value).toBeFalsy();
+      });
+    });
+
+    describe('and value was false', () => {
+      beforeEach(() => {
+        component.value = false;
+        fixture.detectChanges();
+      });
+
+      it('should has true as value', () => {
+        toggleClick();
+
+        expect(component.value).toBeTruthy();
+      });
+    });
   });
 });
