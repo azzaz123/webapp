d9c6f44c114f37d39a66292e8cf7f39715c2d6a3
Irene Blanco <irene.blanco@wallapop.com>
2021-02-23 18:25:43 +0100

    [WEB-314]: ToggleFormComponent - Template and styles

--- src/app/shared/form/components/toggle/toggle-form.module.ts
+++ src/app/shared/form/components/toggle/toggle-form.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { ToggleFormComponent } from './toggle-form.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   exports: [ToggleFormComponent],
   declarations: [ToggleFormComponent],
 })
