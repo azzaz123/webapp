8f246f7f8233b1ccf7377c30c188e7babe840025
Angel <ajarac92@gmail.com>
2021-02-24 15:51:06 +0100

    [WEB-342]: Set Item Detial AD

---
+++ src/app/shared/ad/ad.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { AdComponent } from './ad.component';
+
+@NgModule({
+  declarations: [AdComponent],
+  exports: [AdModule],
+})
+export class AdModule {}
