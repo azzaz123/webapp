de2e22bad72483483ff7bd2ebbf689d7fd6572df
Angel <ajarac92@gmail.com>
2021-02-24 15:58:13 +0100

    [WEB-341]: Create an Ad Module

--- src/app/public/features/public-profile/public-profile.module.ts
+++ src/app/public/features/public-profile/public-profile.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PublicProfileRoutingModule, publicProfileRoutedComponents, publicProfileRoutedModules } from './public-profile-routing.module';
-import { ProfileTabsComponent } from './components/profile-tabs/profile-tabs.component';
-import { FavouriteUserModule } from './components/favourite-user/favourite-user.module';
-import { PublicProfileService } from './core/services/public-profile.service';
-import { SpinnerModule } from '@shared/spinner/spinner.module';
-import { UserProfileHeaderModule } from './components/user-profile-header/user-profile-header.module';
+import { NgModule } from '@angular/core';
 import { PublicPipesModule } from '@public/core/pipes/public-pipes.module';
 import { PublicUserApiModule } from '@public/core/services/api/public-user/public-user-api.module';
-import { SharedModule } from '@shared/shared.module';
+import { AdModule } from '@shared/ad/ad.module';
+import { SpinnerModule } from '@shared/spinner/spinner.module';
+import { FavouriteUserModule } from './components/favourite-user/favourite-user.module';
+import { ProfileTabsComponent } from './components/profile-tabs/profile-tabs.component';
+import { UserProfileHeaderModule } from './components/user-profile-header/user-profile-header.module';
+import { PublicProfileService } from './core/services/public-profile.service';
+import { publicProfileRoutedComponents, publicProfileRoutedModules, PublicProfileRoutingModule } from './public-profile-routing.module';
 
 @NgModule({
   declarations: [publicProfileRoutedComponents, ProfileTabsComponent],
   imports: [
     CommonModule,
-    SharedModule,
+    AdModule,
     publicProfileRoutedModules,
     PublicProfileRoutingModule,
     SpinnerModule,
