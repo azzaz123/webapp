3128d095eac3acd75af34e369bf2c83050e1777f
Laia Lopez <laialopezz99@gmail.com>
2021-02-24 18:22:45 +0100

    [WEB-321]: [WIP] Transform date + Translations

--- src/app/public/features/item-detail/core/directives/ellapsed-time.pipe.ts
+++ src/app/public/features/item-detail/core/directives/ellapsed-time.pipe.ts
@@ -4,7 +4,44 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'ellapsedTime',
 })
 export class EllapsedTimePipe implements PipeTransform {
-  constructor() {}
+  transform(itemDate: number) {
+    const currentTimestamp = Date.now();
+    const ellapsedTimestamp = (currentTimestamp - itemDate) / 1000;
 
-  transform(date: number) {}
+    if (ellapsedTimestamp > 0) {
+      const minutes = ellapsedTimestamp / 60;
+      if (minutes < 60) {
+        return $localize`:@@Edited ${minutes} minutes ago:EditedMinutesAgo${minutes}`; // Menos de 24h
+      } else {
+        const hours = minutes / 60;
+        if (hours < 24) {
+          return $localize`:@@Edited ${hours} hours ago:EditedHoursAgo${minutes}`; // Menos de 24h
+        } else {
+          const days = hours / 24;
+          if (days < 2) {
+            // De 1 a 2 días
+            return $localize`:@@Edited 1 day ago:EditedOneDayAgo`;
+          } else if (days < 3) {
+            // De 2 a 3 días
+            return $localize`:@@Edited 2 days ago:EditedTwoDaysAgo`;
+          } else if (days < 4) {
+            // De 3 a 4 días
+            return $localize`:@@Edited 3 days ago:EditedThreeDaysAgo`;
+          } else if (days < 7) {
+            // De 4 a 7 días
+            return $localize`:@@Edited 1 week ago:EditedOneWeekAgo`;
+          } else if (days < 14) {
+            // De 7 a 14 días
+            return $localize`:@@Edited less than 2 weeks ago:EditedMoreTwoWeeks`;
+          } else if (days < 30) {
+            // De 14 a 30 días
+            return $localize`:@@Edited less than 1 month:EditedLessMonth`;
+          } else {
+            // Más de 30 días
+            return $localize`:@@Edited more than a month:EditedMoreMonth`;
+          }
+        }
+      }
+    } else return $localize`:@@Edited more than a month:EditedMoreMonth`;
+  }
 }
