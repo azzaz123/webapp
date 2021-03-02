39b88a11e056399e67bbbc88d49a7b27c5836336
Irene Blanco <irene.blanco@wallapop.com>
2021-03-02 10:12:55 +0100

    [WEB-359]: Created FilterFactoryService

---
+++ src/app/public/shared/components/filters/components/filter-group/services/filter-factory.service.ts
@@ -0,0 +1,30 @@
+import { ComponentFactory, ComponentFactoryResolver, Injectable } from '@angular/core';
+import { FilterConfig } from '../../../interfaces/filter-config.interface';
+import { AbstractFilter } from '../../abstract-filter/abstract-filter';
+import { FILTER_VARIANT } from '../../abstract-filter/abstract-filter.enum';
+import { RangeFilterComponent } from '../../range-filter/range-filter.component';
+import { ToggleFilterComponent } from '../../toggle-filter/toggle-filter.component';
+import { FilterHostDirective } from '../directives/filter-host.directive';
+
+export const FILTERS = {
+  // TODO relocate and type
+  range: RangeFilterComponent,
+  toggle: ToggleFilterComponent,
+};
+
+@Injectable()
+export class FilterFactoryService {
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) {}
+  createFilter(config: FilterConfig<unknown>, variant: FILTER_VARIANT, container: FilterHostDirective) {
+    const component = FILTERS[config.id];
+    const componentFactory: ComponentFactory<AbstractFilter<unknown>> = this.componentFactoryResolver.resolveComponentFactory(component);
+
+    const viewContainerRef = container.viewContainerRef;
+    viewContainerRef.clear();
+
+    const componentRef = viewContainerRef.createComponent(componentFactory);
+
+    componentRef.instance.config = config;
+    componentRef.instance.variant = variant;
+  }
+}
