c1900f8f91cca4dd98e95398f96ddfe06cb95d7e
Irene Blanco <irene.blanco@wallapop.com>
2021-03-03 16:19:09 +0100

    [WEB-359]: FilterFactoryService - Test

--- src/app/public/shared/components/filters/components/filter-group/constants/filter-type-component.constant.ts
+++ src/app/public/shared/components/filters/components/filter-group/constants/filter-type-component.constant.ts
@@ -1,8 +1,10 @@
 import { FILTER_TYPES } from '../../../core/enums/filter-types/filter-types.enum';
+import { AbstractFilter } from '../../abstract-filter/abstract-filter';
 import { RangeFilterComponent } from '../../range-filter/range-filter.component';
 import { ToggleFilterComponent } from '../../toggle-filter/toggle-filter.component';
 
-export const FILTER_TYPE_COMPONENT: Record<FILTER_TYPES, unknown> = {
+export const FILTER_TYPE_COMPONENT: Record<FILTER_TYPES, any> = {
+  // TODO AbstractFilter<unknown> instead of any is not working, why
   [FILTER_TYPES.RANGE]: RangeFilterComponent,
   [FILTER_TYPES.TOGGLE]: ToggleFilterComponent,
 };
---
+++ src/app/public/shared/components/filters/components/filter-group/services/filter-factory.service.spec.ts
@@ -0,0 +1,75 @@
+import { Component, DebugElement, ViewChild } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FILTER_TYPES } from '../../../core/enums/filter-types/filter-types.enum';
+import { FilterConfig } from '../../../interfaces/filter-config.interface';
+import { FilterParameter } from '../../../interfaces/filter-parameter.interface';
+import { FILTER_VARIANT } from '../../abstract-filter/abstract-filter.enum';
+import { FILTER_TYPE_COMPONENT } from '../constants/filter-type-component.constant';
+import { FilterHostDirective } from '../directives/filter-host.directive';
+import { FilterFactoryService } from './filter-factory.service';
+
+@Component({
+  template: `<div tslFilterHost></div>`,
+})
+class TestComponent {
+  @ViewChild(FilterHostDirective) host: FilterHostDirective;
+}
+
+describe('FilterFactoryService', () => {
+  let service: FilterFactoryService;
+  let component: TestComponent;
+  let de: DebugElement;
+  let el: HTMLElement;
+  let fixture: ComponentFixture<TestComponent>;
+
+  const toggleFilterSelector = 'tsl-toggle-filter';
+
+  const config: FilterConfig<any> = {
+    id: 'id',
+    type: FILTER_TYPES.TOGGLE,
+    mapKey: {
+      key: 'key',
+    },
+    title: 'How much do you want to pay?',
+    icon: '/assets/icons/joke.svg',
+    bubblePlaceholder: 'Price',
+  };
+
+  const value: FilterParameter[] = [{ key: 'key', value: 'true' }];
+
+  const variant = FILTER_VARIANT.BUBBLE;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TestComponent, FilterHostDirective],
+      imports: [],
+      providers: [FilterFactoryService],
+    }).compileComponents();
+
+    service = TestBed.inject(FilterFactoryService);
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    de = fixture.debugElement;
+    el = de.nativeElement;
+
+    fixture.detectChanges();
+  });
+
+  it('should create test component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('when creating filter', () => {
+    it('should insert it on the template', () => {
+      service.createFilter(config, value, variant, component.host);
+
+      expect(el.querySelector(toggleFilterSelector)).toBeTruthy();
+    });
+
+    it('should add it correctly to the form group', () => {
+      service.createFilter(config, value, variant, component.host);
+
+      expect(service.getFilterGroup()['filters'][0]).toBeInstanceOf(FILTER_TYPE_COMPONENT[config.type]);
+    });
+  });
+});
