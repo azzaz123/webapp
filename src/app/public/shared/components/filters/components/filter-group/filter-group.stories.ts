59931b4b9a4d4626f9e70d1d28e84b1f401dcc87
Irene Blanco <irene.blanco@wallapop.com>
2021-03-03 12:33:10 +0100

    [WEB-359]: FilterGroupComponent - Storybook - Added more story cases

--- src/app/public/shared/components/filters/components/filter-group/filter-group.stories.ts
+++ src/app/public/shared/components/filters/components/filter-group/filter-group.stories.ts
@@ -6,6 +6,7 @@ import { moduleMetadata } from '@storybook/angular';
 import { Story, Meta } from '@storybook/angular/types-6-0';
 import { CookieService } from 'ngx-cookie';
 import { FiltersModule } from '../../filters.module';
+import { FilterConfig } from '../../interfaces/filter-config.interface';
 import { FILTER_VARIANT } from '../abstract-filter/abstract-filter.enum';
 import { FilterHostDirective } from './directives/filter-host.directive';
 import { FilterGroupComponent } from './filter-group.component';
@@ -15,10 +16,10 @@ import { FilterFactoryService } from './services/filter-factory.service';
   selector: 'tsl-filters',
   template: `
     <h1>Bubble</h1>
-    <tsl-filter-group [variant]="${FILTER_VARIANT.BUBBLE}" [config]="config"> </tsl-filter-group>
+    <tsl-filter-group [variant]="${FILTER_VARIANT.BUBBLE}" [config]="config" [initialValues]="initialValues || []"> </tsl-filter-group>
     <h1>Content</h1>
     <div style="border: 1px dashed black; background-color: white">
-      <tsl-filter-group [variant]="${FILTER_VARIANT.CONTENT}" [config]="config"> </tsl-filter-group>
+      <tsl-filter-group [variant]="${FILTER_VARIANT.CONTENT}" [config]="config" [initialValues]="initialValues || []"> </tsl-filter-group>
     </div>
   `,
 })
@@ -40,7 +41,7 @@ const Template: Story<FiltersComponent> = (args: FiltersComponent) => ({
   props: args,
 });
 
-const CONFIG = [
+const CONFIG: FilterConfig<unknown>[] = [
   {
     id: 'range',
     mapKey: {
@@ -49,7 +50,7 @@ const CONFIG = [
     },
     title: 'How much do you want to pay?',
     icon: '/assets/icons/joke.svg',
-    range: [10, 200],
+    range: [10, 10000],
     bubblePlaceholder: 'Price',
     units: '€',
   },
@@ -66,6 +67,32 @@ const CONFIG = [
 
 export const Default = Template.bind({});
 Default.args = {
-  variant: FILTER_VARIANT.BUBBLE,
   config: CONFIG,
+};
+
+export const WithDefaultValues = Template.bind({});
+WithDefaultValues.args = {
+  config: [
+    {
+      ...CONFIG[0],
+      defaultValue: [
+        { key: 'max', value: '2000' },
+        { key: 'min', value: '20' },
+      ],
+    },
+    {
+      ...CONFIG[1],
+      defaultValue: [{ key: 'warranty', value: 'true' }],
+    },
+  ],
+};
+
+export const WithInitialValues = Template.bind({});
+WithInitialValues.args = {
+  config: CONFIG,
+  initialValues: [
+    { key: 'warranty', value: 'true' },
+    { key: 'max', value: '5000' },
+    { key: 'min', value: '1000' },
+  ],
 };
