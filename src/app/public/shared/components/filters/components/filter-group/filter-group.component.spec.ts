8ddf6c723e1fe29174e463006ed6afa3ae111db5
Irene Blanco <irene.blanco@wallapop.com>
2021-03-03 17:17:10 +0100

    [WEB-359]: FilterGroupComponent - Tests

--- src/app/public/shared/components/filters/components/filter-group/filter-group.component.spec.ts
+++ src/app/public/shared/components/filters/components/filter-group/filter-group.component.spec.ts
@@ -1,29 +1,71 @@
 import { CommonModule } from '@angular/common';
 import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FilterGroupComponent } from './ filter-group.component';
+import { FormGroup } from '@angular/forms';
+import { FILTER_TYPES } from '../../core/enums/filter-types/filter-types.enum';
+import { FILTER_VARIANT } from '../abstract-filter/abstract-filter.enum';
+import { FilterGroup } from './classes/filter-group';
+import { FilterGroupComponent } from './filter-group.component';
+import { FilterFactoryService } from './services/filter-factory.service';
 
 describe('FilterGroupComponent', () => {
   let component: FilterGroupComponent;
   let fixture: ComponentFixture<FilterGroupComponent>;
-  let de: DebugElement;
-  let el: HTMLElement;
+  let filterFactoryService: FilterFactoryService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [FilterGroupComponent],
       imports: [CommonModule],
+      providers: [
+        {
+          provide: FilterFactoryService,
+          useValue: {
+            insertFilters() {},
+            getFilterGroup() {
+              return new FilterGroup([]);
+            },
+          },
+        },
+      ],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FilterGroupComponent);
     component = fixture.componentInstance;
-    de = fixture.debugElement;
-    el = de.nativeElement;
+    filterFactoryService = TestBed.inject(FilterFactoryService);
+
+    component.config = [
+      {
+        id: 'id',
+        type: FILTER_TYPES.TOGGLE,
+        mapKey: {
+          key: 'key',
+        },
+        title: 'title',
+        bubblePlaceholder: 'bubblePlaceholder',
+      },
+    ];
+    component.initialValues = [{ key: 'key', value: 'true' }];
+    component.variant = FILTER_VARIANT.BUBBLE;
+    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should insert filters', () => {
+    spyOn(filterFactoryService, 'insertFilters');
+
+    component.ngAfterViewInit();
+
+    expect(filterFactoryService.insertFilters).toHaveBeenCalledWith(
+      component.config,
+      component.initialValues,
+      component.variant,
+      component.query
+    );
   });
 });
