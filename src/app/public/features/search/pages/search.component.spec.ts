0e97323dd1075f5c45bf3605627a4224ee486724
Angel <ajarac92@gmail.com>
2021-03-04 12:59:25 +0100

    [WEB-365]: Fix tests

--- src/app/public/features/search/pages/search.component.spec.ts
+++ src/app/public/features/search/pages/search.component.spec.ts
@@ -1,22 +1,33 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { AdsService } from '@core/ads/services/ads/ads.service';
+import { DeviceService } from '@core/device/device.service';
+import { DeviceType } from '@core/device/deviceType.enum';
 import { MockAdsService } from '@fixtures/ads.fixtures.spec';
 import { AdComponentStub } from '@fixtures/shared/components/ad.component.stub';
+import { random } from 'faker';
 import { AD_PUBLIC_SEARCH } from '../core/ads/search-ads.config';
 import { SearchComponent } from './search.component';
 
 describe('WallComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
+  let deviceServiceMock;
 
   beforeEach(
     waitForAsync(() => {
+      deviceServiceMock = {
+        getDeviceType: () => random.arrayElement([DeviceType.DESKTOP, DeviceType.MOBILE, DeviceType.TABLET]),
+      };
       TestBed.configureTestingModule({
         declarations: [SearchComponent, AdComponentStub],
         providers: [
           {
             provide: AdsService,
             useValue: MockAdsService,
+          },
+          {
+            provide: DeviceService,
+            useValue: deviceServiceMock,
           },
         ],
       }).compileComponents();
