1582ab35ddf4f0c98ae00338b5647978710ada21
Laia Lopez <laialopezz99@gmail.com>
2021-03-04 15:27:13 +0100

    [WEB-273]: [Translations] Item Conditions

--- src/app/public/features/item-detail/core/services/map-extra-info/map-extra-info.service.ts
+++ src/app/public/features/item-detail/core/services/map-extra-info/map-extra-info.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Car } from '@core/item/car';
 import { Item } from '@core/item/item';
 
+export type ItemCondition = 'un_opened' | 'un_worn' | 'in_box' | 'new' | 'as_good_as_new' | 'good' | 'fair' | 'has_given_it_all';
 @Injectable({
   providedIn: 'root',
 })
@@ -42,6 +43,9 @@ export class MapExtraInfoService {
     if (key === 'size') {
       return value?.text;
     }
+    if (key === 'condition') {
+      return this.translateCondition(value);
+    }
     return value;
   }
 
@@ -49,8 +53,29 @@ export class MapExtraInfoService {
     return Object.keys(item.extraInfo).find((itemKey) => itemKey === key) && this.specificationIsDefined(item.extraInfo[key]);
   }
 
-  private specificationIsDefined(specification): boolean {
+  private specificationIsDefined(specification: string): boolean {
     return specification !== null && specification !== undefined && specification !== '';
+  }
+
+  private translateCondition(condition: ItemCondition): string {
+    switch (condition) {
+      case 'un_opened':
+        return $localize`:@@Condition_UnOpened:Unopened`;
+      case 'un_worn':
+        return $localize`:@@Condition_Unworn:Unworn`;
+      case 'in_box':
+        return $localize`:@@Condition_InBox:In its box`;
+      case 'new':
+        return $localize`:@@New:New`;
+      case 'as_good_as_new':
+        return $localize`:@@Condition_AsGoodAsNew:As good as new`;
+      case 'good':
+        return $localize`:@@GoodCondition:Good condition`;
+      case 'fair':
+        return $localize`:@@Condition_Fair:Fair condition`;
+      case 'has_given_it_all':
+        return $localize`:@@Condition_HasGivenItAll:May have to be repaired`;
+    }
   }
 
   private capitalizeLabels(labels: string[]): string[] {
