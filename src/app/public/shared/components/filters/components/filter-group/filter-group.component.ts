e218263ebe3d1f2ddacec1eab2491c02ec74fd7d
Irene Blanco <irene.blanco@wallapop.com>
2021-03-05 13:18:57 +0100

    [WEB-359]: FilterGroupComponent - Added filterGroupSubscriptions to unsubscribe

--- src/app/public/shared/components/filters/components/filter-group/filter-group.component.ts
+++ src/app/public/shared/components/filters/components/filter-group/filter-group.component.ts
@@ -1,17 +1,28 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, EventEmitter, Input, Output, QueryList, ViewChildren } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  Output,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 import { FilterConfig } from '../../interfaces/filter-config.interface';
 import { FilterParameter } from '../../interfaces/filter-parameter.interface';
 import { FILTER_VARIANT } from '../abstract-filter/abstract-filter.enum';
 import { FilterHostDirective } from './directives/filter-host.directive';
 import { FilterFactoryService } from './services/filter-factory.service';
 import { FilterGroup } from './classes/filter-group';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'tsl-filter-group',
   templateUrl: './filter-group.component.html',
   styleUrls: ['./filter-group.component.scss'],
 })
-export class FilterGroupComponent implements AfterViewInit {
+export class FilterGroupComponent implements AfterViewInit, OnDestroy {
   @ViewChildren(FilterHostDirective) query: QueryList<FilterHostDirective>;
   @Input() initialValues: FilterParameter[];
   @Input() config: FilterConfig<unknown>[] = [];
@@ -21,6 +32,7 @@ export class FilterGroupComponent implements AfterViewInit {
   readonly FILTER_VARIANT = FILTER_VARIANT;
 
   private filterGroup: FilterGroup;
+  private filterGroupSubscriptions: Subscription[];
 
   constructor(private filterFactory: FilterFactoryService) {}
 
@@ -35,5 +47,11 @@ export class FilterGroupComponent implements AfterViewInit {
 
   private insertFilters() {
     this.filterFactory.insertFilters(this.config, this.initialValues, this.variant, this.query);
+  }
+
+  ngOnDestroy(): void {
+    this.filterGroupSubscriptions.forEach((subscription: Subscription) => {
+      subscription.unsubscribe();
+    });
   }
 }
