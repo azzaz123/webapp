c62cbe7a7083760918087f7ef2c832bead322d6e
Irene Blanco <irene.blanco@wallapop.com>
2021-03-09 15:17:38 +0100

    [WEB-367]: SearchFiltersComponent - First version

--- src/app/public/features/search/components/search-filters/search-filters.component.ts
+++ src/app/public/features/search/components/search-filters/search-filters.component.ts
@@ -1,12 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { FILTER_VARIANT } from '@public/shared/components/filters/components/abstract-filter/abstract-filter.enum';
+import { CAR_CONFIGURATION_FILTERS } from '@public/shared/components/filters/core/enums/configuration/car/car-configuration-filters';
 
 @Component({
   selector: 'tsl-search-filters',
   templateUrl: './search-filters.component.html',
   styleUrls: ['./search-filters.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SearchFiltersComponent {
+  public readonly FILTER_VARIANT = FILTER_VARIANT;
   public drawer = {
     isOpen: false,
     offsetTop: 66,
@@ -14,6 +16,9 @@ export class SearchFiltersComponent {
   };
 
   public activeFiltersCount = 0;
+
+  public bubbleFiltersConfig = CAR_CONFIGURATION_FILTERS.BUBBLE;
+  public drawerFiltersConfig = CAR_CONFIGURATION_FILTERS.CONTENT;
 
   constructor() {}
 
