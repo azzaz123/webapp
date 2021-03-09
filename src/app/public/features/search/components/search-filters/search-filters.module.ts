887ad47d225d8490aeede7c1ea292f8cb1a2f62d
Irene Blanco <irene.blanco@wallapop.com>
2021-03-09 15:17:09 +0100

    [WEB-367]: SearchFilterModule - Added FilterGroupModule

--- src/app/public/features/search/components/search-filters/search-filters.module.ts
+++ src/app/public/features/search/components/search-filters/search-filters.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BubbleModule } from '@public/shared/components/bubble/bubble.module';
 import { DrawerModule } from '@public/shared/components/drawer/drawer.module';
+import { FilterGroupModule } from '@public/shared/components/filters/components/filter-group/filter-group.module';
 import { SearchFiltersComponent } from './search-filters.component';
 
 @NgModule({
-  imports: [DrawerModule, BubbleModule],
+  imports: [DrawerModule, BubbleModule, FilterGroupModule],
   providers: [],
   declarations: [SearchFiltersComponent],
   exports: [SearchFiltersComponent],
