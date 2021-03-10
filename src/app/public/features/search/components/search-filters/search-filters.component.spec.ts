fdb96d61d863a03964ebee96b0ec786d415d556a
Irene Blanco <irene.blanco@wallapop.com>
2021-03-10 15:35:23 +0100

    [WEB-367]: SearchFiltersComponent - Tests

--- src/app/public/features/search/components/search-filters/search-filters.component.spec.ts
+++ src/app/public/features/search/components/search-filters/search-filters.component.spec.ts
@@ -1,28 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SearchFiltersComponent } from './search-filters.component';
-import { ViewportService } from '@core/viewport/viewport.service';
-import { ReplaySubject } from 'rxjs';
-import { ViewportType } from '@core/viewport/viewport.enum';
+import { BubbleModule } from '@public/shared/components/bubble/bubble.module';
+import { DrawerModule } from '@public/shared/components/drawer/drawer.module';
+import { FilterGroupComponentStub } from '@public/shared/components/filters/components/filter-group/services/filter-group.component.stub';
 
 describe('SearchFiltersComponent', () => {
   let component: SearchFiltersComponent;
   let fixture: ComponentFixture<SearchFiltersComponent>;
-  let viewportSubject: ReplaySubject<ViewportType>;
 
   beforeEach(async () => {
-    viewportSubject = new ReplaySubject(1);
-
     await TestBed.configureTestingModule({
-      declarations: [SearchFiltersComponent],
-      providers: [
-        {
-          provide: ViewportService,
-          useValue: {
-            onViewportChange: viewportSubject.asObservable(),
-          },
-        },
-      ],
+      declarations: [SearchFiltersComponent, FilterGroupComponentStub],
+      imports: [BubbleModule, DrawerModule],
+      providers: [],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SearchFiltersComponent);
@@ -32,5 +22,64 @@ describe('SearchFiltersComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('when closing the drawer', () => {
+    beforeEach(() => {
+      component.closeDrawer();
+    });
+
+    it('should close the drawer', () => {
+      expect(component.drawer.isOpen).toBeFalsy();
+    });
+  });
+
+  describe('when toggling the drawer', () => {
+    describe('if was previously opened', () => {
+      beforeEach(() => {
+        component.drawer.isOpen = true;
+        component.toggleDrawer();
+      });
+
+      it('should close the drawer', () => {
+        expect(component.drawer.isOpen).toBeFalsy();
+      });
+    });
+
+    describe('if was previously closed', () => {
+      beforeEach(() => {
+        component.drawer.isOpen = false;
+        component.toggleDrawer();
+      });
+
+      it('should open the drawer', () => {
+        expect(component.drawer.isOpen).toBeTruthy();
+      });
+    });
+  });
+
+  describe('when filters button is clicked', () => {
+    let initialState: boolean;
+    const filtersButtonSelector = 'tsl-bubble';
+
+    beforeEach(() => {
+      initialState = component.drawer.isOpen;
+      fixture.nativeElement.querySelector(filtersButtonSelector).click();
+    });
+
+    it('should toggle the drawer', () => {
+      expect(!initialState).toEqual(component.drawer.isOpen);
+    });
+  });
+
+  describe('when filter bubble open state changes', () => {
+    it('should emit open state change event with the same value', () => {
+      const value = true;
+      spyOn(component.bubbleFilterOpenStateChange, 'emit');
+
+      component.bubbleOpenStateChange(value);
+
+      expect(component.bubbleFilterOpenStateChange.emit).toBeCalledWith(value);
+    });
   });
 });
