0a9944a718a918ff41608a2fe7a5ada2213bec9c
Irene Blanco <irene.blanco@wallapop.com>
2021-03-10 15:34:44 +0100

    [WEB-367]: Created FilterGroupComponentStub

---
+++ src/app/public/shared/components/filters/components/filter-group/services/filter-group.component.stub.ts
@@ -0,0 +1,24 @@
+import { Component, AfterViewInit, OnDestroy, ViewChildren, QueryList, Input, Output, EventEmitter } from '@angular/core';
+import { FilterConfig } from '../../../interfaces/filter-config.interface';
+import { FilterParameter } from '../../../interfaces/filter-parameter.interface';
+import { FILTER_VARIANT } from '../../abstract-filter/abstract-filter.enum';
+import { FilterHostDirective } from '../directives/filter-host.directive';
+
+@Component({
+  selector: 'tsl-filter-group',
+  template: '',
+})
+export class FilterGroupComponentStub implements AfterViewInit, OnDestroy {
+  @ViewChildren(FilterHostDirective) query: QueryList<FilterHostDirective>;
+  @Input() initialValues: FilterParameter[];
+  @Input() config: FilterConfig<unknown>[] = [];
+  @Input() variant: FILTER_VARIANT = FILTER_VARIANT.BUBBLE;
+  @Output() valueChange = new EventEmitter<FilterParameter[]>();
+  @Output() openStateChange = new EventEmitter<boolean>();
+
+  constructor() {}
+
+  ngAfterViewInit(): void {}
+
+  ngOnDestroy(): void {}
+}
