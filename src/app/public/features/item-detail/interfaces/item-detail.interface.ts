f7bbbfabad1ea3be809504504f38d3eb047e464f
Laia Lopez <laialopezz99@gmail.com>
2021-03-11 17:51:00 +0100

    [WEB-363]: [Interfaces] ItemDetail + Response + SocialShare

--- src/app/public/features/item-detail/interfaces/item-detail.interface.ts
+++ src/app/public/features/item-detail/interfaces/item-detail.interface.ts
@@ -1,9 +1,21 @@
-import { Car } from '@core/item/car';
-import { Item } from '@core/item/item';
-import { Realestate } from '@core/item/realestate';
-import { User } from '@core/user/user';
+import { Coordinate } from '@core/geolocation/address-response.interface';
+import { CounterSpecifications } from '../components/item-specifications/interfaces/item.specifications.interface';
+import { ItemDetailLocation } from '../pages/constants/item-detail.interface';
+import { ItemDetailResponse } from './item-detail-response.interface';
+import { SocialShare } from './social-share.interface';
 
-export interface ItemDetail {
-  item: Item | Car | Realestate;
-  user: User;
+export interface ItemDetail extends ItemDetailResponse {
+  itemSpecifications: CounterSpecifications[];
+  itemLocation: ItemDetailLocation;
+  socialShare: SocialShare;
+  coordinate: Coordinate;
+  images: string[];
+  bigImages: string[];
+  extraInfo: string[];
+  locationSpecifications: string;
+  haveCoordinates: boolean;
+  isApproximatedLocation: boolean;
+  isItemACar: boolean;
+  isItemAPhone: boolean;
+  isAFashionItem: boolean;
 }
