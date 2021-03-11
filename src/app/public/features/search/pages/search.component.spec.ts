053351b273103cefcdc6e60221ff5645b5d2a5e0
Pablo Macias Moreira <pablo.macias@mati-dev.es>
2021-03-11 20:27:03 +0100

    [WEB-336]: [Testing] Added tests for integration part

--- src/app/public/features/search/pages/search.component.spec.ts
+++ src/app/public/features/search/pages/search.component.spec.ts
@@ -18,10 +18,13 @@ import { CoreModule } from '@core/core.module';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AdComponentStub } from '@tests/shared';
 import { Store } from '@ngrx/store';
+import { MOCK_SEARCH_ITEM } from '../core/services/search-store.service.fixtures';
+import { Item } from '@core/item/item';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
+  let searchStoreService: SearchStoreService;
   let deviceServiceMock;
   let storeMock;
 
@@ -62,6 +65,7 @@ describe('SearchComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
+    searchStoreService = TestBed.inject(SearchStoreService);
     fixture.detectChanges();
   });
 
@@ -70,10 +74,7 @@ describe('SearchComponent', () => {
   });
 
   describe('when the component init', () => {
-    describe('when is desktop', () => {
-      beforeEach(() => {
-        spyOn(deviceServiceMock, 'getDeviceType').and.returnValue(DeviceType.DESKTOP);
-      });
+    describe('on init', () => {
       it('should configure ads', () => {
         spyOn(MockAdsService, 'setSlots').and.callThrough();
 
@@ -85,6 +86,35 @@ describe('SearchComponent', () => {
           AD_PUBLIC_SEARCH.search2r,
           AD_PUBLIC_SEARCH.search3r,
         ]);
+      });
+
+      it('should initialise items observable', () => {
+        component.ngOnInit();
+
+        expect(component.items$).toBeTruthy();
+      });
+    });
+  });
+
+  describe('when items change', () => {
+    const oldItems = [{ ...MOCK_SEARCH_ITEM, id: 'old_item' }];
+    beforeEach(() => {
+      component.ngOnInit();
+      searchStoreService.setItems(oldItems);
+    });
+    it('should update items', () => {
+      const newItems = [MOCK_SEARCH_ITEM, MOCK_SEARCH_ITEM];
+      let nextItems: Item[] = [];
+
+      component.items$.subscribe((items) => (nextItems = items));
+
+      searchStoreService.appendItems(newItems);
+
+      expect(nextItems.length).toBe(3);
+
+      nextItems.forEach((nextItem, index) => {
+        expect(nextItem).toBeInstanceOf(Item);
+        expect(nextItem.id).toBe(index !== 0 ? MOCK_SEARCH_ITEM.id : 'old_item');
       });
     });
   });
