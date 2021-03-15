8872f0b56bb4f8e5a4c9e34c6b9a0e06b004b561
Laia Lopez <laialopezz99@gmail.com>
2021-03-15 13:29:46 +0100

    [WEB-363]: [Fixtures] ItemDetail + SocialShare

---
+++ src/tests/item-detail.fixtures.spec.ts
@@ -0,0 +1,58 @@
+import { MOCK_CAR_EXTRA_INFO } from '@public/features/item-detail/core/services/map-extra-info/map-extra-info.fixtures.spec';
+import { ItemDetail } from '@public/features/item-detail/interfaces/item-detail.interface';
+import { ItemDetailLocation } from '@public/features/item-detail/pages/constants/item-detail.interface';
+import { ItemTaxonomies } from '@public/features/item-detail/components/item-taxonomies/interfaces/item-taxonomies.interface';
+
+import { of } from 'rxjs';
+import { MOCK_COUNTER_SPECIFICATIONS_CAR } from './map-specifications.fixtures.spec';
+import { MOCK_SOCIAL_SHARE } from './social-share.fixtures.spec';
+import { MOCK_USER_STATS } from './user.fixtures.spec';
+import { Coordinate } from '@core/geolocation/address-response.interface';
+import { MOCK_ITEM_CAR } from '@fixtures/item.fixtures.spec';
+import { MOCK_USER } from '@fixtures/user.fixtures.spec';
+import { ItemDetailResponse } from '@public/features/item-detail/interfaces/item-detail-response.interface';
+
+const MOCK_ITEM_DETAIL_LOCATION: ItemDetailLocation = {
+  city: 'Barcelona',
+  zip: '08027',
+  latitude: 2323,
+  longitude: 2323,
+};
+
+const MOCK_ITEM_DETAIL_COORDINATE: Coordinate = {
+  latitude: 2323,
+  longitude: 2323,
+};
+
+const MOCK_ITEM_TAXONOMIES: ItemTaxonomies = {
+  parentTaxonomy: 'Parent Taxonomy',
+  childTaxonomy: 'Chils Taxonomy',
+  icon: 'path/icon',
+};
+
+const MOCK_ITEM_DETAIL_LOCATION_SPECIFICATIONS = '08027, Barcelona';
+
+export const MOCK_ITEM_DETAIL_RESPONSE: ItemDetailResponse = {
+  item: MOCK_ITEM_CAR,
+  user: MOCK_USER,
+};
+
+export const MOCK_CAR_ITEM_DETAIL: ItemDetail = {
+  item: MOCK_ITEM_CAR,
+  user: MOCK_USER,
+  counterSpecifications: MOCK_COUNTER_SPECIFICATIONS_CAR,
+  location: MOCK_ITEM_DETAIL_LOCATION,
+  socialShare: MOCK_SOCIAL_SHARE,
+  coordinate: MOCK_ITEM_DETAIL_COORDINATE,
+  images: null,
+  bigImages: null,
+  userStats: of(MOCK_USER_STATS),
+  extraInfo: MOCK_CAR_EXTRA_INFO,
+  locationSpecifications: MOCK_ITEM_DETAIL_LOCATION_SPECIFICATIONS,
+  taxonomiesSpecifications: of(MOCK_ITEM_TAXONOMIES),
+  haveCoordinates: true,
+  isApproximatedLocation: true,
+  isItemACar: true,
+  isItemAPhone: false,
+  isAFashionItem: false,
+};
