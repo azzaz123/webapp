28338dfad547de4cdad662b0defc35e7c010cd04
Pablo Macias Moreira <pablo.macias@mati-dev.es>
2021-03-15 12:31:57 +0100

    [WEB-376]: [Functionality] Implemented service and filled up tests

--- src/app/public/shared/components/filters/core/services/filter-option-service/services/filter-parameter-draft.service.ts
+++ src/app/public/shared/components/filters/core/services/filter-option-service/services/filter-parameter-draft.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { FilterParameter } from '@public/shared/components/filters/interfaces/filter-parameter.interface';
 
 @Injectable()
-export class FilterParameterDraftService {}
+export class FilterParameterDraftService {
+  private parametersSubject = new BehaviorSubject<FilterParameter[]>([]);
+
+  public get parameters$(): Observable<FilterParameter[]> {
+    return this.parametersSubject.asObservable();
+  }
+
+  public getParameters(): FilterParameter[] {
+    return this.parametersSubject.value;
+  }
+
+  public getParametersByKey(keys: string[]): FilterParameter[] {
+    return this.getParameters().filter((parameter) => keys.includes(parameter.key));
+  }
+
+  public setParameters(parameters: FilterParameter[]): void {
+    this.parametersSubject.next(parameters);
+  }
+
+  public upsertParameters(parameters: FilterParameter[]): void {
+    const mergedParameters = this.mergeParameters(parameters, this.getParameters());
+    this.parametersSubject.next(mergedParameters);
+  }
+
+  private mergeParameters(newParameters: FilterParameter[], oldParameters: FilterParameter[]): FilterParameter[] {
+    return [...newParameters, ...oldParameters].reduce((accumulatedParameters, newParameter) => {
+      const accumulatedKeys = accumulatedParameters.map((parameter) => parameter.key);
+      if (accumulatedKeys.includes(newParameter.key)) {
+        return accumulatedParameters;
+      }
+
+      return [...accumulatedParameters, newParameter];
+    }, []);
+  }
+}
