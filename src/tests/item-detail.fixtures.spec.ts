de95c8cb6e7666efa849acdd59e13707bda772d6
Laia Lopez <laialopezz99@gmail.com>
2021-03-16 10:55:16 +0100

    [WEB-363]: [Fixtures] Testing ItemDetail

--- src/tests/item-detail.fixtures.spec.ts
+++ src/tests/item-detail.fixtures.spec.ts
@@ -2,13 +2,20 @@ import { MOCK_CAR_EXTRA_INFO } from '@public/features/item-detail/core/services/
 import { ItemDetail } from '@public/features/item-detail/interfaces/item-detail.interface';
 import { ItemDetailLocation } from '@public/features/item-detail/pages/constants/item-detail.interface';
 import { ItemTaxonomies } from '@public/features/item-detail/components/item-taxonomies/interfaces/item-taxonomies.interface';
-
 import { of } from 'rxjs';
 import { MOCK_COUNTER_SPECIFICATIONS_CAR } from './map-specifications.fixtures.spec';
 import { MOCK_SOCIAL_SHARE } from './social-share.fixtures.spec';
-import { MOCK_USER_STATS } from './user.fixtures.spec';
+import { MOCK_USER_STATS, MOCK_USER_WITHOUT_LOCATION } from './user.fixtures.spec';
 import { Coordinate } from '@core/geolocation/address-response.interface';
-import { MOCK_ITEM_CAR, MOCK_ITEM_CAR_WITHOUT_COORDINATES } from '@fixtures/item.fixtures.spec';
+import {
+  MOCK_ITEM_APROX_LOCATION,
+  MOCK_ITEM_CAR,
+  MOCK_ITEM_CELLPHONES,
+  MOCK_ITEM_CELLPHONES_NO_SUBCATEGORY,
+  MOCK_ITEM_CELLPHONES_PARENT_SUBCATEGORY,
+  MOCK_ITEM_WITHOUT_COORDINATES,
+  MOCK_ITEM_WITHOUT_LOCATION,
+} from '@fixtures/item.fixtures.spec';
 import { MOCK_USER } from '@fixtures/user.fixtures.spec';
 import { ItemDetailResponse } from '@public/features/item-detail/interfaces/item-detail-response.interface';
 
@@ -37,8 +44,33 @@ export const MOCK_ITEM_DETAIL_RESPONSE: ItemDetailResponse = {
   user: MOCK_USER,
 };
 
+export const MOCK_ITEM_DETAIL_RESPONSE_WITH_APROX_LOCATION: ItemDetailResponse = {
+  item: MOCK_ITEM_APROX_LOCATION,
+  user: MOCK_USER,
+};
+
+export const MOCK_ITEM_DETAIL_RESPONSE_WITHOUT_LOCATION: ItemDetailResponse = {
+  item: MOCK_ITEM_WITHOUT_LOCATION,
+  user: MOCK_USER_WITHOUT_LOCATION,
+};
+
 export const MOCK_ITEM_DETAIL_RESPONSE_WITHOUT_COORDINATES: ItemDetailResponse = {
-  item: MOCK_ITEM_CAR_WITHOUT_COORDINATES,
+  item: MOCK_ITEM_WITHOUT_COORDINATES,
+  user: MOCK_USER,
+};
+
+export const MOCK_CELLPHONE_RESPONSE: ItemDetailResponse = {
+  item: MOCK_ITEM_CELLPHONES,
+  user: MOCK_USER,
+};
+
+export const MOCK_CELLPHONE_WITHOUT_SUBCATEGORY_RESPONSE: ItemDetailResponse = {
+  item: MOCK_ITEM_CELLPHONES_NO_SUBCATEGORY,
+  user: MOCK_USER,
+};
+
+export const MOCK_CELLPHONE_PARENT_RESPONSE: ItemDetailResponse = {
+  item: MOCK_ITEM_CELLPHONES_PARENT_SUBCATEGORY,
   user: MOCK_USER,
 };
 
