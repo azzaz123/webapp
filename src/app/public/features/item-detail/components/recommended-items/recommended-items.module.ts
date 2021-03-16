5a23807795ce6c78cabdbb252c46e741df5725ad
Irene Blanco <irene.blanco@wallapop.com>
2021-03-16 11:56:49 +0100

    [WEB-377]: Updated ItemCardListModule imports

--- src/app/public/features/item-detail/components/recommended-items/recommended-items.module.ts
+++ src/app/public/features/item-detail/components/recommended-items/recommended-items.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RecommendedItemsComponent } from './recommended-items.component';
-import { ItemCardListModule } from '@public/features/public-profile/pages/user-published/components/item-card-list/item-card-list.module';
+import { ItemCardListModule } from '@public/shared/components/item-card-list/item-card-list.module';
 import { CommonModule } from '@angular/common';
 import { CheckSessionModule } from '@public/core/directives/check-session/check-session.module';
 
--- src/app/public/features/search/search.module.ts
+++ src/app/public/features/search/search.module.ts
@@ -4,7 +4,7 @@ import { CheckSessionModule } from '@public/core/directives/check-session/check-
 import { PublicLayoutModule } from '@public/layout/public-layout.module';
 import { AdModule } from '@shared/ad/ad.module';
 import { SharedModule } from '@shared/shared.module';
-import { ItemCardListModule } from '../public-profile/pages/user-published/components/item-card-list/item-card-list.module';
+import { ItemCardListModule } from '@public/shared/components/item-card-list/item-card-list.module';
 import { SearchFiltersModule } from './components/search-filters/search-filters.module';
 import { SearchLayoutComponent } from './components/search-layout/search-layout.component';
 import { SearchComponent } from './pages/search.component';
