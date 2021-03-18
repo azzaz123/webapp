c485f1c18ef4f8871496c20e6a13c08bb6759ec1
Laia Lopez <laialopezz99@gmail.com>
2021-03-18 15:31:15 +0100

    [WEB-389]: [Storybook] ItemWideCard

---
+++ src/app/public/shared/components/item-wide-card/item-wide-card.stories.ts
@@ -0,0 +1,77 @@
+import { CommonModule } from '@angular/common';
+import { SvgIconModule } from '@core/svg-icon/svg-icon.module';
+import { FavouriteIconModule } from '@public/shared/components/favourite-icon/favourite-icon.module';
+import { CustomCurrencyModule } from '@shared/pipes/custom-currency/custom-currency.module';
+import { CUSTOM_VIEWPORT_NAME } from '@storybook-config/viewports/custom-viewports';
+import { Story, Meta } from '@storybook/angular/types-6-0';
+import { SlidesCarouselModule } from '../carousel-slides/carousel-slides.module';
+import { ItemExtraInfoModule } from '../item-extra-info/item-extra-info.module';
+import { ItemWideCardComponent } from './item-wide-card.component';
+import { MOCK_ITEM } from './item-wide-card.mock.stories';
+
+export default {
+  title: 'Webapp/Public/Shared/Components/ItemWideCard',
+  component: ItemWideCardComponent,
+  argTypes: { toggleFavourite: { action: 'toggleFavourite' } },
+} as Meta;
+
+const Template: Story<ItemWideCardComponent> = (args: ItemWideCardComponent) => ({
+  component: ItemWideCardComponent,
+  props: args,
+  moduleMetadata: {
+    declarations: [ItemWideCardComponent],
+    imports: [CommonModule, FavouriteIconModule, CustomCurrencyModule, SvgIconModule, SlidesCarouselModule, ItemExtraInfoModule],
+  },
+  template:
+    '<tsl-item-wide-card [item]="item" [showFavourite]="showFavourite" [itemExtraInfo]="itemExtraInfo" (toggleFavourite)="toggleFavourite()"></tsl-item-wide-card>',
+});
+
+const MOCK_ITEM_EXTRA_INFO = ['Cámara De Fotos', '20 cm', 'Digital'];
+
+export const DefaultMD = Template.bind({});
+DefaultMD.args = {
+  item: MOCK_ITEM,
+  itemExtraInfo: MOCK_ITEM_EXTRA_INFO,
+};
+
+DefaultMD.parameters = {
+  viewport: {
+    defaultViewport: CUSTOM_VIEWPORT_NAME.MD,
+  },
+};
+
+export const DefaultXS = Template.bind({});
+DefaultXS.args = {
+  item: MOCK_ITEM,
+  itemExtraInfo: MOCK_ITEM_EXTRA_INFO,
+};
+
+DefaultXS.parameters = {
+  viewport: {
+    defaultViewport: CUSTOM_VIEWPORT_NAME.XS,
+  },
+};
+
+export const DefaultMDWithoutFavourite = Template.bind({});
+DefaultMDWithoutFavourite.args = {
+  item: MOCK_ITEM,
+  itemExtraInfo: MOCK_ITEM_EXTRA_INFO,
+};
+
+DefaultMDWithoutFavourite.parameters = {
+  viewport: {
+    defaultViewport: CUSTOM_VIEWPORT_NAME.MD,
+  },
+};
+
+export const DefaultXSWithoutFavourite = Template.bind({});
+DefaultXSWithoutFavourite.args = {
+  item: MOCK_ITEM,
+  itemExtraInfo: MOCK_ITEM_EXTRA_INFO,
+};
+
+DefaultXSWithoutFavourite.parameters = {
+  viewport: {
+    defaultViewport: CUSTOM_VIEWPORT_NAME.XS,
+  },
+};
