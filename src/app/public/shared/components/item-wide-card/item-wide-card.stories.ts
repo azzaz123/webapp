e57cc7d0644b63de6cb196e66137d26d808b73a1
Laia Lopez <laialopezz99@gmail.com>
2021-03-18 17:48:35 +0100

    [WEB-389]: [Storybook] More Cases

--- src/app/public/shared/components/item-wide-card/item-wide-card.stories.ts
+++ src/app/public/shared/components/item-wide-card/item-wide-card.stories.ts
@@ -8,12 +8,19 @@ import { Story, Meta } from '@storybook/angular/types-6-0';
 import { SlidesCarouselModule } from '../carousel-slides/carousel-slides.module';
 import { ItemExtraInfoModule } from '../item-extra-info/item-extra-info.module';
 import { ItemWideCardComponent } from './item-wide-card.component';
-import { MOCK_ITEM } from './item-wide-card.mock.stories';
+import {
+  MOCK_ITEM,
+  MOCK_ITEM_BUMPED,
+  MOCK_ITEM_COUNTRY_BUMPED,
+  MOCK_ITEM_FAVOURITE,
+  MOCK_ITEM_RESERVED,
+  MOCK_ITEM_SOLD,
+  MOCK_ITEM_WITHOUT_IMAGES,
+} from './item-wide-card.mock.stories';
 
 export default {
   title: 'Webapp/Public/Shared/Components/ItemWideCard',
   component: ItemWideCardComponent,
-  argTypes: { toggleFavourite: { action: 'toggleFavourite' } },
 } as Meta;
 
 const Template: Story<ItemWideCardComponent> = (args: ItemWideCardComponent) => ({
@@ -31,7 +38,8 @@ const Template: Story<ItemWideCardComponent> = (args: ItemWideCardComponent) => 
       HttpClientModule,
     ],
   },
-  template: '<tsl-item-wide-card [item]="item" [itemExtraInfo]="itemExtraInfo" (toggleFavourite)="toggleFavourite()"></tsl-item-wide-card>',
+  template:
+    '<tsl-item-wide-card [item]="item" [showFavourite]="showFavourite" [itemExtraInfo]="itemExtraInfo" (toggleFavourite)="toggleFavourite()"></tsl-item-wide-card>',
 });
 
 const MOCK_ITEM_EXTRA_INFO = ['Cámara De Fotos', '20 cm', 'Digital'];
@@ -40,6 +48,7 @@ export const DefaultMD = Template.bind({});
 DefaultMD.args = {
   item: MOCK_ITEM,
   itemExtraInfo: MOCK_ITEM_EXTRA_INFO,
+  showFavourite: true,
 };
 
 DefaultMD.parameters = {
@@ -52,6 +61,7 @@ export const DefaultXS = Template.bind({});
 DefaultXS.args = {
   item: MOCK_ITEM,
   itemExtraInfo: MOCK_ITEM_EXTRA_INFO,
+  showFavourite: true,
 };
 
 DefaultXS.parameters = {
@@ -64,6 +74,7 @@ export const DefaultMDWithoutFavourite = Template.bind({});
 DefaultMDWithoutFavourite.args = {
   item: MOCK_ITEM,
   itemExtraInfo: MOCK_ITEM_EXTRA_INFO,
+  showFavourite: false,
 };
 
 DefaultMDWithoutFavourite.parameters = {
@@ -76,9 +87,94 @@ export const DefaultXSWithoutFavourite = Template.bind({});
 DefaultXSWithoutFavourite.args = {
   item: MOCK_ITEM,
   itemExtraInfo: MOCK_ITEM_EXTRA_INFO,
+  showFavourite: false,
 };
 
 DefaultXSWithoutFavourite.parameters = {
+  viewport: {
+    defaultViewport: CUSTOM_VIEWPORT_NAME.XS,
+  },
+};
+
+export const ReservedMD = Template.bind({});
+ReservedMD.args = {
+  item: MOCK_ITEM_RESERVED,
+  itemExtraInfo: MOCK_ITEM_EXTRA_INFO,
+  showFavourite: true,
+};
+ReservedMD.parameters = {
+  viewport: {
+    defaultViewport: CUSTOM_VIEWPORT_NAME.MD,
+  },
+};
+
+export const SoldXS = Template.bind({});
+SoldXS.args = {
+  item: MOCK_ITEM_SOLD,
+  itemExtraInfo: MOCK_ITEM_EXTRA_INFO,
+  showFavourite: true,
+};
+SoldXS.parameters = {
+  viewport: {
+    defaultViewport: CUSTOM_VIEWPORT_NAME.XS,
+  },
+};
+
+export const BumpedMD = Template.bind({});
+BumpedMD.args = {
+  item: MOCK_ITEM_BUMPED,
+  itemExtraInfo: MOCK_ITEM_EXTRA_INFO,
+  showFavourite: true,
+};
+BumpedMD.parameters = {
+  viewport: {
+    defaultViewport: CUSTOM_VIEWPORT_NAME.MD,
+  },
+};
+
+export const CountryBumpedXS = Template.bind({});
+CountryBumpedXS.args = {
+  item: MOCK_ITEM_COUNTRY_BUMPED,
+  itemExtraInfo: MOCK_ITEM_EXTRA_INFO,
+  showFavourite: true,
+};
+CountryBumpedXS.parameters = {
+  viewport: {
+    defaultViewport: CUSTOM_VIEWPORT_NAME.XS,
+  },
+};
+
+export const FavouritedMD = Template.bind({});
+FavouritedMD.args = {
+  item: MOCK_ITEM_FAVOURITE,
+  itemExtraInfo: MOCK_ITEM_EXTRA_INFO,
+  showFavourite: true,
+};
+FavouritedMD.parameters = {
+  viewport: {
+    defaultViewport: CUSTOM_VIEWPORT_NAME.MD,
+  },
+};
+
+export const WithoutImagesMD = Template.bind({});
+WithoutImagesMD.args = {
+  item: MOCK_ITEM_WITHOUT_IMAGES,
+  itemExtraInfo: MOCK_ITEM_EXTRA_INFO,
+  showFavourite: true,
+};
+WithoutImagesMD.parameters = {
+  viewport: {
+    defaultViewport: CUSTOM_VIEWPORT_NAME.MD,
+  },
+};
+
+export const WithoutImagesXS = Template.bind({});
+WithoutImagesXS.args = {
+  item: MOCK_ITEM_WITHOUT_IMAGES,
+  itemExtraInfo: MOCK_ITEM_EXTRA_INFO,
+  showFavourite: true,
+};
+WithoutImagesXS.parameters = {
   viewport: {
     defaultViewport: CUSTOM_VIEWPORT_NAME.XS,
   },
