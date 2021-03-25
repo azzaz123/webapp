dfb4e70724eaf252e415d468d68bae62f2bb1e4a
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-03-25 23:49:18 +0100

    [WEB-402]: Tested share Item event

--- src/app/shared/social-share/social-share.component.spec.ts
+++ src/app/shared/social-share/social-share.component.spec.ts
@@ -7,6 +7,11 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { FacebookShare } from './interfaces/facebook-share.interface';
 import { TwitterShare } from './interfaces/twitter-share.interface';
 import { EmailShare } from './interfaces/email-share.interface';
+import { AnalyticsEvent, ANALYTICS_EVENT_NAMES, ANALYTIC_EVENT_TYPES, SCREEN_IDS, ShareItem } from '@core/analytics/analytics-constants';
+import { MOCK_CAR } from '@fixtures/car.fixtures.spec';
+import { MOCK_USER } from '@fixtures/user.fixtures.spec';
+import { AnalyticsService } from '@core/analytics/analytics.service';
+import { MockAnalyticsService } from '@fixtures/analytics.fixtures.spec';
 
 describe('SocialShareComponent', () => {
   let component: SocialShareComponent;
@@ -14,12 +19,17 @@ describe('SocialShareComponent', () => {
   let de: DebugElement;
   let el: HTMLElement;
   let socialShareService: SocialShareService;
+  let analyticsService: AnalyticsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [SocialShareComponent],
       imports: [SvgIconModule, HttpClientTestingModule],
       providers: [
+        {
+          provide: AnalyticsService,
+          useClass: MockAnalyticsService,
+        },
         {
           provide: SocialShareService,
           useValue: {
@@ -34,10 +44,13 @@ describe('SocialShareComponent', () => {
 
   beforeEach(() => {
     socialShareService = TestBed.inject(SocialShareService);
+    analyticsService = TestBed.inject(AnalyticsService);
     fixture = TestBed.createComponent(SocialShareComponent);
     component = fixture.componentInstance;
     de = fixture.debugElement;
     el = de.nativeElement;
+    component.item = MOCK_CAR;
+    component.user = MOCK_USER;
     fixture.detectChanges();
   });
 
@@ -49,6 +62,18 @@ describe('SocialShareComponent', () => {
     const facebookIconSelector = '[src*="facebook"]';
     const twitterIconSelector = '[src*="twitter"]';
     const emailIconSelector = '[src*="email"]';
+    const shareItemEvent: AnalyticsEvent<ShareItem> = {
+      name: ANALYTICS_EVENT_NAMES.ShareItem,
+      eventType: ANALYTIC_EVENT_TYPES.Social,
+      attributes: {
+        itemId: MOCK_CAR.id,
+        categoryId: MOCK_CAR.categoryId,
+        channel: 'facebook',
+        screenId: SCREEN_IDS.ItemDetail,
+        isPro: MOCK_USER.featured,
+        salePrice: MOCK_CAR.salePrice,
+      },
+    };
 
     describe('When any data is passed', () => {
       it('should be created', () => {
@@ -84,6 +109,14 @@ describe('SocialShareComponent', () => {
 
         expect(socialShareService.facebookShare).toBeCalledWith(component.facebook.url);
       });
+
+      it('should send share item event after clicking facebook icon', () => {
+        spyOn(analyticsService, 'trackEvent');
+
+        facebookIconElement.click();
+
+        expect(analyticsService.trackEvent).toHaveBeenCalledWith(shareItemEvent);
+      });
     });
 
     describe('When twitter data is passed', () => {
@@ -109,6 +142,16 @@ describe('SocialShareComponent', () => {
         twitterIconElement.click();
 
         expect(socialShareService.twitterShare).toBeCalledWith(component.twitter.url, component.twitter.text);
+      });
+
+      it('should send share item event after clicking twitter icon', () => {
+        spyOn(analyticsService, 'trackEvent');
+
+        twitterIconElement.click();
+
+        shareItemEvent.attributes.channel = 'twitter';
+
+        expect(analyticsService.trackEvent).toHaveBeenCalledWith(shareItemEvent);
       });
     });
 
@@ -136,6 +179,16 @@ describe('SocialShareComponent', () => {
         emailIconElement.click();
 
         expect(socialShareService.emailShare).toBeCalledWith(component.email.url, component.email.subject, component.email.message);
+      });
+
+      it('should send share item event after clicking twitter icon', () => {
+        spyOn(analyticsService, 'trackEvent');
+
+        emailIconElement.click();
+
+        shareItemEvent.attributes.channel = 'email';
+
+        expect(analyticsService.trackEvent).toHaveBeenCalledWith(shareItemEvent);
       });
     });
   });
