584c67dcd6c65a2943d9c4510e4bc2dd56327de2
Irene Blanco <irene.blanco@wallapop.com>
2021-03-26 13:43:09 +0100

    [WEB-407]: Created FilterConfigurationService

--- src/app/public/shared/services/filter-configuration/filter-configuration.service.ts
+++ src/app/public/shared/services/filter-configuration/filter-configuration.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@angular/core';
+import { CAR_CONFIGURATION_FILTERS } from '@public/shared/components/filters/core/enums/configuration/car/car-configuration-filters';
+import { FILTER_CONFIGURATIONS } from '@public/shared/components/filters/core/enums/configuration/filter-configurations';
+import { FilterConfig } from '@public/shared/components/filters/interfaces/filter-config.interface';
+import { FilterParameter } from '@public/shared/components/filters/interfaces/filter-parameter.interface';
+import { FilterConfigurations } from './interfaces/filter-group-config.interface';
 
 @Injectable()
-export class FilterConfigurationService {}
+export class FilterConfigurationService {
+  getConfiguration(parameters: FilterParameter[]): FilterConfigurations {
+    const configurationFilters = CAR_CONFIGURATION_FILTERS;
+    const filters: FilterConfigurations = {
+      bubble: [],
+      drawer: [],
+    };
+
+    configurationFilters.bubble.forEach((filterConfigurationId: string) => {
+      filters.bubble.push(this.getFilterConfigById(filterConfigurationId));
+    });
+
+    configurationFilters.drawer.forEach((filterConfigurationId: string) => {
+      filters.drawer.push(this.getFilterConfigById(filterConfigurationId));
+    });
+
+    return filters;
+  }
+
+  private getFilterConfigById(filterConfigurationId: string): FilterConfig<any> {
+    // TODO REMOVE THAT ANY
+    return FILTER_CONFIGURATIONS.find((filterConfiguration) => filterConfiguration.id === filterConfigurationId);
+  }
+}
