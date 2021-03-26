186896f7e06df242d8c76e44b4dba4e1929478d3
Irene Blanco <irene.blanco@wallapop.com>
2021-03-26 13:35:03 +0100

    [WEB-407]: Created FilterConfigurations interface

---
+++ src/app/public/shared/services/filter-configuration/interfaces/filter-group-config.interface.ts
@@ -0,0 +1,6 @@
+import { FilterConfig } from '@public/shared/components/filters/interfaces/filter-config.interface';
+
+export interface FilterConfigurations {
+  bubble: FilterConfig<any>[]; // TODO REMOVE ANY
+  drawer: FilterConfig<any>[]; // TODO REMOVE ANY
+}
