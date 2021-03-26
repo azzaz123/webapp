9d8a1d0d46e4edda0ced1832eb7595a88c91cd85
Irene Blanco <irene.blanco@wallapop.com>
2021-03-26 14:14:23 +0100

    [WEB-407]: FILTER_CONFIGURATIONS - Added COMMON_CONSUMER_GOODS_CONFIGURATION_ID.PRICE

--- src/app/public/shared/components/filters/core/enums/configuration/filter-configurations.ts
+++ src/app/public/shared/components/filters/core/enums/configuration/filter-configurations.ts
@@ -1,12 +1,39 @@
 import { RangeFilterConfig } from '../../../components/range-filter/interfaces/range-filter-config.interface';
 import { ToggleFilterConfig } from '../../../components/toggle-filter/interfaces/toggle-filter-config.interface';
 import { CAR_CONFIGURATION_ID } from '../configuration-ids/car-configuration-ids';
+import { COMMON_CONSUMER_GOODS_CONFIGURATION_ID } from '../configuration-ids/consumer-goods-configuration-ids.enum';
 import { FILTER_TYPES } from '../filter-types/filter-types.enum';
 
 type FilterConfigType = RangeFilterConfig | ToggleFilterConfig;
 
 export const FILTER_CONFIGURATIONS: FilterConfigType[] = [
   // TODO ESE ANY NO ME GUSTA
+  // COMMON
+  {
+    id: COMMON_CONSUMER_GOODS_CONFIGURATION_ID.PRICE,
+    type: FILTER_TYPES.RANGE,
+    title: $localize`:@@filterPriceTitle:How much?`,
+    icon: '/assets/icons/filters/price.svg',
+    bubblePlaceholder: $localize`:@@filterPriceBubblePlaceholder:Price`,
+    mapKey: {
+      minKey: 'min_sale_price',
+      maxKey: 'max_sale_price',
+    },
+    units: '€',
+    range: [0, 20000],
+    stepsConfig: [
+      { range: [0, 50], step: 1 },
+      { range: [50, 100], step: 5 },
+      { range: [100, 300], step: 10 },
+      { range: [300, 500], step: 50 },
+      { range: [500, 1000], step: 100 },
+      { range: [1000, 3000], step: 250 },
+      { range: [3000, 5000], step: 500 },
+      { range: [5000, 10000], step: 1000 },
+      { range: [10000, 20000], step: 1250 },
+    ],
+    limitless: true,
+  },
   // CARS
   {
     id: CAR_CONFIGURATION_ID.PRICE,
