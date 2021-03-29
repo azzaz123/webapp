1c50e6311dda8c5d42c4522626e348c8e249e571
Pablo Macias Moreira <pablo.macias@mati-dev.es>
2021-03-29 09:54:19 +0200

    [WEB-315]: [PR comments] Removed HttpClientModule from abstract-filter.module.ts

--- src/app/public/shared/components/filters/components/abstract-filter/abstract-filter.module.ts
+++ src/app/public/shared/components/filters/components/abstract-filter/abstract-filter.module.ts
@@ -4,10 +4,9 @@ import { BubbleModule } from '@public/shared/components/bubble/bubble.module';
 import { FilterTemplateComponent } from '@public/shared/components/filters/components/abstract-filter/filter-template/filter-template.component';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { ButtonModule } from '@shared/button/button.module';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports: [CommonModule, BubbleModule, HttpClientModule, NgbDropdownModule, ButtonModule],
+  imports: [CommonModule, BubbleModule, NgbDropdownModule, ButtonModule],
   declarations: [FilterTemplateComponent],
   exports: [FilterTemplateComponent],
 })
