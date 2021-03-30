fda07c6e51a0acdbd9c7adc6196b5ed50398a505
Pablo Macias Moreira <pablo.macias@mati-dev.es>
2021-03-30 20:14:40 +0200

    [WEB-316]: [Fix] Fixed wrong import

--- src/app/public/shared/components/filters/components/abstract-select-filter/select-parent-option/select-parent-option.component.spec.ts
+++ src/app/public/shared/components/filters/components/abstract-select-filter/select-parent-option/select-parent-option.component.spec.ts
@@ -5,8 +5,7 @@ import { Component, DebugElement, Input } from '@angular/core';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { SvgIconComponent } from '@core/svg-icon/svg-icon/svg-icon.component';
 import { By } from '@angular/platform-browser';
-import { SelectOptionComponent } from '@public/../../../../../../../shared/form/components/select/select-option/select-option.component';
-import spyOn = jest.spyOn;
+import { SelectOptionComponent } from '@shared/form/components/select/select-option/select-option.component';
 
 @Component({
   // tslint:disable-next-line:component-selector
