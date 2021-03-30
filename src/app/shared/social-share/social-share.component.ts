770ed0c6c079a100d38462f6c965f227cd8a3c6e
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-03-30 16:11:01 +0200

    [WEB-402]: Pass data from social share component to itemdetail once social media icon is clicked

--- src/app/shared/social-share/social-share.component.ts
+++ src/app/shared/social-share/social-share.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { SocialShareService } from '@core/social-share/social-share.service';
+import { SOCIAL_SHARE_CHANNELS } from './enums/social-share-channels.enum';
 import { EmailShare } from './interfaces/email-share.interface';
 import { FacebookShare } from './interfaces/facebook-share.interface';
 import { TwitterShare } from './interfaces/twitter-share.interface';
@@ -14,18 +15,22 @@ export class SocialShareComponent {
   @Input() facebook: FacebookShare;
   @Input() twitter: TwitterShare;
   @Input() email: EmailShare;
+  @Output() socialMediaChannel: EventEmitter<string> = new EventEmitter();
 
   constructor(private socialShareService: SocialShareService) {}
 
   public facebookShare(): void {
     this.socialShareService.facebookShare(this.facebook.url);
+    this.socialMediaChannel.emit(SOCIAL_SHARE_CHANNELS.FACEBOOK);
   }
 
   public twitterShare(): void {
     this.socialShareService.twitterShare(this.twitter.url, this.twitter.text);
+    this.socialMediaChannel.emit(SOCIAL_SHARE_CHANNELS.TWITTER);
   }
 
   public emailShare(): void {
     this.socialShareService.emailShare(this.email.url, this.email.subject, this.email.message);
+    this.socialMediaChannel.emit(SOCIAL_SHARE_CHANNELS.EMAIL);
   }
 }
