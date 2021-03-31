7280b9a8ceaf26a861a47c017ce1f96aace5d0c0
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-03-31 16:09:36 +0200

    [WPA-1081]: Get keys generated automatically

--- scripts/i18n/add-missing-keys.js
+++ scripts/i18n/add-missing-keys.js
@@ -1,4 +1,28 @@
-const getNodeMessagesWithoutKeyname = () => []
+const fs = require('fs');
+const xmlParser = require('xml2json');
+
+const englishCopiesFileLocation = 'src/locale/messages.xmb';
+
+const getNodeMessagesWithoutKeyname = () => {
+  const nodesWithoutKeyname = [];
+  const rawEnglishCopiesFile = fs.readFileSync(englishCopiesFileLocation, 'utf8');
+  const rawMessagesBundle = xmlParser.toJson(rawEnglishCopiesFile);
+  const englishCopiesFileObject = JSON.parse(rawMessagesBundle);
+  const translations = englishCopiesFileObject.messagebundle.msg;
+
+  translations.forEach(translation => {
+    const id = translation.id
+    const isKeyIdGenerated = !isNaN(id) && !isNaN(parseFloat(id));
+
+    
+    if (isKeyIdGenerated) {
+      nodesWithoutKeyname.push(translation)
+    }
+  })
+
+  return nodesWithoutKeyname;
+}
+
 const getNewKeyName = () => {}
 const setNewKeyNameInHTML = () => {}
 const setNewKeyNameInCopies = () => {}
@@ -8,14 +32,14 @@ const main = () => {
   const nodeMessagesWithoutKeyname = getNodeMessagesWithoutKeyname();
 
   nodeMessagesWithoutKeyname.forEach((node, i) => {
-    // Get new key name
-    const newKeyName = getNewKeyName(node, i);
+    // // Get new key name
+    // const newKeyName = getNewKeyName(node, i);
 
-    // Set key in HTML file
-    setNewKeyNameInHTML(html, line, newKeyName);
+    // // Set key in HTML file
+    // setNewKeyNameInHTML(html, line, newKeyName);
 
-    // Replace old key for new one in copies
-    setNewKeyNameInCopies(node.id, newKeyName);
+    // // Replace old key for new one in copies
+    // setNewKeyNameInCopies(node.id, newKeyName);
   });
 }
 
