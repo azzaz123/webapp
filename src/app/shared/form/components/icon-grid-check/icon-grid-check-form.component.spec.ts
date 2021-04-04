69fdb9464cc99fbb729f847a126808ce62cbcd5b
Pablo Macias Moreira <pablo.macias@mati-dev.es>
2021-04-04 18:57:14 +0200

    [WEB-419]: [Testing] Added test for onChange when value changes

--- src/app/shared/form/components/icon-grid-check/icon-grid-check-form.component.spec.ts
+++ src/app/shared/form/components/icon-grid-check/icon-grid-check-form.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IconGridCheckFormComponent } from './icon-grid-check-form.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('IconGridCheckFormComponent', () => {
   let component: IconGridCheckFormComponent;
@@ -9,6 +10,7 @@ describe('IconGridCheckFormComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [IconGridCheckFormComponent],
+      imports: [HttpClientTestingModule],
     }).compileComponents();
   });
 
@@ -20,5 +22,15 @@ describe('IconGridCheckFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('when value changes', () => {
+    it('should call value change', () => {
+      spyOn(component, 'onChange');
+
+      component.writeValue(['test']);
+
+      expect(component.onChange).toHaveBeenCalledWith(['test']);
+    });
   });
 });
