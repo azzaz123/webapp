e2ba0d317719ea077f9d1e310952d5c4a383e9e1
Pablo Macias Moreira <pablo.macias@mati-dev.es>
2021-04-04 19:30:47 +0200

    [WEB-419]: [Testing] Added tests for selecting options

--- src/app/shared/form/components/icon-grid-check/icon-grid-check-form.component.spec.ts
+++ src/app/shared/form/components/icon-grid-check/icon-grid-check-form.component.spec.ts
@@ -2,21 +2,45 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IconGridCheckFormComponent } from './icon-grid-check-form.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { IconGridOption } from '@shared/form/components/icon-grid-check/interfaces/icon-grid-option';
+import { DebugElement } from '@angular/core';
+import { IconCheckComponent } from '@shared/form/components/icon-grid-check/icon-check/icon-check.component';
+import { By } from '@angular/platform-browser';
+import { IconCheckModule } from '@shared/form/components/icon-grid-check/icon-check/icon-check.module';
+import { CommonModule } from '@angular/common';
 
 describe('IconGridCheckFormComponent', () => {
   let component: IconGridCheckFormComponent;
   let fixture: ComponentFixture<IconGridCheckFormComponent>;
+  let debugElement: DebugElement;
+
+  const iconPredicate = By.directive(IconCheckComponent);
+  const options: IconGridOption[] = [
+    {
+      label: 'Test 1',
+      value: 'test_1',
+      icon: 'icon.svg',
+    },
+    {
+      label: 'Test 2',
+      value: 'test_2',
+      icon: 'icon.svg',
+    },
+  ];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [IconGridCheckFormComponent],
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, IconCheckModule, CommonModule],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(IconGridCheckFormComponent);
+    debugElement = fixture.debugElement;
     component = fixture.componentInstance;
+    component.options = options;
+    component.columns = 4;
     fixture.detectChanges();
   });
 
@@ -28,9 +52,87 @@ describe('IconGridCheckFormComponent', () => {
     it('should call value change', () => {
       spyOn(component, 'onChange');
 
-      component.writeValue(['test']);
+      clickIconCheck('Test 1');
 
-      expect(component.onChange).toHaveBeenCalledWith(['test']);
+      expect(component.onChange).toHaveBeenCalledWith(['test_1']);
     });
   });
+
+  describe('when option value is in value list', () => {
+    beforeEach(() => {
+      clickIconCheck('Test 1');
+      fixture.detectChanges();
+    });
+    it('should set option as active', () => {
+      expect(component.isValueActive('test_1')).toBeTruthy();
+    });
+  });
+
+  describe('when option is clicked', () => {
+    describe('and option was active', () => {
+      beforeEach(() => {
+        clickIconCheck('Test 1');
+        fixture.detectChanges();
+      });
+
+      it('should remove the option', () => {
+        let iconCheck = findIconCheck('Test 1');
+        expect(iconCheck.componentInstance.isActive).toBeTruthy();
+
+        clickIconCheck('Test 1');
+        fixture.detectChanges();
+
+        iconCheck = findIconCheck('Test 1');
+        expect(iconCheck.componentInstance.isActive).toBeFalsy();
+      });
+    });
+
+    describe('and option was not active', () => {
+      beforeEach(() => {
+        clickIconCheck('Test 1');
+        fixture.detectChanges();
+      });
+      describe('and is not multiselect', () => {
+        it('should overwrite the value list', () => {
+          let icon1 = findIconCheck('Test 1');
+          expect(icon1.componentInstance.isActive).toBeTruthy();
+
+          clickIconCheck('Test 2');
+          fixture.detectChanges();
+
+          icon1 = findIconCheck('Test 1');
+          const icon2 = findIconCheck('Test 2');
+          expect(icon1.componentInstance.isActive).toBeFalsy();
+          expect(icon2.componentInstance.isActive).toBeTruthy();
+        });
+      });
+      describe('and is multiselect', () => {
+        beforeEach(() => {
+          component.isMultiselect = true;
+          fixture.detectChanges();
+        });
+        it('should add to the value list', () => {
+          let icon1 = findIconCheck('Test 1');
+          expect(icon1.componentInstance.isActive).toBeTruthy();
+
+          clickIconCheck('Test 2');
+          fixture.detectChanges();
+
+          icon1 = findIconCheck('Test 1');
+          const icon2 = findIconCheck('Test 2');
+          expect(icon1.componentInstance.isActive).toBeTruthy();
+          expect(icon2.componentInstance.isActive).toBeTruthy();
+        });
+      });
+    });
+  });
+
+  function clickIconCheck(label: string): void {
+    const iconCheck = findIconCheck(label);
+    iconCheck.nativeElement.click();
+  }
+
+  function findIconCheck(label: string): DebugElement {
+    return debugElement.queryAll(iconPredicate).find((debug) => debug.componentInstance.label === label);
+  }
 });
