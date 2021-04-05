f8e04aaf3622c9edc1e0bf85a5c419eecf9b62a2
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-04-06 01:11:27 +0200

    [WEB-399]: Added trackViewOthersItemREDetailEvent to MockItemdDetailTrackEventService

--- src/app/public/features/item-detail/core/services/item-detail-track-events/track-events.fixtures.spec.ts
+++ src/app/public/features/item-detail/core/services/item-detail-track-events/track-events.fixtures.spec.ts
@@ -103,4 +103,5 @@ export class MockItemdDetailTrackEventService {
   trackClickChatButton() {}
   trackViewOwnItemDetail() {}
   trackViewOthersCGDetailEvent() {}
+  trackViewOthersItemREDetailEvent() {}
 }
