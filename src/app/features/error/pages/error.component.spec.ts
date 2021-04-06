178cecfbcaa64f7a5342bc485a99ed2db8e7d02d
Alex Masnou <alex.masnou@wallapop.com>
2021-04-06 20:59:25 +0200

    [WEB-191]: Add missing tests

--- src/app/features/error/pages/error.component.spec.ts
+++ src/app/features/error/pages/error.component.spec.ts
@@ -1,15 +1,20 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ErrorBoxModule } from '@shared/error-box/error-box.module';
+import { APP_PATHS } from 'app/app-routing-constants';
 import { ErrorComponent } from './error.component';
 
-describe('ErrorComponent', () => {
+fdescribe('ErrorComponent', () => {
   let component: ErrorComponent;
   let fixture: ComponentFixture<ErrorComponent>;
+  let router: Router;
 
   beforeEach(
     waitForAsync(() => {
       TestBed.configureTestingModule({
-        imports: [ErrorBoxModule],
+        imports: [ErrorBoxModule, RouterTestingModule],
         declarations: [ErrorComponent],
       }).compileComponents();
     })
@@ -18,10 +23,22 @@ describe('ErrorComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ErrorComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('when the exitClick event is fired', () => {
+    it('should redirect to the home page', () => {
+      const errorBoxElement = fixture.debugElement.query(By.css('tsl-error-box'));
+      spyOn(router, 'navigate');
+
+      errorBoxElement.triggerEventHandler('exitClick', {});
+
+      expect(router.navigate).toHaveBeenCalledWith([APP_PATHS.PUBLIC]);
+    });
   });
 });
--- src/app/shared/error-box/error-box.component.spec.ts
+++ src/app/shared/error-box/error-box.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { MOCK_ERROR_BOX_EXIT_BUTTON, MOCK_ERROR_BOX_EXIT_LINK } from '@fixtures/error-box.fixtures.spec';
+import { ButtonModule } from '@shared/button/button.module';
 import { ErrorBoxComponent } from './error-box.component';
 
 describe('ErrorBoxComponent', () => {
@@ -8,7 +10,7 @@ describe('ErrorBoxComponent', () => {
   beforeEach(
     waitForAsync(() => {
       TestBed.configureTestingModule({
-        imports: [],
+        imports: [ButtonModule],
         declarations: [ErrorBoxComponent],
       }).compileComponents();
     })
@@ -17,10 +19,27 @@ describe('ErrorBoxComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ErrorBoxComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('when the clickable element is a button', () => {
+    it('should have the appearance of a button', () => {
+      component.exit = MOCK_ERROR_BOX_EXIT_BUTTON;
+      const className = component.getButtonClassName();
+
+      expect(className).toEqual('btn-primary');
+    });
+  });
+
+  describe('when the clickable element is a link', () => {
+    it('should have the appearance of a link', () => {
+      component.exit = MOCK_ERROR_BOX_EXIT_LINK;
+      const className = component.getButtonClassName();
+
+      expect(className).toEqual('basic');
+    });
   });
 });
