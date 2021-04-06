6634dc787f41d2af2f08043963c9deb71070c485
Alex Masnou <alex.masnou@wallapop.com>
2021-04-06 16:22:30 +0200

    [WEB-191]: Import ButtonModule in ErrorBoxModule

--- src/app/shared/error-box/error-box.module.ts
+++ src/app/shared/error-box/error-box.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorBoxComponent } from './error-box.component';
+import { ButtonModule } from '@shared/button/button.module';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonModule],
   exports: [ErrorBoxComponent],
   declarations: [ErrorBoxComponent],
 })
