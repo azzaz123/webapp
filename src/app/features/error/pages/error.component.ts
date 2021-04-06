4378912f528b1f8003c6f55ddd460cca88ef71aa
Alex Masnou <alex.masnou@wallapop.com>
2021-04-06 16:46:56 +0200

    [WEB-191]: Add translated messages

--- src/app/features/error/pages/error.component.ts
+++ src/app/features/error/pages/error.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./error.component.scss'],
 })
 export class ErrorComponent {
+  public title = $localize`:@@NotFoundTitle:Whoops, the page you're looking for can't be found`;
+  public subtitle = $localize`:@@NotFoundMessage:Our office unicorn might have impaled it. But, there's lots to be discovered in the land of Wallapop.`;
 
   constructor(private router: Router) {}
 
