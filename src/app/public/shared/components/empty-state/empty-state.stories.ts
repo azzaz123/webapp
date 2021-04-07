b5b0f8f4227f6c043d4271907c7cb9a67fd0a4d5
Laia Lopez <laialopezz99@gmail.com>
2021-04-07 11:26:23 +0200

    [WEB-222]: [Style + Storybook] Empty state

---
+++ src/app/public/shared/components/empty-state/empty-state.stories.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { SvgIconModule } from '@core/svg-icon/svg-icon.module';
+import { Story, Meta } from '@storybook/angular/types-6-0';
+import { EmptyStateComponent } from './empty-state.component';
+
+export default {
+  title: 'Webapp/Public/Shared/Components/EmptyState',
+  component: EmptyStateComponent,
+} as Meta;
+
+const Template: Story<EmptyStateComponent> = (args: EmptyStateComponent) => ({
+  component: EmptyStateComponent,
+  props: args,
+  moduleMetadata: {
+    declarations: [EmptyStateComponent],
+    imports: [CommonModule, SvgIconModule, HttpClientModule],
+  },
+  template: '<tsl-empty-state [title]="title" [description]="description" [drawingPath]="drawingPath"></tsl-empty-state>',
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  title: 'Nadie ha opinado nada',
+  description: 'Esta persona no ha recibido valoraciones. ¿Será la tuya la primera?',
+  drawingPath: '/assets/images/dolls/balloon.svg',
+};
+
+export const WithoutDrawing = Template.bind({});
+WithoutDrawing.args = {
+  title: 'Nadie ha opinado nada',
+  description: 'Esta persona no ha recibido valoraciones. ¿Será la tuya la primera?',
+};
