8306eec216ef43622777cc3fb3d6107e7d71e82a
Alex Masnou <alex.masnou@wallapop.com>
2021-04-07 10:12:38 +0200

    [WEB-191]: Create pure pipe for handling exit button className

---
+++ src/app/shared/error-box/core/pipes/error-box-btn-class-name.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ErrorBoxBtnClassNamePipe } from './error-box-btn-class-name.pipe';
+
+describe('ErrorBoxBtnClassNamePipe', () => {
+  it('create an instance', () => {
+    const pipe = new ErrorBoxBtnClassNamePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
