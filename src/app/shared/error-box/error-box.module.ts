ed8cfdc5cae00bb2d7d13d32a9956da19d477434
Alex Masnou <alex.masnou@wallapop.com>
2021-04-07 10:14:24 +0200

    [WEB-191]: Add pipe to NgMOdule

--- src/app/shared/error-box/error-box.module.ts
+++ src/app/shared/error-box/error-box.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorBoxComponent } from './error-box.component';
 import { ButtonModule } from '@shared/button/button.module';
+import { ErrorBoxBtnClassNamePipe } from './core/pipes/error-box-btn-class-name.pipe';
 
 @NgModule({
   imports: [CommonModule, ButtonModule],
   exports: [ErrorBoxComponent],
-  declarations: [ErrorBoxComponent],
+  declarations: [ErrorBoxComponent, ErrorBoxBtnClassNamePipe],
 })
 export class ErrorBoxModule {}
