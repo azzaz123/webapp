c45af5ac334d202f256841e1f921b31f51bc8e3d
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-04-08 12:52:13 +0200

    [WEB-409]: Emit data of recommendedItemIds and engine to the parent component

--- src/app/public/features/item-detail/components/recommended-items/recommended-items.component.ts
+++ src/app/public/features/item-detail/components/recommended-items/recommended-items.component.ts
@@ -1,23 +1,21 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
-import { AnalyticsPageView, ANALYTICS_EVENT_NAMES, ViewItemDetailRecommendationSlider } from '@core/analytics/analytics-constants';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { ANALYTICS_EVENT_NAMES, AnalyticsPageView, ViewItemDetailRecommendationSlider } from '@core/analytics/analytics-constants';
 import { AnalyticsService } from '@core/analytics/analytics.service';
 import { Item } from '@core/item/item';
 import { User } from '@core/user/user';
+import { SEARCH_TECHNIQUE_ENGINE } from '@public/core/services/api/recommender/enums/recomender-type.enum';
 import { RecommendedItemsBodyResponse } from '@public/core/services/api/recommender/interfaces/recommender-response.interface';
 import { MapItemService } from '@public/features/public-profile/pages/user-published/services/map-item/map-item.service';
 import { ColumnsConfig } from '@public/shared/components/item-card-list/interfaces/cols-config.interface';
-import { userLocationReducer } from 'app/data/user/store/reducer/location.reducer';
 
 @Component({
   selector: 'tsl-recommended-items',
   templateUrl: './recommended-items.component.html',
   styleUrls: ['./recommended-items.component.scss'],
 })
-export class RecommendedItemsComponent implements OnChanges, OnInit {
+export class RecommendedItemsComponent implements OnInit {
   @Input() recommendedItems: RecommendedItemsBodyResponse;
-  @Input() itemSource: Item;
-  @Input() itemSourceUser: User;
-
+  @Output() initRecommendedItemsSlider: EventEmitter<{ recommendedItemIds: string; engine: SEARCH_TECHNIQUE_ENGINE }> = new EventEmitter();
   public items: Item[];
   public showDescription = false;
   public columnsConfig: ColumnsConfig = {
@@ -27,31 +25,24 @@ export class RecommendedItemsComponent implements OnChanges, OnInit {
     xs: 2,
   };
 
-  constructor(private mapItemService: MapItemService, private analyticsService: AnalyticsService) {}
+  constructor(private mapItemService: MapItemService) {}
 
-  ngOnInit() {}
-
-  ngOnChanges() {
+  ngOnInit() {
     if (this.recommendedItems) {
       this.items = this.mapItemService.mapRecommendedItem(this.recommendedItems).slice(0, 6);
-      // also send track event?
+      this.initRecommendedItemsSlider.emit({
+        recommendedItemIds: this.getRecommendedItemIds(this.items),
+        engine: this.getRecommendedItemSearchEngine(),
+      });
     }
   }
 
-  // Get recommendedItemIds
+  private getRecommendedItemIds(items: Item[]): string {
+    return items.map((item: Item) => item.id).toString();
+  }
 
-  private trackViewItemDetailRecommendationSliderEvent(): void {
-    const event: AnalyticsPageView<ViewItemDetailRecommendationSlider> = {
-      name: ANALYTICS_EVENT_NAMES.ViewItemDetailRecommendationSlider,
-      attributes: {
-        itemSourceId: this.itemSource.id,
-        categoryId: this.itemSource.categoryId,
-        engine: 'more_like_this_solr',
-        recommendedItemIds: null, // set at the moment
-        screenId: 115,
-        isPro: this.itemSourceUser.featured,
-      },
-    };
-    this.analyticsService.trackPageView(event);
+  private getRecommendedItemSearchEngine(): SEARCH_TECHNIQUE_ENGINE {
+    // do the logic
+    return SEARCH_TECHNIQUE_ENGINE.MORE_LIKE_THIS_SOLR;
   }
 }
