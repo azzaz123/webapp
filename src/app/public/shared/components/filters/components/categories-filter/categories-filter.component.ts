bc28d3d127a4bb4d0b6b6ead47d4cdb7baf92cb4
Pablo Macias Moreira <pablo.macias@mati-dev.es>
2021-04-09 12:11:46 +0200

    [WEB-426]: [Initialization] Set up subjects

--- src/app/public/shared/components/filters/components/categories-filter/categories-filter.component.ts
+++ src/app/public/shared/components/filters/components/categories-filter/categories-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AbstractFilter } from '@public/shared/components/filters/components/abstract-filter/abstract-filter';
 import { CategoriesFilterParams } from '@public/shared/components/filters/components/categories-filter/interfaces/categories-filter-params.interface';
 import { FILTER_VARIANT } from '@public/shared/components/filters/components/abstract-filter/abstract-filter.enum';
@@ -6,17 +6,41 @@ import { GridSelectFormOption } from '@shared/form/components/grid-select/interf
 import { FormControl, FormGroup } from '@angular/forms';
 import { SelectFormOption } from '@shared/form/components/select/interfaces/select-form-option.interface';
 import { CATEGORY_OPTIONS } from '@public/shared/components/filters/components/categories-filter/data/category_options';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { CategoriesFilterIcon } from '@public/shared/components/filters/components/categories-filter/interfaces/categories-filter-icon.interface';
+import { CategoriesFilterOption } from '@public/shared/components/filters/components/categories-filter/interfaces/categories-filter-option.interface';
 
 @Component({
   selector: 'tsl-categories-filter',
   templateUrl: './categories-filter.component.html',
   styleUrls: ['./categories-filter.component.scss'],
 })
-export class CategoriesFilterComponent extends AbstractFilter<CategoriesFilterParams> {
+export class CategoriesFilterComponent extends AbstractFilter<CategoriesFilterParams> implements OnInit {
   public VARIANT = FILTER_VARIANT;
   public formGroup = new FormGroup({
     select: new FormControl([]),
   });
+
+  private iconSubject = new BehaviorSubject('');
+  private placeholderIconSubject = new BehaviorSubject('');
+  private labelSubject = new BehaviorSubject('');
+
+  public get icon$(): Observable<string> {
+    return this.iconSubject.asObservable();
+  }
+
+  public get placeholderIcon$(): Observable<string> {
+    return this.placeholderIconSubject.asObservable();
+  }
+
+  public get label$(): Observable<string> {
+    return this.labelSubject.asObservable();
+  }
+
+  public ngOnInit() {
+    super.ngOnInit();
+    this.initializeSubjects();
+  }
 
   public getGridOptions(): GridSelectFormOption[] {
     return CATEGORY_OPTIONS;
@@ -26,11 +50,21 @@ export class CategoriesFilterComponent extends AbstractFilter<CategoriesFilterPa
     return [];
   }
 
-  public getLabel(): string {
-    return 'label';
+  private initializeSubjects(): void {
+    this.iconSubject.next(this.getCategoryIcon(null, 'standard'));
+    this.placeholderIconSubject.next(this.getCategoryIcon(null, 'standard'));
+    this.labelSubject.next(this.getCategoryLabel(null));
   }
 
-  public getIcon(): string {
-    return this.config.icon;
+  private getCategoryIcon(value: string | null, iconType: keyof CategoriesFilterIcon): string {
+    return this.getCategoryByValue(value).icon[iconType];
+  }
+
+  private getCategoryLabel(value: string | null): string {
+    return this.getCategoryByValue(value).label;
+  }
+
+  private getCategoryByValue(value: string | null): CategoriesFilterOption {
+    return CATEGORY_OPTIONS.find((option) => option.value === value);
   }
 }
---
+++ src/app/public/shared/components/filters/components/categories-filter/data/category_options.ts
@@ -0,0 +1,224 @@
+import { CategoriesFilterOption } from '@public/shared/components/filters/components/categories-filter/interfaces/categories-filter-option.interface';
+
+export const CATEGORY_OPTIONS: CategoriesFilterOption[] = [
+  {
+    label: $localize`:@@filtersCategoryAllCategories:All categories`,
+    value: null,
+    icon: {
+      standard: '/assets/icons/filters/categories/all.svg',
+      active: '/assets/icons/filters/categories/all_selected.svg',
+      hover: '/assets/icons/filters/categories/all_hover.svg',
+      stroke: '/assets/icons/filters/categories/all_stroke.svg',
+    },
+  },
+  {
+    label: $localize`:@@filtersCategoryCars:Cars`,
+    value: '100',
+    icon: {
+      standard: '/assets/icons/filters/categories/car.svg',
+      active: '/assets/icons/filters/categories/car_selected.svg',
+      hover: '/assets/icons/filters/categories/car_hover.svg',
+      stroke: '/assets/icons/filters/categories/car_stroke.svg',
+    },
+  },
+  {
+    label: $localize`:@@filtersCategoryMotorbike:Motorbike`,
+    value: '14000',
+    icon: {
+      standard: '/assets/icons/filters/categories/motorbike.svg',
+      active: '/assets/icons/filters/categories/motorbike_selected.svg',
+      hover: '/assets/icons/filters/categories/motorbike_hover.svg',
+      stroke: '/assets/icons/filters/categories/motorbike_stroke.svg',
+    },
+  },
+  {
+    label: $localize`:@@filtersCategoryMotorsNAccessories:Motors & Accessories`,
+    value: '12800',
+    icon: {
+      standard: '/assets/icons/filters/categories/helmet.svg',
+      active: '/assets/icons/filters/categories/helmet_selected.svg',
+      hover: '/assets/icons/filters/categories/helmet_hover.svg',
+      stroke: '/assets/icons/filters/categories/helmet_stroke.svg',
+    },
+  },
+  {
+    label: $localize`:@@filtersCategoryFashion:Fashion & Accessories`,
+    value: '12465',
+    icon: {
+      standard: '/assets/icons/filters/categories/t-shirt.svg',
+      active: '/assets/icons/filters/categories/t-shirt_selected.svg',
+      hover: '/assets/icons/filters/categories/t-shirt_hover.svg',
+      stroke: '/assets/icons/filters/categories/t-shirt_stroke.svg',
+    },
+  },
+  {
+    label: $localize`:@@filtersCategoryRealEstate:Real Estate`,
+    value: '200',
+    icon: {
+      standard: '/assets/icons/filters/categories/house.svg',
+      active: '/assets/icons/filters/categories/house_selected.svg',
+      hover: '/assets/icons/filters/categories/house_hover.svg',
+      stroke: '/assets/icons/filters/categories/house_stroke.svg',
+    },
+  },
+  {
+    label: $localize`:@@filtersCategoryTVAudioNCameras:TV, Audio & Cameras`,
+    value: '12545',
+    icon: {
+      standard: '/assets/icons/filters/categories/tv.svg',
+      active: '/assets/icons/filters/categories/tv_selected.svg',
+      hover: '/assets/icons/filters/categories/tv_hover.svg',
+      stroke: '/assets/icons/filters/categories/tv_stroke.svg',
+    },
+  },
+  {
+    label: $localize`:@@filtersCategoryCellPhonesNAccessories:Cell Phones & Accessories`,
+    value: '16000',
+    icon: {
+      standard: '/assets/icons/filters/categories/phone.svg',
+      active: '/assets/icons/filters/categories/phone_selected.svg',
+      hover: '/assets/icons/filters/categories/phone_hover.svg',
+      stroke: '/assets/icons/filters/categories/phone_stroke.svg',
+    },
+  },
+  {
+    label: $localize`:@@filtersCategoryComputersNElectronics:Computers & Electronics`,
+    value: '15000',
+    icon: {
+      standard: '/assets/icons/filters/categories/pc.svg',
+      active: '/assets/icons/filters/categories/pc_selected.svg',
+      hover: '/assets/icons/filters/categories/pc_hover.svg',
+      stroke: '/assets/icons/filters/categories/pc_stroke.svg',
+    },
+  },
+  {
+    label: $localize`:@@filtersCategorySportsNLeisure:Sports & Leisure`,
+    value: '12579',
+    icon: {
+      standard: '/assets/icons/filters/categories/ball.svg',
+      active: '/assets/icons/filters/categories/ball_selected.svg',
+      hover: '/assets/icons/filters/categories/ball_hover.svg',
+      stroke: '/assets/icons/filters/categories/ball_stroke.svg',
+    },
+  },
+  {
+    label: $localize`:@@filtersCategoryBikes:Bikes`,
+    value: '17000',
+    icon: {
+      standard: '/assets/icons/filters/categories/bike.svg',
+      active: '/assets/icons/filters/categories/bike_selected.svg',
+      hover: '/assets/icons/filters/categories/bike_hover.svg',
+      stroke: '/assets/icons/filters/categories/bike_stroke.svg',
+    },
+  },
+  {
+    label: $localize`:@@filtersCategoryGamesNConsoles:Games & Consoles`,
+    value: '12900',
+    icon: {
+      standard: '/assets/icons/filters/categories/gamepad.svg',
+      active: '/assets/icons/filters/categories/gamepad_selected.svg',
+      hover: '/assets/icons/filters/categories/gamepad_hover.svg',
+      stroke: '/assets/icons/filters/categories/gamepad_stroke.svg',
+    },
+  },
+  {
+    label: $localize`:@@filtersCategoryHomeNGarden:Home & Garden`,
+    value: '12467',
+    icon: {
+      standard: '/assets/icons/filters/categories/furniture.svg',
+      active: '/assets/icons/filters/categories/furniture_selected.svg',
+      hover: '/assets/icons/filters/categories/furniture_hover.svg',
+      stroke: '/assets/icons/filters/categories/furniture_stroke.svg',
+    },
+  },
+  {
+    label: $localize`:@@filtersCategoryAppliances:Appliances`,
+    value: '13100',
+    icon: {
+      standard: '/assets/icons/filters/categories/laundry.svg',
+      active: '/assets/icons/filters/categories/laundry_selected.svg',
+      hover: '/assets/icons/filters/categories/laundry_hover.svg',
+      stroke: '/assets/icons/filters/categories/laundry_stroke.svg',
+    },
+  },
+  {
+    label: $localize`:@@filtersCategoryMoviesBooksNMusic:Movies, Books & Music`,
+    value: '12463',
+    icon: {
+      standard: '/assets/icons/filters/categories/bookshelf.svg',
+      active: '/assets/icons/filters/categories/bookshelf_selected.svg',
+      hover: '/assets/icons/filters/categories/bookshelf_hover.svg',
+      stroke: '/assets/icons/filters/categories/bookshelf_stroke.svg',
+    },
+  },
+  {
+    label: $localize`:@@filtersCategoryBabyNChild:Baby & Child`,
+    value: '12461',
+    icon: {
+      standard: '/assets/icons/filters/categories/baby_car.svg',
+      active: '/assets/icons/filters/categories/baby_car_selected.svg',
+      hover: '/assets/icons/filters/categories/baby_car_hover.svg',
+      stroke: '/assets/icons/filters/categories/baby_car_stroke.svg',
+    },
+  },
+  {
+    label: $localize`:@@filtersCategoryCollectiblesNArt:Collectibles & Art`,
+    value: '18000',
+    icon: {
+      standard: '/assets/icons/filters/categories/collecting.svg',
+      active: '/assets/icons/filters/categories/collecting_selected.svg',
+      hover: '/assets/icons/filters/categories/collecting_hover.svg',
+      stroke: '/assets/icons/filters/categories/collecting_stroke.svg',
+    },
+  },
+  {
+    label: $localize`:@@filtersCategoryBuildingMaterials:Building Materials`,
+    value: '19000',
+    icon: {
+      standard: '/assets/icons/filters/categories/building.svg',
+      active: '/assets/icons/filters/categories/building_selected.svg',
+      hover: '/assets/icons/filters/categories/building_hover.svg',
+      stroke: '/assets/icons/filters/categories/building_stroke.svg',
+    },
+  },
+  {
+    label: $localize`:@@filtersCategoryIndustrialNAgriculture:Industrial & Agriculture`,
+    value: '20000',
+    icon: {
+      standard: '/assets/icons/filters/categories/farming.svg',
+      active: '/assets/icons/filters/categories/farming_selected.svg',
+      hover: '/assets/icons/filters/categories/farming_hover.svg',
+      stroke: '/assets/icons/filters/categories/farming_stroke.svg',
+    },
+  },
+  {
+    label: $localize`:@@filtersCategoryJobs:Jobs`,
+    value: '21000',
+    icon: {
+      standard: '/assets/icons/filters/categories/jobs.svg',
+      active: '/assets/icons/filters/categories/jobs_selected.svg',
+      hover: '/assets/icons/filters/categories/jobs_hover.svg',
+      stroke: '/assets/icons/filters/categories/jobs_stroke.svg',
+    },
+  },
+  {
+    label: $localize`:@@filtersCategoryServices:Services`,
+    value: '13200',
+    icon: {
+      standard: '/assets/icons/filters/categories/toolbox.svg',
+      active: '/assets/icons/filters/categories/toolbox_selected.svg',
+      hover: '/assets/icons/filters/categories/toolbox_hover.svg',
+      stroke: '/assets/icons/filters/categories/toolbox_stroke.svg',
+    },
+  },
+  {
+    label: $localize`:@@filtersCategoryOther:Other`,
+    value: '12485',
+    icon: {
+      standard: '/assets/icons/filters/categories/ghost.svg',
+      active: '/assets/icons/filters/categories/ghost_selected.svg',
+      hover: '/assets/icons/filters/categories/ghost_hover.svg',
+      stroke: '/assets/icons/filters/categories/ghost_stroke.svg',
+    },
+  },
+];
