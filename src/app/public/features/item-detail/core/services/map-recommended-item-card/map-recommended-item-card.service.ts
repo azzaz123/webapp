8cad5c98db6b673b5795b0018d948285e33200a3
Laia Lopez <laialopezz99@gmail.com>
2021-04-09 13:13:41 +0200

    [WEB-240]: [Service] mapRecommendedItemsFavoriteCheck

--- src/app/public/features/item-detail/core/services/map-recommended-item-card/map-recommended-item-card.service.ts
+++ src/app/public/features/item-detail/core/services/map-recommended-item-card/map-recommended-item-card.service.ts
@@ -3,10 +3,28 @@ import { Image } from '@core/user/user-response.interface';
 import { UuidService } from '@core/uuid/uuid.service';
 import { ItemCard } from '@public/core/interfaces/item-card.interface';
 import { RecommenderItem, RecommenderItemImage } from '@public/core/services/api/recommender/interfaces/recommender-item.interface';
+import { ItemFavoritesService } from '@public/core/services/item-favorites/item-favorites.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class MapRecommendedItemCardService {
-  constructor(private uuidService: UuidService) {}
+  constructor(private uuidService: UuidService, private itemFavoritesService: ItemFavoritesService) {}
+
+  public mapRecommendedItemsFavoriteCheck(recommendedItems: RecommenderItem[]): Observable<ItemCard[]> {
+    const itemsId = recommendedItems.map((item: RecommenderItem) => item.id);
+    return this.itemFavoritesService.getFavouritedItemIds(itemsId).pipe(
+      map((favoriteIds: string[]) => {
+        const itemsFavouriteChecked = recommendedItems?.map((item: RecommenderItem) => {
+          if (favoriteIds.includes(item.id)) {
+            item.favorited = true;
+          }
+          return item;
+        });
+        return this.mapRecommendedItems(itemsFavouriteChecked);
+      })
+    );
+  }
 
   public mapRecommendedItems(recommendedItems: RecommenderItem[]): ItemCard[] {
     return recommendedItems?.map((recommendedItem) => this.mapRecommendedItem(recommendedItem));
