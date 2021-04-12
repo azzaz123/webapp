82f1926572691113d90587c816279a0b9e2a5982
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-04-12 12:49:05 +0200

    [WEB-434]: [Testing]: Add metadata

--- src/app/public/features/login/core/services/login.service.spec.ts
+++ src/app/public/features/login/core/services/login.service.spec.ts
@@ -16,6 +16,9 @@ describe('LoginService', () => {
   let accessTokenService: AccessTokenService;
   let httpMock: HttpTestingController;
   let eventService: EventService;
+  let deviceService: DeviceService;
+
+  const MOCK_DEVICE_ID = 'lel123';
 
   beforeEach(() => {
     injector = getTestBed();
@@ -37,7 +40,10 @@ describe('LoginService', () => {
     service = injector.inject(LoginService);
     accessTokenService = injector.inject(AccessTokenService);
     eventService = injector.inject(EventService);
+    deviceService = injector.inject(DeviceService);
     httpMock = injector.inject(HttpTestingController);
+
+    spyOn(deviceService, 'getDeviceId').and.returnValue(MOCK_DEVICE_ID);
   });
 
   afterEach(() => {
@@ -56,12 +62,21 @@ describe('LoginService', () => {
   describe('when requesting login', () => {
     describe('and when request is successful', () => {
       it('should ask server for response', () => {
+        const expectedBody: LoginRequest = {
+          ...MOCK_LOGIN_REQUEST,
+          metadata: {
+            installationId: MOCK_DEVICE_ID,
+            installationType: 'WEB',
+            pushToken: '',
+          },
+        };
+
         service.login(MOCK_LOGIN_REQUEST).subscribe();
         const req = httpMock.expectOne(`${environment.baseUrl}${LOGIN_ENDPOINT}`);
         req.flush(MOCK_USER_RESPONSE_BODY);
 
         expect(req.request.method).toBe('POST');
-        expect(req.request.body).toBe(MOCK_LOGIN_REQUEST);
+        expect(req.request.body).toEqual(expectedBody);
       });
 
       it('should emit login event', () => {
