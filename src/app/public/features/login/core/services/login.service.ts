1f54364f0329fa67eaa7704678c8c4f34ec588df
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-04-12 12:22:40 +0200

    [WEB-434]: Use v3 login endpoint

--- src/app/public/features/login/core/services/login.service.ts
+++ src/app/public/features/login/core/services/login.service.ts
@@ -1,38 +1,41 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { DeviceService } from '@core/device/device.service';
 import { EventService } from '@core/event/event.service';
 import { AccessTokenService } from '@core/http/access-token.service';
 import { environment } from '@environments/environment';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { LoginResponse } from '../login-response.interface';
+import { tap } from 'rxjs/operators';
+import { AccessMetadata } from '../interfaces/access-metadata';
+import { LoginResponse } from '../interfaces/login.response';
 
-export const LOGIN_ENDPOINT = 'shnm-portlet/api/v1/access.json/login3';
+export const LOGIN_ENDPOINT = 'api/v3/users/access/login';
 
 @Injectable()
 export class LoginService {
-  constructor(private httpClient: HttpClient, private eventService: EventService, private accessTokenService: AccessTokenService) {}
+  constructor(
+    private httpClient: HttpClient,
+    private eventService: EventService,
+    private accessTokenService: AccessTokenService,
+    private deviceService: DeviceService
+  ) {}
 
-  public login(data: any): Observable<LoginResponse> {
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/x-www-form-urlencoded',
-    });
-    const body = new HttpParams()
-      .set('emailAddress', data.emailAddress)
-      .set('installationType', data.installationType)
-      .set('password', data.password);
-
-    return this.httpClient
-      .post<LoginResponse>(`${environment.baseUrl}${LOGIN_ENDPOINT}`, body, {
-        headers,
-      })
-      .pipe(map((r) => this.storeData(r)));
+  public login(body: any): Observable<LoginResponse> {
+    body.metadata = this.getMetadata();
+    return this.httpClient.post<LoginResponse>(`${environment.baseUrl}${LOGIN_ENDPOINT}`, body).pipe(tap((r) => this.storeData(r)));
   }
 
-  private storeData(data: LoginResponse): LoginResponse {
+  private getMetadata(): AccessMetadata {
+    return {
+      installationId: this.deviceService.getDeviceId(),
+      installationType: 'WEB',
+      pushToken: '',
+    };
+  }
+
+  private storeData(data: LoginResponse): void {
     const { token } = data;
     this.accessTokenService.storeAccessToken(token);
     this.eventService.emit(EventService.USER_LOGIN, token);
-    return data;
   }
 }
