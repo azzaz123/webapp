07cc47c24500e75640f394a010e73e1c6d811281
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-04-12 09:16:52 +0200

    [WEB-432]: Tested trackFavoriteOrUnfavoriteEvent

--- src/app/public/features/item-detail/components/recommended-items/recommended-items.component.spec.ts
+++ src/app/public/features/item-detail/components/recommended-items/recommended-items.component.spec.ts
@@ -1,21 +1,37 @@
 import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { MOCK_ITEM_INDEX } from '../../core/services/item-detail-track-events/track-events.fixtures.spec';
+import { MockItemdDetailTrackEventService, MOCK_ITEM_INDEX } from '../../core/services/item-detail-track-events/track-events.fixtures.spec';
 import { RECOMMENDED_ITEM_MOCK } from './constants/recommended-items.fixtures.spec';
 import { MapRecommendedItemCardService } from '../../core/services/map-recommended-item-card/map-recommended-item-card.service';
 import { RecommendedItemsComponent } from './recommended-items.component';
 import { MOCK_ITEM_CARD } from '@fixtures/item-card.fixtures.spec';
+import { ItemDetailTrackEventsService } from '../../core/services/item-detail-track-events/item-detail-track-events.service';
+import { UserService } from '@core/user/user.service';
+import { MockedUserService, MOCK_USER } from '@fixtures/user.fixtures.spec';
+import { of } from 'rxjs';
 
 describe('RecommendedItemsComponent', () => {
   const itemCardListTag = 'tsl-public-item-card-list';
   let component: RecommendedItemsComponent;
   let fixture: ComponentFixture<RecommendedItemsComponent>;
+  let itemDetailTrackEventsService: ItemDetailTrackEventsService;
+  let userService: UserService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [RecommendedItemsComponent],
-      providers: [MapRecommendedItemCardService],
+      providers: [
+        MapRecommendedItemCardService,
+        {
+          provide: ItemDetailTrackEventsService,
+          useClass: MockItemdDetailTrackEventService,
+        },
+        {
+          provide: UserService,
+          useClass: MockedUserService,
+        },
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   });
@@ -23,6 +39,8 @@ describe('RecommendedItemsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(RecommendedItemsComponent);
     component = fixture.componentInstance;
+    itemDetailTrackEventsService = TestBed.inject(ItemDetailTrackEventsService);
+    userService = TestBed.inject(UserService);
     fixture.detectChanges();
   });
 
@@ -81,6 +99,17 @@ describe('RecommendedItemsComponent', () => {
         fixture.detectChanges();
 
         expect(component.clickedItemAndIndexEvent.emit).toHaveBeenCalledWith({ itemCard: MOCK_ITEM_CARD, index: MOCK_ITEM_INDEX });
+      });
+
+      it('should send favorite or unfavorite event if we favorite or unfavorite the item card in the slider', () => {
+        const itemCard: DebugElement = fixture.debugElement.query(By.css(itemCardListTag));
+        spyOn(itemDetailTrackEventsService, 'trackFavoriteOrUnfavoriteEvent');
+        spyOn(userService, 'get').and.returnValue(of(MOCK_USER));
+
+        itemCard.triggerEventHandler('toggleFavoriteEvent', MOCK_ITEM_CARD);
+        fixture.detectChanges();
+
+        expect(itemDetailTrackEventsService.trackFavoriteOrUnfavoriteEvent).toHaveBeenCalledWith(MOCK_ITEM_CARD, false, true);
       });
     });
   });
