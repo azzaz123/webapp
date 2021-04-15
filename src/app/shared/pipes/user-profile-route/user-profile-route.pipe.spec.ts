5fa3c2232f5c8b4f50d081b95f9184679005e12c
Alex Masnou <alex.masnou@wallapop.com>
2021-04-15 17:36:56 +0200

    [WEB-444]: Add missing tests

--- src/app/shared/pipes/user-profile-route/user-profile-route.pipe.spec.ts
+++ src/app/shared/pipes/user-profile-route/user-profile-route.pipe.spec.ts
@@ -1,8 +1,20 @@
+import { PUBLIC_PATHS } from '@public/public-routing-constants';
+import { APP_PATHS } from 'app/app-routing-constants';
 import { UserProfileRoutePipe } from './user-profile-route.pipe';
 
+const slugName = 'user-slug-';
+const userId = '19931006';
+const slug = `${slugName}${userId}`;
+const hashId = 'ams1993jk';
+
 describe('UserProfileRoutePipe', () => {
-  it('create an instance', () => {
+  it('should convert the user slug into a valid user profile route', () => {
     const pipe = new UserProfileRoutePipe();
-    expect(pipe).toBeTruthy();
+    const expectedPermalink = `${slugName}${hashId}`;
+    const expectedRoute = `/${APP_PATHS.PUBLIC}/${PUBLIC_PATHS.USER_DETAIL}/${expectedPermalink}`;
+
+    const userProfileRoute = pipe.transform(slug, hashId);
+
+    expect(userProfileRoute).toEqual(expectedRoute);
   });
 });
