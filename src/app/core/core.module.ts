8599d51600cb255775f0850e5957aa35dc5fbba2
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-04-15 15:14:09 +0200

    [WPA-1161]: Rename moment calendar spec service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -30,7 +30,7 @@ import { ExternalCommsService } from './external-comms.service';
 import { ReleaseVersionService } from './release-version/release-version.service';
 import { CorePipesModule } from './pipes/core-pipes.module';
 import { SlugsUtilService } from './services/slugs-util/slugs-util.service';
-import { MomentCalendarSpecsService } from './i18n/moment/moment-calendar-spec.service';
+import { MomentCalendarSpecService } from './i18n/moment/moment-calendar-spec.service';
 
 @NgModule({
   imports: [
@@ -67,7 +67,7 @@ import { MomentCalendarSpecsService } from './i18n/moment/moment-calendar-spec.s
     ReleaseVersionService,
     ExternalCommsService,
     SlugsUtilService,
-    MomentCalendarSpecsService,
+    MomentCalendarSpecService,
   ],
 })
 export class CoreModule {
