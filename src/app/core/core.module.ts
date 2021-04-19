420ba81587d5c5e0cc1df45b5b35b960def275aa
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-04-19 11:53:39 +0200

    [WPA-1162]: Add report service to core

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -30,6 +30,7 @@ import { ExternalCommsService } from './external-comms.service';
 import { ReleaseVersionService } from './release-version/release-version.service';
 import { CorePipesModule } from './pipes/core-pipes.module';
 import { SlugsUtilService } from './services/slugs-util/slugs-util.service';
+import { ReportService } from './trust-and-safety/report/report.service';
 
 @NgModule({
   imports: [
@@ -66,6 +67,7 @@ import { SlugsUtilService } from './services/slugs-util/slugs-util.service';
     ReleaseVersionService,
     ExternalCommsService,
     SlugsUtilService,
+    ReportService,
   ],
 })
 export class CoreModule {
