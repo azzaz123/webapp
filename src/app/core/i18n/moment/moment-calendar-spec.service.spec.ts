36c7b2c40ab0665f67024eff6623557b61584b35
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-04-20 12:51:21 +0200

    [WPA-1161]: Add tests for all calendar spec types

--- src/app/core/i18n/moment/moment-calendar-spec.service.spec.ts
+++ src/app/core/i18n/moment/moment-calendar-spec.service.spec.ts
@@ -1,7 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 import { I18nService } from '../i18n.service';
-import { defaultCalendarSpec as defaultEnglishCalendarSpec } from './constants/calendar-specs.en';
-import { defaultCalendarSpec as defaultSpanishCalendarSpec } from './constants/calendar-specs.es';
+import {
+  daysCalendarConfig as daysEnglishCalendarSpec,
+  defaultCalendarSpec as defaultEnglishCalendarSpec,
+  shortCalendarConfig as shortEnglishCalendarSpec,
+} from './constants/calendar-specs.en';
+import {
+  defaultCalendarSpec as defaultSpanishCalendarSpec,
+  daysCalendarConfig as daysSpanishCalendarSpec,
+  shortCalendarConfig as shortSpanishCalendarSpec,
+} from './constants/calendar-specs.es';
+import { CALENDAR_SPEC_TYPE } from './enums/calendar-spec-type.enum';
 import { MomentCalendarSpecService } from './moment-calendar-spec.service';
 
 describe('MomentCalendarSpecService', () => {
@@ -19,11 +28,27 @@ describe('MomentCalendarSpecService', () => {
   describe('when the webapp is in Spanish', () => {
     beforeEach(() => jest.spyOn(i18nService, 'locale', 'get').mockReturnValue('es'));
 
-    describe('and when asking for date format', () => {
+    describe('and when asking for default date format', () => {
       it('should get the Spanish localized date format', () => {
         const result = service.getCalendarSpec();
 
         expect(result).toBe(defaultSpanishCalendarSpec);
+      });
+    });
+
+    describe('and when asking for days date format', () => {
+      it('should get the Spanish localized date format', () => {
+        const result = service.getCalendarSpec(CALENDAR_SPEC_TYPE.DAYS);
+
+        expect(result).toBe(daysSpanishCalendarSpec);
+      });
+    });
+
+    describe('and when asking for short date format', () => {
+      it('should get the Spanish localized date format', () => {
+        const result = service.getCalendarSpec(CALENDAR_SPEC_TYPE.SHORT);
+
+        expect(result).toBe(shortSpanishCalendarSpec);
       });
     });
   });
@@ -31,11 +56,27 @@ describe('MomentCalendarSpecService', () => {
   describe('when the webapp is in English', () => {
     beforeEach(() => jest.spyOn(i18nService, 'locale', 'get').mockReturnValue('en'));
 
-    describe('and when asking for date format', () => {
+    describe('and when asking for default date format', () => {
       it('should get the English localized date format', () => {
         const result = service.getCalendarSpec();
 
         expect(result).toBe(defaultEnglishCalendarSpec);
+      });
+    });
+
+    describe('and when asking for days date format', () => {
+      it('should get the Spanish localized date format', () => {
+        const result = service.getCalendarSpec(CALENDAR_SPEC_TYPE.DAYS);
+
+        expect(result).toBe(daysEnglishCalendarSpec);
+      });
+    });
+
+    describe('and when asking for short date format', () => {
+      it('should get the Spanish localized date format', () => {
+        const result = service.getCalendarSpec(CALENDAR_SPEC_TYPE.SHORT);
+
+        expect(result).toBe(shortEnglishCalendarSpec);
       });
     });
   });
@@ -43,7 +84,7 @@ describe('MomentCalendarSpecService', () => {
   describe('when the webapp is in a language that is not supported by this service', () => {
     beforeEach(() => jest.spyOn(i18nService, 'locale', 'get').mockReturnValue('jp'));
 
-    describe('and when asking for date format', () => {
+    describe('and when asking for default date format', () => {
       it('should get the English localized date format', () => {
         const result = service.getCalendarSpec();
 
