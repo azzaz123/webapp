558e6e0725a6cf8d95db95b95f86401991bbd4a9
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-04-20 17:33:34 +0200

    [WPA-1163]: Add subscription benefits English translations

--- src/app/core/subscriptions/subscription-benefits/constants/subscription-benefits.ts
+++ src/app/core/subscriptions/subscription-benefits/constants/subscription-benefits.ts
@@ -5,22 +5,22 @@ export const SUBSCRIPTION_IMAGES_FOLDER = 'assets/images/subscriptions/';
 export const subscriptionBenefits: SubscriptionBenefit[] = [
   {
     iconPath: `${SUBSCRIPTION_IMAGES_FOLDER}img-1.svg`,
-    title: 'Empower your brand',
-    description: 'Complete your profile with a cover photo and a description.',
+    title: $localize`:@@web_subscription_benefit_title_your_brand:Empower your brand`,
+    description: $localize`:@@web_subscription_benefit_description_your_brand:Complete your profile with a cover photo and a description.`,
   },
   {
     iconPath: `${SUBSCRIPTION_IMAGES_FOLDER}img-4.svg`,
-    title: 'Connect with more customers',
-    description: 'Share your phone number and website with millions of people.',
+    title: $localize`:@@web_subscription_benefit_title_connect:Connect with more customers`,
+    description: $localize`:@@web_subscription_benefit_description_connect:Share your phone number and website with millions of people.`,
   },
   {
     iconPath: `${SUBSCRIPTION_IMAGES_FOLDER}img-3.svg`,
-    title: 'Gain more visibility',
-    description: 'Appear at the top of search results and attract more customers.',
+    title: $localize`:@@web_subscription_benefit_title_visibility:Gain more visibility`,
+    description: $localize`:@@web_subscription_benefit_description_visibility:Appear at the top of search results and attract more customers.`,
   },
   {
     iconPath: `${SUBSCRIPTION_IMAGES_FOLDER}img-2.svg`,
-    title: 'Save management time',
-    description: 'Your products will always be active and ready for sale.',
+    title: $localize`:@@web_subscription_benefit_title_time:Save management time`,
+    description: $localize`:@@web_subscription_benefit_description_time:Your products will always be active and ready for sale.`,
   },
 ];
