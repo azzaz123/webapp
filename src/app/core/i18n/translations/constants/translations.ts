0c6336cc9778ca5eed21c45ba8251103f56b9a12
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-04-21 21:19:23 +0200

    [WPA-1164]: Move desktop notification title to translations constant

--- src/app/core/i18n/translations/constants/translations.ts
+++ src/app/core/i18n/translations/constants/translations.ts
@@ -3,4 +3,5 @@ import { Translations } from '../interfaces/translations.interface';
 
 export const translations: Translations = {
   [TRANSLATION_KEY.CONSUMER_GOODS_GENERAL_CATEGORY_TITLE]: $localize`:@@web_consumer_goods_category_name:Everything else`,
+  [TRANSLATION_KEY.CHAT_DESKTOP_NOTIFICATION_TITLE]: $localize`:@@web_chat_desktop_notification:New message from`,
 };
