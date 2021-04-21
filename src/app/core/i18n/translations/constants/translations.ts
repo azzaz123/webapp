586ab114a9b3be0fcfb6c81bc8ef47a674c236d1
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-04-21 21:32:30 +0200

    [WPA-1164]: Move my phone number message to translations constant

--- src/app/core/i18n/translations/constants/translations.ts
+++ src/app/core/i18n/translations/constants/translations.ts
@@ -4,4 +4,5 @@ import { Translations } from '../interfaces/translations.interface';
 export const translations: Translations = {
   [TRANSLATION_KEY.CONSUMER_GOODS_GENERAL_CATEGORY_TITLE]: $localize`:@@web_consumer_goods_category_name:Everything else`,
   [TRANSLATION_KEY.CHAT_DESKTOP_NOTIFICATION_TITLE]: $localize`:@@web_chat_desktop_notification:New message from`,
+  [TRANSLATION_KEY.CHAT_MY_PHONE_NUMBER]: $localize`:@@web_chat_my_phone_number:My phone number is`,
 };
