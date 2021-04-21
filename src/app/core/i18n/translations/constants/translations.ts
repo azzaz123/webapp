0d6ff043017e2c982a7b6cc9ffee05f3953ddedc
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-04-21 21:12:27 +0200

    [WPA-1164]: Move consumer goods category name to translations constant

--- src/app/core/i18n/translations/constants/translations.ts
+++ src/app/core/i18n/translations/constants/translations.ts
@@ -1,4 +1,6 @@
 import { TRANSLATION_KEY } from '../enum/translation-keys.enum';
 import { Translations } from '../interfaces/translations.interface';
 
-export const translations: Translations = {};
+export const translations: Translations = {
+  [TRANSLATION_KEY.CONSUMER_GOODS_GENERAL_CATEGORY_TITLE]: $localize`:@@web_consumer_goods_category_name:Everything else`,
+};
