520511d88fca9d5c0823db8b9d55d30697b1a5c3
Pablo Macias Moreira <pablo.macias@mati-dev.es>
2021-04-21 17:09:07 +0200

    [WEB-453]: Forced type for filter configurations

--- src/app/public/shared/components/filters/components/toggle-filter/interfaces/toggle-filter-config.interface.ts
+++ src/app/public/shared/components/filters/components/toggle-filter/interfaces/toggle-filter-config.interface.ts
@@ -1,4 +1,7 @@
 import { FilterConfig } from '@public/shared/components/filters/interfaces/filter-config.interface';
 import { ToggleFilterParams } from './toggle-filter-params.interface';
+import { FILTER_TYPES } from '@public/shared/components/filters/core/enums/filter-types/filter-types.enum';
 
-export interface ToggleFilterConfig extends FilterConfig<ToggleFilterParams> {}
+export interface ToggleFilterConfig extends FilterConfig<ToggleFilterParams> {
+  type: FILTER_TYPES.TOGGLE;
+}
