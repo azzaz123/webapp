6af675182460f515e20dae3d561f220fc9e54217
Laia Lopez <laialopezz99@gmail.com>
2021-04-22 18:04:31 +0200

    [WEB-1273]: [DeliveryRoutingModule] KYCModule

--- src/app/private/features/delivery/delivery.routing.module.ts
+++ src/app/private/features/delivery/delivery.routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
+import { DELIVERY_PATHS } from './delivery-routing-constants';
 import { DeliveryComponent } from './pages/delivery.component';
+import { KYCModule } from './pages/kyc/kyc.module';
 
 const routes: Route[] = [
   {
     path: '',
     component: DeliveryComponent,
+    children: [
+      {
+        path: DELIVERY_PATHS.KYC,
+        loadChildren: () => KYCModule,
+      },
+    ],
   },
 ];
 
