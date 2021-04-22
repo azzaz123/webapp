42b5947cfbc02af3021baf9011f1b04fe80b7a11
Laia Lopez <laialopezz99@gmail.com>
2021-04-22 17:11:31 +0200

    [WPA-1271]: [DeliveryRoutingModule] AcceptScreen added

--- src/app/private/features/delivery/delivery.routing.module.ts
+++ src/app/private/features/delivery/delivery.routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
+import { DELIVERY_PATHS } from './delivery-routing-constants';
 import { DeliveryComponent } from './pages/delivery.component';
+import { DeliveryAcceptModule } from './pages/delivery-accept/delivery-accept.module';
 
 const routes: Route[] = [
   {
     path: '',
     component: DeliveryComponent,
+    children: [
+      {
+        path: DELIVERY_PATHS.ACCEPT_SCREEN,
+        loadChildren: () => DeliveryAcceptModule,
+      },
+    ],
   },
 ];
 
@@ -17,4 +25,4 @@ export class DeliveryRoutingModule {}
 
 export const deliveryRoutedComponents = [DeliveryComponent];
 
-export const deliveryRoutedModules = [];
+export const deliveryRoutedModules = [DeliveryAcceptModule];
