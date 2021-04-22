ab3e3b69327d9c31857f5a5c74d10ca351ceac0b
Laia Lopez <laialopezz99@gmail.com>
2021-04-22 15:29:14 +0200

    [WPA-1275]: [Comments] DeliveryRoutingModule File Rename

--- src/app/private/features/delivery/delivery.module.ts
+++ src/app/private/features/delivery/delivery.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { deliveryRoutedComponents, deliveryRoutedModules, DeliveryRoutingModule } from './delivery.routes';
+import { deliveryRoutedComponents, deliveryRoutedModules, DeliveryRoutingModule } from './delivery-routing.module';
 
 @NgModule({
   declarations: [deliveryRoutedComponents],
