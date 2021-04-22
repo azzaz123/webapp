76b3c64acea30f7890b66e62ee9288cf2a6a5153
Laia Lopez <laialopezz99@gmail.com>
2021-04-22 17:48:21 +0200

    [WPA-1272]: [DeliveryRoutingModule] TTS

--- src/app/private/features/delivery/delivery.routing.module.ts
+++ src/app/private/features/delivery/delivery.routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
+import { DELIVERY_PATHS } from './delivery-routing-constants';
 import { DeliveryComponent } from './pages/delivery.component';
+import { TrackingTimelineModule } from './pages/tracking-timeline/tracking-timeline.module';
 
 const routes: Route[] = [
   {
     path: '',
     component: DeliveryComponent,
+    children: [
+      {
+        path: DELIVERY_PATHS.TIMELINE,
+        loadChildren: () => TrackingTimelineModule,
+      },
+    ],
   },
 ];
 
