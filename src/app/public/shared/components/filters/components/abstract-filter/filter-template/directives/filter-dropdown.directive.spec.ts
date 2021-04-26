b5b23fa64648b72e7d34168a1e07f3d2e5482fef
Alex Masnou <alex.masnou@wallapop.com>
2021-04-26 12:08:34 +0200

    [WEB-466]: WIP dropdown directive tests

--- src/app/public/shared/components/filters/components/abstract-filter/filter-template/directives/filter-dropdown.directive.spec.ts
+++ src/app/public/shared/components/filters/components/abstract-filter/filter-template/directives/filter-dropdown.directive.spec.ts
@@ -1,8 +1,15 @@
+import { ElementRef } from '@angular/core';
 import { FilterDropdownDirective } from './filter-dropdown.directive';
+
+export class MockElementRef extends ElementRef {
+  constructor() {
+    super(null);
+  }
+}
 
 describe('FilterDropdownDirective', () => {
   it('should create an instance', () => {
-    const directive = new FilterDropdownDirective();
+    const directive = new FilterDropdownDirective(new MockElementRef());
     expect(directive).toBeTruthy();
   });
 });
