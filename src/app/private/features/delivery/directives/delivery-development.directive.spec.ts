63886df0fd7caec9261cbfde124593285f567b24
Laia Lopez <laialopezz99@gmail.com>
2021-04-27 13:16:38 +0200

    [WPA-1319]: [Rename + WIP testing] DeliveryDevelopmentDirective

---
+++ src/app/private/features/delivery/directives/delivery-development.directive.spec.ts
@@ -0,0 +1,62 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Component, TemplateRef, ViewContainerRef } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { DeliveryDevelopmentDirective } from './delivery-development.directive';
+import { FeatureflagService } from '@core/user/featureflag.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FeatureFlagServiceMock } from '@fixtures/feature-flag.fixtures.spec';
+
+@Component({
+  template: `<span *tslDeliveryDevelopment></span>`,
+})
+class TestComponent {
+  constructor() {}
+}
+
+describe('DeliveryDevelopmentDirective', () => {
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+  let featureflagService: FeatureflagService;
+
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule],
+        declarations: [DeliveryDevelopmentDirective, TestComponent],
+        providers: [TemplateRef, ViewContainerRef, FeatureFlagServiceMock],
+      }).compileComponents();
+    })
+  );
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    featureflagService = TestBed.inject(FeatureflagService);
+  });
+
+  it('should create an instance', () => {
+    expect(fixture).toBeTruthy();
+  });
+
+  describe('when the delivery feature flag have experimental features or is dev mode...', () => {
+    it('should show the dom element', () => {
+      spyOn(featureflagService, 'getDeliveryFeatureFlag').and.returnValue(true);
+
+      fixture.detectChanges();
+      const spanElement = fixture.debugElement.query(By.directive(DeliveryDevelopmentDirective));
+
+      expect(spanElement).toBeTruthy();
+    });
+  });
+
+  describe(`when the delivery feature flag don't have experimental features and is not dev mode...`, () => {
+    it('should NOT show the dom element', () => {
+      spyOn(featureflagService, 'getDeliveryFeatureFlag').and.returnValue(false);
+
+      fixture.detectChanges();
+      const spanElement = fixture.debugElement.query(By.directive(DeliveryDevelopmentDirective));
+
+      expect(spanElement).toBeFalsy();
+    });
+  });
+});
--- src/app/private/features/delivery/directives/delivery-development.directive.ts
+++ src/app/private/features/delivery/directives/delivery-development.directive.ts
@@ -2,17 +2,18 @@ import { Directive, TemplateRef, ViewContainerRef, OnInit } from '@angular/core'
 import { FeatureflagService } from '@core/user/featureflag.service';
 
 @Directive({
-  selector: '[deliveryDevelopment]',
+  selector: '[tslDeliveryDevelopment]',
 })
 export class DeliveryDevelopmentDirective implements OnInit {
   constructor(
     private featureflagService: FeatureflagService,
-    private templateRef: TemplateRef<any>,
+    private templateRef: TemplateRef<HTMLElement>,
     private viewContainer: ViewContainerRef
   ) {}
 
   ngOnInit() {
     if (this.featureflagService.getDeliveryFeatureFlag()) {
+      console.log('inside iff!!!');
       this.viewContainer.createEmbeddedView(this.templateRef);
     } else {
       this.viewContainer.clear();
