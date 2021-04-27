6706da7ef125f300861951cb04fd0139db28020d
Laia Lopez <laialopezz99@gmail.com>
2021-04-27 13:56:11 +0200

    [WPA-1317]: [PrivateModule] DeliveryDevelopmentGuard added

--- src/app/private/private.module.ts
+++ src/app/private/private.module.ts
@@ -4,9 +4,11 @@ import { PrivateComponent } from './private.component';
 import { PrivateCoreModule } from './core/private-core.module';
 import { PrivateLayoutModule } from './layout/private-layout.module';
 import { PrivateRoutingModule } from './private-routing.module';
+import { DeliveryDevelopmentGuard } from './features/delivery/guards/delivery-development.guard';
 
 @NgModule({
   imports: [CommonModule, PrivateCoreModule, PrivateLayoutModule, PrivateRoutingModule],
   declarations: [PrivateComponent],
+  providers: [DeliveryDevelopmentGuard],
 })
 export class PrivateModule {}
