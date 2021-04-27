2e75be42319de5522eb0b2979b29b5ada04d185e
Laia Lopez <laialopezz99@gmail.com>
2021-04-27 09:52:28 +0200

    [WPA-1317]: [DeliveryDevelopmentGuard] Logic

--- src/app/private/features/delivery/guards/delivery-development.guard.ts
+++ src/app/private/features/delivery/guards/delivery-development.guard.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { CanLoad } from '@angular/router';
+import { FeatureflagService } from '@core/user/featureflag.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DeliveryDevelopmentGuard implements CanLoad {
-  constructor() {}
+  constructor(private featureflagService: FeatureflagService) {}
 
   canLoad(): boolean {
-    return true;
+    return this.featureflagService.getDeliveryFeatureFlag();
   }
 }
