56ae724036260b093c1b0768fbf8142392cd187c
Laia Lopez <laialopezz99@gmail.com>
2021-04-27 13:21:45 +0200

    [WPA-1319]: [DeliveryModule] DeliveryDevelopmentDirective Added

--- src/app/private/features/delivery/delivery.module.ts
+++ src/app/private/features/delivery/delivery.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { deliveryRoutedComponents, DeliveryRoutingModule } from './delivery.routing.module';
+import { DeliveryDevelopmentDirective } from './directives/delivery-development.directive';
 
 @NgModule({
   declarations: [deliveryRoutedComponents],
-  imports: [CommonModule, DeliveryRoutingModule],
+  imports: [CommonModule, DeliveryRoutingModule, DeliveryDevelopmentDirective],
+  exports: [DeliveryDevelopmentDirective],
 })
 export class DeliveryModule {}
