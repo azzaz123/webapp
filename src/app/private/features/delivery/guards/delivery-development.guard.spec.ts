a8303c070dd6b4d1711fc697f25dde4ead3dd4e3
Laia Lopez <laialopezz99@gmail.com>
2021-04-27 09:52:40 +0200

    [WPA-1317]: [DeliveryDevelopmentGuard] Testing

--- src/app/private/features/delivery/guards/delivery-development.guard.spec.ts
+++ src/app/private/features/delivery/guards/delivery-development.guard.spec.ts
@@ -1,16 +1,41 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { FeatureflagService } from '@core/user/featureflag.service';
+import { FeatureFlagServiceMock } from '@fixtures/feature-flag.fixtures.spec';
 
 import { DeliveryDevelopmentGuard } from './delivery-development.guard';
 
 describe('DeliveryDevelopmentGuard', () => {
   let guard: DeliveryDevelopmentGuard;
+  let featureFlagService: FeatureflagService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [FeatureFlagServiceMock],
+    });
+
     guard = TestBed.inject(DeliveryDevelopmentGuard);
+    featureFlagService = TestBed.inject(FeatureflagService);
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
+  });
+
+  describe('when the feature flag gets the delivery feature flag...', () => {
+    it('should return true', () => {
+      spyOn(featureFlagService, 'getDeliveryFeatureFlag').and.returnValue(true);
+
+      expect(guard.canLoad()).toBe(true);
+    });
+  });
+
+  describe(`when the feature flag don't get the delivery feature flag...`, () => {
+    it('should return false', () => {
+      spyOn(featureFlagService, 'getDeliveryFeatureFlag').and.returnValue(false);
+
+      expect(guard.canLoad()).toBe(false);
+    });
   });
 });
