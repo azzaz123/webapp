cddb8d790ac9ad797dc797559c2053f07a5c23a7
Laia Lopez <laialopezz99@gmail.com>
2021-04-28 11:24:21 +0200

    [WPA-1333]: [DeliveryAddressApiService] Endpoints

--- src/app/private/features/delivery/services/api/delivery-address-api/delivery-address-api.service.ts
+++ src/app/private/features/delivery/services/api/delivery-address-api/delivery-address-api.service.ts
@@ -1,15 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment.beta';
+import { DeliveryAddress } from '@private/features/delivery/interfaces/delivery-address/delivery-address.interface';
 import { Observable } from 'rxjs';
 
-export const GET_FAVOURITES = `${environment.baseUrl}api/v3/delivery/addresses/main`;
+const DELIVERY_ADDRESS_API_URL = `${environment.baseUrl}api/v3/delivery/addresses/`;
+export const MAIN_DELIVERY_ADDRESS = `${DELIVERY_ADDRESS_API_URL}main`;
+export const DELIVERY_ADDRESS_ID = (addressId: string) => `${DELIVERY_ADDRESS_API_URL}main/${addressId}`;
 
 @Injectable()
 export class DeliveryAddressApiService {
   constructor(private http: HttpClient) {}
 
   public getDeliveryAddress(): Observable<any> {
-    return this.http.get<any>(GET_FAVOURITES);
+    return this.http.get<any>(MAIN_DELIVERY_ADDRESS);
+  }
+
+  public createDeliveryAddress(address: DeliveryAddress): Observable<any> {
+    return this.http.post<any>(MAIN_DELIVERY_ADDRESS, { address });
+  }
+
+  public updateDeliveryAddress(address: DeliveryAddress): Observable<any> {
+    return this.http.put<any>(MAIN_DELIVERY_ADDRESS, { address });
+  }
+
+  public deleteDeliveryAddress(addressId: string): Observable<any> {
+    return this.http.delete<any>(DELIVERY_ADDRESS_ID(addressId));
   }
 }
