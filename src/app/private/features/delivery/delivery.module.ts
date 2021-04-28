8b4e0b24f0e9a3199a3e4d2a2286f357094e5fb1
Laia Lopez <laialopezz99@gmail.com>
2021-04-28 18:39:04 +0200

    [WPA-1319]: [Comments] Refoldering

--- src/app/private/features/delivery/delivery.module.ts
+++ src/app/private/features/delivery/delivery.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { deliveryRoutedComponents, DeliveryRoutingModule } from './delivery.routing.module';
-import { DeliveryDevelopmentDirective } from './directives/delivery-development.directive';
 
 @NgModule({
-  declarations: [deliveryRoutedComponents, DeliveryDevelopmentDirective],
+  declarations: [deliveryRoutedComponents],
   imports: [CommonModule, DeliveryRoutingModule],
-  exports: [DeliveryDevelopmentDirective],
 })
 export class DeliveryModule {}
