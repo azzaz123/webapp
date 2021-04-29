8733efb5022e3fe0afcc53e0229cf3e9af9379ea
Alex Masnou <alex.masnou@wallapop.com>
2021-04-29 12:04:14 +0200

    [WEB-474]: Remove no longer needed code

--- src/app/public/shared/components/item-card-wide/components/item-card-wide-placeholder/item-card-wide-placeholder.component.ts
+++ src/app/public/shared/components/item-card-wide/components/item-card-wide-placeholder/item-card-wide-placeholder.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'tsl-item-card-wide-placeholder',
   templateUrl: './item-card-wide-placeholder.component.html',
   styleUrls: ['./item-card-wide-placeholder.component.scss'],
 })
-export class ItemCardWidePlaceholderComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class ItemCardWidePlaceholderComponent {}
--- src/app/public/shared/components/item-card/components/item-card-placeholder/item-card-placeholder.component.ts
+++ src/app/public/shared/components/item-card/components/item-card-placeholder/item-card-placeholder.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { CARD_TYPES } from '../../../item-card-list/enums/card-types.enum';
 
 @Component({
   selector: 'tsl-item-card-placeholder',
@@ -8,7 +7,4 @@ import { CARD_TYPES } from '../../../item-card-list/enums/card-types.enum';
 })
 export class ItemCardPlaceholderComponent {
   @Input() showDescription: boolean = true;
-  @Input() cardType: CARD_TYPES;
-
-  public cardTypes = CARD_TYPES;
 }
