680d34a03beedf4a962a1529f78d91bf36a3d6be
Irene Blanco <irene.blanco@wallapop.com>
2021-04-29 13:11:41 +0200

    [FIN-1846]: Created ItemReactivationService

---
+++ src/app/private/features/upload/core/services/item-reactivation/item-reactivation.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Toast } from '@layout/toast/core/interfaces/toast.interface';
+import { ToastService } from '@layout/toast/core/services/toast.service';
+
+@Injectable()
+export class ItemReactivationService {
+  private toastValidationError: Toast = {
+    text: $localize`:@@web_upload_seller_reactivation_toast_text:Reactivate your item by completing the new mandatory information fields.`,
+    title: $localize`:@@web_upload_seller_reactivation_toast_title:Add mandatory info.`,
+    type: 'error',
+  };
+  constructor(private toastService: ToastService) {}
+
+  public reactivationValidation(form: FormGroup) {
+    if (!form.valid) {
+      this.toastService.show(this.toastValidationError);
+
+      form.markAsPending();
+    }
+  }
+}
