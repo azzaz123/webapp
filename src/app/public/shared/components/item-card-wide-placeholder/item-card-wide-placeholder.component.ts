b80995bc7c29f8f442ed49ef3cdc0ce2459fe199
Alex Masnou <alex.masnou@wallapop.com>
2021-04-29 10:03:56 +0200

    [WEB-474]: Create placeholder for item card wide

---
+++ src/app/public/shared/components/item-card-wide-placeholder/item-card-wide-placeholder.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tsl-item-card-wide-placeholder',
+  templateUrl: './item-card-wide-placeholder.component.html',
+  styleUrls: ['./item-card-wide-placeholder.component.scss'],
+})
+export class ItemCardWidePlaceholderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
