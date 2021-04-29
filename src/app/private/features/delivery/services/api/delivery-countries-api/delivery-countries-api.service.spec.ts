6303aed6c9f196e8d40568cb811b87dabd0837bb
Laia Lopez <laialopezz99@gmail.com>
2021-04-29 09:50:26 +0200

    [WPA-1367]: [DeliveryCountriesApiService] Get

--- src/app/private/features/delivery/services/api/delivery-countries-api/delivery-countries-api.service.spec.ts
+++ src/app/private/features/delivery/services/api/delivery-countries-api/delivery-countries-api.service.spec.ts
@@ -1,13 +1,19 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { DeliveryCountriesApiService } from './delivery-countries-api.service';
 
 describe('DeliveryCountriesApiService', () => {
   let service: DeliveryCountriesApiService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [DeliveryCountriesApiService],
+    });
     service = TestBed.inject(DeliveryCountriesApiService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
