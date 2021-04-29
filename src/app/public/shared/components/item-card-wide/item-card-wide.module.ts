45c35739ada7b25bfc1e1f39d5ed77a67804fa90
Alex Masnou <alex.masnou@wallapop.com>
2021-04-29 10:08:11 +0200

    [WEB-474]: Move folder

--- src/app/public/shared/components/item-card-wide/item-card-wide.module.ts
+++ src/app/public/shared/components/item-card-wide/item-card-wide.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ItemCardWideComponent } from './item-card-wide.component';
+import { ItemCardWidePlaceholderComponent } from './components/item-card-wide-placeholder/item-card-wide-placeholder.component';
 import { FavouriteIconModule } from '../favourite-icon/favourite-icon.module';
 import { CustomCurrencyModule } from '@shared/pipes/custom-currency/custom-currency.module';
 import { SlidesCarouselModule } from '../carousel-slides/carousel-slides.module';
 import { ItemExtraInfoModule } from '@public/shared/components/item-extra-info/item-extra-info.module';
 import { DeviceDetectorService } from 'ngx-device-detector';
-import { ItemCardWidePlaceholderComponent } from '../item-card-wide-placeholder/item-card-wide-placeholder.component';
 
 @NgModule({
   declarations: [ItemCardWideComponent, ItemCardWidePlaceholderComponent],
