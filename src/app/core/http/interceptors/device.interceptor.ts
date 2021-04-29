985ed6285c6bc119eb920b7d8e1d513f170a302b
Laia Lopez <laialopezz99@gmail.com>
2021-04-29 13:07:31 +0200

    [WPA-1376]: [Environment] Corrected

--- src/app/core/http/interceptors/device.interceptor.ts
+++ src/app/core/http/interceptors/device.interceptor.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '@environments/environment.beta';
+import { environment } from '@environments/environment';
 
 export const DEVICE_HEADER_NAME = 'DeviceOS';
 
