2277f131a78e9462ec01294e17d3bba7bc9db250
Laia Lopez <laialopezz99@gmail.com>
2021-05-05 13:44:50 +0200

    [WPA-1332]: [Initial commit] DeliveryAddressComponent

---
+++ src/app/private/features/delivery/pages/delivery-address/delivery-address.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tsl-delivery-address',
+  templateUrl: './delivery-address.component.html',
+  styleUrls: ['./delivery-address.component.scss'],
+})
+export class DeliveryAddressComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
