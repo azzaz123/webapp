b62ed923dad14b1f488962443fda9e0e2fbd2ed5
Laia Lopez <laialopezz99@gmail.com>
2021-05-05 18:37:01 +0200

    [WPA-1332]: [DeliveryAddressModule] Updated + copys

--- src/app/private/features/delivery/pages/delivery-address/delivery-address.module.ts
+++ src/app/private/features/delivery/pages/delivery-address/delivery-address.module.ts
@@ -3,9 +3,17 @@ import { ProfileFormModule } from '@shared/profile/profile-form/profile-form.mod
 import { SpinnerModule } from '@shared/spinner/spinner.module';
 import { deliveryAddressRoutedComponents, DeliveryAddressRoutingModule } from './delivery-address.routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { DropdownModule } from '@shared/dropdown/dropdown.module';
+import { DeliveryAddressService } from '../../services/delivery-address/delivery-address.service';
+import { DeliveryCountriesService } from '../../services/delivery-countries/delivery-countries.service';
+import { ErrorsService } from '@core/errors/errors.service';
+import { DeliveryAddressApiService } from '../../services/api/delivery-address-api/delivery-address-api.service';
+import { DeliveryCountriesApiService } from '../../services/api/delivery-countries-api/delivery-countries-api.service';
+import { ButtonModule } from '@shared/button/button.module';
 
 @NgModule({
-  imports: [DeliveryAddressRoutingModule, ProfileFormModule, SpinnerModule, ReactiveFormsModule],
+  imports: [DeliveryAddressRoutingModule, ProfileFormModule, SpinnerModule, ReactiveFormsModule, DropdownModule, ButtonModule],
   declarations: [deliveryAddressRoutedComponents],
+  providers: [DeliveryAddressService, DeliveryAddressApiService, DeliveryCountriesService, DeliveryCountriesApiService, ErrorsService],
 })
 export class DeliveryAddressModule {}
