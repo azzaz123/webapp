ff9f9797f2f20cab904d3683fa4426bf8337ba51
Alex Masnou <alex.masnou@wallapop.com>
2021-05-06 19:30:35 +0200

    [WEB-313]: Add missing configuration options

--- src/app/public/shared/components/filters/components/location-filter/interfaces/location-filter-config.interface.ts
+++ src/app/public/shared/components/filters/components/location-filter/interfaces/location-filter-config.interface.ts
@@ -1,12 +1,13 @@
 import { FilterConfig } from '../../../interfaces/filter-config.interface';
 import { FILTER_TYPES } from '@public/shared/components/filters/core/enums/filter-types/filter-types.enum';
 import { LocationFilterParams } from './location-filter-params.interface';
+import { SliderFormStepConfig } from '@shared/form/components/slider/interfaces/slider-form-step-config.interface';
 
 export interface LocationFilterConfig extends FilterConfig<LocationFilterParams> {
   type: FILTER_TYPES.LOCATION;
-  options: {
-    maxDistance: number;
-    minDistance: number;
-  };
+  range: [number, number];
+  stepsConfig?: SliderFormStepConfig[];
+  units?: string;
+  limitless?: boolean;
   hasContentPlaceholder: true;
 }
