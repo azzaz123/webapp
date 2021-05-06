1a920f41ff63e9b81a528571001053424d064848
Gonzalo Morales <gonzalomorales93@gmail.com>
2021-05-06 17:14:38 +0200

    [PQP-3477]: add tier selector style

--- src/app/private/features/profile/pages/subscription/subscription-tier-selector/subscription-tier-selector.component.ts
+++ src/app/private/features/profile/pages/subscription/subscription-tier-selector/subscription-tier-selector.component.ts
@@ -1,10 +1,11 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { SubscriptionResponse, SubscriptionsResponse, Tier } from '@core/subscriptions/subscriptions.interface';
 
 @Component({
   selector: 'tsl-subscription-tier-selector',
   templateUrl: './subscription-tier-selector.component.html',
   styleUrls: ['./subscription-tier-selector.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SubscriptionTierSelectorComponent implements OnInit {
   @Input() subscription: SubscriptionsResponse;
@@ -17,5 +18,11 @@ export class SubscriptionTierSelectorComponent implements OnInit {
 
   onTierSelectedChanged(event: Tier) {
     this.changeSelectedTier.emit(event);
+  }
+
+  getLimitText(tier: Tier): string {
+    return tier.limit
+      ? $localize`:@@web_profile_pages_subscription_325:List up to ${tier.limit} items`
+      : $localize`:@@web_profile_pages_subscription_586:List without limits`;
   }
 }
