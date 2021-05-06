80a4e78fba3e42711d47a48c2c2cb93ae66d62d3
Alex Masnou <alex.masnou@wallapop.com>
2021-05-06 17:15:30 +0200

    [WEB-313]: Add NgbTypeahead and forms modules

--- src/app/public/shared/components/filters/components/location-filter/location-filter.module.ts
+++ src/app/public/shared/components/filters/components/location-filter/location-filter.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LocationFilterComponent } from './location-filter/location-filter.component';
 import { AbstractFilterModule } from '../abstract-filter/abstract-filter.module';
+import { SliderFormModule } from '@shared/form/components/slider/slider-form.module';
+import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LocationFilterComponent],
-  imports: [AbstractFilterModule, CommonModule],
+  imports: [AbstractFilterModule, CommonModule, SliderFormModule, NgbTypeaheadModule, FormsModule, ReactiveFormsModule],
 })
 export class LocationFilterModule {}
