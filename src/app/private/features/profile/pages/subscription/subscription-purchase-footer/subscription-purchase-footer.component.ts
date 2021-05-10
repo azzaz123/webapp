6acae0913e2b566686748f7d4f3fb7d71c7ce134
Gonzalo Morales <gonzalomorales93@gmail.com>
2021-05-10 10:02:44 +0200

    [PQP-3477]: add footer copies

--- src/app/private/features/profile/pages/subscription/subscription-purchase-footer/subscription-purchase-footer.component.ts
+++ src/app/private/features/profile/pages/subscription/subscription-purchase-footer/subscription-purchase-footer.component.ts
@@ -1,21 +1,34 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Tier } from '@core/subscriptions/subscriptions.interface';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { PRIVACY_POLICY_URL, TERMS_AND_CONDITIONS_URL } from '@core/constants';
+import { SubscriptionResponse, SubscriptionsResponse, Tier } from '@core/subscriptions/subscriptions.interface';
 
 @Component({
   selector: 'tsl-subscription-purchase-footer',
   templateUrl: './subscription-purchase-footer.component.html',
   styleUrls: ['./subscription-purchase-footer.component.scss'],
 })
-export class SubscriptionPurchaseFooterComponent implements OnInit {
+export class SubscriptionPurchaseFooterComponent implements OnInit, OnChanges {
   @Input() selectedTier: Tier;
   @Input() buttonDisable: boolean;
-  @Input() isFreeTrial: boolean;
+  @Input() subscription: SubscriptionsResponse;
+  @Input() isLoading: boolean;
   @Output() buttonPurchaseClick: EventEmitter<void> = new EventEmitter();
+
+  descriptionText: string;
+
+  public termsAndConditionsURL = TERMS_AND_CONDITIONS_URL;
+  public policyPrivacyURL = PRIVACY_POLICY_URL;
 
   constructor() {}
 
   ngOnInit(): void {
     console.log('test', this.selectedTier);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.descriptionText = this.subscription.trial_available
+      ? $localize`:@@web_profile_pages_subscription_1000:${this.subscription.trial_days} days free, then ${this.selectedTier.price}${this.selectedTier.currency}/month`
+      : $localize`:@@web_profile_pages_subscription_1500:${this.selectedTier.price}${this.selectedTier.currency}/month`;
   }
 
   public onClickButton() {
