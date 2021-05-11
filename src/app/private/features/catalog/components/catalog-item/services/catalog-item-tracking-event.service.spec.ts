625ed345b38091770fd5f228dcd6b190f6d841c1
Irene Blanco <irene.blanco@wallapop.com>
2021-05-11 19:30:04 +0200

    [FIN-1844]: CatalogItemTrackingEventService - Corrected tests

--- src/app/private/features/catalog/components/catalog-item/services/catalog-item-tracking-event.service.spec.ts
+++ src/app/private/features/catalog/components/catalog-item/services/catalog-item-tracking-event.service.spec.ts
@@ -6,6 +6,13 @@ import { MockAnalyticsService } from '@fixtures/analytics.fixtures.spec';
 import { UserService } from '@core/user/user.service';
 import { CatalogItemTrackingEventService } from './catalog-item-tracking-event.service';
 import { ITEM_DATA3 } from '@fixtures/item.fixtures.spec';
+import {
+  AnalyticsEvent,
+  ANALYTICS_EVENT_NAMES,
+  ANALYTIC_EVENT_TYPES,
+  ReactivateItem,
+  SCREEN_IDS,
+} from '@core/analytics/analytics-constants';
 
 describe('CatalogItemTrackingEventService', () => {
   let service: CatalogItemTrackingEventService;
@@ -30,28 +37,53 @@ describe('CatalogItemTrackingEventService', () => {
 
   describe('when item reactivation', () => {
     const item = ITEM_DATA3;
+    let event: AnalyticsEvent<ReactivateItem>;
+
     beforeEach(() => {
       spyOn(service, 'trackReactivateItemEvent').and.callThrough();
       spyOn(analyticsService, 'trackEvent');
+
+      event = {
+        name: ANALYTICS_EVENT_NAMES.ReactivateItem,
+        eventType: ANALYTIC_EVENT_TYPES.Navigation, // TODO check with product
+        attributes: {
+          itemId: item.id,
+          categoryId: service['getCategoryId'](item),
+          subcategoryId: parseInt(item.content.extra_info?.object_type?.id, 10),
+          salePrice: item.content.sale_price,
+          title: item.content.title,
+          brand: item.content.extra_info?.brand,
+          model: item.content.extra_info?.model,
+          objectType: item.content.extra_info?.object_type.name,
+          isPro: null,
+          screenId: SCREEN_IDS.MyCatalog,
+        },
+      };
     });
 
     describe('and user is not pro', () => {
       const isPro = false;
+      beforeEach(() => {
+        event.attributes.isPro = isPro;
+      });
 
       it('should send click item card event', () => {
         service.trackReactivateItemEvent(item, isPro);
 
-        expect(analyticsService.trackEvent).toHaveBeenCalledWith(item, isPro);
+        expect(analyticsService.trackEvent).toHaveBeenCalledWith(event);
       });
     });
 
     describe('and user is pro', () => {
       const isPro = true;
+      beforeEach(() => {
+        event.attributes.isPro = isPro;
+      });
 
       it('should send click item card event', () => {
         service.trackReactivateItemEvent(item, isPro);
 
-        expect(analyticsService.trackEvent).toHaveBeenCalledWith(item, isPro);
+        expect(analyticsService.trackEvent).toHaveBeenCalledWith(event);
       });
     });
   });
