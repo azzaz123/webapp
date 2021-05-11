2566dd6b379fd288f4c2748aa666633b543eee62
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-05-11 09:39:22 +0200

    [FIN-1893]: Added searchId to SearchPagination interface

--- src/app/public/features/search/interfaces/search-pagination.interface.ts
+++ src/app/public/features/search/interfaces/search-pagination.interface.ts
@@ -3,6 +3,7 @@ import { ItemCard } from '@public/core/interfaces/item-card.interface';
 export interface SearchPagination {
   items: ItemCard[];
   hasMore: boolean;
+  searchId?: string;
 }
 
 export interface SearchPaginationWithCategory extends SearchPagination {
