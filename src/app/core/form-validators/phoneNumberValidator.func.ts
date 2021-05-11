62333332114da4b073960d307d5cdcea11291088
Laia Lopez <laialopezz99@gmail.com>
2021-05-11 16:51:10 +0200

    [WPA-1332]: [FrontendValidators] Delete

--- src/app/core/form-validators/phoneNumberValidator.func.ts
+++
@@ -1,6 +0,0 @@
-import { AbstractControl } from '@angular/forms';
-import { DeliveryCountryISOCode } from '@private/features/delivery/interfaces/delivery-address/delivery-address-api.interface';
-
-export function phoneNumberValidator(control: AbstractControl, countryISOCode: DeliveryCountryISOCode): { [key: string]: boolean } {
-  return null;
-}
--- src/app/core/form-validators/postalCodeValidator.func.ts
+++
@@ -1,7 +0,0 @@
-import { AbstractControl } from '@angular/forms';
-
-const postalCodeRegex: RegExp = /^([1-9]{2}|[0-9][1-9]|[1-9][0-9])[0-9]{3}$/;
-
-export function postalCodeValidator(control: AbstractControl): { [key: string]: boolean } {
-  return postalCodeRegex.test(control.value) ? null : { postal_code: true };
-}
