2ccc97a251123fbc9f8da12e1ff5cbfa0581aa3f
Alex Masnou <alex.masnou@wallapop.com>
2021-05-11 10:41:53 +0200

    [WEB-487]: Add search route to reused routes

--- src/app/public/features/search/search.routing.module.ts
+++ src/app/public/features/search/search.routing.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdsResolver } from '@core/ads/resolvers/ads.resolver';
+import { ReusedRoute } from '@core/custom-route-reuse-strategy/interfaces/reused-route.interface';
+import { PUBLIC_PATHS } from '@public/public-routing-constants';
 import { SearchResolver } from './core/services/search.resolver';
 import { SearchComponent } from './pages/search.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    resolve: {
-      ads: AdsResolver,
-      search: SearchResolver,
-    },
-    component: SearchComponent,
+const SEARCH_ROUTE: ReusedRoute = {
+  path: '',
+  component: SearchComponent,
+  resolve: {
+    ads: AdsResolver,
+    search: SearchResolver,
   },
-];
+  data: {
+    shouldReuseRoute: true,
+    routeKey: PUBLIC_PATHS.SEARCH,
+  },
+};
+
+const routes: Routes = [SEARCH_ROUTE];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
