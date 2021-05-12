0de70a09955cd5d753b8bb6186af0741b269a11a
Alex Masnou <alex.masnou@wallapop.com>
2021-05-13 00:19:32 +0200

    [WEB-487]: Create new router outlet directive for handling onAttach and onDetach events

---
+++ src/app/public/core/directives/public-router-outlet/public-router-outlet.directive.ts
@@ -0,0 +1,28 @@
+import { ComponentRef, Directive } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+
+export interface OnAttach {
+  onAttach(activatedRoute: ActivatedRoute): void;
+}
+export interface OnDetach {
+  onDetach(): void;
+}
+
+@Directive({
+  selector: 'public-router-outlet',
+})
+export class PublicRouterOutletDirective extends RouterOutlet {
+  detach(): ComponentRef<any> {
+    const instance: any = this.component;
+    if (instance && typeof instance.onDetach === 'function') {
+      instance.onDetach();
+    }
+    return super.detach();
+  }
+  attach(ref: ComponentRef<any>, activatedRoute: ActivatedRoute): void {
+    super.attach(ref, activatedRoute);
+    if (ref.instance && typeof ref.instance.onAttach === 'function') {
+      ref.instance.onAttach(activatedRoute);
+    }
+  }
+}
