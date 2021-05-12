e49ee06b250b8bebcf65aa3be66652c41546f710
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-05-12 18:34:17 +0200

    [FIN-1899]: Created FILTER_SOURCE enum

---
+++ src/app/public/features/search/core/services/enums/filter-source.enum.ts
@@ -0,0 +1,12 @@
+export enum FILTER_SOURCE {
+  SEARCH_BOX = 'search_box',
+  RECENT_SEARCHES = 'recent_searches',
+  DEFAULT_FILTERS = 'default_filters',
+  QUICK_FILTERS = 'quick_filters',
+  STORED_FILTERS = 'stored_filters',
+  SEO_SECTION = 'seo_section',
+  SEO_LIST = 'seo_list',
+  SEO_LOCATION_LIST = 'seo_location_list',
+  SEO_SITEMAP = 'seo_sitemap',
+  ITEM_DETAIL = 'item_detail',
+}
