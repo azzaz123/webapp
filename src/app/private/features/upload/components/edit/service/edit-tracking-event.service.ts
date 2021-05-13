bad6087c2224138ac84d30b6f9b8ea44ae04342d
Irene Blanco <irene.blanco@wallapop.com>
2021-05-13 10:29:42 +0200

    [FIN-1844]: Created EditTrackingEventService

---
+++ src/app/private/features/upload/components/edit/service/edit-tracking-event.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { ANALYTICS_EVENT_NAMES, SCREEN_IDS, ViewEditItem, AnalyticsPageView } from '@core/analytics/analytics-constants';
+import { AnalyticsService } from '@core/analytics/analytics.service';
+
+@Injectable()
+export class EditTrackingEventService {
+  constructor(private analyticsService: AnalyticsService) {}
+
+  public trackViewEditItemEvent(categoryId: number, mandatoryAdditionalFields: boolean): void {
+    const event: AnalyticsPageView<ViewEditItem> = {
+      name: ANALYTICS_EVENT_NAMES.ViewEditItem,
+      attributes: {
+        screenId: SCREEN_IDS.Upload,
+        categoryId,
+        mandatoryAdditionalFields,
+      },
+    };
+    this.analyticsService.trackPageView(event);
+  }
+}
