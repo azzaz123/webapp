56f4bec28560c1d907b1d0bc61bab57433bd4185
Irene Blanco <irene.blanco@wallapop.com>
2021-05-13 12:20:10 +0200

    [FIN-1844]: EditTrackingEventService - Added viewTrackingReady$

--- src/app/private/features/upload/components/edit/service/edit-tracking-event.service.ts
+++ src/app/private/features/upload/components/edit/service/edit-tracking-event.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
 import { ANALYTICS_EVENT_NAMES, SCREEN_IDS, ViewEditItem, AnalyticsPageView } from '@core/analytics/analytics-constants';
 import { AnalyticsService } from '@core/analytics/analytics.service';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class EditTrackingEventService {
   constructor(private analyticsService: AnalyticsService) {}
+
+  public get viewTrackingReady$(): Observable<void> {
+    return this.analyticsService.mParticleReady$;
+  }
 
   public trackViewEditItemEvent(categoryId: number, mandatoryAdditionalFields: boolean): void {
     const event: AnalyticsPageView<ViewEditItem> = {
