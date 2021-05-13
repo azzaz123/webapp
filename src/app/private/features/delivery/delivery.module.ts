5b8f10180112e841b173e4230b1043355417433e
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-05-13 10:27:26 +0200

    [WPA-1545]: Use nav links in delivery component

--- src/app/private/features/delivery/delivery.module.ts
+++ src/app/private/features/delivery/delivery.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { deliveryRoutedComponents, DeliveryRoutingModule } from './delivery.routing.module';
+import { NavLinksModule } from '@shared/nav-links/nav-links.module';
 
 @NgModule({
   declarations: [deliveryRoutedComponents],
-  imports: [CommonModule, DeliveryRoutingModule],
+  imports: [CommonModule, DeliveryRoutingModule, NavLinksModule],
 })
 export class DeliveryModule {}
