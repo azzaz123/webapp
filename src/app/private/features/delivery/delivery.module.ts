926e0464fd00981997b8b8660ffd798c79f7cafa
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-05-13 11:13:53 +0200

    [WPA-1545]: Add header info in delivery module

--- src/app/private/features/delivery/delivery.module.ts
+++ src/app/private/features/delivery/delivery.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { deliveryRoutedComponents, DeliveryRoutingModule } from './delivery.routing.module';
 import { NavLinksModule } from '@shared/nav-links/nav-links.module';
+import { HeaderModule } from '@shared/header/header.module';
 
 @NgModule({
   declarations: [deliveryRoutedComponents],
-  imports: [CommonModule, DeliveryRoutingModule, NavLinksModule],
+  imports: [CommonModule, DeliveryRoutingModule, NavLinksModule, HeaderModule],
 })
 export class DeliveryModule {}
