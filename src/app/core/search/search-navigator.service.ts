81ddc53f2906717d6f541facbdcc5df3637ed13b
Irene Blanco <irene.blanco@wallapop.com>
2021-05-17 13:07:25 +0200

    [FIN-1899]: SearchNavigatorService - Added filtersSource to navigate

--- src/app/core/search/search-navigator.service.ts
+++ src/app/core/search/search-navigator.service.ts
@@ -6,6 +6,7 @@ import { FILTER_QUERY_PARAM_KEY } from '@public/shared/components/filters/enums/
 import { QueryStringLocationService } from '@core/search/query-string-location.service';
 import { CATEGORY_IDS } from '@core/category/category-ids';
 import { REAL_ESTATE_SPECIFICATION_TYPE } from '@public/core/constants/item-specifications/realestate-constants';
+import { FILTER_PARAMETERS_SEARCH } from '@public/features/search/core/services/constants/filter-parameters';
 
 @Injectable({
   providedIn: 'root',
@@ -25,7 +26,7 @@ export class SearchNavigatorService {
     FILTER_QUERY_PARAM_KEY.orderBy,
   ];
 
-  public navigate(filterParams: FilterParameter[], keepCurrentParams?: boolean): void {
+  public navigate(filterParams: FilterParameter[], filtersSource: any, keepCurrentParams?: boolean): void {
     const currentParams = this.route.snapshot.queryParams;
     let newParams = this.queryStringService.mapFilterToQueryParams(filterParams);
 
@@ -36,7 +37,10 @@ export class SearchNavigatorService {
     const cleanParams = this.cleanParams(currentParams, newParams);
 
     this.router.navigate(['/search'], {
-      queryParams: this.prepareFinalParams(currentParams, newParams, cleanParams),
+      queryParams: {
+        ...this.prepareFinalParams(currentParams, newParams, cleanParams),
+        [FILTER_PARAMETERS_SEARCH.FILTERS_SOURCE]: filtersSource,
+      },
     });
   }
 
