ce1f28c9f076bc290d7ef44562d63fe3b21f238c
Irene Blanco <irene.blanco@wallapop.com>
2021-05-19 14:40:29 +0200

    [FIN-1899]: SearchNavigatorService - Added missing FILTERS_SOURCE type

--- src/app/core/search/search-navigator.service.ts
+++ src/app/core/search/search-navigator.service.ts
@@ -7,6 +7,7 @@ import { QueryStringLocationService } from '@core/search/query-string-location.s
 import { CATEGORY_IDS } from '@core/category/category-ids';
 import { REAL_ESTATE_SPECIFICATION_TYPE } from '@public/core/constants/item-specifications/realestate-constants';
 import { FILTER_PARAMETERS_SEARCH } from '@public/features/search/core/services/constants/filter-parameters';
+import { FILTERS_SOURCE } from '@public/core/services/search-tracking-events/enums/filters-source-enum';
 
 @Injectable({
   providedIn: 'root',
@@ -26,7 +27,7 @@ export class SearchNavigatorService {
     FILTER_QUERY_PARAM_KEY.orderBy,
   ];
 
-  public navigate(filterParams: FilterParameter[], filtersSource: any, keepCurrentParams?: boolean): void {
+  public navigate(filterParams: FilterParameter[], filtersSource: FILTERS_SOURCE, keepCurrentParams?: boolean): void {
     const currentParams = this.route.snapshot.queryParams;
     let newParams = this.queryStringService.mapFilterToQueryParams(filterParams);
 
