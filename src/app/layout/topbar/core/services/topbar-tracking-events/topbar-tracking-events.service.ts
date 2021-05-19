94c9dba4d534210d4a0b16544408c4928e026254
Irene Blanco <irene.blanco@wallapop.com>
2021-05-19 09:20:08 +0200

    [FIN-1894]: TopbarTrackingEventsService - Added trackCancelSearchEvent

--- src/app/layout/topbar/core/services/topbar-tracking-events/topbar-tracking-events.service.ts
+++ src/app/layout/topbar/core/services/topbar-tracking-events/topbar-tracking-events.service.ts
@@ -3,6 +3,7 @@ import {
   AnalyticsEvent,
   ANALYTICS_EVENT_NAMES,
   ANALYTIC_EVENT_TYPES,
+  CancelSearch,
   ClickKeyboardSearchButton,
   SCREEN_IDS,
 } from '@core/analytics/analytics-constants';
@@ -15,6 +16,18 @@ export class TopbarTrackingEventsService {
   public trackClickKeyboardSearchButtonEvent(searchText: string): void {
     const event: AnalyticsEvent<ClickKeyboardSearchButton> = {
       name: ANALYTICS_EVENT_NAMES.ClickKeyboardSearchButton,
+      eventType: ANALYTIC_EVENT_TYPES.Navigation,
+      attributes: {
+        screenId: SCREEN_IDS.Search,
+        searchText: searchText,
+      },
+    };
+    this.analyticsService.trackEvent(event);
+  }
+
+  public trackCancelSearchEvent(searchText: string): void {
+    const event: AnalyticsEvent<CancelSearch> = {
+      name: ANALYTICS_EVENT_NAMES.CancelSearch,
       eventType: ANALYTIC_EVENT_TYPES.Navigation,
       attributes: {
         screenId: SCREEN_IDS.Search,
