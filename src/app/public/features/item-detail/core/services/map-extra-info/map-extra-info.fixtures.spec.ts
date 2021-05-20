7eda174ce053404df38e1c28764e2a2a569063b3
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-05-20 17:01:06 +0200

    [WEB-492]: Use capitalize function in mock file

--- src/app/public/features/item-detail/core/services/map-extra-info/map-extra-info.fixtures.spec.ts
+++ src/app/public/features/item-detail/core/services/map-extra-info/map-extra-info.fixtures.spec.ts
@@ -1,15 +1,18 @@
+import { capitalizeString } from '@core/helpers/capitalize-string/capitalize-string';
 import { MOCK_CAR } from '@fixtures/car.fixtures.spec';
 import { ITEM_CELLPHONES_EXTRA_INFO, ITEM_FASHION_EXTRA_INFO } from '@fixtures/item.fixtures.spec';
 
-export const MOCK_CELLPHONE_EXTRA_INFO = capitalizeLabels([ITEM_CELLPHONES_EXTRA_INFO.brand, ITEM_CELLPHONES_EXTRA_INFO.model]);
-export const MOCK_FASHION_EXTRA_INFO = capitalizeLabels([
+export const MOCK_CELLPHONE_EXTRA_INFO = [ITEM_CELLPHONES_EXTRA_INFO.brand, ITEM_CELLPHONES_EXTRA_INFO.model].map((string) =>
+  capitalizeString(string)
+);
+
+export const MOCK_FASHION_EXTRA_INFO = [
   ITEM_FASHION_EXTRA_INFO.size.text,
   ITEM_FASHION_EXTRA_INFO.brand,
   ITEM_FASHION_EXTRA_INFO.model,
   ITEM_FASHION_EXTRA_INFO.condition,
-]);
-export const MOCK_CAR_EXTRA_INFO = capitalizeLabels([MOCK_CAR.version, MOCK_CAR.year.toString(), `${MOCK_CAR.km}Km`]);
+].map((string) => capitalizeString(string));
 
-function capitalizeLabels(labels: string[]): string[] {
-  return labels.map((label) => label.charAt(0).toUpperCase() + label.substr(1));
-}
+export const MOCK_CAR_EXTRA_INFO = [MOCK_CAR.version, MOCK_CAR.year.toString(), `${MOCK_CAR.km}Km`].map((string) =>
+  capitalizeString(string)
+);
