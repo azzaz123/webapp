6e9b5da10ddf7d202d8df87354e2ffd12df79c11
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-05-25 11:40:57 +0200

    [WEB-492]: Remove unused imports

--- src/app/public/shared/components/item-images-carousel/item-images-carousel.component.spec.ts
+++ src/app/public/shared/components/item-images-carousel/item-images-carousel.component.spec.ts
@@ -1,5 +1,5 @@
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { ChangeDetectionStrategy, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ChangeDetectionStrategy } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DeviceService } from '@core/device/device.service';
--- src/app/public/shared/components/item-images-carousel/item-images-carousel.component.ts
+++ src/app/public/shared/components/item-images-carousel/item-images-carousel.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
-import { DeviceService } from '@core/device/device.service';
 import { CarouselSlide } from '../carousel-slides/carousel-slide.interface';
 import { STATUS_ITEM_FLAG_TYPES, BUMPED_ITEM_FLAG_TYPES } from '../item-flag/item-flag-constants';
 
@@ -16,5 +15,5 @@ export class ItemImagesCarouselComponent {
   @Input() bumpedFlag: BUMPED_ITEM_FLAG_TYPES;
   @Output() imageClick: EventEmitter<CarouselSlide> = new EventEmitter<CarouselSlide>();
 
-  constructor(private deviceService: DeviceService) {}
+  constructor() {}
 }
