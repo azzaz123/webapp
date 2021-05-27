3cc100ffa349e937c711a1f625834194a88c2aa9
Laia Lopez <laialopezz99@gmail.com>
2021-05-27 09:56:22 +0200

    [WPA-1671]: [BankAccount] Created component + implemented module

---
+++ src/app/private/features/delivery/pages/bank-account/bank-account.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tsl-bank-account',
+  templateUrl: './bank-account.component.html',
+  styleUrls: ['./bank-account.component.scss'],
+})
+export class BankAccountComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
