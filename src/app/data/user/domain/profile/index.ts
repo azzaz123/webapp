cd783f51a15e9395e3557ae35057eb74454640da
Alex Masnou <alex.masnou@wallapop.com>
2021-05-28 10:12:52 +0200

    [WEB-502]: Remove NgRx store related files

--- src/app/data/user/domain/profile/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './profile';
-export * from './user-gender';
-export * from './user-id';
-export * from './user-image';
-export * from './user-update';
-export * from '../user.repository';
--- src/app/data/user/domain/stats/user-stats.ts
+++
@@ -1,7 +0,0 @@
-import { UserCounter } from './user-counter';
-import { UserRating } from './user-rating';
-
-export interface UserStats {
-  ratings: UserRating;
-  counters: UserCounter;
-}
