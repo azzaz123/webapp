c888bf1ebe8646d88891ac619e48f2821357e1b2
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-05-28 11:11:20 +0200

    [WPA-1749]: Get error mapper by endpoint

---
+++ src/app/core/http/interceptors/error-mapper/core/functions/error-mapper-factory.ts
@@ -0,0 +1,28 @@
+import { HttpRequest, HttpHandler } from '@angular/common/http';
+import { environment } from '@environments/environment.beta';
+import { DeliveryAddressErrorMapper } from '../classes/delivery-address/delivery-address-error-maper';
+import { ErrorMapper } from '../classes/error-mapper';
+import { ERROR_MAPPABLE_ENDPOINTS } from '../constants/error-mappable-endpoints';
+
+export function ErrorMapperFactory(request: HttpRequest<unknown>, handler: HttpHandler) {
+  const errorMapperType = getErrorMapperByEndpoint(request);
+
+  if (errorMapperType === DeliveryAddressErrorMapper) {
+    return new DeliveryAddressErrorMapper(request, handler);
+  }
+
+  return null;
+}
+
+const getErrorMapperByEndpoint = (request: HttpRequest<unknown>): typeof ErrorMapper => {
+  const { url } = request;
+
+  const isDeliveryAddressRequest = url.startsWith(getMapableEndpointWithBase(ERROR_MAPPABLE_ENDPOINTS.DELIVERY_ADDRESS));
+  if (isDeliveryAddressRequest) {
+    return DeliveryAddressErrorMapper;
+  }
+};
+
+const getMapableEndpointWithBase = (endpoint: ERROR_MAPPABLE_ENDPOINTS): string => {
+  return `${environment.baseUrl}${endpoint}`;
+};
