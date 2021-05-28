2b8f35de777f70704a44ffaeb2f8b48d4963ff4e
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-05-28 19:15:58 +0200

    [WPA-1749]: Move postal code error translations to different file

---
+++ src/app/core/http/interceptors/error-mapper/core/constants/delivery-error-translations.ts
@@ -0,0 +1,5 @@
+export const DeliveryPostalCodesErrorTranslations = {
+  INVALID_POSTAL_CODE: $localize`:@@web_delivery_address_invalid_postal_code_error:This postal code isn't valid`,
+  POSTAL_CODE_NOT_ALLOWED: $localize`:@@web_delivery_address_postal_code_not_allowed_error:Postal code not accepted`,
+  POSTAL_CODE_DOES_NOT_EXIST: $localize`:@@web_delivery_address_postal_code_not_exist_error:Postal code doesn't exist`,
+};
