4e24089d75de54a6fca17acdd529d43d9082d993
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-06-01 14:58:08 +0200

    [WPA-1749]: Modify default behaviour when errors not mapped

--- src/app/private/features/delivery/errors/mappers/error-mapper.ts
+++ src/app/private/features/delivery/errors/mappers/error-mapper.ts
@@ -3,32 +3,41 @@ import { throwError } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
 
 export abstract class ErrorMapper<T extends HttpErrorResponse> {
+  public static DEFAULT_ERROR_MESSAGE = 'Error';
+
   public map(errorResponse: T): Observable<never> {
     const generatedError = this.generateErrorByRequest(errorResponse);
-    const emptyError = this.generateEmptyError(generatedError, errorResponse);
-    if (emptyError) {
-      return throwError(emptyError);
+    const isEmptyError = this.isGeneratedErrorEmpty(generatedError);
+    if (isEmptyError) {
+      const fallbackError = this.generateFallbackError(errorResponse);
+      return throwError(fallbackError);
     }
     return throwError(generatedError);
   }
 
   protected generateErrorByRequest(errorResponse: HttpErrorResponse): Error | Error[] {
-    return new Error(this.getErrorMessageFromResponse(errorResponse));
+    return this.generateFallbackError(errorResponse);
   }
 
-  private generateEmptyError(error: null | Error | Error[], errorResponse: HttpErrorResponse): Error | Error[] {
-    if (!error) {
-      return new Error(this.getErrorMessageFromResponse(errorResponse));
-    }
-
-    if (Array.isArray(error) && !error.length) {
-      return [new Error(this.getErrorMessageFromResponse(errorResponse))];
-    }
+  private isGeneratedErrorEmpty(error: Error | Error[]): boolean {
+    const isDefined = !!error;
+    const isEmptyArray = Array.isArray(error) && !error.length;
+    return !isDefined || isEmptyArray;
   }
 
-  private getErrorMessageFromResponse(errorResponse: HttpErrorResponse): string {
-    const { error } = errorResponse;
-    const message = error[0]?.message || error?.message;
-    return message || 'Error';
+  private generateFallbackError(errorResponse: HttpErrorResponse): Error | Error[] {
+    const errorsFromBackend = this.getErrorMessageFromResponse(errorResponse);
+    if (Array.isArray(errorsFromBackend)) {
+      return errorsFromBackend.map((error) => new Error(error));
+    }
+    return new Error(errorsFromBackend);
+  }
+
+  private getErrorMessageFromResponse(errorResponse: HttpErrorResponse): string | string[] {
+    const { error: backendError } = errorResponse;
+    if (Array.isArray(backendError)) {
+      return backendError.map((error) => (error.message ? error.message : ErrorMapper.DEFAULT_ERROR_MESSAGE));
+    }
+    return backendError?.message || ErrorMapper.DEFAULT_ERROR_MESSAGE;
   }
 }
