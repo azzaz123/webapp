1e8bfe36593a7aec881601b40c9e86669cab730f
Laia Lopez <laialopezz99@gmail.com>
2021-06-01 15:35:50 +0200

    [WPA-1668]: [CardComponent] Created

---
+++ src/app/private/features/delivery/pages/card/card.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UuidService } from '@core/uuid/uuid.service';
+import { ProfileFormComponent } from '@shared/profile/profile-form/profile-form.component';
+
+@Component({
+  selector: 'tsl-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
+})
+export class CardComponent implements OnInit {
+  @ViewChild(ProfileFormComponent, { static: true }) formComponent: ProfileFormComponent;
+
+  public cardForm: FormGroup;
+  public loading = false;
+  public formErrorMessages;
+
+  constructor(private fb: FormBuilder, private uuidService: UuidService) {}
+
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  public initForm(): void {}
+
+  public onSubmit(): void {}
+
+  private buildForm(): void {
+    this.cardForm = this.fb.group({
+      id: this.uuidService.getUUID(),
+      holder_name: ['', Validators.required],
+      number: ['', [Validators.required]],
+      expiration_date: ['', Validators.required],
+      cvv: ['', [Validators.required]],
+    });
+  }
+}
