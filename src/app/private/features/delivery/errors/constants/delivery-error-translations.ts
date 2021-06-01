df43c8d53fff3126aa1bb3eab3ba97b46053a753
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-06-01 12:40:23 +0200

    [WPA-1749]: Fix typo

--- src/app/private/features/delivery/errors/constants/delivery-error-translations.ts
+++ src/app/private/features/delivery/errors/constants/delivery-error-translations.ts
@@ -6,6 +6,6 @@ export const DeliveryPostalCodesErrorTranslations = {
 
 export const DeliveryAddressErrorTranslations = {
   PHONE_NUMBER_INVALID: $localize`:@@web_delivery_address_invalid_phone_number_error:This phone number is invalid`,
-  TOO_LONG_HINT: $localize`:@@web_delivery_address_too_long_hint_error:That's is too long! Please write it shorter.`,
+  TOO_LONG_HINT: $localize`:@@web_delivery_address_too_long_hint_error:That's too long! Please write it shorter.`,
   PHONE_MISSMATCH_LOCATION: $localize`:@@web_address_view_all_users_form_textfield_phone_missmatch_location_inline_error:The contact number must be from the country you are located`,
 };
