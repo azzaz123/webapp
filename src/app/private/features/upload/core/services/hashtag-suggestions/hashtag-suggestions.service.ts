ccd23ac9dfe60d6a8a00519cf8469bb342899eb1
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-06-02 16:35:26 +0200

    [FIN-1929]: Created getHashTagSuggestors

--- src/app/private/features/upload/core/services/hashtag-suggestions/hashtag-suggestions.service.ts
+++ src/app/private/features/upload/core/services/hashtag-suggestions/hashtag-suggestions.service.ts
@@ -1,8 +1,19 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '@environments/environment.beta';
+import { Observable } from 'rxjs';
+
+export const HASHTAG_SUGGESTERS_API_URL = `/api/v3/suggesters/hashtags`;
 
 @Injectable({
   providedIn: 'root',
 })
 export class HashtagSuggestionsService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  public getHashTagSuggestors(category_id: string, prefix?: string, start: string = '0'): Observable<any> {
+    const url = `${environment.baseUrl}${HASHTAG_SUGGESTERS_API_URL}`;
+    let httpParams: HttpParams = new HttpParams({ fromObject: { category_id, prefix, start } });
+    return this.http.get(url, { params: httpParams });
+  }
 }
