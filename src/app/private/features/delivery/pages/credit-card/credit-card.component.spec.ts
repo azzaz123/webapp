f2f4298f200fb4b4af1a1ca5b9cc534d0bb46db4
Laia Lopez <laialopezz99@gmail.com>
2021-06-02 10:24:17 +0200

    [WPA-1668]: [CreditCard] Added NumbersOnlyDirective

--- src/app/private/features/delivery/pages/credit-card/credit-card.component.spec.ts
+++ src/app/private/features/delivery/pages/credit-card/credit-card.component.spec.ts
@@ -2,6 +2,7 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { UuidService } from '@core/uuid/uuid.service';
+import { NumbersOnlyDirective } from '@shared/directives/numbers-only/numbers-only.directive';
 import { ProfileFormComponent } from '@shared/profile/profile-form/profile-form.component';
 
 import { CreditCardComponent } from './credit-card.component';
@@ -13,7 +14,7 @@ describe('CreditCreditCardComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ReactiveFormsModule],
-      declarations: [CreditCardComponent, ProfileFormComponent],
+      declarations: [CreditCardComponent, ProfileFormComponent, NumbersOnlyDirective],
       providers: [
         FormBuilder,
         {
--- src/app/private/features/delivery/pages/credit-card/credit-card.module.ts
+++ src/app/private/features/delivery/pages/credit-card/credit-card.module.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from '@shared/button/button.module';
+import { NumbersOnlyDirective } from '@shared/directives/numbers-only/numbers-only.directive';
 import { ProfileFormModule } from '@shared/profile/profile-form/profile-form.module';
 import { SpinnerModule } from '@shared/spinner/spinner.module';
 import { creditCardRoutedComponents, CreditCardRoutingModule } from './credit-card.routing.module';
 
 @NgModule({
   imports: [CreditCardRoutingModule, CommonModule, SpinnerModule, ReactiveFormsModule, ProfileFormModule, ButtonModule],
-  declarations: [creditCardRoutedComponents],
+  declarations: [creditCardRoutedComponents, NumbersOnlyDirective],
 })
 export class CreditCardModule {}
