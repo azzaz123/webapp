e3370c6477c1a76bccccb47607445c2062b444ca
Laia Lopez <laialopezz99@gmail.com>
2021-06-03 16:19:11 +0200

    [WPA-1668]: [NumbersOnlyDirective] Refactor

--- src/app/shared/directives/numbers-only/numbers-only.directive.spec.ts
+++ src/app/shared/directives/numbers-only/numbers-only.directive.spec.ts
@@ -1,6 +1,5 @@
-import { Component, DebugElement } from '@angular/core';
-import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { NumbersOnlyDirective } from './numbers-only.directive';
 
@@ -10,13 +9,9 @@ import { NumbersOnlyDirective } from './numbers-only.directive';
 class TestComponent {}
 
 describe('NumbersOnlyDirective', () => {
-  const NUMERIC_VALUE = '1234';
-  const VARCHAR_VALUE = 'ABCD';
-  const ALPHANUMERIC_VALUE = '1A2B3C4D';
-
   let component: TestComponent;
   let fixture: ComponentFixture<TestComponent>;
-  let input: DebugElement;
+  let input: HTMLElement;
 
   beforeEach(
     waitForAsync(() => {
@@ -29,45 +24,53 @@ describe('NumbersOnlyDirective', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TestComponent);
     component = fixture.componentInstance;
-    input = fixture.debugElement.query(By.css('input'));
+    input = fixture.debugElement.query(By.directive(NumbersOnlyDirective)).nativeElement;
   });
 
   it('should create an instance', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('when the input value changes...', () => {
+  describe('when the key down have been pulsed...', () => {
     describe('and the new character is a number...', () => {
-      it('should keep it on the value', fakeAsync(() => {
-        input.nativeElement.value = VARCHAR_VALUE;
+      it('should keep it on the value', () => {
+        expect(keyPress('4').defaultPrevented).toBe(false);
+      });
+    });
 
-        input.nativeElement.dispatchEvent(new Event('input'));
-        tick();
+    describe('and the new key is the delete one ...', () => {
+      it('should let us to use it', () => {
+        expect(keyPress('Backspace').defaultPrevented).toBe(false);
+      });
+    });
 
-        expect(input.nativeElement.value).toBe('');
-      }));
+    describe('and the new character is an space...', () => {
+      it('should revert the character', () => {
+        expect(keyPress(' ').defaultPrevented).toBe(true);
+      });
     });
 
     describe('and the new character is NOT a number...', () => {
-      it('should delete it on the value', fakeAsync(() => {
-        input.nativeElement.value = NUMERIC_VALUE;
-
-        input.nativeElement.dispatchEvent(new Event('input'));
-        tick();
-
-        expect(input.nativeElement.value).toBe(NUMERIC_VALUE);
-      }));
-    });
-
-    describe('and the input is filled by letters and numbers...', () => {
-      it('should only keep the numbers', fakeAsync(() => {
-        input.nativeElement.value = ALPHANUMERIC_VALUE;
-
-        input.nativeElement.dispatchEvent(new Event('input'));
-        tick();
-
-        expect(input.nativeElement.value).toBe(NUMERIC_VALUE);
-      }));
+      it('should revert the character', () => {
+        expect(keyPress('a').defaultPrevented).toBe(true);
+      });
     });
   });
+
+  function keyPress(k: string): KeyboardEvent {
+    const event: any = document.createEvent('KeyboardEvent');
+    Object.defineProperty(event, 'key', {
+      get: function (): string {
+        return this.keyVal;
+      },
+      set: function (k: string): void {
+        this.keyVal = k;
+      },
+    });
+
+    event.initEvent('keydown', true, true);
+    event.key = k;
+    input.dispatchEvent(event);
+    return event;
+  }
 });
--- src/app/shared/directives/numbers-only/numbers-only.directive.ts
+++ src/app/shared/directives/numbers-only/numbers-only.directive.ts
@@ -1,16 +1,16 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[tslNumbersOnly]',
 })
 export class NumbersOnlyDirective {
-  constructor(private el: ElementRef) {}
-
-  @HostListener('input', ['$event']) onInputChange(event: Event) {
-    const initalValue = this.el.nativeElement.value;
-    this.el.nativeElement.value = initalValue.replace(/[^0-9]*/g, '');
-    if (initalValue !== this.el.nativeElement.value) {
-      event.stopPropagation();
+  @HostListener('keydown', ['$event']) onKeyDown(e: KeyboardEvent) {
+    const key = e.key;
+    if (key === 'Backspace') {
+      return;
+    }
+    if (isNaN(Number(key)) || key === ' ') {
+      e.preventDefault();
     }
   }
 }
