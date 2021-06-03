cf00dbedd070e4ae0918ee9d54e96e6706c57d7f
Laia Lopez <laialopezz99@gmail.com>
2021-06-03 15:47:06 +0200

    [WPA-1668]: [SeparateWordByCharacterModule] Implemented

--- src/app/private/features/delivery/pages/credit-card/credit-card.module.ts
+++ src/app/private/features/delivery/pages/credit-card/credit-card.module.ts
@@ -3,12 +3,21 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from '@shared/button/button.module';
 import { NumbersOnlyDirective } from '@shared/directives/numbers-only/numbers-only.directive';
+import { SeparateWordByCharacterModule } from '@shared/pipes/separate-word-by-character/separate-word-by-character.module';
 import { ProfileFormModule } from '@shared/profile/profile-form/profile-form.module';
 import { SpinnerModule } from '@shared/spinner/spinner.module';
 import { creditCardRoutedComponents, CreditCardRoutingModule } from './credit-card.routing.module';
 
 @NgModule({
-  imports: [CreditCardRoutingModule, CommonModule, SpinnerModule, ReactiveFormsModule, ProfileFormModule, ButtonModule],
+  imports: [
+    CreditCardRoutingModule,
+    CommonModule,
+    SpinnerModule,
+    ReactiveFormsModule,
+    ProfileFormModule,
+    ButtonModule,
+    SeparateWordByCharacterModule,
+  ],
   declarations: [creditCardRoutedComponents, NumbersOnlyDirective],
 })
 export class CreditCardModule {}
