bf31a2f7d967ecbcd8c2874344e074694011019c
Laia Lopez <laialopezz99@gmail.com>
2021-06-04 13:10:12 +0200

    [WPA-1668]: [NumbersOnlyDirective] Rename + Refactor

--- src/app/shared/directives/numbers-only/numbers-only.directive.ts
+++ src/app/shared/directives/numbers-only/numbers-only.directive.ts
@@ -6,10 +6,11 @@ import { Directive, HostListener } from '@angular/core';
 export class NumbersOnlyDirective {
   @HostListener('keydown', ['$event']) onKeyDown(e: KeyboardEvent) {
     const key = e.key;
-    if (key === 'Backspace') {
-      return;
-    }
-    if (isNaN(Number(key)) || key === ' ') {
+    const isBackSpace = key === 'Backspace';
+    const isNotANumber = isNaN(Number(key));
+    const isASpace = key === ' ';
+
+    if ((isNotANumber || isASpace) && !isBackSpace) {
       e.preventDefault();
     }
   }
