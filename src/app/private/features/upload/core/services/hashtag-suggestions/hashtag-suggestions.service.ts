7dd693331df03f0e792cad97f82d2437ba770323
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-06-07 00:18:07 +0200

    [FIN-1929]: Refactor code

--- src/app/private/features/upload/core/services/hashtag-suggestions/hashtag-suggestions.service.ts
+++ src/app/private/features/upload/core/services/hashtag-suggestions/hashtag-suggestions.service.ts
@@ -4,8 +4,6 @@ import { environment } from '@environments/environment';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { HashtagSuggester, HashtagSuggesterResponse } from '../../models/hashtag-suggester.interface';
-import { MOCK_HASHTAG_SUGGESTERS } from '../../../../../../../tests/hashtag-suggester.fixtures.spec';
-//import { MOCK_HASHTAG_SUGGESTERS } from '@fixtures/hashtag-suggester.fixtures.spec';
 
 export const HASHTAG_SUGGESTERS_API = `api/v3/suggesters/hashtags`;
 export const GENERAL_HASHTAG_SUGGESTERS_API = `${HASHTAG_SUGGESTERS_API}/general`;
@@ -15,45 +13,28 @@ export class HashtagSuggestionsService {
   public readonly suggestors: HashtagSuggester[] = [];
   constructor(private http: HttpClient) {}
 
-  private getHashTagSuggesters(category_id: string, start: string, prefix?: string): Observable<HttpResponse<HashtagSuggesterResponse>> {
-    const url = `${environment.baseUrl}${HASHTAG_SUGGESTERS_API}`;
-    let httpParams: HttpParams = new HttpParams({ fromObject: { category_id, prefix, start } });
-    return this.http.get<HashtagSuggesterResponse>(url, { params: httpParams, observe: 'response' });
-  }
-
-  private getGeneralHashTagSuggesters(
+  private getHashTagSuggesters(
     category_id: string,
     start: string,
+    isGernalHashTag: boolean,
     prefix?: string
   ): Observable<HttpResponse<HashtagSuggesterResponse>> {
-    const url = `${environment.baseUrl}${GENERAL_HASHTAG_SUGGESTERS_API}`;
+    const url = isGernalHashTag
+      ? `${environment.baseUrl}${GENERAL_HASHTAG_SUGGESTERS_API}`
+      : `${environment.baseUrl}${HASHTAG_SUGGESTERS_API}`;
     let httpParams: HttpParams = new HttpParams({ fromObject: { category_id, prefix, start } });
     return this.http.get<HashtagSuggesterResponse>(url, { params: httpParams, observe: 'response' });
   }
 
   public loadHashtagSugessters(
     categoryId: number,
+    isGernalHashTag: boolean,
     prefix?: string,
     page: number = 0
   ): Observable<{ data: HashtagSuggester[]; nextPage: boolean }> {
     const category_id = categoryId.toString();
     const start = page.toString();
-    return this.getHashTagSuggesters(category_id, start, prefix).pipe(
-      map((response) => {
-        const nextPage = !!response.headers.get('x-nextpage');
-        return { data: response.body.hashtags, nextPage: nextPage };
-      })
-    );
-  }
-
-  public loadGeneralHashtagSugessters(
-    categoryId: number,
-    prefix?: string,
-    page: number = 0
-  ): Observable<{ data: HashtagSuggester[]; nextPage: boolean }> {
-    const category_id = categoryId.toString();
-    const start = page.toString();
-    return this.getGeneralHashTagSuggesters(category_id, start, prefix).pipe(
+    return this.getHashTagSuggesters(category_id, start, isGernalHashTag, prefix).pipe(
       map((response) => {
         const nextPage = !!response.headers.get('x-nextpage');
         return { data: response.body.hashtags, nextPage: nextPage };
