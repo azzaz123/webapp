9f777001e830c52e7992eaa4ac057380360ca0b7
Gonzalo Morales <gonzalomorales93@gmail.com>
2021-06-07 12:52:39 +0200

    [PQP-3835]: fix app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'tsl-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
-}
+export class AppComponent {}
