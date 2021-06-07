533b338d1bcd18db6e7241e0c6384e468b52d552
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-06-07 14:42:40 +0200

    [FIN-1929]: Correcting the environment import

--- src/app/private/features/upload/core/services/hashtag-suggestions/hashtag-suggestions.service.spec.ts
+++ src/app/private/features/upload/core/services/hashtag-suggestions/hashtag-suggestions.service.spec.ts
@@ -1,7 +1,7 @@
 import { HttpHeaders } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { environment } from '@environments/environment.beta';
+import { environment } from '@environments/environment';
 import {
   MOCK_HASHTAG_SUGGESTERS,
   MOCK_HASHTAG_SUGGESTERS_RESPONSE,
