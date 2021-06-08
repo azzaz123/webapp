5d88b30e76c9df3a9b056e6526b677ffceceb9f7
Laia Lopez <laialopezz99@gmail.com>
2021-06-08 18:25:00 +0200

    [WPA-1806]: [Style] Refactor

--- src/app/shared/credit-card-info/credit-card-info.component.ts
+++ src/app/shared/credit-card-info/credit-card-info.component.ts
@@ -10,6 +10,7 @@ export class CreditCardInfoComponent implements OnChanges {
   @Input() brand: string;
   @Input() numberCard: string;
   @Input() expireDate: Date;
+  @Input() ownerName: string;
   @Input() error: boolean;
   @Input() hideEdit: boolean;
   @Input() hideDelete: boolean;
@@ -18,15 +19,17 @@ export class CreditCardInfoComponent implements OnChanges {
   @Output() deleteCardClick: EventEmitter<FinancialCard> = new EventEmitter();
 
   public creditCardBrandSrc: string;
+  public isMasterCardOrVisa: boolean;
 
   constructor() {}
 
   ngOnChanges() {
+    this.isMasterCardOrVisa = this.brand === 'mastercard' || this.brand === 'visa';
     this.checkCreditCardBrandSrc();
   }
 
   private checkCreditCardBrandSrc(): void {
-    const brandCard = this.brand !== 'visa' && this.brand !== 'mastercard' ? 'card' : `card-${this.brand}`;
+    const brandCard = this.isMasterCardOrVisa ? `card-${this.brand}` : 'card';
     this.creditCardBrandSrc = `/assets/icons/${brandCard}.svg`;
   }
 }
