fbb8c64d6f6731df3f938a722ed62177b3cbc149
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-06-10 19:57:01 +0200

    [FIN-1929]: Refactor the fixture

--- src/tests/hashtag-suggester.fixtures.spec.ts
+++ src/tests/hashtag-suggester.fixtures.spec.ts
@@ -1,30 +1,15 @@
-import { HashtagSuggester, HashtagSuggesterResponse } from '../app/private/features/upload/core/models/hashtag-suggester.interface';
+import { Hashtag, GeneralHashtag, HashtagResponse } from '../app/private/features/upload/core/models/hashtag-suggester.interface';
 
-export const MOCK_PREFIX_HASHTAG_SUGGESTERS: string = 'aa';
+export const MOCK_PREFIX_HASHTAG: string = 'aa';
 
-export const MOCK_HASHTAG_SUGGESTERS: HashtagSuggester[] = [
-  { text: 'aa', ocurrencies: 14 },
-  { text: 'aabb', ocurrencies: 10 },
-  { text: 'aaag', ocurrencies: 2 },
-  { text: 'aaba', ocurrencies: 5 },
-  { text: 'aafa', ocurrencies: 13 },
-  { text: 'aab', ocurrencies: 18 },
-  { text: 'aaf', ocurrencies: 1 },
-  { text: 'aasf', ocurrencies: 1 },
-  { text: 'aaeaf', ocurrencies: 1 },
-  { text: 'aafd', ocurrencies: 1 },
-  { text: 'aafs', ocurrencies: 1 },
-  { text: 'aafq', ocurrencies: 1 },
-  { text: 'aasaf', ocurrencies: 1 },
-  { text: 'aafe', ocurrencies: 1 },
-  { text: 'aaf', ocurrencies: 1 },
-  { text: 'aawadf', ocurrencies: 1 },
-  { text: 'aasaf', ocurrencies: 1 },
-  { text: 'aadf', ocurrencies: 1 },
-  { text: 'aadsf', ocurrencies: 1 },
+export const MOCK_HASHTAGS: Hashtag[] = [{ text: 'ab' }, { text: 'abc' }];
+
+export const MOCK_GENERAL_HASHTAGS: GeneralHashtag[] = [
+  { text: MOCK_HASHTAGS[0].text, ocurrencies: 1 },
+  { text: MOCK_HASHTAGS[1].text, ocurrencies: 2 },
 ];
 
-export const MOCK_HASHTAG_SUGGESTERS_RESPONSE: HashtagSuggesterResponse = {
-  prefix: MOCK_PREFIX_HASHTAG_SUGGESTERS,
-  hashtags: MOCK_HASHTAG_SUGGESTERS,
+export const MOCK_HASHTAG_RESPONSE: HashtagResponse = {
+  prefix: MOCK_PREFIX_HASHTAG,
+  hashtags: MOCK_GENERAL_HASHTAGS,
 };
