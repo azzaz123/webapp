412cddd3c64ecd10d7b305164da5dd5eb6f37de1
Laia Lopez <laialopezz99@gmail.com>
2021-06-10 18:34:51 +0200

    [WPA-1806]: [Translations] Errors

--- src/app/private/features/delivery/errors/constants/delivery-error-translations.ts
+++ src/app/private/features/delivery/errors/constants/delivery-error-translations.ts
@@ -1,11 +1,12 @@
 export const DeliveryPostalCodesErrorTranslations = {
-  INVALID_POSTAL_CODE: $localize`:@@web_delivery_address_invalid_postal_code_error:This postal code isn't valid`,
-  POSTAL_CODE_NOT_ALLOWED: $localize`:@@web_delivery_address_postal_code_not_allowed_error:Postal code not accepted`,
-  POSTAL_CODE_DOES_NOT_EXIST: $localize`:@@web_delivery_address_postal_code_not_exist_error:Postal code doesn't exist`,
+  INVALID_POSTAL_CODE: $localize`:@@address_view_all_users_form_textfield_postalcode_not_valid_after_save_inline_error:This ZIP code isn't valid`,
+  POSTAL_CODE_NOT_ALLOWED: $localize`:@@address_view_all_users_form_textfield_postalcode_not_allowed_before_save_inline_error:This ZIP code doesn't allow to proceed with the shipment`,
+  POSTAL_CODE_DOES_NOT_EXIST: $localize`:@@address_view_all_users_form_textfield_postalcode_not_exist_inline_error:This ZIP doesn’t belong to the country where you are located`,
 };
 
 export const DeliveryAddressErrorTranslations = {
-  PHONE_NUMBER_INVALID: $localize`:@@web_delivery_address_invalid_phone_number_error:This phone number is invalid`,
-  TOO_LONG_HINT: $localize`:@@web_delivery_address_too_long_hint_error:That's too long! Please write it shorter.`,
+  PHONE_NUMBER_INVALID: $localize`:@@address_view_all_users_form_textfield_phone_not_valid_inline_error:This phone number isn't valid`,
+  ADDRESS_TOO_LONG_HINT: $localize`:@@address_view_all_users_form_textfield_street_too_long_info_inline_error:Too many characters... Try to shorten the text`,
+  FLAT_AND_FLOOR_TOO_LONG_HINT: $localize`:@@address_view_all_users_form_textfield_door_too_long_info_inline_error:Too many characters... Try to shorten the text`,
   PHONE_MISSMATCH_LOCATION: $localize`:@@web_address_view_all_users_form_textfield_phone_missmatch_location_inline_error:The contact number must be from the country you are located`,
 };
