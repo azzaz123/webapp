dd89b0cd9fa0df36a595dc7474ce0c997cb38bd4
Gonzalo Morales <gonzalomorales93@gmail.com>
2021-06-11 12:39:10 +0200

    [PQP-3612]: add hide subscriptions in public profile

--- src/app/public/layout/components/footer/footer.component.ts
+++ src/app/public/layout/components/footer/footer.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AnalyticsEvent, ANALYTICS_EVENT_NAMES, ANALYTIC_EVENT_TYPES, ClickProInfo, SCREEN_IDS } from '@core/analytics/analytics-constants';
 import { AnalyticsService } from '@core/analytics/analytics.service';
 import { I18nService } from '@core/i18n/i18n.service';
+import { PERMISSIONS } from '@core/user/user-constants';
+import { NgxPermissionsObject, NgxPermissionsService } from 'ngx-permissions';
+import { take } from 'rxjs/operators';
 import { FOOTER_APPS, FOOTER_LINKS, FOOTER_SOCIAL } from './constants/footer-constants';
 import { FooterIcon, FooterLink, FooterLinkSection } from './interfaces/footer.interface';
 
@@ -10,18 +13,34 @@ import { FooterIcon, FooterLink, FooterLinkSection } from './interfaces/footer.i
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
   public currentYear = new Date().getFullYear();
-  public readonly FOOTER_LINKS: FooterLinkSection[] = FOOTER_LINKS;
+  public FOOTER_LINKS: FooterLinkSection[] = FOOTER_LINKS;
   public readonly FOOTER_APPS: FooterIcon[] = FOOTER_APPS;
   public readonly FOOTER_SOCIAL: FooterIcon[] = FOOTER_SOCIAL;
   private trackingEventsConfig: { [key in ANALYTICS_EVENT_NAMES]?: { (): void } } = {
     [ANALYTICS_EVENT_NAMES.ClickProInfo]: () => this.trackClickProInfo(),
   };
 
-  constructor(private i18nService: I18nService, private analyticsService: AnalyticsService) {
-    this.FOOTER_LINKS = FOOTER_LINKS.filter((footerLinkSection: FooterLinkSection) => {
-      return !(footerLinkSection.excludedLanguages || []).includes(this.i18nService.locale);
+  constructor(
+    private i18nService: I18nService,
+    private analyticsService: AnalyticsService,
+    private permissionService: NgxPermissionsService
+  ) {}
+
+  ngOnInit() {
+    this.permissionService.permissions$.pipe(take(1)).subscribe((permissions) => {
+      let links = FOOTER_LINKS.filter((footerLinkSection: FooterLinkSection) => {
+        return !(footerLinkSection.excludedLanguages || []).includes(this.i18nService.locale);
+      });
+      links = this.filterByPermissions(permissions, links);
+      this.FOOTER_LINKS = links;
+    });
+  }
+
+  private filterByPermissions(permissions: NgxPermissionsObject, links: FooterLinkSection[]) {
+    return links.filter((footerLinkSection: FooterLinkSection) => {
+      return !footerLinkSection.permission || permissions[footerLinkSection.permission];
     });
   }
 
