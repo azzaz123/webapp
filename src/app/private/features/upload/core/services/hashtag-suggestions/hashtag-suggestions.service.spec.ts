3a0c1bfd65d483bed425cadc7623f022ba191732
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-06-11 08:36:16 +0200

    [FIN-1929]: Refactor the test

--- src/app/private/features/upload/core/services/hashtag-suggestions/hashtag-suggestions.service.spec.ts
+++ src/app/private/features/upload/core/services/hashtag-suggestions/hashtag-suggestions.service.spec.ts
@@ -2,11 +2,7 @@ import { HttpHeaders } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { environment } from '@environments/environment';
-import {
-  MOCK_HASHTAG_SUGGESTERS,
-  MOCK_HASHTAG_SUGGESTERS_RESPONSE,
-  MOCK_PREFIX_HASHTAG_SUGGESTERS,
-} from '@fixtures/hashtag-suggester.fixtures.spec';
+import { MOCK_HASHTAG_RESPONSE, MOCK_PREFIX_HASHTAG } from '@fixtures/hashtag-suggester.fixtures.spec';
 import { NEXT_HEADER_PAGE } from '@public/features/search/core/services/infrastructure/api/search-api.service';
 
 import { GENERAL_HASHTAG_SUGGESTERS_API, HashtagSuggestionsService, HASHTAG_SUGGESTERS_API } from './hashtag-suggestions.service';
@@ -30,47 +26,28 @@ describe('HashtagSuggestionsService', () => {
 
   describe('when we load hashtags...', () => {
     it('if they are general hashtags, we should load general hashtags', () => {
-      const expectedUrl = `${environment.baseUrl}${GENERAL_HASHTAG_SUGGESTERS_API}?category_id=100&prefix=${MOCK_PREFIX_HASHTAG_SUGGESTERS}&start=0`;
+      const expectedUrl = `${environment.baseUrl}${GENERAL_HASHTAG_SUGGESTERS_API}?category_id=100&prefix=${MOCK_PREFIX_HASHTAG}&start=0`;
 
-      service.loadHashtagSugessters(100, true, MOCK_PREFIX_HASHTAG_SUGGESTERS).subscribe((response) => {
-        expect(response).toEqual(MOCK_HASHTAG_SUGGESTERS_RESPONSE);
+      service.loadGeneralHashtags(100, MOCK_PREFIX_HASHTAG).subscribe((response) => {
+        expect(response).toEqual(MOCK_HASHTAG_RESPONSE);
       });
 
       const request = httpMock.expectOne(expectedUrl);
       expect(request.request.method).toBe('GET');
-      request.flush(MOCK_HASHTAG_SUGGESTERS_RESPONSE, {
-        headers: new HttpHeaders().set(NEXT_HEADER_PAGE, 'category_id=100&start=20'),
-      });
+      request.flush(MOCK_HASHTAG_RESPONSE);
     });
 
     it('if they are not general hashtags, we should load hashtags that are not from general endpoint', () => {
-      const expectedUrl = `${environment.baseUrl}${HASHTAG_SUGGESTERS_API}?category_id=100&prefix=${MOCK_PREFIX_HASHTAG_SUGGESTERS}&start=0`;
+      const expectedUrl = `${environment.baseUrl}${HASHTAG_SUGGESTERS_API}?category_id=100&prefix=${MOCK_PREFIX_HASHTAG}&start=0`;
 
-      service.loadHashtagSugessters(100, false, MOCK_PREFIX_HASHTAG_SUGGESTERS).subscribe((response) => {
-        expect(response).toEqual(MOCK_HASHTAG_SUGGESTERS_RESPONSE);
+      service.loadHashtags(100, MOCK_PREFIX_HASHTAG).subscribe((response) => {
+        console.log('response', response);
+        expect(response).toEqual(MOCK_HASHTAG_RESPONSE);
       });
 
       const request = httpMock.expectOne(expectedUrl);
       expect(request.request.method).toBe('GET');
-      request.flush(MOCK_HASHTAG_SUGGESTERS_RESPONSE, {
-        headers: new HttpHeaders().set(NEXT_HEADER_PAGE, 'category_id=100&start=20'),
-      });
-    });
-  });
-
-  describe('when we add hashTag', () => {
-    it('should offer us the selected hashtags with the new added hashtag', () => {
-      service.addSuggesters(MOCK_HASHTAG_SUGGESTERS[0]);
-
-      expect(service.selectedSuggesters).toEqual([MOCK_HASHTAG_SUGGESTERS[0]]);
-    });
-  });
-
-  describe('when we delete hashTag', () => {
-    it('should offer us the selected hashtags without the hashtag we want to delete', () => {
-      service.deleteSuggesters(MOCK_HASHTAG_SUGGESTERS[0]);
-
-      expect(service.selectedSuggesters).toEqual([]);
+      request.flush(MOCK_HASHTAG_RESPONSE);
     });
   });
 });
