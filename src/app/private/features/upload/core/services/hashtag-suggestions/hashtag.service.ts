1db0ba686c6df2538fcb12d4bbfca735610d069e
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-06-14 15:28:22 +0200

    [FIN-1929]: Refactor hashtag service

--- src/app/private/features/upload/core/services/hashtag-suggestions/hashtag.service.ts
+++ src/app/private/features/upload/core/services/hashtag-suggestions/hashtag.service.ts
@@ -1,52 +1,35 @@
 import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
+import { PaginationResponse } from '@public/core/services/pagination/pagination.interface';
+import { PaginationService } from '@public/core/services/pagination/pagination.service';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { HashtagResponse, Hashtag, HashtagWithPrefixResponse, HashtagWithOccurenciesInfo } from '../../models/hashtag-suggester.interface';
+import { HashtagResponse, Hashtag } from '../../models/hashtag.interface';
 
 export const HASHTAG_SUGGESTERS_API = `api/v3/suggesters/hashtags`;
 export const GENERAL_HASHTAG_SUGGESTERS_API = `${HASHTAG_SUGGESTERS_API}/general`;
 
 @Injectable()
 export class HashtagService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private paginationService: PaginationService) {}
 
-  private getHashtagsByPrefix(category_id: string, start: string, prefix: string): Observable<HttpResponse<HashtagWithPrefixResponse>> {
+  private getHashtagsByPrefix(category_id: string, start: string, prefix: string): Observable<HttpResponse<HashtagResponse>> {
     const url = `${environment.baseUrl}${GENERAL_HASHTAG_SUGGESTERS_API}`;
     let httpParams: HttpParams = new HttpParams({ fromObject: { category_id, prefix, start } });
-    return this.http.get<HashtagWithPrefixResponse>(url, { params: httpParams, observe: 'response' });
-  }
-
-  private getlHashtags(category_id: string, start: string): Observable<HttpResponse<HashtagResponse>> {
-    const url = `${environment.baseUrl}${HASHTAG_SUGGESTERS_API}`;
-    let httpParams: HttpParams = new HttpParams({ fromObject: { category_id, start } });
     return this.http.get<HashtagResponse>(url, { params: httpParams, observe: 'response' });
   }
 
-  public loadHashtags(categoryId: number, page: number = 0): Observable<{ data: Hashtag[]; nextPage: boolean }> {
-    const category_id = categoryId.toString();
-    const start = page.toString();
-    return this.getlHashtags(category_id, start).pipe(
-      map((response) => {
-        const nextPage = !!response.headers.get('x-nextpage');
-        return { data: response.body.hashtags, nextPage: nextPage };
-      })
-    );
+  private getHashtags(category_id: string, start: string): Observable<HttpResponse<HashtagResponse>> {
+    const url = `${environment.baseUrl}${HASHTAG_SUGGESTERS_API}`;
+    let httpParams: HttpParams = new HttpParams({ fromObject: { category_id, prefix: null, start } });
+    return this.http.get<HashtagResponse>(url, { params: httpParams, observe: 'response' });
   }
 
-  public loadHashtagsByPrefix(
-    categoryId: number,
-    prefix: string = null,
-    page: number = 0
-  ): Observable<{ data: HashtagWithOccurenciesInfo[]; nextPage: boolean }> {
+  public getPaginationHashtags(categoryId: number, page: number = 0, prefix?: string): Observable<PaginationResponse<Hashtag[]>> {
     const category_id = categoryId.toString();
     const start = page.toString();
-    return this.getHashtagsByPrefix(category_id, start, prefix).pipe(
-      map((response) => {
-        const nextPage = !!response.headers.get('x-nextpage');
-        return { data: response.body.hashtags, nextPage: nextPage };
-      })
+    return this.paginationService.getItems(
+      prefix ? this.getHashtagsByPrefix(category_id, start, prefix) : this.getHashtags(category_id, start)
     );
   }
 }
