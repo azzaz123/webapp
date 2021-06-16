9fe1813b6093d8a38b45cdbfb5768d02ab7cc5c1
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-06-17 00:12:58 +0200

    [FIN-1930]: [WIP] Multiselect

--- src/app/shared/form/components/multi-select-form/multi-select-form.component.ts
+++ src/app/shared/form/components/multi-select-form/multi-select-form.component.ts
@@ -1,12 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { AbstractFormComponent } from '@shared/form/abstract-form/abstract-form-component';
+import { SelectFormOption } from '../select/interfaces/select-form-option.interface';
+import { SelectFormComponent } from '../select/select-form.component';
 import { ComplexSelectValue } from '../select/types/complex-select-value';
 
 @Component({
   selector: 'tsl-multi-select-form',
   templateUrl: './multi-select-form.component.html',
   styleUrls: ['./multi-select-form.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => MultiSelectFormComponent),
+      multi: true,
+    },
+  ],
 })
 export class MultiSelectFormComponent extends AbstractFormComponent<ComplexSelectValue> {
   @Input() options: string[];
+  @Input() isDisabled: boolean = false;
+
+  /* constructor() {
+    super()
+    console.log('options', this.options);
+  } */
 }
--- src/app/shared/form/components/multi-select-form/multi-select-form.module.ts
+++ src/app/shared/form/components/multi-select-form/multi-select-form.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MultiSelectFormComponent } from './multi-select-form.component';
+import { SelectFormModule } from '../select/select-form.module';
 
 @NgModule({
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, SelectFormModule],
   declarations: [MultiSelectFormComponent],
   exports: [MultiSelectFormComponent],
 })
