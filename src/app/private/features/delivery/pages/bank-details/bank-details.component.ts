8705cf74413dd4585961663e2c9a18328bfcfac4
Laia Lopez <laialopezz99@gmail.com>
2021-06-17 20:11:11 +0200

    [WPA-1664]: [BankDetails] Controller logic

--- src/app/private/features/delivery/pages/bank-details/bank-details.component.ts
+++ src/app/private/features/delivery/pages/bank-details/bank-details.component.ts
@@ -1,30 +1,79 @@
-import { Component, OnInit } from '@angular/core';
-import { finalize } from 'rxjs/operators';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ConfirmationModalProperties } from '@shared/confirmation-modal/confirmation-modal.interface';
+import { ConfirmationModalComponent } from '@shared/confirmation-modal/confirmation-modal.component';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { BankAccountService } from '../../services/bank-account/bank-account.service';
+import { TRANSLATION_KEY } from '@core/i18n/translations/enum/translation-keys.enum';
+import { DELIVERY_PATHS } from '../../delivery-routing-constants';
+import { PRIVATE_PATHS } from '@private/private-routing-constants';
 import { BankAccount } from '../../interfaces/bank-account/bank-account-api.interface';
-import { BankDetailsService } from '../../services/bank-details/bank-details.service';
+import { I18nService } from '@core/i18n/i18n.service';
+import { Observable } from 'rxjs';
+import { COLORS } from '@core/colors/colors-constants';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'tsl-bank-details',
   templateUrl: './bank-details.component.html',
   styleUrls: ['./bank-details.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BankDetailsComponent implements OnInit {
-  public bankAccount: BankAccount;
-  public creditCard: any;
-  public loading = true;
+  public bankAccount$: Observable<BankAccount>;
+  public ADD_CREDIT_CARD_LINK = '';
+  public ADD_BANK_ACCOUNT_LINK = '';
 
-  constructor(private bankDetailsService: BankDetailsService) {}
+  constructor(
+    private router: Router,
+    private bankAccountService: BankAccountService,
+    private i18nService: I18nService,
+    private modalService: NgbModal
+  ) {}
 
   ngOnInit(): void {
-    this.requestBankDetailsInfo();
+    this.bankAccount$ = this.bankAccountService.get();
   }
 
-  private requestBankDetailsInfo(): void {
-    this.bankDetailsService
-      .get()
-      .pipe(finalize(() => (this.loading = false)))
-      .subscribe(([bankAccount, creditCard]: [BankAccount, any]) => {
-        (this.bankAccount = bankAccount), (this.creditCard = creditCard);
-      });
+  public editBankAccount(): void {
+    this.router.navigate([`${PRIVATE_PATHS.DELIVERY}/${DELIVERY_PATHS.SHIPMENT_TRACKING}`]);
+  }
+
+  public openDeleteBankAccountModal(): void {
+    this.generateConfirmationModalRef({
+      description: this.i18nService.translate(TRANSLATION_KEY.ACTIVE),
+      confirmMessage: this.i18nService.translate(TRANSLATION_KEY.ACTIVE),
+      cancelMessage: this.i18nService.translate(TRANSLATION_KEY.ACTIVE),
+      confirmColor: COLORS.NEGATIVE_MAIN,
+    }).result.then(() => {
+      this.deleteBankAccount();
+    });
+  }
+
+  public openDeleteCardModal(): void {
+    this.generateConfirmationModalRef({
+      description: this.i18nService.translate(TRANSLATION_KEY.ACTIVE),
+      confirmMessage: this.i18nService.translate(TRANSLATION_KEY.ACTIVE),
+      cancelMessage: this.i18nService.translate(TRANSLATION_KEY.ACTIVE),
+      confirmColor: COLORS.NEGATIVE_MAIN,
+    }).result.then(() => {
+      this.deleteCard();
+    });
+  }
+
+  private deleteCard(): void {}
+
+  private deleteBankAccount(): void {
+    this.bankAccountService.delete().subscribe(
+      () => {},
+      () => {}
+    );
+  }
+
+  private generateConfirmationModalRef(properties: ConfirmationModalProperties): NgbModalRef {
+    const modalRef: NgbModalRef = this.modalService.open(ConfirmationModalComponent);
+
+    modalRef.componentInstance.properties = properties;
+
+    return modalRef;
   }
 }
