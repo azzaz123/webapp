6db931ca619fc4cb9d32202ab94df395df2b86ab
Pablo Macías Moreira <pablo.macias.moreira@gmail.com>
2021-06-18 10:18:54 +0200

    [WPA-1841]: Remove unused ad native component

--- src/app/shared/ads/ad-slot/ad-slot.module.ts
+++ src/app/shared/ads/ad-slot/ad-slot.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
-import { AdNativeSlotComponent } from './ad-native-slot/ad-native-slot.component';
 import { AdSlotComponent } from './ad-slot.component';
 
 @NgModule({
-  declarations: [AdSlotComponent, AdNativeSlotComponent],
-  exports: [AdSlotComponent, AdNativeSlotComponent],
+  declarations: [AdSlotComponent],
+  exports: [AdSlotComponent],
 })
 export class AdSlotModule {}
