c4591b6ffb354257ec641de1f51342e69646ca51
Laia Lopez <laialopezz99@gmail.com>
2021-06-21 12:45:50 +0200

    [WPA-1664]: [BankDetails] Refactor

--- src/app/private/features/delivery/pages/bank-details/bank-details.component.ts
+++ src/app/private/features/delivery/pages/bank-details/bank-details.component.ts
@@ -1,79 +1,8 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { ConfirmationModalProperties } from '@shared/confirmation-modal/confirmation-modal.interface';
-import { ConfirmationModalComponent } from '@shared/confirmation-modal/confirmation-modal.component';
-import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
-import { BankAccountService } from '../../services/bank-account/bank-account.service';
-import { TRANSLATION_KEY } from '@core/i18n/translations/enum/translation-keys.enum';
-import { DELIVERY_PATHS } from '../../delivery-routing-constants';
-import { PRIVATE_PATHS } from '@private/private-routing-constants';
-import { BankAccount } from '../../interfaces/bank-account/bank-account-api.interface';
-import { I18nService } from '@core/i18n/i18n.service';
-import { Observable } from 'rxjs';
-import { COLORS } from '@core/colors/colors-constants';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'tsl-bank-details',
   templateUrl: './bank-details.component.html',
   styleUrls: ['./bank-details.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BankDetailsComponent implements OnInit {
-  public bankAccount$: Observable<BankAccount>;
-  public CREDIT_CARD_FORM_LINK = `${PRIVATE_PATHS.DELIVERY}/${DELIVERY_PATHS.CREDIT_CARD}`;
-  public BANK_ACCOUNT_FORM_LINK = `${PRIVATE_PATHS.DELIVERY}/${DELIVERY_PATHS.BANK_ACCOUNT}`;
-
-  constructor(
-    private router: Router,
-    private bankAccountService: BankAccountService,
-    private i18nService: I18nService,
-    private modalService: NgbModal
-  ) {}
-
-  ngOnInit(): void {
-    this.bankAccount$ = this.bankAccountService.get();
-  }
-
-  public redirect(URL: string): void {
-    this.router.navigate([URL]);
-  }
-
-  public openDeleteBankAccountModal(): void {
-    this.generateConfirmationModalRef({
-      description: this.i18nService.translate(TRANSLATION_KEY.ACTIVE),
-      confirmMessage: this.i18nService.translate(TRANSLATION_KEY.ACTIVE),
-      cancelMessage: this.i18nService.translate(TRANSLATION_KEY.ACTIVE),
-      confirmColor: COLORS.NEGATIVE_MAIN,
-    }).result.then(() => {
-      this.deleteBankAccount();
-    });
-  }
-
-  public openDeleteCardModal(): void {
-    this.generateConfirmationModalRef({
-      description: this.i18nService.translate(TRANSLATION_KEY.ACTIVE),
-      confirmMessage: this.i18nService.translate(TRANSLATION_KEY.ACTIVE),
-      cancelMessage: this.i18nService.translate(TRANSLATION_KEY.ACTIVE),
-      confirmColor: COLORS.NEGATIVE_MAIN,
-    }).result.then(() => {
-      this.deleteCard();
-    });
-  }
-
-  private deleteCard(): void {}
-
-  private deleteBankAccount(): void {
-    this.bankAccountService.delete().subscribe(
-      () => {},
-      () => {}
-    );
-  }
-
-  private generateConfirmationModalRef(properties: ConfirmationModalProperties): NgbModalRef {
-    const modalRef: NgbModalRef = this.modalService.open(ConfirmationModalComponent);
-
-    modalRef.componentInstance.properties = properties;
-
-    return modalRef;
-  }
-}
+export class BankDetailsComponent {}
--- src/app/private/features/delivery/pages/bank-details/bank-details.module.ts
+++ src/app/private/features/delivery/pages/bank-details/bank-details.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bankDetailsRoutedComponents, BankDetailsRoutingModule } from './bank-details.routing.module';
-import { CreditCardInfoModule } from '@shared/credit-card-info/credit-card-info.module';
-import { BankAccountService } from '../../services/bank-account/bank-account.service';
-import { BankAccountApiService } from '../../services/api/bank-account-api/bank-account-api.service';
-import { MapBankAccountService } from '../../services/bank-account/map-bank-account/map-bank-account.service';
-import { AddCreditCardModule } from '@shared/add-credit-card/add-credit-card.module';
 
 @NgModule({
   declarations: [bankDetailsRoutedComponents],
-  imports: [BankDetailsRoutingModule, CommonModule, CreditCardInfoModule, AddCreditCardModule],
-  providers: [BankAccountService, BankAccountApiService, MapBankAccountService],
+  imports: [BankDetailsRoutingModule, CommonModule],
 })
 export class BankDetailsModule {}
