b1a8efa8f087eb8a51f3ccf23da23e32927bd8e2
Irene Blanco <irene.blanco@wallapop.com>
2021-06-22 09:52:58 +0200

    [FIN-1914]: SearchPagination - Added sortBy to the interface

--- src/app/public/features/search/interfaces/search-pagination.interface.ts
+++ src/app/public/features/search/interfaces/search-pagination.interface.ts
@@ -1,9 +1,11 @@
 import { ItemCard } from '@public/core/interfaces/item-card.interface';
+import { SORT_BY } from '../components/sort-filter/services/constants/sort-by-options-constants';
 
 export interface SearchPagination {
   items: ItemCard[];
   hasMore: boolean;
   searchId: string;
+  sortBy: SORT_BY;
 }
 
 export interface SearchPaginationWithCategory extends SearchPagination {
