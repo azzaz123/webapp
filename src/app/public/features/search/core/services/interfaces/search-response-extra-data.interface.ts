c577625470d5c4f61e1b0f34ba9caae33d19b227
Irene Blanco <irene.blanco@wallapop.com>
2021-06-22 09:56:08 +0200

    [FIN-1914]: Created SearchResponseExtraData interface

---
+++ src/app/public/features/search/core/services/interfaces/search-response-extra-data.interface.ts
@@ -0,0 +1,6 @@
+import { SORT_BY } from '@public/features/search/components/sort-filter/services/constants/sort-by-options-constants';
+
+export interface SearchResponseExtraData {
+  searchId: string;
+  sortBy: SORT_BY;
+}
