06c79b22d61c8d905f1175c3c03f29d9e24e47f2
Pablo Macías Moreira <pablo.macias.moreira@gmail.com>
2021-06-22 18:00:20 +0200

    [WPA-1841]: Refactored test

--- src/app/shared/ads/ad-slot/ad-slot-group.directive.spec.ts
+++ src/app/shared/ads/ad-slot/ad-slot-group.directive.spec.ts
@@ -1,90 +1,41 @@
 import { AdSlotGroupDirective } from './ad-slot-group.directive';
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { Component } from '@angular/core';
 import { AdSlotConfiguration } from '@core/ads/models';
 import { AdsService } from '@core/ads/services';
-import { MockAdsService } from '@fixtures/ads.fixtures.spec';
-import spyOn = jest.spyOn;
-import { AdSlotModule } from '@shared/ads/ad-slot/ad-slot.module';
+import { of } from 'rxjs/internal/observable/of';
+import { QueryList } from '@angular/core';
+import { AdSlotComponent } from '@shared/ads/ad-slot/ad-slot.component';
 
-const immediateAdSlot: AdSlotConfiguration = {
-  id: 'slot',
-  name: 'slot',
+const adSlot1: AdSlotConfiguration = {
+  id: 'slot-1',
+  name: 'slot-1',
   device: [],
   networkId: 0,
   sizes: [],
 };
 
-const unrenderedAdSlot: AdSlotConfiguration = {
-  id: 'unrendered-slot',
-  name: 'unrendered-slot',
+const adSlot2: AdSlotConfiguration = {
+  id: 'slot-2',
+  name: 'slot-2',
   device: [],
   networkId: 0,
   sizes: [],
 };
-
-const innerAdSlot: AdSlotConfiguration = {
-  id: 'inner-slot',
-  name: 'inner-slot',
-  device: [],
-  networkId: 0,
-  sizes: [],
-};
-
-@Component({
-  selector: 'tsl-test-component',
-  template: ` <div tslAdSlotGroup>
-    <tsl-sky [adSlot]="adSlot"></tsl-sky>
-    <tsl-sky *ngIf="false" [adSlot]="unrenderedAdSlot"></tsl-sky>
-    <tsl-inner-test-component>
-      <tsl-sky [adSlot]="innerAdSlot"></tsl-sky>
-    </tsl-inner-test-component>
-  </div>`,
-})
-class TestComponent {
-  public adSlot: AdSlotConfiguration = immediateAdSlot;
-  public innerAdSlot: AdSlotConfiguration = innerAdSlot;
-  public unrenderedAdSlot: AdSlotConfiguration = unrenderedAdSlot;
-}
-
-@Component({
-  selector: 'tsl-inner-test-component',
-  template: '<ng-content></ng-content>',
-})
-class InnerTestComponent {}
 
 describe('AdSlotGroupDirective', () => {
-  let fixture: ComponentFixture<TestComponent>;
-  let adsService: AdsService;
+  const adsServiceMock: AdsService = ({ setSlots: () => {}, displayAdBySlotId: () => {} } as unknown) as AdsService;
 
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        declarations: [AdSlotGroupDirective, TestComponent, InnerTestComponent],
-        providers: [{ provide: AdsService, useValue: MockAdsService }],
-        imports: [AdSlotModule],
-      }).compileComponents();
-    })
-  );
+  it('should set all slots and ask for display on the first one', () => {
+    spyOn(adsServiceMock, 'setSlots');
+    spyOn(adsServiceMock, 'displayAdBySlotId');
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TestComponent);
-    adsService = TestBed.inject(AdsService);
-  });
+    const directive = new AdSlotGroupDirective(adsServiceMock);
+    directive.slotsQuery = ({ changes: of([adSlot1, adSlot2]) } as unknown) as QueryList<AdSlotComponent>;
 
-  it('should set slots ONLY to rendered slots', () => {
-    spyOn(adsService, 'setSlots');
-    fixture.detectChanges();
+    directive.ngAfterContentInit();
 
-    expect(adsService.setSlots).toHaveBeenCalledTimes(1);
-    expect(adsService.setSlots).toHaveBeenCalledWith([immediateAdSlot, innerAdSlot]);
-  });
-
-  it('should ask to display ONLY the first rendered slot in the group', () => {
-    spyOn(adsService, 'displayAdBySlotId');
-    fixture.detectChanges();
-
-    expect(adsService.displayAdBySlotId).toHaveBeenCalledTimes(1);
-    expect(adsService.displayAdBySlotId).toHaveBeenCalledWith(immediateAdSlot.id);
+    expect(adsServiceMock.setSlots).toHaveBeenCalledTimes(1);
+    expect(adsServiceMock.setSlots).toHaveBeenCalledWith([adSlot1, adSlot2]);
+    expect(adsServiceMock.displayAdBySlotId).toHaveBeenCalledTimes(1);
+    expect(adsServiceMock.displayAdBySlotId).toHaveBeenCalledWith(adSlot1.id);
   });
 });
