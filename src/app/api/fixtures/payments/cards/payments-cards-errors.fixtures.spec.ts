d27d0732d9bb0c53f4b059709765333d74d4f0d4
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-06-22 15:18:49 +0200

    [WPA-1865]: Create fixtures for payments cards network errors

---
+++ src/app/api/fixtures/payments/cards/payments-cards-errors.fixtures.spec.ts
@@ -0,0 +1,112 @@
+import { CardIsNotAuthorizedError, CardOwnerNameIsInvalidError, PaymentsCardsError } from '@api/core/errors/payments/cards';
+import { MOCK_ERROR_RESPONSE } from '@api/fixtures/error-response-api.fixtures.spec';
+import { PaymentsCardsErrorResponseApi } from '@api/payments/cards/dtos/errors/payments-cards-error-response-api.interface';
+import { PAYMENTS_CARDS_ERROR_CODES } from '@api/payments/cards/mappers/errors/payments-cards-error-codes.enum';
+import { PaymentsErrorApi } from '@api/payments/dtos/errors';
+
+export const MOCK_PAYMENTS_CARDS_BASE_ERROR_RESPONSE: PaymentsCardsErrorResponseApi = {
+  ...MOCK_ERROR_RESPONSE,
+  error: [],
+};
+
+export const MOCK_PAYMENTS_CARDS_ERRORS: PaymentsCardsError[] = [new CardOwnerNameIsInvalidError(), new CardIsNotAuthorizedError()];
+
+export const MOCK_PAYMENTS_CARDS_ERROR_CARD_COUNTRY_IS_INVALID: PaymentsErrorApi<PAYMENTS_CARDS_ERROR_CODES> = {
+  error_code: PAYMENTS_CARDS_ERROR_CODES.CARD_COUNTRY_IS_INVALID,
+  message: 'Sorry, Catalunya is not Spain ( ͡° ͜ʖ ͡°)',
+};
+
+export const MOCK_PAYMENTS_CARDS_ERROR_CARD_IS_NOT_AUTHORIZED: PaymentsErrorApi<PAYMENTS_CARDS_ERROR_CODES> = {
+  error_code: PAYMENTS_CARDS_ERROR_CODES.CARD_IS_NOT_AUTHORIZED,
+  message: 'You need the black card in order to proceed',
+};
+
+export const MOCK_PAYMENTS_CARDS_ERROR_CARD_NOT_FOUND: PaymentsErrorApi<PAYMENTS_CARDS_ERROR_CODES> = {
+  error_code: PAYMENTS_CARDS_ERROR_CODES.CARD_NOT_FOUND,
+  message: 'Card was not found :/',
+};
+
+export const MOCK_PAYMENTS_CARDS_ERROR_CARD_OWNER_IS_INVALID: PaymentsErrorApi<PAYMENTS_CARDS_ERROR_CODES> = {
+  error_code: PAYMENTS_CARDS_ERROR_CODES.CARD_OWNER_IS_INVALID,
+  message: 'Card owner is invalid',
+};
+
+export const MOCK_PAYMENTS_CARDS_ERROR_CARD_OWNER_NAME_IS_INVALID: PaymentsErrorApi<PAYMENTS_CARDS_ERROR_CODES> = {
+  error_code: PAYMENTS_CARDS_ERROR_CODES.CARD_OWNER_NAME_IS_INVALID,
+  message: 'Card owner name is invalid',
+};
+
+export const MOCK_PAYMENTS_CARDS_ERROR_CARD_REGISTRATION_FAILED: PaymentsErrorApi<PAYMENTS_CARDS_ERROR_CODES> = {
+  error_code: PAYMENTS_CARDS_ERROR_CODES.CARD_REGISTRATION_FAILED,
+  message: 'Card registration failed',
+};
+
+export const MOCK_PAYMENTS_CARDS_ERROR_CARD_REGISTRATION_IS_INVALID: PaymentsErrorApi<PAYMENTS_CARDS_ERROR_CODES> = {
+  error_code: PAYMENTS_CARDS_ERROR_CODES.CARD_REGISTRATION_IS_INVALID,
+  message: 'Card registration is invalid',
+};
+
+export const MOCK_PAYMENTS_CARDS_ERROR_CARD_TOKENIZATION_FAILED: PaymentsErrorApi<PAYMENTS_CARDS_ERROR_CODES> = {
+  error_code: PAYMENTS_CARDS_ERROR_CODES.CARD_TOKENIZATION_FAILED,
+  message: 'Card tokenization failed. Sumimasen.',
+};
+
+export const MOCK_PAYMENTS_CARDS_ERROR_COUNTRY_ISO_CODE_IS_INVALID: PaymentsErrorApi<PAYMENTS_CARDS_ERROR_CODES> = {
+  error_code: PAYMENTS_CARDS_ERROR_CODES.COUNTRY_ISO_CODE_IS_INVALID,
+  message: 'Country ISO code is not valid, wtf',
+};
+
+export const MOCK_PAYMENTS_CARDS_ERROR_PLATFORM_RESPONSE_IS_INVALID: PaymentsErrorApi<PAYMENTS_CARDS_ERROR_CODES> = {
+  error_code: PAYMENTS_CARDS_ERROR_CODES.PLATFORM_RESPONSE_IS_INVALID,
+  message: 'Platform response is invalid',
+};
+
+export const MOCK_PAYMENTS_CARDS_ERROR_UNIQUE_CARD_FOR_USER: PaymentsErrorApi<PAYMENTS_CARDS_ERROR_CODES> = {
+  error_code: PAYMENTS_CARDS_ERROR_CODES.UNIQUE_CARD_FOR_USER,
+  message: 'Unique card for user violation :x',
+};
+
+export const MOCK_PAYMENTS_CARDS_ERROR_CARD_COUNTRY_IS_INVALID_RESPONSE: PaymentsCardsErrorResponseApi = {
+  ...MOCK_PAYMENTS_CARDS_BASE_ERROR_RESPONSE,
+  error: [MOCK_PAYMENTS_CARDS_ERROR_CARD_COUNTRY_IS_INVALID],
+};
+
+export const MOCK_PAYMENTS_CARDS_ERROR_CARD_IS_NOT_AUTHORIZED_RESPONSE: PaymentsCardsErrorResponseApi = {
+  ...MOCK_PAYMENTS_CARDS_BASE_ERROR_RESPONSE,
+  error: [MOCK_PAYMENTS_CARDS_ERROR_CARD_IS_NOT_AUTHORIZED],
+};
+
+export const MOCK_PAYMENTS_CARDS_ERROR_CARD_NOT_FOUND_RESPONSE: PaymentsCardsErrorResponseApi = {
+  ...MOCK_PAYMENTS_CARDS_BASE_ERROR_RESPONSE,
+  error: [MOCK_PAYMENTS_CARDS_ERROR_CARD_NOT_FOUND],
+};
+
+export const MOCK_PAYMENTS_CARDS_ERROR_CARD_OWNER_IS_INVALID_RESPONSE: PaymentsCardsErrorResponseApi = {
+  ...MOCK_PAYMENTS_CARDS_BASE_ERROR_RESPONSE,
+  error: [MOCK_PAYMENTS_CARDS_ERROR_CARD_OWNER_IS_INVALID],
+};
+
+export const MOCK_PAYMENTS_CARDS_ERROR_CARD_OWNER_NAME_IS_INVALID_RESPONSE: PaymentsCardsErrorResponseApi = {
+  ...MOCK_PAYMENTS_CARDS_BASE_ERROR_RESPONSE,
+  error: [MOCK_PAYMENTS_CARDS_ERROR_CARD_OWNER_NAME_IS_INVALID],
+};
+
+export const MOCK_PAYMENTS_CARDS_ERROR_CARD_REGISTRATION_FAILED_RESPONSE: PaymentsCardsErrorResponseApi = {
+  ...MOCK_PAYMENTS_CARDS_BASE_ERROR_RESPONSE,
+  error: [MOCK_PAYMENTS_CARDS_ERROR_CARD_REGISTRATION_FAILED],
+};
+
+export const MOCK_PAYMENTS_CARDS_ERROR_CARD_REGISTRATION_IS_INVALID_RESPONSE: PaymentsCardsErrorResponseApi = {
+  ...MOCK_PAYMENTS_CARDS_BASE_ERROR_RESPONSE,
+  error: [MOCK_PAYMENTS_CARDS_ERROR_CARD_REGISTRATION_IS_INVALID],
+};
+
+export const MOCK_PAYMENTS_CARDS_ERROR_PLATFORM_RESPONSE_IS_INVALID_RESPONSE: PaymentsCardsErrorResponseApi = {
+  ...MOCK_PAYMENTS_CARDS_BASE_ERROR_RESPONSE,
+  error: [MOCK_PAYMENTS_CARDS_ERROR_PLATFORM_RESPONSE_IS_INVALID],
+};
+
+export const MOCK_PAYMENTS_CARDS_ERROR_UNIQUE_CARD_FOR_USER_RESPONSE: PaymentsCardsErrorResponseApi = {
+  ...MOCK_PAYMENTS_CARDS_BASE_ERROR_RESPONSE,
+  error: [MOCK_PAYMENTS_CARDS_ERROR_UNIQUE_CARD_FOR_USER],
+};
