e50ee1ce506c1265a4a6173e6707bda3acd6f95a
Pablo Macías Moreira <pablo.macias.moreira@gmail.com>
2021-06-22 15:51:52 +0200

    [WPA-1841]: Moved logic into subscription to avoid race condition

--- src/app/shared/ads/ad-slot/ad-slot-group.directive.ts
+++ src/app/shared/ads/ad-slot/ad-slot-group.directive.ts
@@ -1,32 +1,28 @@
-import { Directive } from '@angular/core';
-import { AfterContentInit, AfterViewInit, ContentChildren, QueryList } from '@angular/core';
+import { AfterContentInit, Directive } from '@angular/core';
+import { ContentChildren, QueryList } from '@angular/core';
 import { AdsService } from '@core/ads/services';
 import { AdSlotComponent } from '@shared/ads/ad-slot/ad-slot.component';
-import { AdSlotConfiguration } from '@core/ads/models';
+import { take } from 'rxjs/operators';
 
 @Directive({
   selector: '[tslAdSlotGroup]',
 })
-export class AdSlotGroupDirective implements AfterContentInit, AfterViewInit {
+export class AdSlotGroupDirective implements AfterContentInit {
   @ContentChildren(AdSlotComponent, { descendants: true }) slotsQuery!: QueryList<AdSlotComponent>;
-
-  private slotConfigurations: AdSlotConfiguration[];
 
   constructor(private adsService: AdsService) {}
 
   public ngAfterContentInit(): void {
-    this.slotConfigurations = this.slotsQuery.toArray().map((component) => component.adSlot);
+    this.slotsQuery.changes.pipe(take(1)).subscribe((components: AdSlotComponent[]) => {
+      const configurations = components.map((component) => component.adSlot);
 
-    if (this.slotConfigurations.length) {
-      this.adsService.setSlots(this.slotConfigurations);
-    }
-  }
+      this.adsService.setSlots(configurations);
 
-  public ngAfterViewInit(): void {
-    if (this.slotConfigurations.length) {
-      // When handling ads with SRA, we only need to call display once per group, using any of the slots in the group.
-      // Check https://developers.google.com/publisher-tag/guides/ad-best-practices for more information
-      this.adsService.displayAdBySlotId(this.slotConfigurations[0].id);
-    }
+      if (configurations.length) {
+        // When handling ads with SRA, we only need to call display once per group, using any of the slots in the group.
+        // Check https://developers.google.com/publisher-tag/guides/ad-best-practices for more information
+        this.adsService.displayAdBySlotId(configurations[0].id);
+      }
+    });
   }
 }
