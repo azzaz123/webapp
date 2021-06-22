816583f2ec37070024f5de1931bcf6c1682bbd61
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-06-22 10:50:43 +0200

    [WPA-1865]: Update temporal translations

--- src/app/api/core/errors/payments/cards/payments-cards-error-translations.ts
+++ src/app/api/core/errors/payments/cards/payments-cards-error-translations.ts
@@ -1,4 +1,6 @@
 // FIXME: Temporal copies
 export const PAYMENTS_CARDS_ERROR_TRANSLATIONS = {
-  CARD_ERROR: $localize`@@payments_error_card: ops! It seems something went wrong`,
+  CARD_ERROR: $localize`@@payment_error_card: Ouch, there is an error with the bank card`,
+  CARD_REFUSED: $localize`@@payment_error_card_invalid_holder_name: Invalid card holder name`,
+  HOLDER_NAME_IS_INVALID: $localize`@@payment_error_card_refused: Credit or debit card refused`,
 };
