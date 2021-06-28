69cd88306898f526ddda3ec019ad26474debaffa
Irene Blanco <irene.blanco@wallapop.com>
2021-06-28 15:25:54 +0200

    [FIN-1984]: Created shipping rules fixtures

---
+++ src/app/api/fixtures/delivery/shipping-rules.fixtures.ts
@@ -0,0 +1,25 @@
+import { ShippingRulesResponse } from '@api/delivery/interfaces/shipping-rules-response.interface';
+import { ShippingRules } from '@api/delivery/interfaces/shipping-rules.interface';
+
+const categories = [1, 2, 3];
+const subcategories = [4, 5, 6];
+const maxPrice = 500;
+const minPrice = 200;
+
+export const shippingRulesResponseFixture: ShippingRulesResponse = {
+  categories_with_shipping_not_allowed: categories,
+  subcategories_with_shipping_not_allowed: subcategories,
+  price_range_allowed: {
+    max_price: maxPrice,
+    min_price: minPrice,
+  },
+};
+
+export const mappedShippingRulesFixture: ShippingRules = {
+  categoriesNotAllowed: categories,
+  subcategoriesNotAllowed: subcategories,
+  priceRangeAllowed: {
+    maxPrice: maxPrice,
+    minPrice: minPrice,
+  },
+};
