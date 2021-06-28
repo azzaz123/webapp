3f26a40cb7d14a89bce92430dc27abb4581f90e6
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-06-28 09:54:05 +0200

    [FIN-1930]: Style checkbox

--- src/app/shared/form/components/multi-select-form/multi-select-option/multi-select-option/multi-select-option.module.ts
+++ src/app/shared/form/components/multi-select-form/multi-select-option/multi-select-option/multi-select-option.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { SvgIconModule } from '@shared/svg-icon/svg-icon.module';
 import { MultiSelectOptionComponent } from './multi-select-option.component';
 
 @NgModule({
-  imports: [FormsModule],
+  imports: [FormsModule, SvgIconModule],
   declarations: [MultiSelectOptionComponent],
   exports: [MultiSelectOptionComponent],
 })
