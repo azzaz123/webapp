012719a4899f7671c75c149830db44d8a9e8bf59
Laia Lopez <laialopezz99@gmail.com>
2021-06-29 12:12:44 +0200

    [WPA-2407]: [Kyc] Component Created + Routing Module Update

---
+++ src/app/private/features/delivery/pages/kyc/kyc.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tsl-kyc',
+  templateUrl: './kyc.component.html',
+  styleUrls: ['./kyc.component.scss'],
+})
+export class KycComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
