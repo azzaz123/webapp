1c3686f86a7898093efdbc28edae1b609769c12f
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-06-30 00:07:03 +0200

    [FIN-1930]: Removed unnecessary imports

--- src/app/shared/form/components/multi-select-form/multi-select-form.module.ts
+++ src/app/shared/form/components/multi-select-form/multi-select-form.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MultiSelectFormComponent } from './multi-select-form.component';
-import { SelectFormModule } from '../select/select-form.module';
 import { MultiSelectOptionModule } from './multi-select-option/multi-select-option/multi-select-option.module';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, SelectFormModule, MultiSelectOptionModule],
+  imports: [CommonModule, FormsModule, MultiSelectOptionModule],
   declarations: [MultiSelectFormComponent],
   exports: [MultiSelectFormComponent],
 })
