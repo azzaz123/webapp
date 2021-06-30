ce321452ea72deae1a4f2365cd746782dda93de0
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-06-30 13:06:05 +0200

    [FIN-1930]: Testing multi select form

--- src/app/shared/form/components/multi-select-form/multi-select-form.component.spec.ts
+++ src/app/shared/form/components/multi-select-form/multi-select-form.component.spec.ts
@@ -1,24 +1,90 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CommonModule } from '@angular/common';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ChangeDetectionStrategy, DebugElement } from '@angular/core';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { SelectFormOption } from '../select/interfaces/select-form-option.interface';
+import { SelectOptionComponent } from '../select/select-option/select-option.component';
 import { MultiSelectFormComponent } from './multi-select-form.component';
+import { MultiSelectOptionComponent } from './multi-select-option/multi-select-option/multi-select-option.component';
+import { MultiSelectOptionModule } from './multi-select-option/multi-select-option/multi-select-option.module';
+
+export const multiSelectedOptionsFixture: SelectFormOption<string>[] = [
+  { label: 'aa', value: 'aa' },
+  { label: 'bb', value: 'bb' },
+  { label: 'cc', value: 'cc' },
+  { label: 'dd', value: 'dd' },
+];
 
 describe('MultiSelectFormComponent', () => {
   let component: MultiSelectFormComponent;
+  let debugElement: DebugElement;
   let fixture: ComponentFixture<MultiSelectFormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [CommonModule, HttpClientTestingModule, FormsModule, MultiSelectOptionModule],
       declarations: [MultiSelectFormComponent],
-    }).compileComponents();
+    })
+      .overrideComponent(MultiSelectFormComponent, {
+        set: { changeDetection: ChangeDetectionStrategy.Default },
+      })
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MultiSelectFormComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+    component.options = multiSelectedOptionsFixture;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show load the options mapped with checked value', () => {
+    const expectedOptions = multiSelectedOptionsFixture.map((option) => {
+      return { ...option, checked: false };
+    });
+    fixture.detectChanges();
+
+    const options = debugElement.queryAll(By.directive(MultiSelectOptionComponent));
+
+    expect(options.length).toBe(multiSelectedOptionsFixture.length);
+    expect(component.extendedOptions).toEqual(expectedOptions);
+  });
+
+  it('should disable the checking behavior if we stop letting users to select options', () => {
+    component.isDisabled = true;
+    fixture.detectChanges();
+
+    const options = debugElement.queryAll(By.directive(MultiSelectOptionComponent));
+    const isDisabled = options.find((option) => {
+      option.componentInstance.isDisabled === false;
+    });
+
+    expect(isDisabled).toBeFalsy();
+  });
+
+  describe('Multi select behavior', () => {
+    it('should have multiple values when multiple options are checked', () => {
+      component.options = multiSelectedOptionsFixture;
+      fixture.detectChanges();
+      spyOn(component, 'handleSelectedOption');
+      spyOn(component, 'onChange');
+
+      const option1 = debugElement.queryAll(By.directive(MultiSelectOptionComponent))[0].componentInstance;
+      const option2 = debugElement.queryAll(By.directive(MultiSelectOptionComponent))[1].componentInstance;
+      option1.toggleOnChange.emit();
+      option2.toggleOnChange.emit();
+
+      expect(component.handleSelectedOption).toHaveBeenCalled();
+      expect(component.extendedOptions).toBe('');
+      //expect(component.onChange).toHaveBeenCalledWith('');
+      // expect(component.value).toBe('');
+    });
   });
 });
--- src/app/shared/form/components/multi-select-form/multi-select-option/multi-select-option/multi-select-option.module.ts
+++ src/app/shared/form/components/multi-select-form/multi-select-option/multi-select-option/multi-select-option.module.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SvgIconModule } from '@shared/svg-icon/svg-icon.module';
 import { MultiSelectOptionComponent } from './multi-select-option.component';
 
 @NgModule({
-  imports: [FormsModule, SvgIconModule],
+  imports: [CommonModule, FormsModule, SvgIconModule],
   declarations: [MultiSelectOptionComponent],
   exports: [MultiSelectOptionComponent],
 })
