d556cd3630bc42afad19ce5aa070fb84cb404567
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-07-01 09:27:09 +0200

    [FIN-1930]: Removed unused import

--- src/app/shared/form/components/multi-select-form/multi-select-option/multi-select-option/multi-select-option.component.ts
+++ src/app/shared/form/components/multi-select-form/multi-select-option/multi-select-option/multi-select-option.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 import { MultiSelectFormOption } from '../../multi-select-form.component';
 
 @Component({
