b8216bcdf406aaf893674377a05f5f2d765c740b
Laia Lopez <laialopezz99@gmail.com>
2021-07-01 14:21:34 +0200

    [WPA-2514]: [Delivery]  Wallet + Bank Details Moved

--- src/app/private/features/delivery/delivery-routing-constants.ts
+++ src/app/private/features/delivery/delivery-routing-constants.ts
@@ -5,11 +5,9 @@ export enum DELIVERY_PATHS {
   TIMELINE = 'timeline',
   KYC = 'kyc',
   SHIPMENT_TRACKING = 'shipments',
-  BANK_DETAILS = 'bank-details',
   BANK_ACCOUNT = 'bank-account',
   DISPUTE = 'dispute',
   CREDIT_CARD = 'card',
-  WALLET = 'wallet',
 }
 
 export enum DELIVERY_PATH_PARAMS {}
