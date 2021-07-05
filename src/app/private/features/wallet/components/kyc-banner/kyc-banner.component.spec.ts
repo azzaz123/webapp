3ef92dd8f43ced7a713d04b21b6fbd7740e092c1
Laia Lopez <laialopezz99@gmail.com>
2021-07-05 13:29:32 +0200

    [WPA-1787]: [Testing] KycBanner + Banner

--- src/app/private/features/wallet/components/kyc-banner/kyc-banner.component.spec.ts
+++ src/app/private/features/wallet/components/kyc-banner/kyc-banner.component.spec.ts
@@ -1,26 +1,37 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ChangeDetectionStrategy, DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { BannerComponent } from '@shared/banner/banner.component';
 import { BannerModule } from '@shared/banner/banner.module';
+import { ButtonComponent } from '@shared/button/button.component';
 import { ButtonModule } from '@shared/button/button.module';
+import { SvgIconComponent } from '@shared/svg-icon/svg-icon.component';
 import { SvgIconModule } from '@shared/svg-icon/svg-icon.module';
 import { KYC_BANNER_TYPES } from './kyc-banner-constants';
 import { KycBannerComponent } from './kyc-banner.component';
 
 describe('KycBannerComponent', () => {
+  const MOCK_KYC_BANNER_SPECIFICATIONS = KYC_BANNER_TYPES[0];
   let component: KycBannerComponent;
+  let debugElement: DebugElement;
   let fixture: ComponentFixture<KycBannerComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [KycBannerComponent],
+      declarations: [KycBannerComponent, BannerComponent, SvgIconComponent],
       imports: [SvgIconModule, ButtonModule, HttpClientTestingModule, BannerModule],
-    }).compileComponents();
+    })
+      .overrideComponent(KycBannerComponent, {
+        set: { changeDetection: ChangeDetectionStrategy.Default },
+      })
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(KycBannerComponent);
     component = fixture.componentInstance;
-    component.specifications = KYC_BANNER_TYPES[0];
+    debugElement = fixture.debugElement;
 
     fixture.detectChanges();
   });
@@ -29,10 +40,50 @@ describe('KycBannerComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  describe('when we do not specify the banner specifications...', () => {
+    beforeEach(() => {
+      component.specifications = null;
+
+      fixture.detectChanges();
+    });
+
+    it('should not show the kyc banner', () => {
+      const banner = debugElement.query(By.directive(BannerComponent));
+
+      expect(banner).toBeFalsy();
+    });
+  });
+
   describe('when we specify the KYC banner specifications', () => {
-    it('should get the correct banner specifications', () => {});
-    it('should show the specified svg', () => {});
-    it('should show the specified description', () => {});
-    it('should show the specified button text', () => {});
+    beforeEach(() => {
+      component.specifications = MOCK_KYC_BANNER_SPECIFICATIONS;
+
+      fixture.detectChanges();
+    });
+
+    it('should show the kyc banner', () => {
+      const banner = debugElement.query(By.directive(BannerComponent));
+
+      expect(banner).toBeTruthy();
+    });
+
+    it('should get the correct banner specifications', () => {
+      expect(component.bannerSpecifications).toStrictEqual({
+        dismissible: MOCK_KYC_BANNER_SPECIFICATIONS.dismissible,
+        type: MOCK_KYC_BANNER_SPECIFICATIONS.type,
+      });
+    });
+
+    it('should show the svg', () => {
+      const icon = debugElement.query(By.directive(SvgIconComponent));
+
+      expect(icon).toBeTruthy();
+    });
+
+    it('should show the specified button text', () => {
+      const button: HTMLElement = debugElement.query(By.directive(ButtonComponent)).nativeElement;
+
+      expect(button.textContent).toEqual(component.specifications.buttonText);
+    });
   });
 });
--- src/app/shared/banner/banner.component.spec.ts
+++ src/app/shared/banner/banner.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
 
 import { BannerComponent } from './banner.component';
 
@@ -8,7 +10,7 @@ describe('BannerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [BannerComponent],
+      declarations: [BannerComponent, NgbAlert],
     }).compileComponents();
   });
 
@@ -22,12 +24,56 @@ describe('BannerComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('when the user define the banner specifications...', () => {
-    it('should apply the specified class', () => {});
+  describe('when we do not define the banner specifications...', () => {
+    beforeEach(() => {
+      component.specifications = null;
+
+      fixture.detectChanges();
+    });
+
+    it('should not show the banner', () => {
+      const banner = fixture.debugElement.query(By.directive(NgbAlert));
+
+      expect(banner).toBeFalsy();
+    });
+  });
+
+  describe('when we define the banner specifications...', () => {
+    beforeEach(() => {
+      component.specifications = {
+        dismissible: false,
+        type: 'success',
+      };
+
+      fixture.detectChanges();
+    });
+
+    it('should show the banner', () => {
+      const banner = fixture.debugElement.query(By.directive(NgbAlert));
+
+      expect(banner).toBeTruthy();
+    });
+
+    it('should apply the specified class', () => {
+      const styledBanner = fixture.debugElement.query(By.css(`.alert-${component.specifications.type}`));
+
+      expect(styledBanner).toBeTruthy();
+    });
 
     describe('and the banner is dismissible', () => {
+      beforeEach(() => {
+        spyOn(component.closeClick, 'emit');
+        component.specifications.dismissible = true;
+
+        fixture.detectChanges();
+        const banner = fixture.debugElement.query(By.directive(NgbAlert));
+        banner.triggerEventHandler('closed', {});
+      });
+
       describe('and we click on the close button', () => {
-        it('should emit the close event', () => {});
+        it('should emit the close event', () => {
+          expect(component.closeClick.emit).toHaveBeenCalled();
+        });
       });
     });
   });
