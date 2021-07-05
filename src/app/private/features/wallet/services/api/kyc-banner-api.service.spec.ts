ac4fa9e2fc8f44247616ef300500cad02c29badd
Laia Lopez <laialopezz99@gmail.com>
2021-07-05 12:33:45 +0200

    [WPA-1787]: [Testing] KycBannerService + KycBannerApiService

--- src/app/private/features/wallet/services/api/kyc-banner-api.service.spec.ts
+++ src/app/private/features/wallet/services/api/kyc-banner-api.service.spec.ts
@@ -1,16 +1,40 @@
+import { HttpClientTestingModule, HttpTestingController, TestRequest } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { MOCK_KYC_BANNER_PENDING_VERIFICATION } from '@fixtures/private/wallet/kyc/kyc.fixtures.spec';
+import { KYCBanner } from '../../interfaces/kyc/kyc-banner.interface';
 
-import { KycBannerApiService } from './kyc-banner-api.service';
+import { KycBannerApiService, KYC_BANNER_STATUS_API_URL } from './kyc-banner-api.service';
 
 describe('KycBannerApiService', () => {
   let service: KycBannerApiService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [KycBannerApiService],
+    });
     service = TestBed.inject(KycBannerApiService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('when requesting the kyc banner status...', () => {
+    it('should get the kyc banner status ', () => {
+      let response: KYCBanner;
+
+      service.getKYCBanner().subscribe((data: KYCBanner) => {
+        response = data;
+      });
+      const req: TestRequest = httpMock.expectOne(KYC_BANNER_STATUS_API_URL);
+      req.flush(MOCK_KYC_BANNER_PENDING_VERIFICATION);
+
+      expect(response).toEqual(MOCK_KYC_BANNER_PENDING_VERIFICATION);
+      expect(req.request.url).toEqual(KYC_BANNER_STATUS_API_URL);
+      expect(req.request.method).toBe('GET');
+    });
   });
 });
--- src/app/private/features/wallet/services/kyc-banner/kyc-banner.service.ts
+++ src/app/private/features/wallet/services/kyc-banner/kyc-banner.service.ts
@@ -2,16 +2,22 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { KYC_BANNER_TYPES } from '../../components/kyc-banner/kyc-banner-constants';
-import { KYCBanner, KYCBannerSpecifications } from '../../interfaces/kyc/kyc-banner.interface';
+import { KYCBanner, KYCBannerSpecifications, KYC_BANNER_STATUS } from '../../interfaces/kyc/kyc-banner.interface';
 import { KycBannerApiService } from '../api/kyc-banner-api.service';
 
 @Injectable()
 export class KycBannerService {
   constructor(private kycBannerApiService: KycBannerApiService) {}
 
-  public getSpecifications(): Observable<KYCBannerSpecifications> {
-    return this.kycBannerApiService
-      .getKYCBanner()
-      .pipe(map((kycBanner: KYCBanner) => KYC_BANNER_TYPES.find((specification) => specification.status === kycBanner.status)));
+  public getSpecifications(): Observable<KYCBannerSpecifications | null> {
+    return this.kycBannerApiService.getKYCBanner().pipe(
+      map((KYCBanner: KYCBanner) => {
+        if (KYCBanner.status === KYC_BANNER_STATUS.NO_NEED) {
+          return null;
+        } else {
+          return KYC_BANNER_TYPES.find((specification) => specification.status === KYCBanner.status);
+        }
+      })
+    );
   }
 }
