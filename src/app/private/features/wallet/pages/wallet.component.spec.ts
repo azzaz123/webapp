17db7f6cca954d2eb1d7529a1f7d556ac27168a7
Laia Lopez <laialopezz99@gmail.com>
2021-07-05 13:36:48 +0200

    [WPA-1787]: [Testing] WalletComponent

--- src/app/private/features/wallet/pages/wallet.component.spec.ts
+++ src/app/private/features/wallet/pages/wallet.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
@@ -5,6 +6,10 @@ import { NavigationEnd, Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { PRIVATE_PATHS } from '@private/private-routing-constants';
 import { of } from 'rxjs';
+import { KYC_BANNER_TYPES } from '../components/kyc-banner/kyc-banner-constants';
+import { KycBannerComponent } from '../components/kyc-banner/kyc-banner.component';
+import { KycBannerApiService } from '../services/api/kyc-banner-api.service';
+import { KycBannerService } from '../services/kyc-banner/kyc-banner.service';
 import { WALLET_PATHS } from '../wallet-routing-constants';
 
 import { WalletComponent } from './wallet.component';
@@ -16,11 +21,12 @@ describe('WalletComponent', () => {
   let component: WalletComponent;
   let fixture: ComponentFixture<WalletComponent>;
   let router: Router;
+  let kycBannerService: KycBannerService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [WalletComponent],
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      declarations: [WalletComponent, KycBannerComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       providers: [
         {
@@ -30,6 +36,8 @@ describe('WalletComponent', () => {
             events: of(new NavigationEnd(1, CREDIT_CARD_FORM_LINK, '')),
           },
         },
+        KycBannerService,
+        KycBannerApiService,
       ],
     }).compileComponents();
   });
@@ -38,6 +46,7 @@ describe('WalletComponent', () => {
     fixture = TestBed.createComponent(WalletComponent);
     component = fixture.componentInstance;
     router = TestBed.inject(Router);
+    kycBannerService = TestBed.inject(KycBannerService);
     fixture.detectChanges();
   });
 
@@ -70,6 +79,36 @@ describe('WalletComponent', () => {
       it('should mark as selected the parent tab', () => {
         expect(component.selectedNavLinkId).toBe(component.navLinks.find((link) => link.id === BANK_DETAILS_URL).id);
       });
+    });
+  });
+
+  describe('when the wallet status for the user is not need', () => {
+    beforeEach(() => {
+      spyOn(kycBannerService, 'getSpecifications').and.returnValue(of(null));
+
+      component.ngOnInit();
+      fixture.detectChanges();
+    });
+
+    it('should not show the kyc banner', () => {
+      const banner = fixture.debugElement.query(By.directive(KycBannerComponent));
+
+      expect(banner).toBeFalsy();
+    });
+  });
+
+  describe('when the wallet status for the user is needed and defined', () => {
+    beforeEach(() => {
+      spyOn(kycBannerService, 'getSpecifications').and.returnValue(of(KYC_BANNER_TYPES[0]));
+
+      component.ngOnInit();
+      fixture.detectChanges();
+    });
+
+    it('should show the kyc banner', () => {
+      const banner = fixture.debugElement.query(By.directive(KycBannerComponent));
+
+      expect(banner).toBeTruthy();
     });
   });
 });
