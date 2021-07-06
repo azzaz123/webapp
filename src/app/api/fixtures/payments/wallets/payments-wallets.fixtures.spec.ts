4ed272026f9321ff2df7fbabed130347d1704901
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-07-06 17:07:00 +0200

    [WPA-2265]: Create Http Service for Wallet information endpoint

---
+++ src/app/api/fixtures/payments/wallets/payments-wallets.fixtures.spec.ts
@@ -0,0 +1,7 @@
+import { PaymentsWalletsApi } from '@api/payments/wallets/dtos/responses';
+
+export const MOCK_PAYMENTS_WALLETS_RESPONSE: PaymentsWalletsApi = {
+  amount: 3,
+  currency: 'EUR',
+  id: 'da3c9db6-2cce-496a-9386-c3a43ce7366a',
+};
