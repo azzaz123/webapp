2c59ee3baa39795692d16e2a92de6e8566483312
Gonzalo Morales <gonzalomorales93@gmail.com>
2021-07-07 16:41:14 +0200

    [PQP-3808]: fix keep style for 4 subscriptions

--- src/app/private/features/profile/pages/subscription/subscription-list/subscription-list.component.ts
+++ src/app/private/features/profile/pages/subscription/subscription-list/subscription-list.component.ts
@@ -14,6 +14,7 @@ export class SubscriptionListComponent {
   @Input() isLoading: boolean;
   @Input() subscriptions: SubscriptionsResponse[];
   @Output() clickButton: EventEmitter<SubscriptionsResponse> = new EventEmitter();
+  MAX_NARROW_CARDS = 4;
 
   private readonly rowOrder = [
     CATEGORY_SUBSCRIPTIONS_IDS.EVERYTHING_ELSE,
