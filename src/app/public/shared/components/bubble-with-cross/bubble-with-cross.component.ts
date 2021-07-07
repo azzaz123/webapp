d31df1b50a85f96957b1ca1791b2046e6dbb8079
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-07-07 10:45:10 +0200

    [FIN-1931]: Added StopPropagation

--- src/app/public/shared/components/bubble-with-cross/bubble-with-cross.component.ts
+++ src/app/public/shared/components/bubble-with-cross/bubble-with-cross.component.ts
@@ -9,7 +9,8 @@ export class BubbleWithCrossComponent {
   @Input() bubbleText: string;
   @Output() clear: EventEmitter<string> = new EventEmitter();
 
-  public emitClear(): void {
+  public emitClear(event: MouseEvent): void {
+    event.stopPropagation();
     this.clear.emit(this.bubbleText);
   }
 }
