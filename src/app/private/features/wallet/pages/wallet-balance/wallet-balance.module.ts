508b6763ab08dcc7ae0ead58354484fe1442572d
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-07-08 16:28:37 +0200

    [WPA-2265]: Add Wallet Balance component controller

--- src/app/private/features/wallet/pages/wallet-balance/wallet-balance.module.ts
+++ src/app/private/features/wallet/pages/wallet-balance/wallet-balance.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { PaymentsWalletsModule } from '@api/payments/wallets/payments-wallets.module';
 import { WalletBalanceRoutedComponents, WalletBalanceRoutingModule } from './wallet-balance.routing.module';
 
 @NgModule({
   declarations: [WalletBalanceRoutedComponents],
-  imports: [WalletBalanceRoutingModule, CommonModule],
+  imports: [WalletBalanceRoutingModule, PaymentsWalletsModule, CommonModule],
 })
 export class WalletBalanceModule {}
