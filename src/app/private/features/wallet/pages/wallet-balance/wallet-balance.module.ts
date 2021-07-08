2b864b7de09d674c59e627a68a838a4b1361ac21
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-07-08 17:13:30 +0200

    [WPA-2265]: Show loading and balance in Wallet Balance template

--- src/app/private/features/wallet/pages/wallet-balance/wallet-balance.module.ts
+++ src/app/private/features/wallet/pages/wallet-balance/wallet-balance.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { PaymentsWalletsModule } from '@api/payments/wallets/payments-wallets.module';
+import { ButtonModule } from '@shared/button/button.module';
+import { SvgIconModule } from '@shared/svg-icon/svg-icon.module';
 import { WalletBalanceRoutedComponents, WalletBalanceRoutingModule } from './wallet-balance.routing.module';
 
 @NgModule({
   declarations: [WalletBalanceRoutedComponents],
-  imports: [WalletBalanceRoutingModule, PaymentsWalletsModule, CommonModule],
+  imports: [WalletBalanceRoutingModule, PaymentsWalletsModule, CommonModule, ButtonModule, SvgIconModule],
 })
 export class WalletBalanceModule {}
