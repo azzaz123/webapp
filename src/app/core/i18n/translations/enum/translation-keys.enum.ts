6bc9c27c8711cdc2ec189fc30fcfeffd00a19f0a
Irene Blanco <irene.blanco@wallapop.com>
2021-07-08 12:08:50 +0200

    [FIN-1980]: Added missing weight error translation key

--- src/app/core/i18n/translations/enum/translation-keys.enum.ts
+++ src/app/core/i18n/translations/enum/translation-keys.enum.ts
@@ -153,7 +153,7 @@ export enum TRANSLATION_KEY {
   DELIVERY_BANK_ACCOUNT_DELETE_ERROR,
   DELIVERY_CREDIT_CARD_DELETE_SUCCESS,
   DELIVERY_CREDIT_CARD_DELETE_ERROR,
-
+  FINDING_MISSING_WEIGHT_ERROR,
   // TODO: Remaining keys with no explicit usage. Some of them might come from the backend so we need to keep it's string value
   //       We leave cleaning this keys after Italy release, as some of them come from some legacy parts (chat, calls..)
   //       and it is really difficult to follow the keys as this part needs a refactor
