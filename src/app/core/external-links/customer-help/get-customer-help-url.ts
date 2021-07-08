d9f3cd4c847b304169fa39070462c28bb37306f0
Irene Blanco <irene.blanco@wallapop.com>
2021-07-08 15:48:21 +0200

    [FIN-1980]: getCustomerHelpUrl - Renamed argument

--- src/app/core/external-links/customer-help/get-customer-help-url.ts
+++ src/app/core/external-links/customer-help/get-customer-help-url.ts
@@ -3,6 +3,6 @@ import { CUSTOMER_HELP_PAGE, HELP_LOCALE } from './customer-help-constants';
 const HELP_BASE_URL = 'https://ayuda.wallapop.com/hc/';
 type CUSTOMER_HELP_PAGE_ID = CUSTOMER_HELP_PAGE;
 
-export function getCustomerHelpUrl(article: CUSTOMER_HELP_PAGE_ID, locale: HELP_LOCALE): string {
-  return `${HELP_BASE_URL}${HELP_LOCALE[locale]}/articles/${article}`;
+export function getCustomerHelpUrl(pageId: CUSTOMER_HELP_PAGE_ID, locale: HELP_LOCALE): string {
+  return `${HELP_BASE_URL}${HELP_LOCALE[locale]}/articles/${pageId}`;
 }
