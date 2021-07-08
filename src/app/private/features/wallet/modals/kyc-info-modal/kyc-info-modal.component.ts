4f153b3f090bfeed24822f4849b03354fd90a6a6
Laia Lopez <laialopezz99@gmail.com>
2021-07-08 09:58:07 +0200

    [WPA-1729]: [Testing + Redirect to KYC] KycInfoModal

--- src/app/private/features/wallet/modals/kyc-info-modal/kyc-info-modal.component.ts
+++ src/app/private/features/wallet/modals/kyc-info-modal/kyc-info-modal.component.ts
@@ -1,6 +1,9 @@
 import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { PRIVATE_PATHS } from '@private/private-routing-constants';
 import { SlidesCarouselComponent } from '@shared/components/carousel-slides/carousel-slides.component';
+import { WALLET_PATHS } from '../../wallet-routing-constants';
 import { KYC_SLIDER_INFO_STEPS } from './kyc-info-modal-constants';
 
 @Component({
@@ -11,13 +14,18 @@ import { KYC_SLIDER_INFO_STEPS } from './kyc-info-modal-constants';
 })
 export class KycInfoModalComponent {
   @ViewChild(SlidesCarouselComponent, { static: true }) slidesCarousel: SlidesCarouselComponent;
+  public readonly KYC_LINK = `${PRIVATE_PATHS.WALLET}/${WALLET_PATHS.KYC}`;
   public readonly KYC_SLIDER_INFO_STEPS = KYC_SLIDER_INFO_STEPS;
   public readonly LAST_SLIDE = 'ngb-slide-2';
 
-  constructor(public activeModal: NgbActiveModal) {}
+  constructor(public activeModal: NgbActiveModal, private router: Router) {}
 
   public swipeToRight(): void {
     this.slidesCarousel.carousel.next();
+  }
+
+  public redirectToKYC(): void {
+    this.router.navigate([this.KYC_LINK]);
   }
 
   get currentSlide(): string {
