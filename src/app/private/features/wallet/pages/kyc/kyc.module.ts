5e3fe8ec63d71b046e1d4be8e025bda85475026c
Laia Lopez <laialopezz99@gmail.com>
2021-07-09 14:48:06 +0200

    [WPA-1784]: [KYC] Steps Added (Components Empty)

--- src/app/private/features/wallet/pages/kyc/kyc.module.ts
+++ src/app/private/features/wallet/pages/kyc/kyc.module.ts
@@ -3,9 +3,11 @@ import { KYCRoutedComponents, KYCRoutingModule } from './kyc.routing.module';
 import { CommonModule } from '@angular/common';
 import { StepperModule } from '@shared/stepper/stepper.module';
 import { BankAccountModule } from '../bank-details/pages/bank-account/bank-account.module';
+import { KycNationalityComponent } from './components/kyc-nationality/kyc-nationality.component';
+import { KycImagesComponent } from './components/kyc-images/kyc-images.component';
 
 @NgModule({
   imports: [CommonModule, KYCRoutingModule, StepperModule, BankAccountModule],
-  declarations: [KYCRoutedComponents],
+  declarations: [KYCRoutedComponents, KycNationalityComponent, KycImagesComponent],
 })
 export class KYCModule {}
