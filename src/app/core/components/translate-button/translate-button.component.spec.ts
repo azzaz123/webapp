16e4b1bd8f06f008082a900f3905e3115b55183b
Pablo Macías Moreira <pablo.macias.moreira@gmail.com>
2021-07-09 10:34:46 +0200

    [WPA-736]: Translate button done

--- src/app/core/components/translate-button/translate-button.component.spec.ts
+++ src/app/core/components/translate-button/translate-button.component.spec.ts
@@ -1,14 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TranslateButtonComponent } from './translate-button.component';
+import { By } from '@angular/platform-browser';
+import { SvgIconComponent } from '@shared/svg-icon/svg-icon.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('TranslateButtonComponent', () => {
+  const translateSelector = By.css('.TranslateButton__text--translate');
+  const showOriginalSelector = By.css('.TranslateButton__text--show-original');
   let component: TranslateButtonComponent;
   let fixture: ComponentFixture<TranslateButtonComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [TranslateButtonComponent],
+      declarations: [TranslateButtonComponent, SvgIconComponent],
+      imports: [HttpClientTestingModule],
     }).compileComponents();
   });
 
@@ -20,5 +26,27 @@ describe('TranslateButtonComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('when related text is not translated', () => {
+    it('should have translate text', () => {
+      component.isTranslated = false;
+
+      fixture.detectChanges();
+
+      expect(fixture.debugElement.query(translateSelector)).toBeTruthy();
+      expect(fixture.debugElement.query(showOriginalSelector)).toBeFalsy();
+    });
+  });
+
+  describe('when related text is translated', () => {
+    it('should have show original text', () => {
+      component.isTranslated = true;
+
+      fixture.detectChanges();
+
+      expect(fixture.debugElement.query(translateSelector)).toBeFalsy();
+      expect(fixture.debugElement.query(showOriginalSelector)).toBeTruthy();
+    });
   });
 });
