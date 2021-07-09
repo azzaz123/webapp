37cce41fbcf7b136b8ae946c1576cb9ff503664f
Laia Lopez <laialopezz99@gmail.com>
2021-07-09 14:37:05 +0200

    [WPA-1784]: [StepperComponent] Logic done

--- src/app/shared/stepper/stepper.component.ts
+++ src/app/shared/stepper/stepper.component.ts
@@ -1,12 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ContentChildren, Input, QueryList } from '@angular/core';
+import { StepDirective } from './step.directive';
 
 @Component({
   selector: 'tsl-stepper',
   templateUrl: './stepper.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./stepper.component.scss'],
 })
-export class StepperComponent implements OnInit {
-  constructor() {}
+export class StepperComponent {
+  @ContentChildren(StepDirective) sections: QueryList<StepDirective>;
+  @Input() initialIndex = 0;
 
-  ngOnInit(): void {}
+  public steps: StepDirective[];
+  public activeId: number;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngAfterContentInit() {
+    this.steps = this.sections.toArray();
+    this.activeId = this.initialIndex;
+    this.cdr.detectChanges();
+  }
+
+  public goBack(): void {
+    if (this.havePreviousSlide()) {
+      this.activeId = --this.activeId;
+    }
+  }
+
+  public goNext(): void {
+    if (this.haveNextSlide()) {
+      this.activeId = ++this.activeId;
+    }
+  }
+
+  private havePreviousSlide(): boolean {
+    return this.activeId - 1 > -1;
+  }
+
+  private haveNextSlide(): boolean {
+    return this.activeId + 1 <= this.steps.length;
+  }
 }
