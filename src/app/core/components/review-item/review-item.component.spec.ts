82d6e135ce615581d211c35d22c686f679d36d4b
Pablo Macías Moreira <pablo.macias.moreira@gmail.com>
2021-07-12 11:13:01 +0200

    [WPA-736]: Reviews translation done

--- src/app/core/components/review-item/review-item.component.spec.ts
+++ src/app/core/components/review-item/review-item.component.spec.ts
@@ -1,24 +1,42 @@
-import { MOCK_REVIEWS } from '@fixtures/review.fixtures.spec';
+import { MOCK_REVIEWS, MOCK_TRANSLATABLE_REVIEW } from '@fixtures/review.fixtures.spec';
 import { CATEGORY_DATA_WEB } from '@fixtures/category.fixtures.spec';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { ReviewItemComponent } from './review-item.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Component, DebugElement, Input, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { of } from 'rxjs';
 import { CategoryService } from 'app/core/category/category.service';
 import { SanitizedBackgroundDirective } from 'app/shared/sanitized-background/sanitized-background.directive';
 import { environment } from 'environments/environment';
 import { UserProfileRoutePipe } from '@shared/pipes';
+import { ReviewsApiModule, ReviewsApiService } from '@api/reviews';
+import { By } from '@angular/platform-browser';
+import { TranslateButtonComponent } from '@core/components/translate-button/translate-button.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Review } from '@private/features/reviews/core/review';
+
+@Component({
+  selector: 'ts-test',
+  template: '<tsl-review-item [isOwner]="isOwner" [review]="review"></tsl-review-item>',
+})
+class TestComponent {
+  @Input() isOwner = false;
+  @Input() review: Review;
+}
 
 describe('ReviewItemComponent', () => {
+  let testComponent: TestComponent;
   let component: ReviewItemComponent;
-  let fixture: ComponentFixture<ReviewItemComponent>;
+  let fixture: ComponentFixture<TestComponent>;
+  let debugElement: DebugElement;
+  let reviewsApiService: ReviewsApiService;
 
   beforeEach(
     waitForAsync(() => {
       TestBed.configureTestingModule({
-        declarations: [ReviewItemComponent, SanitizedBackgroundDirective, UserProfileRoutePipe],
+        declarations: [ReviewItemComponent, SanitizedBackgroundDirective, UserProfileRoutePipe, TestComponent, TranslateButtonComponent],
         schemas: [NO_ERRORS_SCHEMA],
+        imports: [ReviewsApiModule, HttpClientTestingModule],
         providers: [
           { provide: 'SUBDOMAIN', useValue: 'www' },
           {
@@ -35,14 +53,18 @@ describe('ReviewItemComponent', () => {
   );
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ReviewItemComponent);
-    component = fixture.componentInstance;
-    component.review = MOCK_REVIEWS[0];
-    fixture.detectChanges();
-    component.ngOnInit();
+    fixture = TestBed.createComponent(TestComponent);
+    reviewsApiService = TestBed.inject(ReviewsApiService);
+    testComponent = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+    component = debugElement.query(By.directive(ReviewItemComponent)).componentInstance;
+    testComponent.review = MOCK_REVIEWS[0];
   });
 
   describe('ngOnInit', () => {
+    beforeEach(() => {
+      fixture.detectChanges();
+    });
     it('should set itemWebLink', () => {
       expect(component.itemWebLink).toBe(environment.siteUrl.replace('es', 'www') + 'item/' + MOCK_REVIEWS[0].item.webSlug);
     });
@@ -54,9 +76,8 @@ describe('ReviewItemComponent', () => {
 
   describe('when the item is ours...', () => {
     beforeEach(() => {
-      component.isOwner = true;
-
-      component.ngOnInit();
+      testComponent.isOwner = true;
+      fixture.detectChanges();
     });
     it('should return the correct sold copy', () => {
       expect(component.reviewItemCopies.soldCopy).toBe($localize`:@@OwnReview_Sold:Sold`);
@@ -69,9 +90,8 @@ describe('ReviewItemComponent', () => {
 
   describe('when the item is NOT ours...', () => {
     beforeEach(() => {
-      component.isOwner = false;
-
-      component.ngOnInit();
+      testComponent.isOwner = false;
+      fixture.detectChanges();
     });
     it('should return the correct sold copy', () => {
       expect(component.reviewItemCopies.soldCopy).toBe($localize`:@@Review_Sold:Sold`);
@@ -79,6 +99,86 @@ describe('ReviewItemComponent', () => {
 
     it('should return the correct bought copy', () => {
       expect(component.reviewItemCopies.boughtCopy).toBe($localize`:@@Review_Bought:Bought`);
+    });
+  });
+
+  describe('when review cannot be translated', () => {
+    beforeEach(() => {
+      fixture.detectChanges();
+    });
+    it('should not show translate button', () => {
+      const translateButton = debugElement.query(By.directive(TranslateButtonComponent));
+
+      expect(translateButton).toBeFalsy();
+    });
+  });
+
+  describe('when review can be translated', () => {
+    const translateButtonPredicate = By.directive(TranslateButtonComponent);
+
+    beforeEach(() => {
+      testComponent.review = MOCK_TRANSLATABLE_REVIEW;
+      fixture.detectChanges();
+    });
+
+    it('should show translate button', () => {
+      const translateButton = debugElement.query(translateButtonPredicate);
+
+      expect(translateButton).toBeTruthy();
+    });
+
+    describe('and button is clicked', () => {
+      const commentPredicate = By.css('.review-comment');
+      const translatedComment = 'Translated comment';
+
+      describe('when showing original', () => {
+        describe('and translation has not been retrieved yet', () => {
+          it('should retrieve translation and show it', () => {
+            spyOn(reviewsApiService, 'getReviewTranslation').and.returnValue(of(translatedComment));
+
+            debugElement.query(By.directive(TranslateButtonComponent)).nativeElement.click();
+            fixture.detectChanges();
+            const comment = debugElement.query(commentPredicate).nativeElement.innerHTML;
+
+            expect(reviewsApiService.getReviewTranslation).toHaveBeenCalledTimes(1);
+            expect(reviewsApiService.getReviewTranslation).toHaveBeenCalledWith(MOCK_TRANSLATABLE_REVIEW.id);
+            expect(comment).toEqual(translatedComment);
+          });
+        });
+
+        describe('and translation has been retrieved already', () => {
+          beforeEach(() => {
+            component['translation'] = translatedComment;
+          });
+
+          it('should use old translation and show it', () => {
+            spyOn(reviewsApiService, 'getReviewTranslation');
+
+            debugElement.query(By.directive(TranslateButtonComponent)).nativeElement.click();
+            fixture.detectChanges();
+            const comment = debugElement.query(commentPredicate).nativeElement.innerHTML;
+
+            expect(reviewsApiService.getReviewTranslation).toHaveBeenCalledTimes(0);
+            expect(comment).toEqual(translatedComment);
+          });
+        });
+      });
+
+      describe('when showing translation', () => {
+        beforeEach(() => {
+          component['translation'] = translatedComment;
+          debugElement.query(By.directive(TranslateButtonComponent)).nativeElement.click();
+          fixture.detectChanges();
+        });
+
+        it('should show original', () => {
+          debugElement.query(By.directive(TranslateButtonComponent)).nativeElement.click();
+          fixture.detectChanges();
+          const comment = debugElement.query(commentPredicate).nativeElement.innerHTML;
+
+          expect(comment).toEqual(MOCK_TRANSLATABLE_REVIEW.comments);
+        });
+      });
     });
   });
 });
--- src/app/core/components/review-item/review-item.component.ts
+++ src/app/core/components/review-item/review-item.component.ts
@@ -4,6 +4,8 @@ import { CategoryService } from 'app/core/category/category.service';
 import { FAKE_ITEM_IMAGE_SMALL_LIGHT_BASE_PATH } from 'app/core/item/item';
 import { Review } from '@private/features/reviews/core/review';
 import { User } from '@core/user/user';
+import { ReviewsApiService } from '@api/reviews';
+import { take } from 'rxjs/operators';
 
 export interface ReviewItemCopies {
   soldCopy: string;
@@ -25,13 +27,21 @@ export class ReviewItemComponent implements OnInit {
   public reviewItemCopies: ReviewItemCopies;
 
   public isTranslated = false;
+  public reviewComment: string;
+  private translation: string;
+  private loadingTranslation: boolean;
 
-  constructor(@Inject('SUBDOMAIN') private subdomain: string, private categoryService: CategoryService) {}
+  constructor(
+    @Inject('SUBDOMAIN') private subdomain: string,
+    private categoryService: CategoryService,
+    private reviewsApiService: ReviewsApiService
+  ) {}
 
   public ngOnInit(): void {
     this.fallback = FAKE_ITEM_IMAGE_SMALL_LIGHT_BASE_PATH;
     this.itemWebLink = this.review.item ? this.review.item.getUrl(this.subdomain) : null;
     this.reviewUser = this.review.user;
+    this.reviewComment = this.review.comments;
     this.initializeCopies();
     this.categoryService.getCategoryById(this.review.item.categoryId).subscribe((category: CategoryResponse) => {
       this.category = category;
@@ -39,7 +49,33 @@ export class ReviewItemComponent implements OnInit {
   }
 
   public toggleTranslation(): void {
-    this.isTranslated = !this.isTranslated;
+    if (this.loadingTranslation) {
+      return;
+    }
+
+    const willTranslate = !this.isTranslated;
+    if (willTranslate && !this.translation) {
+      this.retrieveTranslation();
+    } else {
+      this.translateReviewText(willTranslate);
+    }
+  }
+
+  private translateReviewText(translated: boolean): void {
+    this.isTranslated = translated;
+    this.reviewComment = translated ? this.translation : this.review.comments;
+  }
+
+  private retrieveTranslation(): void {
+    this.loadingTranslation = true;
+    this.reviewsApiService
+      .getReviewTranslation(this.review.id)
+      .pipe(take(1))
+      .subscribe((translation) => {
+        this.translation = translation;
+        this.translateReviewText(true);
+        this.loadingTranslation = false;
+      });
   }
 
   private initializeCopies(): void {
