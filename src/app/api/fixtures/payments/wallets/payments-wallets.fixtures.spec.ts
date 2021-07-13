ed11925675c7782557f78a92451c97ed06d6b253
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-07-13 16:13:36 +0200

    [WPA-2265]: Add tests for Wallet balance info

--- src/app/api/fixtures/payments/wallets/payments-wallets.fixtures.spec.ts
+++ src/app/api/fixtures/payments/wallets/payments-wallets.fixtures.spec.ts
@@ -1,5 +1,6 @@
 import { Money } from '@api/core/model/money.interface';
 import { PaymentsWalletsApi } from '@api/payments/wallets/dtos/responses';
+import { Observable, of } from 'rxjs';
 
 export const MOCK_PAYMENTS_WALLETS_RESPONSE: PaymentsWalletsApi = {
   amount: 3,
@@ -11,6 +12,18 @@ export const MOCK_PAYMENTS_WALLETS_WITH_DECIMALS_RESPONSE: PaymentsWalletsApi = 
   amount: 1722.41,
   currency: 'EUR',
   id: '92020cb7-f320-47f3-8bbe-d1cbf0d6a205',
+};
+
+export const MOCK_PAYMENTS_WALLET_MAPPED_WITHOUT_MONEY: Money = {
+  amount: {
+    integer: 0,
+    decimals: 0,
+    total: 0,
+  },
+  currency: {
+    code: 'EUR',
+    symbol: '€',
+  },
 };
 
 export const MOCK_PAYMENTS_WALLETS_MAPPED_MONEY: Money = {
@@ -36,3 +49,9 @@ export const MOCK_PAYMENTS_WALLETS_WITH_DECIMALS_MAPPED_MONEY: Money = {
     symbol: '€',
   },
 };
+
+export class MockPaymentsWalletsService {
+  public get walletBalance$(): Observable<Money> {
+    return of(MOCK_PAYMENTS_WALLETS_WITH_DECIMALS_MAPPED_MONEY);
+  }
+}
