d0d68f0209f544f189092b259e2dfdf83e6ec9d3
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-07-14 16:24:01 +0200

    [WPA-2268]: Use public attribute instead of getter for Zendesk URL

--- src/app/private/features/wallet/wallet.component.ts
+++ src/app/private/features/wallet/wallet.component.ts
@@ -26,12 +26,9 @@ export class WalletComponent implements OnInit {
     },
   ];
 
-  //FIXME: This will be moved into a service
-  private zendeskWalletHelpArticleId: number = 360017172677;
-
-  public get zendeskWalletHelpURL(): string {
-    return `https://ayuda.wallapop.com/hc/es-es/articles/${this.zendeskWalletHelpArticleId}`;
-  }
+  //FIXME: These will be moved into a service
+  public zendeskWalletHelpArticleId: number = 360017172677;
+  public zendeskWalletHelpURL = `https://ayuda.wallapop.com/hc/es-es/articles/${this.zendeskWalletHelpArticleId}`;
 
   constructor(private router: Router, private kycBannerService: KYCBannerService) {
     router.events.subscribe((e) => {
