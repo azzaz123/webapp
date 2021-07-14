1910065fd15aa94974e80ccca7d35faf8e4df588
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-07-14 13:15:12 +0200

    [FIN-1931]: Changed name to SuggesterInput

--- src/app/shared/suggester-input/suggester-input.module.ts
+++ src/app/shared/suggester-input/suggester-input.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SuggesterInputComponent } from './suggester-input.component';
+import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
+import { SelectFormModule } from '@shared/form/components/select/select-form.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
+  exports: [SuggesterInputComponent],
   declarations: [SuggesterInputComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, NgbTypeaheadModule, SelectFormModule, FormsModule],
 })
-export class DynamicInputModule {}
+export class SuggesterInputModule {}
