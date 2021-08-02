9504958e52d07e58621482145c74935b40b522dc
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-08-02 17:58:12 +0200

    [FIN-1931]: Created test cases

--- src/app/shared/suggester-input/suggester-input.component.spec.ts
+++ src/app/shared/suggester-input/suggester-input.component.spec.ts
@@ -1,9 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
-import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
 import { HashtagSuggesterApiService } from '@private/features/upload/core/services/hashtag-suggestions/hashtag-suggester-api.service';
 import { MultiSelectFormModule } from '@shared/form/components/multi-select-form/multi-select-form.module';
 import { SelectFormModule } from '@shared/form/components/select/select-form.module';
@@ -17,15 +16,7 @@ describe('SuggesterInputComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       providers: [HashtagSuggesterApiService],
-      imports: [
-        CommonModule,
-        HttpClientModule,
-        NgbTypeaheadModule,
-        SelectFormModule,
-        ReactiveFormsModule,
-        FormsModule,
-        MultiSelectFormModule,
-      ],
+      imports: [CommonModule, HttpClientModule, SelectFormModule, FormsModule, MultiSelectFormModule],
       declarations: [SuggesterInputComponent],
     }).compileComponents();
   });
@@ -40,33 +31,71 @@ describe('SuggesterInputComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  describe.each(HASHTAG_TESTING.isValid)('Check whether the hashtag is valid', (isValid) => {
-    it(`should pass the test if input is ${isValid}`, () => {
-      spyOn(component, 'detectTitleKeyboardChanges').and.callThrough();
-      spyOn(component, 'isValidKey').and.callThrough();
-      const inputElement = fixture.debugElement.query(By.css('.SuggesterInput'));
-      inputElement.nativeElement.value = isValid;
-      inputElement.nativeElement.dispatchEvent(new Event('input'));
-      inputElement.triggerEventHandler('keyup', {});
-      fixture.detectChanges();
-      component.isValidKey();
+  describe('when initate component', () => {
+    it('should know the hashtags that are selected outside this component', () => {});
+  });
 
-      expect(component.isValidKey()).toBeTruthy();
+  describe('when user focus the input', () => {
+    it('should have the placeholder changed to #', () => {});
+  });
+
+  describe('when user type in the input', () => {
+    describe.each(HASHTAG_TESTING.isValid)('Check whether the hashtag is valid', (isValid) => {
+      it(`should pass the test if input is ${isValid}`, () => {
+        spyOn(component, 'detectTitleKeyboardChanges').and.callThrough();
+        spyOn(component, 'isValidKey').and.callThrough();
+        const inputElement = fixture.debugElement.query(By.css('.SuggesterInput'));
+
+        inputElement.nativeElement.value = isValid;
+        inputElement.nativeElement.dispatchEvent(new Event('input'));
+        inputElement.triggerEventHandler('keyup', {});
+        fixture.detectChanges();
+        component.isValidKey();
+
+        expect(component.isValidKey()).toBeTruthy();
+      });
+    });
+
+    describe.each(HASHTAG_TESTING.isNotValid)('Check whether the hashtag is not valid', (isNotValid) => {
+      it(`should pass the test if input is ${isNotValid}`, () => {
+        spyOn(component, 'detectTitleKeyboardChanges').and.callThrough();
+        spyOn(component, 'isValidKey').and.callThrough();
+        const inputElement = fixture.debugElement.query(By.css('.SuggesterInput'));
+
+        inputElement.nativeElement.value = isNotValid;
+        inputElement.nativeElement.dispatchEvent(new Event('input'));
+        inputElement.triggerEventHandler('keyup', {});
+        fixture.detectChanges();
+        component.isValidKey();
+
+        expect(component.isValidKey()).toBeFalsy();
+      });
+    });
+
+    describe('the input value', () => {
+      it('should have the # symbol infront of the value entered by the user', () => {});
+    });
+
+    describe('when we have the hashtag suggestions', () => {
+      it('should load the hashtags from our endpoint', () => {});
+
+      it('the hashtags should have # infront', () => {});
+    });
+
+    describe('when we do not have the hashtags suggestions', () => {
+      it('should create new hashtag for user', () => {});
+
+      it('ths hashtage should have # infront', () => {});
     });
   });
 
-  describe.each(HASHTAG_TESTING.isNotValid)('Check whether the hashtag is not valid', (isNotValid) => {
-    it(`should pass the test if input is ${isNotValid}`, () => {
-      spyOn(component, 'detectTitleKeyboardChanges').and.callThrough();
-      spyOn(component, 'isValidKey').and.callThrough();
-      const inputElement = fixture.debugElement.query(By.css('.SuggesterInput'));
-      inputElement.nativeElement.value = isNotValid;
-      inputElement.nativeElement.dispatchEvent(new Event('input'));
-      inputElement.triggerEventHandler('keyup', {});
-      fixture.detectChanges();
-      component.isValidKey();
+  describe('Select and unslect hashtag suggestions', () => {
+    describe('when select the hashtag', () => {
+      it('should add the selected hashtag to the hashtag list', () => {});
+    });
 
-      expect(component.isValidKey()).toBeFalsy();
+    describe('when unselect the hashtag', () => {
+      it('should remove the hashtag from the hashtag list', () => {});
     });
   });
 });
