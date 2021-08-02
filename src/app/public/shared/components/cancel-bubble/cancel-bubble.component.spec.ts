7b82a77d60b4d64931c3f7c66a82071db446f5ea
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-08-02 09:55:34 +0200

    [FIN-1931]: Added testing for cancel bubble

--- src/app/public/shared/components/cancel-bubble/cancel-bubble.component.spec.ts
+++ src/app/public/shared/components/cancel-bubble/cancel-bubble.component.spec.ts
@@ -1,13 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { HttpModule } from '@core/http/http.module';
+import { SvgIconModule } from '@shared/svg-icon/svg-icon.module';
 
 import { CancelBubbleComponent } from './cancel-bubble.component';
 
 describe('CancelBubbleComponent', () => {
   let component: CancelBubbleComponent;
   let fixture: ComponentFixture<CancelBubbleComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [SvgIconModule, CommonModule, HttpModule],
       declarations: [CancelBubbleComponent],
     }).compileComponents();
   });
@@ -15,10 +22,33 @@ describe('CancelBubbleComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CancelBubbleComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('CancelBubble behavior', () => {
+    let bubbleText = 'bubbleText';
+    beforeEach(() => {
+      component.bubbleText = bubbleText;
+      fixture.detectChanges();
+    });
+    it('should show text in bubble', () => {
+      const bubbleText = debugElement.query(By.css('.CancelBubbleComponent__text')).nativeNode.innerHTML;
+
+      expect(bubbleText).toBe(bubbleText);
+    });
+
+    it('should emit the text of the bubble when we click the cross', () => {
+      spyOn(component.clear, 'emit');
+      const crossIcon = debugElement.query(By.css('tsl-svg-icon')).nativeNode;
+
+      crossIcon.click();
+
+      expect(component.clear.emit).toHaveBeenCalledWith(bubbleText);
+    });
   });
 });
