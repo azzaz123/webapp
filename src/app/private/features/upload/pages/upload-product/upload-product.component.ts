b48b8182d069ae2367897dabd0d840e286e723c6
Irene Blanco <irene.blanco@wallapop.com>
2021-08-02 09:29:49 +0200

    [FIN-1981]: UploadProductComponent - Removed comment

--- src/app/private/features/upload/pages/upload-product/upload-product.component.ts
+++ src/app/private/features/upload/pages/upload-product/upload-product.component.ts
@@ -932,7 +932,6 @@ export class UploadProductComponent implements OnInit, AfterContentInit, OnChang
   }
 
   private detectShippabilityAllowanceChanges(): void {
-    // todo mirar de juntar todos, parece que puedo encolarlos de forma paralela o algo así :D
     this.uploadForm.get('category_id').valueChanges.subscribe(() => {
       this.updateShippingToggleStatus();
     });
