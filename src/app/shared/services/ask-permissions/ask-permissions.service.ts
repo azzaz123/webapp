bc80ee7c749fc399b3b67a4d1b11bdb1e1434244
Laia Lopez <laialopezz99@gmail.com>
2021-08-03 12:04:39 +0200

    [WPA-3298]: [Comment] Fixme added

--- src/app/shared/services/ask-permissions/ask-permissions.service.ts
+++ src/app/shared/services/ask-permissions/ask-permissions.service.ts
@@ -22,6 +22,7 @@ export class AskPermissionsService {
         () => this.updateUserDevicePermissions({ video: DEVICE_PERMISSIONS_STATUS.ACCEPTED }),
         (error: DOMException) => {
           const errorMessage = error + '';
+          // FIXME: take a look at handling error in the future		Date: 2021/08/03
           const errorPermissionStatus: DEVICE_PERMISSIONS_STATUS = errorMessage.includes('denied')
             ? DEVICE_PERMISSIONS_STATUS.DENIED
             : DEVICE_PERMISSIONS_STATUS.CANNOT_ACCESS;
