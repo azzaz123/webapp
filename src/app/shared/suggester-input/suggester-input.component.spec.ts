bdb298aac0eee5df12c25bfc0b1bf1b7b2486c42
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-08-03 08:53:38 +0200

    [FIN-1931]: Adding tests

--- src/app/shared/suggester-input/suggester-input.component.spec.ts
+++ src/app/shared/suggester-input/suggester-input.component.spec.ts
@@ -1,17 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
 import { HashtagSuggesterApiService } from '@private/features/upload/core/services/hashtag-suggestions/hashtag-suggester-api.service';
 import { MultiSelectFormModule } from '@shared/form/components/multi-select-form/multi-select-form.module';
 import { SelectFormModule } from '@shared/form/components/select/select-form.module';
+import { of } from 'rxjs';
 import { SuggesterInputComponent } from './suggester-input.component';
-import { HASHTAG_TESTING } from './suggester-input.fixtures.spec';
+import { HASHTAG_TESTING, INITIAL_HASHTAGS } from './suggester-input.fixtures.spec';
 
 describe('SuggesterInputComponent', () => {
   let component: SuggesterInputComponent;
   let fixture: ComponentFixture<SuggesterInputComponent>;
+  let inputElement: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -24,6 +27,7 @@ describe('SuggesterInputComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SuggesterInputComponent);
     component = fixture.componentInstance;
+    inputElement = fixture.debugElement.query(By.css('.SuggesterInput'));
     fixture.detectChanges();
   });
 
@@ -32,15 +36,29 @@ describe('SuggesterInputComponent', () => {
   });
 
   describe('when initate component', () => {
-    it('should know the hashtags that are selected outside this component', () => {});
+    beforeEach(() => {
+      component.writeValue(INITIAL_HASHTAGS);
+      fixture.detectChanges();
+    });
+    it('should know the hashtags that are selected outside this component', () => {
+      expect(component.value).toBe(INITIAL_HASHTAGS);
+    });
   });
 
   describe('when user focus the input', () => {
-    it('should have the placeholder changed to #', () => {});
+    it('should have the placeholder changed to #', () => {
+      inputElement.nativeElement.focus();
+
+      expect(inputElement.nativeElement.placeholder).toBe('#');
+    });
   });
 
   describe('when user unfocus the input', () => {
-    it('should return the placeholder as initial', () => {});
+    it('should return the placeholder as initial', () => {
+      inputElement.nativeElement.blur();
+
+      expect(inputElement.nativeElement.placeholder).toBe($localize`:@@web_upload_hashtag_placeholder:Find or create a hashtag`);
+    });
   });
 
   describe('when user type in the input', () => {
@@ -48,10 +66,8 @@ describe('SuggesterInputComponent', () => {
       it(`should pass the test if input is ${isValid}`, () => {
         spyOn(component, 'detectTitleKeyboardChanges').and.callThrough();
         spyOn(component, 'isValidKey').and.callThrough();
-        const inputElement = fixture.debugElement.query(By.css('.SuggesterInput'));
 
         inputElement.nativeElement.value = isValid;
-        inputElement.nativeElement.dispatchEvent(new Event('input'));
         inputElement.triggerEventHandler('keyup', {});
         fixture.detectChanges();
         component.isValidKey();
@@ -64,7 +80,6 @@ describe('SuggesterInputComponent', () => {
       it(`should pass the test if input is ${isNotValid}`, () => {
         spyOn(component, 'detectTitleKeyboardChanges').and.callThrough();
         spyOn(component, 'isValidKey').and.callThrough();
-        const inputElement = fixture.debugElement.query(By.css('.SuggesterInput'));
 
         inputElement.nativeElement.value = isNotValid;
         inputElement.nativeElement.dispatchEvent(new Event('input'));
@@ -77,7 +92,24 @@ describe('SuggesterInputComponent', () => {
     });
 
     describe('the input value', () => {
-      it('should have the # symbol infront of the value entered by the user', () => {});
+      it('should have the # symbol infront of the value entered by the user', fakeAsync(() => {
+        spyOn(component, 'detectTitleKeyboardChanges').and.callFake(() => {});
+        inputElement.nativeElement.value = 'sss';
+        inputElement.nativeElement.dispatchEvent(
+          new Event('input', {
+            bubbles: true,
+            cancelable: true,
+          })
+        );
+        // inputElement.triggerEventHandler('keyup', {});
+        // component.detectTitleKeyboardChanges();
+        tick(800);
+        fixture.detectChanges();
+        expect(inputElement.nativeElement.value).toBe('...');
+        /*  fixture.whenStable().then(() => {
+          expect(component.model).toBe('...');
+        }); */
+      }));
     });
 
     describe('when we have the hashtag suggestions', () => {
