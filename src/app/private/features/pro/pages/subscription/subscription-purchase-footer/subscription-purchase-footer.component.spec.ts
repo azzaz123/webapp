60095cde67ee8ced4d8e033ce8a667e3ebd1fed5
Gonzalo Morales <gonzalomorales93@gmail.com>
2021-08-03 13:52:22 +0200

    [PQP-4057]: add test to footer

--- src/app/private/features/pro/pages/subscription/subscription-purchase-footer/subscription-purchase-footer.component.spec.ts
+++ src/app/private/features/pro/pages/subscription/subscription-purchase-footer/subscription-purchase-footer.component.spec.ts
@@ -1,7 +1,8 @@
+import { DatePipe } from '@angular/common';
 import { SimpleChange } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { FREE_TRIAL_AVAILABLE_SUBSCRIPTION, MAPPED_SUBSCRIPTIONS } from '@fixtures/subscriptions.fixtures.spec';
+import { FREE_TRIAL_AVAILABLE_SUBSCRIPTION, MAPPED_SUBSCRIPTIONS, TIER_WITH_DISCOUNT } from '@fixtures/subscriptions.fixtures.spec';
 import { ButtonComponent } from '@shared/button/button.component';
 
 import { SubscriptionPurchaseFooterComponent } from './subscription-purchase-footer.component';
@@ -39,10 +40,24 @@ describe('SubscriptionPurchaseFooterComponent', () => {
       });
     });
     describe('Has no trial available', () => {
-      it('should show pay text', () => {
-        const button: HTMLElement = fixture.debugElement.query(By.directive(ButtonComponent)).nativeElement;
+      describe('and not has discount', () => {
+        it('should show pay text', () => {
+          const button: HTMLElement = fixture.debugElement.query(By.directive(ButtonComponent)).nativeElement;
 
-        expect(button.textContent).toContain($localize`:@@web_pay:Pay`);
+          expect(button.textContent).toContain($localize`:@@web_pay:Pay`);
+        });
+      });
+      describe('and has discount', () => {
+        beforeEach(() => {
+          component.selectedTier = TIER_WITH_DISCOUNT;
+          component.ngOnInit();
+          fixture.detectChanges();
+        });
+        it('should show discount text', () => {
+          const button: HTMLElement = fixture.debugElement.query(By.directive(ButtonComponent)).nativeElement;
+
+          expect(button.textContent).toContain($localize`:@@pro_subscription_purchase_try_discount_button:Try with discount`);
+        });
       });
     });
   });
@@ -109,22 +124,45 @@ describe('SubscriptionPurchaseFooterComponent', () => {
       });
     });
     describe('Has not trial available', () => {
-      beforeEach(() => {
-        fixture.detectChanges();
+      describe('and not has discount', () => {
+        beforeEach(() => {
+          fixture.detectChanges();
+        });
+        it('should not show trial text', () => {
+          const description: HTMLElement = fixture.debugElement.query(By.css('.SubscriptionPurchaseFooter__description')).nativeElement;
+
+          expect(description.textContent).toContain($localize`:@@web_monthly_renewal_plan:The plan will be renewed monthly`);
+        });
+
+        it('should show price', () => {
+          component.ngOnChanges({ selectedTier: new SimpleChange(null, component.selectedTier, false) });
+          fixture.detectChanges();
+
+          const price: HTMLElement = fixture.debugElement.query(By.css('.SubscriptionPurchaseFooter__amount')).nativeElement;
+          expect(price.textContent).toContain(component.selectedTier.price);
+          expect(price.textContent).toContain(component.selectedTier.currency);
+        });
       });
-      it('should not show trial text', () => {
-        const description: HTMLElement = fixture.debugElement.query(By.css('.SubscriptionPurchaseFooter__description')).nativeElement;
+      describe('and has discount', () => {
+        beforeEach(() => {
+          component.selectedTier = TIER_WITH_DISCOUNT;
+          fixture.detectChanges();
+        });
+        it('should show discount text', () => {
+          const description: HTMLElement = fixture.debugElement.query(By.css('.SubscriptionPurchaseFooter__description')).nativeElement;
 
-        expect(description.textContent).toContain($localize`:@@web_monthly_renewal_plan:The plan will be renewed monthly`);
-      });
+          expect(description.textContent).toContain($localize`:@@pro_subscription_purchase_summary_start_period:Starting from`);
+          expect(description.textContent).toContain(new DatePipe('en').transform(component.selectedTier.discount.end_date, 'dd/MM/yy'));
+        });
 
-      it('should show price', () => {
-        component.ngOnChanges({ selectedTier: new SimpleChange(null, component.selectedTier, false) });
-        fixture.detectChanges();
+        it('should show price discounted', () => {
+          component.ngOnChanges({ selectedTier: new SimpleChange(null, component.selectedTier, false) });
+          fixture.detectChanges();
 
-        const price: HTMLElement = fixture.debugElement.query(By.css('.SubscriptionPurchaseFooter__amount')).nativeElement;
-        expect(price.textContent).toContain(component.selectedTier.price);
-        expect(price.textContent).toContain(component.selectedTier.currency);
+          const price: HTMLElement = fixture.debugElement.query(By.css('.SubscriptionPurchaseFooter__amount')).nativeElement;
+          expect(price.textContent).toContain(component.selectedTier.price);
+          expect(price.textContent).toContain(component.selectedTier.currency);
+        });
       });
     });
   });
