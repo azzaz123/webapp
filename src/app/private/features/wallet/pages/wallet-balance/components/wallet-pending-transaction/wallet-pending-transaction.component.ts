c441c0fcedabeb1fd444e49a80abf190582f88dd
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-08-10 13:15:35 +0200

    [WPA-2266]: Style wallet pending transaction component

--- src/app/private/features/wallet/pages/wallet-balance/components/wallet-pending-transaction/wallet-pending-transaction.component.ts
+++ src/app/private/features/wallet/pages/wallet-balance/components/wallet-pending-transaction/wallet-pending-transaction.component.ts
@@ -1,11 +1,18 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { PendingTransaction } from '@api/core/model';
 
 @Component({
   selector: 'tsl-wallet-pending-transaction',
   templateUrl: './wallet-pending-transaction.component.html',
   styleUrls: ['./wallet-pending-transaction.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WalletPendingTransactionComponent {
   @Input() walletPendingTransaction: PendingTransaction;
+
+  public get imageStyle(): Object {
+    return {
+      'background-image': `url(${this.walletPendingTransaction.itemImageUrl})`,
+    };
+  }
 }
