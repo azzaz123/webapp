2714428e7dc72d32f1956f08f2bda66546f18389
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-08-18 18:05:29 +0200

    [WPA-2266]: Animate dropdown of wallet pending transactions

--- src/app/private/features/wallet/pages/wallet-balance/components/wallet-pending-transactions-list/wallet-pending-transactions-list.component.ts
+++ src/app/private/features/wallet/pages/wallet-balance/components/wallet-pending-transactions-list/wallet-pending-transactions-list.component.ts
@@ -1,6 +1,15 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  Input,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 import { PendingTransaction } from '@api/core/model';
+import { WalletPendingTransactionComponent } from '../wallet-pending-transaction/wallet-pending-transaction.component';
 
 const DEFAULT_VISIBLE_PENDING_TRANSACTIONS = 2;
 
@@ -10,17 +19,56 @@ const DEFAULT_VISIBLE_PENDING_TRANSACTIONS = 2;
   styleUrls: ['./wallet-pending-transactions-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class WalletPendingTransactionsListComponent {
+export class WalletPendingTransactionsListComponent implements AfterViewInit {
   @Input() pendingTransactions: PendingTransaction[];
+  @ViewChildren(WalletPendingTransactionComponent, { read: ElementRef }) private pendingTransactionsRef: QueryList<ElementRef>;
+  public isExpanded = false;
 
-  public showAllTransactions = false;
+  constructor(private changeDetectorRef: ChangeDetectorRef) {}
+
+  ngAfterViewInit() {
+    this.changeDetectorRef.detectChanges();
+  }
 
   public isPendingTransactionVisible(index: number): boolean {
-    return this.showAllTransactions || this.isTransactionVisible(index);
+    return this.isExpanded || this.isTransactionVisible(index);
   }
 
   public toggleShowAllTransactions(): void {
-    this.showAllTransactions = !this.showAllTransactions;
+    this.isExpanded = !this.isExpanded;
+    this.changeDetectorRef.detectChanges();
+  }
+
+  public get tansactionsWrapperStyle(): Object {
+    return {
+      height: `${this.getTransactionContainerHeight()}px`,
+    };
+  }
+
+  private getTransactionContainerHeight(): number {
+    const shouldShowAllTransactions = this.isExpanded || this.pendingTransactions.length < DEFAULT_VISIBLE_PENDING_TRANSACTIONS;
+    const shouldShowDefaultTransactions = this.pendingTransactions.length > DEFAULT_VISIBLE_PENDING_TRANSACTIONS;
+
+    if (shouldShowAllTransactions) {
+      return this.getTransactionHeightByPosition(this.pendingTransactions.length);
+    }
+    if (shouldShowDefaultTransactions) {
+      return this.getTransactionHeightByPosition(DEFAULT_VISIBLE_PENDING_TRANSACTIONS);
+    }
+    return 0;
+  }
+
+  private getTransactionHeightByPosition(position: number): number {
+    if (!this.pendingTransactionsRef?.length) {
+      return 0;
+    }
+
+    const pendingTransactionRef = this.pendingTransactionsRef.get(position - 1);
+    const { nativeElement } = pendingTransactionRef;
+    const { parentElement } = nativeElement;
+    const { offsetTop, offsetHeight, parentElement: pendingTransactionRefParent } = parentElement;
+
+    return offsetTop + offsetHeight - pendingTransactionRefParent.offsetTop;
   }
 
   private isTransactionVisible(index: number): boolean {
