7b13c2bbba6858fa033edcc0db471242fc30ae90
Irene Blanco <irene.blanco@wallapop.com>
2021-08-19 12:16:14 +0200

    [FIN-2032]: SearchTrackingEventsService - Added mapping for orderBy values

--- src/app/public/core/services/search-tracking-events/search-tracking-events.service.ts
+++ src/app/public/core/services/search-tracking-events/search-tracking-events.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
+import { SORT_BY } from '@api/core/model';
 import { AnalyticsEvent, ANALYTICS_EVENT_NAMES, ANALYTIC_EVENT_TYPES, SCREEN_IDS, Search } from '@core/analytics/analytics-constants';
 import { AnalyticsService } from '@core/analytics/analytics.service';
+import { SORT_BY_DEFAULT_OPTIONS } from '@public/features/search/components/sort-filter/services/constants/sort-by-options-constants';
 import { FILTER_PARAMETERS_SEARCH } from '@public/features/search/core/services/constants/filter-parameters';
 import { FILTER_QUERY_PARAM_KEY } from '@public/shared/components/filters/enums/filter-query-param-key.enum';
 import { FilterParameter } from '@public/shared/components/filters/interfaces/filter-parameter.interface';
@@ -13,6 +15,14 @@ export class SearchTrackingEventsService {
     [FILTER_QUERY_PARAM_KEY.objectType]: 'objectTypeId',
     [FILTER_QUERY_PARAM_KEY.type]: 'propertyType',
     [FILTER_PARAMETERS_SEARCH.FILTERS_SOURCE]: 'source',
+  };
+
+  private readonly ORDER_BY_VALUE_MAP = {
+    [SORT_BY.DISTANCE]: 'distance',
+    [SORT_BY.NEWEST]: 'newest',
+    [SORT_BY.PRICE_HIGH_TO_LOW]: 'price_high_to_low',
+    [SORT_BY.PRICE_LOW_TO_HIGH]: 'price_low_to_high',
+    [SORT_BY.RELEVANCE]: 'most_relevant',
   };
 
   constructor(private analyticsService: AnalyticsService) {}
@@ -36,10 +46,16 @@ export class SearchTrackingEventsService {
     const FILTER_KEY_EVENT_MAP_KEYS = Object.keys(this.FILTER_KEY_EVENT_MAP);
 
     filterParams.forEach((parameter) => {
+      if (parameter.key === FILTER_QUERY_PARAM_KEY.orderBy) {
+        parameter.value = this.ORDER_BY_VALUE_MAP[parameter.value];
+      }
+
       FILTER_KEY_EVENT_MAP_KEYS.includes(parameter.key)
         ? (filters[this.FILTER_KEY_EVENT_MAP[parameter.key]] = parameter.value)
         : (filters[this.camelize(parameter.key)] = parameter.value);
     });
+
+    console.log(filters);
 
     return filters;
   }
