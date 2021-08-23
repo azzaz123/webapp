b4d29739f0c8c948970cec547298b3c6a57a2048
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-08-23 11:04:16 +0200

    [FIN-1931]: Created placeholder variable to be binded with template

--- src/app/shared/suggester-input/suggester-input.component.ts
+++ src/app/shared/suggester-input/suggester-input.component.ts
@@ -49,6 +49,7 @@ export class SuggesterInputComponent extends AbstractFormComponent<MultiSelectVa
   public isValid: boolean = true;
   public hashtagPlaceholder: string = $localize`:@@web_upload_hashtag_placeholder:Find or create a hashtag`;
   public showOptions: boolean;
+  public placeholder: string;
   private extendedOptions: MultiSelectFormOption[];
   private fromEvent$: Observable<unknown>;
   private subscriptions = new Subscription();
@@ -90,11 +91,11 @@ export class SuggesterInputComponent extends AbstractFormComponent<MultiSelectVa
   }
 
   public focus() {
-    this.hashtagSuggester.nativeElement.placeholder = '#';
+    this.placeholder = '#';
   }
 
   public blur() {
-    this.hashtagSuggester.nativeElement.placeholder = $localize`:@@web_upload_hashtag_placeholder:Find or create a hashtag`;
+    this.placeholder = $localize`:@@web_upload_hashtag_placeholder:Find or create a hashtag`;
   }
 
   public writeValue(value): void {
