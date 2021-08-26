929bfa03ef2ead7dd35e8418b4f772836b518bce
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-08-26 11:50:38 +0200

    [FIN-1931]: Used directive when aiming at the template

--- src/app/public/shared/components/cancel-bubble/cancel-bubble.component.spec.ts
+++ src/app/public/shared/components/cancel-bubble/cancel-bubble.component.spec.ts
@@ -3,8 +3,8 @@ import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { HttpModule } from '@core/http/http.module';
+import { SvgIconComponent } from '@shared/svg-icon/svg-icon.component';
 import { SvgIconModule } from '@shared/svg-icon/svg-icon.module';
-
 import { CancelBubbleComponent } from './cancel-bubble.component';
 
 describe('CancelBubbleComponent', () => {
@@ -45,7 +45,7 @@ describe('CancelBubbleComponent', () => {
     describe('when we click the cross', () => {
       it('should emit the text of the bubble', () => {
         spyOn(component.clear, 'emit');
-        const crossIcon = debugElement.query(By.css('tsl-svg-icon')).nativeNode;
+        const crossIcon = debugElement.query(By.directive(SvgIconComponent)).nativeNode;
 
         crossIcon.click();
 
