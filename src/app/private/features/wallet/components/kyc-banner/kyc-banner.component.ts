62e44e2abf43f4c7e61723ad57f261cf278093b7
Laia Lopez <laialopezz99@gmail.com>
2021-08-31 16:28:16 +0200

    [WPA-1788]: [KYCBannerComponent] Controller logic

--- src/app/private/features/wallet/components/kyc-banner/kyc-banner.component.ts
+++ src/app/private/features/wallet/components/kyc-banner/kyc-banner.component.ts
@@ -1,7 +1,11 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { NgbAlertConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { KYCBannerSpecifications } from '../../interfaces/kyc/kyc-banner.interface';
+import { NgbAlertConfig, NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { KYCBannerSpecifications, KYC_BANNER_STATUS } from '../../interfaces/kyc/kyc-banner.interface';
 import { KYCInfoModalComponent } from '../../modals/kyc-info-modal/kyc-info-modal.component';
+import { KYCStatusModalComponent } from '../../modals/kyc-status-modal/kyc-status-modal.component';
+import { KYC_MODAL_STATUS_PROPERTIES } from '../../modals/kyc/constants/kyc-modal-status-constants';
+import { KYC_MODAL_STATUS_TYPE } from '../../modals/kyc/enums/kyc-modal-status-type-enum';
+import { KYCModalStatus } from '../../modals/kyc/interfaces/kyc-modal-status.interface';
 
 @Component({
   selector: 'tsl-kyc-banner',
@@ -12,18 +16,47 @@ export class KYCBannerComponent {
   @Input() KYCBannerSpecifications: KYCBannerSpecifications;
   constructor(private modalService: NgbModal) {}
 
-  public openKYCSlider(): void {
+  public openModal(): void {
+    if (this.KYCBannerSpecifications.status === KYC_BANNER_STATUS.PENDING) {
+      this.openKYCInformativeSliderModal();
+    } else {
+      this.openKYCStatusModal();
+    }
+  }
+
+  private openKYCInformativeSliderModal(): void {
     this.modalService.open(KYCInfoModalComponent).result.then(() => {});
   }
 
-  get buttonClassName(): string {
+  private openKYCStatusModal(): void {
+    let modalRef: NgbModalRef = this.modalService.open(KYCStatusModalComponent);
+    modalRef.componentInstance.properties = this.KYCStatusProperties;
+
+    modalRef.result.then(() => {});
+  }
+
+  public get buttonClassName(): string {
     return `btn-white btn-white--${this.KYCBannerSpecifications.type}`;
   }
 
-  get ngbAlertConfiguration(): NgbAlertConfig {
+  public get ngbAlertConfiguration(): NgbAlertConfig {
     return {
       dismissible: this.KYCBannerSpecifications.dismissible,
       type: this.KYCBannerSpecifications.type,
     };
+  }
+
+  private get KYCStatusProperties(): KYCModalStatus {
+    const KYCBannerStatus: KYC_BANNER_STATUS = this.KYCBannerSpecifications.status;
+
+    if (KYCBannerStatus === KYC_BANNER_STATUS.PENDING_VERIFICATION) {
+      return KYC_MODAL_STATUS_PROPERTIES.find((property) => property.status === KYC_MODAL_STATUS_TYPE.IN_PROGRESS);
+    }
+    if (KYCBannerStatus === KYC_BANNER_STATUS.VERIFIED) {
+      return KYC_MODAL_STATUS_PROPERTIES.find((property) => property.status === KYC_MODAL_STATUS_TYPE.SUCCEED);
+    }
+    if (KYCBannerStatus === KYC_BANNER_STATUS.REJECTED) {
+      return KYC_MODAL_STATUS_PROPERTIES.find((property) => property.status === KYC_MODAL_STATUS_TYPE.ERROR);
+    }
   }
 }
