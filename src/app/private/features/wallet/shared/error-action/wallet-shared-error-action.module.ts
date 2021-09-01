649016769f07b570a9ae16ced2f2935ac48c8f9a
antoni-casas <toni.casas@wallapop.com>
2021-09-02 00:19:37 +0200

    [WPA-4366]: feat: Add dependencies to error action module

--- src/app/private/features/wallet/shared/error-action/wallet-shared-error-action.module.ts
+++ src/app/private/features/wallet/shared/error-action/wallet-shared-error-action.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { BannerModule } from '@shared/banner/banner.module';
+import { ButtonModule } from '@shared/button/button.module';
 import { WalletSharedErrorActionComponent } from './components/error-action/wallet-shared-error-action.component';
 
 @NgModule({
   declarations: [WalletSharedErrorActionComponent],
-  imports: [CommonModule],
+  imports: [BannerModule, ButtonModule, CommonModule],
   exports: [WalletSharedErrorActionComponent],
 })
 export class WalletSharedErrorActionModule {}
