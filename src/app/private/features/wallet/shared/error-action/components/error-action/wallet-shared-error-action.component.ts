a0f10da7b623e00620496e273e82e22181d44d09
antoni-casas <toni.casas@wallapop.com>
2021-09-02 00:16:39 +0200

    [WPA-4366]: feat: Style the error action component

--- src/app/private/features/wallet/shared/error-action/components/error-action/wallet-shared-error-action.component.ts
+++ src/app/private/features/wallet/shared/error-action/components/error-action/wallet-shared-error-action.component.ts
@@ -1,6 +1,10 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
+import { BANNER_TYPES } from '@shared/banner/banner-types.enum';
+import { PRIVATE_PATHS } from '@private/private-routing-constants';
 import { WalletSharedErrorActionService } from './../../services/wallet-shared-error-action.service';
+import { WalletSharedErrorActionBannerConfigurationInterface } from './../../interfaces/wallet-shared-error-action-banner-configuration.interface';
 
 import { Subscription } from 'rxjs';
 
@@ -15,7 +19,11 @@ export class WalletSharedErrorActionComponent implements OnDestroy, OnInit {
   errorAction: unknown;
   private errorActionSubscription: Subscription;
 
-  constructor(private errorActionService: WalletSharedErrorActionService, private changeDetectionRef: ChangeDetectorRef) {}
+  constructor(
+    private errorActionService: WalletSharedErrorActionService,
+    private changeDetectionRef: ChangeDetectorRef,
+    private router: Router
+  ) {}
 
   ngOnDestroy(): void {
     this.unsubscribe();
@@ -23,6 +31,22 @@ export class WalletSharedErrorActionComponent implements OnDestroy, OnInit {
 
   ngOnInit(): void {
     this.subscribe();
+  }
+
+  get bannerConfiguration(): WalletSharedErrorActionBannerConfigurationInterface {
+    return {
+      buttonText: $localize`:@@wallet_shared_error_action_button:Retry`,
+      description: $localize`:@@wallet_shared_error_action_message:Service unavailable temporarily. Try it later`,
+      dismissible: false,
+      type: BANNER_TYPES.DANGER,
+    };
+  }
+
+  doAction(): void {
+    const currentUrl = this.router.url;
+    this.router.navigateByUrl(`${PRIVATE_PATHS.WALLET}/refresh`, { skipLocationChange: true }).then(() => {
+      this.router.navigate([currentUrl]);
+    });
   }
 
   get showContent(): boolean {
---
+++ src/app/private/features/wallet/shared/error-action/interfaces/wallet-shared-error-action-banner-configuration.interface.ts
@@ -0,0 +1,6 @@
+import { NgbAlertConfig } from '@ng-bootstrap/ng-bootstrap';
+
+export interface WalletSharedErrorActionBannerConfigurationInterface extends NgbAlertConfig {
+  buttonText: string;
+  description: string;
+}
