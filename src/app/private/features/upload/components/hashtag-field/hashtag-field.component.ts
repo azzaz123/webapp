4c087463435a97ce9e42d77f2d5a0399442cc344
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-09-02 15:09:59 +0200

    [FIN-1933]: WIP combine two form

--- src/app/private/features/upload/components/hashtag-field/hashtag-field.component.ts
+++ src/app/private/features/upload/components/hashtag-field/hashtag-field.component.ts
@@ -25,12 +25,13 @@ export class HashtagFieldComponent implements OnInit {
   ];
 
   ngOnInit() {
-    this.detectFormControls();
+    // this.detectFormControls();
     this.form.valueChanges.pipe(startWith({ hashtags: [], searchedHashtags: [] }), pairwise()).subscribe(([prev, next]) => {
-      const newValue = this.onChangeFormControlValue(prev, next).newValue;
-      const formToBePatched = this.onChangeFormControlValue(prev, next).formToBePatched;
-      console.log('final newValue', this.onChangeFormControlValue(prev, next));
-      this.form.get(formToBePatched).patchValue(newValue);
+      const onChangeForm = this.onChangeFormControlValue(prev, next);
+      const newValue = onChangeForm.newValue;
+      const formToBePatched = onChangeForm.formToBePatched;
+      console.log('final newValue', newValue, formToBePatched, this.form.get('hashtags').value, this.form.get('searchedHashtags').value);
+      this.form.get(formToBePatched).patchValue(newValue, { emitEvent: false });
       console.log(this.form.controls.hashtags);
     });
   }
@@ -38,18 +39,20 @@ export class HashtagFieldComponent implements OnInit {
   private detectFormControls() {
     this.form.get('hashtags').valueChanges.subscribe((n) => {
       console.log('hashtags', n);
+      this.form.get('searchedHashtags').valueChanges.subscribe(() => {});
     });
 
-    this.form.get('searchedHashtags').valueChanges.subscribe((n) => {
+    /* this.form.get('searchedHashtags').valueChanges.subscribe((n) => {
       console.log('searched', n);
-    });
+    }); */
   }
 
   private onChangeFormControlValue(prev, next): { newValue: string[]; formToBePatched: string } {
     console.log(prev, next);
     let newValue = [];
     // Compare the property of object and only patch the one that is not changed
-    if (prev['hashtags'] !== next['hashtags']) {
+    if (prev.hashtags.length !== next.hashtags.length) {
+      console.log('hashtags changed', prev, next);
       let formToBePatched = 'searchedHashtags';
       if (next['hashtags'].length > prev['hashtags'].length) {
         const lastItemIndex = next['hashtags'].length - 1;
@@ -64,7 +67,8 @@ export class HashtagFieldComponent implements OnInit {
         console.log('remove', newValue);
         return { newValue, formToBePatched };
       }
-    } else if (prev['searchedHashtags'] !== next['searchedHashtags']) {
+    } else if (prev.searchedHashtags.length !== next.searchedHashtags.length) {
+      console.log('searchedHashtags changed', prev, next);
       let formToBePatched = 'hashtags';
       if (next['searchedHashtags'].length > prev['searchedHashtags'].length) {
         const lastItemIndex = next['searchedHashtags'].length - 1;
@@ -75,10 +79,18 @@ export class HashtagFieldComponent implements OnInit {
         return { newValue, formToBePatched };
       } else {
         next['hashtags'].pop();
+        /* newValue = prev['hashtags'].filter((n) => {
+          if (!next['hashtags'].includes(n)) {
+            return n;
+          }
+        }); */
         newValue = next['hashtags'];
-        console.log('remove', newValue);
+        console.log('searchedHashtags remove', newValue);
         return { newValue, formToBePatched };
       }
+    } else {
+      console.log('NON ABOVE');
+      return;
     }
   }
 
