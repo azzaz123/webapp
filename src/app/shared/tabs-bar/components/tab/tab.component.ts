fad3fe97cdd1f101c97d57af8ebf819628204eca
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-09-02 15:19:34 +0200

    [WPA-2270]: Show label in tab component

--- src/app/shared/tabs-bar/components/tab/tab.component.ts
+++ src/app/shared/tabs-bar/components/tab/tab.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { TabsBarElement } from '@shared/tabs-bar/interfaces/tabs-bar-element.interface';
 
 @Component({
   selector: 'tsl-tab',
   templateUrl: './tab.component.html',
   styleUrls: ['./tab.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TabComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class TabComponent {
+  @Input() tabsBarElement: TabsBarElement;
+  @Input() isSelected = false;
 }
