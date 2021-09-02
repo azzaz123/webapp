5659ec1e13242962ff9e41dc8b73769af9236a4a
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-09-02 11:55:40 +0200

    [WPA-2270]: Create Tab component

--- src/app/shared/tabs-bar/tabs-bar.module.ts
+++ src/app/shared/tabs-bar/tabs-bar.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TabsBarComponent } from './components/tabs-bar.component';
+import { TabComponent } from './components/tab/tab.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [TabsBarComponent],
-  exports: [TabsBarComponent],
+  declarations: [TabsBarComponent, TabComponent],
+  exports: [TabsBarComponent, TabComponent],
 })
 export class TabsBarModule {}
