0ebaa7190d1ea3cd44906d266cb837d6030027cb
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-09-02 11:44:02 +0200

    [WPA-2270]: Create TabsBar component

---
+++ src/app/shared/tabs-bar/components/tabs-bar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tsl-tabs-bar',
+  templateUrl: './tabs-bar.component.html',
+  styleUrls: ['./tabs-bar.component.scss'],
+})
+export class TabsBarComponent {}
