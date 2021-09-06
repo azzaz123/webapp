5bb8acb47570fa6f1a8f31c7c5edeb9d8ebf2be3
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-09-06 20:24:23 +0200

    [WPA-2270]: Add Wallet Balance History module to UI module

--- src/app/private/features/wallet/pages/wallet-history-movements/wallet-history-movements.module.ts
+++ src/app/private/features/wallet/pages/wallet-history-movements/wallet-history-movements.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { walletHistoryMovementsRoutedComponents, WalletHistoryMovementsRoutingModule } from './wallet-history-movements.routing.module';
 import { TabsBarModule } from '@shared/tabs-bar/tabs-bar.module';
+import { WalletBalanceHistoryModule } from '@api/bff/delivery/wallets/balance_history/wallet-balance-history.module';
 
 @NgModule({
   declarations: [walletHistoryMovementsRoutedComponents],
-  imports: [CommonModule, WalletHistoryMovementsRoutingModule, TabsBarModule],
+  imports: [CommonModule, WalletBalanceHistoryModule, WalletHistoryMovementsRoutingModule, TabsBarModule],
 })
 export class WalletHistoryMovementsModule {}
