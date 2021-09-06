438f9b824acf61c45d0170d58ffa8a49581af9c8
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-09-06 20:28:44 +0200

    [WPA-2270]: Add Wallet History Movement interface

---
+++ src/app/api/core/model/wallet/history/movement-history-detail.ts
@@ -0,0 +1,14 @@
+import { Money } from '../../money.interface';
+
+export enum WALLET_HISTORY_MOVEMENT_TYPE {
+  IN,
+  OUT,
+}
+
+export interface WalletMovementHistoryDetail {
+  imageUrl: string;
+  type: WALLET_HISTORY_MOVEMENT_TYPE;
+  title: string;
+  date: Date;
+  moneyAmmount: Money;
+}
