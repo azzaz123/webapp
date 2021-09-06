7fde5086db072f3a15bf82610c554728f1dc8992
UXDESIGNERLIN <chia-ling@wallapop.com>
2021-09-06 08:31:51 +0200

    [FIN-1933]: Refactor code

--- src/app/private/features/upload/components/hashtag-field/hashtag-field.component.ts
+++ src/app/private/features/upload/components/hashtag-field/hashtag-field.component.ts
@@ -1,3 +1,4 @@
+import { JsonpClientBackend } from '@angular/common/http';
 import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { FormControlName, FormGroup } from '@angular/forms';
 import { SelectFormOption } from '@shared/form/components/select/interfaces/select-form-option.interface';
@@ -16,118 +17,46 @@ export class HashtagFieldComponent implements OnInit {
   @ViewChild('generalHashtagForm') generalHashtagForm: ElementRef;
   public maxHashtagsNumber: number = 5;
   public page: number = 0;
-  /* public disabled: boolean = (this.form.value.hashtags.length === this.maxHashtagsNumber) */
-  public searchedOptions = [
-    { label: '#searched', value: '#searched' },
-    { label: '#faa', value: '#faa' },
-    { label: '#d', value: '#d' },
-    { label: '#xx', value: '#xx' },
-  ];
 
   ngOnInit() {
-    // this.detectFormControls();
     this.form.valueChanges
-      .pipe(startWith({ hashtags: [], searchedHashtags: [] }), distinctUntilChanged() /**for the emitEvent */, pairwise())
+      .pipe(
+        startWith({ hashtags: [], searchedHashtags: [] }),
+        distinctUntilChanged((prev, next) => {
+          return JSON.stringify(prev) === JSON.stringify(next);
+        }),
+        pairwise()
+      )
       .subscribe(([prev, next]) => {
-        const onChangeForm = this.onChangeFormControlValue(prev, next);
-        const newValue = onChangeForm.newValue;
-        const formToBePatched = onChangeForm.formToBePatched;
-        console.log('final newValue', newValue, formToBePatched, this.form.get('hashtags').value, this.form.get('searchedHashtags').value);
-        this.form.get(formToBePatched).patchValue(newValue, { emitEvent: false }); // Problem might be here, change this code to the code below
-        // this.form.patchValue(next, { emitEvent: true });
-        console.log(this.form.controls.hashtags);
+        this.onChangeFormControlValue(prev, next); // Problem might be here, change this code to the code below
+        this.form.patchValue(next, { emitEvent: true });
       });
   }
 
-  private onChangeFormControlValue(prev, next): { newValue: string[]; formToBePatched: string } {
-    console.log(prev, next);
-    let newValue = [];
+  private onChangeFormControlValue(prev, next): void {
     let formToBePatched: string;
     let modifiedForm: string;
     // Compare the property of object and only patch the one that is not changed
     if (prev.hashtags.length !== next.hashtags.length) {
-      console.log('hashtags changed', prev, next);
       formToBePatched = 'searchedHashtags';
       modifiedForm = 'hashtags';
-      /*  let formToBePatched = 'searchedHashtags'; */
-      /*   if (next['hashtags'].length > prev['hashtags'].length) {
-        const lastItemIndex = next['hashtags'].length - 1;
-        const newSelected = next['hashtags'][lastItemIndex];
-        // Add it and remove the repeated value (Add it and remove it if it was already selected)
-        next['searchedHashtags'].push(newSelected);
-        next['searchedHashtags'] = next['searchedHashtags'].filter((item, pos, self) => self.indexOf(item) === pos); // Remove repeate value
-        newValue = next['searchedHashtags'];
-        console.log('add', newValue);
-        return { newValue, formToBePatched };
-      } else {
-        const hashtagToRemove = prev['hashtags'].find((n) => {
-          return !next['hashtags'].includes(n);
-        });
-
-        newValue = next['searchedHashtags'].filter((n) => {
-          return n !== hashtagToRemove;
-        });
-        console.log('remove', newValue);
-        return { newValue, formToBePatched };
-      } */
     } else if (prev.searchedHashtags.length !== next.searchedHashtags.length) {
-      console.log('searchedHashtags changed', prev, next);
-      let formToBePatched = 'hashtags';
-      if (next['searchedHashtags'].length > prev['searchedHashtags'].length) {
-        const lastItemIndex = next['searchedHashtags'].length - 1;
-        const newSelected = next['searchedHashtags'][lastItemIndex];
-        next['hashtags'].push(newSelected);
-        newValue = next['hashtags'];
-        console.log('add', newValue);
-        return { newValue, formToBePatched };
-      } else {
-        next['hashtags'].pop();
-        newValue = next['hashtags'];
-        console.log('searchedHashtags remove', newValue);
-        return { newValue, formToBePatched };
-      }
-    } else {
-      console.log('NON ABOVE');
-      return;
+      formToBePatched = 'hashtags';
+      modifiedForm = 'searchedHashtags';
     }
     if (next[modifiedForm].length > prev[modifiedForm].length) {
-      const lastItemIndex = next['hashtags'].length - 1;
-      const newSelected = next['hashtags'][lastItemIndex];
+      const lastItemIndex = next[modifiedForm].length - 1;
+      const newSelected = next[modifiedForm][lastItemIndex];
       // Add it and remove the repeated value (Add it and remove it if it was already selected)
-      next['searchedHashtags'].push(newSelected);
-      next['searchedHashtags'] = next['searchedHashtags'].filter((item, pos, self) => self.indexOf(item) === pos); // Remove repeate value
-      newValue = next['searchedHashtags'];
-      console.log('add', newValue);
-      return { newValue, formToBePatched };
+      next[formToBePatched].push(newSelected);
+      next[formToBePatched] = next[formToBePatched].filter((item, pos, self) => self.indexOf(item) === pos); // Remove repeate value
     } else {
-      const hashtagToRemove = prev['hashtags'].find((n) => {
-        return !next['hashtags'].includes(n);
+      const hashtagToRemove = prev[modifiedForm].find((n) => {
+        return !next[modifiedForm].includes(n);
       });
-
-      newValue = next['searchedHashtags'].filter((n) => {
+      next[formToBePatched] = next[formToBePatched].filter((n) => {
         return n !== hashtagToRemove;
       });
-      console.log('remove', newValue);
-      return { newValue, formToBePatched };
-    }
-  }
-
-  public bindFormValues(): string[] {
-    const loadedHashtags = this.form.get('hashtags').value;
-    const searchedHashtags = this.form.get('searchedHashtags').value;
-
-    let array = loadedHashtags.concat(searchedHashtags);
-    array = [...new Set([...loadedHashtags, ...searchedHashtags])];
-
-    console.log('a', array);
-    return array;
-  }
-
-  public loadMore(event): void {
-    if ((event.target.scrollTop = event.target.offsetTop)) {
-      this.page++;
-      this.start.emit(this.page.toString());
-      return;
     }
   }
 }
