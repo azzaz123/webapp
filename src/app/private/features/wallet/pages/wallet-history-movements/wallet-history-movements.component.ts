556a6ddb8700ee8b37e7e1d03654c893dadef983
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-09-08 13:30:36 +0200

    [WPA-2270]: Use and style a bit the Wallet History Movement component

--- src/app/private/features/wallet/pages/wallet-history-movements/wallet-history-movements.component.ts
+++ src/app/private/features/wallet/pages/wallet-history-movements/wallet-history-movements.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WalletBalanceHistoryService } from '@api/bff/delivery/wallets/balance_history/wallet-balance-history.service';
+import { WalletMovementsHistory } from '@api/core/model/wallet/history/movements-history';
 import { TabsBarElement } from '@shared/tabs-bar/interfaces/tabs-bar-element.interface';
+import { Observable } from 'rxjs';
 import { WALLET_HISTORY_FILTERS } from './wallet-history-filters.enum';
 
 @Component({
@@ -7,10 +10,18 @@ import { WALLET_HISTORY_FILTERS } from './wallet-history-filters.enum';
   templateUrl: './wallet-history-movements.component.html',
   styleUrls: ['./wallet-history-movements.component.scss'],
 })
-export class WalletHistoryMovementsComponent {
+export class WalletHistoryMovementsComponent implements OnInit {
+  public historicMovements$: Observable<WalletMovementsHistory>;
+
   public tabBarElements: TabsBarElement<WALLET_HISTORY_FILTERS>[] = [
     { value: WALLET_HISTORY_FILTERS.ALL, label: $localize`:@@movements_history_all_users_all_movements_tap_bar_title:All` },
     { value: WALLET_HISTORY_FILTERS.IN, label: $localize`:@@movements_history_all_users_inflows_tap_bar_title:Inflows` },
     { value: WALLET_HISTORY_FILTERS.OUT, label: $localize`:@@movements_history_all_users_outflows_tap_bar_title:Outflows` },
   ];
+
+  constructor(private walletBalanceHistoryService: WalletBalanceHistoryService) {}
+
+  ngOnInit() {
+    this.historicMovements$ = this.walletBalanceHistoryService.get(0);
+  }
 }
--- src/app/private/features/wallet/pages/wallet-history-movements/wallet-history-movements.module.ts
+++ src/app/private/features/wallet/pages/wallet-history-movements/wallet-history-movements.module.ts
@@ -3,9 +3,13 @@ import { CommonModule } from '@angular/common';
 import { walletHistoryMovementsRoutedComponents, WalletHistoryMovementsRoutingModule } from './wallet-history-movements.routing.module';
 import { TabsBarModule } from '@shared/tabs-bar/tabs-bar.module';
 import { WalletBalanceHistoryModule } from '@api/bff/delivery/wallets/balance_history/wallet-balance-history.module';
+import { WalletHistoryMovementComponent } from './components/wallet-history-movement/wallet-history-movement.component';
+import { SvgIconModule } from '@shared/svg-icon/svg-icon.module';
+
+const walletHistoryMovementsNonRoutedComponents = [WalletHistoryMovementComponent];
 
 @NgModule({
-  declarations: [walletHistoryMovementsRoutedComponents],
-  imports: [CommonModule, WalletBalanceHistoryModule, WalletHistoryMovementsRoutingModule, TabsBarModule],
+  declarations: [walletHistoryMovementsRoutedComponents, walletHistoryMovementsNonRoutedComponents],
+  imports: [CommonModule, WalletBalanceHistoryModule, WalletHistoryMovementsRoutingModule, SvgIconModule, TabsBarModule],
 })
 export class WalletHistoryMovementsModule {}
