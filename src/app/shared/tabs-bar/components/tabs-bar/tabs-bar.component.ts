0951fe7ee4632c75037c3682770b61e82f3121c5
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-09-13 19:01:09 +0200

    [WPA-4745]: Refactor Tabs Bar component logic and remove initial selected value

--- src/app/shared/tabs-bar/components/tabs-bar/tabs-bar.component.ts
+++ src/app/shared/tabs-bar/components/tabs-bar/tabs-bar.component.ts
@@ -1,47 +1,38 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { TabsBarElement } from '../../interfaces/tabs-bar-element.interface';
 
 @Component({
   selector: 'tsl-tabs-bar',
   templateUrl: './tabs-bar.component.html',
   styleUrls: ['./tabs-bar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TabsBarComponent implements OnInit {
-  @Input() tabsBarElements: TabsBarElement[] = [];
-  @Input() initialSelectedTabBarElement: TabsBarElement;
-  @Output() onChange: EventEmitter<TabsBarElement> = new EventEmitter<TabsBarElement>();
+export class TabsBarComponent<T> {
+  @Input()
+  set tabsBarElements(newTabsBarElements: TabsBarElement<T>[]) {
+    this.setFirstElementAsSelected(newTabsBarElements);
+    this._tabsBarElements$.next(newTabsBarElements);
+  }
+  @Output() onChange: EventEmitter<TabsBarElement<T>> = new EventEmitter<TabsBarElement<T>>();
 
-  public selectedTabBarElement: TabsBarElement;
-
-  ngOnInit() {
-    this.checkDefaultSelectedTabBarElement();
+  private selectedTabBarElement: TabsBarElement<T>;
+  private readonly _tabsBarElements$: BehaviorSubject<TabsBarElement<T>[]> = new BehaviorSubject<TabsBarElement<T>[]>(null);
+  public get tabsBarElements$(): Observable<TabsBarElement<T>[]> {
+    return this._tabsBarElements$.asObservable();
   }
 
-  public handleClick(tabBarElement: TabsBarElement): void {
+  public handleClick(tabBarElement: TabsBarElement<T>): void {
     this.onChange.emit(tabBarElement);
     this.selectedTabBarElement = tabBarElement;
   }
 
-  public isTabBarSelected(tabBarElement: TabsBarElement): boolean {
+  public isTabBarSelected(tabBarElement: TabsBarElement<T>): boolean {
     return tabBarElement === this.selectedTabBarElement;
   }
 
-  private checkDefaultSelectedTabBarElement(): void {
-    const isInitialTabBarElementDefined = !!this.initialSelectedTabBarElement;
-    if (isInitialTabBarElementDefined) {
-      this.selectedTabBarElement = Object.assign({}, this.initialSelectedTabBarElement);
-      return;
-    }
-
-    this.setFirstElementAsSelectedTabBarElement();
-  }
-
-  private setFirstElementAsSelectedTabBarElement(): void {
-    const hasNoElements = this.tabsBarElements.length === 0;
-    if (hasNoElements) {
-      this.selectedTabBarElement = null;
-      return;
-    }
-    this.selectedTabBarElement = this.tabsBarElements[0];
+  private setFirstElementAsSelected(tabsBarElements: TabsBarElement<T>[]): void {
+    const firstElement: TabsBarElement<T> | null = tabsBarElements.length ? tabsBarElements[0] : null;
+    this.selectedTabBarElement = firstElement;
   }
 }
