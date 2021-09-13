8d5ad436cde5a3edc96f86ad9a394f85bde5d9ef
antoni-casas <toni.casas@wallapop.com>
2021-09-13 12:05:40 +0200

    [WPA-2269]: feat: styling modal component

--- src/app/private/features/wallet/modals/transfer/components/modal/wallet-transfer-modal.component.spec.ts
+++ src/app/private/features/wallet/modals/transfer/components/modal/wallet-transfer-modal.component.spec.ts
@@ -1,19 +1,27 @@
+import { By } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-import { SvgIconComponent } from '@shared/svg-icon/svg-icon.component';
+import { ButtonModule } from '@shared/button/button.module';
+import { SvgIconModule } from '@shared/svg-icon/svg-icon.module';
 import { WalletTransferModalComponent } from '@private/features/wallet/modals/transfer/components/modal/wallet-transfer-modal.component';
 
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('GIVEN the WalletTransferModalComponent', () => {
+  const walletTransferModalSelector = '.WalletTransferModal';
+  const spinnerSelector = `${walletTransferModalSelector}__spinner`;
+  const balanceSelector = `${walletTransferModalSelector}__amount`;
+  const errorSelector = `${walletTransferModalSelector}__error`;
+
   let component: WalletTransferModalComponent;
   let fixture: ComponentFixture<WalletTransferModalComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SvgIconComponent, WalletTransferModalComponent],
-      imports: [HttpClientTestingModule],
+      declarations: [WalletTransferModalComponent],
+      imports: [ButtonModule, CommonModule, HttpClientTestingModule, SvgIconModule],
       providers: [NgbActiveModal],
     }).compileComponents();
   });
@@ -27,6 +35,13 @@ describe('GIVEN the WalletTransferModalComponent', () => {
   describe('WHEN displaying the view', () => {
     it('should be created', () => {
       expect(component).toBeTruthy();
+    });
+
+    it('should show the spinner', () => {
+      component.ngOnInit();
+
+      const target = fixture.debugElement.query(By.css(spinnerSelector));
+      expect(target).toBeTruthy();
     });
   });
 });
--- src/app/private/features/wallet/modals/transfer/components/modal/wallet-transfer-modal.component.ts
+++ src/app/private/features/wallet/modals/transfer/components/modal/wallet-transfer-modal.component.ts
@@ -1,16 +1,66 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 
+import { Money } from '@api/core/model/money.interface';
+import { PaymentsWalletsHttpService } from '@api/payments/wallets/http/payments-wallets-http.service';
+import { PaymentsWalletsService } from '@api/payments/wallets/payments-wallets.service';
+
+import { finalize } from 'rxjs/operators';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'tsl-wallet-transfer-modal',
   templateUrl: './wallet-transfer-modal.component.html',
   styleUrls: ['./wallet-transfer-modal.component.scss'],
+  providers: [PaymentsWalletsHttpService, PaymentsWalletsService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class WalletTransferModalComponent {
-  constructor(private activeModal: NgbActiveModal) {}
+export class WalletTransferModalComponent implements OnInit {
+  private error: boolean;
+  private loading: boolean = true;
+  private walletBalance: Money;
+
+  constructor(
+    private activeModal: NgbActiveModal,
+    private changeDetectorRef: ChangeDetectorRef,
+    private paymentsWalletsService: PaymentsWalletsService
+  ) {}
+
+  public ngOnInit(): void {
+    this.loadBalance();
+  }
 
   public closeModal(): void {
     this.activeModal.close();
+  }
+
+  public get showBalance(): boolean {
+    return !this.showError && !!this.walletBalance;
+  }
+
+  public get showError(): boolean {
+    return !this.showSpinner && this.error;
+  }
+  public get showSpinner(): boolean {
+    return this.loading;
+  }
+
+  private loadBalance(): void {
+    this.changeDetectorRef.detectChanges();
+
+    this.paymentsWalletsService.walletBalance$
+      .pipe(
+        finalize(() => {
+          this.loading = false;
+          this.changeDetectorRef.detectChanges();
+        })
+      )
+      .subscribe({
+        next: (walletBalance) => {
+          this.walletBalance = walletBalance;
+        },
+        error: () => {
+          this.error = true;
+        },
+      });
   }
 }
