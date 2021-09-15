b658646bf053f096d93c2646350cd19e4855c60c
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-09-15 15:02:26 +0200

    [WPA-2270]: Remove unnecessary attribute from wallet history list interface

--- src/app/api/core/model/wallet/history/wallet-movements-history-list.interface.ts
+++ src/app/api/core/model/wallet/history/wallet-movements-history-list.interface.ts
@@ -3,6 +3,5 @@ import { Money } from '../../money.interface';
 import { WalletMovementHistoryDetail } from './movement-history-detail';
 
 export interface WalletMovementsHistoryList extends PaginatedList<WalletMovementHistoryDetail, number | null> {
-  list: WalletMovementHistoryDetail[];
   walletBalance: Money;
 }
