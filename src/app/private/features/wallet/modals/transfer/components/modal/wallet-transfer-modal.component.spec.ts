287b8685fa12c84e30e2305646f3889a38e19756
antoni-casas <toni.casas@wallapop.com>
2021-09-16 09:39:04 +0200

    [WPA-2269]: feat: style modal component

--- src/app/private/features/wallet/modals/transfer/components/modal/wallet-transfer-modal.component.spec.ts
+++ src/app/private/features/wallet/modals/transfer/components/modal/wallet-transfer-modal.component.spec.ts
@@ -1,27 +1,20 @@
-import { By } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-import { ButtonModule } from '@shared/button/button.module';
-import { SvgIconModule } from '@shared/svg-icon/svg-icon.module';
+import { SvgIconComponent } from '@shared/svg-icon/svg-icon.component';
+import { WalletTransferAmountComponent } from '@private/features/wallet/modals/transfer/components/amount/wallet-transfer-amount.component';
 import { WalletTransferModalComponent } from '@private/features/wallet/modals/transfer/components/modal/wallet-transfer-modal.component';
 
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 describe('GIVEN the WalletTransferModalComponent', () => {
-  const walletTransferModalSelector = '.WalletTransferModal';
-  const spinnerSelector = `${walletTransferModalSelector}__spinner`;
-  const balanceSelector = `${walletTransferModalSelector}__amount`;
-  const errorSelector = `${walletTransferModalSelector}__error`;
-
   let component: WalletTransferModalComponent;
   let fixture: ComponentFixture<WalletTransferModalComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [WalletTransferModalComponent],
-      imports: [ButtonModule, CommonModule, HttpClientTestingModule, SvgIconModule],
+      declarations: [SvgIconComponent, WalletTransferAmountComponent, WalletTransferModalComponent],
+      imports: [HttpClientTestingModule],
       providers: [NgbActiveModal],
     }).compileComponents();
   });
@@ -35,13 +28,6 @@ describe('GIVEN the WalletTransferModalComponent', () => {
   describe('WHEN displaying the view', () => {
     it('should be created', () => {
       expect(component).toBeTruthy();
-    });
-
-    it('should show the spinner', () => {
-      component.ngOnInit();
-
-      const target = fixture.debugElement.query(By.css(spinnerSelector));
-      expect(target).toBeTruthy();
     });
   });
 });
