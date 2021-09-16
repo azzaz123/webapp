78e7c401793403f56260419637bf7b4b4777bf98
Irene Blanco <irene.blanco@wallapop.com>
2021-09-16 17:14:45 +0200

    [FIN-2072]: MultiSelectFormComponent - Added interface to manage nested options

--- src/app/shared/form/components/multi-select-form/multi-select-form.component.ts
+++ src/app/shared/form/components/multi-select-form/multi-select-form.component.ts
@@ -3,7 +3,7 @@ import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { AbstractFormComponent } from '@shared/form/abstract-form/abstract-form-component';
 import { Observable, BehaviorSubject } from 'rxjs';
 import { SelectFormOption } from '../select/interfaces/select-form-option.interface';
-import { MultiSelectFormOption } from './interfaces/multi-select-form-option.interface';
+import { MultiSelectFormOption, TemplateMultiSelectFormOption } from './interfaces/multi-select-form-option.interface';
 import { MultiSelectValue } from './interfaces/multi-select-value.type';
 @Component({
   selector: 'tsl-multi-select-form',
@@ -19,7 +19,7 @@ import { MultiSelectValue } from './interfaces/multi-select-value.type';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MultiSelectFormComponent extends AbstractFormComponent<MultiSelectValue> {
-  @Input() set options(value: SelectFormOption<string>[]) {
+  @Input() set options(value: MultiSelectFormOption[]) {
     this.extendedOptions = value.map((option) => {
       return { ...option, checked: false };
     });
@@ -29,9 +29,9 @@ export class MultiSelectFormComponent extends AbstractFormComponent<MultiSelectV
   }
   @Input() disabled: boolean = false;
 
-  private extendedOptions: MultiSelectFormOption[] = [];
-  private extendedOptionsSubject: BehaviorSubject<MultiSelectFormOption[]> = new BehaviorSubject([]);
-  public extendedOptions$: Observable<MultiSelectFormOption[]> = this.extendedOptionsSubject.asObservable();
+  private extendedOptions: TemplateMultiSelectFormOption[] = [];
+  private extendedOptionsSubject: BehaviorSubject<TemplateMultiSelectFormOption[]> = new BehaviorSubject([]);
+  public extendedOptions$: Observable<TemplateMultiSelectFormOption[]> = this.extendedOptionsSubject.asObservable();
 
   public writeValue(value: MultiSelectValue): void {
     this.value = value;
@@ -50,7 +50,7 @@ export class MultiSelectFormComponent extends AbstractFormComponent<MultiSelectV
   }
 
   private mapCheckedValue(): void {
-    this.extendedOptions = this.extendedOptions.map((option: MultiSelectFormOption) => {
+    this.extendedOptions = this.extendedOptions.map((option: TemplateMultiSelectFormOption) => {
       option.checked = this.value?.includes(option.value);
       return { ...option };
     });
