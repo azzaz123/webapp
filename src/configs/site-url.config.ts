80c3e19246e06223ac5bacbb4d0a376b7a83bb5c
Alex Masnou <alex.masnou@wallapop.com>
2021-09-17 15:45:59 +0200

    [WEB-563]: Refactor code for being more readable

--- src/configs/site-url.config.ts
+++ src/configs/site-url.config.ts
@@ -1,15 +1,23 @@
 import { InjectionToken } from '@angular/core';
 import { environment } from '@environments/environment';
-import { SITE_URL_SUBDOMAINS } from './subdomains.config';
+import { SEO_WEB_SUBDOMAIN, SITE_URL_SUBDOMAINS } from './subdomains.config';
 
 export const SITE_URL: InjectionToken<string> = new InjectionToken<string>('SITE_URL');
-const SUBDOMAIN_REGEX = new RegExp('/(?:http[s]*://)*(.*?).(?=[^/]*..{2,5})/i');
+const SUBDOMAIN_REGEX = /(?:http[s]*\:\/\/)*(.*?)\.(?=[^\/]*\..{2,5})/i;
 
 export function siteUrlFactory(window: Window): string {
   const hostname = window.location.hostname;
   const hasSubdomain = SUBDOMAIN_REGEX.test(hostname);
-  const subdomain = hasSubdomain ? hostname.match(SUBDOMAIN_REGEX)[1] : 'www';
-  const siteUrlSubdomain = SITE_URL_SUBDOMAINS[subdomain];
 
-  return `${environment.protocol}${siteUrlSubdomain}${environment.appDomain}`;
+  if (hasSubdomain) {
+    const subdomain = hostname.match(SUBDOMAIN_REGEX)[1];
+    const siteUrlSubdomain: SEO_WEB_SUBDOMAIN = SITE_URL_SUBDOMAINS[subdomain] || 'www';
+
+    return getSiteUrl(siteUrlSubdomain);
+  }
+  return getSiteUrl(SITE_URL_SUBDOMAINS['www']);
+}
+
+function getSiteUrl(subdomain: SEO_WEB_SUBDOMAIN) {
+  return `${environment.protocol}${SITE_URL_SUBDOMAINS[subdomain]}${environment.appDomain}`;
 }
