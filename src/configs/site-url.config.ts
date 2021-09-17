dbd1020503b8a0cd3b4c7e97387ef29ba591d866
Alex Masnou <alex.masnou@wallapop.com>
2021-09-17 12:27:25 +0200

    [WEB-563]: Add function for generating site url based on subdomain

--- src/configs/site-url.config.ts
+++ src/configs/site-url.config.ts
@@ -1,9 +1,15 @@
 import { InjectionToken } from '@angular/core';
 import { environment } from '@environments/environment';
-import { APP_LOCALE, SUBDOMAINS } from './subdomains.config';
+import { SITE_URL_SUBDOMAINS } from './subdomains.config';
 
 export const SITE_URL: InjectionToken<string> = new InjectionToken<string>('SITE_URL');
+const SUBDOMAIN_REGEX = new RegExp('/(?:http[s]*://)*(.*?).(?=[^/]*..{2,5})/i');
 
-export function siteUrlFactory(locale: APP_LOCALE): string {
-  return `${environment.protocol}${SUBDOMAINS[locale]}${environment.appDomain}`;
+export function siteUrlFactory(window: Window): string {
+  const hostname = window.location.hostname;
+  const hasSubdomain = SUBDOMAIN_REGEX.test(hostname);
+  const subdomain = hasSubdomain ? hostname.match(SUBDOMAIN_REGEX)[1] : 'www';
+  const siteUrlSubdomain = SITE_URL_SUBDOMAINS[subdomain];
+
+  return `${environment.protocol}${siteUrlSubdomain}${environment.appDomain}`;
 }
