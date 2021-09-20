707057841992790a658f7604605a4876215ed8ff
antoni-casas <toni.casas@wallapop.com>
2021-09-20 18:40:59 +0200

    [WPA-2269]: feat: Add directive to jump between controls

---
+++ src/app/private/features/wallet/modals/transfer/directives/jump/wallet-transfer-jump.directive.spec.ts
@@ -0,0 +1,8 @@
+import { WalletTransferJumpDirective } from './wallet-transfer-jump.directive';
+
+describe('WalletTransferJumpDirective', () => {
+  it('should create an instance', () => {
+    const directive = new WalletTransferJumpDirective();
+    expect(directive).toBeTruthy();
+  });
+});
