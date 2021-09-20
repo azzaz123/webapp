4442dc2cb78ef81db50528402d4b4d41b9734f40
antoni-casas <toni.casas@wallapop.com>
2021-09-20 18:41:58 +0200

    [WPA-2269]: feat: Add directive to manage maximum length in number inputs

---
+++ src/app/private/features/wallet/modals/transfer/directives/max-length/wallet-transfer-max-length.directive.spec.ts
@@ -0,0 +1,8 @@
+import { WalletTransferMaxLengthDirective } from '@private/features/wallet/modals/transfer/directives/max-length/wallet-transfer-max-length.directive';
+
+describe('WalletTransferMaxLengthDirective', () => {
+  it('should create an instance', () => {
+    const directive = new WalletTransferMaxLengthDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/private/features/wallet/modals/transfer/directives/max-length/wallet-transfer-max-length.directive.ts
@@ -0,0 +1,31 @@
+import { Directive, ElementRef, HostListener, Inject, Input } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+const allowedKeys: string[] = ['ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowUp', 'Backspace', 'Delete', 'Tab'];
+
+@Directive({
+  selector: '[tslWalletTransferMaxLength]',
+})
+export class WalletTransferMaxLengthDirective {
+  @Input()
+  public tslWalletTransferMaxLength: number;
+
+  constructor(@Inject(DOCUMENT) private document: Document, private element: ElementRef) {}
+
+  @HostListener('keydown', ['$event']) onKeyDown(e: KeyboardEvent) {
+    if (this.isFull && !this.hasSelection && !allowedKeys.find((item) => item === e.key)) {
+      e.preventDefault();
+    }
+  }
+
+  private get isFull(): boolean {
+    return (
+      this.tslWalletTransferMaxLength > 0 &&
+      (this.element.nativeElement as HTMLInputElement).value.length >= this.tslWalletTransferMaxLength
+    );
+  }
+
+  private get hasSelection(): boolean {
+    return this.document.getSelection().toString().length > 0;
+  }
+}
