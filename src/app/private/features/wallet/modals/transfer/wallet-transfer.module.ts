794484a26a4e0c328f813357ebf77ea73b6f30b2
antoni-casas <toni.casas@wallapop.com>
2021-09-20 18:45:09 +0200

    [WPA-2269]: feat: Include directives

--- src/app/private/features/wallet/modals/transfer/wallet-transfer.module.ts
+++ src/app/private/features/wallet/modals/transfer/wallet-transfer.module.ts
@@ -1,13 +1,22 @@
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { ButtonModule } from '@shared/button/button.module';
+import { NumbersOnlyDirectiveModule } from '@shared/directives/numbers-only/numbers-only.directive.module';
 import { SvgIconModule } from '@shared/svg-icon/svg-icon.module';
 import { WalletTransferAmountComponent } from '@private/features/wallet/modals/transfer/components/amount/wallet-transfer-amount.component';
+import { WalletTransferJumpDirective } from '@private/features/wallet/modals/transfer/directives/jump/wallet-transfer-jump.directive';
+import { WalletTransferMaxLengthDirective } from '@private/features/wallet/modals/transfer/directives/max-length/wallet-transfer-max-length.directive';
 import { WalletTransferModalComponent } from '@private/features/wallet/modals/transfer/components/modal/wallet-transfer-modal.component';
 
 @NgModule({
-  declarations: [WalletTransferAmountComponent, WalletTransferModalComponent],
-  imports: [ButtonModule, CommonModule, SvgIconModule],
+  declarations: [
+    WalletTransferAmountComponent,
+    WalletTransferJumpDirective,
+    WalletTransferMaxLengthDirective,
+    WalletTransferModalComponent,
+  ],
+  imports: [ButtonModule, CommonModule, FormsModule, NumbersOnlyDirectiveModule, SvgIconModule],
 })
 export class WalletTransferModule {}
