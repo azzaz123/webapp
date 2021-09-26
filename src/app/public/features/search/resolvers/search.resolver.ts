9907eaa15adce4d5a9104d9faa33b4e0a475a1b9
Alex Masnou <alex.masnou@wallapop.com>
2021-09-26 22:30:30 +0200

    [WEB-565]: Move inject location logic to resolver

--- src/app/public/features/search/resolvers/search.resolver.ts
+++ src/app/public/features/search/resolvers/search.resolver.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, Params } from '@angular/router';
+import { QueryStringLocationService } from '@core/search/query-string-location.service';
 import { SearchNavigatorService } from '@core/search/search-navigator.service';
 import { SearchQueryStringService } from '@core/search/search-query-string.service';
 import { FILTER_QUERY_PARAM_KEY } from '@public/shared/components/filters/enums/filter-query-param-key.enum';
@@ -7,13 +8,23 @@ import { Observable, of } from 'rxjs';
 
 @Injectable()
 export class SearchResolver implements Resolve<boolean> {
-  constructor(private searchNavigatorService: SearchNavigatorService, private queryStringService: SearchQueryStringService) {}
+  constructor(
+    private searchNavigatorService: SearchNavigatorService,
+    private queryStringService: SearchQueryStringService,
+    private locationService: QueryStringLocationService
+  ) {}
 
   resolve(route: ActivatedRouteSnapshot): Observable<boolean> {
     const queryParams = route.queryParams;
 
     if (!this.routeContainLocationParams(queryParams)) {
       const filterParams = this.queryStringService.mapQueryToFilterParams(queryParams);
+      const locationParams = this.locationService.getLocationParameters();
+
+      filterParams.push(
+        { key: FILTER_QUERY_PARAM_KEY.latitude, value: locationParams.latitude },
+        { key: FILTER_QUERY_PARAM_KEY.longitude, value: locationParams.longitude }
+      );
 
       this.searchNavigatorService.navigate(filterParams, null, false, true);
     }
