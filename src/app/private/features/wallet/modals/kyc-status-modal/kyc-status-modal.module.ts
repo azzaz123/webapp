f9e10015c485821c2fceb5fea06b844b05942172
Laia Lopez <laialopezz99@gmail.com>
2021-09-29 13:33:00 +0200

    [WPA-4434]: [KYCAckModule] Wallet module corrected

--- src/app/private/features/wallet/modals/kyc-status-modal/kyc-status-modal.module.ts
+++ src/app/private/features/wallet/modals/kyc-status-modal/kyc-status-modal.module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { KYCAckModule } from '@api/delivery/kyc-ack/kyc-ack.module';
 import { SvgIconModule } from '@shared/svg-icon/svg-icon.module';
 import { KYCStatusModule } from '../kyc/components/kyc-status/kyc-status.module';
 import { KYCStatusModalComponent } from './kyc-status-modal.component';
@@ -9,6 +8,5 @@ import { KYCStatusModalComponent } from './kyc-status-modal.component';
   imports: [CommonModule, KYCStatusModule, SvgIconModule],
   declarations: [KYCStatusModalComponent],
   exports: [KYCStatusModalComponent],
-  providers: [KYCAckModule],
 })
 export class KYCStatusModalModule {}
