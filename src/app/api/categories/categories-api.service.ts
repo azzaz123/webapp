de4c601203a960980a68c68f69adfabc0502bf30
Irene Blanco <irene.blanco@wallapop.com>
2021-09-29 10:24:51 +0200

    [FIN-2076]: CategoriesApiService - searchCategories now stored in memory

--- src/app/api/categories/categories-api.service.ts
+++ src/app/api/categories/categories-api.service.ts
@@ -3,19 +3,26 @@ import { CategoriesHttpService } from './http/categories-http.service';
 import { mapCategoriesToSearchCategories, mapCategoriesToUploadCategories } from './mappers/category-mapper';
 import { CategoriesFilterOption } from '@public/shared/components/filters/components/categories-filter/interfaces/categories-filter-option.interface';
 import { CategoryResponse } from '@core/category/category-response.interface';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class CategoriesApiService {
+  private searchCategories: CategoriesFilterOption[];
+
   constructor(private categoriesHttpService: CategoriesHttpService) {}
 
   public getSearchCategories(): Observable<CategoriesFilterOption[]> {
-    return this.categoriesHttpService.getCategories('search').pipe(
-      map((categories) => {
-        return mapCategoriesToSearchCategories(categories);
-      })
-    );
+    if (this.searchCategories) {
+      return of(this.searchCategories);
+    } else {
+      return this.categoriesHttpService.getCategories('search').pipe(
+        map((categories) => {
+          return mapCategoriesToSearchCategories(categories);
+        }),
+        tap((categories) => (this.searchCategories = categories))
+      );
+    }
   }
 
   public getUploadCategories(): Observable<CategoryResponse[]> {
