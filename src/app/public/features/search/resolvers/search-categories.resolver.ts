343a7d5c1f68b1e1034e6d6b30649e8c384269fe
Irene Blanco <irene.blanco@wallapop.com>
2021-09-29 12:30:59 +0200

    [FIN-2076]: SearchCategoriesResolver - Modified config and removed unused imports

--- src/app/public/features/search/resolvers/search-categories.resolver.ts
+++ src/app/public/features/search/resolvers/search-categories.resolver.ts
@@ -1,12 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Resolve, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
+import { Resolve } from '@angular/router';
 import { CategoriesApiService } from '@api/categories/categories-api.service';
 import { CategoriesFilterOption } from '@public/shared/components/filters/components/categories-filter/interfaces/categories-filter-option.interface';
 import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable()
 export class SearchCategoriesResolver implements Resolve<CategoriesFilterOption[]> {
   constructor(private categoriesApiService: CategoriesApiService) {}
 
