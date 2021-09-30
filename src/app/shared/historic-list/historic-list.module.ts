a882c22a6fd3dd9ac9edee512aa8d00450791749
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-09-30 13:27:59 +0200

    [WPA-5299]: Define modules in HistoricList module

--- src/app/shared/historic-list/historic-list.module.ts
+++ src/app/shared/historic-list/historic-list.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InfiniteScrollModule } from '@shared/infinite-scroll/infinite-scroll.module';
+import { TabsBarModule } from '@shared/tabs-bar/tabs-bar.module';
+import { SvgIconModule } from '@shared/svg-icon/svg-icon.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [CommonModule, InfiniteScrollModule, TabsBarModule, SvgIconModule],
 })
 export class HistoricListModule {}
