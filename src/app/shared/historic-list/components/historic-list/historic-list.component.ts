fe0c2fbc3943d7699775eadb199e4bab2e161382
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-09-30 13:12:16 +0200

    [WPA-5299]: Create empty Historic List component

---
+++ src/app/shared/historic-list/components/historic-list/historic-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tsl-historic-list',
+  templateUrl: './historic-list.component.html',
+  styleUrls: ['./historic-list.component.scss'],
+})
+export class HistoricListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
