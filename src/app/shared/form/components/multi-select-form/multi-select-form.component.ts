d96538abd24d044a5bafa9aacc88786d1986cd9c
Irene Blanco <irene.blanco@wallapop.com>
2021-10-01 11:44:06 +0200

    [FIN-2072]: MultiSelectFormComponent - Now able to work with nested options

--- src/app/shared/form/components/multi-select-form/multi-select-form.component.ts
+++ src/app/shared/form/components/multi-select-form/multi-select-form.component.ts
@@ -2,7 +2,6 @@ import { ChangeDetectionStrategy, Component, forwardRef, Input } from '@angular/
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { AbstractFormComponent } from '@shared/form/abstract-form/abstract-form-component';
 import { Observable, BehaviorSubject } from 'rxjs';
-import { SelectFormOption } from '../select/interfaces/select-form-option.interface';
 import { MultiSelectFormOption, TemplateMultiSelectFormOption } from './interfaces/multi-select-form-option.interface';
 import { MultiSelectValue } from './interfaces/multi-select-value.type';
 @Component({
@@ -20,10 +19,7 @@ import { MultiSelectValue } from './interfaces/multi-select-value.type';
 })
 export class MultiSelectFormComponent extends AbstractFormComponent<MultiSelectValue> {
   @Input() set options(value: MultiSelectFormOption[]) {
-    this.extendedOptions = value.map((option) => {
-      return { ...option, checked: false };
-    });
-
+    this.extendedOptions = this.formatToExtendedOptions(value);
     this.mapCheckedValue();
     this.extendedOptionsSubject.next(this.extendedOptions);
   }
@@ -32,6 +28,7 @@ export class MultiSelectFormComponent extends AbstractFormComponent<MultiSelectV
   private extendedOptions: TemplateMultiSelectFormOption[] = [];
   private extendedOptionsSubject: BehaviorSubject<TemplateMultiSelectFormOption[]> = new BehaviorSubject([]);
   public extendedOptions$: Observable<TemplateMultiSelectFormOption[]> = this.extendedOptionsSubject.asObservable();
+  public shownChildrenOptionId: string;
 
   public writeValue(value: MultiSelectValue): void {
     this.value = value;
@@ -46,15 +43,68 @@ export class MultiSelectFormComponent extends AbstractFormComponent<MultiSelectV
       .map((option) => {
         return option.value;
       });
+
+    this.value = this.getValue(this.extendedOptions);
     this.onChange(this.value);
   }
 
   private mapCheckedValue(): void {
-    this.extendedOptions = this.extendedOptions.map((option: TemplateMultiSelectFormOption) => {
-      option.checked = this.value?.includes(option.value);
+    this.extendedOptions = this.mapCheckedOptions(this.extendedOptions);
+    this.extendedOptionsSubject.next(this.extendedOptions);
+  }
+
+  private mapCheckedOptions(options: TemplateMultiSelectFormOption[]): TemplateMultiSelectFormOption[] {
+    const mappedCheckedOptions: TemplateMultiSelectFormOption[] = options.map((option) => {
+      const isChecked = this.value?.includes(option.value);
+
+      if (option.children?.length) {
+        if (isChecked) {
+          option.checked = false;
+          option.children.map((childOption) => (childOption.checked = true));
+        } else {
+          option.children = this.mapCheckedOptions(option.children);
+        }
+      } else {
+        option.checked = isChecked;
+      }
+
       return { ...option };
     });
 
-    this.extendedOptionsSubject.next(this.extendedOptions);
+    return mappedCheckedOptions;
+  }
+
+  private formatToExtendedOptions(options: MultiSelectFormOption[]): TemplateMultiSelectFormOption[] {
+    const formattedExtendedOptions: TemplateMultiSelectFormOption[] = options.map((option) => {
+      const { label, sublabel, icon, value } = option;
+
+      if (option.children?.length) {
+        return { label, sublabel, icon, value, children: this.formatToExtendedOptions(option.children), checked: false };
+      } else {
+        return { label, sublabel, icon, value, checked: false };
+      }
+    });
+
+    return formattedExtendedOptions;
+  }
+
+  private getValue(options: TemplateMultiSelectFormOption[]): string[] {
+    let value = [];
+    options.forEach((option: TemplateMultiSelectFormOption) => {
+      if (option.checked) {
+        value.push(option.value);
+      } else {
+        if (option.children?.length) {
+          const checkedChildOptions = this.getValue(option.children);
+          if (checkedChildOptions.length === option.children.length) {
+            value = [...value, option.value];
+          } else {
+            value = [...value, ...this.getValue(option.children)];
+          }
+        }
+      }
+    });
+
+    return value;
   }
 }
