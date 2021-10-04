7824ec523a2fb52efa96f3981b2bcb2aaa44b3ce
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-10-04 11:17:52 +0200

    [WPA-5299]: Declare Historic components in module

--- src/app/shared/historic-list/historic-list.module.ts
+++ src/app/shared/historic-list/historic-list.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { InfiniteScrollModule } from '@shared/infinite-scroll/infinite-scroll.module';
 import { TabsBarModule } from '@shared/tabs-bar/tabs-bar.module';
 import { SvgIconModule } from '@shared/svg-icon/svg-icon.module';
+import { HistoricElementComponent } from './components/historic-element/historic-element.component';
+import { HistoricListComponent } from './components/historic-list/historic-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HistoricElementComponent, HistoricListComponent],
   imports: [CommonModule, InfiniteScrollModule, TabsBarModule, SvgIconModule],
 })
 export class HistoricListModule {}
