84c82ac4f90e20aec0fa45e6d7b2a52efb2c8f9c
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-10-04 11:38:12 +0200

    [WPA-5299]: Add Historic List interfaces

---
+++ src/app/shared/historic-list/interfaces/historic-list.interface.ts
@@ -0,0 +1,4 @@
+import { HistoricDateElement } from './historic-date-element.interface';
+import { HistoricListHeader } from './historic-list-header.interface';
+
+export interface HistoricList extends HistoricDateElement<HistoricListHeader> {}
