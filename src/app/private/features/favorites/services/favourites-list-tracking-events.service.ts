0a29e912cda6905f2866494e0478992cf496e712
Alejandro Delgado <alejandrodelgado@ip-192-168-1-63.eu-west-1.compute.internal>
2021-10-05 17:26:12 +0200

    [WPA-3775]: WPA-3775 => added new item event, trackUnfavouriteItemEvent

--- src/app/private/features/favorites/services/favourites-list-tracking-events.service.ts
+++ src/app/private/features/favorites/services/favourites-list-tracking-events.service.ts
@@ -5,6 +5,7 @@ import {
   ANALYTIC_EVENT_TYPES,
   ClickItemCard,
   SCREEN_IDS,
+  UnfavoriteItem,
 } from '@core/analytics/analytics-constants';
 import { AnalyticsService } from '@core/analytics/analytics.service';
 import { Item } from '@core/item/item';
@@ -23,6 +24,25 @@ export class FavouritesListTrackingEventsService {
         itemId: id,
         categoryId: categoryId,
         position: index + 1,
+        screenId: SCREEN_IDS.MyFavoriteItemsSection,
+        salePrice: salePrice,
+        title: title,
+        isBumped: flags?.bumped,
+      },
+    };
+
+    this.analyticsService.trackEvent(event);
+  }
+
+  public trackUnfavouriteItemEvent(item: Item): void {
+    const { id, categoryId, salePrice, title, flags } = item;
+
+    const event: AnalyticsEvent<UnfavoriteItem> = {
+      name: ANALYTICS_EVENT_NAMES.UnfavoriteItem,
+      eventType: ANALYTIC_EVENT_TYPES.Navigation,
+      attributes: {
+        itemId: id,
+        categoryId: categoryId,
         screenId: SCREEN_IDS.MyFavoriteItemsSection,
         salePrice: salePrice,
         title: title,
