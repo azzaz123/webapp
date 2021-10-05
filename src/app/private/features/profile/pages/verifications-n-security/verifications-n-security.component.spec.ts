61a8323c4d1d03ee64972ff2707c91599690cb15
Leonardo De Caria <leonardo.decaria@wallapop.com>
2021-10-05 15:22:31 +0200

    [WPA-4544]: feat: add verification email component

--- src/app/private/features/profile/pages/verifications-n-security/verifications-n-security.component.spec.ts
+++ src/app/private/features/profile/pages/verifications-n-security/verifications-n-security.component.spec.ts
@@ -1,24 +1,111 @@
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { UserVerifications } from '@api/core/model/verifications';
+import {
+  MOCK_USER_VERIFICATIONS_EMAIL_VERIFIED,
+  MOCK_USER_VERIFICATIONS_MAPPED,
+} from '@api/fixtures/user-verifications/user-verifications.fixtures.spec';
+import { UserVerificationsService } from '@api/user-verifications/user-verifications.service';
+import { UserService } from '@core/user/user.service';
+import { MockedUserService, MOCK_FULL_USER } from '@fixtures/user.fixtures.spec';
+import { Observable, of } from 'rxjs';
 
-import { VerificationsNSecurityComponent } from './verifications-n-security.component';
+import { VerificationsNSecurityComponent, VERIFICATIONS_N_SECURITY_TYPES } from './verifications-n-security.component';
+
+@Component({
+  selector: 'tsl-verification-card',
+  template: '',
+})
+class MockVerificationCardComponent {}
 
 describe('VerificationsNSecurityComponent', () => {
   let component: VerificationsNSecurityComponent;
   let fixture: ComponentFixture<VerificationsNSecurityComponent>;
+  let mockUserVerifications$: Observable<UserVerifications> = of(MOCK_USER_VERIFICATIONS_MAPPED);
+  let userService: UserService;
+  let userVerificationsService: UserVerificationsService;
+  let spyUserVerificationsService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [VerificationsNSecurityComponent],
+      declarations: [VerificationsNSecurityComponent, MockVerificationCardComponent],
+      providers: [
+        { provide: UserService, useClass: MockedUserService },
+        {
+          provide: UserVerificationsService,
+          useValue: {
+            get userVerifications$() {
+              return mockUserVerifications$;
+            },
+          },
+        },
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
+    userService = TestBed.inject(UserService);
+    userVerificationsService = TestBed.inject(UserVerificationsService);
   });
 
   beforeEach(() => {
+    jest.spyOn(userService, 'user', 'get').mockReturnValue(MOCK_FULL_USER);
+    spyUserVerificationsService = jest.spyOn(userVerificationsService, 'userVerifications$', 'get');
     fixture = TestBed.createComponent(VerificationsNSecurityComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+  describe('when the verifications view is loaded', () => {
+    beforeEach(() => {
+      fixture.detectChanges();
+    });
+    it('should be created', () => {
+      expect(component).toBeTruthy();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    it('should call to the user verifications service', () => {
+      expect(spyUserVerificationsService).toHaveBeenCalledTimes(1);
+    });
+
+    it('should retrieve the user verifications', () => {
+      component.userVerifications$.subscribe((result) => {
+        expect(result).toEqual(MOCK_USER_VERIFICATIONS_MAPPED);
+      });
+    });
+
+    it('should show the email card', () => {
+      const card = fixture.debugElement.queryAll(By.directive(MockVerificationCardComponent));
+      expect(card).toBeTruthy();
+    });
+  });
+
+  describe('verifications text', () => {
+    describe('when the verifications service is loaded', () => {
+      it('should show E-mail title', () => {
+        const title = component.getTitleVerification(VERIFICATIONS_N_SECURITY_TYPES.EMAIL);
+        expect(title).toBe($localize`:@@verification_and_security_all_users_verifications_email_label:E-mail`);
+      });
+      describe('and the email is verified', () => {
+        beforeEach(() => {
+          spyUserVerificationsService = jest
+            .spyOn(userVerificationsService, 'userVerifications$', 'get')
+            .mockReturnValue(of(MOCK_USER_VERIFICATIONS_EMAIL_VERIFIED));
+          fixture.detectChanges();
+        });
+        it('should show Change button text', () => {
+          component.userVerifications$.subscribe((userVerifications) => {
+            const text = component.getTextButton(userVerifications.email);
+            expect(text).toBe($localize`:@@verification_and_security_all_users_change_button:Change`);
+          });
+        });
+      });
+      describe('and the email is not verified', () => {
+        it('should show Verify button text', () => {
+          component.userVerifications$.subscribe((userVerifications) => {
+            const text = component.getTextButton(userVerifications.email);
+            expect(text).toBe($localize`:@@verification_and_security_all_users_verify_button:Verify`);
+          });
+        });
+      });
+    });
   });
 });
--- src/app/private/features/profile/pages/verifications-n-security/verifications-n-security.component.ts
+++ src/app/private/features/profile/pages/verifications-n-security/verifications-n-security.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { UserVerifications } from '@api/core/model/verifications';
+import { UserVerificationsService } from '@api/user-verifications/user-verifications.service';
+import { User } from '@core/user/user';
+import { UserService } from '@core/user/user.service';
+import { Observable } from 'rxjs';
+
+export enum VERIFICATIONS_N_SECURITY_TYPES {
+  EMAIL = 'e-mail',
+}
 
 @Component({
   selector: 'tsl-verifications-n-security',
@@ -6,7 +15,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./verifications-n-security.component.scss'],
 })
 export class VerificationsNSecurityComponent {
-  constructor() {}
+  public userVerifications$: Observable<UserVerifications>;
+  public user: User;
+  public readonly VERIFICATIONS_N_SECURITY_TYPES = VERIFICATIONS_N_SECURITY_TYPES;
+  public readonly titleVerifications = {
+    [VERIFICATIONS_N_SECURITY_TYPES.EMAIL]: $localize`:@@verification_and_security_all_users_verifications_email_label:E-mail`,
+  };
 
-  ngOnInit(): void {}
+  constructor(private userService: UserService, private userVerificationsService: UserVerificationsService) {
+    this.userVerifications$ = this.userVerificationsService.userVerifications$;
+  }
+
+  ngOnInit(): void {
+    this.user = this.userService.user;
+  }
+
+  onClickButton(): void {}
+
+  public getTitleVerification(verificationsType: VERIFICATIONS_N_SECURITY_TYPES): string {
+    return this.titleVerifications[verificationsType];
+  }
+
+  public getTextButton(isVerifiedAttribute: boolean): string {
+    if (isVerifiedAttribute) {
+      return $localize`:@@verification_and_security_all_users_change_button:Change`;
+    }
+    return $localize`:@@verification_and_security_all_users_verify_button:Verify`;
+  }
 }
