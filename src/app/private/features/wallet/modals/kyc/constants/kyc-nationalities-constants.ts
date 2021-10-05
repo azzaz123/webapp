7ec77ca49bd59ead049c6a3072a5e1dcd55eb0e2
Laia Lopez <laialopezz99@gmail.com>
2021-10-05 16:14:37 +0200

    [WPA-3947]: [Nationalitys + Status + Image Options] KYC Translations Added

--- src/app/private/features/wallet/modals/kyc/constants/kyc-nationalities-constants.ts
+++ src/app/private/features/wallet/modals/kyc/constants/kyc-nationalities-constants.ts
@@ -5,7 +5,7 @@ import { getKYCAvailableDocuments } from '../mappers/kyc-documents/kyc-documents
 export const KYC_NATIONALITIES: KYCNationality[] = [
   {
     label: $localize`:@@kyc_select_nationality_view_list_eu_placeholder:European Union`,
-    headerText: $localize`:@@kyc_select_document_view_if_eu_top bar_title:European Union`,
+    headerText: $localize`:@@kyc_select_document_view_if_eu_top_bar_title:European Union`,
     value: KYC_NATIONALITY_TYPE.EUROPEAN_UNION,
     svgPath: '/assets/icons/wallet/kyc/stepper/european_nationality.svg',
     availableDocuments: getKYCAvailableDocuments(KYC_NATIONALITY_TYPE.EUROPEAN_UNION),
@@ -13,7 +13,7 @@ export const KYC_NATIONALITIES: KYCNationality[] = [
   },
   {
     label: $localize`:@@kyc_select_nationality_view_list_uk_usa_can_placeholder:UK, USA or Canada`,
-    headerText: $localize`:@@kyc_select_document_view_if_usa_uk_can_top bar_title:UK, USA or Canada`,
+    headerText: $localize`:@@kyc_select_document_view_if_usa_uk_can_top_bar_title:UK, USA or Canada`,
     value: KYC_NATIONALITY_TYPE.UK_USA_CANADA,
     svgPath: '/assets/icons/wallet/kyc/stepper/ukusaca_nationality.svg',
     availableDocuments: getKYCAvailableDocuments(KYC_NATIONALITY_TYPE.UK_USA_CANADA),
@@ -21,7 +21,7 @@ export const KYC_NATIONALITIES: KYCNationality[] = [
   },
   {
     label: $localize`:@@kyc_select_nationality_view_list_other_place_placeholder:Rest of the world`,
-    headerText: $localize`:@@kyc_select_document_view_if_rest_of_the_world_top bar_title:Rest of the world`,
+    headerText: $localize`:@@kyc_select_document_view_if_rest_of_the_world_top_bar_title:Rest of the world`,
     value: KYC_NATIONALITY_TYPE.OTHER,
     svgPath: '/assets/icons/wallet/kyc/stepper/world_nationality.svg',
     availableDocuments: getKYCAvailableDocuments(KYC_NATIONALITY_TYPE.OTHER),
