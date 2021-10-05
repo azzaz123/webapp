ed3a162555cc1eec3edc94c4a3b79f5ed7dc768c
Laia Lopez <laialopezz99@gmail.com>
2021-10-05 15:20:53 +0200

    [WPA-3107]: [KYCInfoModalComponent] zendesk link

--- src/app/api/payments/kyc/mappers/responses/kyc-properties.mapper.ts
+++ src/app/api/payments/kyc/mappers/responses/kyc-properties.mapper.ts
@@ -7,7 +7,7 @@ import { KYCDocumentStatusApi, KYCFlowStatusApi, KYCPropertiesApi, KYCRefusedRea
 
 export function mapKYCPropertiesApiToKYCProperties(KYCPropertiesApi: KYCPropertiesApi): KYCProperties {
   return {
-    status: getDocumentStatus(KYCPropertiesApi.user_kyc_status),
+    status: KYC_STATUS.PENDING,
     refusedReason: getRefusedReason(KYCPropertiesApi.document_refused_reason_type),
     inflowStatus: getFlowStatus(KYCPropertiesApi.inflow_status),
     outflowStatus: getFlowStatus(KYCPropertiesApi.outflow_status),
