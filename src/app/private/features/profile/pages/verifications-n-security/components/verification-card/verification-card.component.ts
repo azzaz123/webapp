a8b26357bbfb1ac40e99f045f49192b0f9e7bb25
Leonardo De Caria <leonardo.decaria@wallapop.com>
2021-10-08 11:20:23 +0200

    [WPA-4544]: fix: remove ngOnInit

--- src/app/private/features/profile/pages/verifications-n-security/components/verification-card/verification-card.component.ts
+++ src/app/private/features/profile/pages/verifications-n-security/components/verification-card/verification-card.component.ts
@@ -1,19 +1,17 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'tsl-verification-card',
   templateUrl: './verification-card.component.html',
   styleUrls: ['./verification-card.component.scss'],
 })
-export class VerificationCardComponent implements OnInit {
+export class VerificationCardComponent {
   @Input() isVerified: boolean;
   @Input() title: string;
   @Input() footerLegend: string;
   @Input() textButton: string;
   @Output() buttonClick: EventEmitter<void> = new EventEmitter();
   constructor() {}
-
-  ngOnInit(): void {}
 
   public onButtonClick(): void {
     this.buttonClick.emit();
