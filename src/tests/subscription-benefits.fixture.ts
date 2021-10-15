7dd9bf7dadc6d9f7b6bacbb98af58de6c2c66d81
Gonzalo Morales <gonzalomorales93@gmail.com>
2021-10-15 14:46:05 +0200

    [PQP-4376]: fix tests

--- src/tests/subscription-benefits.fixture.ts
+++ src/tests/subscription-benefits.fixture.ts
@@ -1,5 +1,30 @@
+import { SubscriptionBenefit } from '@core/subscriptions/subscription-benefits/interfaces/subscription-benefit.interface';
+
 export class MockSubscriptionBenefitsService {
   public getBenefitsByCategory(id: number): string[] {
     return [];
   }
 }
+
+export const subscriptionBenefitsMapped: SubscriptionBenefit[] = [
+  {
+    iconPath: 'assets/images/subscriptions/benefits/en/img-1.png',
+    title: $localize`:@@web_subscription_benefit_title_your_brand:Empower your brand`,
+    description: $localize`:@@web_subscription_benefit_description_your_brand:Complete your profile with a cover photo and a description.`,
+  },
+  {
+    iconPath: 'assets/images/subscriptions/benefits/en/img-2.png',
+    title: $localize`:@@web_subscription_benefit_title_connect:Connect with more customers`,
+    description: $localize`:@@web_subscription_benefit_description_connect:Share your phone number and website with millions of people.`,
+  },
+  {
+    iconPath: 'assets/images/subscriptions/benefits/en/img-3.png',
+    title: $localize`:@@web_subscription_benefit_title_visibility:Gain more visibility`,
+    description: $localize`:@@web_subscription_benefit_description_visibility:Appear at the top of search results and attract more customers.`,
+  },
+  {
+    iconPath: 'assets/images/subscriptions/benefits/en/img-4.png',
+    title: $localize`:@@web_subscription_benefit_title_time:Save management time`,
+    description: $localize`:@@web_subscription_benefit_description_time:Your products will always be active and ready for sale.`,
+  },
+];
