17ef9786f6044ea0dec8fa4c690c37ed998c1010
Leonardo De Caria <leonardo.decaria@wallapop.com>
2021-10-20 10:58:58 +0200

    [WPA-4318]: feat: change mobile attr to phone

--- src/app/api/core/model/verifications/user-verifications.interface.ts
+++ src/app/api/core/model/verifications/user-verifications.interface.ts
@@ -1,6 +1,11 @@
+export enum VERIFICATION_METHOD {
+  PHONE = 'phone',
+  EMAIL = 'email',
+  FACEBOOK = 'facebook',
+}
 export interface UserVerifications {
   email: boolean;
-  mobile: boolean;
+  phone: boolean;
   facebook: boolean;
   google_plus: boolean;
 }
