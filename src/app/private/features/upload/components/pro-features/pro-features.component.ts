aecb836a760aa6d98e62d2ae736d47f8c1829396
Gonzalo Morales <gonzalomorales93@gmail.com>
2021-10-21 17:45:46 +0200

    [PQP-4013]: add features inside all forms

--- src/app/private/features/upload/components/pro-features/pro-features.component.ts
+++ src/app/private/features/upload/components/pro-features/pro-features.component.ts
@@ -1,8 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { AnalyticsEvent, ANALYTICS_EVENT_NAMES, ANALYTIC_EVENT_TYPES, SCREEN_IDS } from '@core/analytics/analytics-constants';
+import { Component, Input } from '@angular/core';
+import {
+  AnalyticsEvent,
+  ANALYTICS_EVENT_NAMES,
+  ANALYTIC_EVENT_TYPES,
+  ClickConfigurationAdditionalServicesUpload,
+  ClickWarrantyAdditionalServicesUpload,
+  SCREEN_IDS,
+} from '@core/analytics/analytics-constants';
 import { AnalyticsService } from '@core/analytics/analytics.service';
 import { ClickInstallationAdditionalServicesUpload } from '@core/analytics/resources/events-interfaces/click-installation-additional-services-upload.interface';
-import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ProModalComponent } from '@shared/modals/pro-modal/pro-modal.component';
 import { modalConfig, PRO_MODAL_TYPE } from '@shared/modals/pro-modal/pro-modal.constants';
 
@@ -12,12 +19,27 @@ import { modalConfig, PRO_MODAL_TYPE } from '@shared/modals/pro-modal/pro-modal.
   styleUrls: ['./pro-features.component.scss'],
 })
 export class ProFeaturesComponent {
-  @Input() categoryId;
+  @Input() categoryId: number;
+
+  public readonly fieldsConfig: { text: string; eventName: ANALYTICS_EVENT_NAMES }[] = [
+    {
+      text: $localize`:@@additional_services_selector_pro_user_install_service_info_title:Installation service`,
+      eventName: ANALYTICS_EVENT_NAMES.ClickInstallationAdditionalServicesUpload,
+    },
+    {
+      text: $localize`:@@additional_services_selector_pro_user_config_service_info_title:Configuration service`,
+      eventName: ANALYTICS_EVENT_NAMES.ClickConfigurationAdditionalServicesUpload,
+    },
+    {
+      text: $localize`:@@additional_services_selector_pro_user_guarantee_service_info_title:Guarantee`,
+      eventName: ANALYTICS_EVENT_NAMES.ClickWarrantyAdditionalServicesUpload,
+    },
+  ];
   constructor(private analyticsService: AnalyticsService, private modalService: NgbModal) {}
 
-  public onClick(isActive: boolean): void {
+  public onClick(isActive: boolean, eventName: ANALYTICS_EVENT_NAMES): void {
     this.openModal();
-    this.trackEvent(isActive);
+    this.trackEvent(isActive, eventName);
   }
 
   private openModal(): void {
@@ -27,14 +49,16 @@ export class ProFeaturesComponent {
     modal.componentInstance.modalConfig = modalConfig[PRO_MODAL_TYPE.simulation];
   }
 
-  private trackEvent(isActive: boolean): void {
-    const event: AnalyticsEvent<ClickInstallationAdditionalServicesUpload> = {
-      name: ANALYTICS_EVENT_NAMES.ClickInstallationAdditionalServicesUpload,
+  private trackEvent(isActive: boolean, eventName: ANALYTICS_EVENT_NAMES): void {
+    const event: AnalyticsEvent<
+      ClickInstallationAdditionalServicesUpload | ClickConfigurationAdditionalServicesUpload | ClickWarrantyAdditionalServicesUpload
+    > = {
+      name: eventName,
       eventType: ANALYTIC_EVENT_TYPES.Navigation,
       attributes: {
         screenId: SCREEN_IDS.Upload,
         switchOn: isActive,
-        categoryId: this.categoryId,
+        categoryId: +this.categoryId,
       },
     };
 
