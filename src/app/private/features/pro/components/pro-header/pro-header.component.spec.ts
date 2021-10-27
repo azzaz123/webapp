b8cfcb469ec56f57c263acab0c4018581a3c3651
Gonzalo Morales <gonzalomorales93@gmail.com>
2021-10-27 17:08:24 +0200

    [PQP-4434]: create component

---
+++ src/app/private/features/pro/components/pro-header/pro-header.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProHeaderComponent } from './pro-header.component';
+
+describe('ProHeaderComponent', () => {
+  let component: ProHeaderComponent;
+  let fixture: ComponentFixture<ProHeaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ProHeaderComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProHeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/private/features/pro/components/pro-header/pro-header.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tsl-pro-header',
+  templateUrl: './pro-header.component.html',
+  styleUrls: ['./pro-header.component.scss'],
+})
+export class ProHeaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
