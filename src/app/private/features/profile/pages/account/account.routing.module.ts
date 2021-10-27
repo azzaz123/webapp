d357dd5197e6873c5d74a3094f5541053f242671
Leonardo De Caria <leonardo.decaria@wallapop.com>
2021-10-27 15:59:42 +0200

    [WPA-5704]: feat: create account module

---
+++ src/app/private/features/profile/pages/account/account.routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { Route, RouterModule } from '@angular/router';
+import { ExitConfirmGuard } from '@core/guards/exit-confirm.guard';
+import { PRIVATE_PATHS } from '@private/private-routing-constants';
+import { PROFILE_PATHS } from '../../profile-routing-constants';
+import { VERIFICATIONS_N_SECURITY_PATHS } from '../verifications-n-security/verifications-n-security.routing.constants';
+import { AccountComponent } from './account.component';
+
+const routes: Route[] = [
+  {
+    path: '',
+    component: AccountComponent,
+    canDeactivate: [ExitConfirmGuard],
+    data: {
+      isMyZone: true,
+      isProfile: true,
+    },
+    children: [
+      {
+        path: VERIFICATIONS_N_SECURITY_PATHS.VERIFY_EMAIL,
+        redirectTo: `/${PRIVATE_PATHS.PROFILE}/${PROFILE_PATHS.VERIFICATIONS}/${VERIFICATIONS_N_SECURITY_PATHS.VERIFY_EMAIL}`,
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AccountRoutingModule {}
+
+export const AccountRoutedComponents = [AccountComponent];
