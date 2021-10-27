a91f57ee6750fdb64ff2ad8fb09214f681132470
Leonardo De Caria <leonardo.decaria@wallapop.com>
2021-10-27 16:05:46 +0200

    [WPA-5704]: feat: verify Email deep Link

--- src/app/private/features/profile/pages/verifications-n-security/verifications-n-security.routing.module.ts
+++ src/app/private/features/profile/pages/verifications-n-security/verifications-n-security.routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
+import { DevelopmentGuard } from '@core/user/development.guard';
 import { VerificationsNSecurityComponent } from './verifications-n-security.component';
+import { VERIFICATIONS_N_SECURITY_PATHS } from './verifications-n-security.routing.constants';
 
 const routes: Route[] = [
   {
     path: '',
     component: VerificationsNSecurityComponent,
+    children: [
+      {
+        path: VERIFICATIONS_N_SECURITY_PATHS.VERIFY_EMAIL,
+        loadChildren: () =>
+          import('@private/features/profile/modal/email-verification/email-verification.module').then((m) => m.EmailVerificationModule),
+      },
+    ],
   },
 ];
 
