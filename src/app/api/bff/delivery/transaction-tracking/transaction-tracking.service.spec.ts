a6468059ada021b4fdf3c687cf3c3e2b7a3f3c19
antoni-casas <toni.casas@wallapop.com>
2021-10-28 11:16:01 +0200

    [WPA-4949]: [TransactionTrackingService] Get + Testing

--- src/app/api/bff/delivery/transaction-tracking/transaction-tracking.service.spec.ts
+++ src/app/api/bff/delivery/transaction-tracking/transaction-tracking.service.spec.ts
@@ -1,16 +1,42 @@
+import { HttpClientTestingModule, HttpTestingController, TestRequest } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { TRANSACTION_TRACKING_ENDPOINT } from './endpoints';
 
 import { TransactionTrackingService } from './transaction-tracking.service';
 
 describe('TransactionTrackingService', () => {
   let service: TransactionTrackingService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [TransactionTrackingService],
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(TransactionTrackingService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('when asking transaction tracking info to server', () => {
+    it('should get transaction tracking info', () => {
+      const MOCK_REQUEST_ID = '123';
+      const EXPECTED_ENDPOINT = `${TRANSACTION_TRACKING_ENDPOINT}?requestId=${MOCK_REQUEST_ID}`;
+      let response: any;
+
+      service.get(MOCK_REQUEST_ID).subscribe((properties: any) => {
+        response = properties;
+      });
+
+      const transactionTrackingInfoRequest: TestRequest = httpMock.expectOne(EXPECTED_ENDPOINT);
+      transactionTrackingInfoRequest.flush({});
+
+      expect(response).toEqual({});
+      expect(transactionTrackingInfoRequest.request.url).toEqual(TRANSACTION_TRACKING_ENDPOINT);
+      expect(transactionTrackingInfoRequest.request.method).toBe('GET');
+    });
   });
 });
--- src/app/api/bff/delivery/transaction-tracking/transaction-tracking.service.ts
+++ src/app/api/bff/delivery/transaction-tracking/transaction-tracking.service.ts
@@ -1,6 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TRANSACTION_TRACKING_ENDPOINT } from './endpoints';
 
 @Injectable()
 export class TransactionTrackingService {
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
+
+  public get(requestId: string): Observable<any> {
+    return this.httpClient.get(TRANSACTION_TRACKING_ENDPOINT, { params: { requestId } });
+  }
 }
