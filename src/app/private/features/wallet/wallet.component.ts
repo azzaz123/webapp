4e37d2e71620ab2eeddc88e30079faeb8a24d10b
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-10-29 12:22:09 +0200

    [WPA-6103]: Use different Customer Service URL on Italian site for Wallet Help

--- src/app/private/features/wallet/wallet.component.ts
+++ src/app/private/features/wallet/wallet.component.ts
@@ -1,7 +1,11 @@
-import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, LOCALE_ID, OnDestroy, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 
-import { CUSTOMER_HELP_PAGE } from '@core/external-links/customer-help/customer-help-constants';
+import {
+  CUSTOMER_HELP_BASE,
+  CUSTOMER_HELP_PAGE,
+  ITALIAN_CUSTOMER_HELP_PAGE,
+} from '@core/external-links/customer-help/customer-help-constants';
 import { CustomerHelpService } from '@core/external-links/customer-help/customer-help.service';
 import { KYCBannerSpecifications } from '@api/core/model/kyc-properties/interfaces/kyc-banner-specifications.interface';
 import { KYCProperties } from '@api/core/model/kyc-properties/interfaces/kyc-properties.interface';
@@ -12,6 +16,7 @@ import { WALLET_PATHS } from './wallet.routing.constants';
 import { WalletTrackingEventService } from '@private/features/wallet/services/tracking-event/wallet-tracking-event.service';
 
 import { Observable, Subscription } from 'rxjs';
+import { APP_LOCALE } from '@configs/subdomains.config';
 
 const bankDetailsId: string = `/${PRIVATE_PATHS.WALLET}/${WALLET_PATHS.BANK_DETAILS}`;
 
@@ -38,10 +43,11 @@ export class WalletComponent implements OnInit, OnDestroy {
       display: $localize`:@@wallet_view_balance_history_title_web_specific:Movements`,
     },
   ];
-  public ZENDESK_WALLET_HELP_URL: string = this.customerHelpService.getPageUrl(CUSTOMER_HELP_PAGE.WALLET_HELP);
+  public ZENDESK_WALLET_HELP_URL: string;
   private subscriptions: Subscription = new Subscription();
 
   constructor(
+    @Inject(LOCALE_ID) private locale: APP_LOCALE,
     private router: Router,
     private customerHelpService: CustomerHelpService,
     private kycPropertiesService: KYCPropertiesService,
@@ -57,6 +63,7 @@ export class WalletComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.KYCProperties$ = this.kycPropertiesService.KYCProperties$;
+    this.ZENDESK_WALLET_HELP_URL = this.getHelpURL();
   }
 
   ngOnDestroy() {
@@ -88,5 +95,12 @@ export class WalletComponent implements OnInit, OnDestroy {
       return;
     }
     this.walletTrackingEventService.trackClickBankDetails();
+  }
+
+  private getHelpURL(): string {
+    const isItalianSite = this.locale === 'it';
+    return isItalianSite
+      ? this.customerHelpService.getPageUrl(ITALIAN_CUSTOMER_HELP_PAGE.WALLET_HELP, CUSTOMER_HELP_BASE.ITALIAN_SITE)
+      : this.customerHelpService.getPageUrl(CUSTOMER_HELP_PAGE.WALLET_HELP);
   }
 }
