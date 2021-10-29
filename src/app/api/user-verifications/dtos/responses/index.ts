0b24771fb6963c8bdbc207d980eacbfab7c977c4
Leonardo De Caria <leonardo.decaria@wallapop.com>
2021-10-29 11:42:07 +0200

    [WPA-5782]: feat: add phone verification endpoint

--- src/app/api/user-verifications/dtos/responses/index.ts
+++ src/app/api/user-verifications/dtos/responses/index.ts
@@ -1,2 +1,4 @@
+export * from './verification-status-api.enum';
 export * from './user-verifications-api.interface';
 export * from './email-verification-api.interface';
+export * from './phone-verification-api.interface';
