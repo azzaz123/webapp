8717a74fc33cef5badcc64a219479684add0b85e
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-10-29 11:23:01 +0200

    [WPA-6103]: Allow Italian customer pages as parameter to getCustomerHelpUrl

--- src/app/core/external-links/customer-help/customer-help.service.ts
+++ src/app/core/external-links/customer-help/customer-help.service.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable, LOCALE_ID } from '@angular/core';
 import { APP_LOCALE } from '@configs/subdomains.config';
-import { CUSTOMER_HELP_BASE, CUSTOMER_HELP_PAGE, CUSTOMER_TICKET_FORM } from './customer-help-constants';
+import { CUSTOMER_HELP_BASE, CUSTOMER_HELP_PAGE, CUSTOMER_TICKET_FORM, ITALIAN_CUSTOMER_HELP_PAGE } from './customer-help-constants';
 import { getCustomerHelpUrl } from './get-customer-help-url';
 import { getTicketFormUrl } from './get-ticket-form-url';
 
@@ -10,7 +10,10 @@ import { getTicketFormUrl } from './get-ticket-form-url';
 export class CustomerHelpService {
   constructor(@Inject(LOCALE_ID) private locale: APP_LOCALE) {}
 
-  public getPageUrl(articleId: CUSTOMER_HELP_PAGE, baseUrl: CUSTOMER_HELP_BASE = CUSTOMER_HELP_BASE.DEFAULT): string {
+  public getPageUrl(
+    articleId: CUSTOMER_HELP_PAGE | ITALIAN_CUSTOMER_HELP_PAGE,
+    baseUrl: CUSTOMER_HELP_BASE = CUSTOMER_HELP_BASE.DEFAULT
+  ): string {
     return getCustomerHelpUrl(articleId, this.locale, baseUrl);
   }
 
--- src/app/core/external-links/customer-help/get-customer-help-url.ts
+++ src/app/core/external-links/customer-help/get-customer-help-url.ts
@@ -1,6 +1,10 @@
 import { APP_LOCALE } from '@configs/subdomains.config';
-import { CUSTOMER_HELP_PAGE, HELP_LOCALE, CUSTOMER_HELP_BASE } from './customer-help-constants';
+import { CUSTOMER_HELP_PAGE, HELP_LOCALE, CUSTOMER_HELP_BASE, ITALIAN_CUSTOMER_HELP_PAGE } from './customer-help-constants';
 
-export function getCustomerHelpUrl(pageId: CUSTOMER_HELP_PAGE, locale: APP_LOCALE, baseUrl = CUSTOMER_HELP_BASE.DEFAULT): string {
+export function getCustomerHelpUrl(
+  pageId: CUSTOMER_HELP_PAGE | ITALIAN_CUSTOMER_HELP_PAGE,
+  locale: APP_LOCALE,
+  baseUrl = CUSTOMER_HELP_BASE.DEFAULT
+): string {
   return `${baseUrl}${HELP_LOCALE[locale]}/articles/${pageId}`;
 }
