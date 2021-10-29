02f3d3b74b7bae5714d0500e47520778076e90bc
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-10-29 12:31:29 +0200

    [WPA-6103]: Use local mock instead of analytics one

--- src/app/core/external-links/customer-help/customer-help.service.spec.ts
+++ src/app/core/external-links/customer-help/customer-help.service.spec.ts
@@ -1,6 +1,5 @@
 import { LOCALE_ID } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { APP_LOCALE_MOCK } from '@fixtures/analytics.fixtures.spec';
 import { CUSTOMER_HELP_BASE, CUSTOMER_HELP_PAGE, CUSTOMER_TICKET_FORM } from './customer-help-constants';
 import { CustomerHelpService } from './customer-help.service';
 import * as GetCustomerHelpUrl from './get-customer-help-url';
@@ -10,6 +9,7 @@ describe('CustomerHelpService', () => {
   let customerHelpService: CustomerHelpService;
   const pageId = CUSTOMER_HELP_PAGE.SHIPPING_SELL_WITH_SHIPPING;
   const formId = CUSTOMER_TICKET_FORM.KYC;
+  const MOCK_LOCALE_ID = 'es';
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -17,7 +17,7 @@ describe('CustomerHelpService', () => {
         CustomerHelpService,
         {
           provide: LOCALE_ID,
-          useValue: APP_LOCALE_MOCK,
+          useValue: MOCK_LOCALE_ID,
         },
       ],
     });
@@ -31,7 +31,7 @@ describe('CustomerHelpService', () => {
 
       customerHelpService.getPageUrl(pageId);
 
-      expect(GetCustomerHelpUrl.getCustomerHelpUrl).toHaveBeenCalledWith(pageId, APP_LOCALE_MOCK, CUSTOMER_HELP_BASE.DEFAULT);
+      expect(GetCustomerHelpUrl.getCustomerHelpUrl).toHaveBeenCalledWith(pageId, MOCK_LOCALE_ID, CUSTOMER_HELP_BASE.DEFAULT);
     });
   });
 
@@ -41,7 +41,7 @@ describe('CustomerHelpService', () => {
 
       customerHelpService.getFormPageUrl(formId);
 
-      expect(GetTicketFormUrl.getTicketFormUrl).toHaveBeenCalledWith(formId, APP_LOCALE_MOCK, CUSTOMER_HELP_BASE.DEFAULT);
+      expect(GetTicketFormUrl.getTicketFormUrl).toHaveBeenCalledWith(formId, MOCK_LOCALE_ID, CUSTOMER_HELP_BASE.DEFAULT);
     });
   });
 });
