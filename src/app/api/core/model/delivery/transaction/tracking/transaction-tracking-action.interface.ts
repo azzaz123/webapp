6763d3f4302c00faed550798a8ad3da869df0bf4
antoni-casas <toni.casas@wallapop.com>
2021-11-02 15:25:15 +0100

    [WPA-4949]: feat: Add transaction-tracking-action interface

--- src/app/api/core/model/delivery/transaction/tracking/transaction-tracking-action.interface.ts
+++ src/app/api/core/model/delivery/transaction/tracking/transaction-tracking-action.interface.ts
@@ -1,1 +1,19 @@
-export interface TransactionTrackingAction {}
+import { TransactionTrackingTopAction } from './header/transaction-tracking-top-action.interface';
+
+export interface TransactionTrackingAction {
+  analytics: unknown;
+  payload: TransactionTrackingActionPayload;
+  type: TransactionTrackingActionType;
+}
+export type TransactionTrackingActionType = 'deeplink' | 'dialog' | 'user_action' | 'carrier_tracking_webview';
+export interface TransactionTrackingActionPayload {
+  description: string;
+  linkUrl: string;
+  negative: TransactionTrackingActionPayloadConfirmation;
+  positive: TransactionTrackingActionPayloadConfirmation;
+  title: string;
+}
+export interface TransactionTrackingActionPayloadConfirmation {
+  action?: TransactionTrackingAction;
+  title: string;
+}
