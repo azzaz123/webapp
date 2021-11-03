6f78cbdfed9ce3b33c6f5964321cbd4cb9cab8d4
Gonzalo Morales <gonzalomorales93@gmail.com>
2021-11-03 17:08:18 +0100

    [PQP-4489]: add translations

--- src/app/core/subscriptions/listing-limit/listing-limit.service.ts
+++ src/app/core/subscriptions/listing-limit/listing-limit.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
 import { AnalyticsPageView, ANALYTICS_EVENT_NAMES, SCREEN_IDS, ViewProSubscriptionPopup } from '@core/analytics/analytics-constants';
 import { AnalyticsService } from '@core/analytics/analytics.service';
+import { CATEGORY_IDS } from '@core/category/category-ids';
 import { ItemService } from '@core/item/item.service';
-import { SUBSCRIPTION_CATEGORIES } from '@core/subscriptions/subscriptions.interface';
+import { SUBSCRIPTION_CATEGORIES, SUBSCRIPTION_CATEGORY_TYPES } from '@core/subscriptions/subscriptions.interface';
 import { SubscriptionsService, SUBSCRIPTION_TYPES } from '@core/subscriptions/subscriptions.service';
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
-import {
-  CATEGORIES_WITH_HIGHEST_LIMIT_ACTIVE,
-  TooManyItemsModalComponent,
-} from '@shared/catalog/modals/too-many-items-modal/too-many-items-modal.component';
+import { TooManyItemsModalComponent } from '@shared/catalog/modals/too-many-items-modal/too-many-items-modal.component';
+
+export const CATEGORIES_WITH_HIGHEST_LIMIT_ACTIVE = [
+  CATEGORY_SUBSCRIPTIONS_IDS.REAL_ESTATE,
+  CATEGORY_SUBSCRIPTIONS_IDS.CONSUMER_GOODS,
+  CATEGORY_SUBSCRIPTIONS_IDS.CAR,
+];
+
 import { ProModalComponent } from '@shared/modals/pro-modal/pro-modal.component';
 import { modalConfig, PRO_MODAL_TYPE } from '@shared/modals/pro-modal/pro-modal.constants';
 import { forkJoin, Observable } from 'rxjs';
 import { map, take } from 'rxjs/operators';
+import { CATEGORY_SUBSCRIPTIONS_IDS } from '../category-subscription-ids';
 @Injectable()
 export class ListingLimitService {
   constructor(
@@ -37,7 +43,31 @@ export class ListingLimitService {
         const isFreeTrial = this.subscriptionsService.hasFreeTrialByCategoryId(subscriptions, item.categoryId);
         const tierDicounted = this.subscriptionsService.tierDiscountByCategoryId(subscriptions, item.categoryId);
 
-        modal.componentInstance.modalConfig = modalConfig[PRO_MODAL_TYPE.simulation];
+        if (type === SUBSCRIPTION_TYPES.carDealer) {
+          modal.componentInstance.modalConfig = modalConfig[PRO_MODAL_TYPE.listing_limit_cars_highest_limit];
+        }
+
+        if (isHighestLimit) {
+          if (categorySubscription.type === SUBSCRIPTION_CATEGORY_TYPES.CARS) {
+            modal.componentInstance.modalConfig = modalConfig[PRO_MODAL_TYPE.listing_limit_cars_highest_limit];
+          }
+          if (categorySubscription.type === SUBSCRIPTION_CATEGORY_TYPES.REAL_ESTATE) {
+            modal.componentInstance.modalConfig = modalConfig[PRO_MODAL_TYPE.listing_limit_real_estate_highest_limit];
+          }
+          if (categorySubscription.type === SUBSCRIPTION_CATEGORY_TYPES.CONSUMER_GOODS) {
+            modal.componentInstance.modalConfig = modalConfig[PRO_MODAL_TYPE.listing_limit_real_estate_highest_limit];
+          }
+        }
+
+        if (isFreeTrial) {
+          modal.componentInstance.modalConfig = modalConfig[PRO_MODAL_TYPE.listing_limit_trial];
+        }
+
+        if (tierDicounted) {
+          modal.componentInstance.modalConfig = modalConfig[PRO_MODAL_TYPE.listing_limit_discount];
+        }
+
+        modal.componentInstance.modalConfig = modalConfig[PRO_MODAL_TYPE.listing_limit_no_trial_no_discount];
 
         this.trackPageView(categorySubscription, type, isFreeTrial, tierDicounted);
       });
--- src/app/shared/modals/pro-modal/pro-modal.constants.ts
+++ src/app/shared/modals/pro-modal/pro-modal.constants.ts
@@ -5,6 +5,10 @@ export enum PRO_MODAL_TYPE {
   listing_limit_cars_highest_limit,
   listing_limit_real_estate_highest_limit,
   listing_limit_consumer_good_highest_limit,
+  listing_limit_tier_limit,
+  listing_limit_no_trial_no_discount,
+  listing_limit_trial,
+  listing_limit_discount,
 }
 
 export const modalConfig: Record<PRO_MODAL_TYPE, ProModalConfig> = {
@@ -28,20 +32,63 @@ export const modalConfig: Record<PRO_MODAL_TYPE, ProModalConfig> = {
   },
   [PRO_MODAL_TYPE.listing_limit_consumer_good_highest_limit]: {
     img: '/assets/icons/pro/modals/listing-limit.svg',
-    title: $localize`:@@listing_limit_pro_user_real_estate_subscription_modal_title:You've reached the subscription's listing limit`,
-    text1: $localize`:@@listing_limit_pro_user_real_estate_subscription_modal_description:It’s not possible to have so many Real Estate items for sale at the same time. The item will be listed to your catalog, but it will remain inactive.`,
+    title: $localize`:@@listing_limit_pro_user_cg_subscription_modal_title:You've reached the subscription's listing limit`,
+    text1: $localize`:@@listing_limit_pro_user_cg_subscription_modal_description:It’s not possible to have so many items for sale at the same time. The item will be listed to your catalog, but it will remain inactive.`,
     buttons: {
-      secondary: { text: $localize`:@@listing_limit_pro_user_real_estate_subscription_modal_understood_button:Understood` },
-      primary: { text: $localize`:@@listing_limit_pro_user_real_estate_subscription_modal_more_info_button:More info` },
+      secondary: { text: $localize`:@@listing_limit_pro_user_cg_subscription_modal_understood_button:Understood` },
+      primary: { text: $localize`:@@listing_limit_pro_user_cg_subscription_modal_more_info_button:More info` },
     },
   },
   [PRO_MODAL_TYPE.listing_limit_cars_highest_limit]: {
     img: '/assets/icons/pro/modals/listing-limit.svg',
-    title: $localize`:@@listing_limit_pro_user_real_estate_subscription_modal_title:You've reached the subscription's listing limit`,
-    text1: $localize`:@@listing_limit_pro_user_real_estate_subscription_modal_description:It’s not possible to have so many Real Estate items for sale at the same time. The item will be listed to your catalog, but it will remain inactive.`,
+    title: $localize`:@@listing_limit_pro_user_cars_subscription_modal_title:You've reached the subscription's listing limit`,
+    text1: $localize`:@@listing_limit_pro_user_cars_subscription_modal_description_1:In order to have more cars for sale, you need to contact our sales team.`,
+    text2: $localize`:@@listing_limit_pro_user_cars_subscription_modal_description_2:If you decide to keep with your plan, this item will become inactive – nobody will be able to buy it.`,
     buttons: {
-      secondary: { text: $localize`:@@listing_limit_pro_user_real_estate_subscription_modal_understood_button:Understood` },
-      primary: { text: $localize`:@@listing_limit_pro_user_real_estate_subscription_modal_more_info_button:More info` },
+      secondary: { text: $localize`:@@listing_limit_pro_user_cg_subscription_modal_understood_button:Understood` },
+      primary: { text: $localize`:@@listing_limit_pro_user_cg_subscription_modal_more_info_button:More info` },
+    },
+  },
+  [PRO_MODAL_TYPE.listing_limit_tier_limit]: {
+    img: '/assets/icons/pro/modals/listing-limit.svg',
+    title: $localize`:@@listing_limit_pro_user_upgrade_tier_modal_title:You've reached your plan's listing limit`,
+    text1: $localize`:@@listing_limit_pro_user_upgrade_tier_modal_description_1:To list more items of this category upgrade your subscription plan. You will sell more!`,
+    text2: $localize`:@@listing_limit_pro_user_upgrade_tier_modal_description_2:If you choose to keep your plan, this item will become inactive – nobody will be able to buy it.`,
+    buttons: {
+      secondary: {
+        text: $localize`:@@listing_limit_pro_user_upgrade_tier_modal_dismiss_button:Leave as inactive`,
+      },
+      primary: { text: $localize`:@@listing_limit_pro_user_upgrade_tier_modal_view_plans_button:View plans` },
+    },
+  },
+  [PRO_MODAL_TYPE.listing_limit_no_trial_no_discount]: {
+    img: '/assets/icons/pro/modals/listing-limit.svg',
+    title: $localize`:@@listing_limit_non_pro_users_discount_or_no_free_trial_modal_title:You've reached the free listing limit`,
+    text1: $localize`:@@listing_limit_non_pro_users_discount_or_no_free_trial_modal_description_part_1:Want to list more items of this category? You might be interested in signing up for wallapop PRO. Professionalise your online sales, and sell more!`,
+    text2: $localize`:@@listing_limit_non_pro_users_discount_or_no_free_trial_modal_description_part_2:If you decide wallapop PRO isn't for you, this item will become inactive – nobody will be able to buy it.`,
+    buttons: {
+      secondary: { text: $localize`:@@listing_limit_non_pro_users_discount_or_no_free_trial_modal_dismiss_button:Leave as inactive` },
+      primary: { text: $localize`:@@listing_limit_non_pro_users_no_free_trial_modal_view_plans_button:View plans` },
+    },
+  },
+  [PRO_MODAL_TYPE.listing_limit_trial]: {
+    img: '/assets/icons/pro/modals/listing-limit.svg',
+    title: $localize`:@@listing_limit_non_pro_users_free_trial_available_title:You've reached the free listing limit`,
+    text1: $localize`:@@listing_limit_non_pro_users_free_trial_available_description_part_1:Want to list more items of this category? You might be interested in signing up for wallapop PRO. Professionalise your online sales, and sell more!`,
+    text2: $localize`:@@listing_limit_non_pro_users_free_trial_available_description_part_2:If you decide wallapop PRO isn't for you, this item will become inactive – nobody will be able to buy it.`,
+    buttons: {
+      secondary: { text: $localize`:@@listing_limit_non_pro_users_free_trial_available:Leave as inactive` },
+      primary: { text: $localize`:@@listing_limit_non_pro_users_free_trial_available_ok_button:Start free trial` },
+    },
+  },
+  [PRO_MODAL_TYPE.listing_limit_discount]: {
+    img: '/assets/icons/pro/modals/listing-limit.svg',
+    title: $localize`:@@listing_limit_non_pro_users_discount_or_no_free_trial_modal_title:You've reached the free listing limit`,
+    text1: $localize`:@@listing_limit_non_pro_users_discount_or_no_free_trial_modal_description_part_1:Want to list more items of this category? You might be interested in signing up for wallapop PRO. Professionalise your online sales, and sell more!`,
+    text2: $localize`:@@listing_limit_non_pro_users_discount_or_no_free_trial_modal_description_part_2:If you decide wallapop PRO isn't for you, this item will become inactive – nobody will be able to buy it.`,
+    buttons: {
+      secondary: { text: $localize`:@@listing_limit_non_pro_users_discount_or_no_free_trial_modal_dismiss_button:Leave as inactive` },
+      primary: { text: $localize`:@@listing_limit_non_pro_users_discount_modal_start_button:Try with ${50}:INTERPOLATION:% discount` },
     },
   },
 };
