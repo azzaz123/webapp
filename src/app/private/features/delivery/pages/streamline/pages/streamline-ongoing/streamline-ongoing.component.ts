2cba0252f8585c01c81f44926d29894dce5a0d93
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-11-03 12:06:41 +0100

    [WPA-4781]: Redirect to TTS when click on ongoing shipping

--- src/app/private/features/delivery/pages/streamline/pages/streamline-ongoing/streamline-ongoing.component.ts
+++ src/app/private/features/delivery/pages/streamline/pages/streamline-ongoing/streamline-ongoing.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DELIVERY_PATHS } from '@private/features/delivery/delivery-routing-constants';
+import { PRIVATE_PATHS } from '@private/private-routing-constants';
 import { HistoricElement } from '@shared/historic-list/interfaces/historic-element.interface';
 import { HistoricList } from '@shared/historic-list/interfaces/historic-list.interface';
 import { Observable } from 'rxjs';
@@ -14,7 +17,7 @@ export class StreamlineOngoingComponent implements OnInit, OnDestroy {
   public loadingIconSrc: string = '/assets/icons/spinner.svg';
   public loadingIconSizePixels: number = 32;
 
-  constructor(private streamlineOngoingUIService: StreamlineOngoingUIService) {}
+  constructor(private streamlineOngoingUIService: StreamlineOngoingUIService, private router: Router) {}
 
   public get historicList$(): Observable<HistoricList> {
     return this.streamlineOngoingUIService.historicList$;
@@ -32,7 +35,9 @@ export class StreamlineOngoingComponent implements OnInit, OnDestroy {
     this.streamlineOngoingUIService.reset();
   }
 
+  // TODO: Implement redirection to TTS
   public onItemClick(historicElement: HistoricElement): void {
-    // TODO: redirect to TTS
+    const pathToTransactionTracking = `${PRIVATE_PATHS.DELIVERY}/${DELIVERY_PATHS.TTS}/${historicElement.id}`;
+    this.router.navigate([pathToTransactionTracking]);
   }
 }
