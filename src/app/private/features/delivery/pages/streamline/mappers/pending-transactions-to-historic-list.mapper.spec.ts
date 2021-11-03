7b6b6248d4fea00deb19780a70d1aa61ed4ab6c2
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-11-03 10:38:46 +0100

    [WPA-4781]: Add tests for Pending Transactions to Historic List mapper

--- src/app/private/features/delivery/pages/streamline/mappers/pending-transactions-to-historic-list.mapper.spec.ts
+++ src/app/private/features/delivery/pages/streamline/mappers/pending-transactions-to-historic-list.mapper.spec.ts
@@ -1,4 +1,5 @@
 import { MOCK_PENDING_TRANSACTIONS } from '@api/fixtures/core/pending-transactions-fixtures.spec';
+import { MOCK_HISTORIC_LIST_FROM_PENDING_TRANSACTIONS } from '@shared/historic-list/fixtures/historic-list.fixtures.spec';
 import { HistoricList } from '@shared/historic-list/interfaces/historic-list.interface';
 import { mapPendingTransactionToHistoricList } from './pending-transactions-to-historic-list.mapper';
 
@@ -6,7 +7,7 @@ describe('mapPendingTransactionToHistoricList', () => {
   describe('when converting pending transactions to web context', () => {
     it('should map to web context', () => {
       let result: HistoricList;
-      const expectedResult = { elements: [] };
+      const expectedResult = MOCK_HISTORIC_LIST_FROM_PENDING_TRANSACTIONS;
 
       result = mapPendingTransactionToHistoricList(MOCK_PENDING_TRANSACTIONS);
 
