252a56889b13428124b2ac08a47786061be1d33e
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-11-03 12:47:16 +0100

    [WPA-4781]: Use streamline paths as filters in Streamline component

--- src/app/private/features/delivery/pages/streamline/enums/streamline-filter.enum.ts
+++
@@ -1,3 +0,0 @@
-export enum STREAMLINE_FILTER {
-  ONGOING,
-}
--- src/app/private/features/delivery/pages/streamline/streamline.component.ts
+++ src/app/private/features/delivery/pages/streamline/streamline.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TabsBarElement } from '@shared/tabs-bar/interfaces/tabs-bar-element.interface';
-import { DELIVERY_PATHS } from '../../delivery-routing-constants';
 import { STREAMLINE_PATHS } from './streamline.routing.constants';
 
 @Component({
@@ -10,14 +9,15 @@ import { STREAMLINE_PATHS } from './streamline.routing.constants';
   styleUrls: ['./streamline.component.scss'],
 })
 export class StreamlineComponent implements OnInit {
+  // TODO: Use valid copies when ready
+  public tabsBarElements: TabsBarElement<STREAMLINE_PATHS>[] = [{ value: STREAMLINE_PATHS.ONGOING, label: 'On going' }];
+
   constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
     const firstTabsBarElement = this.tabsBarElements[0].value;
     this.redirect(firstTabsBarElement);
   }
-
-  public tabsBarElements: TabsBarElement[] = [{ label: 'On going', value: STREAMLINE_PATHS.ONGOING }];
 
   public onTabsBarChange(tabsBarElement: TabsBarElement): void {
     this.redirect(tabsBarElement.value);
