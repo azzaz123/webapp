913dc6f7e346567cd134f5edf4a24312f932f980
antoni-casas <toni.casas@wallapop.com>
2021-11-04 11:55:42 +0100

    [WPA-4949]: feat: Create status info interfaces

--- src/app/api/core/model/delivery/transaction/tracking/status-info/transaction-tracking-status-info.interface.ts
+++ src/app/api/core/model/delivery/transaction/tracking/status-info/transaction-tracking-status-info.interface.ts
@@ -1,1 +1,14 @@
-export interface TransactionTrackingStatusInfo {}
+import { TransactionTrackingActionDetail } from '../transaction-tracking-action-detail.interface';
+import { TransactionTrackingStyle } from '../transaction-tracking-style.interface';
+
+export interface TransactionTrackingStatusInfo {
+  action: TransactionTrackingActionDetail;
+  actionIcon: TransactionTrackingStyle;
+  description: string;
+  icon: TransactionTrackingStatusInfoIcon;
+}
+export interface TransactionTrackingStatusInfoIcon {
+  url: string;
+  thumbnailUrl: string;
+  style: TransactionTrackingStyle;
+}
--- src/app/api/core/model/delivery/transaction/tracking/transaction-tracking-action-detail.interface.ts
+++ src/app/api/core/model/delivery/transaction/tracking/transaction-tracking-action-detail.interface.ts
@@ -12,13 +12,24 @@ export interface TransactionTrackingActionAnalytics {
   user_id: string;
 }
 export interface TransactionTrackingActionPayload {
-  description: string;
-  linkUrl: string;
-  negative: TransactionTrackingActionPayloadConfirmation;
-  positive: TransactionTrackingActionPayloadConfirmation;
+  banner?: TransactionTrackingActionPayloadBanner;
+  description?: string;
+  linkUrl?: string;
+  name?: string;
+  negative?: TransactionTrackingActionPayloadConfirmation;
+  parameters?: TransactionTrackingActionPayloadParameters;
+  positive?: TransactionTrackingActionPayloadConfirmation;
+  success?: TransactionTrackingActionDetail;
+  title?: string;
+}
+export interface TransactionTrackingActionPayloadBanner {
   title: string;
+  trackingCode: string;
 }
 export interface TransactionTrackingActionPayloadConfirmation {
   action?: TransactionTrackingActionDetail;
   title: string;
+}
+export interface TransactionTrackingActionPayloadParameters {
+  transactionId: string;
 }
