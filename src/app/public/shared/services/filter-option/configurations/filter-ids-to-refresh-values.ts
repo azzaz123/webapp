ce5e979f18300cf519b593e3daf670028c51f707
Irene Blanco <irene.blanco@wallapop.com>
2021-11-04 17:59:21 +0100

    [FIN-1453]: FILTER_IDS_TO_REFRESH_VALUES - Removed null value

--- src/app/public/shared/services/filter-option/configurations/filter-ids-to-refresh-values.ts
+++ src/app/public/shared/services/filter-option/configurations/filter-ids-to-refresh-values.ts
@@ -1,4 +1,4 @@
 import { COMMON_CONSUMER_GOODS_CONFIGURATION_ID } from '@public/shared/components/filters/core/enums/configuration-ids/consumer-goods-configuration-ids.enum';
 import { ConfigurationId } from '@public/shared/components/filters/core/types/configuration-id.type';
 
-export const FILTER_IDS_TO_REFRESH_VALUES: ConfigurationId[] = [null, COMMON_CONSUMER_GOODS_CONFIGURATION_ID.OBJECT_TYPE];
+export const FILTER_IDS_TO_REFRESH_VALUES: ConfigurationId[] = [COMMON_CONSUMER_GOODS_CONFIGURATION_ID.OBJECT_TYPE];
