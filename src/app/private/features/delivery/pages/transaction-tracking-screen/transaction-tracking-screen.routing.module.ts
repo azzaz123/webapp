d658b38fc9db654971d64fc7a421cff92836752f
Laia Lopez <laialopezz99@gmail.com>
2021-11-08 12:51:47 +0100

    [WPA-5986]: [TransactionTrackingScreenRoutingModule] LoadChildren Change

--- src/app/private/features/delivery/pages/transaction-tracking-screen/transaction-tracking-screen.routing.module.ts
+++ src/app/private/features/delivery/pages/transaction-tracking-screen/transaction-tracking-screen.routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
+import { TransactionTrackingInstructionsModule } from '../../modals/transaction-tracking-screen/transaction-tracking-instructions/transaction-tracking-instructions.module';
 import { TransactionTrackingOverviewComponent } from './transaction-tracking-overview/transaction-tracking-overview.component';
 import { TRANSACTION_TRACKING_PATHS } from './transaction-tracking-screen-routing-constants';
 import { TransactionTrackingScreenComponent } from './transaction-tracking-screen.component';
@@ -15,10 +16,7 @@ const routes: Route[] = [
       },
       {
         path: TRANSACTION_TRACKING_PATHS.INSTRUCTIONS,
-        loadChildren: () =>
-          import(
-            '@private/features/delivery/modals/transaction-tracking-screen/transaction-tracking-instructions/transaction-tracking-instructions.module'
-          ).then((m) => m.TransactionTrackingInstructionsModule),
+        loadChildren: () => TransactionTrackingInstructionsModule,
       },
       {
         path: '**',
