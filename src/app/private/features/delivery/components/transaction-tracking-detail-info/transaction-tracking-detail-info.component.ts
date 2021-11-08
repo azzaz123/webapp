7d29e1e0678f28db6ebfbc3c19a8d31030f9e3ca
Laia Lopez <laialopezz99@gmail.com>
2021-11-08 16:49:20 +0100

    [WPA-5809]: [Rename] TransactionTrackingDetailInfoComponent

--- src/app/private/features/delivery/components/transaction-tracking-detail-info/transaction-tracking-detail-info.component.ts
+++ src/app/private/features/delivery/components/transaction-tracking-detail-info/transaction-tracking-detail-info.component.ts
@@ -8,9 +8,8 @@ import { Component, Input } from '@angular/core';
 export class TransactionTrackingDetailInfoComponent {
   @Input() title: string;
   @Input() subtitle: string;
-  @Input() srcImage: string;
-  @Input() defaultSrcSvg: string;
+  @Input() imageSrc: string;
+  @Input() defaultSvgSrc: string;
   @Input() showCaret: boolean;
   @Input() isRoundedImage: boolean;
-  @Input() isRoundedBorderImage: boolean;
 }
