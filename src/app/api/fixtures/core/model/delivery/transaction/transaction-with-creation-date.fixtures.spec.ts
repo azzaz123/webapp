37a6eaaf54dd7b1f455ddf429bf987ef3008a2e2
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-11-08 20:18:39 +0100

    [WPA-5300]: Add fixtures for transaction interfaces

---
+++ src/app/api/fixtures/core/model/delivery/transaction/transaction-with-creation-date.fixtures.spec.ts
@@ -0,0 +1,34 @@
+import { mapNumberAndCurrencyCodeToMoney } from '@api/core/mappers';
+import { TransactionWithCreationDate } from '@api/core/model';
+import { TRANSACTION_DELIVERY_STATUS, TRANSACTION_PAYMENT_STATUS, TRANSACTION_STATUS } from '@api/core/model/delivery/transaction/status';
+import { MOCK_TRANSACTION_ITEM_1, MOCK_TRANSACTION_ITEM_2 } from './transaction-item.fixture.spec';
+import { MOCK_TRANSACTION_USER_1, MOCK_TRANSACTION_USER_2 } from './transaction-user.fixture.spec';
+
+export const MOCK_TRANSACTIONS_WITH_CREATION_DATE: TransactionWithCreationDate[] = [
+  {
+    id: '3b7560cc-b4f8-48bf-ba27-4d070952b3e8',
+    creationDate: new Date('2021-10-22T10:57:14.000Z'),
+    item: MOCK_TRANSACTION_ITEM_1,
+    buyer: MOCK_TRANSACTION_USER_1,
+    seller: MOCK_TRANSACTION_USER_2,
+    moneyAmount: mapNumberAndCurrencyCodeToMoney({ number: 19.75, currency: 'EUR' }),
+    status: {
+      transaction: TRANSACTION_STATUS.SUCCEEDED,
+      delivery: TRANSACTION_DELIVERY_STATUS.NONE,
+      payment: TRANSACTION_PAYMENT_STATUS.NONE,
+    },
+  },
+  {
+    id: '001cf831-d040-4e31-b4e7-aa50d2a3cadc',
+    creationDate: new Date('2021-10-22T10:45:55.000Z'),
+    item: MOCK_TRANSACTION_ITEM_2,
+    buyer: MOCK_TRANSACTION_USER_1,
+    seller: MOCK_TRANSACTION_USER_2,
+    moneyAmount: mapNumberAndCurrencyCodeToMoney({ number: 12, currency: 'EUR' }),
+    status: {
+      transaction: TRANSACTION_STATUS.SUCCEEDED,
+      delivery: TRANSACTION_DELIVERY_STATUS.NONE,
+      payment: TRANSACTION_PAYMENT_STATUS.NONE,
+    },
+  },
+];
