c35ab7c9bced06c385fa9396cfd76a614b59e112
Laia Lopez <laialopezz99@gmail.com>
2021-11-08 14:35:41 +0100

    [WPA-5809]: [TransactionTrackingDetailInfoComponent] Define properties

--- src/app/private/features/delivery/components/transaction-tracking-detail-info/transaction-tracking-detail-info.component.ts
+++ src/app/private/features/delivery/components/transaction-tracking-detail-info/transaction-tracking-detail-info.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'tsl-transaction-tracking-detail-info',
   templateUrl: './transaction-tracking-detail-info.component.html',
   styleUrls: ['./transaction-tracking-detail-info.component.scss'],
 })
-export class TransactionTrackingDetailInfoComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class TransactionTrackingDetailInfoComponent {
+  @Input() title: string;
+  @Input() subtitle: string;
+  @Input() srcImage: string;
+  @Input() defaultSrcSvg: string;
+  @Input() showCaret: boolean;
+  @Input() isRoundedImage: boolean;
+  @Input() isRoundedBorderImage: boolean;
 }
