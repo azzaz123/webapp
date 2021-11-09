c43481e3c9c9c40f89dfd3f6283ade7432bf2b53
Laia Lopez <laialopezz99@gmail.com>
2021-11-09 16:29:23 +0100

    [WPA-5809]: [ViewEncapsulationNone] Deleted

--- src/app/private/features/delivery/components/transaction-tracking-detail-info/transaction-tracking-detail-info.component.ts
+++ src/app/private/features/delivery/components/transaction-tracking-detail-info/transaction-tracking-detail-info.component.ts
@@ -1,11 +1,10 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { Component, Input } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'tsl-transaction-tracking-detail-info',
   templateUrl: './transaction-tracking-detail-info.component.html',
   styleUrls: ['./transaction-tracking-detail-info.component.scss'],
-  encapsulation: ViewEncapsulation.None,
 })
 export class TransactionTrackingDetailInfoComponent {
   constructor(public sanitizer: DomSanitizer) {}
