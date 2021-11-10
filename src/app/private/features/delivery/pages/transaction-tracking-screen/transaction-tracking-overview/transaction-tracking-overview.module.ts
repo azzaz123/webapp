8233c9d4b396a31ead3d9422f5c6cf91887b67a0
Laia Lopez <laialopezz99@gmail.com>
2021-11-10 14:14:10 +0100

    [WPA-3744]: [TransactionTrackingOverviewComponent] WIP

--- src/app/private/features/delivery/pages/transaction-tracking-screen/transaction-tracking-overview/transaction-tracking-overview.module.ts
+++ src/app/private/features/delivery/pages/transaction-tracking-screen/transaction-tracking-overview/transaction-tracking-overview.module.ts
@@ -5,10 +5,11 @@ import {
   TransactionTrackingOverviewRoutingModule,
 } from './transaction-tracking-overview.routing.module';
 import { ButtonModule } from '@shared/button/button.module';
+import { SvgIconModule } from '@shared/svg-icon/svg-icon.module';
 
 @NgModule({
   declarations: [transactionTrackingOverviewRoutedComponents],
-  imports: [TransactionTrackingOverviewRoutingModule, CommonModule, ButtonModule],
+  imports: [TransactionTrackingOverviewRoutingModule, CommonModule, ButtonModule, SvgIconModule],
   providers: [],
 })
 export class TransactionTrackingOverviewModule {}
