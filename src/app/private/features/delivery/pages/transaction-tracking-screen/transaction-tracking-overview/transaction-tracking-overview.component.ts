247d8a7fc4e9b27cadfdfb88f5e6afc1feb323e6
Laia Lopez <laialopezz99@gmail.com>
2021-11-11 17:19:31 +0100

    [WPA-3747]: [StatusInfoProperties] WIP

--- src/app/private/features/delivery/pages/transaction-tracking-screen/transaction-tracking-overview/transaction-tracking-overview.component.ts
+++ src/app/private/features/delivery/pages/transaction-tracking-screen/transaction-tracking-overview/transaction-tracking-overview.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TransactionTrackingInfo } from '@private/features/delivery/interfaces/tts/transaction-tracking-info.interface';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'tsl-transaction-tracking-overview',
@@ -6,7 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transaction-tracking-overview.component.scss'],
 })
 export class TransactionTrackingOverviewComponent implements OnInit {
+  public transactionTracking$: Observable<any> = of(null);
+  public statusInfoProperties$: Observable<TransactionTrackingInfo[]>;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initializeStatusInfoProperties();
+  }
+
+  public initializeStatusInfoProperties(): void {
+    this.transactionTracking$.pipe(
+      map((transactionTracking: any) => {
+        const propertiesMapped: TransactionTrackingInfo[] = [];
+        transactionTracking.statusInfo.foreach((slot: any) => {
+          propertiesMapped.push({
+            description: slot.description,
+            iconSrc: slot.icon.url,
+            showCaret: slot.showCaret,
+            iconClassName: slot.style.className,
+          });
+          return propertiesMapped;
+        });
+      })
+    );
+  }
 }
