4b368a2eaf346eec0e77e9661b3f4aec65ce5863
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-11-15 12:12:22 +0100

    [WPA-5356]: Add Streamline Completed UI service to module

--- src/app/private/features/delivery/pages/streamline/pages/streamline-completed/streamline-completed.module.ts
+++ src/app/private/features/delivery/pages/streamline/pages/streamline-completed/streamline-completed.module.ts
@@ -4,9 +4,11 @@ import { streamlineCompletedRoutedComponents, StreamlineCompletedRoutingModule }
 import { HistoricListModule } from '@shared/historic-list/historic-list.module';
 import { SvgIconModule } from '@shared/svg-icon/svg-icon.module';
 import { TransactionsHistoryModule } from '@api/delivery/transactions/history/transactions-history-api.module';
+import { StreamlineCompletedUIService } from '../../services/streamline-completed-ui/streamline-completed-ui.service';
 
 @NgModule({
   imports: [CommonModule, HistoricListModule, SvgIconModule, TransactionsHistoryModule, StreamlineCompletedRoutingModule],
   declarations: [streamlineCompletedRoutedComponents],
+  providers: [StreamlineCompletedUIService],
 })
 export class StreamlineCompletedModule {}
