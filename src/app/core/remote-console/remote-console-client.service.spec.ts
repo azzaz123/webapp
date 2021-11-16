0250a65c0e17526905eb50d78cba8f743efbe418
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-11-16 12:48:28 +0100

    [WPA-6872]: Send chat metrics only in production environment

--- src/app/core/remote-console/remote-console-client.service.spec.ts
+++ src/app/core/remote-console/remote-console-client.service.spec.ts
@@ -1,12 +1,16 @@
-import { getTestBed, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
 import { RemoteConsoleClientService } from './remote-console-client.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { environment } from '../../../environments/environment';
+import * as localEnvironmentFile from '@environments/environment';
+import * as prodEnvironmentFile from '@environments/environment.prod';
+import { Environment } from '@environments/environment.interface';
 
 describe('RemoteConsoleClientService', () => {
   let service: RemoteConsoleClientService;
   let httpMock: HttpTestingController;
+
+  const localEnvironment: Environment = localEnvironmentFile.environment;
+  const prodEnvironment: Environment = prodEnvironmentFile.environment;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -22,13 +26,33 @@ describe('RemoteConsoleClientService', () => {
     httpMock.verify();
   });
 
-  describe('info', () => {
-    it('should send info log', () => {
-      service.info({ json: 'json' });
+  describe('when environment is production', () => {
+    beforeEach(() => {
+      Object.defineProperty(localEnvironmentFile, 'environment', { value: prodEnvironment });
+    });
 
-      const req = httpMock.expectOne(environment.remoteConsoleUrl);
-      expect(req.request.method).toEqual('POST');
-      expect(req.request.body).toEqual({ logs: [{ json: 'json' }] });
+    describe('and when sending metrics', () => {
+      it('should send info log', () => {
+        service.info({ json: 'json' });
+
+        const req = httpMock.expectOne(prodEnvironment.remoteConsoleUrl);
+        expect(req.request.method).toEqual('POST');
+        expect(req.request.body).toEqual({ logs: [{ json: 'json' }] });
+      });
+    });
+  });
+
+  describe('when environment is development', () => {
+    beforeEach(() => {
+      Object.defineProperty(localEnvironmentFile, 'environment', { value: localEnvironment });
+    });
+
+    describe('and when sending metrics', () => {
+      it('should NOT send info log', () => {
+        service.info({ json: 'json' });
+
+        httpMock.expectNone(localEnvironment.remoteConsoleUrl);
+      });
     });
   });
 });
