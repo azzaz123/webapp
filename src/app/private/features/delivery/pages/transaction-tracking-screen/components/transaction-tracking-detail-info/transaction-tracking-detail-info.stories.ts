26a54a42608718c2a2f2d5985f1ac1da6835de2b
Laia Lopez <laialopezz99@gmail.com>
2021-11-17 09:31:36 +0100

    [WPA-3747]: [Storybook] Updated

--- src/app/private/features/delivery/pages/transaction-tracking-screen/components/transaction-tracking-detail-info/transaction-tracking-detail-info.stories.ts
+++ src/app/private/features/delivery/pages/transaction-tracking-screen/components/transaction-tracking-detail-info/transaction-tracking-detail-info.stories.ts
@@ -19,7 +19,7 @@ const Template: Story<TransactionTrackingDetailInfoComponent> = (args: Transacti
     imports: [CommonModule, SvgIconModule, HttpClientModule],
   },
   template:
-    '<tsl-transaction-tracking-detail-info [transactionTrackingInfo]="transactionTrackingInfo"></tsl-transaction-tracking-detail-info>',
+    '<tsl-transaction-tracking-detail-info [transactionTrackingInfo]="transactionTrackingInfo" [isClickableAction]="isClickableAction" [isBorderBottom]="isBorderBottom"></tsl-transaction-tracking-detail-info>',
 });
 
 export const ProductExample = Template.bind({});
@@ -50,4 +50,28 @@ PriceExample.args = {
     iconClassName: 'rounded',
     iconSrc: 'https://prod-delivery-resources.wallapop.com/transaction-tracking-screen/transaction_tracking_details/price_element.png',
   },
+};
+
+export const ActionClickable = Template.bind({});
+ActionClickable.args = {
+  transactionTrackingInfo: {
+    description:
+      '<strong>Tu compra está protegida</strong> por Wallapop Protect, nuestra política de protección.<br><span style="color: #13C1AC">¿Quieres saber más?</span>',
+    showCaret: false,
+    iconClassName: 'rounded',
+    iconSrc:
+      'https://prod-delivery-resources.wallapop.com/transaction-tracking-screen/payment+status/Icons/Delivery/Bank+Account+Warranty%402x.png',
+  },
+  isClickableAction: true,
+};
+
+export const BorderBottom = Template.bind({});
+BorderBottom.args = {
+  transactionTrackingInfo: {
+    description: '<span style="color: #AFB6B6">Producto:</span><br>crayones',
+    showCaret: true,
+    iconClassName: 'rounded',
+    iconSrc: 'https://cdn-beta.wallapop.com/images/10420/35/gw/__/c10420p190584802/i421614104.jpg?pictureSize=W800',
+  },
+  isBorderBottom: true,
 };
