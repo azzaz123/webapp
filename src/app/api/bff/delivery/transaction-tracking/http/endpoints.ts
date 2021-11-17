14a0ec5a146c03f3bb08e57a294e853d024178af
antoni-casas <toni.casas@wallapop.com>
2021-11-17 11:29:09 +0100

    [WPA-6082]: feat: Create endpoint

--- src/app/api/bff/delivery/transaction-tracking/http/endpoints.ts
+++ src/app/api/bff/delivery/transaction-tracking/http/endpoints.ts
@@ -1,4 +1,5 @@
 import { environment } from '@environments/environment';
 
-export const TRANSACTION_TRACKING_ENDPOINT = `${environment.baseUrl}bff/delivery/transaction-tracking`;
 export const TRANSACTION_TRACKING_DETAILS_ENDPOINT = `${environment.baseUrl}bff/delivery/transaction-tracking-details`;
+export const TRANSACTION_TRACKING_ENDPOINT = `${environment.baseUrl}bff/delivery/transaction-tracking`;
+export const TRANSACTION_TRACKING_INSTRUCTIONS_ENDPOINT = `${environment.baseUrl}bff/delivery/transaction-tracking/instructions`;
