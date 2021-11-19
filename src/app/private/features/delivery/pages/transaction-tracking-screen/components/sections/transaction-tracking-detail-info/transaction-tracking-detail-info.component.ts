8e320558b17f17d6f130fc1bcbd1acca89ca07b3
Laia Lopez <laialopezz99@gmail.com>
2021-11-19 11:12:42 +0100

    [WPA-7038]: [Styling] Transaction Info no longer used

--- src/app/private/features/delivery/pages/transaction-tracking-screen/components/sections/transaction-tracking-detail-info/transaction-tracking-detail-info.component.ts
+++ src/app/private/features/delivery/pages/transaction-tracking-screen/components/sections/transaction-tracking-detail-info/transaction-tracking-detail-info.component.ts
@@ -1,9 +1,6 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { TransactionTrackingDetails } from '@api/core/model/delivery/transaction/tracking';
-import { DeviceService } from '@core/device/device.service';
 import { TransactionDetail } from '../../../interfaces/transaction-detail.interface';
-import { TransactionInfo } from '../../../interfaces/transaction-info.interface';
-import { mapTransactionTrackingDetailsToInfo } from '../../../mappers/transaction-info.mapper';
 import { mapTransactionsDetail } from '../../../mappers/transaction-detail.mapper';
 
 @Component({
@@ -15,17 +12,9 @@ import { mapTransactionsDetail } from '../../../mappers/transaction-detail.mappe
 export class TransactionTrackingDetailInfoComponent {
   @Input() transactionTrackingDetails: TransactionTrackingDetails;
 
-  constructor(private deviceService: DeviceService) {}
-
-  public get transactionInfo(): TransactionInfo {
-    return mapTransactionTrackingDetailsToInfo(this.transactionTrackingDetails);
-  }
+  constructor() {}
 
   public get transactionDetails(): TransactionDetail[] {
     return mapTransactionsDetail(this.transactionTrackingDetails.info);
-  }
-
-  public get isMobile(): boolean {
-    return this.deviceService.isMobile();
   }
 }
--- src/app/private/features/delivery/pages/transaction-tracking-screen/components/sections/transaction-tracking-detail-info/transaction-tracking-detail-info.module.ts
+++ src/app/private/features/delivery/pages/transaction-tracking-screen/components/sections/transaction-tracking-detail-info/transaction-tracking-detail-info.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TransactionTrackingDetailInfoComponent } from './transaction-tracking-detail-info.component';
 import { TransactionDetailModule } from '../../transaction-detail/transaction-detail.module';
-import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
-import { SvgIconModule } from '@shared/svg-icon/svg-icon.module';
-import { TransactionInfoModule } from '../../transaction-info/transaction-info.module';
 
 @NgModule({
   declarations: [TransactionTrackingDetailInfoComponent],
-  imports: [CommonModule, TransactionDetailModule, NgbAccordionModule, SvgIconModule, TransactionInfoModule],
+  imports: [CommonModule, TransactionDetailModule],
   exports: [TransactionTrackingDetailInfoComponent],
 })
 export class TransactionTrackingDetailInfoModule {}
