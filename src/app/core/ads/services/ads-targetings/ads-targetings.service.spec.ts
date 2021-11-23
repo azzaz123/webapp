a0f60a129103d31179ed658cee2fd9611b6a885d
Robert Vera <robertveraroige@gmail.com>
2021-11-23 10:53:40 +0100

    [WPA-6575]: add tests

--- src/app/core/ads/services/ads-targetings/ads-targetings.service.spec.ts
+++ src/app/core/ads/services/ads-targetings/ads-targetings.service.spec.ts
@@ -1,21 +1,37 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ActivatedRoute } from '@angular/router';
 import { UserService } from '@core/user/user.service';
 import { AdsTargetingsService } from './ads-targetings.service';
-import { MockUser, MockUserService } from '@fixtures/user.fixtures.spec';
-import { SearchBoxValue } from '@layout/topbar/core/interfaces/suggester-response.interface';
+import { MockUserService } from '@fixtures/user.fixtures.spec';
+import { YieldBirdService } from '@core/ads/vendors/yieldbird/yieldbird.service';
+import { MockYieldBirdService } from '@fixtures/ads.fixtures.spec';
+import { FilterParameter } from '@public/shared/components/filters/interfaces/filter-parameter.interface';
 import { FILTER_QUERY_PARAM_KEY } from '@public/shared/components/filters/enums/filter-query-param-key.enum';
-import { BehaviorSubject } from 'rxjs';
-
-const MOCK_SEARCH_KEYWORD: SearchBoxValue = { [FILTER_QUERY_PARAM_KEY.keywords]: 'iphone' };
-
-const searchParamsMockSubject = new BehaviorSubject(MOCK_SEARCH_KEYWORD);
-const searchParamsMock = searchParamsMockSubject.asObservable();
+import { CATEGORY_IDS } from '@core/category/category-ids';
+import { AdTargetings } from '@core/ads/models/ad-targetings';
 
 describe('AdsTargetingsService', () => {
   let service: AdsTargetingsService;
+  const MOCK_PARAMS: FilterParameter[] = [
+    { key: FILTER_QUERY_PARAM_KEY.keywords, value: 'OnePlus One' },
+    { key: FILTER_QUERY_PARAM_KEY.categoryId, value: `${CATEGORY_IDS.CELL_PHONES_ACCESSORIES}` },
+    { key: FILTER_QUERY_PARAM_KEY.minPrice, value: `${200}` },
+    { key: FILTER_QUERY_PARAM_KEY.maxPrice, value: `${400}` },
+  ];
+
+  const expectedTargetings: AdTargetings = {
+    content: 'OnePlus One',
+    category: '16000',
+    minprice: '200',
+    maxprice: '400',
+    gender: 'male',
+    userId: 'abcd-1234',
+    age: '3',
+    latitude: '41.399132621722174',
+    longitude: '2.17585484411869',
+    yb_ab: 'test',
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -26,10 +42,8 @@ describe('AdsTargetingsService', () => {
           useValue: MockUserService,
         },
         {
-          provide: ActivatedRoute,
-          useValue: {
-            queryParams: searchParamsMock,
-          },
+          provide: YieldBirdService,
+          useValue: MockYieldBirdService,
         },
       ],
     });
@@ -38,5 +52,28 @@ describe('AdsTargetingsService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should set single targetings in memory', () => {
+    const adTargetings = { latitude: '1', longitude: '2', category: '123', content: 'test' };
+
+    service.setAdTargeting(adTargetings);
+
+    expect(service.adTargetings).toEqual(adTargetings);
+  });
+
+  it('should set all targetings from params, users and vendors', () => {
+    service.setAdTargetings(MOCK_PARAMS);
+
+    expect(service.adTargetings).toEqual(expectedTargetings);
+  });
+
+  it('should clean all targetings before setting them all again', () => {
+    const previousAdTargetings = { latitude: '1', longitude: '2', category: '123', brand: 'wow', content: 'old' };
+
+    service.setAdTargeting(previousAdTargetings);
+    service.setAdTargetings(MOCK_PARAMS);
+
+    expect(service.adTargetings).toEqual(expectedTargetings);
   });
 });
--- src/app/core/ads/vendors/yieldbird/yieldbierd.service.spec.ts
+++ src/app/core/ads/vendors/yieldbird/yieldbierd.service.spec.ts
@@ -1,41 +1,13 @@
 import { TestBed } from '@angular/core/testing';
-import { AdSlotConfiguration } from '@core/ads/models';
-import { AdsKeywordsService } from '@core/ads/services/ads-keywords/ads-keywords.service';
-import { DeviceService } from '@core/device/device.service';
-import { DeviceType } from '@core/device/deviceType.enum';
-import { WINDOW_TOKEN } from '@core/window/window.token';
-import {
-  MockAdShoppingPageOptions,
-  MockAdsKeywords,
-  MockAdsKeywordsService,
-  MockAdSlots,
-  MockAdSlotShopping,
-} from '@fixtures/ads.fixtures.spec';
 import { random } from 'faker';
-import { MOCK_GOOGLE_TAG } from './../../../../../configs/jest/global-mocks.fixtures.spec';
 import { YieldBirdService } from './yieldbird.service';
 
 describe('YieldBirdService', () => {
   let service: YieldBirdService;
-  let windowMock;
 
   beforeEach(() => {
-    windowMock = {
-      googletag: MOCK_GOOGLE_TAG,
-    };
-
     TestBed.configureTestingModule({
-      providers: [
-        YieldBirdService,
-        {
-          provide: WINDOW_TOKEN,
-          useValue: windowMock,
-        },
-        {
-          provide: AdsKeywordsService,
-          useValue: MockAdsKeywordsService,
-        },
-      ],
+      providers: [YieldBirdService],
     });
     service = TestBed.inject(YieldBirdService);
   });
