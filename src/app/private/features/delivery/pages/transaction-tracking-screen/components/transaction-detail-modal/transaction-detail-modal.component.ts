0780ca593bd2759513d30111cf19fd8fea79d216
Laia Lopez <laialopezz99@gmail.com>
2021-11-23 16:19:23 +0100

    [WPA-7237]: [TransactionDetailModalComponent] Created

---
+++ src/app/private/features/delivery/pages/transaction-tracking-screen/components/transaction-detail-modal/transaction-detail-modal.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { TransactionDetail } from '../../interfaces/transaction-detail.interface';
+
+@Component({
+  selector: 'tsl-transaction-detail-modal',
+  templateUrl: './transaction-detail-modal.component.html',
+  styleUrls: ['./transaction-detail-modal.component.scss'],
+})
+export class TransactionDetailModalComponent implements OnInit {
+  @Input() transactionDetail: TransactionDetail;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public openModal() {}
+}
