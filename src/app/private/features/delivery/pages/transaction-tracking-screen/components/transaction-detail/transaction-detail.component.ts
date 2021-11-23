d03a3783fdce8332a9dbcc87ae465fcdc0fdddb5
Laia Lopez <laialopezz99@gmail.com>
2021-11-23 15:55:37 +0100

    [WPA-7237]: [Refactor] TransactionDetailComponent

--- src/app/private/features/delivery/pages/transaction-tracking-screen/components/transaction-detail/transaction-detail.component.ts
+++ src/app/private/features/delivery/pages/transaction-tracking-screen/components/transaction-detail/transaction-detail.component.ts
@@ -1,5 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { Component, Input } from '@angular/core';
 import { FALLBACK_NOT_FOUND_SRC } from '../../constants/fallback-images-src-constants';
 import { TransactionDetail } from '../../interfaces/transaction-detail.interface';
 @Component({
@@ -7,25 +6,11 @@ import { TransactionDetail } from '../../interfaces/transaction-detail.interface
   templateUrl: './transaction-detail.component.html',
   styleUrls: ['./transaction-detail.component.scss'],
 })
-export class TransactionDetailComponent implements OnInit {
+export class TransactionDetailComponent {
   @Input() transactionDetail: TransactionDetail;
-  @Input() isClickableAction: boolean;
   @Input() isBorderBottom: boolean;
-  @Output() caretClick: EventEmitter<void> = new EventEmitter<void>();
-  @Output() actionClick: EventEmitter<void> = new EventEmitter<void>();
 
   public readonly FALLBACK_NOT_FOUND_SRC = FALLBACK_NOT_FOUND_SRC;
-  public descriptionByPass: SafeHtml;
 
-  constructor(private sanitizer: DomSanitizer) {}
-
-  ngOnInit() {
-    this.descriptionByPass = this.sanitizer.bypassSecurityTrustHtml(this.transactionDetail.description);
-  }
-
-  public emitActionClick(): void {
-    if (this.isClickableAction) {
-      this.actionClick.emit();
-    }
-  }
+  constructor() {}
 }
