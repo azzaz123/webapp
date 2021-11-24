bc70d66aef63d07f1c4aaddafff13ecd7fa8bd57
Laia Lopez <laialopezz99@gmail.com>
2021-11-24 12:04:04 +0100

    [WPA-7237]: [Testing] TransactionTrackingDetailInfoComponent

--- src/app/private/features/delivery/pages/transaction-tracking-screen/components/sections/transaction-tracking-detail-info/transaction-tracking-detail-info.component.spec.ts
+++ src/app/private/features/delivery/pages/transaction-tracking-screen/components/sections/transaction-tracking-detail-info/transaction-tracking-detail-info.component.spec.ts
@@ -1,24 +1,39 @@
-import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { MOCK_TRANSACTION_TRACKING_DETAILS } from '@api/fixtures/core/model/transaction/tracking/transaction-tracking-details.fixtures.spec';
+import { SITE_URL } from '@configs/site-url.config';
 import { MOCK_TRANSACTION_DETAILS } from '@fixtures/private/delivery/transactional-tracking-screen/transaction-details.fixtures.spec';
-import { TransactionTrackingActionsService } from '@private/features/delivery/services/transaction-tracking/transaction-tracking-actions/transaction-tracking-actions.service';
+import { MOCK_SITE_URL } from '@fixtures/site-url.fixtures.spec';
 import { ImageFallbackModule } from '@public/core/directives/image-fallback/image-fallback.module';
-import { TransactionDetailComponent } from '../../transaction-details/transaction-detail/transaction-detail.component';
+import { ItemDetailRouteModule, UserProfileRouteModule } from '@shared/pipes';
+import { BypassHTMLModule } from '@shared/pipes/bypass-html/bypass-html.module';
+import { TransactionTrackingDeeplinkModule } from '../../../pipes/transaction-tracking-deeplink.module';
+import { TransactionDetailModalComponent } from '../../transaction-details/transaction-detail-modal/transaction-detail-modal.component';
+import { TransactionDetailRedirectionComponent } from '../../transaction-details/transaction-detail-redirection/transaction-detail-redirection.component';
+import { TransactionDetailWithoutActionComponent } from '../../transaction-details/transaction-detail-without-action/transaction-detail-without-action.component';
 
 import { TransactionTrackingDetailInfoComponent } from './transaction-tracking-detail-info.component';
 
 describe('TransactionTrackingDetailInfoComponent', () => {
   let component: TransactionTrackingDetailInfoComponent;
   let fixture: ComponentFixture<TransactionTrackingDetailInfoComponent>;
-  let transactionTrackingActionsService: TransactionTrackingActionsService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [TransactionTrackingDetailInfoComponent, TransactionDetailComponent],
-      imports: [ImageFallbackModule],
-      providers: [{ provide: TransactionTrackingActionsService, useValue: { manageAction() {} } }],
+      declarations: [
+        TransactionTrackingDetailInfoComponent,
+        TransactionDetailWithoutActionComponent,
+        TransactionDetailModalComponent,
+        TransactionDetailRedirectionComponent,
+      ],
+      imports: [ImageFallbackModule, TransactionTrackingDeeplinkModule, ItemDetailRouteModule, UserProfileRouteModule, BypassHTMLModule],
+      providers: [
+        {
+          provide: SITE_URL,
+          useValue: MOCK_SITE_URL,
+        },
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   });
@@ -26,7 +41,6 @@ describe('TransactionTrackingDetailInfoComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TransactionTrackingDetailInfoComponent);
     component = fixture.componentInstance;
-    transactionTrackingActionsService = TestBed.inject(TransactionTrackingActionsService);
     component.transactionTrackingDetails = MOCK_TRANSACTION_TRACKING_DETAILS;
     fixture.detectChanges();
   });
@@ -36,28 +50,33 @@ describe('TransactionTrackingDetailInfoComponent', () => {
   });
 
   describe('when getting transaction tracking details...', () => {
-    it('should have the same transaction tracking detail info as slots', () => {
-      const detailInfoSlots = fixture.debugElement.queryAll(By.directive(TransactionDetailComponent));
-      expect(detailInfoSlots.length).toEqual(MOCK_TRANSACTION_TRACKING_DETAILS.info.length);
-    });
-
     describe('and we get the detail info slots', () => {
-      it('should return the transaction tracking status info mapped', () => {
+      it('should return the transaction tracking details mapped', () => {
         expect(component.detailInfoSlots).toStrictEqual(MOCK_TRANSACTION_DETAILS);
       });
     });
 
-    MOCK_TRANSACTION_TRACKING_DETAILS.info.forEach((action, index) => {
-      describe('and we click on the slot', () => {
-        it('should manage the provided action', () => {
-          spyOn(transactionTrackingActionsService, 'manageAction');
-          const slot: DebugElement = fixture.debugElement.queryAll(By.directive(TransactionDetailComponent))[index];
+    describe('and we show the slots', () => {
+      it('should show the components related to the action types', () => {
+        const transactionDetailWithoutAction = fixture.debugElement.queryAll(By.directive(TransactionDetailWithoutActionComponent)).length;
+        const transactionDetailModalAction = fixture.debugElement.queryAll(By.directive(TransactionDetailModalComponent)).length;
+        const transactionDetailRedirectionAction = fixture.debugElement.queryAll(
+          By.directive(TransactionDetailRedirectionComponent)
+        ).length;
 
-          slot.nativeElement.click();
+        const transactionDetailWithoutActionExpected = MOCK_TRANSACTION_DETAILS.filter(
+          (transactionDetail) => !transactionDetail.action
+        ).length;
+        const transactionDetailModalActionExpected = MOCK_TRANSACTION_DETAILS.filter(
+          (transactionDetail) => transactionDetail.action?.isDialog
+        ).length;
+        const transactionDetailRedirectionActionExpected = MOCK_TRANSACTION_DETAILS.filter(
+          (transactionDetail) => transactionDetail.action?.isDeeplink || transactionDetail.action?.isCarrierTrackingWebview
+        ).length;
 
-          expect(transactionTrackingActionsService.manageAction).toHaveBeenCalledTimes(1);
-          expect(transactionTrackingActionsService.manageAction).toHaveBeenCalledWith(action.action);
-        });
+        expect(transactionDetailWithoutAction).toEqual(transactionDetailWithoutActionExpected);
+        expect(transactionDetailModalAction).toEqual(transactionDetailModalActionExpected);
+        expect(transactionDetailRedirectionAction).toEqual(transactionDetailRedirectionActionExpected);
       });
     });
   });
--- src/tests/private/delivery/transactional-tracking-screen/transaction-details.fixtures.spec.ts
+++ src/tests/private/delivery/transactional-tracking-screen/transaction-details.fixtures.spec.ts
@@ -6,18 +6,41 @@ export const MOCK_TRANSACTION_DETAILS: TransactionDetail[] = [
     iconClassName: 'rounded',
     iconSrc: 'https://cdn-beta.wallapop.com/images/10420/35/gw/__/c10420p190584802/i421614104.jpg?pictureSize=W800',
     showCaret: true,
+    action: {
+      isCarrierTrackingWebview: false,
+      isDeeplink: true,
+      isDialog: false,
+      isDismiss: false,
+      isReload: false,
+      isUserAction: false,
+      payload: {
+        linkUrl: 'wallapop://i/190584802',
+      },
+    },
   },
   {
     description: '<span style="color: #AFB6B6">Total:</span><br>5.90€',
     iconClassName: null,
     iconSrc: 'https://prod-delivery-resources.wallapop.com/transaction-tracking-screen/transaction_tracking_details/price_element.png',
     showCaret: false,
+    action: null,
   },
   {
     description: '<span style="color: #AFB6B6">Vendido por:</span><br>Coccofresco F.',
     iconClassName: 'circle',
     iconSrc: 'https://cdn-beta.wallapop.com/images/13/19/so/__/c13p76921033/i416234105.jpg?pictureSize=W800',
     showCaret: true,
+    action: {
+      isCarrierTrackingWebview: false,
+      isDeeplink: true,
+      isDialog: false,
+      isDismiss: false,
+      isReload: false,
+      isUserAction: false,
+      payload: {
+        linkUrl: 'wallapop://p/76921033',
+      },
+    },
   },
   {
     description: '<span style="color: #AFB6B6">Dirección de envío:</span><br>1234 No FLoor<br>08018 Barcelona',
@@ -25,6 +48,7 @@ export const MOCK_TRANSACTION_DETAILS: TransactionDetail[] = [
     iconSrc:
       'https://prod-delivery-resources.wallapop.com/transaction-tracking-screen/transaction_tracking_details/shipping_address_element.png',
     showCaret: false,
+    action: null,
   },
   {
     description: '<span style="color: #AFB6B6">Método de cobro:</span><br>Monedero',
@@ -32,5 +56,6 @@ export const MOCK_TRANSACTION_DETAILS: TransactionDetail[] = [
     iconSrc:
       'https://prod-delivery-resources.wallapop.com/transaction-tracking-screen/transaction_tracking_details/payment_method_wallet_element.png',
     showCaret: false,
+    action: null,
   },
 ];
