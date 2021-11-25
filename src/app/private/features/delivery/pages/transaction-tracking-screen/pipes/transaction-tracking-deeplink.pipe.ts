6a6c00709d1b60dea0a4660022af00821dde36d4
Laia Lopez <laialopezz99@gmail.com>
2021-11-25 09:38:01 +0100

    [WPA-7237]: [Created + Tested] mapDeeplinkToCustomerHelpForm

--- src/app/private/features/delivery/pages/transaction-tracking-screen/pipes/transaction-tracking-deeplink.pipe.ts
+++ src/app/private/features/delivery/pages/transaction-tracking-screen/pipes/transaction-tracking-deeplink.pipe.ts
@@ -1,5 +1,6 @@
 import { Inject, LOCALE_ID, Pipe, PipeTransform } from '@angular/core';
 import { APP_LOCALE } from '@configs/subdomains.config';
+import { mapDeeplinkToCustomerHelpForm } from '@core/external-links/deeplink-to-customer-help-form/deeplink-to-customer-help-form.mapper';
 import { mapDeeplinkToCustomerHelp } from '@core/external-links/deeplink-to-customer-help/deeplink-to-customer-help.mapper';
 import { ItemDetailRoutePipe, UserProfileRoutePipe } from '@shared/pipes';
 
@@ -10,7 +11,8 @@ export class TransactionTrackingDeeplinkPipe implements PipeTransform {
   private readonly itemDeeplinkPrefix = 'wallapop://i/';
   private readonly userProfileDeeplinkPrefix = 'wallapop://p/';
   private readonly printableLabelPrefix = 'wallapop://trackinglabel?url=';
-  private readonly zendeskArticlePrefix = 'wallapop://customerSupport/faq/article?z=';
+  private readonly customerSupportArticlePrefix = 'wallapop://customerSupport/faq/article?z=';
+  private readonly customerSupportFormPrefix = 'wallapop://customerSupport/form?f=';
 
   constructor(
     @Inject(LOCALE_ID) private locale: APP_LOCALE,
@@ -22,12 +24,17 @@ export class TransactionTrackingDeeplinkPipe implements PipeTransform {
     const isItemURL = deeplink.startsWith(this.itemDeeplinkPrefix);
     const isUserURL = deeplink.startsWith(this.userProfileDeeplinkPrefix);
     const isPrintableLabelURL = deeplink.startsWith(this.printableLabelPrefix);
-    const isCustomerHelpURL = deeplink.startsWith(this.zendeskArticlePrefix);
+    const isCustomerHelpURL = deeplink.startsWith(this.customerSupportArticlePrefix);
+    const isCustomerHelpFormURL = deeplink.startsWith(this.customerSupportFormPrefix);
 
     let mappedURL: string = null;
 
     if (isCustomerHelpURL) {
       mappedURL = mapDeeplinkToCustomerHelp(this.locale, deeplink);
+    }
+
+    if (isCustomerHelpFormURL) {
+      mappedURL = mapDeeplinkToCustomerHelpForm(this.locale, deeplink);
     }
 
     if (isPrintableLabelURL) {
@@ -48,3 +55,8 @@ export class TransactionTrackingDeeplinkPipe implements PipeTransform {
     return mappedURL ?? deeplink;
   }
 }
+
+// BarcodeLabelDeeplink -> "wallapop://delivery/barcode?b=${deliveryTag.asString()}"
+
+// PackagingInstructionsDeeplink -> "wallapop://shipping/transactiontracking/instructions?request_id=$requestId&type=${Packaging.asString()}"
+// CheckDeliveryInstructionsDeeplink -> "wallapop://shipping/transactiontracking/instructions?request_id=$requestId&type=${CheckDelivery.asString()}"
