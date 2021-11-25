b4a3550e8a2ba0813b224b41901f82ac9bb24429
antoni-casas <toni.casas@wallapop.com>
2021-11-25 18:17:13 +0100

    [WPA-7549]: feat: Refactor TransactionTrackingActionDetail

--- src/app/api/core/model/delivery/transaction/tracking/interfaces/transaction-tracking-action-detail.interface.ts
+++ src/app/api/core/model/delivery/transaction/tracking/interfaces/transaction-tracking-action-detail.interface.ts
@@ -1,39 +1,61 @@
 import { TransactionTrackingBanner } from '@api/core/model/delivery/transaction/tracking';
 
-export interface TransactionTrackingActionDetail {
+export type TransactionTrackingActionDetail =
+  | TransactionTrackingActionCarrierTrackingWebview
+  | TransactionTrackingActionDeeplink
+  | TransactionTrackingActionDialog
+  | TransactionTrackingActionUserAction
+  | TransactionTrackingActionDismiss
+  | TransactionTrackingActionReload;
+
+export interface TransactionTrackingActionBase {
   analytics?: TransactionTrackingActionDetailAnalytics;
+}
+
+export interface TransactionTrackingActionCarrierTrackingWebview extends TransactionTrackingActionBase {
   isCarrierTrackingWebview: boolean;
+  linkUrl: string;
+  title: string;
+}
+
+export interface TransactionTrackingActionDeeplink extends TransactionTrackingActionBase {
   isDeeplink: boolean;
+  linkUrl: string;
+}
+
+export interface TransactionTrackingActionDialog extends TransactionTrackingActionBase {
+  description: string;
   isDialog: boolean;
-  isDismiss: boolean;
-  isReload: boolean;
+  negative: TransactionTrackingActionNegative;
+  positive: TransactionTrackingActionPositive;
+  title: string;
+}
+
+export interface TransactionTrackingActionUserAction extends TransactionTrackingActionBase {
   isUserAction: boolean;
-  payload: TransactionTrackingActionDetailPayload;
+  name: string;
+  success: TransactionTrackingActionDetail;
+  transactionId: string;
+}
+
+export interface TransactionTrackingActionDismiss extends TransactionTrackingActionBase {
+  isDismiss: boolean;
+}
+
+export interface TransactionTrackingActionReload extends TransactionTrackingActionBase {
+  isReload: boolean;
+}
+
+export interface TransactionTrackingActionNegative {
+  title: string;
+}
+
+export interface TransactionTrackingActionPositive extends TransactionTrackingActionNegative {
+  action: TransactionTrackingActionDetail;
 }
 
 export interface TransactionTrackingActionDetailAnalytics {
   requestId: string;
   source: string;
   userId: string;
-}
-
-export interface TransactionTrackingActionDetailPayload {
-  banner?: TransactionTrackingBanner;
-  description?: string;
-  linkUrl?: string;
-  name?: string;
-  negative?: TransactionTrackingActionDetailPayloadConfirmation;
-  parameters?: TransactionTrackingActionDetailPayloadParameters;
-  positive?: TransactionTrackingActionDetailPayloadConfirmation;
-  success?: TransactionTrackingActionDetail;
-  title?: string;
-}
-
-export interface TransactionTrackingActionDetailPayloadConfirmation {
-  action?: TransactionTrackingActionDetail;
-  title: string;
-}
-
-export interface TransactionTrackingActionDetailPayloadParameters {
-  transactionId: string;
 }
