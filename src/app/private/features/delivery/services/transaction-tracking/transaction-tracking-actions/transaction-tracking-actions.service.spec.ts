22960fa9c73a9789d5f4280b08bb947e3771ae34
antoni-casas <toni.casas@wallapop.com>
2021-11-26 09:48:35 +0100

    [WPA-7549]: chore: Fix UI service

--- src/app/private/features/delivery/services/transaction-tracking/transaction-tracking-actions/transaction-tracking-actions.service.spec.ts
+++ src/app/private/features/delivery/services/transaction-tracking/transaction-tracking-actions/transaction-tracking-actions.service.spec.ts
@@ -1,5 +1,6 @@
 import { LOCALE_ID } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { TransactionTrackingActionDeeplink } from '@api/core/model/delivery/transaction/tracking';
 import { MOCK_TRANSACTION_TRACKING } from '@api/fixtures/core/model/transaction/tracking/transaction-tracking.fixtures.spec';
 import { mapDeeplinkToCustomerHelp } from '@core/external-links/deeplink-to-customer-help/deeplink-to-customer-help.mapper';
 import { APP_LOCALE_MOCK } from '@fixtures/analytics.fixtures.spec';
@@ -7,8 +8,8 @@ import { APP_LOCALE_MOCK } from '@fixtures/analytics.fixtures.spec';
 import { TransactionTrackingActionsService } from './transaction-tracking-actions.service';
 
 describe('TransactionTrackingActionsService', () => {
-  const MOCK_DEEP_LINK_ACTION = MOCK_TRANSACTION_TRACKING.header.detail.action;
-  const MOCK_CARRIER_TRACKING_WEBVIEW_ACTION = MOCK_TRANSACTION_TRACKING.statusInfo[0].action;
+  const MOCK_DEEP_LINK_ACTION = MOCK_TRANSACTION_TRACKING.header.detail.action as TransactionTrackingActionDeeplink;
+  const MOCK_CARRIER_TRACKING_WEBVIEW_ACTION = MOCK_TRANSACTION_TRACKING.statusInfo[0].action as TransactionTrackingActionDeeplink;
 
   let service: TransactionTrackingActionsService;
 
@@ -32,7 +33,7 @@ describe('TransactionTrackingActionsService', () => {
   describe('when we handle a transaction tracking action...', () => {
     describe('and the action is a deeplink', () => {
       it('should open the mapped url in a new page', () => {
-        const deeplinkURLMapped = mapDeeplinkToCustomerHelp(APP_LOCALE_MOCK, MOCK_DEEP_LINK_ACTION.payload.linkUrl);
+        const deeplinkURLMapped = mapDeeplinkToCustomerHelp(APP_LOCALE_MOCK, MOCK_DEEP_LINK_ACTION.linkUrl);
         spyOn(window, 'open');
 
         service.manageAction(MOCK_DEEP_LINK_ACTION);
@@ -49,7 +50,7 @@ describe('TransactionTrackingActionsService', () => {
         service.manageAction(MOCK_CARRIER_TRACKING_WEBVIEW_ACTION);
 
         expect(window.open).toHaveBeenCalledTimes(1);
-        expect(window.open).toHaveBeenCalledWith(MOCK_CARRIER_TRACKING_WEBVIEW_ACTION.payload.linkUrl, '_blank');
+        expect(window.open).toHaveBeenCalledWith(MOCK_CARRIER_TRACKING_WEBVIEW_ACTION.linkUrl, '_blank');
       });
     });
   });
