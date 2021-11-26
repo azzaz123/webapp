1c2bde10ac45d610e1f5d37ac53d94152e698a3b
antoni-casas <toni.casas@wallapop.com>
2021-11-26 12:16:12 +0100

    [WPA-7655]: feat: WIP

--- src/app/api/core/model/delivery/transaction/tracking/models/action/transaction-tracking-action-deeplink.model.ts
+++ src/app/api/core/model/delivery/transaction/tracking/models/action/transaction-tracking-action-deeplink.model.ts
@@ -5,9 +5,16 @@ import {
 } from '@api/core/model/delivery/transaction/tracking';
 import {
   TransactionTrackingActionDetailDto,
-  TransactionTrackingActionDetailPayloadDeeplinkDto,
   TransactionTrackingActionDetailPayloadDto,
 } from '@api/bff/delivery/transaction-tracking/dtos/responses';
+import { Injector } from '@angular/core';
+import { ItemDetailRoutePipe, UserProfileRoutePipe } from '@shared/pipes';
+
+const itemDeeplinkPrefix = 'wallapop://i/';
+const userProfileDeeplinkPrefix = 'wallapop://p/';
+const printableLabelPrefix = 'wallapop://trackinglabel?url=';
+const customerSupportArticlePrefix = 'wallapop://customerSupport/faq/article?z=';
+const customerSupportFormPrefix = 'wallapop://customerSupport/form?f=';
 
 export class TransactionTrackingActionDeeplinkModel implements TransactionTrackingActionDeeplink {
   public analytics?: TransactionTrackingActionDetailAnalytics;
@@ -21,7 +28,41 @@ export class TransactionTrackingActionDeeplinkModel implements TransactionTracki
     this.linkUrl = this.getLinkUrl(actionDetailDto.payload);
   }
 
+  private convertToWebLink(deeplink: string): string {
+    const isItemURL = deeplink.startsWith(itemDeeplinkPrefix);
+    const isUserURL = deeplink.startsWith(userProfileDeeplinkPrefix);
+    const isPrintableLabelURL = deeplink.startsWith(printableLabelPrefix);
+    const isCustomerHelpURL = deeplink.startsWith(customerSupportArticlePrefix);
+    const isCustomerHelpFormURL = deeplink.startsWith(customerSupportFormPrefix);
+    let mappedURL: string = null;
+
+    if (isCustomerHelpURL) {
+      // mappedURL = mapDeeplinkToCustomerHelp(this.locale, deeplink);
+    }
+    if (isCustomerHelpFormURL) {
+      // mappedURL = mapDeeplinkToCustomerHelpForm(this.locale, deeplink);
+    }
+    if (isPrintableLabelURL) {
+      mappedURL = deeplink.split(printableLabelPrefix)[1];
+    }
+    if (isItemURL) {
+      const id = deeplink.split(itemDeeplinkPrefix)[1];
+      const injector: Injector = Injector.create({ providers: [{ provide: ItemDetailRoutePipe }, { provide: UserProfileRoutePipe }] });
+      const itemDetailRoutePipe = injector.get(ItemDetailRoutePipe);
+
+      mappedURL = itemDetailRoutePipe.transform(id);
+    }
+    if (isUserURL) {
+      // askSlug
+      const injector: Injector = Injector.create({ providers: [{ provide: ItemDetailRoutePipe }, { provide: UserProfileRoutePipe }] });
+      const userProfileRoutePipe = injector.get(UserProfileRoutePipe);
+      const id = deeplink.split(userProfileDeeplinkPrefix)[1];
+      mappedURL = userProfileRoutePipe.transform('', id);
+    }
+    return mappedURL;
+  }
+
   private getLinkUrl(actionDetailPayloadDto: TransactionTrackingActionDetailPayloadDto): string {
-    return (actionDetailPayloadDto as TransactionTrackingActionDetailPayloadDeeplinkDto).link_url ?? undefined;
+    return this.convertToWebLink((actionDetailPayloadDto as TransactionTrackingActionDeeplink).linkUrl) ?? undefined;
   }
 }
