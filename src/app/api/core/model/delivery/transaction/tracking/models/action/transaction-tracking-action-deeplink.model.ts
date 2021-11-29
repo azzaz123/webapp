cbc9e18cb61aedf7cb22d66688c2224285a400e6
antoni-casas <toni.casas@wallapop.com>
2021-11-29 17:59:59 +0100

    [WPA-7655]: feat: refactor action deeplink model

--- src/app/api/core/model/delivery/transaction/tracking/models/action/transaction-tracking-action-deeplink.model.ts
+++ src/app/api/core/model/delivery/transaction/tracking/models/action/transaction-tracking-action-deeplink.model.ts
@@ -1,3 +1,6 @@
+import { Injector } from '@angular/core';
+
+import { DeeplinkService } from '@api/core/utils/deeplink/deeplink.service';
 import {
   TransactionTrackingActionAnalyticsModel,
   TransactionTrackingActionDeeplink,
@@ -5,64 +8,36 @@ import {
 } from '@api/core/model/delivery/transaction/tracking';
 import {
   TransactionTrackingActionDetailDto,
+  TransactionTrackingActionDetailPayloadDeeplinkDto,
   TransactionTrackingActionDetailPayloadDto,
 } from '@api/bff/delivery/transaction-tracking/dtos/responses';
-import { Injector } from '@angular/core';
-import { ItemDetailRoutePipe, UserProfileRoutePipe } from '@shared/pipes';
-
-const itemDeeplinkPrefix = 'wallapop://i/';
-const userProfileDeeplinkPrefix = 'wallapop://p/';
-const printableLabelPrefix = 'wallapop://trackinglabel?url=';
-const customerSupportArticlePrefix = 'wallapop://customerSupport/faq/article?z=';
-const customerSupportFormPrefix = 'wallapop://customerSupport/form?f=';
 
 export class TransactionTrackingActionDeeplinkModel implements TransactionTrackingActionDeeplink {
   public analytics?: TransactionTrackingActionDetailAnalytics;
   public isDeeplink: boolean = true;
   public linkUrl: string;
 
+  private deeplinkService: DeeplinkService;
+
   constructor(actionDetailDto: TransactionTrackingActionDetailDto) {
     if (!!actionDetailDto.analytics) {
       this.analytics = new TransactionTrackingActionAnalyticsModel(actionDetailDto.analytics);
     }
+    this.createDeeplinkService();
     this.linkUrl = this.getLinkUrl(actionDetailDto.payload);
   }
 
-  private convertToWebLink(deeplink: string): string {
-    const isItemURL = deeplink.startsWith(itemDeeplinkPrefix);
-    const isUserURL = deeplink.startsWith(userProfileDeeplinkPrefix);
-    const isPrintableLabelURL = deeplink.startsWith(printableLabelPrefix);
-    const isCustomerHelpURL = deeplink.startsWith(customerSupportArticlePrefix);
-    const isCustomerHelpFormURL = deeplink.startsWith(customerSupportFormPrefix);
-    let mappedURL: string = null;
+  private createDeeplinkService(): void {
+    const injector = Injector.create({
+      providers: [{ provide: DeeplinkService }],
+    });
 
-    if (isCustomerHelpURL) {
-      // mappedURL = mapDeeplinkToCustomerHelp(this.locale, deeplink);
-    }
-    if (isCustomerHelpFormURL) {
-      // mappedURL = mapDeeplinkToCustomerHelpForm(this.locale, deeplink);
-    }
-    if (isPrintableLabelURL) {
-      mappedURL = deeplink.split(printableLabelPrefix)[1];
-    }
-    if (isItemURL) {
-      const id = deeplink.split(itemDeeplinkPrefix)[1];
-      const injector: Injector = Injector.create({ providers: [{ provide: ItemDetailRoutePipe }, { provide: UserProfileRoutePipe }] });
-      const itemDetailRoutePipe = injector.get(ItemDetailRoutePipe);
-
-      mappedURL = itemDetailRoutePipe.transform(id);
-    }
-    if (isUserURL) {
-      // askSlug
-      const injector: Injector = Injector.create({ providers: [{ provide: ItemDetailRoutePipe }, { provide: UserProfileRoutePipe }] });
-      const userProfileRoutePipe = injector.get(UserProfileRoutePipe);
-      const id = deeplink.split(userProfileDeeplinkPrefix)[1];
-      mappedURL = userProfileRoutePipe.transform('', id);
-    }
-    return mappedURL;
+    this.deeplinkService = injector.get(DeeplinkService);
   }
 
   private getLinkUrl(actionDetailPayloadDto: TransactionTrackingActionDetailPayloadDto): string {
-    return this.convertToWebLink((actionDetailPayloadDto as TransactionTrackingActionDeeplink).linkUrl) ?? undefined;
+    return (
+      this.deeplinkService.toWebLink((actionDetailPayloadDto as TransactionTrackingActionDetailPayloadDeeplinkDto).link_url) ?? undefined
+    );
   }
 }
