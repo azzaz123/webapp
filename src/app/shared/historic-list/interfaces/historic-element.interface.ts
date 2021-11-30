06551e8752e2b055bd7c52ae76f0c6268732ece7
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-11-30 10:58:16 +0100

    [WPA-5357]: Strongly type historic element id

--- src/app/shared/historic-list/interfaces/historic-element.interface.ts
+++ src/app/shared/historic-list/interfaces/historic-element.interface.ts
@@ -1,8 +1,9 @@
 import { Money } from '@api/core/model/money.interface';
 import { HISTORIC_ELEMENT_SUBDESCRIPTION_TYPE } from '../enums/historic-element-subdescription-type.enum';
+import { HistoricElementId } from '../types/historic-element-id.type';
 
 export interface HistoricElement<T = Object> {
-  id: string;
+  id: HistoricElementId;
   imageUrl: string;
   iconUrl: string;
   title: string;
