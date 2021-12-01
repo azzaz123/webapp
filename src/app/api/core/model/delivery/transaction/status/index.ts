e041a22a662bc01cf8ab22fc61861657e41eaa22
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-12-01 17:34:08 +0100

    [WPA-5321]: Create Completed transaction tracking status

--- src/app/api/core/model/delivery/transaction/status/index.ts
+++ src/app/api/core/model/delivery/transaction/status/index.ts
@@ -1,3 +1,4 @@
+export * from './completed-transaction-tracking-status.enum';
 export * from './transaction-delivery-status.enum';
 export * from './transaction-payment-status.enum';
 export * from './transaction-status.enum';
