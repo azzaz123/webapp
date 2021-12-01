c4e5b116bd85ff4df168fecd0dc4a0681c5f4ef0
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2021-12-01 18:19:14 +0100

    [WPA-5321]: Add Trackeable Transaction interface

--- src/app/api/core/model/delivery/transaction/pending-transaction.interface.ts
+++ src/app/api/core/model/delivery/transaction/pending-transaction.interface.ts
@@ -1,7 +1,8 @@
 import { TRANSACTION_STATUS, TRANSACTION_DELIVERY_STATUS, TRANSACTION_PAYMENT_STATUS, ONGOING_TRANSACTION_TRACKING_STATUS } from './status';
+import { TrackeableTransaction } from './trackeable-transaction.interface';
 import { Transaction } from './transaction.interface';
 
-export interface PendingTransaction extends Transaction {
+export interface PendingTransaction extends Transaction, TrackeableTransaction<ONGOING_TRANSACTION_TRACKING_STATUS> {
   status: {
     transaction: TRANSACTION_STATUS.PENDING;
     delivery: TRANSACTION_DELIVERY_STATUS;
