04c644a325fb3962ddc548f4920012bfee746f7b
Robert Vera <robertveraroige@gmail.com>
2021-12-02 15:39:10 +0100

    [WPA-7267]: implement sentry performance

--- src/app/core/monitoring/services/monitoring.service.ts
+++ src/app/core/monitoring/services/monitoring.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
-import * as Sentry from '@sentry/angular';
+import * as Sentry from '@sentry/browser';
+import { Integrations as TracingIntegrations } from '@sentry/tracing';
 
 export const SENTRY_DSN = 'https://1d0e1781157041509326a1072f4ad490@o608018.ingest.sentry.io/5839391';
-export const TRACES_SAMPLE_RATE = 0.1;
+export const TRACES_SAMPLE_RATE = 1;
 export const WALLAPOP_DOMAIN_REGEX = /https?:\/\/it\.wallapop\.com/;
 
 @Injectable({
@@ -13,13 +14,21 @@ export class MonitoringService {
   constructor() {}
 
   public initialize(): void {
-    if (environment.production) {
-      Sentry.init({
-        environment: environment.name,
-        dsn: SENTRY_DSN,
-        tracesSampleRate: TRACES_SAMPLE_RATE,
-        allowUrls: [WALLAPOP_DOMAIN_REGEX],
-      });
-    }
+    // if (environment.production) {
+    //   Sentry.init({
+    //     environment: environment.name,
+    //     dsn: SENTRY_DSN,
+    //     integrations: [new TracingIntegrations.BrowserTracing()],
+    //     tracesSampleRate: TRACES_SAMPLE_RATE,
+    //     allowUrls: [WALLAPOP_DOMAIN_REGEX],
+    //   });
+    // }
+    Sentry.init({
+      environment: environment.name,
+      dsn: SENTRY_DSN,
+      integrations: [new TracingIntegrations.BrowserTracing()],
+      tracesSampleRate: TRACES_SAMPLE_RATE,
+      allowUrls: [WALLAPOP_DOMAIN_REGEX],
+    });
   }
 }
