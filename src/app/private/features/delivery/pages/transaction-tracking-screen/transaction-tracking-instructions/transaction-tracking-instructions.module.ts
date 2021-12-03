43ed33c6b0347561c317be51542fc2381bbba16e
antoni-casas <toni.casas@wallapop.com>
2021-12-03 11:36:53 +0100

    [WPA-7214]: feat: Add body to instructions component

--- src/app/private/features/delivery/pages/transaction-tracking-screen/transaction-tracking-instructions/transaction-tracking-instructions.module.ts
+++ src/app/private/features/delivery/pages/transaction-tracking-screen/transaction-tracking-instructions/transaction-tracking-instructions.module.ts
@@ -1,18 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { BypassHTMLModule } from '@shared/pipes/bypass-html/bypass-html.module';
+import { TransactionTrackingActionSelectorModule } from '@private/features/delivery/pages/transaction-tracking-screen/components/transaction-tracking-action-details/transaction-tracking-action-selector/transaction-tracking-action-selector.module';
 import { TransactionTrackingBannerModule } from '@private/features/delivery/pages/transaction-tracking-screen';
+import { TransactionTrackingHeaderModule } from '@private/features/delivery/pages/transaction-tracking-screen/components/sections';
+import { TransactionTrackingHttpService } from '@api/bff/delivery/transaction-tracking/http/transaction-tracking-http.service';
 import {
   transactionTrackingInstructionsRoutedComponents,
   TransactionTrackingInstructionsRoutingModule,
 } from '@private/features/delivery/pages/transaction-tracking-screen/transaction-tracking-instructions/transaction-tracking-instructions.routing.module';
-import { TransactionTrackingHeaderModule } from '@private/features/delivery/pages/transaction-tracking-screen/components/sections';
-import { TransactionTrackingHttpService } from '@api/bff/delivery/transaction-tracking/http/transaction-tracking-http.service';
 import { TransactionTrackingService } from '@api/bff/delivery/transaction-tracking/transaction-tracking.service';
 
 @NgModule({
   declarations: [transactionTrackingInstructionsRoutedComponents],
-  imports: [CommonModule, TransactionTrackingBannerModule, TransactionTrackingHeaderModule, TransactionTrackingInstructionsRoutingModule],
+  imports: [
+    BypassHTMLModule,
+    CommonModule,
+    TransactionTrackingActionSelectorModule,
+    TransactionTrackingBannerModule,
+    TransactionTrackingHeaderModule,
+    TransactionTrackingInstructionsRoutingModule,
+  ],
   providers: [TransactionTrackingService, TransactionTrackingHttpService],
 })
 export class TransactionTrackingInstructionsModule {}
