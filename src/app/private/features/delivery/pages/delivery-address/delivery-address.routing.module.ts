ff3a5d39c2492286b7914e44706d605c9841ae1c
Laia Lopez <laialopezz99@gmail.com>
2021-12-09 11:50:19 +0100

    [WPA-7723]: [Created] DeliveryAddressResolver

--- src/app/private/features/delivery/pages/delivery-address/delivery-address.routing.module.ts
+++ src/app/private/features/delivery/pages/delivery-address/delivery-address.routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
 import { ExitConfirmGuard } from '@core/guards/exit-confirm.guard';
 import { DeliveryAddressComponent } from './delivery-address.component';
+import { DeliveryAddressResolver } from './resolvers/delivery-address.resolver';
 
 const routes: Route[] = [
   {
     path: '',
     component: DeliveryAddressComponent,
     canDeactivate: [ExitConfirmGuard],
+    resolve: {
+      hero: DeliveryAddressResolver,
+    },
   },
 ];
 
--- src/tests/private/delivery/delivery-countries.fixtures.spec.ts
+++ src/tests/private/delivery/delivery-countries.fixtures.spec.ts
@@ -8,10 +8,20 @@ export const MOCK_DELIVERY_COUNTRIES_API: DeliveryCountriesApi = {
     {
       iso_code: 'ES',
       label: 'Spain',
+      address_restrictions: {
+        name: 12,
+        street: 33,
+        flat_and_floor: 3,
+      },
     },
     {
       iso_code: 'IT',
       label: 'Italy',
+      address_restrictions: {
+        name: 11,
+        street: 41,
+        flat_and_floor: 50,
+      },
     },
   ],
   default: {
@@ -24,13 +34,23 @@ export const MOCK_DELIVERY_COUNTRIES_OPTIONS_AND_DEFAULT: CountryOptionsAndDefau
     {
       label: 'Spain',
       value: 'ES',
+      addressFormRestrictions: {
+        name: 5,
+        street: 4,
+        flat_and_floor: 10,
+      },
     },
     {
       label: 'Italy',
       value: 'IT',
+      addressFormRestrictions: {
+        name: 5,
+        street: 4,
+        flat_and_floor: 10,
+      },
     },
   ],
   defaultCountry: {
-    iso_code: 'ES',
+    isoCode: 'ES',
   },
 };
