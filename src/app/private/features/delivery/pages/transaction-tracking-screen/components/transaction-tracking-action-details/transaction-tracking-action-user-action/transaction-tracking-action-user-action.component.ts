37ab8310bdd75f592274b586dc4c00c862dfdb32
Laia Lopez <laialopezz99@gmail.com>
2021-12-09 10:04:11 +0100

    [WPA-3788]: [Event tracking] TransactionTrackingActionUserAction

--- src/app/private/features/delivery/pages/transaction-tracking-screen/components/transaction-tracking-action-details/transaction-tracking-action-user-action/transaction-tracking-action-user-action.component.ts
+++ src/app/private/features/delivery/pages/transaction-tracking-screen/components/transaction-tracking-action-details/transaction-tracking-action-user-action/transaction-tracking-action-user-action.component.ts
@@ -1,8 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { TransactionTrackingService } from '@api/bff/delivery/transaction-tracking/transaction-tracking.service';
 import { TransactionTrackingActionUserAction } from '@api/core/model/delivery/transaction/tracking';
 import { ErrorsService } from '@core/errors/errors.service';
 import { TRANSLATION_KEY } from '@core/i18n/translations/enum/translation-keys.enum';
+import {
+  ActionNameAnalytics,
+  TransactionTrackingScreenTrackingEventsService,
+} from '../../../services/transaction-tracking-screen-tracking-events/transaction-tracking-screen-tracking-events.service';
 
 @Component({
   selector: 'tsl-transaction-tracking-action-user-action',
@@ -12,14 +16,29 @@ import { TRANSLATION_KEY } from '@core/i18n/translations/enum/translation-keys.e
 export class TransactionTrackingActionUserActionComponent {
   @Input() userAction: TransactionTrackingActionUserAction;
 
-  constructor(private transactionTrackingService: TransactionTrackingService, private errorsService: ErrorsService) {}
+  constructor(
+    private transactionTrackingService: TransactionTrackingService,
+    private errorsService: ErrorsService,
+    private transactionTrackingScreenTrackingEventsService: TransactionTrackingScreenTrackingEventsService
+  ) {}
 
   public requestUserAction(): void {
+    this.trackEvent();
     this.transactionTrackingService.sendUserAction(this.userAction.transactionId, this.userAction.name).subscribe({
       error: () => {
         // TODO: Error management states should be improved by cases		Date: 2021/12/02
         this.errorsService.i18nError(TRANSLATION_KEY.DEFAULT_ERROR_MESSAGE);
       },
     });
+  }
+
+  public trackEvent(): void {
+    const analyticsEvent = this.userAction.analytics;
+    if (analyticsEvent) {
+      this.transactionTrackingScreenTrackingEventsService.trackClickActionTTS(
+        analyticsEvent.requestId,
+        analyticsEvent.source as ActionNameAnalytics
+      );
+    }
   }
 }
