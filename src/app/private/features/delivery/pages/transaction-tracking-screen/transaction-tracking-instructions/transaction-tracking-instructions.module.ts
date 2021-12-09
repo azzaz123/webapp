8543cbbcc4b690bd307505da3dda3f4e57594f89
antoni-casas <toni.casas@wallapop.com>
2021-12-09 15:36:47 +0100

    [WPA-7800]: style: Fix styles

--- src/app/private/features/delivery/pages/transaction-tracking-screen/transaction-tracking-instructions/transaction-tracking-instructions.module.ts
+++ src/app/private/features/delivery/pages/transaction-tracking-screen/transaction-tracking-instructions/transaction-tracking-instructions.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { ButtonModule } from '@shared/button/button.module';
 import { BypassHTMLModule } from '@shared/pipes/bypass-html/bypass-html.module';
 import { TransactionTrackingActionSelectorModule } from '@private/features/delivery/pages/transaction-tracking-screen/components/transaction-tracking-action-details/transaction-tracking-action-selector/transaction-tracking-action-selector.module';
 import { TransactionTrackingBannerModule } from '@private/features/delivery/pages/transaction-tracking-screen';
@@ -15,6 +16,7 @@ import { TransactionTrackingService } from '@api/bff/delivery/transaction-tracki
 @NgModule({
   declarations: [transactionTrackingInstructionsRoutedComponents],
   imports: [
+    ButtonModule,
     BypassHTMLModule,
     CommonModule,
     TransactionTrackingActionSelectorModule,
