25ee0c859b0912f1f41be1d514f055d0e1094ea1
Laia Lopez <laialopezz99@gmail.com>
2021-12-13 13:33:33 +0100

    [WPA-6631]: [Styling + lottie]

--- src/app/private/features/delivery/modals/trx-awareness-modal/trx-awareness-modal.component.ts
+++ src/app/private/features/delivery/modals/trx-awareness-modal/trx-awareness-modal.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trx-awareness-modal.component.scss'],
 })
 export class TRXAwarenessModalComponent implements OnInit {
+  public readonly TRX_AWARENESS_LOTTIE =
+    'https://prod-delivery-resources.s3.eu-west-1.amazonaws.com/transaction-tracking-screen/shipping_status_in_transit_animation.json';
+
   constructor() {}
 
   ngOnInit(): void {}
---
+++ src/app/private/features/delivery/modals/trx-awareness-modal/trx-awareness-modal.stories.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { Story, Meta } from '@storybook/angular/types-6-0';
+import { styledWrapperDecorator } from '@stories/decorators/styled-wrapper/styled-wrapper.decorator';
+import { TRXAwarenessModalComponent } from './trx-awareness-modal.component';
+import { LottieModule } from '@shared/lottie/lottie.module';
+import { ButtonModule } from '@shared/button/button.module';
+import { HttpClientModule } from '@angular/common/http';
+
+export default {
+  title: 'Webapp/Private/Features/Delivery/Modals/TRXAwarenessModal',
+  component: TRXAwarenessModalComponent,
+  decorators: [styledWrapperDecorator('max-width: 375px;')],
+} as Meta;
+
+const Template: Story<TRXAwarenessModalComponent> = (args: TRXAwarenessModalComponent) => ({
+  component: TRXAwarenessModalComponent,
+  props: args,
+  moduleMetadata: {
+    declarations: [TRXAwarenessModalComponent],
+    imports: [CommonModule, LottieModule, ButtonModule, HttpClientModule],
+  },
+  template: '<tsl-trx-awareness-modal></tsl-trx-awareness-modal>',
+});
+
+export const Default = Template.bind({});
+Default.args = {};
