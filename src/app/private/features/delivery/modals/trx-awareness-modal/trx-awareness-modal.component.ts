ff9ea8b42053e075f45c6f102447d1b78b379e87
Laia Lopez <laialopezz99@gmail.com>
2021-12-14 10:10:12 +0100

    [WPA-6631]: [TRXAwarenessModalComponent] onClose

--- src/app/private/features/delivery/modals/trx-awareness-modal/trx-awareness-modal.component.ts
+++ src/app/private/features/delivery/modals/trx-awareness-modal/trx-awareness-modal.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'tsl-trx-awareness-modal',
   templateUrl: './trx-awareness-modal.component.html',
   styleUrls: ['./trx-awareness-modal.component.scss'],
 })
-export class TRXAwarenessModalComponent implements OnInit {
+export class TRXAwarenessModalComponent {
   public readonly TRX_AWARENESS_LOTTIE =
     'https://prod-delivery-resources.s3.eu-west-1.amazonaws.com/transaction-tracking-screen/shipping_status_in_transit_animation.json';
 
-  constructor() {}
+  constructor(private activeModal: NgbActiveModal) {}
 
-  ngOnInit(): void {}
+  public onClose(): void {
+    this.activeModal.close();
+  }
 }
