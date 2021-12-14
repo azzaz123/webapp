d5a8d7f3d7c432c65a71d5073a25bd07741d9580
Laia Lopez <laialopezz99@gmail.com>
2021-12-14 18:08:23 +0100

    [WPA-7725]: [StreamlineModule] Updated

--- src/app/private/features/delivery/pages/streamline/streamline.module.ts
+++ src/app/private/features/delivery/pages/streamline/streamline.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TabsBarModule } from '@shared/tabs-bar/tabs-bar.module';
+import { StreamlineTrackingEventsService } from './services/streamline-tracking-events/streamline-tracking-events.service';
 import { streamlineRoutedComponents, StreamlineRoutingModule } from './streamline.routing.module';
 
 @NgModule({
   imports: [StreamlineRoutingModule, TabsBarModule, RouterModule],
   declarations: [streamlineRoutedComponents],
+  providers: [StreamlineTrackingEventsService],
 })
 export class StreamlineModule {}
