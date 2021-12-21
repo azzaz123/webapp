31fdd1b8bdab9762e0fd41037ee6481b0b9d5bb4
ALBERTO <alberto.ayllon@wallapop.com>
2021-12-21 20:52:13 +0100

    Fix wrong url for mongoose in Dockx environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ const environmentLocal: Environment = {
   protocol: 'https://',
   baseUrl: `https://apigw.dock${dockNum}.devel.wallapop.com/`,
   xmppDomain: `dock${dockNum}.devel.wallapop.com`,
-  wsUrl: `wss://dock${dockNum}.devel.wallapop.com:5282/ws-xmpp`,
+  wsUrl: `wss://mongooseimprotool-${docknum}.wallapop.com:5282/ws-xmpp`,
   appboy: '516bd193-25b2-48b9-b79d-f8516f104d2f',
   cookieSuffix: 'Local',
   clickStreamURL: 'https://precollector.wallapop.com/clickstream.json/sendEvents',
