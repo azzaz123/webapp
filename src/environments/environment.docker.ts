9cabe5a7420d5ed6a3bdc6ab6d74cc241b24c70b
Fabio Perrone <fabio.perrone@wallapop.com>
2021-12-28 19:16:08 +0100

    fix duplicate entries in environment.docker.ts

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -26,8 +26,6 @@ export const environment: Environment = {
   baseUrl: `https://apigw.${docknum}.devel.wallapop.com/`,
   xmppDomain: `mongooseimprotool-${docknum}.wallapop.com`,
   wsUrl: `wss://mongooseimprotool-${docknum}.wallapop.com:5282/ws-xmpp`,
-  xmppDomain: `${docknum}.wallapop.com`,
-  wsUrl: `wss://${docknum}.wallapop.com:5282/ws-xmpp`,
   appboy: '516bd193-25b2-48b9-b79d-f8516f104d2f',
   cookieSuffix: `${docknum}`,
   clickStreamURL: 'https://precollector.wallapop.com/clickstream.json/sendEvents',
