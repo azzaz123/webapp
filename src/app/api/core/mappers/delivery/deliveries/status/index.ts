4ae5ff2f754f9c1ba785ad5a016e4082f84d2cc8
Laia Lopez <laialopezz99@gmail.com>
2022-01-04 16:11:59 +0100

    [WPA-8451]: [State mapper] Created

--- src/app/api/core/mappers/delivery/deliveries/status/index.ts
+++ src/app/api/core/mappers/delivery/deliveries/status/index.ts
@@ -1,2 +1,3 @@
 export * from './delivery-pending-transaction-status-name.mapper';
 export * from './delivery-pending-transaction-status-translation.mapper';
+export * from './delivery-pending-transaction-state.mapper';
--- src/app/api/core/model/delivery/transaction/delivery-pending-transaction.interface.ts
+++ src/app/api/core/model/delivery/transaction/delivery-pending-transaction.interface.ts
@@ -1,5 +1,6 @@
 import { TransactionItem, TransactionUser } from '.';
 import { Money } from '../../money.interface';
+import { DELIVERY_ONGOING_STATES } from './delivery-status/delivery-ongoing-states.enum';
 import { DELIVERY_ONGOING_STATUS } from './delivery-status/delivery-ongoing-status.enum';
 
 export interface DeliveryPendingTransaction {
@@ -8,10 +9,10 @@ export interface DeliveryPendingTransaction {
   buyer: TransactionUser;
   seller: TransactionUser;
   moneyAmount: Money;
+  state: DELIVERY_ONGOING_STATES;
   status: {
     name: DELIVERY_ONGOING_STATUS;
     translation: string;
   };
   isCurrentUserTheSeller: boolean;
-  requestId: string;
 }
