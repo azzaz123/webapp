bf4af01fe8491137ccdd867b554d8726a2cce25c
Carlos Roth <carlos@ulex.es>
2022-01-10 21:10:22 +0100

    [WPA-8476]: Fix user agent condition

--- src/tests/user-agent.fixtures.spec.ts
+++ src/tests/user-agent.fixtures.spec.ts
@@ -1,5 +1,5 @@
 export const MOCK_HUAWEI_USER_AGENT: string =
-  'Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; HUAWEI; W1-U00)';
+  'Mozilla/5.0 (Linux; Android 9; Redmi Note 8 Pro Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/80.0.3987.149 Mobile Safari/537.36 hap/1.6/xiaomi com.miui.hybrid/1.6.1.0 com.xpspeed.qmouse/1.2.1 ({"packageName":"com.miui.home","type":"shortcut","extra":{"original":{"packageName":"com.tencent.mtt","type":"url","extra":{}},"scene":"api"}})';
 
 export const MOCK_MACOS_USER_AGENT: string =
   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36';
