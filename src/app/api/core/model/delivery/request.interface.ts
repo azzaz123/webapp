2badf75b8f7a01407193801a5c388d57e5feb3e0
Laia Lopez <laialopezz99@gmail.com>
2022-01-12 09:25:13 +0100

    [WPA-8833]: [Bug + Tests] Solved and updated

--- src/app/api/core/model/delivery/request.interface.ts
+++ src/app/api/core/model/delivery/request.interface.ts
@@ -1,11 +1,11 @@
-import { TransactionItem, TransactionUser } from './transaction';
-import { Money } from '../money.interface';
+import { DELIVERY_ONGOING_STATUS } from './transaction/delivery-status/delivery-ongoing-status.enum';
+import { DELIVERY_ONGOING_STATES } from './transaction/delivery-status/delivery-ongoing-states.enum';
+import { DeliveryPendingTransaction } from './transaction/delivery-pending-transaction.interface';
 
-export interface Request {
-  id: string;
-  item: TransactionItem;
-  buyer: TransactionUser;
-  seller: TransactionUser;
-  moneyAmount: Money;
-  isCurrentUserTheSeller: boolean;
+export interface Request extends DeliveryPendingTransaction {
+  state: DELIVERY_ONGOING_STATES.REQUEST_CREATED;
+  status: {
+    name: DELIVERY_ONGOING_STATUS.REQUEST_CREATED;
+    translation: string;
+  };
 }
