770d660cfbe0a65d91476942f25212e0897e4e9b
Laia Lopez <laialopezz99@gmail.com>
2022-01-12 17:07:57 +0100

    [WPA-8833]: [Comments] TransactionalEntity created and implemented

--- src/app/api/core/model/delivery/request.interface.ts
+++ src/app/api/core/model/delivery/request.interface.ts
@@ -1,8 +1,8 @@
 import { DELIVERY_ONGOING_STATUS } from './transaction/delivery-status/delivery-ongoing-status.enum';
 import { DELIVERY_ONGOING_STATES } from './transaction/delivery-status/delivery-ongoing-states.enum';
-import { DeliveryPendingTransaction } from './transaction/delivery-pending-transaction.interface';
+import { TransactionalEntity } from './transaction/transactional-entity.interface';
 
-export interface Request extends DeliveryPendingTransaction {
+export interface Request extends TransactionalEntity {
   state: DELIVERY_ONGOING_STATES.REQUEST_CREATED;
   status: {
     name: DELIVERY_ONGOING_STATUS.REQUEST_CREATED;
