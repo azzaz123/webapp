a325d54459cf84e68be4d465196a6ed4c7b1a59b
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-01-17 10:38:53 +0100

    [WPA-8038]: Remove feature flag for delivery nav links

--- src/app/private/features/delivery/pages/delivery.component.ts
+++ src/app/private/features/delivery/pages/delivery.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
-import { FEATURE_FLAGS_ENUM } from '@core/user/featureflag-constants';
-import { FeatureFlagService } from '@core/user/featureflag.service';
 import { UserService } from '@core/user/user.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { PRIVATE_PATHS } from '@private/private-routing-constants';
@@ -36,21 +34,16 @@ export class DeliveryComponent implements OnInit, OnDestroy {
     display: $localize`:@@web_delivery_shipping_address:Address`,
   };
 
-  public navLinks: NavLink[] = [this.deliveryAddressNavLink];
+  public navLinks: NavLink[] = [this.buysNavLink, this.sellsNavLink, this.completedNavLink, this.deliveryAddressNavLink];
 
   public selectedNavLinkId: string;
   private subscriptions: Subscription = new Subscription();
 
-  constructor(
-    private router: Router,
-    private userService: UserService,
-    private modalService: NgbModal,
-    private featureflagService: FeatureFlagService
-  ) {
+  constructor(private router: Router, private userService: UserService, private modalService: NgbModal) {
     this.subscriptions.add(
       router.events.subscribe((event) => {
         if (event instanceof NavigationEnd && this.navLinks) {
-          const defaultUrl: string = this.deliveryAddressNavLink.id;
+          const defaultUrl: string = this.buysNavLink.id;
           const isDefaultUrlSelected: boolean = event.urlAfterRedirects === defaultUrl;
 
           this.selectNavLink(isDefaultUrlSelected ? defaultUrl : event.url);
@@ -60,7 +53,6 @@ export class DeliveryComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this.showStreamlineWhenDeliveryFeatureFlagEnabled();
     this.showTRXAwarenessModalForFirstTime();
   }
 
@@ -76,17 +68,6 @@ export class DeliveryComponent implements OnInit, OnDestroy {
     this.modalService.open(TRXAwarenessModalComponent).result.then(
       () => {},
       () => {}
-    );
-  }
-
-  private showStreamlineWhenDeliveryFeatureFlagEnabled(): void {
-    this.subscriptions.add(
-      this.featureflagService.getLocalFlag(FEATURE_FLAGS_ENUM.DELIVERY).subscribe((isActive: boolean) => {
-        if (isActive) {
-          this.navLinks = [this.buysNavLink, this.sellsNavLink, this.completedNavLink, this.deliveryAddressNavLink];
-        }
-        this.selectNavLink(this.router.url);
-      })
     );
   }
 
