76b030c96dd6088628471e9d6a57bf520036849c
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-01-18 13:44:57 +0100

    Use local translations instead of Phrase for Streamline copies

--- src/app/private/features/delivery/translations/completed-transaction-tracking-status-as-seller.translations.ts
+++ src/app/private/features/delivery/translations/completed-transaction-tracking-status-as-seller.translations.ts
@@ -6,7 +6,7 @@ export const completedTransactionTrackingStatusAsSellerTranslations = (
 ): string => {
   const translations: Record<COMPLETED_TRANSACTION_TRACKING_STATUS, string> = {
     [COMPLETED_TRANSACTION_TRACKING_STATUS.DISPUTE_REJECTED_BY_WALLAPOP]: $localize`:@@sales_view_seller_all_all_dispute_rejected_by_wallapop_finished_tab_shipping_status_label:Dispute rejected on ${interpolation}:INTERPOLATION:`,
-    [COMPLETED_TRANSACTION_TRACKING_STATUS.DISPUTE_CLOSED]: $localize`:@@sales_view_seller_all_all_dispute_closed_finished_tab_shipping_status_label:Dispute closed on ${interpolation}:INTERPOLATION:`,
+    [COMPLETED_TRANSACTION_TRACKING_STATUS.DISPUTE_CLOSED]: $localize`:@@web_sales_view_seller_all_all_dispute_closed_finished_tab_shipping_status_label:Dispute closed on ${interpolation}:INTERPOLATION:`,
     [COMPLETED_TRANSACTION_TRACKING_STATUS.DISPUTE_EXPIRED]: $localize`:@@sales_view_seller_all_all_dispute_expired_finished_tab_shipping_status_label:Dispute expired on ${interpolation}:INTERPOLATION:`,
     [COMPLETED_TRANSACTION_TRACKING_STATUS.TRANSACTION_CANCELLED_BY_SELLER]: $localize`:@@sales_view_seller_all_all_transaction_cancelled_by_seller_finished_tab_shipping_status_label:Cancelled on ${interpolation}:INTERPOLATION:`,
     [COMPLETED_TRANSACTION_TRACKING_STATUS.TRANSACTION_EXPIRED]: $localize`:@@sales_view_seller_all_all_transaction_expired_finished_tab_shipping_status_label:Cancelled on ${interpolation}:INTERPOLATION:`,
