112edfd07ac8610e454060a5ab43a55e74fe310b
Alex Masnou <alex.masnou@wallapop.com>
2022-01-18 20:32:55 +0100

    [WPA-9122]: Add test case for standalone service

--- src/app/core/standalone/services/standalone.service.spec.ts
+++ src/app/core/standalone/services/standalone.service.spec.ts
@@ -5,11 +5,19 @@ import { StandaloneService } from './standalone.service';
 import { STANDALONE_STATUS } from '@core/standalone/enums/standalone-status.enum';
 import { MOCK_HUAWEI_USER_AGENT, MOCK_MACOS_USER_AGENT } from '@fixtures/user-agent.fixtures.spec';
 import { USER_AGENT } from '@core/user-agent/user-agent';
+import { WINDOW_TOKEN } from '@core/window/window.token';
 
 describe('StandaloneService', () => {
   let service: StandaloneService;
   let route: ActivatedRoute;
   let userAgentMock = MOCK_MACOS_USER_AGENT;
+  let windowMock = {
+    matchMedia() {
+      return {
+        matches: false,
+      };
+    },
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -28,6 +36,10 @@ describe('StandaloneService', () => {
         {
           provide: USER_AGENT,
           useValue: userAgentMock,
+        },
+        {
+          provide: WINDOW_TOKEN,
+          useValue: windowMock,
         },
       ],
     });
@@ -77,6 +89,29 @@ describe('StandaloneService', () => {
             expect(value).toBe(true);
             done();
           });
+        });
+      });
+    });
+
+    describe('when the application is running in an standalone window', () => {
+      beforeAll(() => {
+        userAgentMock = MOCK_MACOS_USER_AGENT;
+        windowMock = {
+          matchMedia() {
+            return {
+              matches: true,
+            };
+          },
+        };
+      });
+
+      it('should enable standalone mode', (done) => {
+        spyOn(route.snapshot.queryParamMap, 'get').and.returnValue('false');
+        service = TestBed.inject(StandaloneService);
+
+        service.standalone$.subscribe((value: boolean) => {
+          expect(value).toBe(true);
+          done();
         });
       });
     });
