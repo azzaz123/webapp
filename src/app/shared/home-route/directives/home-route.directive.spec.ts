5bb5b64fd6b5a71d0709d8cfef620621a3c4490f
Alex Masnou <alex.masnou@wallapop.com>
2022-01-18 19:50:23 +0100

    [WPA-9122]: Create directive for handling home route

---
+++ src/app/shared/home-route/directives/home-route.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HomeRouteDirective } from './home-route.directive';
+
+describe('HomeRouteDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HomeRouteDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/shared/home-route/directives/home-route.directive.ts
@@ -0,0 +1,28 @@
+import { HostListener, Directive, Inject } from '@angular/core';
+import { SITE_URL } from '@configs/site-url.config';
+import { SearchNavigatorService } from '@core/search/search-navigator.service';
+import { StandaloneService } from '@core/standalone/services/standalone.service';
+import { WINDOW_TOKEN } from '@core/window/window.token';
+
+@Directive({
+  selector: '[tslHomeRoute]',
+})
+export class HomeRouteDirective {
+  constructor(
+    private standaloneService: StandaloneService,
+    private searchNavigatorService: SearchNavigatorService,
+    @Inject(WINDOW_TOKEN) private window,
+    @Inject(SITE_URL) private siteUrl: string
+  ) {}
+
+  @HostListener('click', ['$event']) onClick(event: MouseEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (this.standaloneService.standalone) {
+      this.searchNavigatorService.navigateWithLocationParams({});
+    } else {
+      this.window.open(this.siteUrl, '_self');
+    }
+  }
+}
