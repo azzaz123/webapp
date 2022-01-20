07d9d7ae5c0f8b33ad0315ec0b2a81b82c551be0
Laia Lopez <laialopezz99@gmail.com>
2022-01-20 19:54:27 +0100

    [WPA-8996]: [Refolder] AcceptScreenModal into modals folder

--- src/app/private/features/accept-screen/accept-screen.module.ts
+++ src/app/private/features/accept-screen/accept-screen.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { acceptScreenRoutedComponents, AcceptScreenRoutingModule } from './accept-screen.routing.module';
 import { CommonModule } from '@angular/common';
-import { AcceptScreenModalComponent } from './components/accept-screen-modal/accept-screen-modal.component';
+import { AcceptScreenModalComponent } from './modals/accept-screen-modal/accept-screen-modal.component';
 
 @NgModule({
   imports: [AcceptScreenRoutingModule, CommonModule],
