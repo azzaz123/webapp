fab487757669eccb9fa9293569895c8c698d5350
Laia Lopez <laialopezz99@gmail.com>
2022-01-24 10:22:04 +0100

    [WPA-9202]: [Deleted] Unused Module

--- src/app/api/delivery/seller/requests/seller-requests-api.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { SellerRequestsApiService } from './seller-requests-api.service';
-import { SellerRequestsHttpService } from './http/seller-requests-http.service';
-
-@NgModule({
-  providers: [SellerRequestsApiService, SellerRequestsHttpService],
-})
-export class SellerRequestsApiModule {}
