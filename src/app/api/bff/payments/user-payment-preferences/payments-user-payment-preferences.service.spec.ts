d5ac6e319fcded2c28805311b25df5c2fb09f065
antoni-casas <toni.casas@wallapop.com>
2022-01-24 17:44:16 +0100

    [WPA-9126]: feat: Add user payment preferences service

---
+++ src/app/api/bff/payments/user-payment-preferences/payments-user-payment-preferences.service.spec.ts
@@ -0,0 +1,52 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+
+import {
+  MOCK_PAYMENTS_USER_PAYMENT_PREFERENCES,
+  MOCK_PAYMENTS_USER_PAYMENT_PREFERENCES_RESPONSE,
+} from '@api/fixtures/bff/payments/user-payment-preferences/payments-user-payment-preferences-dto.fixtures.spec';
+import { PaymentsUserPaymentPreferences } from '@api/bff/payments/user-payment-preferences/interfaces/payments-user-payment-preferences.interface';
+import { PaymentsUserPaymentPreferencesHttpService } from '@api/bff/payments/user-payment-preferences/http/payments-user-payment-preferences-http.service';
+import { PaymentsUserPreferencesService } from '@api/bff/payments/user-payment-preferences/payments-user-payment-preferences.service';
+
+import { of } from 'rxjs';
+
+describe('PaymentsUserPreferencesService', () => {
+  let service: PaymentsUserPreferencesService;
+  let userPaymentPreferencesHttpService: PaymentsUserPaymentPreferencesHttpService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PaymentsUserPreferencesService, PaymentsUserPaymentPreferencesHttpService],
+    });
+    service = TestBed.inject(PaymentsUserPreferencesService);
+    userPaymentPreferencesHttpService = TestBed.inject(PaymentsUserPaymentPreferencesHttpService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('when asking to get the user payment preferences', () => {
+    beforeEach(() => {
+      spyOn(userPaymentPreferencesHttpService, 'getUserPaymentPreferences').and.returnValue(
+        of(MOCK_PAYMENTS_USER_PAYMENT_PREFERENCES_RESPONSE)
+      );
+    });
+
+    it('should retrieve the user payment preferences', () => {
+      service.paymentUserPreferences.subscribe();
+
+      expect(userPaymentPreferencesHttpService.getUserPaymentPreferences).toHaveBeenCalledTimes(1);
+    });
+
+    it('should map server response to web context', () => {
+      let response: PaymentsUserPaymentPreferences;
+
+      service.paymentUserPreferences.subscribe((data) => (response = data));
+
+      expect(JSON.stringify(response)).toEqual(JSON.stringify(MOCK_PAYMENTS_USER_PAYMENT_PREFERENCES));
+    });
+  });
+});
---
+++ src/app/api/bff/payments/user-payment-preferences/payments-user-payment-preferences.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+import { mapPaymentsUserPaymentPreferncesDtoToPaymentsUserPaymentPreferences } from '@api/bff/payments/user-payment-preferences/mappers/payments-user-payment-preferences.mapper';
+import { PaymentsUserPaymentPreferences } from '@api/bff/payments/user-payment-preferences/interfaces/payments-user-payment-preferences.interface';
+import { PaymentsUserPaymentPreferencesHttpService } from '@api/bff/payments/user-payment-preferences/http/payments-user-payment-preferences-http.service';
+
+import { map, take } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class PaymentsUserPreferencesService {
+  constructor(private paymentUserPreferencesHttpService: PaymentsUserPaymentPreferencesHttpService) {}
+
+  public get paymentUserPreferences(): Observable<PaymentsUserPaymentPreferences> {
+    return this.paymentUserPreferencesHttpService
+      .getUserPaymentPreferences()
+      .pipe(take(1), map(mapPaymentsUserPaymentPreferncesDtoToPaymentsUserPaymentPreferences));
+  }
+}
