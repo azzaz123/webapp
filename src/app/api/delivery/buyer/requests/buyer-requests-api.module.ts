582f301247d3ae122dd5715aae9cdf1d7c244ff7
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-01-25 18:54:25 +0100

    [WPA-9287]: Create buyer requests api module

---
+++ src/app/api/delivery/buyer/requests/buyer-requests-api.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BuyerRequestsHttpService } from './http/buyer-requests-http.service';
+
+@NgModule({
+  providers: [BuyerRequestsHttpService],
+})
+export class BuyerRequestsModule {}
