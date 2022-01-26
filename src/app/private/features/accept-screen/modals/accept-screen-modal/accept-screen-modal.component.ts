9112852e5938a93a13cd6367a4293e7dee5886a6
Laia Lopez <laialopezz99@gmail.com>
2022-01-26 16:56:44 +0100

    [WPA-8995]: [Testing] AcceptScreenModalComponent

--- src/app/private/features/accept-screen/modals/accept-screen-modal/accept-screen-modal.component.ts
+++ src/app/private/features/accept-screen/modals/accept-screen-modal/accept-screen-modal.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AcceptScreenProperties } from '../../interfaces';
 import { AcceptScreenStoreService } from '../../services/accept-screen-store/accept-screen-store.service';
@@ -14,7 +13,7 @@ export class AcceptScreenModalComponent implements OnInit {
   public requestId: string;
   public acceptScreenProperties$: Observable<AcceptScreenProperties>;
 
-  constructor(private route: ActivatedRoute, private acceptScreenStoreService: AcceptScreenStoreService) {}
+  constructor(private acceptScreenStoreService: AcceptScreenStoreService) {}
 
   ngOnInit(): void {
     this.acceptScreenStoreService.initialize(this.requestId);
