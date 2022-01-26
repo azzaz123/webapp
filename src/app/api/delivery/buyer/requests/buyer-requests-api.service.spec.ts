71115f1deb899024693a6ac50d39aca00e8d80a1
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-01-26 17:59:47 +0100

    [WPA-9289]: Remove unused imports

--- src/app/api/delivery/buyer/requests/buyer-requests-api.service.spec.ts
+++ src/app/api/delivery/buyer/requests/buyer-requests-api.service.spec.ts
@@ -1,10 +1,7 @@
 import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { BuyerRequest } from '@api/core/model/delivery/buyer-request/buyer-request.interface';
-import { SellerRequest } from '@api/core/model/delivery/seller-requests/seller-request.interface';
 import { MOCK_BUYER_REQUESTS } from '@api/fixtures/core/model/delivery/buyer-requests/buyer-request.fixtures.spec';
 import { MOCK_BUYER_REQUESTS_DTO } from '@api/fixtures/delivery/buyer/requests/buyer-requests-dto.fixtures.spec';
-import { MOCK_SELLER_REQUEST_DTO } from '@api/fixtures/delivery/seller/requests/seller-request-dto.fixtures.spec';
-import { MOCK_SELLER_REQUEST } from '@fixtures/private/delivery/seller-requests/seller-request.fixtures.spec';
 import { of } from 'rxjs';
 
 import { BuyerRequestsApiService } from './buyer-requests-api.service';
