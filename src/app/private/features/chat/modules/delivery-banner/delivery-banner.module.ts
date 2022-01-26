33c9ebc405932fcf226a2bd1447fa1a4144c56b1
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-01-26 15:49:27 +0100

    [WPA-9285]: Remove banner module from delivery banner module

--- src/app/private/features/chat/modules/delivery-banner/delivery-banner.module.ts
+++ src/app/private/features/chat/modules/delivery-banner/delivery-banner.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DeliveryBannerComponent } from './components/delivery-banner.component';
-import { BannerModule } from '@shared/banner/banner.module';
 import { SvgIconModule } from '@shared/svg-icon/svg-icon.module';
 import { ButtonModule } from '@shared/button/button.module';
 
 @NgModule({
-  imports: [CommonModule, BannerModule, SvgIconModule, ButtonModule],
+  imports: [CommonModule, SvgIconModule, ButtonModule],
   exports: [DeliveryBannerComponent],
   declarations: [DeliveryBannerComponent],
 })
