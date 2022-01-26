e4bf290cb6e5bf6817ea78a9219b51b0a7092acf
Laia Lopez <laialopezz99@gmail.com>
2022-01-26 16:08:25 +0100

    [WPA-8997]: [Comments] Typing MOCK_MONEY_DTO

--- src/app/api/fixtures/core/money.fixtures.ts
+++ src/app/api/fixtures/core/money.fixtures.ts
@@ -1,9 +1,10 @@
 import { NumberCurrencyCode } from '@api/core/mappers/money/money-mapper';
 import { Money } from '@api/core/model/money.interface';
+import { SellerCostsDto } from '@api/delivery/carrier-drop-off-mode/request/dtos/carrier-drop-off-mode-request-dto.interface';
 import { MOCK_CURRENCY_EURO } from './currency.fixtures';
 import { MOCK_NUMERIC_AMOUNT } from './numeric-amount.fixtures';
 
-export const MOCK_MONEY_DTO: any = {
+export const MOCK_MONEY_DTO: SellerCostsDto = {
   amount: 13,
   currency: 'EUR',
 };
