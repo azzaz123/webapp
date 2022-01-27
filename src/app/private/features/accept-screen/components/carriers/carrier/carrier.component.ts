807800e54b64eb6e2d4f7e5b2298a77c7a61db8e
Carlos Roth <carlos@ulex.es>
2022-01-27 20:48:35 +0100

    [WPA-9002]: Add carriers and carrier templates

--- src/app/private/features/accept-screen/components/carriers/carrier/carrier.component.ts
+++ src/app/private/features/accept-screen/components/carriers/carrier/carrier.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { AcceptScreenCarrier } from '../../../interfaces/accept-screen-carrier.interface';
+import { CARRIER_DROP_OFF_MODE } from '@api/core/model/delivery/carrier-drop-off-mode.type';
 
 @Component({
   selector: 'tsl-carrier',
@@ -8,4 +9,6 @@ import { AcceptScreenCarrier } from '../../../interfaces/accept-screen-carrier.i
 })
 export class CarrierComponent {
   @Input() carrier: AcceptScreenCarrier;
+  public readonly postOfficeDropOff: number = CARRIER_DROP_OFF_MODE.POST_OFFICE;
+  public readonly homePickUp: number = CARRIER_DROP_OFF_MODE.HOME_PICK_UP;
 }
--- src/app/private/features/accept-screen/components/carriers/carriers.module.ts
+++ src/app/private/features/accept-screen/components/carriers/carriers.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarriersComponent } from './carriers.component';
 import { CarrierComponent } from './carrier/carrier.component';
+import { ButtonModule } from '@shared/button/button.module';
 
 @NgModule({
   declarations: [CarriersComponent, CarrierComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonModule],
   exports: [CarriersComponent, CarrierComponent],
 })
 export class CarriersModule {}
