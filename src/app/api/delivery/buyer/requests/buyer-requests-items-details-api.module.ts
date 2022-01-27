c7862741242146d636d7ea4df2f138af043cd5fe
antoni-casas <toni.casas@wallapop.com>
2022-01-27 16:15:02 +0100

    [WPA-9129]: chore: Remove unnecessary files

--- src/app/api/delivery/buyer/requests/buyer-requests-items-details-api.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-
-import { BuyerRequestsItemsDetailsApiService } from '@api/delivery/buyer/requests/buyer-requests-items-details-api.service';
-import { BuyerRequestsItemsDetailsHttpService } from '@api/delivery/buyer/requests/http/buyer-requests-items-details-http.service';
-
-@NgModule({
-  providers: [BuyerRequestsItemsDetailsHttpService, BuyerRequestsItemsDetailsApiService],
-})
-export class BuyerRequestsItemsDetailsApiModule {}
