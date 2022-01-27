fe5e05e6d8a9b61e5c17b3adc450dc6ba919d3b3
antoni-casas <toni.casas@wallapop.com>
2022-01-27 16:35:02 +0100

    [WPA-9129]: chore: Fix endpoint

--- src/app/api/delivery/buyer/requests/http/endpoints.ts
+++ src/app/api/delivery/buyer/requests/http/endpoints.ts
@@ -3,5 +3,4 @@ import { environment } from '@environments/environment';
 export const BUYER_REQUESTS_ENDPOINT: string = `${environment.baseUrl}api/v3/delivery/buyer/requests`;
 
 export const BUYER_REQUESTS_ITEM_HASH_QUERY_PARAM_KEY: string = 'item_hash';
-export const BUYER_REQUESTS_ITEMS_DETAILS = (itemHash: string): string =>
-  `${environment.baseUrl}${BUYER_REQUESTS_ENDPOINT}/items/${itemHash}/details`;
+export const BUYER_REQUESTS_ITEMS_DETAILS = (itemHash: string): string => `${BUYER_REQUESTS_ENDPOINT}/items/${itemHash}/details`;
