36aee874eaddb2bea3ac5429f2dfeec78724a53b
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-01-28 16:21:08 +0100

    [WPA-9290]: Move delivery context buyer logic to service

--- src/app/private/features/chat/services/delivery-conversation-context/delivery-conversation-context.module.ts
+++ src/app/private/features/chat/services/delivery-conversation-context/delivery-conversation-context.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { DeliveryItemDetailsApiModule } from '@api/bff/delivery/items/detail/delivery-item-details-api.module';
 import { BuyerRequestsApiModule } from '@api/delivery/buyer/requests/buyer-requests-api.module';
 import { DeliveryConversationContextService } from './delivery-conversation-context.service';
+import { DeliveryConversationContextAsBuyerService } from './services/delivery-conversation-context-as-buyer/delivery-conversation-context-as-buyer.service';
 
 @NgModule({
   imports: [BuyerRequestsApiModule, DeliveryItemDetailsApiModule],
-  providers: [DeliveryConversationContextService],
+  providers: [DeliveryConversationContextAsBuyerService, DeliveryConversationContextService],
 })
 export class DeliveryConversationContextModule {}
--- src/app/private/features/chat/services/delivery-conversation-context/delivery-conversation-context.service.ts
+++ src/app/private/features/chat/services/delivery-conversation-context/delivery-conversation-context.service.ts
@@ -1,29 +1,17 @@
 import { Injectable } from '@angular/core';
-import { DeliveryItemDetailsApiService } from '@api/bff/delivery/items/detail/delivery-item-details-api.service';
-import { BuyerRequest } from '@api/core/model/delivery/buyer-request/buyer-request.interface';
-import { DeliveryItemDetails } from '@api/core/model/delivery/item-detail/delivery-item-details.interface';
-import { Money } from '@api/core/model/money.interface';
-import { BuyerRequestsApiService } from '@api/delivery/buyer/requests/buyer-requests-api.service';
 import { FEATURE_FLAGS_ENUM } from '@core/user/featureflag-constants';
 import { FeatureFlagService } from '@core/user/featureflag.service';
 import { Observable, of, ReplaySubject, Subscription } from 'rxjs';
-import { concatMap, map, take } from 'rxjs/operators';
+import { concatMap, take } from 'rxjs/operators';
 import { InboxConversation } from '../../core/model';
-import { DELIVERY_BANNER_TYPE } from '../../modules/delivery-banner/enums/delivery-banner-type.enum';
 import { DeliveryBanner } from '../../modules/delivery-banner/interfaces/delivery-banner.interface';
-import {
-  BUYER_ASK_SELLER_FOR_SHIPPING_BANNER_PROPERTIES,
-  BUYER_BUY_DELIVERY_BANNER_PROPERTIES,
-  SELLER_ACTIVATE_SHIPPING_BANNER_PROPERTIES,
-  SELLER_EDIT_PRICE_BANNER_PROPERTIES,
-} from './constants/delivery-banner-configs';
+import { DeliveryConversationContextAsBuyerService } from './services/delivery-conversation-context-as-buyer/delivery-conversation-context-as-buyer.service';
 
 @Injectable()
 export class DeliveryConversationContextService {
   constructor(
     private featureFlagService: FeatureFlagService,
-    private buyerRequestsServices: BuyerRequestsApiService,
-    private deliveryItemDetailsApiService: DeliveryItemDetailsApiService
+    private deliveryConversationContextAsBuyerService: DeliveryConversationContextAsBuyerService
   ) {}
 
   private _bannerProperties$: ReplaySubject<DeliveryBanner> = new ReplaySubject(1);
@@ -56,79 +44,13 @@ export class DeliveryConversationContextService {
   private getBannerProperties(conversation: InboxConversation): Observable<DeliveryBanner> {
     const { item } = conversation;
     const { id: itemHash, isMine } = item;
-    return isMine ? this.getBannerPropertiesAsSeller(itemHash) : this.getBannerPropertiesAsBuyer(itemHash);
-  }
-
-  private getBannerPropertiesAsBuyer(itemHash: string): Observable<DeliveryBanner | null> {
-    return this.buyerRequestsServices.getRequestsAsBuyerByItemHash(itemHash).pipe(
-      concatMap((buyerRequests: BuyerRequest[]) => {
-        return this.deliveryItemDetailsApiService.getDeliveryDetailsByItemHash(itemHash).pipe(
-          map((deliveryItemDetails: DeliveryItemDetails) => {
-            return this.mapDeliveryItemDetailsAsBuyerToProperties(buyerRequests, deliveryItemDetails);
-          })
-        );
-      })
-    );
-  }
-
-  private mapDeliveryItemDetailsAsBuyerToProperties(
-    buyerRequests: BuyerRequest[],
-    deliveryItemDetails: DeliveryItemDetails
-  ): DeliveryBanner | null {
-    const bannerType: DELIVERY_BANNER_TYPE = this.mapDeliveryDetailsAsBuyerToBannerType(buyerRequests, deliveryItemDetails);
-    const minimumPurchaseCost: Money | null = deliveryItemDetails ? deliveryItemDetails.minimumPurchaseCost : null;
-    return this.mapBannerTypeToProperties(bannerType, minimumPurchaseCost);
+    return isMine
+      ? this.getBannerPropertiesAsSeller(itemHash)
+      : this.deliveryConversationContextAsBuyerService.getBannerPropertiesAsBuyer(itemHash);
   }
 
   // TODO
   private getBannerPropertiesAsSeller(itemHash: string): Observable<DeliveryBanner | null> {
     return of(null);
-  }
-
-  private mapDeliveryDetailsAsBuyerToBannerType(
-    buyerRequests: BuyerRequest[],
-    deliveryItemDetails?: DeliveryItemDetails
-  ): DELIVERY_BANNER_TYPE {
-    const noDeliveryItemDetails: boolean = !deliveryItemDetails;
-    const buyerHasNoRequests: boolean = buyerRequests.length === 0;
-    const buyerHasRequests: boolean = !buyerHasNoRequests;
-
-    if (noDeliveryItemDetails) {
-      return DELIVERY_BANNER_TYPE.HIDDEN;
-    }
-
-    if (buyerHasRequests) {
-      return DELIVERY_BANNER_TYPE.HIDDEN;
-    }
-
-    if (buyerHasNoRequests) {
-      return DELIVERY_BANNER_TYPE.BUYER_BUY;
-    }
-
-    return DELIVERY_BANNER_TYPE.HIDDEN;
-  }
-
-  private mapBannerTypeToProperties(type: DELIVERY_BANNER_TYPE, price?: Money): DeliveryBanner | null {
-    if (type === DELIVERY_BANNER_TYPE.HIDDEN) {
-      return null;
-    }
-
-    if (type === DELIVERY_BANNER_TYPE.BUYER_BUY) {
-      return BUYER_BUY_DELIVERY_BANNER_PROPERTIES(price);
-    }
-
-    if (type === DELIVERY_BANNER_TYPE.BUYER_ASK_SELLER_FOR_SHIPPING) {
-      return BUYER_ASK_SELLER_FOR_SHIPPING_BANNER_PROPERTIES;
-    }
-
-    if (type === DELIVERY_BANNER_TYPE.SELLER_ACTIVATE_SHIPPING) {
-      return SELLER_ACTIVATE_SHIPPING_BANNER_PROPERTIES;
-    }
-
-    if (type === DELIVERY_BANNER_TYPE.SELLER_EDIT_PRICE) {
-      return SELLER_EDIT_PRICE_BANNER_PROPERTIES;
-    }
-
-    return null;
   }
 }
