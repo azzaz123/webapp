dab6a649ce8ab76eb55d99a04c189e220ccb34af
Carlos Roth <carlos@ulex.es>
2022-01-28 09:15:15 +0100

    [WPA-9002]: Add carrier selection and conditional

--- src/app/private/features/accept-screen/components/carriers/carrier/carrier.component.ts
+++ src/app/private/features/accept-screen/components/carriers/carrier/carrier.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { AcceptScreenCarrier } from '../../../interfaces/accept-screen-carrier.interface';
 import { CARRIER_DROP_OFF_MODE } from '@api/core/model/delivery/carrier-drop-off-mode.type';
 
@@ -9,6 +9,13 @@ import { CARRIER_DROP_OFF_MODE } from '@api/core/model/delivery/carrier-drop-off
 })
 export class CarrierComponent {
   @Input() carrier: AcceptScreenCarrier;
+  @Output() carrierSelected: EventEmitter<MouseEvent> = new EventEmitter();
+
   public readonly postOfficeDropOff: number = CARRIER_DROP_OFF_MODE.POST_OFFICE;
   public readonly homePickUp: number = CARRIER_DROP_OFF_MODE.HOME_PICK_UP;
+
+  public onSelectCarrier(selectedCarrier: MouseEvent): void {
+    this.carrier.isSelected = true;
+    this.carrierSelected.emit(selectedCarrier);
+  }
 }
