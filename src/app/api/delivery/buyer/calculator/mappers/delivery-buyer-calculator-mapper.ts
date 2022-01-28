884d1544f724f235b098a12ea8b62185651814de
antoni-casas <toni.casas@wallapop.com>
2022-01-28 14:08:25 +0100

    [WPA-9128]: chore: Fix reviewed issues

--- src/app/api/delivery/buyer/calculator/mappers/delivery-buyer-calculator-mapper.ts
+++ src/app/api/delivery/buyer/calculator/mappers/delivery-buyer-calculator-mapper.ts
@@ -1,12 +1,10 @@
-import { CurrencyCode } from '@api/core/model/currency.interface';
 import { DeliveryBuyerCalculatorCost } from '@api/core/model/delivery/buyer/calculator/delivery-buyer-calculator-cost.interface';
 import { DeliveryBuyerCalculatorCostDto } from '@api/delivery/buyer/calculator/dtos/delivery-buyer-calculator-cost-dto.interface';
 import { DeliveryBuyerCalculatorCosts } from '@api/core/model/delivery/buyer/calculator/delivery-buyer-calculator-costs.interface';
 import { DeliveryBuyerCalculatorCostsDto } from '@api/delivery/buyer/calculator/dtos/delivery-buyer-calculator-costs-dto.interface';
 import { DeliveryBuyerCalculatorPromotionCost } from '@api/core/model/delivery/buyer/calculator/delivery-buyer-calculator-promotion-cost.interface';
 import { DeliveryBuyerCalculatorPromotionCostDto } from '@api/delivery/buyer/calculator/dtos/delivery-buyer-calculator-promotion-cost-dto.interface';
-import { mapNumberAndCurrencyCodeToMoney, NumberCurrencyCode } from '@api/core/mappers';
-import { Money } from '@api/core/model/money.interface';
+import { mapAmountAndCurrenyToMoney } from '@api/core/mappers';
 import { PriceDto } from '@api/core/dtos';
 import { ToDomainMapper } from '@api/core/utils/types';
 
@@ -24,10 +22,10 @@ export const mapDeliveryBuyerCalculatorCostsDtoToDeliveryBuyerCalculatorCosts: T
 
 const mapToDeliveryBuyerCalculatorCost = (input: DeliveryBuyerCalculatorCostDto): DeliveryBuyerCalculatorCost => {
   return {
-    deliveryCost: mapToMoney(input.delivery_cost),
-    fees: mapToMoney(input.fees),
-    productPrice: mapToMoney(input.product_price),
-    total: mapToMoney(input.total),
+    deliveryCost: mapAmountAndCurrenyToMoney<PriceDto>(input.delivery_cost),
+    fees: mapAmountAndCurrenyToMoney<PriceDto>(input.fees),
+    productPrice: mapAmountAndCurrenyToMoney<PriceDto>(input.product_price),
+    total: mapAmountAndCurrenyToMoney<PriceDto>(input.total),
   };
 };
 
@@ -37,17 +35,10 @@ const mapToDeliveryBuyerCalculatorPromotionCost = (
   return !!input
     ? {
         deliveryCostDiscountPercentage: input.delivery_cost_discount_percentage,
-        deliveryCostFixedPrice: mapToMoney(input.delivery_cost_fixed_price),
-        feesFixedPrice: mapToMoney(input.fees_fixed_price),
+        deliveryCostFixedPrice: mapAmountAndCurrenyToMoney<PriceDto>(input.delivery_cost_fixed_price),
+        feesFixedPrice: mapAmountAndCurrenyToMoney<PriceDto>(input.fees_fixed_price),
         originalBuyerCost: mapToDeliveryBuyerCalculatorCost(input.original_buyer_cost),
         promocode: input.promocode,
       }
     : null;
-};
-
-const mapToMoney = (priceDto: PriceDto): Money => {
-  return mapNumberAndCurrencyCodeToMoney({
-    number: priceDto.amount,
-    currency: priceDto.currency as CurrencyCode,
-  });
 };
