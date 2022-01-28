915116124ac27ae89bd8ebfeba3368369084b098
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-01-28 14:57:58 +0100

    [WPA-9290]: Create delivery banner configurations file

---
+++ src/app/private/features/chat/services/delivery-conversation-context/constants/delivery-banner-configs.ts
@@ -0,0 +1,41 @@
+import { Money } from '@api/core/model/money.interface';
+import { DELIVERY_BANNER_BUTTON_TYPE } from '@private/features/chat/modules/delivery-banner/enums/delivery-banner-button-type.enum';
+import { ActionableDeliveryBanner } from '@private/features/chat/modules/delivery-banner/interfaces/actionable-delivery-banner.interface';
+import { DeliveryBanner } from '@private/features/chat/modules/delivery-banner/interfaces/delivery-banner.interface';
+import { DescriptiveDeliveryBanner } from '@private/features/chat/modules/delivery-banner/interfaces/descriptive-delivery-banner.interface';
+
+type BuyerBuyDeliveryBannerProperties = DescriptiveDeliveryBanner & ActionableDeliveryBanner;
+export const BUYER_BUY_DELIVERY_BANNER_PROPERTIES = (price: Money): BuyerBuyDeliveryBannerProperties => {
+  const bannerProperties: BuyerBuyDeliveryBannerProperties = {
+    svgPath: 'assets/icons/joke.svg',
+    description: { text: `Shipping available for ${price}`, helpLink: 'www.google.com' },
+    action: {
+      label: 'Buy',
+      type: DELIVERY_BANNER_BUTTON_TYPE.CONTAINED,
+    },
+  };
+  return bannerProperties;
+};
+
+export const BUYER_ASK_SELLER_FOR_SHIPPING_BANNER_PROPERTIES: DeliveryBanner = {
+  svgPath: 'assets/icons/joke.svg',
+  description: { text: `Ask seller for shipping` },
+};
+
+export const SELLER_ACTIVATE_SHIPPING_BANNER_PROPERTIES: ActionableDeliveryBanner = {
+  svgPath: 'assets/icons/joke.svg',
+  description: { text: `Ask seller for shipping` },
+  action: {
+    label: 'Edit',
+    type: DELIVERY_BANNER_BUTTON_TYPE.CONTAINED,
+  },
+};
+
+export const SELLER_EDIT_PRICE_BANNER_PROPERTIES: ActionableDeliveryBanner = {
+  svgPath: 'assets/icons/joke.svg',
+  description: { text: `Change price` },
+  action: {
+    label: 'Change',
+    type: DELIVERY_BANNER_BUTTON_TYPE.OUTLINED,
+  },
+};
