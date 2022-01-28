cc71b3c09fc9e808e78c5518d20f6019f6bfed2d
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-01-28 16:38:28 +0100

    [WPA-9290]: Split delivery banner types into buyer and seller enums

--- src/app/private/features/chat/modules/delivery-conversation-context/services/delivery-conversation-context-as-buyer/delivery-conversation-context-as-buyer.service.ts
+++ src/app/private/features/chat/modules/delivery-conversation-context/services/delivery-conversation-context-as-buyer/delivery-conversation-context-as-buyer.service.ts
@@ -4,16 +4,14 @@ import { BuyerRequest } from '@api/core/model/delivery/buyer-request/buyer-reque
 import { DeliveryItemDetails } from '@api/core/model/delivery/item-detail/delivery-item-details.interface';
 import { Money } from '@api/core/model/money.interface';
 import { BuyerRequestsApiService } from '@api/delivery/buyer/requests/buyer-requests-api.service';
-import { DELIVERY_BANNER_TYPE } from '@private/features/chat/modules/delivery-banner/enums/delivery-banner-type.enum';
 import { DeliveryBanner } from '@private/features/chat/modules/delivery-banner/interfaces/delivery-banner.interface';
 import { Observable } from 'rxjs';
 import { concatMap, map } from 'rxjs/operators';
 import {
   BUYER_BUY_DELIVERY_BANNER_PROPERTIES,
   BUYER_ASK_SELLER_FOR_SHIPPING_BANNER_PROPERTIES,
-  SELLER_ACTIVATE_SHIPPING_BANNER_PROPERTIES,
-  SELLER_EDIT_PRICE_BANNER_PROPERTIES,
 } from '@private/features/chat/modules/delivery-banner/constants/delivery-banner-configs';
+import { BUYER_DELIVERY_BANNER_TYPE } from '@private/features/chat/modules/delivery-banner/enums/buyer-delivery-banner-type.enum';
 
 @Injectable()
 export class DeliveryConversationContextAsBuyerService {
@@ -38,7 +36,7 @@ export class DeliveryConversationContextAsBuyerService {
     buyerRequests: BuyerRequest[],
     deliveryItemDetails: DeliveryItemDetails
   ): DeliveryBanner | null {
-    const bannerType: DELIVERY_BANNER_TYPE = this.mapDeliveryDetailsAsBuyerToBannerType(buyerRequests, deliveryItemDetails);
+    const bannerType: BUYER_DELIVERY_BANNER_TYPE | null = this.mapDeliveryDetailsAsBuyerToBannerType(buyerRequests, deliveryItemDetails);
     const minimumPurchaseCost: Money | null = deliveryItemDetails ? deliveryItemDetails.minimumPurchaseCost : null;
     return this.mapBannerTypeToBuyerBannerProperties(bannerType, minimumPurchaseCost);
   }
@@ -46,45 +44,38 @@ export class DeliveryConversationContextAsBuyerService {
   private mapDeliveryDetailsAsBuyerToBannerType(
     buyerRequests: BuyerRequest[],
     deliveryItemDetails?: DeliveryItemDetails
-  ): DELIVERY_BANNER_TYPE {
+  ): BUYER_DELIVERY_BANNER_TYPE | null {
     const noDeliveryItemDetails: boolean = !deliveryItemDetails;
     const buyerHasNoRequests: boolean = buyerRequests.length === 0;
     const buyerHasRequests: boolean = !buyerHasNoRequests;
 
     if (noDeliveryItemDetails) {
-      return DELIVERY_BANNER_TYPE.HIDDEN;
-    }
-
-    if (buyerHasRequests) {
-      return DELIVERY_BANNER_TYPE.HIDDEN;
-    }
-
-    if (buyerHasNoRequests) {
-      return DELIVERY_BANNER_TYPE.BUYER_BUY;
-    }
-
-    return DELIVERY_BANNER_TYPE.HIDDEN;
-  }
-
-  private mapBannerTypeToBuyerBannerProperties(type: DELIVERY_BANNER_TYPE, price?: Money): DeliveryBanner | null {
-    if (type === DELIVERY_BANNER_TYPE.HIDDEN) {
       return null;
     }
 
-    if (type === DELIVERY_BANNER_TYPE.BUYER_BUY) {
+    if (buyerHasRequests) {
+      return null;
+    }
+
+    if (buyerHasNoRequests) {
+      return BUYER_DELIVERY_BANNER_TYPE.BUY;
+    }
+
+    return null;
+  }
+
+  private mapBannerTypeToBuyerBannerProperties(type?: BUYER_DELIVERY_BANNER_TYPE, price?: Money): DeliveryBanner | null {
+    const isTypeNotDefined = !type;
+    if (isTypeNotDefined) {
+      return null;
+    }
+
+    if (type === BUYER_DELIVERY_BANNER_TYPE.BUY) {
       return BUYER_BUY_DELIVERY_BANNER_PROPERTIES(price);
     }
 
-    if (type === DELIVERY_BANNER_TYPE.BUYER_ASK_SELLER_FOR_SHIPPING) {
+    if (type === BUYER_DELIVERY_BANNER_TYPE.ASK_SELLER_FOR_SHIPPING) {
       return BUYER_ASK_SELLER_FOR_SHIPPING_BANNER_PROPERTIES;
-    }
-
-    if (type === DELIVERY_BANNER_TYPE.SELLER_ACTIVATE_SHIPPING) {
-      return SELLER_ACTIVATE_SHIPPING_BANNER_PROPERTIES;
-    }
-
-    if (type === DELIVERY_BANNER_TYPE.SELLER_EDIT_PRICE) {
-      return SELLER_EDIT_PRICE_BANNER_PROPERTIES;
     }
 
     return null;
