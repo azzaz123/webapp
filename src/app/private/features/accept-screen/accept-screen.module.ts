9bf04313c3a7e4be296e6b55ef4fec47cc8f0052
Carlos Roth <carlos@ulex.es>
2022-02-01 11:51:33 +0100

    [WPA-8995]: Add CustomCurrencyPipe on Product card

--- src/app/private/features/accept-screen/accept-screen.module.ts
+++ src/app/private/features/accept-screen/accept-screen.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AcceptScreenModalComponent } from './modals/accept-screen-modal/accept-screen-modal.component';
 import { ProductCardComponent } from './components/product-card/product-card.component';
 import { UserAvatarModule } from '@shared/user-avatar/user-avatar.module';
+import { CustomCurrencyModule } from '@shared/pipes/custom-currency/custom-currency.module';
 
 @NgModule({
-  imports: [AcceptScreenRoutingModule, CommonModule, UserAvatarModule],
+  imports: [AcceptScreenRoutingModule, CommonModule, UserAvatarModule, CustomCurrencyModule],
   declarations: [acceptScreenRoutedComponents, AcceptScreenModalComponent, ProductCardComponent],
 })
 export class AcceptScreenModule {}
