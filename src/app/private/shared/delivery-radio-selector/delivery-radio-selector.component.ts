50135fa8bea30b388bc3ce280c03dc61ff56dd7d
Laia Lopez <laialopezz99@gmail.com>
2022-02-03 16:50:54 +0100

    [WPA-9002]: [Testing] DeliveryRadioSelectorComponent

--- src/app/private/shared/delivery-radio-selector/delivery-radio-selector.component.ts
+++ src/app/private/shared/delivery-radio-selector/delivery-radio-selector.component.ts
@@ -1,10 +1,20 @@
-import { AfterContentInit, Component, ContentChildren, EventEmitter, Input, OnInit, Output, QueryList } from '@angular/core';
+import {
+  AfterContentInit,
+  ChangeDetectionStrategy,
+  Component,
+  ContentChildren,
+  EventEmitter,
+  Input,
+  Output,
+  QueryList,
+} from '@angular/core';
 import { DeliveryRadioOptionDirective } from '@private/shared/delivery-radio-selector/delivery-radio-option.directive';
 
 @Component({
   selector: 'tsl-delivery-radio-selector',
   templateUrl: './delivery-radio-selector.component.html',
   styleUrls: ['./delivery-radio-selector.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DeliveryRadioSelectorComponent implements AfterContentInit {
   @Input() selectedId: number;
