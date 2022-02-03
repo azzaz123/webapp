6f0549b1b4d6521f3d7ce13f96a9ae165c7d9f67
antoni-casas <toni.casas@wallapop.com>
2022-02-03 09:42:29 +0100

    [WPA-9152]: feat: Create payview module

--- src/app/private/features/payview/components/overview/payview-overview.component.ts
+++ src/app/private/features/payview/components/overview/payview-overview.component.ts
@@ -2,6 +2,8 @@ import { ActivatedRoute } from '@angular/router';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 import { PRIVATE_PATH_PARAMS } from '@private/private-routing-constants';
+import { PayviewService } from '../../services/payview.service';
+import { PayviewState } from '../../interfaces/payview-state.interface';
 
 @Component({
   selector: 'tsl-payview-overview',
@@ -10,9 +12,14 @@ import { PRIVATE_PATH_PARAMS } from '@private/private-routing-constants';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PayviewOverviewComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private payviewService: PayviewService) {}
 
   ngOnInit(): void {
     const requestId: string = this.route.snapshot.paramMap.get(PRIVATE_PATH_PARAMS.ID);
+    debugger;
+    this.payviewService.getCurrentState().subscribe((result: PayviewState) => {
+      debugger;
+      console.log(result);
+    });
   }
 }
--- src/app/private/features/payview/components/payview/payview.component.ts
+++ src/app/private/features/payview/components/payview/payview.component.ts
@@ -1,17 +1,24 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 import { PayviewOverviewComponent } from '@private/features/payview/components/overview/payview-overview.component';
+import { PRIVATE_PATH_PARAMS } from '@private/private-routing-constants';
 
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { PayviewService } from '../../services/payview.service';
 
 @Component({
   selector: 'tsl-payview',
   template: '',
 })
 export class PayviewComponent implements OnInit {
-  constructor(private modalService: NgbModal) {}
+  constructor(private modalService: NgbModal, private route: ActivatedRoute, private payviewService: PayviewService) {}
 
   ngOnInit(): void {
+    const itemId: string = this.route.snapshot.paramMap.get(PRIVATE_PATH_PARAMS.ID);
+    debugger;
+    this.payviewService.itemHash = itemId;
+
     this.modalService.open(PayviewOverviewComponent).result.then(
       () => {},
       () => {}
