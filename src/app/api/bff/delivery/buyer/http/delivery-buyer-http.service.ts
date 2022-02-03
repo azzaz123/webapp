033c0c465c0140cd7ae3ddbdd98c4a4a31605f6a
antoni-casas <toni.casas@wallapop.com>
2022-02-03 09:36:29 +0100

    [WPA-9152]: chore: Fix delivery buyer service

--- src/app/api/bff/delivery/buyer/http/delivery-buyer-http.service.ts
+++ src/app/api/bff/delivery/buyer/http/delivery-buyer-http.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { APP_VERSION } from '@environments/version';
 import { DELIVERY_BUYER_DELIVERY_METHODS_ENDPOINT } from '@api/bff/delivery/buyer/http/endpoints';
 import { DeliveryBuyerDeliveryMethodsDto } from '@api/bff/delivery/buyer/dtos';
 
@@ -11,6 +12,13 @@ export class DeliveryBuyerHttpService {
   constructor(private http: HttpClient) {}
 
   public getDeliveryMethods(itemHash: string): Observable<DeliveryBuyerDeliveryMethodsDto> {
-    return this.http.get<DeliveryBuyerDeliveryMethodsDto>(DELIVERY_BUYER_DELIVERY_METHODS_ENDPOINT(itemHash));
+    return this.http.get<DeliveryBuyerDeliveryMethodsDto>(DELIVERY_BUYER_DELIVERY_METHODS_ENDPOINT, {
+      params: { item_hash: itemHash },
+      headers: this.getHeaders,
+    });
+  }
+
+  private get getHeaders(): { [header: string]: string | string[] } {
+    return { 'X-AppVersion': APP_VERSION.replace(/\./g, '') };
   }
 }
