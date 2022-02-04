3f14a734cb106ae54fc0da4f275d6f020a8e75b6
Àlex Delgado <alejandro.delgado@wallapop.com>
2022-02-04 10:05:23 +0100

    [WPA-9378]: refactor notifications component for accept subject and async

--- src/app/private/features/profile/pages/notifications/notifications.component.ts
+++ src/app/private/features/profile/pages/notifications/notifications.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NotificationConsent, NotificationSettings } from '@api/core/model/notifications';
 import { NotificationsApiService } from '@api/notifications/notifications-api.service';
+import { ReplaySubject } from 'rxjs';
 
 @Component({
   selector: 'tsl-notifications',
@@ -8,8 +9,8 @@ import { NotificationsApiService } from '@api/notifications/notifications-api.se
   styleUrls: ['./notifications.component.scss'],
 })
 export class NotificationsComponent implements OnInit {
-  public notificationsSettingsGroup: NotificationSettings[];
   public allowSegmentation: boolean;
+  public notificationsSettings$: ReplaySubject<NotificationSettings[]> = new ReplaySubject<NotificationSettings[]>();
   private savedSearchedNotificationId = 'l1kmzng6n3p8';
 
   constructor(private notificationsApiService: NotificationsApiService) {}
@@ -20,14 +21,16 @@ export class NotificationsComponent implements OnInit {
 
   public getMyNotificationsSettings() {
     this.notificationsApiService.getMyNotificationsSettings().subscribe((data) => {
-      this.notificationsSettingsGroup = data
-        .map((nGroup) => {
-          const noSavedSearchesId = nGroup.notifications.find((notification) => notification.id !== this.savedSearchedNotificationId);
-          if (noSavedSearchesId) {
-            return nGroup;
-          }
-        })
-        .filter((ngroup) => !!ngroup);
+      this.notificationsSettings$.next(
+        data
+          .map((nGroup) => {
+            const noSavedSearchesId = nGroup.notifications.find((notification) => notification.id !== this.savedSearchedNotificationId);
+            if (noSavedSearchesId) {
+              return nGroup;
+            }
+          })
+          .filter((ngroup) => !!ngroup)
+      );
     });
   }
 
