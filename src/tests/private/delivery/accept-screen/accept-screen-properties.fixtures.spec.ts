a33cc768108afb12cc5a826981758aca8b8fbda2
Laia Lopez <laialopezz99@gmail.com>
2022-02-04 16:31:28 +0100

    [WPA-9002]: [Mock] Fixed

--- src/tests/private/delivery/accept-screen/accept-screen-properties.fixtures.spec.ts
+++ src/tests/private/delivery/accept-screen/accept-screen-properties.fixtures.spec.ts
@@ -13,10 +13,7 @@ import { MOCK_SELLER_REQUEST } from '@fixtures/private/delivery/seller-requests/
 import { FALLBACK_NOT_FOUND_SRC } from '@private/core/constants/fallback-images-src-constants';
 import { MOCK_DELIVERY_ADDRESS, MOCK_DELIVERY_ADDRESS_WITHOUT_FLAT_AND_FLOOR } from '../delivery-address.fixtures.spec';
 import { AcceptScreenDeliveryAddress } from '@private/features/accept-screen/interfaces/accept-screen-delivery-address.interface';
-import {
-  MOCK_ACCEPT_SCREEN_CARRIERS,
-  MOCK_ACCEPT_SCREEN_CARRIERS_FIRST_WITH_BUYER_ADDRESS,
-} from './accept-screen-properties-carriers.fixtures.spec';
+import { MOCK_ACCEPT_SCREEN_CARRIERS, MOCK_ACCEPT_SCREEN_CARRIERS_2 } from './accept-screen-properties-carriers.fixtures.spec';
 
 export const MOCK_ACCEPT_SCREEN_ITEM: AcceptScreenItem = {
   id: MOCK_ITEM.id,
@@ -75,7 +72,7 @@ export const MOCK_ACCEPT_SCREEN_PROPERTIES: AcceptScreenProperties = {
   item: MOCK_ACCEPT_SCREEN_ITEM,
   buyer: MOCK_ACCEPT_SCREEN_BUYER,
   seller: MOCK_ACCEPT_SCREEN_SELLER,
-  carriers: MOCK_ACCEPT_SCREEN_CARRIERS_FIRST_WITH_BUYER_ADDRESS,
+  carriers: MOCK_ACCEPT_SCREEN_CARRIERS_2,
   deliveryAddress: MOCK_ACCEPT_SCREEN_DELIVERY_ADDRESS,
 };
 
