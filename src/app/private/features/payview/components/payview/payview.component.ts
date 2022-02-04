2638bff872ebb8a4a9dfce809ed60083f2ccdf17
antoni-casas <toni.casas@wallapop.com>
2022-02-04 11:38:25 +0100

    [WPA-9152]: feat: Add payview component

--- src/app/private/features/payview/components/payview/payview.component.ts
+++ src/app/private/features/payview/components/payview/payview.component.ts
@@ -2,23 +2,33 @@ import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 import { PayviewOverviewComponent } from '@private/features/payview/components/overview/payview-overview.component';
+import { PayviewStateManagementService } from '@private/features/payview/services/state-management/payview-state-management.service';
 import { PRIVATE_PATH_PARAMS } from '@private/private-routing-constants';
 
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { PayviewService } from '../../services/payview.service';
 
 @Component({
   selector: 'tsl-payview',
   template: '',
 })
 export class PayviewComponent implements OnInit {
-  constructor(private modalService: NgbModal, private route: ActivatedRoute, private payviewService: PayviewService) {}
+  constructor(
+    private modalService: NgbModal,
+    private route: ActivatedRoute,
+    private payviewStateManagementService: PayviewStateManagementService
+  ) {}
 
   ngOnInit(): void {
-    const itemId: string = this.route.snapshot.paramMap.get(PRIVATE_PATH_PARAMS.ID);
-    debugger;
-    this.payviewService.itemHash = itemId;
+    this.loadPayviewData();
+    this.openModal();
+  }
 
+  private loadPayviewData(): void {
+    const itemId: string = this.route.snapshot.paramMap.get(PRIVATE_PATH_PARAMS.ID);
+    this.payviewStateManagementService.itemHash = itemId;
+  }
+
+  private openModal(): void {
     this.modalService.open(PayviewOverviewComponent).result.then(
       () => {},
       () => {}
