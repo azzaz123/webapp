2d529a9559ffadc126572e2879d3ecbcec702a8b
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-02-07 12:14:38 +0100

    [WPA-9538]: Display delivery third voice CTA if needed

--- src/app/private/features/chat/children/message/components/third-voice-delivery/third-voice-delivery.component.ts
+++ src/app/private/features/chat/children/message/components/third-voice-delivery/third-voice-delivery.component.ts
@@ -1,12 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
 import { InboxMessage } from '@private/features/chat/core/model';
 
 @Component({
   selector: 'tsl-third-voice-delivery',
   templateUrl: './third-voice-delivery.component.html',
   styleUrls: ['./third-voice-delivery.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ThirdVoiceDeliveryComponent {
+export class ThirdVoiceDeliveryComponent implements OnChanges {
   @Input() message: InboxMessage;
   @Input() shortMessage = false;
+  @Output() clickedCTA: EventEmitter<void> = new EventEmitter<void>();
+
+  public isCTAVisible: boolean = true;
+
+  ngOnChanges(): void {
+    this.isCTAVisible = this.shouldDisplayCTA();
+  }
+
+  public handleClickCTA(): void {
+    this.clickedCTA.emit();
+  }
+
+  private shouldDisplayCTA(): boolean {
+    return !!this.message.payload;
+  }
 }
