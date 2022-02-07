be971c5fdc945837efc3f6444fd75e276c0a03b8
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-02-07 19:47:06 +0100

    [WPA-9538]: Add tests for third voice delivery component

--- src/app/private/features/chat/children/message/components/third-voice-delivery/third-voice-delivery.component.spec.ts
+++ src/app/private/features/chat/children/message/components/third-voice-delivery/third-voice-delivery.component.spec.ts
@@ -1,24 +1,117 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA, DebugElement, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import {
+  MOCK_INBOX_THIRD_VOICE_DELIVERY_GENERIC_MESSAGE,
+  MOCK_INBOX_THIRD_VOICE_DELIVERY_GENERIC_MESSAGE_WITHOUT_PAYLOAD,
+} from '@fixtures/chat';
+import { InboxMessage } from '@private/features/chat/core/model';
+import { ButtonComponent } from '@shared/button/button.component';
+import { ThirdVoiceMessageComponent } from '../third-voice-message';
 
 import { ThirdVoiceDeliveryComponent } from './third-voice-delivery.component';
 
+@Component({
+  selector: 'tsl-test-wrapper',
+  template: `
+    <tsl-third-voice-delivery [message]="message" [shortMessage]="shortMessage" (clickedCTA)="clickedCTA()"></tsl-third-voice-delivery>
+  `,
+})
+class TestWrapperComponent {
+  @Input() message: InboxMessage;
+  @Input() shortMessage: boolean;
+  clickedCTA(): void {}
+}
+
 describe('ThirdVoiceDeliveryComponent', () => {
-  let component: ThirdVoiceDeliveryComponent;
-  let fixture: ComponentFixture<ThirdVoiceDeliveryComponent>;
+  let component: TestWrapperComponent;
+  let fixture: ComponentFixture<TestWrapperComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ThirdVoiceDeliveryComponent],
+      declarations: [ThirdVoiceDeliveryComponent, TestWrapperComponent, ButtonComponent, ThirdVoiceMessageComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ThirdVoiceDeliveryComponent);
+    fixture = TestBed.createComponent(TestWrapperComponent);
     component = fixture.componentInstance;
+    component.message = MOCK_INBOX_THIRD_VOICE_DELIVERY_GENERIC_MESSAGE;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('when it is opened as a large message', () => {
+    beforeEach(() => {
+      component.shortMessage = false;
+    });
+
+    it('should show third voice message', () => {
+      const thirdVoiceElement: DebugElement = fixture.debugElement.query(By.directive(ThirdVoiceMessageComponent));
+
+      expect(thirdVoiceElement).toBeTruthy();
+    });
+
+    describe('and when it has payload', () => {
+      beforeEach(() => {
+        component.message = MOCK_INBOX_THIRD_VOICE_DELIVERY_GENERIC_MESSAGE;
+        fixture.detectChanges();
+      });
+
+      it('should display CTA', () => {
+        const thirdVoiceCTAElement: DebugElement = fixture.debugElement.query(By.directive(ButtonComponent));
+
+        expect(thirdVoiceCTAElement).toBeTruthy();
+      });
+
+      describe('and when user clicks on CTA', () => {
+        beforeEach(() => {
+          spyOn(component, 'clickedCTA');
+          const thirdVoiceCTAElement: DebugElement = fixture.debugElement.query(By.directive(ButtonComponent));
+          thirdVoiceCTAElement.nativeElement.click();
+        });
+
+        it('should notify click', () => {
+          expect(component.clickedCTA).toHaveBeenCalledTimes(1);
+        });
+      });
+    });
+
+    describe('and when it does not have payload', () => {
+      beforeEach(() => {
+        component.message = MOCK_INBOX_THIRD_VOICE_DELIVERY_GENERIC_MESSAGE_WITHOUT_PAYLOAD;
+        fixture.detectChanges();
+      });
+
+      it('should hide the CTA', () => {
+        const thirdVoiceCTAElement: DebugElement = fixture.debugElement.query(By.directive(ButtonComponent));
+
+        expect(thirdVoiceCTAElement).toBeFalsy();
+      });
+    });
+  });
+
+  describe('when it is opened as a short message', () => {
+    beforeEach(() => {
+      component.shortMessage = true;
+      fixture.detectChanges();
+    });
+
+    it('should show third voice message', () => {
+      const thirdVoiceTextElement: DebugElement = fixture.debugElement.query(By.css('span'));
+      const thirdVoiceText: string = thirdVoiceTextElement.nativeElement.innerHTML;
+
+      expect(thirdVoiceText).toEqual(MOCK_INBOX_THIRD_VOICE_DELIVERY_GENERIC_MESSAGE.text);
+    });
+
+    it('should NOT show third voice message with style', () => {
+      const thirdVoiceElement: DebugElement = fixture.debugElement.query(By.directive(ThirdVoiceMessageComponent));
+
+      expect(thirdVoiceElement).toBeFalsy();
+    });
   });
 });
