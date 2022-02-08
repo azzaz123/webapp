51f2155f4a907c44ea38e70a6cb2314aae27ac9e
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-02-08 10:33:42 +0100

    [WPA-9538]: Handle delivery seller third voices CTA click

--- src/app/private/features/chat/modules/delivery-conversation-context/services/delivery-conversation-context-as-seller/delivery-conversation-context-as-seller.service.spec.ts
+++ src/app/private/features/chat/modules/delivery-conversation-context/services/delivery-conversation-context-as-seller/delivery-conversation-context-as-seller.service.spec.ts
@@ -1,16 +1,35 @@
 import { TestBed } from '@angular/core/testing';
+import { MOCK_INBOX_CONVERSATION_AS_SELLER, MOCK_INBOX_CONVERSATION_WITH_DELIVERY_THIRD_VOICES } from '@fixtures/chat';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { TRXAwarenessModalComponent } from '@private/features/delivery/modals/trx-awareness-modal/trx-awareness-modal.component';
 
 import { DeliveryConversationContextAsSellerService } from './delivery-conversation-context-as-seller.service';
 
 describe('DeliveryConversationContextAsSellerService', () => {
   let service: DeliveryConversationContextAsSellerService;
+  let modalService: NgbModal;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({ providers: [DeliveryConversationContextAsSellerService] });
+    TestBed.configureTestingModule({
+      providers: [DeliveryConversationContextAsSellerService, { provide: NgbModal, useValue: { open: () => {} } }],
+    });
     service = TestBed.inject(DeliveryConversationContextAsSellerService);
+    modalService = TestBed.inject(NgbModal);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('when handling CTA button', () => {
+    beforeEach(() => {
+      spyOn(modalService, 'open');
+      service.handleThirdVoiceCTAClick(MOCK_INBOX_CONVERSATION_WITH_DELIVERY_THIRD_VOICES);
+    });
+
+    it('should open TRX awareness modal', () => {
+      expect(modalService.open).toHaveBeenCalledTimes(1);
+      expect(modalService.open).toHaveBeenCalledWith(TRXAwarenessModalComponent);
+    });
   });
 });
--- src/app/private/features/chat/modules/delivery-conversation-context/services/delivery-conversation-context-as-seller/delivery-conversation-context-as-seller.service.ts
+++ src/app/private/features/chat/modules/delivery-conversation-context/services/delivery-conversation-context-as-seller/delivery-conversation-context-as-seller.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { InboxConversation } from '@private/features/chat/core/model';
+import { TRXAwarenessModalComponent } from '@private/features/delivery/modals/trx-awareness-modal/trx-awareness-modal.component';
 import { Observable, of } from 'rxjs';
 import { DeliveryBanner } from '../../../delivery-banner/interfaces/delivery-banner.interface';
 
 @Injectable()
 export class DeliveryConversationContextAsSellerService {
+  constructor(private modalService: NgbModal) {}
+
   // TODO
   public getBannerPropertiesAsSeller(itemHash: string): Observable<DeliveryBanner | null> {
     return of(null);
+  }
+
+  public handleThirdVoiceCTAClick(conversation: InboxConversation): void {
+    this.modalService.open(TRXAwarenessModalComponent);
   }
 }
