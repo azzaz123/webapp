5ed3f23ea32964b72a92e5f442f466b8d1bb959b
Laia Lopez <laialopezz99@gmail.com>
2022-02-09 18:08:20 +0100

    [WPA-9004]: [Testing NW] AcceptScreenStoreService

--- src/app/private/features/accept-screen/services/accept-screen-store/accept-screen-store.service.spec.ts
+++ src/app/private/features/accept-screen/services/accept-screen-store/accept-screen-store.service.spec.ts
@@ -3,8 +3,9 @@ import { CARRIER_DROP_OFF_MODE } from '@api/core/model/delivery';
 import {
   MOCK_ACCEPT_SCREEN_PROPERTIES,
   MOCK_ACCEPT_SCREEN_PROPERTIES_SELECTED_HPU,
+  MOCK_ACCEPT_SCREEN_PROPERTIES_WITHOUT_SELLER_ADDRESS,
 } from '@fixtures/private/delivery/accept-screen/accept-screen-properties.fixtures.spec';
-import { Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { AcceptScreenCarrier, AcceptScreenProperties } from '../../interfaces';
 import { AcceptScreenService } from '../accept-screen/accept-screen.service';
 
@@ -12,6 +13,8 @@ import { AcceptScreenStoreService } from './accept-screen-store.service';
 
 describe('AcceptScreenStoreService', () => {
   const MOCK_REQUEST_ID: string = '2387283dsbd';
+  const acceptScreenServiceSubjectMock: BehaviorSubject<AcceptScreenProperties> = new BehaviorSubject(MOCK_ACCEPT_SCREEN_PROPERTIES);
+
   let service: AcceptScreenStoreService;
   let acceptScreenService: AcceptScreenService;
   let expectedAcceptScreenProperties: AcceptScreenProperties;
@@ -25,7 +28,7 @@ describe('AcceptScreenStoreService', () => {
           provide: AcceptScreenService,
           useValue: {
             getAcceptScreenProperties(): Observable<AcceptScreenProperties> {
-              return of(MOCK_ACCEPT_SCREEN_PROPERTIES);
+              return acceptScreenServiceSubjectMock.asObservable();
             },
           },
         },
@@ -46,7 +49,7 @@ describe('AcceptScreenStoreService', () => {
         expectedAcceptScreenProperties = newProperties;
       });
 
-      service.initialize$(MOCK_REQUEST_ID).subscribe();
+      service.initialize(MOCK_REQUEST_ID);
       tick();
     }));
 
@@ -63,11 +66,11 @@ describe('AcceptScreenStoreService', () => {
       const carrierPositionUpdatedByUser: number = 1;
 
       beforeEach(fakeAsync(() => {
-        service.selectedDropOffModeByUser$.subscribe((newModeSelectedByUser: CARRIER_DROP_OFF_MODE) => {
-          expectedDropOffMode = newModeSelectedByUser;
+        service.carrierSelectedIndex$.subscribe((newCarrierSelected: CARRIER_DROP_OFF_MODE) => {
+          expectedDropOffMode = newCarrierSelected;
         });
 
-        service.notifySelectedDropOffModeByUser(carrierPositionUpdatedByUser);
+        service.selectNewDropOffMode(carrierPositionUpdatedByUser);
         tick();
       }));
 
@@ -88,28 +91,23 @@ describe('AcceptScreenStoreService', () => {
         expect(expectedAcceptScreenProperties).toStrictEqual(MOCK_ACCEPT_SCREEN_PROPERTIES_SELECTED_HPU);
       });
     });
-  });
 
-  describe('when we update the accept screen store', () => {
-    let expectedAcceptScreenProperties: AcceptScreenProperties;
+    describe('and when we update the accept screen store', () => {
+      beforeEach(fakeAsync(() => {
+        acceptScreenServiceSubjectMock.next(MOCK_ACCEPT_SCREEN_PROPERTIES_WITHOUT_SELLER_ADDRESS);
 
-    beforeEach(fakeAsync(() => {
-      spyOn(acceptScreenService, 'getAcceptScreenProperties').and.callThrough();
-      service.properties$.subscribe((newProperties: AcceptScreenProperties) => {
-        expectedAcceptScreenProperties = newProperties;
+        service.update(MOCK_REQUEST_ID);
+        tick();
+      }));
+
+      it('should request the accept screen properties', () => {
+        expect(acceptScreenService.getAcceptScreenProperties).toHaveBeenCalledTimes(1);
+        expect(acceptScreenService.getAcceptScreenProperties).toHaveBeenCalledWith(MOCK_REQUEST_ID);
       });
 
-      service.update(MOCK_REQUEST_ID);
-      tick();
-    }));
-
-    it('should request the accept screen properties', () => {
-      expect(acceptScreenService.getAcceptScreenProperties).toHaveBeenCalledTimes(1);
-      expect(acceptScreenService.getAcceptScreenProperties).toHaveBeenCalledWith(MOCK_REQUEST_ID);
-    });
-
-    it('should update the accept screen store properties ', () => {
-      expect(expectedAcceptScreenProperties).toStrictEqual(MOCK_ACCEPT_SCREEN_PROPERTIES);
+      it('should update the accept screen store properties ', () => {
+        expect(expectedAcceptScreenProperties).toStrictEqual(MOCK_ACCEPT_SCREEN_PROPERTIES_WITHOUT_SELLER_ADDRESS);
+      });
     });
   });
 });
