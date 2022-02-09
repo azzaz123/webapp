20e5efe94efd043c41c4c5e77180500c74ffd360
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-02-09 13:12:39 +0100

    [WPA-9953]: Handle edit item inpu validations

--- src/app/private/features/chat/modules/delivery-banner/modals/edit-item-sale-price-modal/edit-item-sale-price-modal.component.ts
+++ src/app/private/features/chat/modules/delivery-banner/modals/edit-item-sale-price-modal/edit-item-sale-price-modal.component.ts
@@ -1,12 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'tsl-edit-item-sale-price-modal',
   templateUrl: './edit-item-sale-price-modal.component.html',
   styleUrls: ['./edit-item-sale-price-modal.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EditItemSalePriceModalComponent implements OnInit {
-  constructor() {}
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {}
+  public newItemSalePriceForm: FormGroup;
+  public isInvalidInput: boolean = false;
+  public isButtonDisabled: boolean = true;
+
+  private readonly MIN_ITEM_PRICE: number = 1;
+  private readonly MAX_ITEM_PRICE: number = 1000;
+
+  ngOnInit(): void {
+    this.buildForm();
+    this.attachListeners();
+  }
+
+  private attachListeners(): void {
+    this.newPriceFormControl.valueChanges.subscribe(() => this.updateButtonDisabledStyles());
+  }
+
+  private buildForm(): void {
+    this.newItemSalePriceForm = this.formBuilder.group({
+      newPrice: [null, [Validators.required, Validators.min(this.MIN_ITEM_PRICE), Validators.max(this.MAX_ITEM_PRICE)]],
+    });
+  }
+
+  public handleSubmit(): void {
+    this.updateInputValidationStyles();
+  }
+
+  private get newPriceFormControl(): FormControl {
+    return this.newItemSalePriceForm.get('newPrice') as FormControl;
+  }
+
+  private updateInputValidationStyles(): void {
+    this.isInvalidInput = !!this.newPriceFormControl.errors;
+  }
+
+  private updateButtonDisabledStyles(): void {
+    this.isButtonDisabled = this.newPriceFormControl.errors?.required;
+  }
 }
--- src/app/private/features/chat/modules/delivery-banner/modals/edit-item-sale-price-modal/edit-item-sale-price-modal.module.ts
+++ src/app/private/features/chat/modules/delivery-banner/modals/edit-item-sale-price-modal/edit-item-sale-price-modal.module.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from '@shared/button/button.module';
 import { SvgIconModule } from '@shared/svg-icon/svg-icon.module';
 import { EditItemSalePriceModalComponent } from './edit-item-sale-price-modal.component';
 
 @NgModule({
-  imports: [ButtonModule, SvgIconModule],
+  imports: [CommonModule, ButtonModule, SvgIconModule, ReactiveFormsModule],
   declarations: [EditItemSalePriceModalComponent],
 })
 export class EditItemSalePriceModalModule {}
