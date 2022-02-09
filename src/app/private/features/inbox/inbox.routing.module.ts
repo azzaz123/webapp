eeb4fe17e4f46bfbf28be2b0890e425dcec72cf1
Àlex Delgado <alejandro.delgado@wallapop.com>
2022-02-09 16:37:07 +0100

    [WPA-9903]: solve missplace capital letter

--- src/app/private/features/inbox/inbox.routing.module.ts
+++ src/app/private/features/inbox/inbox.routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
 import { NgxPermissionsGuard } from 'ngx-permissions';
 import { NotificationsInboxComponent } from './components/notifications/notifications-inbox.component';
-import { InboxComponent } from './pages/Inbox.component';
+import { InboxComponent } from './pages/inbox.component';
 
 const routes: Route[] = [
   {
