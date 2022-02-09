7f2a8dd4e348301ed8aa68869536ce4bf0019b7c
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-02-09 11:55:36 +0100

    [WPA-9953]: Add basic edit price modal layout

--- src/app/private/features/chat/modules/delivery-banner/modals/edit-item-sale-price-modal/edit-item-sale-price-modal.module.ts
+++ src/app/private/features/chat/modules/delivery-banner/modals/edit-item-sale-price-modal/edit-item-sale-price-modal.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
+import { ButtonModule } from '@shared/button/button.module';
+import { SvgIconModule } from '@shared/svg-icon/svg-icon.module';
 import { EditItemSalePriceModalComponent } from './edit-item-sale-price-modal.component';
 
 @NgModule({
+  imports: [ButtonModule, SvgIconModule],
   declarations: [EditItemSalePriceModalComponent],
 })
 export class EditItemSalePriceModalModule {}
