05075578a26f7c832752e0b37335b5fc44bc28ad
Alex Masnou <alex.masnou@wallapop.com>
2022-02-11 11:49:03 +0100

    [WPA-10325]: Lint InboxItem

--- src/app/private/features/chat/core/model/inbox-item.ts
+++ src/app/private/features/chat/core/model/inbox-item.ts
@@ -18,6 +18,14 @@ export class InboxImage {
 }
 
 export class InboxItem {
+  public sold = false;
+  public reserved = false;
+  public published = false;
+  public unpublished = false;
+  public notAvailable = false;
+  public views: number;
+  public favorites: number;
+
   constructor(
     private _id: string,
     private _price: InboxItemPrice,
@@ -31,13 +39,13 @@ export class InboxItem {
     this.mapStatusToFlags(this.status);
   }
 
-  public sold = false;
-  public reserved = false;
-  public published = false;
-  public unpublished = false;
-  public notAvailable = false;
-  public views: number;
-  public favorites: number;
+  public setFakeImage(image: string) {
+    this._mainImage = {
+      urls_by_size: {
+        small: '',
+      },
+    };
+  }
 
   private mapStatusToFlags(status: string) {
     switch (status) {
@@ -100,14 +108,6 @@ export class InboxItem {
 
   set itemSlug(value: string) {
     this._itemSlug = value;
-  }
-
-  public setFakeImage(image: string) {
-    this._mainImage = {
-      urls_by_size: {
-        small: '',
-      },
-    };
   }
 }
 
