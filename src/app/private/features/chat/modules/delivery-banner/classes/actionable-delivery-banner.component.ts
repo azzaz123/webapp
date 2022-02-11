5e028778b10ad881171ec1c03b453726806cb373
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-02-11 18:45:33 +0100

    [WPA-9956]: Move output of banner CTA to abstract actionable class component

--- src/app/private/features/chat/modules/delivery-banner/classes/actionable-delivery-banner.component.ts
+++ src/app/private/features/chat/modules/delivery-banner/classes/actionable-delivery-banner.component.ts
@@ -1,8 +1,14 @@
-import { Directive, EventEmitter, Output } from '@angular/core';
+import { Directive, EventEmitter, Input, Output } from '@angular/core';
 import { DELIVERY_BANNER_ACTION } from '../enums/delivery-banner-action.enum';
+import { ActionableDeliveryBanner } from '../interfaces/actionable-delivery-banner.interface';
 import { AbstractDeliveryBannerComponent } from './abstract-delivery-banner.component';
 
 @Directive()
 export abstract class ActionableDeliveryBannerComponent extends AbstractDeliveryBannerComponent {
-  @Output() clickedCTA: EventEmitter<DELIVERY_BANNER_ACTION>;
+  @Input() bannerProperties: ActionableDeliveryBanner;
+  @Output() clickedCTA: EventEmitter<DELIVERY_BANNER_ACTION> = new EventEmitter<DELIVERY_BANNER_ACTION>();
+
+  public handleClickCTA(): void {
+    this.clickedCTA.emit(this.bannerProperties.action);
+  }
 }
--- src/app/private/features/chat/modules/delivery-banner/components/banners/buy-banner/buy-banner.component.ts
+++ src/app/private/features/chat/modules/delivery-banner/components/banners/buy-banner/buy-banner.component.ts
@@ -11,9 +11,8 @@ import { PriceableDeliveryBanner } from '../../../interfaces/priceable-delivery-
   styleUrls: ['./buy-banner.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BuyBannerComponent implements ActionableDeliveryBannerComponent {
+export class BuyBannerComponent extends ActionableDeliveryBannerComponent {
   @Input() bannerProperties: PriceableDeliveryBanner & ActionableDeliveryBanner;
-  @Output() clickedCTA: EventEmitter<DELIVERY_BANNER_ACTION> = new EventEmitter<DELIVERY_BANNER_ACTION>();
 
   public readonly bannerStyleProperties: BannerSpecifications = {
     dismissible: false,
@@ -28,8 +27,4 @@ export class BuyBannerComponent implements ActionableDeliveryBannerComponent {
   ];
 
   public readonly lottie: string = this.lotties[0];
-
-  public handleClickBuy(): void {
-    this.clickedCTA.emit(this.bannerProperties.action);
-  }
 }
