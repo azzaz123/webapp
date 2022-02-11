690de231933bc1a67ba0890678fabcd9f9540cd6
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-02-11 16:29:19 +0100

    [WPA-9956]: Get delivery banner properties as seller

--- src/app/private/features/chat/modules/delivery-conversation-context/services/delivery-conversation-context-as-seller/delivery-conversation-context-as-seller.service.spec.ts
+++ src/app/private/features/chat/modules/delivery-conversation-context/services/delivery-conversation-context-as-seller/delivery-conversation-context-as-seller.service.spec.ts
@@ -1,24 +1,65 @@
-import { TestBed } from '@angular/core/testing';
-import { MOCK_INBOX_CONVERSATION_AS_SELLER, MOCK_INBOX_CONVERSATION_WITH_DELIVERY_THIRD_VOICES } from '@fixtures/chat';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { SellerRequestsApiService } from '@api/delivery/seller/requests/seller-requests-api.service';
+import { MOCK_INBOX_CONVERSATION_AS_SELLER } from '@fixtures/chat';
+import { MOCK_SELLER_REQUEST } from '@fixtures/private/delivery/seller-requests/seller-request.fixtures.spec';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { TRXAwarenessModalComponent } from '@private/features/delivery/modals/trx-awareness-modal/trx-awareness-modal.component';
+import { of } from 'rxjs';
+import { SELLER_EDIT_PRICE_BANNER_PROPERTIES } from '../../../delivery-banner/constants/delivery-banner-configs';
+import { ActionableDeliveryBanner } from '../../../delivery-banner/interfaces/actionable-delivery-banner.interface';
 
 import { DeliveryConversationContextAsSellerService } from './delivery-conversation-context-as-seller.service';
 
 describe('DeliveryConversationContextAsSellerService', () => {
   let service: DeliveryConversationContextAsSellerService;
   let modalService: NgbModal;
+  let sellerRequestsApiService: SellerRequestsApiService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [DeliveryConversationContextAsSellerService, { provide: NgbModal, useValue: { open: () => {} } }],
+      providers: [
+        DeliveryConversationContextAsSellerService,
+        { provide: NgbModal, useValue: { open: () => {} } },
+        { provide: SellerRequestsApiService, useValue: { getRequestsByBuyerAndItem: () => {} } },
+      ],
     });
     service = TestBed.inject(DeliveryConversationContextAsSellerService);
     modalService = TestBed.inject(NgbModal);
+    sellerRequestsApiService = TestBed.inject(SellerRequestsApiService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('when asking for seller context', () => {
+    describe('when seller has received previously buy requests for current item', () => {
+      beforeEach(() => {
+        spyOn(sellerRequestsApiService, 'getRequestsByBuyerAndItem').and.returnValue(of([MOCK_SELLER_REQUEST]));
+      });
+
+      it('should hide banner', fakeAsync(() => {
+        service.getBannerPropertiesAsSeller(MOCK_INBOX_CONVERSATION_AS_SELLER).subscribe((result) => {
+          expect(result).toBeFalsy();
+        });
+        tick();
+      }));
+    });
+
+    describe('when seller has received no buy requests from buyer for current item', () => {
+      beforeEach(() => {
+        spyOn(sellerRequestsApiService, 'getRequestsByBuyerAndItem').and.returnValue(of([]));
+      });
+
+      it('should show edit price banner', fakeAsync(() => {
+        const expectedBanner: ActionableDeliveryBanner = SELLER_EDIT_PRICE_BANNER_PROPERTIES;
+
+        service.getBannerPropertiesAsSeller(MOCK_INBOX_CONVERSATION_AS_SELLER).subscribe((result) => {
+          expect(result).toEqual(expectedBanner);
+        });
+        tick();
+      }));
+    });
   });
 
   describe('when handling CTA button', () => {
--- src/app/private/features/chat/modules/delivery-conversation-context/services/delivery-conversation-context-as-seller/delivery-conversation-context-as-seller.service.ts
+++ src/app/private/features/chat/modules/delivery-conversation-context/services/delivery-conversation-context-as-seller/delivery-conversation-context-as-seller.service.ts
@@ -1,20 +1,42 @@
 import { Injectable } from '@angular/core';
+import { SellerRequest } from '@api/core/model/delivery/seller-requests/seller-request.interface';
+import { SellerRequestsApiService } from '@api/delivery/seller/requests/seller-requests-api.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { InboxConversation } from '@private/features/chat/core/model';
 import { TRXAwarenessModalComponent } from '@private/features/delivery/modals/trx-awareness-modal/trx-awareness-modal.component';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { SELLER_EDIT_PRICE_BANNER_PROPERTIES } from '../../../delivery-banner/constants/delivery-banner-configs';
 import { DeliveryBanner } from '../../../delivery-banner/interfaces/delivery-banner.interface';
 
 @Injectable()
 export class DeliveryConversationContextAsSellerService {
-  constructor(private modalService: NgbModal) {}
+  constructor(private modalService: NgbModal, private sellerRequestsApiService: SellerRequestsApiService) {}
 
-  // TODO: Implement method for getting banner properties as seller
   public getBannerPropertiesAsSeller(conversation: InboxConversation): Observable<DeliveryBanner | null> {
-    return of(null);
+    const { item, user: buyer } = conversation;
+    const { id: itemHash } = item;
+    const { id: buyerHash } = buyer;
+
+    return this.sellerRequestsApiService.getRequestsByBuyerAndItem(buyerHash, itemHash).pipe(map(this.mapSellerRequestsToBannerProperties));
   }
 
   public handleThirdVoiceCTAClick(): void {
     this.modalService.open(TRXAwarenessModalComponent);
+  }
+
+  private mapSellerRequestsToBannerProperties(sellerRequests: SellerRequest[]): DeliveryBanner | null {
+    const sellerHasNoRequests: boolean = sellerRequests.length === 0;
+    const sellerHasRequests: boolean = !sellerHasNoRequests;
+
+    if (sellerHasRequests) {
+      return null;
+    }
+
+    if (sellerHasNoRequests) {
+      return SELLER_EDIT_PRICE_BANNER_PROPERTIES;
+    }
+
+    return null;
   }
 }
