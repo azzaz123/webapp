a76fb4e65a1ee9ed4a654754c9b794e0dfd71365
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-02-11 19:01:28 +0100

    [WPA-9956]: Add tests for edit price banner component

--- src/app/private/features/chat/modules/delivery-banner/components/banners/edit-price-banner/edit-price-banner.component.spec.ts
+++ src/app/private/features/chat/modules/delivery-banner/components/banners/edit-price-banner/edit-price-banner.component.spec.ts
@@ -1,4 +1,8 @@
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { ButtonComponent } from '@shared/button/button.component';
+import { SELLER_EDIT_PRICE_BANNER_PROPERTIES } from '../../../constants/delivery-banner-configs';
 
 import { EditPriceBannerComponent } from './edit-price-banner.component';
 
@@ -8,17 +12,32 @@ describe('EditPriceBannerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [EditPriceBannerComponent],
+      declarations: [EditPriceBannerComponent, ButtonComponent],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(EditPriceBannerComponent);
     component = fixture.componentInstance;
+    component.bannerProperties = SELLER_EDIT_PRICE_BANNER_PROPERTIES;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('and when the user clicks on the CTA', () => {
+    beforeEach(() => {
+      spyOn(component.clickedCTA, 'emit');
+      const buttonElement: DebugElement = fixture.debugElement.query(By.directive(ButtonComponent));
+      buttonElement.nativeElement.click();
+    });
+
+    it('should emit the action', () => {
+      expect(component.clickedCTA.emit).toHaveBeenCalledTimes(1);
+      expect(component.clickedCTA.emit).toHaveBeenCalledWith(SELLER_EDIT_PRICE_BANNER_PROPERTIES.action);
+    });
   });
 });
