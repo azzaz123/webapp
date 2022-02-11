5062fc7793a1c9944cefccf0c61e172e1052908e
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-02-11 17:08:02 +0100

    [WPA-9956]: Implement actionable delivery banner class in banners

--- src/app/private/features/chat/modules/delivery-banner/components/banners/buy-banner/buy-banner.component.ts
+++ src/app/private/features/chat/modules/delivery-banner/components/banners/buy-banner/buy-banner.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { BannerSpecifications } from '@shared/banner/banner-specifications.interface';
+import { ActionableDeliveryBannerComponent } from '../../../classes/actionable-delivery-banner.component';
 import { DELIVERY_BANNER_ACTION } from '../../../enums/delivery-banner-action.enum';
 import { ActionableDeliveryBanner } from '../../../interfaces/actionable-delivery-banner.interface';
 import { PriceableDeliveryBanner } from '../../../interfaces/priceable-delivery-banner.interface';
@@ -10,7 +11,7 @@ import { PriceableDeliveryBanner } from '../../../interfaces/priceable-delivery-
   styleUrls: ['./buy-banner.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BuyBannerComponent {
+export class BuyBannerComponent implements ActionableDeliveryBannerComponent {
   @Input() bannerProperties: PriceableDeliveryBanner & ActionableDeliveryBanner;
   @Output() clickedCTA: EventEmitter<DELIVERY_BANNER_ACTION> = new EventEmitter<DELIVERY_BANNER_ACTION>();
 
--- src/app/private/features/chat/modules/delivery-banner/components/banners/edit-price-banner/edit-price-banner.component.ts
+++ src/app/private/features/chat/modules/delivery-banner/components/banners/edit-price-banner/edit-price-banner.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ActionableDeliveryBannerComponent } from '../../../classes/actionable-delivery-banner.component';
+import { DELIVERY_BANNER_ACTION } from '../../../enums/delivery-banner-action.enum';
+import { ActionableDeliveryBanner } from '../../../interfaces/actionable-delivery-banner.interface';
+import { PriceableDeliveryBanner } from '../../../interfaces/priceable-delivery-banner.interface';
 
 @Component({
   selector: 'tsl-edit-price-banner',
   templateUrl: './edit-price-banner.component.html',
   styleUrls: ['./edit-price-banner.component.scss'],
 })
-export class EditPriceBannerComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class EditPriceBannerComponent implements ActionableDeliveryBannerComponent {
+  @Input() bannerProperties: PriceableDeliveryBanner & ActionableDeliveryBanner;
+  @Output() clickedCTA: EventEmitter<DELIVERY_BANNER_ACTION> = new EventEmitter<DELIVERY_BANNER_ACTION>();
 }
