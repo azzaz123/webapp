f73e6327b4952904e199d788886607128702c566
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-02-11 14:35:28 +0100

    [WPA-9956]: Create edit price banner module

---
+++ src/app/private/features/chat/modules/delivery-banner/components/banners/edit-price-banner/edit-price-banner.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EditPriceBannerComponent } from './edit-price-banner.component';
+
+@NgModule({
+  declarations: [EditPriceBannerComponent],
+  imports: [CommonModule],
+})
+export class EditPriceBannerModule {}
