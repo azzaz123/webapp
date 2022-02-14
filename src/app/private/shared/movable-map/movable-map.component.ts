dce2879ef5fd3f06dbbc0e3865b6d85adff837fc
Laia Lopez <laialopezz99@gmail.com>
2022-02-14 14:39:29 +0100

    [WPA-9949]: [MovableMap] Emit on drag end

--- src/app/private/shared/movable-map/movable-map.component.ts
+++ src/app/private/shared/movable-map/movable-map.component.ts
@@ -1,53 +1,94 @@
-import { Component, ViewChild, Input, AfterViewInit, ElementRef, Output, EventEmitter } from '@angular/core';
-import { Location } from '@api/core/model';
+import {
+  Component,
+  ViewChild,
+  Input,
+  AfterViewInit,
+  ElementRef,
+  Output,
+  EventEmitter,
+  ChangeDetectionStrategy,
+  Inject,
+  LOCALE_ID,
+} from '@angular/core';
+import { Location, LocationWithRatio } from '@api/core/model';
+import { APP_LOCALE } from '@configs/subdomains.config';
+import { DEFAULT_LOCATIONS } from '@public/features/search/core/services/constants/default-locations';
+import { LabeledSearchLocation } from '@public/features/search/core/services/interfaces/search-location.interface';
 import { HereMapsService } from '@shared/geolocation/here-maps/here-maps.service';
 import { distinctUntilChanged } from 'rxjs/operators';
+
 @Component({
   selector: 'tsl-movable-map',
   templateUrl: './movable-map.component.html',
   styleUrls: ['./movable-map.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MovableMapComponent implements AfterViewInit {
-  @Input() centerCoordinates: Location = { latitude: 40.42028, longitude: -3.70578 };
-  @Input() markers: Location[];
-  @Output() markerClick: EventEmitter<Location> = new EventEmitter();
+  @Input() centerCoordinates: Location;
+  @Input() markers: Location[] = [];
+  @Input() zoom: number = 8;
+  @Input() ratio: number = 1;
+
+  @Output() dragEnd: EventEmitter<LocationWithRatio> = new EventEmitter();
+  @Output()
+  markerClick: EventEmitter<Location> = new EventEmitter();
   @ViewChild('map', { static: true })
   mapEl: ElementRef;
 
   public readonly loading$ = this.hereMapsService.isLibraryLoading$();
   private map: H.Map;
 
-  constructor(private hereMapsService: HereMapsService) {}
+  constructor(@Inject(LOCALE_ID) private locale: APP_LOCALE, private hereMapsService: HereMapsService) {}
 
   public ngAfterViewInit(): void {
+    this.centerCoordinates = this.centerCoordinates || this.fallbackCenterCoordinates;
     this.initHereMaps();
   }
 
   private initializeMap(): H.Map {
-    const defaultLayers = this.hereMapsService.platform.createDefaultLayers();
+    const defaultLayers: H.service.DefaultLayers = this.hereMapsService.platform.createDefaultLayers();
     const map: H.Map = new H.Map(this.mapEl.nativeElement, defaultLayers.vector.normal.map, {
-      zoom: 10,
+      zoom: this.zoom,
+      pixelRatio: this.ratio,
       center: { lat: this.centerCoordinates.latitude, lng: this.centerCoordinates.longitude },
     });
     const implementInteractions: H.mapevents.Behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
     const defaultUI: H.ui.UI = H.ui.UI.createDefault(map, defaultLayers);
 
-    this.addInfoBubble(map);
+    this.emitOnDragEnd(map);
+    this.addGroupMarker(map);
 
     return map;
   }
 
-  private addInfoBubble(map: H.Map): void {
+  private emitOnDragEnd(map: H.Map): void {
+    map.addEventListener('dragend', () => {
+      const updatedLocation: H.geo.IPoint = map.getCenter();
+      // TODO: Change it to radio		Date: 2022/02/14
+      const updatedZoom: number = map.getZoom();
+
+      this.dragEnd.emit({
+        latitude: updatedLocation.lat,
+        longitude: updatedLocation.lng,
+        ratioInKm: updatedZoom,
+      });
+    });
+  }
+
+  private addGroupMarker(map: H.Map): void {
     const group: H.map.Group = new H.map.Group();
     map.addObject(group);
 
+    this.emitOnTapMarker(group);
+
+    this.markers.forEach((marker: Location) => this.addMarkerToGroup(group, { lng: marker.longitude, lat: marker.latitude }));
+  }
+
+  private emitOnTapMarker(group: H.map.Group): void {
     group.addEventListener('tap', (evt: H.util.Event) => {
       const currentLocation: H.geo.IPoint = evt.target.b;
       this.markerClick.emit({ latitude: currentLocation.lat, longitude: currentLocation.lng });
     });
-
-    this.addMarkerToGroup(group, { lng: -3.6833, lat: 40.4 });
-    this.addMarkerToGroup(group, { lng: 2.169, lat: 41.387 });
   }
 
   private addMarkerToGroup(group: H.map.Group, coordinate: H.geo.IPoint): void {
@@ -64,5 +105,14 @@ export class MovableMapComponent implements AfterViewInit {
           this.map = this.initializeMap();
         }
       });
+  }
+
+  private get fallbackCenterCoordinates(): Location {
+    const fallbackLocation: LabeledSearchLocation = DEFAULT_LOCATIONS[this.locale] || DEFAULT_LOCATIONS.en;
+
+    return {
+      latitude: +fallbackLocation.latitude,
+      longitude: +fallbackLocation.longitude,
+    };
   }
 }
