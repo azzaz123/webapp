12582f84ea90a734bb0f71c0bb3a4765ea93ba4b
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-02-14 12:10:31 +0100

    [WPA-9957]: Add edit price modal in edit price banner module

--- src/app/private/features/chat/modules/delivery-banner/components/banners/edit-price-banner/edit-price-banner.module.ts
+++ src/app/private/features/chat/modules/delivery-banner/components/banners/edit-price-banner/edit-price-banner.module.ts
@@ -4,10 +4,11 @@ import { EditPriceBannerComponent } from './edit-price-banner.component';
 import { SvgIconModule } from '@shared/svg-icon/svg-icon.module';
 import { ButtonModule } from '@shared/button/button.module';
 import { BannerModule } from '@shared/banner/banner.module';
+import { EditItemSalePriceModalModule } from './modals/edit-item-sale-price-modal/edit-item-sale-price-modal.module';
 
 @NgModule({
   declarations: [EditPriceBannerComponent],
-  imports: [CommonModule, ButtonModule, SvgIconModule, BannerModule],
+  imports: [CommonModule, ButtonModule, SvgIconModule, BannerModule, EditItemSalePriceModalModule],
   exports: [EditPriceBannerComponent],
 })
 export class EditPriceBannerModule {}
