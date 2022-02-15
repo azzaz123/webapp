fe75baca89c35ff53f56cd2f8eaf16cfcc8009fe
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-02-15 14:39:47 +0100

    [WPA-10457]: Add content for ask seller for shipping banner

--- src/app/private/features/chat/modules/delivery-banner/components/banners/ask-seller-for-shipping-banner/ask-seller-for-shipping-banner.component.ts
+++ src/app/private/features/chat/modules/delivery-banner/components/banners/ask-seller-for-shipping-banner/ask-seller-for-shipping-banner.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { BannerSpecifications } from '@shared/banner/banner-specifications.interface';
+import { AbstractDeliveryBannerComponent } from '../../../classes/abstract-delivery-banner.component';
 
 @Component({
   selector: 'tsl-ask-seller-for-shipping-banner',
   templateUrl: './ask-seller-for-shipping-banner.component.html',
   styleUrls: ['./ask-seller-for-shipping-banner.component.scss'],
 })
-export class AskSellerForShippingBannerComponent implements OnInit {
-  constructor() {}
+export class AskSellerForShippingBannerComponent extends AbstractDeliveryBannerComponent {
+  public readonly iconPath: string = 'assets/icons/crossed-box.svg';
 
-  ngOnInit(): void {}
+  public readonly bannerStyleProperties: BannerSpecifications = {
+    dismissible: false,
+    type: 'info',
+    isFullHeight: true,
+  };
 }
--- src/app/private/features/chat/modules/delivery-banner/components/banners/ask-seller-for-shipping-banner/ask-seller-for-shipping-banner.module.ts
+++ src/app/private/features/chat/modules/delivery-banner/components/banners/ask-seller-for-shipping-banner/ask-seller-for-shipping-banner.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
+import { BannerModule } from '@shared/banner/banner.module';
+import { SvgIconModule } from '@shared/svg-icon/svg-icon.module';
 import { AskSellerForShippingBannerComponent } from './ask-seller-for-shipping-banner.component';
 
 @NgModule({
+  imports: [BannerModule, SvgIconModule],
   declarations: [AskSellerForShippingBannerComponent],
   exports: [AskSellerForShippingBannerComponent],
 })
