4500b649928af732fd1c5e4faedda3671cd792d0
antoni-casas <toni.casas@wallapop.com>
2022-02-15 18:04:50 +0100

    [WPA-9134]: feat: Use summary payment method component

--- src/app/private/features/payview/modules/summary/components/overview/payview-summary-overview.component.spec.ts
+++ src/app/private/features/payview/modules/summary/components/overview/payview-summary-overview.component.spec.ts
@@ -8,6 +8,7 @@ import { MOCK_PAYVIEW_STATE } from '@fixtures/private/delivery/payview/payview-s
 import { PayviewSummaryCostDetailComponent } from '@private/features/payview/modules/summary/components/cost-detail/payview-summary-cost-detail.component';
 import { PayviewSummaryHeaderComponent } from '@private/features/payview/modules/summary/components/header/payview-summary-header.component';
 import { PayviewSummaryOverviewComponent } from '@private/features/payview/modules/summary/components/overview/payview-summary-overview.component';
+import { PayviewSummaryPaymentMethodComponent } from '@private/features/payview/modules/summary/components/payment-method/payview-summary-payment-method.component';
 import { SvgIconComponent } from '@shared/svg-icon/svg-icon.component';
 
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
@@ -19,6 +20,7 @@ describe('PayviewSummaryOverviewComponent', () => {
   const payviewSummaryCostDetail: string = 'tsl-payview-summary-cost-detail';
   const payviewSummaryHeader: string = 'tsl-payview-summary-header';
   const payviewSummaryHelp: string = '#helpLink';
+  const payviewSummaryPaymentMethod: string = 'tsl-payview-summary-payment-method';
 
   let activeModalService: NgbActiveModal;
   let component: PayviewSummaryOverviewComponent;
@@ -28,7 +30,13 @@ describe('PayviewSummaryOverviewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [PayviewSummaryCostDetailComponent, PayviewSummaryHeaderComponent, PayviewSummaryOverviewComponent, SvgIconComponent],
+      declarations: [
+        PayviewSummaryCostDetailComponent,
+        PayviewSummaryHeaderComponent,
+        PayviewSummaryOverviewComponent,
+        PayviewSummaryPaymentMethodComponent,
+        SvgIconComponent,
+      ],
       imports: [HttpClientTestingModule],
       providers: [
         {
@@ -155,6 +163,119 @@ describe('PayviewSummaryOverviewComponent', () => {
         const target = debugElement.query(By.css(payviewSummaryCostDetail));
 
         expect((target.componentInstance as PayviewSummaryCostDetailComponent).productName).toEqual(MOCK_PAYVIEW_STATE.item.title);
+      });
+    });
+
+    describe('WHEN the preferences have been reported', () => {
+      beforeEach(() => {
+        fixture = TestBed.createComponent(PayviewSummaryOverviewComponent);
+        component = fixture.componentInstance;
+        debugElement = fixture.debugElement;
+
+        component.payviewState = MOCK_PAYVIEW_STATE;
+
+        fixture.detectChanges();
+      });
+
+      it('should show the summary payment method', () => {
+        const target = debugElement.query(By.css(payviewSummaryPaymentMethod));
+
+        expect(target).toBeTruthy();
+      });
+
+      it('should assign the corresponding card', () => {
+        const target = debugElement.query(By.css(payviewSummaryPaymentMethod));
+
+        expect((target.componentInstance as PayviewSummaryPaymentMethodComponent).creditCard).toEqual(MOCK_PAYVIEW_STATE.payment.card);
+      });
+
+      it('should assign the corresponding payment method', () => {
+        const target = debugElement.query(By.css(payviewSummaryPaymentMethod));
+
+        expect((target.componentInstance as PayviewSummaryPaymentMethodComponent).paymentMethod).toEqual(
+          MOCK_PAYVIEW_STATE.payment.preferences.preferences
+        );
+      });
+    });
+
+    describe('WHEN the item has not been reported', () => {
+      beforeEach(() => {
+        fixture = TestBed.createComponent(PayviewSummaryOverviewComponent);
+        component = fixture.componentInstance;
+        debugElement = fixture.debugElement;
+
+        component.payviewState = { ...MOCK_PAYVIEW_STATE };
+        component.payviewState.item = null;
+        component.payviewState.costs = null;
+
+        fixture.detectChanges();
+      });
+
+      it('should not show the summary header', () => {
+        const target = debugElement.query(By.css(payviewSummaryHeader));
+
+        expect(target).toBeFalsy();
+      });
+    });
+
+    describe('WHEN the delivery methods have not been reported', () => {
+      beforeEach(() => {
+        fixture = TestBed.createComponent(PayviewSummaryOverviewComponent);
+        component = fixture.componentInstance;
+        debugElement = fixture.debugElement;
+
+        component.payviewState = { ...MOCK_PAYVIEW_STATE };
+        component.payviewState.delivery.methods = null;
+        component.payviewState.costs = null;
+
+        fixture.detectChanges();
+      });
+
+      it('should not show the summary header', () => {
+        const target = debugElement.query(By.css(payviewSummaryHeader));
+
+        expect(target).toBeFalsy();
+      });
+    });
+
+    describe('WHEN the costs have not been reported', () => {
+      beforeEach(() => {
+        fixture = TestBed.createComponent(PayviewSummaryOverviewComponent);
+        component = fixture.componentInstance;
+        debugElement = fixture.debugElement;
+
+        component.payviewState = { ...MOCK_PAYVIEW_STATE };
+        component.payviewState.delivery.methods = null;
+        component.payviewState.costs = null;
+
+        fixture.detectChanges();
+      });
+
+      it('should not show the summary body', () => {
+        const target = debugElement.query(By.css(payviewSummaryCostDetail));
+
+        expect(target).toBeFalsy();
+      });
+    });
+
+    describe('WHEN the payment preferences have not been reported', () => {
+      beforeEach(() => {
+        fixture = TestBed.createComponent(PayviewSummaryOverviewComponent);
+        component = fixture.componentInstance;
+        debugElement = fixture.debugElement;
+
+        component.payviewState = { ...MOCK_PAYVIEW_STATE };
+        component.payviewState.delivery.methods = null;
+        component.payviewState.costs = null;
+        component.payviewState.payment.preferences.preferences = null;
+
+        fixture.detectChanges();
+      });
+
+      it('should not show the summary footer', () => {
+        const target = debugElement.query(By.css(payviewSummaryPaymentMethod));
+
+        expect(target).toBeFalsy();
       });
     });
   });
--- src/app/private/features/payview/modules/summary/components/overview/payview-summary-overview.component.ts
+++ src/app/private/features/payview/modules/summary/components/overview/payview-summary-overview.component.ts
@@ -1,14 +1,16 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
+import { CreditCard } from '@api/core/model';
 import { CUSTOMER_HELP_PAGE } from '@core/external-links/customer-help/customer-help-constants';
 import { CustomerHelpService } from '@core/external-links/customer-help/customer-help.service';
+import { DeliveryBuyerCalculatorCosts } from '@api/core/model/delivery/buyer/calculator/delivery-buyer-calculator-costs.interface';
 import { DeliveryBuyerDeliveryMethod } from '@api/core/model/delivery/buyer/delivery-methods';
 import { I18nService } from '@core/i18n/i18n.service';
 import { Image } from '@core/user/user-response.interface';
+import { PaymentsUserPaymentPreference } from '@api/core/model/payments/interfaces/payments-user-payment-preference.interface';
 import { PayviewState } from '@private/features/payview/interfaces/payview-state.interface';
 
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { DeliveryBuyerCalculatorCosts } from '@api/core/model/delivery/buyer/calculator/delivery-buyer-calculator-costs.interface';
 
 @Component({
   selector: 'tsl-payview-summary-overview',
@@ -23,6 +25,10 @@ export class PayviewSummaryOverviewComponent {
 
   public closeModal(): void {
     this.activeModal.close();
+  }
+
+  public get creditCard(): CreditCard {
+    return this.payviewState.payment.card;
   }
 
   public get costs(): DeliveryBuyerCalculatorCosts {
@@ -41,12 +47,20 @@ export class PayviewSummaryOverviewComponent {
     return this.payviewState.item.mainImage;
   }
 
+  public get paymentMethod(): PaymentsUserPaymentPreference {
+    return this.payviewState.payment.preferences.preferences;
+  }
+
   public get showBody(): boolean {
     return !!this.payviewState.costs;
   }
 
   public get showHeader(): boolean {
     return !!this.payviewState.item && !!this.payviewState.delivery.methods;
+  }
+
+  public get showPaymentMethod(): boolean {
+    return !!this.payviewState.payment.preferences.preferences;
   }
 
   public get title(): string {
