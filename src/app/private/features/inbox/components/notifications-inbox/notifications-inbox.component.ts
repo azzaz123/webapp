d047db32b9724320e2e656058de87d0e055bb866
Àlex Delgado <alejandro.delgado@wallapop.com>
2022-02-15 18:04:20 +0100

    [WPA-9901]: added trackViewNotificationCenter for analytitcs

--- src/app/private/features/inbox/components/notifications-inbox/notifications-inbox.component.ts
+++ src/app/private/features/inbox/components/notifications-inbox/notifications-inbox.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AnalyticsPageView, ViewNotificationCenter, ANALYTICS_EVENT_NAMES, SCREEN_IDS } from '@core/analytics/analytics-constants';
+import { AnalyticsService } from '@core/analytics/analytics.service';
 import { Notification } from '../../core/interfaces/notification.interface';
 
 @Component({
@@ -6,6 +8,24 @@ import { Notification } from '../../core/interfaces/notification.interface';
   templateUrl: './notifications-inbox.component.html',
   styleUrls: ['./notifications-inbox.component.scss'],
 })
-export class NotificationsInboxComponent {
+export class NotificationsInboxComponent implements OnInit {
   public notifications: Notification[];
+  constructor(private analyticsService: AnalyticsService) {}
+
+  ngOnInit(): void {
+    this.trackViewNotificationCenter();
+  }
+
+  private trackViewNotificationCenter(): void {
+    const event: AnalyticsPageView<ViewNotificationCenter> = {
+      name: ANALYTICS_EVENT_NAMES.ViewNotificationCenter,
+      attributes: {
+        screenId: SCREEN_IDS.NotificationCenter,
+        numberOfTotalNotifications: this.notifications.length,
+        numberOfUnreadNotifications: this.notifications.map((notification) => !notification.isRead).length,
+      },
+    };
+
+    this.analyticsService.trackPageView(event);
+  }
 }
