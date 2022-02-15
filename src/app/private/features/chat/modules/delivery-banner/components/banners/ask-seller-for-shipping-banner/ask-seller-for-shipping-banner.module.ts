2120b250ba1c170ffe288860260173afbb3f7b80
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-02-15 11:42:01 +0100

    [WPA-10457]: Create ask seller for shipping module

---
+++ src/app/private/features/chat/modules/delivery-banner/components/banners/ask-seller-for-shipping-banner/ask-seller-for-shipping-banner.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { AskSellerForShippingBannerComponent } from './ask-seller-for-shipping-banner.component';
+
+@NgModule({
+  declarations: [AskSellerForShippingBannerComponent],
+})
+export class AskSellerForShippingBannerModule {}
