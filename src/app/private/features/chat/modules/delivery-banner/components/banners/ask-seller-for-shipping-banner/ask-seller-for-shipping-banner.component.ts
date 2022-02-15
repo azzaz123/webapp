cffa36cbb26cd35417ccf8e4c2a0e3828d4b0fb7
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-02-15 15:45:10 +0100

    [WPA-10457]: Set onpush strategy for ask seller shipping banner

--- src/app/private/features/chat/modules/delivery-banner/components/banners/ask-seller-for-shipping-banner/ask-seller-for-shipping-banner.component.ts
+++ src/app/private/features/chat/modules/delivery-banner/components/banners/ask-seller-for-shipping-banner/ask-seller-for-shipping-banner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { BannerSpecifications } from '@shared/banner/banner-specifications.interface';
 import { AbstractDeliveryBannerComponent } from '../../../classes/abstract-delivery-banner.component';
 
@@ -6,6 +6,7 @@ import { AbstractDeliveryBannerComponent } from '../../../classes/abstract-deliv
   selector: 'tsl-ask-seller-for-shipping-banner',
   templateUrl: './ask-seller-for-shipping-banner.component.html',
   styleUrls: ['./ask-seller-for-shipping-banner.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AskSellerForShippingBannerComponent extends AbstractDeliveryBannerComponent {
   public readonly iconPath: string = 'assets/icons/crossed-box.svg';
