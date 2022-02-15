87753c07d95c2acb5a1489f58e0a3dc0d34fe84f
Gonzalo Morales <gonzalomorales93@gmail.com>
2022-02-15 19:03:37 +0100

    [WPA-9051]: simplify product types

--- src/app/private/features/bumps/components/subscription-header-checkout/subscription-header-checkout.component.spec.ts
+++ src/app/private/features/bumps/components/subscription-header-checkout/subscription-header-checkout.component.spec.ts
@@ -17,8 +17,4 @@ describe('SubscriptionHeaderCheckoutComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
