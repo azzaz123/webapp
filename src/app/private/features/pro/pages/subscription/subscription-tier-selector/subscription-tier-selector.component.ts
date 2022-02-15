292349ca0cb3ddc8424e90b512bf1c6a70451e4b
Gonzalo Morales <gonzalomorales93@gmail.com>
2022-02-15 16:58:04 +0100

    [WPA-9057]: unify copies

--- src/app/private/features/pro/pages/subscription/subscription-tier-selector/subscription-tier-selector.component.ts
+++ src/app/private/features/pro/pages/subscription/subscription-tier-selector/subscription-tier-selector.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
-import { CATEGORY_SUBSCRIPTIONS_IDS } from '@core/subscriptions/category-subscription-ids';
 import { SubscriptionsResponse, Tier } from '@core/subscriptions/subscriptions.interface';
 
 @Component({
@@ -25,13 +24,7 @@ export class SubscriptionTierSelectorComponent {
 
   public getLimitText(tier: Tier): string {
     return tier.limit
-      ? this.getTextWithLimit(tier)
+      ? $localize`:@@pro_subscription_purchase_subscription_details_list_tier_text:Manage up to ${tier.limit}:INTERPOLATION: items`
       : $localize`:@@pro_subscription_purchase_subscription_details_list_unlimited_tier_text:Manage unlimited active item`;
-  }
-
-  private getTextWithLimit(tier: Tier): string {
-    return this.subscription.category_id === CATEGORY_SUBSCRIPTIONS_IDS.REAL_ESTATE
-      ? $localize`:@@web_profile_pages_subscription_332:List up to ${tier.limit} real estate`
-      : $localize`:@@pro_subscription_purchase_subscription_details_list_tier_text:Manage up to ${tier.limit}:INTERPOLATION: items`;
   }
 }
