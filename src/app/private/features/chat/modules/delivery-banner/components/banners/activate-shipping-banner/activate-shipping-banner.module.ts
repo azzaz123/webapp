e75b24ca74bdf336cbdf73a1c59f7df76cf722cc
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-02-16 14:47:53 +0100

    [WPA-10455]: Create activate shipping banner module

---
+++ src/app/private/features/chat/modules/delivery-banner/components/banners/activate-shipping-banner/activate-shipping-banner.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { ActivateShippingBannerComponent } from './activate-shipping-banner.component';
+
+@NgModule({
+  declarations: [ActivateShippingBannerComponent],
+  exports: [ActivateShippingBannerComponent],
+})
+export class ActivateShippingBannerModule {}
