69b001754cd1096d77a6ab17ae59568d3fb4d25f
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-02-16 16:06:57 +0100

    [WPA-10455]: Set layout for activate shipping banner component

--- src/app/private/features/chat/modules/delivery-banner/components/banners/activate-shipping-banner/activate-shipping-banner.component.ts
+++ src/app/private/features/chat/modules/delivery-banner/components/banners/activate-shipping-banner/activate-shipping-banner.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActionableDeliveryBannerComponent } from '../../../classes/actionable-delivery-banner.component';
 
 @Component({
   selector: 'tsl-activate-shipping-banner',
   templateUrl: './activate-shipping-banner.component.html',
   styleUrls: ['./activate-shipping-banner.component.scss'],
 })
-export class ActivateShippingBannerComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class ActivateShippingBannerComponent extends ActionableDeliveryBannerComponent {
+  public readonly ACTIVATE_SHIPPING_ICON_URL: string = 'assets/icons/shipping-available.svg';
 }
