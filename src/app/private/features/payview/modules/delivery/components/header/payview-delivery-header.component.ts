cbd9748c68daa2484657ad11116eb4d9e05227a0
antoni-casas <toni.casas@wallapop.com>
2022-02-17 17:11:41 +0100

    [WPA-9136]: feat: Add payview delivery module

---
+++ src/app/private/features/payview/modules/delivery/components/header/payview-delivery-header.component.ts
@@ -0,0 +1,18 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+import { CUSTOMER_HELP_PAGE } from '@core/external-links/customer-help/customer-help-constants';
+import { CustomerHelpService } from '@core/external-links/customer-help/customer-help.service';
+
+@Component({
+  selector: 'tsl-payview-delivery-header',
+  templateUrl: './payview-delivery-header.component.html',
+  styleUrls: ['./payview-delivery-header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PayviewDeliveryHeaderComponent {
+  constructor(private customerHelpService: CustomerHelpService) {}
+
+  public get helpUrl(): string {
+    return this.customerHelpService.getPageUrl(CUSTOMER_HELP_PAGE.PAYVIEW_DELIVERY);
+  }
+}
