85387ca276820b67f330a2f282a4b866de17c6ff
Laia Lopez <laialopezz99@gmail.com>
2022-02-18 11:29:59 +0100

    [WPA-9955]: [Input rename] UPDATE_SELECTED_CARRIER_OFFICE_ENDPOINT

--- src/app/api/delivery/me/carrier-office-addresses/http/endpoints.ts
+++ src/app/api/delivery/me/carrier-office-addresses/http/endpoints.ts
@@ -2,4 +2,4 @@ import { environment } from '@environments/environment';
 
 const SELECT_CARRIER_OFFICE_BASE_ENDPOINT = `${environment.baseUrl}api/v3/delivery/me/carrier-office-addresses`;
 export const CREATE_SELECTED_CARRIER_OFFICE_ENDPOINT: string = SELECT_CARRIER_OFFICE_BASE_ENDPOINT;
-export const UPDATE_SELECTED_CARRIER_OFFICE_ENDPOINT = (carrierId: string): string => `${SELECT_CARRIER_OFFICE_BASE_ENDPOINT}/${carrierId}`;
+export const UPDATE_SELECTED_CARRIER_OFFICE_ENDPOINT = (officeId: string): string => `${SELECT_CARRIER_OFFICE_BASE_ENDPOINT}/${officeId}`;
