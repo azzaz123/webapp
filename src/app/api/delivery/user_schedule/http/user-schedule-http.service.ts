fc73ae38b9d3826ebaf2c6f66c6d0e488be7f05e
Laia Lopez <laialopezz99@gmail.com>
2022-02-21 11:10:50 +0100

    [WPA-10604]: [selectHomePickUpDeliverySchedule] Done

--- src/app/api/delivery/user_schedule/http/user-schedule-http.service.ts
+++ src/app/api/delivery/user_schedule/http/user-schedule-http.service.ts
@@ -1,8 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ScheduleTypeDto } from '../dtos/requests/schedule-type-dto.type';
+import { UPDATE_DELIVERY_USER_SCHEDULE_ENDPOINT } from './endpoints';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserScheduleHttpService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  public selectHomePickUpDeliverySchedule(scheduleId: string, scheduleType: ScheduleTypeDto): Observable<void> {
+    return this.http.put<void>(UPDATE_DELIVERY_USER_SCHEDULE_ENDPOINT, {
+      id: scheduleId,
+      schedule_time_range: scheduleType,
+    });
+  }
 }
