bce4b6734f3badb887364f24031fea5abfaf4952
Laia Lopez <laialopezz99@gmail.com>
2022-02-21 15:41:03 +0100

    [WPA-10605]: [Logic WIP] selectScheduleTypePreference

--- src/app/private/features/accept-screen/components/steps/delivery-preference-schedule/delivery-preference-schedule.component.ts
+++ src/app/private/features/accept-screen/components/steps/delivery-preference-schedule/delivery-preference-schedule.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DeliverySchedule } from '@api/core/model/delivery/schedule/delivery-schedule.interface';
+import { SCHEDULE_TYPE } from '@api/core/model/delivery/schedule/schedule-type.type';
+import { UserScheduleApiService } from '@api/delivery/user_schedule/user-schedule-api.service';
 
 @Component({
   selector: 'tsl-delivery-preference-schedule',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./delivery-preference-schedule.component.scss'],
 })
 export class DeliveryPreferenceScheduleComponent {
-  constructor() {}
+  @Input() scheduleTypeSelected: SCHEDULE_TYPE;
+  public readonly deliverySchedule$: DeliverySchedule;
+  public readonly SCHEDULE_TYPE = SCHEDULE_TYPE;
+
+  constructor(public userScheduleApiService: UserScheduleApiService) {}
+
+  public selectScheduleTypePreference(type: SCHEDULE_TYPE): void {
+    this.scheduleTypeSelected = type;
+  }
+
+  public savePreference(): void {
+    this.userScheduleApiService.selectHomePickUpDeliverySchedule(null, this.scheduleTypeSelected).subscribe(
+      () => {},
+      () => {}
+    );
+  }
 }
