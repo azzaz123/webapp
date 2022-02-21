e47afd1a3c829f08df1abaf897d7e1a49632f810
Laia Lopez <laialopezz99@gmail.com>
2022-02-21 12:51:52 +0100

    [WPA-10604]: [Testing] UserScheduleHttpService

--- src/app/api/bff/delivery/user_schedule/dtos/responses/delivery-schedule-dto.interface.ts
+++ src/app/api/bff/delivery/user_schedule/dtos/responses/delivery-schedule-dto.interface.ts
@@ -1,15 +1,17 @@
+export type ScheduleTypeDto = 'all day' | 'morning' | 'afternoon';
+
 export interface DeliveryScheduleDto {
   schedule_options: ScheduleOptionDto[];
   user_schedule: UserScheduleDto;
 }
 
 export interface ScheduleOptionDto {
-  schedule_time_range: string;
+  schedule_time_range: ScheduleTypeDto;
   pickup_start_date: string;
   pickup_end_date: string;
 }
 
 export interface UserScheduleDto {
   id: string;
-  schedule_time_range: string;
+  schedule_time_range: ScheduleTypeDto;
 }
--- src/app/api/bff/delivery/user_schedule/http/user-schedule-http.service.spec.ts
+++ src/app/api/bff/delivery/user_schedule/http/user-schedule-http.service.spec.ts
@@ -1,16 +1,48 @@
+import { HttpClientTestingModule, HttpTestingController, TestRequest } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { MOCK_USER_DELIVERY_SCHEDULE_DTO } from '@api/fixtures/delivery/user-schedule/user-delivery-schedule.dto.fixtures.spec';
+import { DeliveryScheduleDto } from '../dtos/responses/delivery-schedule-dto.interface';
+import { GET_DELIVERY_SCHEDULES_ENDPOINT } from './endpoints';
 
 import { UserScheduleHttpService } from './user-schedule-http.service';
 
 describe('UserScheduleHttpService', () => {
   let service: UserScheduleHttpService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(UserScheduleHttpService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  describe('when the user asks to get home pick up delivery schedules', () => {
+    let response: DeliveryScheduleDto;
+
+    beforeEach(() => {
+      service.getHomePickUpDeliverySchedules().subscribe((dto: DeliveryScheduleDto) => {
+        response = dto;
+      });
+    });
+
+    it('should ask server to get them', () => {
+      const req: TestRequest = httpMock.expectOne(GET_DELIVERY_SCHEDULES_ENDPOINT);
+      req.flush(MOCK_USER_DELIVERY_SCHEDULE_DTO);
+
+      expect(req.request.method).toBe('GET');
+      expect(req.request.url).toEqual(GET_DELIVERY_SCHEDULES_ENDPOINT);
+      expect(req.request.body).toStrictEqual(null);
+      expect(response).toStrictEqual(MOCK_USER_DELIVERY_SCHEDULE_DTO);
+    });
   });
 });
