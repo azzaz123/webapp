0e651ac188972bedf8f94dc8541881e762cecf49
Laia Lopez <laialopezz99@gmail.com>
2022-02-21 11:35:41 +0100

    [WPA-10604]: [Testing] UserSchedule

--- src/app/api/delivery/user_schedule/http/user-schedule-http.service.spec.ts
+++ src/app/api/delivery/user_schedule/http/user-schedule-http.service.spec.ts
@@ -1,16 +1,45 @@
+import { HttpClientTestingModule, HttpTestingController, TestRequest } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { ScheduleTypeDto } from '../dtos/requests/schedule-type-dto.type';
+import { UPDATE_DELIVERY_USER_SCHEDULE_ENDPOINT } from './endpoints';
 
 import { UserScheduleHttpService } from './user-schedule-http.service';
 
 describe('UserScheduleHttpService', () => {
   let service: UserScheduleHttpService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(UserScheduleHttpService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  describe('when the user asks to select home pick up delivery schedule', () => {
+    it('should ask server to select it', () => {
+      const MOCK_SCHEDULE_ID: string = '2bewuhd8w';
+      const MOCK_SCHEDULE_TYPE: ScheduleTypeDto = 'afternoon';
+
+      service.selectHomePickUpDeliverySchedule(MOCK_SCHEDULE_ID, MOCK_SCHEDULE_TYPE).subscribe();
+      const req: TestRequest = httpMock.expectOne(UPDATE_DELIVERY_USER_SCHEDULE_ENDPOINT);
+      req.flush({});
+
+      expect(req.request.method).toBe('PUT');
+      expect(req.request.url).toEqual(UPDATE_DELIVERY_USER_SCHEDULE_ENDPOINT);
+      expect(req.request.body).toStrictEqual({
+        id: MOCK_SCHEDULE_ID,
+        schedule_time_range: MOCK_SCHEDULE_TYPE,
+      });
+    });
   });
 });
--- src/app/api/delivery/user_schedule/user-schedule-api.service.spec.ts
+++ src/app/api/delivery/user_schedule/user-schedule-api.service.spec.ts
@@ -1,16 +1,50 @@
 import { TestBed } from '@angular/core/testing';
+import { SCHEDULE_TYPE } from '@api/core/model/delivery/schedule/schedule-type.type';
+import { Observable, of } from 'rxjs';
+import { UserScheduleHttpService } from './http/user-schedule-http.service';
+import { mapScheduleTypeDomainToDto } from './mappers/requests/schedule-type.mapper';
 
 import { UserScheduleApiService } from './user-schedule-api.service';
 
 describe('UserScheduleApiService', () => {
   let service: UserScheduleApiService;
+  let userScheduleHttpService: UserScheduleHttpService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: UserScheduleHttpService,
+          useValue: {
+            selectHomePickUpDeliverySchedule(): Observable<void> {
+              return of(null);
+            },
+          },
+        },
+      ],
+    });
     service = TestBed.inject(UserScheduleApiService);
+    userScheduleHttpService = TestBed.inject(UserScheduleHttpService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('when the user asks to select home pick up delivery schedule', () => {
+    const MOCK_SCHEDULE_ID: string = 'sbudwuy28';
+
+    beforeEach(() => {
+      spyOn(userScheduleHttpService, 'selectHomePickUpDeliverySchedule').and.callThrough();
+      service.selectHomePickUpDeliverySchedule(MOCK_SCHEDULE_ID, SCHEDULE_TYPE.MORNING).subscribe();
+    });
+
+    it('should request it with mapped schedule type', () => {
+      expect(userScheduleHttpService.selectHomePickUpDeliverySchedule).toHaveBeenCalledTimes(1);
+      expect(userScheduleHttpService.selectHomePickUpDeliverySchedule).toHaveBeenCalledWith(
+        MOCK_SCHEDULE_ID,
+        mapScheduleTypeDomainToDto[SCHEDULE_TYPE.MORNING]
+      );
+    });
   });
 });
