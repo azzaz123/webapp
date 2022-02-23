e320e57941f5c32df5b2999a9a270cc45bb8827a
Laia Lopez <laialopezz99@gmail.com>
2022-02-23 15:32:23 +0100

    [WPA-10605]: [DeliveryRadioSelectorComponent] OnPush

--- src/app/private/shared/delivery-radio-selector/delivery-radio-selector.component.ts
+++ src/app/private/shared/delivery-radio-selector/delivery-radio-selector.component.ts
@@ -1,15 +1,38 @@
-import { Component, ContentChildren, EventEmitter, Input, Output, QueryList } from '@angular/core';
+import {
+  AfterContentInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ContentChildren,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  Output,
+  QueryList,
+} from '@angular/core';
 import { DeliveryRadioOptionDirective } from '@private/shared/delivery-radio-selector/delivery-radio-option.directive';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'tsl-delivery-radio-selector',
   templateUrl: './delivery-radio-selector.component.html',
   styleUrls: ['./delivery-radio-selector.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DeliveryRadioSelectorComponent {
+export class DeliveryRadioSelectorComponent implements OnDestroy, AfterContentInit {
   @Input() selectedId: number;
   @Output() changed: EventEmitter<number> = new EventEmitter();
 
   @ContentChildren(DeliveryRadioOptionDirective)
   options: QueryList<DeliveryRadioOptionDirective>;
+  private optionsSubscription: Subscription = new Subscription();
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngAfterContentInit(): void {
+    this.optionsSubscription.add(this.options.changes.subscribe(() => this.cdr.markForCheck()));
+  }
+
+  ngOnDestroy(): void {
+    this.optionsSubscription.unsubscribe();
+  }
 }
