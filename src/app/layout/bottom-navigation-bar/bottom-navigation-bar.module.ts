32f8c249efcda080ddc788e0a940c66550a85fad
Alex Masnou <alex.masnou@wallapop.com>
2022-03-01 16:46:50 +0100

    [WPA-8003]: Create new module for bottom navigation bar element

---
+++ src/app/layout/bottom-navigation-bar/bottom-navigation-bar.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BottomNavigationBarComponent } from './components/bottom-navigation-bar.component';
+
+@NgModule({
+  declarations: [BottomNavigationBarComponent],
+  imports: [CommonModule],
+})
+export class BottomNavigationBarModule {}
---
+++ src/app/layout/bottom-navigation-bar/components/bottom-navigation-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tsl-bottom-navigation-bar',
+  templateUrl: './bottom-navigation-bar.component.html',
+  styleUrls: ['./bottom-navigation-bar.component.scss'],
+})
+export class BottomNavigationBarComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
