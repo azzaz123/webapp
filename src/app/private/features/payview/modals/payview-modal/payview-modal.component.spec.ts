6409664e58c868a571f99e9909cec1939be583be
antoni-casas <toni.casas@wallapop.com>
2022-03-01 17:09:43 +0100

    [WPA-9140]: feat: Use delivery event bus

--- src/app/private/features/payview/modals/payview-modal/payview-modal.component.spec.ts
+++ src/app/private/features/payview/modals/payview-modal/payview-modal.component.spec.ts
@@ -9,12 +9,15 @@ import { BuyerRequestsApiModule } from '@api/delivery/buyer/requests/buyer-reque
 import { CustomerHelpService } from '@core/external-links/customer-help/customer-help.service';
 import { DeliveryAddressService } from '@private/features/delivery/services/address/delivery-address/delivery-address.service';
 import { DeliveryAddressStoreService } from '@private/features/delivery/services/address/delivery-address-store/delivery-address-store.service';
+import { DeliveryBuyerDeliveryMethod } from '@api/core/model/delivery/buyer/delivery-methods';
+import { DeliveryCountriesService } from '@private/features/delivery/services/countries/delivery-countries/delivery-countries.service';
 import { DeliveryRadioSelectorModule } from '@private/shared/delivery-radio-selector/delivery-radio-selector.module';
 import { ItemService } from '@core/item/item.service';
 import { MOCK_DELIVERY_BUYER_DELIVERY_METHODS } from '@api/fixtures/bff/delivery/buyer/delivery-buyer.fixtures.spec';
 import { MOCK_PAYVIEW_STATE } from '@fixtures/private/delivery/payview/payview-state.fixtures.spec';
 import { PaymentsWalletsHttpService } from '@api/payments/wallets/http/payments-wallets-http.service';
 import { PaymentsWalletsService } from '@api/payments/wallets/payments-wallets.service';
+import { PayviewDeliveryEventType } from '@private/features/payview/modules/delivery/enums/payview-delivery-event-type.interface';
 import { PayviewDeliveryHeaderComponent } from '@private/features/payview/modules/delivery/components/header/payview-delivery-header.component';
 import { PayviewDeliveryOverviewComponent } from '@private/features/payview/modules/delivery/components/overview/payview-delivery-overview.component';
 import { PayviewDeliveryPointComponent } from '@private/features/payview/modules/delivery/components/point/payview-delivery-point.component';
@@ -22,10 +25,12 @@ import { PayviewDeliveryPointsComponent } from '@private/features/payview/module
 import { PayviewDeliveryService } from '@private/features/payview/modules/delivery/services/payview-delivery.service';
 import { PayviewModalComponent } from '@private/features/payview/modals/payview-modal/payview-modal.component';
 import { PayviewStateManagementService } from '@private/features/payview/services/state-management/payview-state-management.service';
+import { PayviewSteps } from '@private/features/payview/enums/payview-steps.enum';
 import { PayviewSummaryCostDetailComponent } from '@private/features/payview/modules/summary/components/cost-detail/payview-summary-cost-detail.component';
 import { PayviewSummaryHeaderComponent } from '@private/features/payview/modules/summary/components/header/payview-summary-header.component';
 import { PayviewSummaryOverviewComponent } from '@private/features/payview/modules/summary/components/overview/payview-summary-overview.component';
 import { PayviewSummaryPaymentMethodComponent } from '@private/features/payview/modules/summary/components/payment-method/payview-summary-payment-method.component';
+import { StepperModule } from '@shared/stepper/stepper.module';
 import { SvgIconComponent } from '@shared/svg-icon/svg-icon.component';
 
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
@@ -34,16 +39,17 @@ import { of } from 'rxjs';
 @Component({
   selector: 'tsl-fake-component',
   templateUrl: './payview-modal.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.Default,
 })
 class FakeComponent extends PayviewModalComponent {
   constructor(
     payviewStateManagementService: PayviewStateManagementService,
     payviewDeliveryService: PayviewDeliveryService,
     activeModal: NgbActiveModal,
-    customerHelpService: CustomerHelpService
+    customerHelpService: CustomerHelpService,
+    deliveryCountries: DeliveryCountriesService
   ) {
-    super(payviewStateManagementService, payviewDeliveryService, activeModal, customerHelpService);
+    super(payviewStateManagementService, payviewDeliveryService, activeModal, customerHelpService, deliveryCountries);
   }
 }
 
@@ -53,6 +59,7 @@ describe('PayviewModalComponent', () => {
 
   const payviewModal: string = '.PayviewModal';
   const payviewModalCloseSelector: string = `${payviewModal}__close`;
+  const payviewModalDeliveryAddressSelector: string = `${payviewModal}__deliveryAddress`;
   const payviewModalHelpSelector: string = '#helpLink';
   const payviewModalSpinnerSelector: string = `${payviewModal}__spinner`;
   const payviewModalSummarySelector: string = `${payviewModal}__summary`;
@@ -65,7 +72,7 @@ describe('PayviewModalComponent', () => {
   let fixture: ComponentFixture<PayviewModalComponent>;
   let itemHashSpy: jest.SpyInstance;
   let payviewDeliveryService: PayviewDeliveryService;
-  let payviewDeliverySpy;
+  let payviewDeliverySpy: jasmine.Spy;
   let payviewStateManagementService: PayviewStateManagementService;
 
   beforeEach(async () => {
@@ -83,7 +90,7 @@ describe('PayviewModalComponent', () => {
         PayviewSummaryPaymentMethodComponent,
         SvgIconComponent,
       ],
-      imports: [BrowserAnimationsModule, BuyerRequestsApiModule, DeliveryRadioSelectorModule, HttpClientTestingModule],
+      imports: [BrowserAnimationsModule, BuyerRequestsApiModule, DeliveryRadioSelectorModule, HttpClientTestingModule, StepperModule],
       providers: [
         {
           provide: CustomerHelpService,
@@ -159,7 +166,7 @@ describe('PayviewModalComponent', () => {
         payviewStateManagementService = TestBed.inject(PayviewStateManagementService);
         itemHashSpy = jest.spyOn(payviewStateManagementService, 'itemHash', 'set');
         payviewDeliveryService = TestBed.inject(PayviewDeliveryService);
-        payviewDeliverySpy = jest.spyOn(payviewDeliveryService, 'deliveryMethodSelected', 'get');
+        payviewDeliverySpy = spyOn(payviewDeliveryService, 'on');
 
         fixture = TestBed.createComponent(FakeComponent);
         component = fixture.componentInstance;
@@ -178,8 +185,8 @@ describe('PayviewModalComponent', () => {
         expect(itemHashSpy).toBeCalledWith(fakeItemHash);
       }));
 
-      it('should subscribe to the listener', () => {
-        expect(payviewDeliverySpy).toHaveBeenCalledTimes(1);
+      it('should subscribe to all listeners', () => {
+        expect(payviewDeliverySpy).toHaveBeenCalledTimes(3);
       });
 
       describe('WHEN the payview gets the state', () => {
@@ -204,32 +211,52 @@ describe('PayviewModalComponent', () => {
           const loadingContainerRef = fixture.debugElement.query(By.css(payviewModalSpinnerSelector));
           expect(loadingContainerRef).toBeFalsy();
         }));
+
+        it('should not show the delivery address', () => {
+          const deliveryAddressBlock = debugElement.query(By.css(payviewModalDeliveryAddressSelector));
+
+          expect(deliveryAddressBlock).toBeFalsy();
+        });
+
+        describe('WHEN stepper is on the second step', () => {
+          it('should show the address', () => {
+            component.stepper.goToStep(PayviewSteps.DeliveryAddress);
+
+            const deliveryAddressBlock = fixture.debugElement.query(By.css(payviewModalDeliveryAddressSelector));
+            expect(deliveryAddressBlock).toBeTruthy();
+          });
+        });
+
+        describe('WHEN stepper is not on the second step', () => {
+          it('should not show the address', () => {
+            component.stepper.goToStep(PayviewSteps.Payview);
+
+            const deliveryAddressBlock = fixture.debugElement.query(By.css(payviewModalDeliveryAddressSelector));
+            expect(deliveryAddressBlock).toBeFalsy();
+          });
+        });
       });
     });
 
     describe('WHEN the delivery method has been selected', () => {
-      let targetSpy;
-
       beforeEach(() => {
-        payviewStateManagementService = TestBed.inject(PayviewStateManagementService);
-        itemHashSpy = jest.spyOn(payviewStateManagementService, 'itemHash', 'set');
-        payviewDeliveryService = TestBed.inject(PayviewDeliveryService);
-        payviewDeliverySpy = jest
-          .spyOn(payviewDeliveryService, 'deliveryMethodSelected', 'get')
-          .mockReturnValue(of(MOCK_DELIVERY_BUYER_DELIVERY_METHODS[0]));
-        targetSpy = spyOn(payviewStateManagementService, 'setDeliveryMethod').and.callFake(() => {});
-
-        fixture = TestBed.createComponent(FakeComponent);
-        component = fixture.componentInstance;
-        debugElement = fixture.debugElement;
-        component.itemHash = fakeItemHash;
-
-        fixture.detectChanges();
+        spyOn(payviewStateManagementService, 'setDeliveryMethod').and.callFake(() => {});
+        spyOn(payviewDeliveryService, 'on').and.callThrough();
       });
 
       it('should set the delivery method received', () => {
-        expect(targetSpy).toHaveBeenCalledTimes(1);
-        expect(targetSpy).toHaveBeenCalledWith(MOCK_DELIVERY_BUYER_DELIVERY_METHODS[0]);
+        let result: DeliveryBuyerDeliveryMethod;
+        const subscription = payviewDeliveryService.on(
+          PayviewDeliveryEventType.DeliveryMethodSelected,
+          (data: DeliveryBuyerDeliveryMethod) => {
+            result = data;
+          }
+        );
+
+        payviewDeliveryService.setDeliveryMethod(MOCK_DELIVERY_BUYER_DELIVERY_METHODS.deliveryMethods[0]);
+
+        expect(payviewDeliveryService.on).toHaveBeenCalledTimes(1);
+        expect(result).toEqual(MOCK_DELIVERY_BUYER_DELIVERY_METHODS.deliveryMethods[0]);
       });
     });
 
@@ -272,10 +299,16 @@ describe('PayviewModalComponent', () => {
           expect(summaryBlock).toBeFalsy();
         });
 
-        it('should show the summary overview component', () => {
+        it('should not show the summary overview component', () => {
           const summaryOverviewComponent = debugElement.query(By.css(payviewSummaryOverviewSelector));
 
           expect(summaryOverviewComponent).toBeFalsy();
+        });
+
+        it('should not show the delivery address component', () => {
+          const deliveryAddressComponent = debugElement.query(By.css(payviewModalDeliveryAddressSelector));
+
+          expect(deliveryAddressComponent).toBeFalsy();
         });
       });
 
--- src/app/private/features/payview/modals/payview-modal/payview-modal.component.ts
+++ src/app/private/features/payview/modals/payview-modal/payview-modal.component.ts
@@ -1,12 +1,18 @@
-import { ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 
+import { CountryOptionsAndDefault } from '@private/features/delivery/interfaces/delivery-countries/delivery-countries-api.interface';
 import { CUSTOMER_HELP_PAGE } from '@core/external-links/customer-help/customer-help-constants';
 import { CustomerHelpService } from '@core/external-links/customer-help/customer-help.service';
+import { DELIVERY_ADDRESS_PREVIOUS_PAGE } from '@private/features/delivery/enums/delivery-address-previous-pages.enum';
 import { DeliveryBuyerDeliveryMethod } from '@api/core/model/delivery/buyer/delivery-methods';
+import { DeliveryCountriesService } from '@private/features/delivery/services/countries/delivery-countries/delivery-countries.service';
+import { PayviewDeliveryEventType } from '@private/features/payview/modules/delivery/enums/payview-delivery-event-type.interface';
 import { PayviewDeliveryService } from '@private/features/payview/modules/delivery/services/payview-delivery.service';
 import { PayviewService } from '@private/features/payview/services/payview/payview.service';
 import { PayviewState } from '@private/features/payview/interfaces/payview-state.interface';
 import { PayviewStateManagementService } from '@private/features/payview/services/state-management/payview-state-management.service';
+import { PayviewSteps } from '@private/features/payview/enums/payview-steps.enum';
+import { StepperComponent } from '@shared/stepper/stepper.component';
 
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Observable, Subscription } from 'rxjs';
@@ -19,15 +25,20 @@ import { Observable, Subscription } from 'rxjs';
   providers: [PayviewService, PayviewStateManagementService],
 })
 export class PayviewModalComponent implements OnDestroy, OnInit {
+  @ViewChild(StepperComponent) stepper: StepperComponent;
+
   @Input() public itemHash: string;
 
-  private deliveryMethodSubscription: Subscription;
+  public countries$: Observable<CountryOptionsAndDefault> = this.deliveryCountries.getCountriesAsOptionsAndDefault();
+  public readonly DELIVERY_ADDRESS_PREVIOUS_PAGE: DELIVERY_ADDRESS_PREVIOUS_PAGE = DELIVERY_ADDRESS_PREVIOUS_PAGE.ACCEPT_SCREEN;
+  private subscriptions: Subscription[] = [];
 
   constructor(
     private payviewStateManagementService: PayviewStateManagementService,
     private deliveryService: PayviewDeliveryService,
     private activeModal: NgbActiveModal,
-    private customerHelpService: CustomerHelpService
+    private customerHelpService: CustomerHelpService,
+    private deliveryCountries: DeliveryCountriesService
   ) {}
 
   public ngOnDestroy(): void {
@@ -43,6 +54,11 @@ export class PayviewModalComponent implements OnDestroy, OnInit {
     this.activeModal.close();
   }
 
+  public goToPayview(): void {
+    this.stepper.goToStep(PayviewSteps.Payview);
+    this.payviewStateManagementService.refreshPayviewState();
+  }
+
   public get helpUrl(): string {
     return this.customerHelpService.getPageUrl(CUSTOMER_HELP_PAGE.PAYVIEW);
   }
@@ -51,25 +67,46 @@ export class PayviewModalComponent implements OnDestroy, OnInit {
     return this.payviewStateManagementService.payViewState$;
   }
 
+  private goToDeliveryAddress(): void {
+    this.stepper.goToStep(PayviewSteps.DeliveryAddress);
+  }
+
+  private goToPickUpPoint(): void {
+    // TODO - Uncomment the following line when the pick-up point map is ended
+    // this.stepper.goToStep(PayviewSteps.PickUpPointMap);
+  }
+
+  private setDeliveryMethod(deliveryMethod: DeliveryBuyerDeliveryMethod): void {
+    this.payviewStateManagementService.setDeliveryMethod(deliveryMethod);
+  }
+
   private subscribe(): void {
     this.subscribeToDeliveryMethod();
   }
 
   private subscribeToDeliveryMethod(): void {
-    this.deliveryMethodSubscription = this.deliveryService.deliveryMethodSelected.subscribe(
-      (deliveryMethod: DeliveryBuyerDeliveryMethod) => {
-        this.payviewStateManagementService.setDeliveryMethod(deliveryMethod);
-      }
+    this.subscriptions.push(
+      this.deliveryService.on(PayviewDeliveryEventType.DeliveryMethodSelected, (payload: DeliveryBuyerDeliveryMethod) => {
+        this.setDeliveryMethod(payload);
+      })
+    );
+    this.subscriptions.push(
+      this.deliveryService.on(PayviewDeliveryEventType.OpenAddressScreen, () => {
+        this.goToDeliveryAddress();
+      })
+    );
+    this.subscriptions.push(
+      this.deliveryService.on(PayviewDeliveryEventType.OpenPickUpPointMap, () => {
+        this.goToPickUpPoint();
+      })
     );
   }
 
   private unsubscribe(): void {
-    this.unsubscribeFromDeliveryMethod();
-  }
-
-  private unsubscribeFromDeliveryMethod(): void {
-    if (!!this.deliveryMethodSubscription) {
-      this.deliveryMethodSubscription.unsubscribe();
-    }
+    this.subscriptions.forEach((subscription: Subscription) => {
+      if (!!subscription) {
+        subscription.unsubscribe();
+      }
+    });
   }
 }
