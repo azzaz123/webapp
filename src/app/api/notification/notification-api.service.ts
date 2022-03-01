bff9fc675ed32a6972ba15380ef575b2be03011e
Àlex Delgado <alejandro.delgado@wallapop.com>
2022-03-01 12:15:32 +0100

    [WPA-10377]: added new subject for notification-api.service.ts => _totalUnreadNotifications$

--- src/app/api/notification/notification-api.service.ts
+++ src/app/api/notification/notification-api.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@angular/core';
 import { mapNotificationsFromBraze } from './mappers/notification-mapper';
 import { Notification } from '@api/core/model/notification/notification.interface';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { Title } from '@angular/platform-browser';
 
 @Injectable()
 export class NotificationApiService {
+  private _totalUnreadNotifications$: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+
+  get totalUnreadNotifications$(): Observable<number> {
+    return this._totalUnreadNotifications$.asObservable();
+  }
+
   public getNotifications(): Notification[] {
     const brazeNotifications = appboy.getCachedContentCards();
     return mapNotificationsFromBraze(brazeNotifications.cards);
+  }
+
+  public refreshUnreadNotifications(): void {
+    const unreadNotifications = this.getNotifications().filter((notification) => !notification.isRead).length;
+    this._totalUnreadNotifications$.next(unreadNotifications);
   }
 }
