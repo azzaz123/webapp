3086f13fa7eeccf8d0d58b2539cf8f20653f00a4
Alex Masnou <alex.masnou@wallapop.com>
2022-03-03 11:42:04 +0100

    [WPA-8003]: Add missing modules

--- src/app/layout/bottom-navigation-bar/bottom-navigation-bar.module.ts
+++ src/app/layout/bottom-navigation-bar/bottom-navigation-bar.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { BottomNavigationBarComponent } from './components/bottom-navigation-bar.component';
+import { SvgIconModule } from '@shared/svg-icon/svg-icon.module';
 
 @NgModule({
   declarations: [BottomNavigationBarComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, SvgIconModule],
+  exports: [BottomNavigationBarComponent],
 })
 export class BottomNavigationBarModule {}
