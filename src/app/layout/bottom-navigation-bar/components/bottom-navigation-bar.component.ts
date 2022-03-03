4e5f9466ebac644e9c95dd8def3ffd972ecd4c94
Alex Masnou <alex.masnou@wallapop.com>
2022-03-03 11:43:08 +0100

    [WPA-8003]: Expose navigationElements in controller

--- src/app/layout/bottom-navigation-bar/components/bottom-navigation-bar.component.ts
+++ src/app/layout/bottom-navigation-bar/components/bottom-navigation-bar.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { BOTTOM_NAVIGATION_BAR_ELMENTS } from '../constants/bottom-navigation-bar-elements';
+import { BottomNavigationBarElement } from '../interfaces/bottom-navigation-bar-element.interface';
 
 @Component({
   selector: 'tsl-bottom-navigation-bar',
   templateUrl: './bottom-navigation-bar.component.html',
   styleUrls: ['./bottom-navigation-bar.component.scss'],
 })
-export class BottomNavigationBarComponent implements OnInit {
-  constructor() {}
+export class BottomNavigationBarComponent {
+  public navigationElements: BottomNavigationBarElement[] = BOTTOM_NAVIGATION_BAR_ELMENTS;
 
-  ngOnInit(): void {}
+  constructor() {}
 }
