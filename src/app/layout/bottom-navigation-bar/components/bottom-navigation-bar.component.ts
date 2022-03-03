c2d990579900232b82564bee45898f998e808494
Alex Masnou <alex.masnou@wallapop.com>
2022-03-03 11:59:53 +0100

    [WPA-8003]: Add logic to hide/show navigation bar when focusing on text elements

--- src/app/layout/bottom-navigation-bar/components/bottom-navigation-bar.component.ts
+++ src/app/layout/bottom-navigation-bar/components/bottom-navigation-bar.component.ts
@@ -1,14 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener } from '@angular/core';
+import { Observable } from 'rxjs';
 import { BOTTOM_NAVIGATION_BAR_ELMENTS } from '../constants/bottom-navigation-bar-elements';
 import { BottomNavigationBarElement } from '../interfaces/bottom-navigation-bar-element.interface';
+import { BottomNavigationBarService } from '../services/bottom-navigation-bar.service';
+
+export const INPUT_TYPE = {
+  TEXT: 'text',
+  DATE: 'date',
+  PASSWORD: 'password',
+  NUMBER: 'number',
+  radio: 'radio',
+};
+
+export const KEYBOARD_INPUT_TYPES = [INPUT_TYPE.TEXT, INPUT_TYPE.DATE, INPUT_TYPE.PASSWORD, INPUT_TYPE.NUMBER];
+
+export const ELEMENT_TYPE = {
+  TEXT_AREA: 'TEXTAREA',
+  INPUT: 'INPUT',
+};
 
 @Component({
   selector: 'tsl-bottom-navigation-bar',
   templateUrl: './bottom-navigation-bar.component.html',
   styleUrls: ['./bottom-navigation-bar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BottomNavigationBarComponent {
   public navigationElements: BottomNavigationBarElement[] = BOTTOM_NAVIGATION_BAR_ELMENTS;
+  public readonly hidden$: Observable<boolean> = this.bottomNavigationBarService.hidden$;
 
-  constructor() {}
+  constructor(private bottomNavigationBarService: BottomNavigationBarService) {}
+
+  @HostListener('window:focusin', ['$event'])
+  onFocusIn(elementType: unknown) {
+    if (this.isTextInputOrTextarea(elementType)) {
+      this.bottomNavigationBarService.hideNavigationBar();
+    }
+  }
+
+  @HostListener('window:focusout', ['$event'])
+  onFocusOut(elementType: unknown) {
+    if (this.isTextInputOrTextarea(elementType)) {
+      this.bottomNavigationBarService.showNavigationBar();
+    }
+  }
+
+  private isTextInputOrTextarea(element: any): boolean {
+    const elementTarget = element.target;
+    if (elementTarget.nodeName === ELEMENT_TYPE.TEXT_AREA) {
+      return true;
+    }
+
+    if (elementTarget.nodeName !== ELEMENT_TYPE.INPUT) {
+      return false;
+    }
+
+    const inputType = elementTarget.attributes?.type?.nodeValue;
+    return KEYBOARD_INPUT_TYPES.includes(inputType);
+  }
 }
--- src/app/layout/bottom-navigation-bar/services/bottom-navigation-bar.service.ts
+++ src/app/layout/bottom-navigation-bar/services/bottom-navigation-bar.service.ts
@@ -3,13 +3,21 @@ import { BehaviorSubject } from 'rxjs';
 
 @Injectable()
 export class BottomNavigationBarService {
-  public hidden$: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  private readonly hiddenSubject: BehaviorSubject<boolean> = new BehaviorSubject(false);
+
+  get hidden$() {
+    return this.hiddenSubject.asObservable();
+  }
+
+  private set hidden(value: boolean) {
+    this.hiddenSubject.next(value);
+  }
 
   public hideNavigationBar() {
-    this.hidden$.next(true);
+    this.hidden = true;
   }
 
   public showNavigationBar() {
-    this.hidden$.next(false);
+    this.hidden = false;
   }
 }
