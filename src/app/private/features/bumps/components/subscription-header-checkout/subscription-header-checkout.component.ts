c897cf1539162673aea08491576b7c05b5800111
Gonzalo Morales <gonzalomorales93@gmail.com>
2022-03-03 13:23:20 +0100

    [WPA-9804]: Update keys

--- src/app/private/features/bumps/components/subscription-header-checkout/subscription-header-checkout.component.ts
+++ src/app/private/features/bumps/components/subscription-header-checkout/subscription-header-checkout.component.ts
@@ -1,11 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { SubscriptionsResponse } from '@core/subscriptions/subscriptions.interface';
-
+import { BUMP_TYPE } from '@api/core/model/bumps/bump.interface';
 @Component({
   selector: 'tsl-subscription-header-checkout',
   templateUrl: './subscription-header-checkout.component.html',
   styleUrls: ['./subscription-header-checkout.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SubscriptionHeaderCheckoutComponent {
   @Input() subscription: SubscriptionsResponse;
+
+  public getBumpName(bumpType: BUMP_TYPE): string {
+    switch (bumpType) {
+      case BUMP_TYPE.ZONE_BUMP:
+        return $localize`:@@web_cart_454:Local`;
+      case BUMP_TYPE.CITY_BUMP:
+        return $localize`:@@web_cart_663:City`;
+      case BUMP_TYPE.COUNTRY_BUMP:
+        return $localize`:@@web_cart_664:Country`;
+      default:
+        return '';
+    }
+  }
 }
