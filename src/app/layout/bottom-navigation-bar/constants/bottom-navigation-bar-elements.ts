71d5f6513f3f407e0bbfca04047d1c7ad181390c
Alex Masnou <alex.masnou@wallapop.com>
2022-03-04 15:20:41 +0100

    [WPA-8003]: Add pending notification logic to navigationOptions

--- src/app/layout/bottom-navigation-bar/constants/bottom-navigation-bar-elements.ts
+++ src/app/layout/bottom-navigation-bar/constants/bottom-navigation-bar-elements.ts
@@ -3,8 +3,16 @@ import { BottomNavigationBarElement } from '../interfaces/bottom-navigation-bar-
 
 const ICONS_FOLDER = '/assets/icons/bottom-navigation-bar';
 
-export const BOTTOM_NAVIGATION_BAR_ELMENTS: BottomNavigationBarElement[] = [
-  {
+export enum BOTTOM_NAVIGATION_BAR_ELEMENTS_ENUM {
+  HOME,
+  FAVORITES,
+  UPLOAD,
+  INBOX,
+  YOU,
+}
+
+export const BOTTOM_NAVIGATION_BAR_ELMENTS_RECORD: Record<BOTTOM_NAVIGATION_BAR_ELEMENTS_ENUM, BottomNavigationBarElement> = {
+  [BOTTOM_NAVIGATION_BAR_ELEMENTS_ENUM.HOME]: {
     text: $localize`:@@web_components_tabbar_3:Home`,
     alternativeText: $localize`:@@web_components_tabbar_3:Home`,
     icon: `${ICONS_FOLDER}/home.svg`,
@@ -12,7 +20,7 @@ export const BOTTOM_NAVIGATION_BAR_ELMENTS: BottomNavigationBarElement[] = [
     href: `/${PUBLIC_PATHS.SEARCH}`,
     external: false,
   },
-  {
+  [BOTTOM_NAVIGATION_BAR_ELEMENTS_ENUM.FAVORITES]: {
     text: $localize`:@@web_components_tabbar_4:Favorites`,
     alternativeText: $localize`:@@web_components_tabbar_4:Favorites`,
     icon: `${ICONS_FOLDER}/favorites.svg`,
@@ -20,7 +28,7 @@ export const BOTTOM_NAVIGATION_BAR_ELMENTS: BottomNavigationBarElement[] = [
     href: `/favorites`,
     external: false,
   },
-  {
+  [BOTTOM_NAVIGATION_BAR_ELEMENTS_ENUM.UPLOAD]: {
     text: $localize`:@@web_components_tabbar_5:Upload`,
     alternativeText: $localize`:@@web_components_tabbar_5:Upload`,
     icon: `${ICONS_FOLDER}/upload.svg`,
@@ -28,21 +36,21 @@ export const BOTTOM_NAVIGATION_BAR_ELMENTS: BottomNavigationBarElement[] = [
     href: `/catalog/upload`,
     external: false,
   },
-  {
+  [BOTTOM_NAVIGATION_BAR_ELEMENTS_ENUM.INBOX]: {
     text: $localize`:@@web_components_tabbar_6:Messages`,
     alternativeText: $localize`:@@web_components_tabbar_6:Messages`,
     icon: `${ICONS_FOLDER}/inbox.svg`,
     activeIcon: `${ICONS_FOLDER}/inbox-active.svg`,
     href: `/chat`,
     external: false,
-    pendingNotification: true,
   },
-  {
+  [BOTTOM_NAVIGATION_BAR_ELEMENTS_ENUM.YOU]: {
     text: 'You',
     alternativeText: 'You',
     icon: `${ICONS_FOLDER}/you.svg`,
     activeIcon: `${ICONS_FOLDER}/you-active.svg`,
-    href: `/you`,
+    // TODO: Change with /you tab when available
+    href: `/catalog/list`,
     external: false,
   },
-];
+};
--- src/app/layout/bottom-navigation-bar/services/bottom-navigation-bar.service.ts
+++ src/app/layout/bottom-navigation-bar/services/bottom-navigation-bar.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { UnreadChatMessagesService } from '@core/unread-chat-messages/unread-chat-messages.service';
+import { combineLatest, Observable, BehaviorSubject, of } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { BOTTOM_NAVIGATION_BAR_ELEMENTS_ENUM, BOTTOM_NAVIGATION_BAR_ELMENTS_RECORD } from '../constants/bottom-navigation-bar-elements';
+import { BottomNavigationBarElement } from '../interfaces/bottom-navigation-bar-element.interface';
 
 @Injectable()
 export class BottomNavigationBarService {
   private readonly hiddenSubject: BehaviorSubject<boolean> = new BehaviorSubject(false);
+
+  constructor(private unreadChatMessagesService: UnreadChatMessagesService) {}
+
+  get navigationElements$(): Observable<BottomNavigationBarElement[]> {
+    return combineLatest([
+      of(BOTTOM_NAVIGATION_BAR_ELMENTS_RECORD),
+      this.unreadChatMessagesService.totalUnreadMessages$.pipe(startWith(0)),
+    ]).pipe(
+      map(([elements, totalUnreadMessages]) => {
+        elements[BOTTOM_NAVIGATION_BAR_ELEMENTS_ENUM.INBOX].pendingNotification = !!totalUnreadMessages;
+
+        return Object.values(elements);
+      })
+    );
+  }
 
   get hidden$() {
     return this.hiddenSubject.asObservable();
