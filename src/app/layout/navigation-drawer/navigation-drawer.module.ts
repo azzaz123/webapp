5b383733f7dbe1011dee7b5a94007c6ac160a752
Alex Masnou <alex.masnou@wallapop.com>
2022-03-05 20:18:03 +0100

    [WPA-11227]: Create new module for drawer navigation

---
+++ src/app/layout/navigation-drawer/navigation-drawer.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavigationDrawerComponent } from './components//navigation-drawer.component';
+
+@NgModule({
+  declarations: [NavigationDrawerComponent],
+  imports: [CommonModule],
+})
+export class NavigationDrawerModule {}
