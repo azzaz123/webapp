a37fdf0b7de8641654a0256d238428235292f04a
Alex Masnou <alex.masnou@wallapop.com>
2022-03-05 20:18:13 +0100

    [WPA-11227]: Create new component for drawer navigation

---
+++ src/app/layout/navigation-drawer/components/navigation-drawer.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tsl-navigation-drawer',
+  templateUrl: './navigation-drawer.component.html',
+  styleUrls: ['./navigation-drawer.component.scss'],
+})
+export class NavigationDrawerComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
