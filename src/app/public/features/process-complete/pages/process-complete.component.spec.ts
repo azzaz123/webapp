cfbb1e24dde16561183b945ef4780792fb195eeb
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-03-08 18:52:25 +0100

    [WPA-11341]: Implement process complete UI logic

--- src/app/public/features/process-complete/pages/process-complete.component.spec.ts
+++ src/app/public/features/process-complete/pages/process-complete.component.spec.ts
@@ -1,13 +1,46 @@
+import { Injector } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { WindowMessage } from '@core/window-message/interfaces/window-message.interface';
+import { WindowMessageService } from '@core/window-message/services/window-message.service';
+import { WINDOW_TOKEN } from '@core/window/window.token';
+import { environment } from '@environments/environment';
 
 import { ProcessCompleteComponent } from './process-complete.component';
 
 describe('ProcessCompleteComponent', () => {
   let component: ProcessCompleteComponent;
   let fixture: ComponentFixture<ProcessCompleteComponent>;
+  let injectedWindow: Window;
+  let windowMessageService: WindowMessageService;
+  let mockParamIdValue: string = null;
+
+  const MOCK_ID: string = '1234';
+  const MOCK_WINDOW_MESSAGE: WindowMessage = { id: MOCK_ID };
+  const MOCK_ORIGIN: string = environment.baseUrl;
+  const MOCK_WINDOW: Partial<Window> = {
+    close: () => {},
+    origin: MOCK_ORIGIN,
+    opener: null,
+  };
+  const MOCK_WINDOW_OPENER: Partial<Window> = { ...MOCK_WINDOW };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: { snapshot: { paramMap: { get: (_key) => mockParamIdValue } } },
+        },
+        {
+          provide: WINDOW_TOKEN,
+          useValue: MOCK_WINDOW,
+        },
+        {
+          provide: WindowMessageService,
+          useValue: { send: () => {} },
+        },
+      ],
       declarations: [ProcessCompleteComponent],
     }).compileComponents();
   });
@@ -15,10 +48,71 @@ describe('ProcessCompleteComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ProcessCompleteComponent);
     component = fixture.componentInstance;
+    injectedWindow = TestBed.inject(WINDOW_TOKEN);
+    windowMessageService = TestBed.inject(WindowMessageService);
+
     fixture.detectChanges();
+
+    spyOn(injectedWindow, 'close');
+    spyOn(windowMessageService, 'send');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('when displaying component', () => {
+    describe('and when no ID is present in URL', () => {
+      beforeEach(() => {
+        mockParamIdValue = null;
+
+        component.ngOnInit();
+      });
+
+      it('should close the current browser window', () => {
+        expect(injectedWindow.close).toHaveBeenCalledTimes(1);
+      });
+
+      it('should not send message', () => {
+        expect(windowMessageService.send).not.toHaveBeenCalled();
+      });
+    });
+
+    describe('and when ID is present in URL', () => {
+      beforeEach(() => {
+        mockParamIdValue = MOCK_ID;
+      });
+
+      describe('and when the window was NOT opened by another window', () => {
+        beforeEach(() => {
+          component.ngOnInit();
+        });
+
+        it('should close the current browser window', () => {
+          expect(injectedWindow.close).toHaveBeenCalledTimes(1);
+        });
+
+        it('should not send message', () => {
+          expect(windowMessageService.send).not.toHaveBeenCalled();
+        });
+      });
+
+      describe('and when the window was opened by another window', () => {
+        beforeEach(() => {
+          injectedWindow.opener = MOCK_WINDOW_OPENER as Window;
+
+          component.ngOnInit();
+        });
+
+        it('should send message', () => {
+          expect(windowMessageService.send).toHaveBeenCalledTimes(1);
+          expect(windowMessageService.send).toHaveBeenCalledWith(MOCK_WINDOW_MESSAGE);
+        });
+
+        it('should close the current browser window', () => {
+          expect(injectedWindow.close).toHaveBeenCalledTimes(1);
+        });
+      });
+    });
   });
 });
--- src/app/public/features/process-complete/pages/process-complete.component.ts
+++ src/app/public/features/process-complete/pages/process-complete.component.ts
@@ -1,8 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { WindowMessage } from '@core/window-message/interfaces/window-message.interface';
+import { WindowMessageService } from '@core/window-message/services/window-message.service';
+import { WINDOW_TOKEN } from '@core/window/window.token';
+import { PUBLIC_PATH_PARAMS } from '@public/public-routing-constants';
 
 @Component({
   selector: 'tsl-process-complete',
   templateUrl: './process-complete.component.html',
   styleUrls: ['./process-complete.component.scss'],
 })
-export class ProcessCompleteComponent {}
+export class ProcessCompleteComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private windowMessageService: WindowMessageService,
+    @Inject(WINDOW_TOKEN) private window: Window
+  ) {}
+
+  ngOnInit(): void {
+    const isNotIdInUrl: boolean = !this.id;
+    if (isNotIdInUrl) {
+      return this.closeCurrentWindow();
+    }
+
+    const windowDoesNotHaveOpener: boolean = !this.windowOpener;
+    if (windowDoesNotHaveOpener) {
+      return this.closeCurrentWindow();
+    }
+
+    this.sendSuccessMessage();
+    this.closeCurrentWindow();
+  }
+
+  private get id(): string {
+    return this.route.snapshot.paramMap.get(PUBLIC_PATH_PARAMS.ID);
+  }
+
+  private get windowOpener(): Window {
+    return this.window.opener;
+  }
+
+  private closeCurrentWindow(): void {
+    this.window.close();
+  }
+
+  private sendSuccessMessage(): void {
+    const success: WindowMessage = { id: this.id };
+    this.windowMessageService.send(success);
+  }
+}
