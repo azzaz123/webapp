cd20591905b8bda5e53fe2486903678b344ef46e
Laia Lopez <laialopezz99@gmail.com>
2022-03-09 16:05:46 +0100

    [WPA-11138]: [Routing] Fixed

--- src/app/private/features/accept-screen/accept-screen.component.ts
+++ src/app/private/features/accept-screen/accept-screen.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { PRIVATE_PATH_PARAMS } from '@private/private-routing-constants';
+import { PRIVATE_PATHS, PRIVATE_PATH_PARAMS } from '@private/private-routing-constants';
 import { AcceptScreenModalComponent } from './modals/accept-screen-modal/accept-screen-modal.component';
 import { DELIVERY_MODAL_CLASSNAME } from '../delivery/constants/delivery-constants';
+import { STREAMLINE_PATHS } from '../delivery/pages/streamline/streamline.routing.constants';
+import { DELIVERY_PATHS } from '../delivery/delivery-routing-constants';
 
 @Component({
   selector: 'tsl-accept-screen',
   template: '',
 })
 export class AcceptScreenComponent implements OnInit, OnDestroy {
-  constructor(private modalService: NgbModal, private route: ActivatedRoute) {}
+  private readonly ONGOING_SELLS_LINK: string = `${PRIVATE_PATHS.DELIVERY}/${DELIVERY_PATHS.SELLS}/${STREAMLINE_PATHS.ONGOING}`;
+
+  constructor(private modalService: NgbModal, private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
     const requestId: string = this.route.snapshot.paramMap.get(PRIVATE_PATH_PARAMS.ID);
@@ -18,12 +22,20 @@ export class AcceptScreenComponent implements OnInit, OnDestroy {
     modalRef.componentInstance.requestId = requestId;
 
     modalRef.result.then(
-      () => {},
-      () => {}
+      () => {
+        this.navigateToOngoingSells();
+      },
+      () => {
+        this.navigateToOngoingSells();
+      }
     );
   }
 
   ngOnDestroy(): void {
     this.modalService.dismissAll();
+  }
+
+  private navigateToOngoingSells(): void {
+    this.router.navigate([this.ONGOING_SELLS_LINK]);
   }
 }
