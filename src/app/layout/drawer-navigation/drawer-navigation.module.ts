2df58e7b5875eaaa6d0d460e5765eef3980ae74f
Alex Masnou <alex.masnou@wallapop.com>
2022-03-14 23:53:11 +0100

    [WPA-11227]: Import RouterModule

--- src/app/layout/drawer-navigation/drawer-navigation.module.ts
+++ src/app/layout/drawer-navigation/drawer-navigation.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DrawerNavigationComponent } from './components/drawer-navigation.component';
 import { DrawerNavigationService } from './services/drawer-navigation.service';
+import { SvgIconModule } from '@shared/svg-icon/svg-icon.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [DrawerNavigationComponent],
   exports: [DrawerNavigationComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SvgIconModule, RouterModule],
   providers: [DrawerNavigationService],
 })
 export class DrawerNavigationModule {}
