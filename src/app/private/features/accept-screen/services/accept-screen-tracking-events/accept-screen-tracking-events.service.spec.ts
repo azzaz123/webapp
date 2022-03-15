050f8909ada281b7e617f01cfd07d90805ea81fb
Laia Lopez <laialopezz99@gmail.com>
2022-03-15 16:47:34 +0100

    [WPA-11139]: [Testing] AcceptScreenTrackingEventsService

--- src/app/private/features/accept-screen/services/accept-screen-tracking-events/accept-screen-tracking-events.service.spec.ts
+++ src/app/private/features/accept-screen/services/accept-screen-tracking-events/accept-screen-tracking-events.service.spec.ts
@@ -1,16 +1,256 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { ANALYTICS_EVENT_NAMES, ANALYTIC_EVENT_TYPES, SCREEN_IDS } from '@core/analytics/analytics-constants';
+import { AnalyticsService } from '@core/analytics/analytics.service';
+import { FeatureFlagService } from '@core/user/featureflag.service';
+import { MockAnalyticsService } from '@fixtures/analytics.fixtures.spec';
+import {
+  MOCK_CLICK_ACCEPT_OFFER_EVENT_PROPERTIES_WITH_HPU,
+  MOCK_CLICK_ADD_EDIT_ADDRESS_EVENT_PROPERTIES_WITH_FULL_ADDRESS_AND_PO,
+  MOCK_CLICK_HELP_TRANSACTIONAL_EVENT_PROPERTIES,
+  MOCK_CLICK_ITEM_CARD_EVENT_PROPERTIES,
+  MOCK_CLICK_OTHER_PROFILE_EVENT_PROPERTIES,
+  MOCK_CLICK_REJECT_OFFER_EVENT_PROPERTIES_WITH_HPU,
+  MOCK_CLICK_SCHEDULE_HPU_EVENT_PROPERTIES,
+  MOCK_VIEW_ACCEPT_OFFER_EVENT_PROPERTIES_WITH_HPU,
+} from '@fixtures/private/delivery/accept-screen/accept-screen-event-properties.fixtures.spec';
+import { of } from 'rxjs';
 
 import { AcceptScreenTrackingEventsService } from './accept-screen-tracking-events.service';
 
 describe('AcceptScreenTrackingEventsService', () => {
   let service: AcceptScreenTrackingEventsService;
+  let analyticsService: AnalyticsService;
+  let featureFlagService: FeatureFlagService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        { provide: AnalyticsService, useClass: MockAnalyticsService },
+        {
+          provide: FeatureFlagService,
+          useValue: {
+            getLocalFlag() {
+              return of(false);
+            },
+          },
+        },
+      ],
+    });
     service = TestBed.inject(AcceptScreenTrackingEventsService);
+    analyticsService = TestBed.inject(AnalyticsService);
+    featureFlagService = TestBed.inject(FeatureFlagService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('and we call to track view accept offer event', () => {
+    beforeEach(() => {
+      spyOn(analyticsService, 'trackPageView');
+      service.trackViewAcceptOffer(MOCK_VIEW_ACCEPT_OFFER_EVENT_PROPERTIES_WITH_HPU);
+    });
+
+    it('should track the event', () => {
+      expect(analyticsService.trackPageView).toHaveBeenCalledTimes(1);
+    });
+
+    it('should track the event with specified properties', () => {
+      expect(analyticsService.trackPageView).toHaveBeenCalledWith({
+        name: ANALYTICS_EVENT_NAMES.ViewAcceptOffer,
+        attributes: {
+          ...MOCK_VIEW_ACCEPT_OFFER_EVENT_PROPERTIES_WITH_HPU,
+          screenId: SCREEN_IDS.AcceptOffer,
+        },
+      });
+    });
+  });
+
+  describe('and we call to track click item card event', () => {
+    beforeEach(() => {
+      spyOn(analyticsService, 'trackEvent');
+    });
+
+    describe('and the delivery feature flag is enabled', () => {
+      beforeEach(() => {
+        spyOn(featureFlagService, 'getLocalFlag').and.returnValue(of(true));
+        service.trackClickItemCard(MOCK_CLICK_ITEM_CARD_EVENT_PROPERTIES);
+      });
+
+      it('should track the event', () => {
+        expect(analyticsService.trackEvent).toHaveBeenCalledTimes(1);
+      });
+
+      it('should track the event with specified properties with shipping allowed', () => {
+        expect(analyticsService.trackEvent).toHaveBeenCalledWith({
+          name: ANALYTICS_EVENT_NAMES.ClickItemCard,
+          eventType: ANALYTIC_EVENT_TYPES.Navigation,
+          attributes: {
+            ...MOCK_CLICK_ITEM_CARD_EVENT_PROPERTIES,
+            screenId: SCREEN_IDS.AcceptOffer,
+            position: 0,
+            shippingAllowed: true,
+          },
+        });
+      });
+    });
+
+    describe('and the delivery feature flag is disabled', () => {
+      beforeEach(() => {
+        spyOn(featureFlagService, 'getLocalFlag').and.returnValue(of(false));
+        service.trackClickItemCard(MOCK_CLICK_ITEM_CARD_EVENT_PROPERTIES);
+      });
+
+      it('should track the event', () => {
+        expect(analyticsService.trackEvent).toHaveBeenCalledTimes(1);
+      });
+
+      it('should track the event with specified properties with NOT shipping allowed', () => {
+        expect(analyticsService.trackEvent).toHaveBeenCalledWith({
+          name: ANALYTICS_EVENT_NAMES.ClickItemCard,
+          eventType: ANALYTIC_EVENT_TYPES.Navigation,
+          attributes: {
+            ...MOCK_CLICK_ITEM_CARD_EVENT_PROPERTIES,
+            screenId: SCREEN_IDS.AcceptOffer,
+            position: 0,
+            shippingAllowed: false,
+          },
+        });
+      });
+    });
+  });
+
+  describe('and we call to track click other profile event', () => {
+    beforeEach(() => {
+      spyOn(analyticsService, 'trackEvent');
+      service.trackClickOtherProfile(MOCK_CLICK_OTHER_PROFILE_EVENT_PROPERTIES);
+    });
+
+    it('should track the event', () => {
+      expect(analyticsService.trackEvent).toHaveBeenCalledTimes(1);
+    });
+
+    it('should track the event with specified properties', () => {
+      expect(analyticsService.trackEvent).toHaveBeenCalledWith({
+        name: ANALYTICS_EVENT_NAMES.ClickItemCard,
+        eventType: ANALYTIC_EVENT_TYPES.Navigation,
+        attributes: {
+          ...MOCK_CLICK_OTHER_PROFILE_EVENT_PROPERTIES,
+          screenId: SCREEN_IDS.AcceptOffer,
+        },
+      });
+    });
+  });
+
+  describe('and we call to track click accept offer event', () => {
+    beforeEach(() => {
+      spyOn(analyticsService, 'trackEvent');
+      service.trackClickAcceptOffer(MOCK_CLICK_ACCEPT_OFFER_EVENT_PROPERTIES_WITH_HPU);
+    });
+
+    it('should track the event', () => {
+      expect(analyticsService.trackEvent).toHaveBeenCalledTimes(1);
+    });
+
+    it('should track the event with specified properties', () => {
+      expect(analyticsService.trackEvent).toHaveBeenCalledWith({
+        name: ANALYTICS_EVENT_NAMES.ClickAcceptOffer,
+        eventType: ANALYTIC_EVENT_TYPES.Navigation,
+        attributes: {
+          ...MOCK_CLICK_ACCEPT_OFFER_EVENT_PROPERTIES_WITH_HPU,
+          screenId: SCREEN_IDS.AcceptOffer,
+        },
+      });
+    });
+  });
+
+  describe('and we call to track click reject offer event', () => {
+    beforeEach(() => {
+      spyOn(analyticsService, 'trackEvent');
+      service.trackClickRejectOffer(MOCK_CLICK_REJECT_OFFER_EVENT_PROPERTIES_WITH_HPU);
+    });
+
+    it('should track the event', () => {
+      expect(analyticsService.trackEvent).toHaveBeenCalledTimes(1);
+    });
+
+    it('should track the event with specified properties', () => {
+      expect(analyticsService.trackEvent).toHaveBeenCalledWith({
+        name: ANALYTICS_EVENT_NAMES.ClickRejectOffer,
+        eventType: ANALYTIC_EVENT_TYPES.Navigation,
+        attributes: {
+          ...MOCK_CLICK_REJECT_OFFER_EVENT_PROPERTIES_WITH_HPU,
+          screenId: SCREEN_IDS.AcceptOffer,
+        },
+      });
+    });
+  });
+
+  describe('and we call to track click add edit address event', () => {
+    beforeEach(() => {
+      spyOn(analyticsService, 'trackEvent');
+      service.trackClickAddEditAddress(MOCK_CLICK_ADD_EDIT_ADDRESS_EVENT_PROPERTIES_WITH_FULL_ADDRESS_AND_PO);
+    });
+
+    it('should track the event', () => {
+      expect(analyticsService.trackEvent).toHaveBeenCalledTimes(1);
+    });
+
+    it('should track the event with specified properties', () => {
+      expect(analyticsService.trackEvent).toHaveBeenCalledWith({
+        name: ANALYTICS_EVENT_NAMES.ClickAddEditAddress,
+        eventType: ANALYTIC_EVENT_TYPES.Navigation,
+        attributes: {
+          ...MOCK_CLICK_ADD_EDIT_ADDRESS_EVENT_PROPERTIES_WITH_FULL_ADDRESS_AND_PO,
+          screenId: SCREEN_IDS.AcceptOffer,
+        },
+      });
+    });
+  });
+
+  describe('and we call to track click help transactional event', () => {
+    beforeEach(() => {
+      spyOn(analyticsService, 'trackEvent');
+      service.trackClickHelpTransactional(MOCK_CLICK_HELP_TRANSACTIONAL_EVENT_PROPERTIES);
+    });
+
+    it('should track the event', () => {
+      expect(analyticsService.trackEvent).toHaveBeenCalledTimes(1);
+    });
+
+    it('should track the event with specified properties', () => {
+      expect(analyticsService.trackEvent).toHaveBeenCalledWith({
+        name: ANALYTICS_EVENT_NAMES.ClickHelpTransactional,
+        eventType: ANALYTIC_EVENT_TYPES.Navigation,
+        attributes: {
+          ...MOCK_CLICK_HELP_TRANSACTIONAL_EVENT_PROPERTIES,
+          screenId: SCREEN_IDS.AcceptOffer,
+        },
+      });
+    });
+  });
+
+  describe('and we call to track click schedule HPU event', () => {
+    beforeEach(() => {
+      spyOn(analyticsService, 'trackEvent');
+      service.trackClickScheduleHPU(MOCK_CLICK_SCHEDULE_HPU_EVENT_PROPERTIES);
+    });
+
+    it('should track the event', () => {
+      expect(analyticsService.trackEvent).toHaveBeenCalledTimes(1);
+    });
+
+    it('should track the event with specified properties', () => {
+      expect(analyticsService.trackEvent).toHaveBeenCalledWith({
+        name: ANALYTICS_EVENT_NAMES.ClickScheduleHPU,
+        eventType: ANALYTIC_EVENT_TYPES.UserPreference,
+        attributes: {
+          ...MOCK_CLICK_SCHEDULE_HPU_EVENT_PROPERTIES,
+          screenId: SCREEN_IDS.AcceptOffer,
+        },
+      });
+    });
   });
 });
