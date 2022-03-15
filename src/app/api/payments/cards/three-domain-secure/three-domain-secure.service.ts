51c118edcaeb802151f826b6c3651e9d01062243
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-03-15 11:33:41 +0100

    [WPA-11375]: Simplify 3DS service observables logic

--- src/app/api/payments/cards/three-domain-secure/three-domain-secure.service.ts
+++ src/app/api/payments/cards/three-domain-secure/three-domain-secure.service.ts
@@ -6,8 +6,8 @@ import { FEATURE_FLAGS_ENUM } from '@core/user/featureflag-constants';
 import { FeatureFlagService } from '@core/user/featureflag.service';
 import { environment } from '@environments/environment.beta';
 import { WebViewModalService } from '@shared/web-view-modal/services/web-view-modal.service';
-import { Observable, of, Subject, timer } from 'rxjs';
-import { filter, concatMap, take, takeUntil, tap } from 'rxjs/operators';
+import { Observable, of, ReplaySubject, throwError, timer } from 'rxjs';
+import { filter, concatMap, take, takeUntil, tap, catchError } from 'rxjs/operators';
 
 const THREE_DOMAIN_SECURE_START_URL = (id: string): string => `${environment.baseUrl}api/v3/payments/cards/start_3ds/${id}`;
 type GetCreditCardRequest = (ignoreInvalidCard: boolean) => Observable<CreditCard>;
@@ -16,25 +16,18 @@ type GetCreditCardRequest = (ignoreInvalidCard: boolean) => Observable<CreditCar
   providedIn: 'root',
 })
 export class ThreeDomainSecureService {
-  private readonly processCompleted: Subject<void> = new Subject<void>();
-  private cardValidationError: Error[] = [new CardRegistrationFailedError()];
-
   constructor(private featureFlagService: FeatureFlagService, private webViewModalService: WebViewModalService) {}
 
   public checkThreeDomainSecure(getCreditCardRequest: GetCreditCardRequest): Observable<void> {
-    this.isThreeDSecureEnabled()
-      .pipe(
-        filter((enabled) => enabled),
-        concatMap(() =>
-          this.checkCardUntilKnownStatus(getCreditCardRequest).pipe(
-            concatMap((card) => (this.isPending3DSCard(card) ? this.start3DSValidation(card) : of(card)))
-          )
-        ),
-        take(1)
-      )
-      .subscribe({ next: this.trigger3DSDone, error: this.trigger3DSError, complete: this.trigger3DSDone });
-
-    return this.processCompleted;
+    return this.isThreeDSecureEnabled().pipe(
+      concatMap((enabled) => {
+        const threeDSFlow = this.checkCardUntilKnownStatus(getCreditCardRequest).pipe(
+          concatMap((card) => (this.isPending3DSCard(card) ? this.start3DSValidation(card) : of(card)))
+        );
+        return enabled ? threeDSFlow : of(null);
+      }),
+      take(1)
+    );
   }
 
   public cardNeedsToBeRemoved(card: CreditCard): boolean {
@@ -44,7 +37,7 @@ export class ThreeDomainSecureService {
   private checkCardUntilKnownStatus(getCreditCard: GetCreditCardRequest): Observable<CreditCard> {
     const checkIntervalMs = 1000;
     const maxChecks = 30;
-    const checkCardSubject: Subject<CreditCard> = new Subject<CreditCard>();
+    const checkCardSubject: ReplaySubject<CreditCard> = new ReplaySubject<CreditCard>();
 
     timer(0, checkIntervalMs)
       .pipe(
@@ -68,23 +61,9 @@ export class ThreeDomainSecureService {
   }
 
   private start3DSValidation(card: CreditCard): Observable<void> {
-    const modalDoneSubject: Subject<void> = new Subject<void>();
-
-    const modalDoneTrigger = () => {
-      modalDoneSubject.next();
-    };
-
-    const modalErrorTrigger = () => {
-      modalDoneSubject.error(this.cardValidationError);
-    };
-
     const { id } = card;
     const threeDSecureStartUrl: string = THREE_DOMAIN_SECURE_START_URL(id);
-    this.webViewModalService
-      .open(threeDSecureStartUrl)
-      .subscribe({ next: modalDoneTrigger, error: modalErrorTrigger, complete: modalDoneTrigger });
-
-    return modalDoneSubject;
+    return this.webViewModalService.open(threeDSecureStartUrl).pipe(catchError(() => throwError([new CardRegistrationFailedError()])));
   }
 
   private isInvalidCard(card: CreditCard): boolean {
@@ -94,12 +73,4 @@ export class ThreeDomainSecureService {
   private isPending3DSCard(card: CreditCard): boolean {
     return card.status === CREDIT_CARD_STATUS.PENDING_3DS;
   }
-
-  private trigger3DSDone = () => {
-    this.processCompleted.next();
-  };
-
-  private trigger3DSError = () => {
-    this.processCompleted.error(this.cardValidationError);
-  };
 }
