3aa96b51c6b0b8666252b3b404fc237fc286c8be
Carlos Roth <carlos@ulex.es>
2022-03-16 19:20:44 +0100

    [WPA-11560]: Add new copy for cancelled request

--- src/app/private/features/accept-screen/modals/accept-screen-modal/accept-screen-modal.component.ts
+++ src/app/private/features/accept-screen/modals/accept-screen-modal/accept-screen-modal.component.ts
@@ -50,6 +50,8 @@ export class AcceptScreenModalComponent implements OnInit {
   private readonly deliveryMapSlideId: number = ACCEPT_SCREEN_STEPS.MAP;
   private readonly ACCEPT_SCREEN_HEADER_TRANSLATIONS = ACCEPT_SCREEN_HEADER_TRANSLATIONS;
   private readonly GENERIC_ERROR_TRANSLATION: string = $localize`:@@accept_view_seller_all_all_snackbar_generic_error:¡Oops! Something has gone wrong. Try again.`;
+  private readonly EXPIRED_OR_CANCELLED_ERROR_TRANSLATION: string = $localize`:@@accept_view_seller_all_all_snackbar_expired_or_cancelled_purchase:The purchase has expired or the buyer has cancelled it. Talk to them to try again
+  .`;
   private isMapPreviousPage$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   constructor(
@@ -67,7 +69,7 @@ export class AcceptScreenModalComponent implements OnInit {
     this.acceptScreenStoreService.initialize(this.requestId).then(
       () => {},
       () => {
-        this.showError(this.GENERIC_ERROR_TRANSLATION);
+        this.showError(this.EXPIRED_OR_CANCELLED_ERROR_TRANSLATION);
         this.closeModal();
       }
     );
