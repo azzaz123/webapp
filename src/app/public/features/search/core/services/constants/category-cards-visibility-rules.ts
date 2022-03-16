dc86cb551a38bc4ed26717be00c89f071b14f4ff
Irene Blanco <irene.blanco@wallapop.com>
2022-03-16 10:41:40 +0100

    [WPA-7749]: CATEGORY_CARDS_VISIBILITY_RULES - Added   NOT_ALLOWED_PARAMETER_VALUES

--- src/app/public/features/search/core/services/constants/category-cards-visibility-rules.ts
+++ src/app/public/features/search/core/services/constants/category-cards-visibility-rules.ts
@@ -1,3 +1,4 @@
+import { CATEGORY_IDS } from '@core/category/category-ids';
 import { FILTER_QUERY_PARAM_KEY } from '@public/shared/components/filters/enums/filter-query-param-key.enum';
 import { FILTER_PARAMETERS_SEARCH } from './filter-parameters';
 
@@ -11,4 +12,7 @@ export const CATEGORY_CARDS_VISIBILITY_RULES = {
     FILTER_PARAMETERS_SEARCH.LONGITUDE,
   ],
   REQUIRED_PARAMETER_KEYS: [FILTER_QUERY_PARAM_KEY.categoryId],
+  NOT_ALLOWED_PARAMETER_VALUES: {
+    [FILTER_QUERY_PARAM_KEY.categoryId]: [CATEGORY_IDS.CAR, CATEGORY_IDS.REAL_ESTATE, CATEGORY_IDS.MOTORBIKE],
+  },
 };
