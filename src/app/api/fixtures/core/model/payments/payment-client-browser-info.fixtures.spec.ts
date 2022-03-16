f77aa5b4f1004a637908dee7cdb2391e7b8d530c
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-03-16 15:53:04 +0100

    [WPA-11211]: Create fixture for payments client browser info interface

---
+++ src/app/api/fixtures/core/model/payments/payment-client-browser-info.fixtures.spec.ts
@@ -0,0 +1,15 @@
+import { PaymentsClientBrowserInfo } from '@api/core/model/payments';
+
+export const MOCK_PAYMENT_CLIENT_BROWSER_INFO: PaymentsClientBrowserInfo = {
+  acceptHeader:
+    'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3, Upgrade-Insecure-Requests=1',
+  isJavaEnabled: false,
+  isJavaScriptEnabled: false,
+  language: 'ca',
+  colorDepth: 24,
+  screenHeight: 2034,
+  screenWidth: 1080,
+  timeZoneOffset: '+60',
+  userAgent:
+    'Mozilla/5.0 (Linux; Android 10; ONEPLUS A5010 Build/QKQ1.191014.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/99.0.4844.58 Mobile Safari/537.36',
+};
