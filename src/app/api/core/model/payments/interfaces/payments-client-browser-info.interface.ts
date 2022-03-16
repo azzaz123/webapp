53ac297c7880e681a2ca992877a4fb2fb9417bf7
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-03-16 15:17:41 +0100

    [WPA-11211]: Create payments client browser info interface

---
+++ src/app/api/core/model/payments/interfaces/payments-client-browser-info.interface.ts
@@ -0,0 +1,11 @@
+export interface PaymentsClientBrowserInfo {
+  acceptHeader: string;
+  isJavaEnabled: boolean;
+  isJavaScriptEnabled: boolean;
+  language: string;
+  colorDepth: string;
+  screenHeight: string;
+  screenWidth: string;
+  timeZoneOffset: string;
+  userAgent: string;
+}
