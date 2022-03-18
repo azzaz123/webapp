54e98ca06450b3b21cb4f08153e2b0d15f0efb74
Alex Masnou <alex.masnou@wallapop.com>
2022-03-18 16:19:08 +0100

    [WPA-11227]: Add methods to get child navigation elements

--- src/app/layout/drawer-navigation/services/drawer-navigation.service.ts
+++ src/app/layout/drawer-navigation/services/drawer-navigation.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { DRAWER_NAVIGATION_SECTIONS_COLLECTION } from '../constants/drawer-navigation-sections';
-import { DrawerNavigationSection } from '../interfaces/drawer-navigation-element.interface';
+import { DrawerNavigationElement, DrawerNavigationSection } from '../interfaces/drawer-navigation-element.interface';
 
 @Injectable()
 export class DrawerNavigationService {
@@ -9,5 +10,18 @@ export class DrawerNavigationService {
 
   get navigationSections$(): Observable<DrawerNavigationSection[]> {
     return of(Object.values(DRAWER_NAVIGATION_SECTIONS_COLLECTION));
+  }
+
+  public getChildNavigationElements(route: string) {
+    return this.navigationSections$.pipe(
+      map((sections) => this.findChildNavigationElements(sections, route)),
+      tap((x) => console.log(x))
+    );
+  }
+
+  private findChildNavigationElements(sections: DrawerNavigationSection[], route: string): DrawerNavigationElement[] {
+    return sections.reduce((acc, section) => {
+      return section.elements.find((y) => y.href === route)?.children ?? acc;
+    }, []);
   }
 }
