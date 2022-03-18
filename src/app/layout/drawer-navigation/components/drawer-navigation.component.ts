62fe373782e1cd0a263541a0c9b7751bf38b94d1
Alex Masnou <alex.masnou@wallapop.com>
2022-03-18 16:58:15 +0100

    [WPA-11227]: Add missing navigation options

--- src/app/layout/drawer-navigation/components/drawer-navigation.component.ts
+++ src/app/layout/drawer-navigation/components/drawer-navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { DrawerNavigationSection } from '../interfaces/drawer-navigation-element.interface';
 import { DrawerNavigationService } from '../services/drawer-navigation.service';
