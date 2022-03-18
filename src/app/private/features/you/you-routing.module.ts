0285762937020b7b10a88eedaa114506bb709093
Alex Masnou <alex.masnou@wallapop.com>
2022-03-18 16:18:44 +0100

    [WPA-11227]: Add routing for you module

--- src/app/private/features/you/you-routing.module.ts
+++ src/app/private/features/you/you-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DrawerNavigationSecondaryPageComponent } from '@layout/drawer-navigation/components/drawer-navigation-secondary-page/drawer-navigation-secondary-page.component';
+import { DrawerNavigationComponent } from '@layout/drawer-navigation/components/drawer-navigation.component';
 import { YouComponent } from './pages/you.component';
 
 const routes: Routes = [
   {
     path: '',
     component: YouComponent,
+    children: [
+      {
+        path: '',
+        component: DrawerNavigationComponent,
+      },
+      {
+        path: 'settings',
+        component: DrawerNavigationSecondaryPageComponent,
+      },
+    ],
   },
 ];
 
