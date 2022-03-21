cc96085b7edb008b9091cf5a56cabd97177f3264
Alex Masnou <alex.masnou@wallapop.com>
2022-03-21 13:28:44 +0100

    [WPA-11227]: Remove not needed files

--- src/app/private/features/you/pages/you.component.ts
+++ src/app/private/features/you/pages/you.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'tsl-you',
   templateUrl: './you.component.html',
-  styleUrls: ['./you.component.scss'],
 })
 export class YouComponent {}
