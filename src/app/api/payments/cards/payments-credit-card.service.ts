8c4243c876241880506eefeaaf51415e8e5a823a
antoni-casas <toni.casas@wallapop.com>
2022-03-22 14:37:16 +0100

    [WPA-9148]: feat: Propagate error when retrieving credit card information

--- src/app/api/payments/cards/payments-credit-card.service.ts
+++ src/app/api/payments/cards/payments-credit-card.service.ts
@@ -51,10 +51,13 @@ export class PaymentsCreditCardService {
           );
         })
       )
-      .subscribe((creditCard) => {
-        this.creditCard = creditCard;
-        creditCardGetSubject.next(creditCard);
-      });
+      .subscribe(
+        (creditCard) => {
+          this.creditCard = creditCard;
+          creditCardGetSubject.next(creditCard);
+        },
+        (error) => creditCardGetSubject.error(error)
+      );
 
     return creditCardGetSubject.asObservable();
   }
