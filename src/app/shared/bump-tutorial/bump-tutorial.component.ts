d872b6e035d15657d589b7446d89b550d9945a4e
Gonzalo Morales <gonzalomorales93@gmail.com>
2022-03-23 17:33:22 +0100

    [WPA-11830]: add tests

--- src/app/shared/bump-tutorial/bump-tutorial.component.ts
+++ src/app/shared/bump-tutorial/bump-tutorial.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnDestroy } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { range } from 'lodash-es';
 import { BumpTutorialService } from './services/bump-tutorial.service';
 import { CustomerHelpService } from '@core/external-links/customer-help/customer-help.service';
@@ -17,13 +17,16 @@ export const SLIDES_NUMBER = 2;
   templateUrl: './bump-tutorial.component.html',
   styleUrls: ['./bump-tutorial.component.scss'],
 })
-export class BumpTutorialComponent implements OnDestroy {
+export class BumpTutorialComponent implements OnInit, OnDestroy {
   public dots: number;
   public hidden = true;
   public subscriptionBumpsUrl: string;
 
   constructor(public tutorialService: BumpTutorialService, private customerHelpService: CustomerHelpService) {
     this.dots = range(SLIDES_NUMBER);
+  }
+
+  ngOnInit() {
     this.subscriptionBumpsUrl = this.customerHelpService.getPageUrl(CUSTOMER_HELP_PAGE.PROS_SUBSCRIPTION_BUMPS);
   }
 
