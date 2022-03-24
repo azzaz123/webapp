f80e16c04a37d243e6b7267233af444c857cb099
Alex Masnou <alex.masnou@wallapop.com>
2022-03-24 16:06:43 +0100

    [WPA-12098]: Extract to constant

--- src/app/private/features/you/guards/you.guard.ts
+++ src/app/private/features/you/guards/you.guard.ts
@@ -1,17 +1,18 @@
 import { Route } from '@angular/compiler/src/core';
 import { Inject, Injectable } from '@angular/core';
-import { CanLoad, Router, UrlSegment, UrlTree } from '@angular/router';
+import { CanLoad, Router, UrlSegment } from '@angular/router';
 import { WINDOW_TOKEN } from '@core/window/window.token';
 import { PRIVATE_PATHS } from '@private/private-routing-constants';
 import { APP_PATHS } from 'app/app-routing-constants';
-import { Observable } from 'rxjs';
+
+const LG_BREAKPOINT = '991px';
 
 @Injectable()
 export class YouGuard implements CanLoad {
   constructor(@Inject(WINDOW_TOKEN) private window: Window, private router: Router) {}
 
   canLoad(_: Route, segments: UrlSegment[]): boolean {
-    const isBottomNavigationBarVisible = this.window.matchMedia('(max-width: 991px)').matches;
+    const isBottomNavigationBarVisible = this.window.matchMedia(`(max-width: ${LG_BREAKPOINT})`).matches;
     const isChildRoute = segments.length > 1;
 
     if (isBottomNavigationBarVisible) {
