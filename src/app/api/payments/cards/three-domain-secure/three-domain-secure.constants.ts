123b0137445f5620b966c1001a381e8d45ea0b0c
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-03-24 12:45:04 +0100

    [WPA-11452]: Increase 3DS modal size to match missing padding

--- src/app/api/payments/cards/three-domain-secure/three-domain-secure.constants.ts
+++ src/app/api/payments/cards/three-domain-secure/three-domain-secure.constants.ts
@@ -1,2 +1,2 @@
-export const THREE_DOMAIN_SECURE_MODAL_WIDTH: number = 375;
-export const THREE_DOMAIN_SECURE_MODAL_HEIGHT: number = 640;
+export const THREE_DOMAIN_SECURE_MODAL_WIDTH: number = 400;
+export const THREE_DOMAIN_SECURE_MODAL_HEIGHT: number = 650;
