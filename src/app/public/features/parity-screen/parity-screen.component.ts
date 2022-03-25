0ebc1c986b0794025039720908ce606fc2dcc034
Robert Vera <robertveraroige@gmail.com>
2022-03-25 16:20:25 +0100

    [WPA-10903]: final styles

--- src/app/public/features/parity-screen/parity-screen.component.ts
+++ src/app/public/features/parity-screen/parity-screen.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { DeviceService } from '@core/device/device.service';
 import { PublicFooterService } from '@public/core/services/footer/public-footer.service';
-import { take } from 'rxjs/operators';
+import { QR_CODE_SIZE } from '@public/features/parity-screen/parity-screen.enum';
 
 @Component({
   selector: 'tsl-parity',
@@ -10,12 +11,16 @@ import { take } from 'rxjs/operators';
 export class ParityScreenComponent implements AfterViewInit {
   @ViewChild('qrCode', { static: false }) qrCode: any;
 
+  public qrSize: QR_CODE_SIZE;
+  public qrData: string;
   private imgURL: string = 'assets/images/generic-landing/wallapop-logo-black-round.svg';
   private mutationObserverExists: boolean;
 
-  constructor(private publicFooterService: PublicFooterService) {
+  constructor(private publicFooterService: PublicFooterService, private deviceService: DeviceService) {
     this.publicFooterService.setShow(false);
     this.mutationObserverExists = !!window.MutationObserver;
+    this.qrSize = this.setQrSize(this.deviceService.isMobile());
+    this.qrData = 'https://www.wallapop.com/';
   }
 
   public ngAfterViewInit(): void {
@@ -38,13 +43,18 @@ export class ParityScreenComponent implements AfterViewInit {
           const context = target.getContext('2d');
           const image = new Image();
           image.src = this.imgURL;
-          const iWidth = 60;
-          const iHeight = 60;
+          const iWidth = 38;
+          const iHeight = 38;
+          target.style.verticalAlign = 'bottom';
           image.onload = () => {
             context.drawImage(image, target.width / 2 - iWidth / 2, target.height / 2 - iHeight / 2, iWidth, iHeight);
           };
         }
       }
     });
+  }
+
+  private setQrSize(isMobile: boolean): QR_CODE_SIZE {
+    return isMobile ? QR_CODE_SIZE.SMALL : QR_CODE_SIZE.BIG;
   }
 }
---
+++ src/app/public/features/parity-screen/parity-screen.enum.ts
@@ -0,0 +1,4 @@
+export enum QR_CODE_SIZE {
+  SMALL = '140',
+  BIG = '180',
+}
