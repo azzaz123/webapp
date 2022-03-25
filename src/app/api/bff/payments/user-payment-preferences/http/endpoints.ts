95ecf4e784a29df425dbae76b5ea4a1e578c0d67
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-03-25 15:15:57 +0100

    [WPA-11988]: Use valid update user payment preferences endpoint

--- src/app/api/bff/payments/user-payment-preferences/http/endpoints.ts
+++ src/app/api/bff/payments/user-payment-preferences/http/endpoints.ts
@@ -2,4 +2,5 @@ import { environment } from '@environments/environment';
 
 const PAYMENTS_URL: string = `${environment.baseUrl}bff/payments`;
 export const USER_PAYMENT_PREFERENCES_ENDPOINT: string = `${PAYMENTS_URL}/user_payment_preferences`;
-export const USER_PAYMENT_PREFERENCES_UPDATE_ENDPOINT = (preferencesId: string) => `${USER_PAYMENT_PREFERENCES_ENDPOINT}/${preferencesId}`;
+export const USER_PAYMENT_PREFERENCES_UPDATE_ENDPOINT = (preferencesId: string) =>
+  `${environment.baseUrl}api/v3/payments/user_payment_preferences/${preferencesId}`;
