0308a669e73461e723737c6980103dc47318d77f
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-03-25 14:45:17 +0100

    [WPA-11988]: Add update user payment preferences to http service

--- src/app/api/bff/payments/user-payment-preferences/http/payments-user-payment-preferences-http.service.ts
+++ src/app/api/bff/payments/user-payment-preferences/http/payments-user-payment-preferences-http.service.ts
@@ -5,9 +5,9 @@ import {
   USER_PAYMENT_PREFERENCES_ENDPOINT,
   USER_PAYMENT_PREFERENCES_UPDATE_ENDPOINT,
 } from '@api/bff/payments/user-payment-preferences/http/endpoints';
-import { PaymentsUserPaymentPreferences } from '@api/core/model/payments';
 
 import { Observable } from 'rxjs';
+import { PaymentsUserPaymentPreferencesUpdateDto } from '../dtos/requests/payments-user-payment-preferences-update.interface';
 import { PaymentsUserPaymentPreferencesDto } from '../dtos/responses/payments-user-payment-preferences-dto.interface';
 
 @Injectable({
@@ -18,5 +18,9 @@ export class PaymentsUserPaymentPreferencesHttpService {
 
   public getUserPaymentPreferences(): Observable<PaymentsUserPaymentPreferencesDto> {
     return this.http.get<PaymentsUserPaymentPreferencesDto>(USER_PAYMENT_PREFERENCES_ENDPOINT);
+  }
+
+  public update(id: string, body: PaymentsUserPaymentPreferencesUpdateDto): Observable<void> {
+    return this.http.put<void>(USER_PAYMENT_PREFERENCES_UPDATE_ENDPOINT(id), body);
   }
 }
--- src/app/api/bff/payments/user-payment-preferences/payments-user-payment-preferences.service.ts
+++ src/app/api/bff/payments/user-payment-preferences/payments-user-payment-preferences.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 
-import { mapPaymentsUserPaymentPreferencesDtoToPaymentsUserPaymentPreferences } from '@api/bff/payments/user-payment-preferences/mappers/payments-user-payment-preferences.mapper';
+import { mapPaymentsUserPaymentPreferencesDtoToPaymentsUserPaymentPreferences } from '@api/bff/payments/user-payment-preferences/mappers/responses/payments-user-payment-preferences.mapper';
 import { PaymentsUserPaymentPreferences } from '@api/core/model/payments/interfaces/payments-user-payment-preferences.interface';
 import { PaymentsUserPaymentPreferencesHttpService } from '@api/bff/payments/user-payment-preferences/http/payments-user-payment-preferences-http.service';
 
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { PaymentsUserPaymentPreferencesUpdateDto } from './dtos/requests/payments-user-payment-preferences-update.interface';
+import { mapUserPaymentsPreferencesToDto } from './mappers/requests/payments-user-payment-preferences-update.mapper';
 
 @Injectable({
   providedIn: 'root',
@@ -17,5 +19,11 @@ export class PaymentsUserPaymentPreferencesService {
     return this.paymentUserPreferencesHttpService
       .getUserPaymentPreferences()
       .pipe(map(mapPaymentsUserPaymentPreferencesDtoToPaymentsUserPaymentPreferences));
+  }
+
+  public update(newPreferences: PaymentsUserPaymentPreferences): Observable<void> {
+    const { id } = newPreferences.preferences;
+    const preferencesDto: PaymentsUserPaymentPreferencesUpdateDto = mapUserPaymentsPreferencesToDto(newPreferences);
+    return this.paymentUserPreferencesHttpService.update(id, preferencesDto);
   }
 }
