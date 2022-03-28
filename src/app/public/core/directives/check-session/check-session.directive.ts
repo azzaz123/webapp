0c9e2deb50115e0d8516b84fd83d65820b5e300b
Àlex Delgado <alejandro.delgado@wallapop.com>
2022-03-28 11:45:36 +0200

    [WPA-7949]: added event and data.attribute to fav-user

--- src/app/public/core/directives/check-session/check-session.directive.ts
+++ src/app/public/core/directives/check-session/check-session.directive.ts
@@ -7,7 +7,8 @@ import { CheckSessionService } from '@public/core/services/check-session/check-s
 export class CheckSessionDirective {
   constructor(private checkSessionService: CheckSessionService) {}
 
-  @HostListener('click') onClick() {
-    this.checkSessionService.checkSessionAction();
+  @HostListener('click', ['$event']) onClick($event) {
+    const loginSource = $event.currentTarget.getAttribute('data-loginsource');
+    this.checkSessionService.checkSessionAction(loginSource);
   }
 }
