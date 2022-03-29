7d1e5722d939c6c94e6b1e0577eac91acc73525d
Irene Blanco <irene.blanco@wallapop.com>
2022-03-29 12:57:53 +0200

    [WPA-10544]: Created FilterGroupReloadRule interface

--- src/app/public/shared/components/filters/core/interfaces/filter-group-rules.interface.ts
+++ src/app/public/shared/components/filters/core/interfaces/filter-group-rules.interface.ts
@@ -2,10 +2,9 @@ import { FILTER_QUERY_PARAM_KEY } from '../../enums/filter-query-param-key.enum'
 import { ConfigurationId } from '../types/configuration-id.type';
 
 export interface FilterGroupRules {
-  reload?: [
-    {
-      parentParamKey: FILTER_QUERY_PARAM_KEY;
-      childFilterConfigId: ConfigurationId[keyof ConfigurationId];
-    }
-  ];
+  reload?: FilterGroupReloadRule[];
+}
+export interface FilterGroupReloadRule {
+  parentParamKey: FILTER_QUERY_PARAM_KEY;
+  childFilterConfigId: ConfigurationId[keyof ConfigurationId];
 }
