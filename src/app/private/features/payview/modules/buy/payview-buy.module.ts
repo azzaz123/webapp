fba98f8a08384521af683833372a9a8af1d0dce1
Laia Lopez <laialopezz99@gmail.com>
2022-03-29 15:28:48 +0200

    [WPA-9150]: [Button] Added

--- src/app/private/features/payview/modules/buy/payview-buy.module.ts
+++ src/app/private/features/payview/modules/buy/payview-buy.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ButtonModule } from '@shared/button/button.module';
+import { PayviewBuyOverviewComponent } from './components/overview/payview-buy-overview.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [],
-  providers: [],
-  exports: [],
+  imports: [CommonModule, ButtonModule],
+  declarations: [PayviewBuyOverviewComponent],
+  exports: [PayviewBuyOverviewComponent],
 })
 export class PayviewBuyModule {}
