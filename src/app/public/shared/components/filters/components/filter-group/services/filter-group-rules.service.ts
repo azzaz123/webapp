d5bb2ef8e5e72d5250794b61e210e1d7ea43974d
Irene Blanco <irene.blanco@wallapop.com>
2022-03-30 10:09:37 +0200

    [WPA-10544]: Created FilterGroupRulesService

---
+++ src/app/public/shared/components/filters/components/filter-group/services/filter-group-rules.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, SimpleChange } from '@angular/core';
+import { FilterGroupReloadRule } from '../../../core/interfaces/filter-group-rules.interface';
+import { ConfigurationId } from '../../../core/types/configuration-id.type';
+
+@Injectable()
+export class FilterGroupRulesService {
+  public getFilterConfigIdsToBeReloaded(
+    reloadRules: FilterGroupReloadRule[],
+    values: SimpleChange
+  ): ConfigurationId[keyof ConfigurationId][] {
+    const parentParamKeysWithReloadRules = reloadRules?.map((rule) => rule.parentParamKey);
+
+    return parentParamKeysWithReloadRules
+      ?.filter((parentParamKey) => {
+        const previousValue = values.previousValue.find((value) => value.key === parentParamKey)?.value; //esto ha de ser de config!
+        const currentValue = values.currentValue.find((value) => value.key === parentParamKey)?.value; //esto ha de ser de config!
+
+        return previousValue !== currentValue;
+      })
+      .map((parentParamKeyToBeReloaded) => {
+        return reloadRules?.find((rule) => {
+          return rule.parentParamKey === parentParamKeyToBeReloaded;
+        })?.childFilterConfigId;
+      });
+  }
+}
