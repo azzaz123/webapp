1c82ffdcb5a28bec329bf53464229b32caf6589f
Laia Lopez <laialopezz99@gmail.com>
2022-03-31 11:13:12 +0200

    [WPA-12259]: [PayviewBuyOverviewComponent] Frontend checking logic

--- src/app/private/features/payview/modules/buy/components/overview/payview-buy-overview.component.ts
+++ src/app/private/features/payview/modules/buy/components/overview/payview-buy-overview.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnDestroy, OnInit } from '@angular/core';
 
 import { PAYVIEW_BUY_DEFAULT_ERROR, PAYVIEW_BUY_ERRORS } from '@private/features/payview/modules/buy/constants/payview-buy-copies';
 import { PAYVIEW_BUY_EVENT_TYPE } from '@private/features/payview/modules/buy/enums/payview-buy-event-type.enum';
@@ -6,6 +6,11 @@ import { PayviewBuyService } from '@private/features/payview/modules/buy/service
 import { PayviewError } from '@private/features/payview/interfaces/payview-error.interface';
 
 import { Subscription } from 'rxjs';
+import { PayviewState } from '@private/features/payview/interfaces/payview-state.interface';
+import { PayviewStateDelivery } from '@private/features/payview/interfaces/payview-state-delivery.interface';
+import { DELIVERY_MODE } from '@api/core/model/delivery/delivery-mode.type';
+import { DeliveryBuyerDeliveryMethod } from '@api/core/model/delivery/buyer/delivery-methods';
+import { PAYVIEW_PAYMENT_METHOD } from '@api/core/model/payments';
 
 @Component({
   selector: 'tsl-payview-buy-overview',
@@ -14,6 +19,7 @@ import { Subscription } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PayviewBuyOverviewComponent implements OnDestroy, OnInit {
+  @Input() payviewState: PayviewState;
   public errorMessage: string;
 
   private subscription: Subscription;
@@ -29,7 +35,46 @@ export class PayviewBuyOverviewComponent implements OnDestroy, OnInit {
   }
 
   public buy(): void {
+    this.checkPrePaymentConditions();
     this.buyService.buy();
+  }
+
+  public checkPrePaymentConditions() {
+    if (!this.payviewState) {
+      return 'unknown error';
+    }
+    this.checkDeliveryMethodConditions(this.payviewState.delivery);
+    if (!this.payviewState.payment.preferences.preferences) {
+      return 'no payment selected';
+    }
+    if (this.selectedPaymentMethodIsAnEmptyCard) {
+      return 'payment info missing';
+    }
+  }
+
+  private checkDeliveryMethodConditions(deliveryMethods: PayviewStateDelivery) {
+    const selectedCarrier: DeliveryBuyerDeliveryMethod = deliveryMethods.methods.current;
+    if (!deliveryMethods.methods.current) {
+      return 'no delivery method selected';
+    }
+    if (!selectedCarrier.lastAddressUsed) {
+      if (selectedCarrier.method === DELIVERY_MODE.BUYER_ADDRESS) {
+        return 'empty buyer address';
+      }
+      if (selectedCarrier.method === DELIVERY_MODE.CARRIER_OFFICE) {
+        return 'empty post office address';
+      }
+    }
+    if (selectedCarrier.method === DELIVERY_MODE.CARRIER_OFFICE && !deliveryMethods.address) {
+      return 'empty post office return address';
+    }
+  }
+
+  private get selectedPaymentMethodIsAnEmptyCard(): boolean {
+    return (
+      this.payviewState.payment.preferences.preferences.paymentMethod === PAYVIEW_PAYMENT_METHOD.CREDIT_CARD &&
+      !this.payviewState.payment.card
+    );
   }
 
   private getErrorMessage(error: PayviewError): string {
