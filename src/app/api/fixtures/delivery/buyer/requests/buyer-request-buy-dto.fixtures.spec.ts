51ff3e195d8108df50105c68aa400ee2008fac29
Carlos Roth <carlos@ulex.es>
2022-03-31 16:41:02 +0200

    [WPA-12213]: Add mapper with tests

--- src/app/api/fixtures/delivery/buyer/requests/buyer-request-buy-dto.fixtures.spec.ts
+++ src/app/api/fixtures/delivery/buyer/requests/buyer-request-buy-dto.fixtures.spec.ts
@@ -2,10 +2,10 @@ import { BuyerRequestBuyDto } from '@api/delivery/buyer/requests/dtos/buyer-requ
 
 export const MOCK_BUYER_REQUEST_BUY_DTO_WITH_BUYER_ADDRESS: BuyerRequestBuyDto = {
   id: '12345',
-  item_hash: '54321',
-  seller_user_hash: '98765',
+  item_hash: 'p61v99q1rx65',
+  seller_user_hash: 'v4z4rv0lx86y',
   offered_price: {
-    amount: 20,
+    amount: 63,
     currency: 'EUR',
   },
   carrier_delivery_mode: 'BUYER_ADDRESS',
@@ -16,7 +16,7 @@ export const MOCK_BUYER_REQUEST_BUY_DTO_WITH_CARRIER_OFFICE: BuyerRequestBuyDto 
   carrier_delivery_mode: 'CARRIER_OFFICE',
 };
 
-export const MOCK_BUYER_REQUEST_BUY_DTO_WITH_CARRIER_OFFICE_AND_PROMOCODE: BuyerRequestBuyDto = {
-  ...MOCK_BUYER_REQUEST_BUY_DTO_WITH_CARRIER_OFFICE,
-  promocode: 'WALLA',
+export const MOCK_BUYER_REQUEST_BUY_DTO_WITH_BUYER_ADDRESS_AND_PROMOCODE: BuyerRequestBuyDto = {
+  ...MOCK_BUYER_REQUEST_BUY_DTO_WITH_BUYER_ADDRESS,
+  promocode: 'fakepromocode123321',
 };
--- src/tests/private/delivery/payview/payview-state.fixtures.spec.ts
+++ src/tests/private/delivery/payview/payview-state.fixtures.spec.ts
@@ -1,6 +1,7 @@
 import {
   MOCK_DELIVERY_BUYER_DELIVERY_METHODS,
   MOCK_DELIVERY_BUYER_DELIVERY_METHODS_WITHOUT_LASTADDRESSUSED,
+  MOCK_DELIVERY_BUYER_DELIVERY_METHODS_WITH_CARRIER_OFFICE_METHOD,
 } from '@api/fixtures/bff/delivery/buyer/delivery-buyer.fixtures.spec';
 import { MOCK_DELIVERY_COSTS_ITEM } from '@api/fixtures/bff/delivery/costs/delivery-costs.fixtures.spec';
 import {
@@ -11,7 +12,10 @@ import {
   MOCK_PAYMENTS_USER_PAYMENT_PREFERENCES_WITH_WALLET_AND_PAYPAL,
 } from '@api/fixtures/bff/payments/user-payment-preferences/payments-user-payment-preferences-dto.fixtures.spec';
 import { MOCK_DELIVERY_ADDRESS } from '@api/fixtures/delivery/address/delivery-address.fixtures.spec';
-import { MOCK_DELIVERY_BUYER_CALCULATOR_COSTS } from '@api/fixtures/delivery/buyer/delivery-buyer-calculator-costs-dto.fixtures.spec';
+import {
+  MOCK_DELIVERY_BUYER_CALCULATOR_COSTS,
+  MOCK_DELIVERY_BUYER_CALCULATOR_COSTS_WITH_PROMOTION,
+} from '@api/fixtures/delivery/buyer/delivery-buyer-calculator-costs-dto.fixtures.spec';
 import { MOCK_BUYER_REQUESTS_ITEMS_DETAILS_2 } from '@api/fixtures/delivery/buyer/requests/buyer-requests-items-details-dto.fixtures.spec';
 import { MOCK_CREDIT_CARD } from '@api/fixtures/payments/cards/credit-card.fixtures.spec';
 import { MOCK_PAYMENTS_PAYMENT_METHODS } from '@api/fixtures/payments/payment-methods/payments-payment-methods-dto.fixtures.spec';
@@ -34,6 +38,11 @@ export const MOCK_PAYVIEW_STATE: PayviewState = {
     preferences: MOCK_PAYMENTS_USER_PAYMENT_PREFERENCES,
     wallet: MOCK_PAYMENTS_WALLET_MAPPED_WITHOUT_MONEY,
   },
+};
+
+export const MOCK_PAYVIEW_STATE_WITH_PROMOCODE: PayviewState = {
+  ...MOCK_PAYVIEW_STATE,
+  costs: MOCK_DELIVERY_BUYER_CALCULATOR_COSTS_WITH_PROMOTION,
 };
 
 export const MOCK_PAYVIEW_STATE_WITHOUT_CREDIT_CARD: PayviewState = {
@@ -144,6 +153,23 @@ export const MOCK_PAYVIEW_STATE_WITHOUT_DELIVERY_ADDRESS: PayviewState = {
     address: null,
     costs: MOCK_DELIVERY_COSTS_ITEM,
     methods: MOCK_DELIVERY_BUYER_DELIVERY_METHODS,
+  },
+  item: MOCK_PAYVIEW_ITEM,
+  itemDetails: MOCK_BUYER_REQUESTS_ITEMS_DETAILS_2,
+  payment: {
+    card: MOCK_CREDIT_CARD,
+    methods: MOCK_PAYMENTS_PAYMENT_METHODS,
+    preferences: MOCK_PAYMENTS_USER_PAYMENT_PREFERENCES,
+    wallet: MOCK_PAYMENTS_WALLET_MAPPED_WITHOUT_MONEY,
+  },
+};
+
+export const MOCK_PAYVIEW_STATE_WITH_CARRIER_OFFICE_DELIVERY_METHOD: PayviewState = {
+  costs: MOCK_DELIVERY_BUYER_CALCULATOR_COSTS,
+  delivery: {
+    address: MOCK_DELIVERY_ADDRESS,
+    costs: MOCK_DELIVERY_COSTS_ITEM,
+    methods: MOCK_DELIVERY_BUYER_DELIVERY_METHODS_WITH_CARRIER_OFFICE_METHOD,
   },
   item: MOCK_PAYVIEW_ITEM,
   itemDetails: MOCK_BUYER_REQUESTS_ITEMS_DETAILS_2,
