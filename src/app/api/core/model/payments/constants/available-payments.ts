4670dc86027107877eb4cac476ff05c022c756c9
Laia Lopez <laialopezz99@gmail.com>
2022-04-04 17:37:22 +0200

    [WPA-12246]: [Comment] Change comment

--- src/app/api/core/model/payments/constants/available-payments.ts
+++ src/app/api/core/model/payments/constants/available-payments.ts
@@ -1,4 +1,4 @@
 import { PAYVIEW_PAYMENT_METHOD } from '@api/core/model/payments';
 
-// TODO: Add Paypal when needed		Date: 2022/04/04
+// TODO: Delete the file when all the payment methods are available		Date: 2022/04/04
 export const AVAILABLE_PAYMENT_METHODS = [PAYVIEW_PAYMENT_METHOD.CREDIT_CARD];
