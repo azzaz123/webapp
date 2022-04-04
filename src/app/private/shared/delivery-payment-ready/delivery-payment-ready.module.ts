191ce725d0356567f79ca73b4d43f5406586d391
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-04-04 17:35:32 +0200

    [WPA-12309]: Import web view module in delivery payment ready module

--- src/app/private/shared/delivery-payment-ready/delivery-payment-ready.module.ts
+++ src/app/private/shared/delivery-payment-ready/delivery-payment-ready.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
+import { WebViewModalModule } from '@shared/web-view-modal/web-view-modal.module';
 import { DeliveryPaymentReadyService } from './delivery-payment-ready.service';
 
 @NgModule({
+  imports: [WebViewModalModule],
   providers: [DeliveryPaymentReadyService],
 })
 export class DeliveryPaymentReadyModule {}
