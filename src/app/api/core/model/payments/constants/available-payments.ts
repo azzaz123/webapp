4bc8025329933a31e9da2e09880ac60b839a22ac
Laia Lopez <laialopezz99@gmail.com>
2022-04-04 15:19:03 +0200

    [WPA-12246]: [PaymentsUserPreference] control available

---
+++ src/app/api/core/model/payments/constants/available-payments.ts
@@ -0,0 +1,4 @@
+import { PAYVIEW_PAYMENT_METHOD } from '@api/core/model/payments';
+
+// TODO: Add Paypal when needed		Date: 2022/04/04
+export const AVAILABLE_PAYMENT_METHODS = [PAYVIEW_PAYMENT_METHOD.CREDIT_CARD];
