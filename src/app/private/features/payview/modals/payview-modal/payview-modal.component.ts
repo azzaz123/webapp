3cbb587c2a8c77843e7c8650023a0d19d3232ab7
Laia Lopez <laialopezz99@gmail.com>
2022-04-05 11:10:04 +0200

    [WPA-12399]: [Header] Dynamic title

--- src/app/private/features/payview/modals/payview-modal/payview-modal.component.ts
+++ src/app/private/features/payview/modals/payview-modal/payview-modal.component.ts
@@ -57,6 +57,13 @@ export class PayviewModalComponent implements OnDestroy, OnInit {
   private subscriptions: Subscription[] = [];
   private readonly trackViewTransactionPayScreen$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   private isMapPreviousPage$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  private headerTitles: Record<PAYVIEW_STEPS, string> = {
+    [PAYVIEW_STEPS.CREDIT_CARD]: $localize`:@@delivery_add_credit_card_edit_mode_title:Edit credit card`,
+    [PAYVIEW_STEPS.DELIVERY_ADDRESS]: $localize`:@@address_view_all_users_top_bar_edit_title:Edit your address`,
+    [PAYVIEW_STEPS.PAYVIEW]: $localize`:@@pay_view_buyer_top_bar_title:Make a purchase`,
+    [PAYVIEW_STEPS.PICK_UP_POINT_MAP]: $localize`:@@delivery_package_drop_selector_title:Select a point`,
+    [PAYVIEW_STEPS.PROMOTION_EDITOR]: $localize`:@@pay_view_buyer_top_bar_title:Make a purchase`,
+  };
 
   constructor(
     private payviewStateManagementService: PayviewStateManagementService,
@@ -133,6 +140,10 @@ export class PayviewModalComponent implements OnDestroy, OnInit {
 
   public get payviewState$(): Observable<PayviewState> {
     return this.payviewStateManagementService.payViewState$;
+  }
+
+  public get headerTitle(): string {
+    return this.headerTitles[this.stepper.activeId];
   }
 
   public trackCliCkHelpTransactionalEvent(): void {
