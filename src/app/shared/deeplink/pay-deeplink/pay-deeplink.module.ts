4a02f84140b783bba0cb75c68df6fdd22567af8c
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-04-11 21:50:42 +0200

    [WPA-12456]: Create pay deeplink module

---
+++ src/app/shared/deeplink/pay-deeplink/pay-deeplink.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { PayDeeplinkService } from './pay-deeplink.service';
+
+@NgModule({
+  providers: [PayDeeplinkService],
+})
+export class PayDeeplinkModule {}
