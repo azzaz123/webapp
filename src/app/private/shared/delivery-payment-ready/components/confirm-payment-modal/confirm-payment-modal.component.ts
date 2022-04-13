efc95ce812cabe71234c7f9141c2744d3c7c04bd
Carlos Roth <carlos@ulex.es>
2022-04-13 10:49:29 +0200

    [WPA-12827]: Create delivery-payment-ready modal

---
+++ src/app/private/shared/delivery-payment-ready/components/confirm-payment-modal/confirm-payment-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tsl-confirm-payment-modal',
+  templateUrl: './confirm-payment-modal.component.html',
+  styleUrls: ['./confirm-payment-modal.component.scss'],
+})
+export class ConfirmPaymentModalComponent {
+  constructor() {}
+}
--- src/app/private/shared/delivery-payment-ready/delivery-payment-ready.module.ts
+++ src/app/private/shared/delivery-payment-ready/delivery-payment-ready.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { WebViewModalModule } from '@shared/web-view-modal/web-view-modal.module';
+import { ConfirmPaymentModalModule } from './components/confirm-payment-modal/confirm-payment-modal.module';
 
 @NgModule({
-  imports: [WebViewModalModule],
+  imports: [WebViewModalModule, ConfirmPaymentModalModule],
 })
 export class DeliveryPaymentReadyModule {}
