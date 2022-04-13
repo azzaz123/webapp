4d1330f5cb95c2caded9bc7b57afaab82dff4a7b
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-04-13 14:39:05 +0200

    [WPA-12826]: Explicit type for transaction tracking status in mapper

--- src/app/api/bff/delivery/transaction-tracking/mappers/responses/details/is-paypal-payment-method/is-paypal-payment-method.mapper.ts
+++ src/app/api/bff/delivery/transaction-tracking/mappers/responses/details/is-paypal-payment-method/is-paypal-payment-method.mapper.ts
@@ -1,6 +1,6 @@
-import { TransactionTrackingDetails } from '@api/core/model/delivery/transaction/tracking';
+import { TransactionTrackingDetails, TransactionTrackingStatusInfo } from '@api/core/model/delivery/transaction/tracking';
 import { ToDomainMapper } from '@api/core/utils/types';
 
 export const mapTransactionTrackingDescriptionToIsPayPalPaymentMethod: ToDomainMapper<TransactionTrackingDetails, boolean> = (
   details: TransactionTrackingDetails
-) => details.info.some((element) => element.description.toUpperCase().includes('PAYPAL'));
+) => details.info.some((element: TransactionTrackingStatusInfo) => element.description.toUpperCase().includes('PAYPAL'));
