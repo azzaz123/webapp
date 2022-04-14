d2c798fd0b8892f82791e91c27bb03c58c7ac3c3
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-04-14 11:27:57 +0200

    [WPA-12883]: Create continue to PayPal service

---
+++ src/app/private/shared/delivery-payment-ready/modules/continue-to-pay-pal.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ContinueToPayPalService } from './continue-to-pay-pal.service';
+
+describe('ContinueToPayPalService', () => {
+  let service: ContinueToPayPalService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ContinueToPayPalService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
