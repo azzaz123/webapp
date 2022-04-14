53148ab1df13c21f5c757bc413e8f6d1ffe52d71
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-04-14 11:30:38 +0200

    [WPA-12883]: Create continue to PayPal module

---
+++ src/app/private/shared/delivery-payment-ready/modules/continue-to-pay-pal/continue-to-pay-pal.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { ContinueToPayPalService } from './continue-to-pay-pal.service';
+
+@NgModule({
+  providers: [ContinueToPayPalService],
+})
+export class ContinueToPayPalModule {}
