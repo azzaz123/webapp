0bb70215518bf80253a66dc4602706da52d067a2
Lluís Rodríguez <lluis.rodriguez@wallapop.com>
2022-04-14 13:53:22 +0200

    [WPA-12883]: Import valid modules in delivery payment ready module

--- src/app/private/shared/delivery-payment-ready/delivery-payment-ready.module.ts
+++ src/app/private/shared/delivery-payment-ready/delivery-payment-ready.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { TransactionTrackingModule } from '@api/bff/delivery/transaction-tracking/transaction-tracking.module';
 import { WebViewModalModule } from '@shared/web-view-modal/web-view-modal.module';
 import { DeliveryPaymentReadyService } from './delivery-payment-ready.service';
-import { ContinueToPayPalModalModule } from './modules/continue-to-pay-pal/modals/continue-to-paypal/continue-to-paypal-modal.module';
+import { ContinueToPayPalModule } from './modules/continue-to-pay-pal/continue-to-pay-pal.module';
+import { ContinueWithCreditCardModule } from './modules/continue-with-credit-card/continue-with-credit-card.module';
 
 @NgModule({
-  imports: [WebViewModalModule, TransactionTrackingModule, ContinueToPayPalModalModule],
+  imports: [WebViewModalModule, TransactionTrackingModule, ContinueToPayPalModule, ContinueWithCreditCardModule],
   providers: [DeliveryPaymentReadyService],
 })
 export class DeliveryPaymentReadyModule {}
