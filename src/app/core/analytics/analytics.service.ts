b1f866368a2e2458ea3d92572f8f341a05369299
Alex Masnou <alex.masnou@wallapop.com>
2022-04-19 18:31:59 +0200

    [TRXAN-77]: Send user device type as user attribute

--- src/app/core/analytics/analytics.service.ts
+++ src/app/core/analytics/analytics.service.ts
@@ -92,6 +92,7 @@ export class AnalyticsService {
 
     if (mParticleUser) {
       mParticleUser.setUserAttribute('deviceId', this.deviceService.getDeviceId());
+      mParticleUser.setUserAttribute('deviceType', this.deviceService.getDeviceType());
     }
   }
 
